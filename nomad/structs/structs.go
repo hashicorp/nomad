ad1849353e7be8162787fe070eef12e04705d8ae
Armon Dadgar <armon.dadgar@gmail.com>
2015-06-06 00:41:03 +0200

    nomad: adding basic RPC interfaces

--- nomad/structs/structs.go
+++ nomad/structs/structs.go
@@ -2,8 +2,15 @@ package structs
 
 import (
 	"bytes"
+	"fmt"
+	"time"
 
 	"github.com/hashicorp/go-msgpack/codec"
+)
+
+var (
+	ErrNoLeader     = fmt.Errorf("No cluster leader")
+	ErrNoRegionPath = fmt.Errorf("No path to region")
 )
 
 type MessageType uint8
@@ -20,6 +27,69 @@ const (
 	// old servers to crash when the FSM attempts to process them.
 	IgnoreUnknownTypeFlag MessageType = 128
 )
+
+// RPCInfo is used to describe common information about query
+type RPCInfo interface {
+	RequestRegion() string
+	IsRead() bool
+	AllowStaleRead() bool
+}
+
+// QueryOptions is used to specify various flags for read queries
+type QueryOptions struct {
+	// The target region for this query
+	Region string
+
+	// If set, any follower can service the request. Results
+	// may be arbitrarily stale.
+	AllowStale bool
+}
+
+func (q QueryOptions) RequestRegion() string {
+	return q.Region
+}
+
+// QueryOption only applies to reads, so always true
+func (q QueryOptions) IsRead() bool {
+	return true
+}
+
+func (q QueryOptions) AllowStaleRead() bool {
+	return q.AllowStale
+}
+
+type WriteRequest struct {
+	Region string
+}
+
+func (w WriteRequest) RequestRegion() string {
+	// The target region for this request
+	return w.Region
+}
+
+// WriteRequest only applies to writes, always false
+func (w WriteRequest) IsRead() bool {
+	return false
+}
+
+func (w WriteRequest) AllowStaleRead() bool {
+	return false
+}
+
+// QueryMeta allows a query response to include potentially
+// useful metadata about a query
+type QueryMeta struct {
+	// This is the index associated with the read
+	Index uint64
+
+	// If AllowStale is used, this is time elapsed since
+	// last contact between the follower and leader. This
+	// can be used to gauge staleness.
+	LastContact time.Duration
+
+	// Used to indicate if there is a known leader node
+	KnownLeader bool
+}
 
 // msgpackHandle is a shared handle for encoding/decoding of structs
 var msgpackHandle = &codec.MsgpackHandle{}
