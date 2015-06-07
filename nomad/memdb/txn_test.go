284f4103783b820f08c4c8b32b8f82b3152846df
Armon Dadgar <armon.dadgar@gmail.com>
2015-06-07 13:18:50 -0700

    memdb: testin txn abort and commit

---
+++ nomad/memdb/txn_test.go
@@ -0,0 +1,38 @@
+package memdb
+
+import "testing"
+
+func testDB(t *testing.T) *MemDB {
+	db, err := NewMemDB(testValidSchema())
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	return db
+}
+
+func TestTxn_Read_AbortCommit(t *testing.T) {
+	db := testDB(t)
+	txn := db.Txn(false) // Readonly
+
+	txn.Abort()
+	txn.Abort()
+	txn.Commit()
+	txn.Commit()
+}
+
+func TestTxn_Write_AbortCommit(t *testing.T) {
+	db := testDB(t)
+	txn := db.Txn(true) // Write
+
+	txn.Abort()
+	txn.Abort()
+	txn.Commit()
+	txn.Commit()
+
+	txn = db.Txn(true) // Write
+
+	txn.Commit()
+	txn.Commit()
+	txn.Abort()
+	txn.Abort()
+}
