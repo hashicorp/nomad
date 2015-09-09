33e7d0cd2d5c8bd1ffe3f08765f223da95120e2d
Ryan Uber <ru@ryanuber.com>
2015-09-08 19:27:04 -0700

    api: add constraints generators

--- api/jobs.go
+++ api/jobs.go
@@ -82,6 +82,7 @@ type Job struct {
 	Priority          int
 	AllAtOnce         bool
 	Datacenters       []string
+	Constraints       []*Constraint
 	Meta              map[string]string
 	Status            string
 	StatusDescription string
@@ -95,4 +96,36 @@ type registerJobRequest struct {
 // registerJobResponse is used to deserialize a job response
 type registerJobResponse struct {
 	EvalID string
+}
+
+// Constraint is used to serialize a job placement constraint.
+type Constraint struct {
+	Hard    bool
+	LTarget string
+	RTarget string
+	Operand string
+	Weight  int
+}
+
+// HardConstraint is used to create a new hard constraint.
+func HardConstraint(left, operand, right string) *Constraint {
+	return constraint(left, operand, right, true, 0)
+}
+
+// SoftConstraint is used to create a new soft constraint. It
+// takes an additional weight parameter to allow balancing
+// multiple soft constraints amongst eachother.
+func SoftConstraint(left, operand, right string, weight int) *Constraint {
+	return constraint(left, operand, right, false, weight)
+}
+
+// constraint generates a new job placement constraint.
+func constraint(left, operand, right string, hard bool, weight int) *Constraint {
+	return &Constraint{
+		Hard:    hard,
+		LTarget: left,
+		RTarget: right,
+		Operand: operand,
+		Weight:  weight,
+	}
 }
