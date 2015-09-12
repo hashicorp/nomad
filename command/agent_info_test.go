8246db154de887b612fd03579becf5fcbb7dec21
Ryan Uber <ru@ryanuber.com>
2015-09-12 14:50:05 -0700

    command: cli tests

--- command/agent_info_test.go
+++ command/agent_info_test.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/mitchellh/cli"
@@ -11,7 +12,36 @@ func TestAgentInfoCommand_Implements(t *testing.T) {
 }
 
 func TestAgentInfoCommand_Run(t *testing.T) {
-	agent := testAgent(t)
+	agent, http, _, url := testAgent(t)
 	defer agent.Shutdown()
-	println("yay")
+	defer http.Shutdown()
+
+	ui := new(cli.MockUi)
+	cmd := &AgentInfoCommand{Ui: ui}
+
+	code := cmd.Run([]string{"-http-addr=" + url})
+	if code != 0 {
+		t.Fatalf("expected exit 0, got: %d %s", code)
+	}
+}
+
+func TestAgentInfoCommand_Fails(t *testing.T) {
+	ui := new(cli.MockUi)
+	cmd := &AgentInfoCommand{Ui: ui}
+
+	// Fails on misuse
+	if code := cmd.Run([]string{"some", "bad", "args"}); code != 1 {
+		t.Fatalf("expected exit code 1, got: %d", code)
+	}
+	if out := ui.ErrorWriter.String(); !strings.Contains(out, cmd.Help()) {
+		t.Fatalf("expected help output, got: %s", out)
+	}
+
+	// Fails on connection failure
+	if code := cmd.Run([]string{"-http-addr=nope"}); code != 1 {
+		t.Fatalf("expected exit code 1, got: %d", code)
+	}
+	if out := ui.ErrorWriter.String(); !strings.Contains(out, "Failed querying agent info") {
+		t.Fatalf("expected failed query error, got: %s", out)
+	}
 }
--- command/util_test.go
+++ command/util_test.go
@@ -6,7 +6,9 @@ import (
 	"sync/atomic"
 	"testing"
 
+	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/nomad/command/agent"
+	"github.com/hashicorp/nomad/testutil"
 )
 
 var offset uint64
@@ -29,8 +31,10 @@ func nextConfig() *agent.Config {
 	return conf
 }
 
-func testAgent(t *testing.T) (*agent.Agent, *agent.HTTPServer) {
+func testAgent(t *testing.T) (*agent.Agent, *agent.HTTPServer, *api.Client, string) {
+	// Make the agent
 	conf := nextConfig()
+	conf.DevMode = true
 	a, err := agent.NewAgent(conf, os.Stderr)
 	if err != nil {
 		t.Fatalf("err: %s", err)
@@ -40,5 +44,27 @@ func testAgent(t *testing.T) (*agent.Agent, *agent.HTTPServer) {
 		a.Shutdown()
 		t.Fatalf("err: %s", err)
 	}
-	return a, http
+	url := fmt.Sprintf("http://127.0.0.1:%d", conf.Ports.HTTP)
+
+	// Make a client
+	clientConf := api.DefaultConfig()
+	clientConf.URL = url
+	client, err := api.NewClient(clientConf)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	waitForLeader(t, client)
+	return a, http, client, url
+}
+
+func waitForLeader(t *testing.T, client *api.Client) {
+	testutil.WaitForResult(func() (bool, error) {
+		if _, err := client.Status().Leader(); err != nil {
+			return false, err
+		}
+		return true, nil
+	}, func(err error) {
+		t.Fatalf("timeout waiting for leader: %s", err)
+	})
 }
