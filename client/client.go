5529aab866fc379386aa74101e8c8285c836ae8a
Ryan Uber <ru@ryanuber.com>
2015-09-12 13:16:27 -0700

    client: skip init in dev mode

--- client/client.go
+++ client/client.go
@@ -131,6 +131,10 @@ func NewClient(cfg *config.Config) (*Client, error) {
 // init is used to initialize the client and perform any setup
 // needed before we begin starting its various components.
 func (c *Client) init() error {
+	if c.config.DevMode {
+		return nil
+	}
+
 	// Ensure the alloc dir exists
 	if err := os.MkdirAll(c.config.AllocDir, 0700); err != nil {
 		return fmt.Errorf("failed creating alloc dir: %s", err)
