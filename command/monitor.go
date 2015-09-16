a8b91603652441beba8c52be5e1893a147672c98
Ryan Uber <ru@ryanuber.com>
2015-09-16 14:45:21 -0700

    command: working on monitor

--- command/monitor.go
+++ command/monitor.go
@@ -32,13 +32,15 @@ func newMonitor(ui cli.Ui, client *api.Client) *monitor {
 	return &monitor{
 		ui:     ui,
 		client: client,
-		state:  new(monitorState),
+		state: &monitorState{
+			allocs: make(map[string]*allocState),
+		},
 	}
 }
 
 // output is used to write informational messages to the ui.
 func (m *monitor) output(msg string) {
-	m.ui.Output(fmt.Sprintf("%s %s", time.Now().Format(dateFmt), msg))
+	m.ui.Output(fmt.Sprintf("    %s %s", time.Now().Format(dateFmt), msg))
 }
 
 // monitorState is used to store the current "state of the world"
@@ -46,13 +48,22 @@ func (m *monitor) output(msg string) {
 type monitorState struct {
 	status string
 	nodeID string
+	allocs map[string]*allocState
 	wait   time.Duration
+}
+
+// allocState is used to track the state of an allocation
+type allocState struct {
+	group   string
+	node    string
+	desired string
+	client  string
 }
 
 // update is used to update our monitor with new state. It can be
 // called whether the passed information is new or not, and will
 // only dump update messages when state changes.
-func (m *monitor) update(eval *api.Evaluation) {
+func (m *monitor) update(eval *api.Evaluation, allocs []*api.AllocationListStub) {
 	m.Lock()
 	defer m.Unlock()
 
@@ -62,9 +73,31 @@ func (m *monitor) update(eval *api.Evaluation) {
 	update := &monitorState{
 		status: eval.Status,
 		nodeID: eval.NodeID,
+		allocs: make(map[string]*allocState),
 		wait:   eval.Wait,
 	}
+	for _, alloc := range allocs {
+		update.allocs[alloc.ID] = &allocState{
+			group:   alloc.TaskGroup,
+			node:    alloc.NodeID,
+			desired: alloc.DesiredStatus,
+			client:  alloc.ClientStatus,
+		}
+	}
 	defer func() { m.state = update }()
+
+	// Check the allocations
+	for allocID, alloc := range update.allocs {
+		if _, ok := existing.allocs[allocID]; !ok {
+			// Check if this is a failure indication allocation
+			if alloc.desired == structs.AllocDesiredStatusFailed {
+				m.output("Scheduling failed")
+			} else {
+				m.output(fmt.Sprintf("Allocated task group %q on node %q",
+					alloc.group, alloc.node))
+			}
+		}
+	}
 
 	// Check if the status changed
 	if existing.status != update.status {
@@ -91,6 +124,7 @@ func (m *monitor) update(eval *api.Evaluation) {
 // succeeded and exited successfully, or 1 if an error was encountered
 // or the eval status was returned as failed.
 func (m *monitor) monitor(evalID string) int {
+	m.ui.Info(fmt.Sprintf("Monitoring evaluation %q", evalID))
 	for {
 		// Check the current state of things
 		eval, _, err := m.client.Evaluations().Info(evalID, nil)
@@ -99,8 +133,29 @@ func (m *monitor) monitor(evalID string) int {
 			return 1
 		}
 
+		allocs, _, err := m.client.Evaluations().Allocations(evalID, nil)
+		if err != nil {
+			m.ui.Error(fmt.Sprintf("Error reading allocations: %s", err))
+			return 1
+		}
+
 		// Update the state
-		m.update(eval)
+		m.update(eval, allocs)
+
+		switch eval.Status {
+		case structs.EvalStatusComplete, structs.EvalStatusFailed:
+			m.ui.Info(fmt.Sprintf("Evaluation %q finished with status %q",
+				eval.ID, eval.Status))
+		default:
+			// Wait for the next update
+			time.Sleep(time.Second)
+			continue
+		}
+
+		// Monitor the next eval, if it exists.
+		if eval.NextEval != "" {
+			return m.monitor(eval.NextEval)
+		}
 
 		// Check if the eval is complete
 		switch eval.Status {
@@ -109,9 +164,6 @@ func (m *monitor) monitor(evalID string) int {
 		case structs.EvalStatusFailed:
 			return 1
 		}
-
-		// Wait for the next poll
-		time.Sleep(time.Second)
 	}
 
 	return 0
