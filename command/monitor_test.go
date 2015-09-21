3accfbb58e80887ea42f630f83a0e24396aaf27e
Ryan Uber <ru@ryanuber.com>
2015-09-21 12:31:06 -0700

    command: remove unnecessary alloc-status command

--- command/monitor_test.go
+++ command/monitor_test.go
@@ -262,3 +262,54 @@ func TestMonitor_Monitor(t *testing.T) {
 		t.Fatalf("missing final status\n\n%s", out)
 	}
 }
+
+func TestMonitor_DumpAllocStatus(t *testing.T) {
+	ui := new(cli.MockUi)
+
+	// Create an allocation and dump its status to the UI
+	alloc := &api.Allocation{
+		ID:           "alloc1",
+		TaskGroup:    "group1",
+		ClientStatus: structs.AllocClientStatusRunning,
+		Metrics: &api.AllocationMetric{
+			NodesEvaluated: 10,
+			NodesFiltered:  5,
+			NodesExhausted: 1,
+			DimensionExhausted: map[string]int{
+				"cpu": 1,
+			},
+			ConstraintFiltered: map[string]int{
+				"$attr.kernel.name = linux": 1,
+			},
+			ClassExhausted: map[string]int{
+				"web-large": 1,
+			},
+		},
+	}
+	dumpAllocStatus(ui, alloc)
+
+	// Check the output
+	out := ui.OutputWriter.String()
+	if !strings.Contains(out, "alloc1") {
+		t.Fatalf("missing alloc\n\n%s", out)
+	}
+	if !strings.Contains(out, structs.AllocClientStatusRunning) {
+		t.Fatalf("missing status\n\n%s", out)
+	}
+	if !strings.Contains(out, "5/10") {
+		t.Fatalf("missing filter stats\n\n%s", out)
+	}
+	if !strings.Contains(
+		out, `Constraint "$attr.kernel.name = linux" filtered 1 nodes`) {
+		t.Fatalf("missing constraint\n\n%s", out)
+	}
+	if !strings.Contains(out, "Resources exhausted on 1 nodes") {
+		t.Fatalf("missing resource exhaustion\n\n%s", out)
+	}
+	if !strings.Contains(out, `Class "web-large" exhausted on 1 nodes`) {
+		t.Fatalf("missing class exhaustion\n\n%s", out)
+	}
+	if !strings.Contains(out, `Dimension "cpu" exhausted on 1 nodes`) {
+		t.Fatalf("missing dimension exhaustion\n\n%s", out)
+	}
+}
