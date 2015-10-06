257be30096c64555c78b97698317743d2523786b
Alex Dadgar <alex.dadgar@gmail.com>
2015-10-06 10:51:18 -0700

    OS dependant build dependencies

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -5,6 +5,8 @@ GOOS=windows go get $DEP_ARGS github.com/StackExchange/wmi
 GOOS=windows go get $DEP_ARGS github.com/shirou/w32
 GOOS=linux go get $DEP_ARGS github.com/docker/docker/pkg/units
 GOOS=linux go get $DEP_ARGS github.com/docker/docker/pkg/mount
+GOOS=linux go get $DEP_ARGS github.com/opencontainers/runc/libcontainer/cgroups/fs
+GOOS=linux go get $DEP_ARGS github.com/opencontainers/runc/libcontainer/configs
 
 # Get the rest of the deps
 DEPS=$(go list -f '{{range .TestImports}}{{.}} {{end}}' ./...)
