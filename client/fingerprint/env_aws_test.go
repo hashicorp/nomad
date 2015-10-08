ae6829c4f6e74fcbc4aae1023ad9f900a31295fc
Alex Dadgar <alex.dadgar@gmail.com>
2015-10-08 12:49:43 -0700

    Fix AWS Networking Fingerprint tests failing when actually on AWS

--- client/fingerprint/env_aws_test.go
+++ client/fingerprint/env_aws_test.go
@@ -13,6 +13,7 @@ import (
 )
 
 func TestEnvAWSFingerprint_nonAws(t *testing.T) {
+	os.Setenv("AWS_ENV_URL", "http://127.0.0.1/latest/meta-data/")
 	f := NewEnvAWSFingerprint(testLogger())
 	node := &structs.Node{
 		Attributes: make(map[string]string),
@@ -199,6 +200,7 @@ func TestNetworkFingerprint_AWS(t *testing.T) {
 }
 
 func TestNetworkFingerprint_notAWS(t *testing.T) {
+	os.Setenv("AWS_ENV_URL", "http://127.0.0.1/latest/meta-data/")
 	f := NewEnvAWSFingerprint(testLogger())
 	node := &structs.Node{
 		Attributes: make(map[string]string),
