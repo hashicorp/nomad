c390a84e6b7e4629de8bd3bd9a0963d59c8a7f7e
Antoine POPINEAU <antoine.popineau@appscho.com>
2015-10-25 18:10:33 +0100

    Split test cases into separate functions.

--- helper/inet/inet_test.go
+++ helper/inet/inet_test.go
@@ -5,18 +5,19 @@ import (
 	"testing"
 )
 
-func TestAdvertiseFromSubnet(t *testing.T) {
-	// CIDR is out of range
+func TestAdvertiseFromSubnet_CidrOutOfRange(t *testing.T) {
 	if ad, err := AdvertiseIpFromSubnet("127.0.0.0/43:4648"); err == nil {
 		t.Fatalf("expected advertise subnet error, got: %s", ad)
 	}
+}
 
-	// IP subnet is out of range
+func TestAdvertiseFromSubnet_IpSubnetOutOfRange(t *testing.T) {
 	if ad, err := AdvertiseIpFromSubnet("333.0.0.0/8:4648"); err == nil {
 		t.Fatalf("expected advertise subnet error, got: %s", ad)
 	}
+}
 
-	// Regular unicast IP definition: should return untouched string
+func TestAdvertiseFromSubnet_ShouldPassthrough(t *testing.T) {
 	expected := "127.0.0.1:4648"
 	ad, err := AdvertiseIpFromSubnet(expected)
 	if err != nil {
@@ -25,9 +26,11 @@ func TestAdvertiseFromSubnet(t *testing.T) {
 	if !reflect.DeepEqual(ad, expected) {
 		t.Fatalf("expected to get %s, got %s", expected, ad)
 	}
+}
 
-	// Actual subnet definition: should succeed
-	ad, err = AdvertiseIpFromSubnet("127.0.0.0/8:4648")
+func TestAdvertiseFromSubnet_ShouldPass(t *testing.T) {
+	expected := "127.0.0.1:4648"
+  ad, err := AdvertiseIpFromSubnet("127.0.0.0/8:4648")
 	if err != nil {
 		t.Fatalf("expected success, got: %s", err)
 	}
