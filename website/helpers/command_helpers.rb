b422b8eaa1a1072716c4af79c7e3a7807ad9838b
Chris Bednarski <banzaimonkey@gmail.com>
2015-11-18 14:29:52 -0800

    Wrap literal strings in backticks so they don't turn into URLs

--- website/helpers/command_helpers.rb
+++ website/helpers/command_helpers.rb
@@ -2,8 +2,8 @@ module CommandHelpers
   # Returns the markdown text for the general options usage.
   def general_options_usage()
     <<EOF
-* `-address=<addr>`: The address of the Nomad server. Overrides the "NOMAD_ADDR"
-  environment variable if set. Defaults to "http://127.0.0.1:4646".
+* `-address=<addr>`: The address of the Nomad server. Overrides the `NOMAD_ADDR`
+  environment variable if set. Defaults to `http://127.0.0.1:4646`.
 EOF
   end
 end
