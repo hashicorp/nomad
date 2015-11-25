ef7388549d127b1ab76ec4a59dcd6d11e89d574f
Chris Hines <chines@comscore.com>
2015-11-25 14:05:08 -0500

    Eliminate special case in production code for embedding test binary into chroot.

--- client/driver/executor/exec_linux_test.go
+++ client/driver/executor/exec_linux_test.go
@@ -4,7 +4,13 @@ import (
 	"testing"
 
 	ctestutil "github.com/hashicorp/nomad/client/testutil"
+	"github.com/hashicorp/nomad/helper/testtask"
 )
+
+func init() {
+	// Add test binary to chroot during test run.
+	chrootEnv[testtask.Path()] = testtask.Path()
+}
 
 func TestExecutorLinux(t *testing.T) {
 	testExecutor(t, NewLinuxExecutor, ctestutil.ExecCompatible)
