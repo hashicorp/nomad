c2f81fe8452ad93e54ca02f620e2960aab51c4cf
Diptanu Choudhury <diptanuc@gmail.com>
2015-12-17 13:12:28 -0800

    Removed a broken dependency

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -3,7 +3,6 @@
 # First get the OS-specific packages
 GOOS=windows go get $DEP_ARGS github.com/StackExchange/wmi
 GOOS=windows go get $DEP_ARGS github.com/shirou/w32
-GOOS=linux go get $DEP_ARGS github.com/docker/docker/pkg/units
 GOOS=linux go get $DEP_ARGS github.com/docker/docker/pkg/mount
 GOOS=linux go get $DEP_ARGS github.com/opencontainers/runc/libcontainer/cgroups/fs
 GOOS=linux go get $DEP_ARGS github.com/opencontainers/runc/libcontainer/configs
