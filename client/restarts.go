b48a8e37625bed5fe1ea10885b93dcc0c391db16
Alex Dadgar <alex.dadgar@gmail.com>
2015-12-17 10:37:53 -0800

    Add jitter

--- client/restarts.go
+++ client/restarts.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/hashicorp/nomad/nomad/structs"
@@ -10,6 +11,7 @@ func newRestartTracker(policy *structs.RestartPolicy) *RestartTracker {
 	return &RestartTracker{
 		startTime: time.Now(),
 		policy:    policy,
+		rand:      rand.New(rand.NewSource(time.Now().Unix())),
 	}
 }
 
@@ -17,6 +19,7 @@ type RestartTracker struct {
 	count     int       // Current number of attempts.
 	startTime time.Time // When the interval began
 	policy    *structs.RestartPolicy
+	rand      *rand.Rand
 }
 
 func (r *RestartTracker) NextRestart(exitCode int) (bool, time.Duration) {
@@ -26,14 +29,14 @@ func (r *RestartTracker) NextRestart(exitCode int) (bool, time.Duration) {
 	if now.After(end) {
 		r.count = 0
 		r.startTime = now
-		return true, r.policy.Delay
+		return true, r.jitter()
 	}
 
 	r.count++
 
 	// If we are under the attempts, restart with delay.
 	if r.count <= r.policy.Attempts {
-		return r.shouldRestart(exitCode), r.policy.Delay
+		return r.shouldRestart(exitCode), r.jitter()
 	}
 
 	// Don't restart since mode is "fail"
@@ -48,7 +51,14 @@ func (r *RestartTracker) NextRestart(exitCode int) (bool, time.Duration) {
 // shouldRestart returns whether a restart should occur based on the exit code
 // and the RestartOnSuccess configuration.
 func (r *RestartTracker) shouldRestart(exitCode int) bool {
-	return exitCode != 0 || r.policy.RestartOnSuccess && exitCode == 0
+	return exitCode != 0 || r.policy.RestartOnSuccess
+}
+
+// jitter returns the delay time plus a jitter.
+func (r *RestartTracker) jitter() time.Duration {
+	d := r.policy.Delay.Nanoseconds()
+	j := r.rand.Int63n(d) / 4 // Up to 25% jitter
+	return time.Duration(d + j)
 }
 
 // Returns a tracker that never restarts.
