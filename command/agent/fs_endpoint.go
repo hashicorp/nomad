f7c367397f9e6bb7c58dd3793ac840a6874e3f0a
Diptanu Choudhury <diptanuc@gmail.com>
2016-01-12 22:25:12 -0800

    Refactored the code

--- command/agent/fs_endpoint.go
+++ command/agent/fs_endpoint.go
@@ -8,69 +8,51 @@ import (
 )
 
 func (s *HTTPServer) DirectoryListRequest(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
-	allocID := strings.TrimPrefix(req.URL.Path, "/v1/client/fs/ls/")
-	path := req.URL.Query().Get("path")
-	if path == "" {
-		path = "/"
-	}
-	if allocID == "" {
-		resp.WriteHeader(http.StatusNotFound)
+	var allocID, path string
+
+	if allocID = strings.TrimPrefix(req.URL.Path, "/v1/client/fs/ls/"); allocID == "" {
 		return nil, fmt.Errorf("alloc id not found")
 	}
-	files, err := s.agent.client.FSList(allocID, path)
-	if err != nil {
-		return nil, err
+	if path = req.URL.Query().Get("path"); path == "" {
+		path = "/"
 	}
-	return files, nil
+	return s.agent.client.FSList(allocID, path)
 }
 
 func (s *HTTPServer) FileStatRequest(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
-	allocID := strings.TrimPrefix(req.URL.Path, "/v1/client/fs/stat/")
-	path := req.URL.Query().Get("path")
-	if path == "" {
-		resp.WriteHeader(http.StatusNotFound)
-		return nil, fmt.Errorf("must provide a file name")
-	}
-	if allocID == "" {
-		resp.WriteHeader(http.StatusNotFound)
+	var allocID, path string
+
+	if allocID = strings.TrimPrefix(req.URL.Path, "/v1/client/fs/stat/"); allocID == "" {
 		return nil, fmt.Errorf("alloc id not found")
 	}
-	fileInfo, err := s.agent.client.FSStat(allocID, path)
-	if err != nil {
-		return nil, err
+	if path := req.URL.Query().Get("path"); path == "" {
+		return nil, fmt.Errorf("must provide a file name")
 	}
-	return fileInfo, nil
+	return s.agent.client.FSStat(allocID, path)
 }
 
 func (s *HTTPServer) FileReadAtRequest(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
-	allocID := strings.TrimPrefix(req.URL.Path, "/v1/client/fs/readat/")
-	path := req.URL.Query().Get("path")
-	ofs := req.URL.Query().Get("offset")
-	if ofs == "" {
-		ofs = "0"
-	}
+	var allocID, path string
+	var offset, limit int64
+	var err error
 
-	offset, err := strconv.ParseInt(ofs, 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing offset: %v", err)
-	}
-	lim := req.URL.Query().Get("limit")
-	limit, err := strconv.ParseInt(lim, 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing limit: %v", err)
-	}
+	q := req.URL.Query()
 
-	if path == "" {
-		resp.WriteHeader(http.StatusNotFound)
+	if allocID = strings.TrimPrefix(req.URL.Path, "/v1/client/fs/readat/"); allocID == "" {
+		return nil, fmt.Errorf("alloc id not found")
+	}
+	if path = q.Get("path"); path == "" {
 		return nil, fmt.Errorf("must provide a file name")
 	}
-	if allocID == "" {
-		resp.WriteHeader(http.StatusNotFound)
-		return nil, fmt.Errorf("alloc id not found")
+
+	if offset, err = strconv.ParseInt(q.Get("offset"), 10, 64); err != nil {
+		return nil, fmt.Errorf("error parsing offset: %v", err)
+	}
+	if limit, err = strconv.ParseInt(q.Get("limit"), 10, 64); err != nil {
+		return nil, fmt.Errorf("error parsing limit: %v", err)
 	}
 	if err = s.agent.client.FSReadAt(allocID, path, offset, limit, resp); err != nil {
 		return nil, err
 	}
 	return nil, nil
-
 }
