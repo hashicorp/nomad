8636fb61f7b8177b7f2d3391c5ff7f503f069aae
Diptanu Choudhury <diptanuc@gmail.com>
2016-01-13 11:37:29 -0800

    Added a test for the readat api endpoint

--- command/agent/fs_endpoint_test.go
+++ command/agent/fs_endpoint_test.go
@@ -3,6 +3,7 @@ package agent
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -30,7 +31,7 @@ func TestHTTP_FSStat(t *testing.T) {
 		respW := httptest.NewRecorder()
 
 		_, err = s.Server.FileStatRequest(respW, req)
-		if err == nil {
+		if !strings.HasPrefix(err.Error(), "alloc id not found") {
 			t.Fatal("expected error")
 		}
 
@@ -41,6 +42,43 @@ func TestHTTP_FSStat(t *testing.T) {
 		respW = httptest.NewRecorder()
 
 		_, err = s.Server.FileStatRequest(respW, req)
+		if !strings.HasPrefix(err.Error(), "must provide a file name") {
+			t.Fatal("expected error")
+		}
+	})
+}
+
+func TestHTTP_FSReadAt(t *testing.T) {
+	httpTest(t, nil, func(s *TestServer) {
+		req, err := http.NewRequest("GET", "/v1/client/fs/readat/", nil)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+		respW := httptest.NewRecorder()
+
+		_, err = s.Server.FileReadAtRequest(respW, req)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+
+		req, err = http.NewRequest("GET", "/v1/client/fs/readat/foo", nil)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+		respW = httptest.NewRecorder()
+
+		_, err = s.Server.FileReadAtRequest(respW, req)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+
+		req, err = http.NewRequest("GET", "/v1/client/fs/readat/foo?path=/path/to/file", nil)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+		respW = httptest.NewRecorder()
+
+		_, err = s.Server.FileReadAtRequest(respW, req)
 		if err == nil {
 			t.Fatal("expected error")
 		}
