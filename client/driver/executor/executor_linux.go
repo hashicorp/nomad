4a243e78d3b459f54e63b2b8eac88c1ec5fbd6ff
Alex Dadgar <alex.dadgar@gmail.com>
2016-03-02 16:32:31 -0800

    Add /sbin and /usr/sbin/ to chroot

--- client/driver/executor/executor_linux.go
+++ client/driver/executor/executor_linux.go
@@ -27,7 +27,9 @@ var (
 		"/lib":       "/lib",
 		"/lib32":     "/lib32",
 		"/lib64":     "/lib64",
+		"/sbin":      "/sbin",
 		"/usr/bin":   "/usr/bin",
+		"/usr/sbin":  "/usr/sbin",
 		"/usr/lib":   "/usr/lib",
 		"/usr/share": "/usr/share",
 	}
