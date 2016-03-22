9544291833777475e1e756f1ebe2790014f3409b
Alex Dadgar <alex.dadgar@gmail.com>
2016-03-21 18:16:57 -0700

    Fix indentation of init file

--- command/init.go
+++ command/init.go
@@ -163,16 +163,16 @@ job "example" {
 			# artifacts prior to the task being started. This is convenient for
 			# shipping configs or data needed by the task.
 			# artifact {
-			#   source = "http://foo.com/artifact.tar.gz"
-			#   options {
-			#     checksum = "md5:c4aa853ad2215426eb7d70a21922e794"
-			#   }
+			#	  source = "http://foo.com/artifact.tar.gz"
+			#	  options {
+			#	      checksum = "md5:c4aa853ad2215426eb7d70a21922e794"
+			#     }
 			# }
 			
 			# Specify configuration related to log rotation
 			# logs {
-			#	max_files = 10
-			#	max_file_size = 15
+			#     max_files = 10
+			#	  max_file_size = 15
 			# }
 			 
 			# Controls the timeout between signalling a task it will be killed
