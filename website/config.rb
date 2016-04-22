496a8eb9b14245d908c6a6e0137a8839889537dc
Diptanu Choudhury <diptanuc@gmail.com>
2016-04-22 16:56:35 -0700

    Bumping up the version of the website

--- website/config.rb
+++ website/config.rb
@@ -2,6 +2,6 @@ set :base_url, "https://www.nomadproject.io/"
 
 activate :hashicorp do |h|
   h.name        = "nomad"
-  h.version     = "0.3.1"
+  h.version     = "0.3.2"
   h.github_slug = "hashicorp/nomad"
 end
