16b0ad4263fbf566f9daf55e5b1c171bf8ebab6c
Sean Chittenden <sean@chittenden.org>
2016-05-07 12:37:15 -0700

    Break out the windows vs UNIX Fingerprinter
    cgroup's are applicable to Windows and will require a more specific abstraction.  Stub out the difference.  The *NIX exec drivers will likely be broken out over time (e.g. *BSD and Solaris).

--- client/driver/exec.go
+++ client/driver/exec.go
@@ -9,8 +9,6 @@ import (
 	"strings"
 	"time"
 
-	"golang.org/x/sys/unix"
-
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/nomad/client/allocdir"
@@ -80,33 +78,6 @@ func (d *ExecDriver) Validate(config map[string]interface{}) error {
 	}
 
 	return nil
-}
-
-func (d *ExecDriver) Fingerprint(cfg *config.Config, node *structs.Node) (bool, error) {
-	// Get the current status so that we can log any debug messages only if the
-	// state changes
-	_, currentlyEnabled := node.Attributes[execDriverAttr]
-
-	// Only enable if cgroups are available and we are root
-	if _, ok := node.Attributes["unique.cgroup.mountpoint"]; !ok {
-		if currentlyEnabled {
-			d.logger.Printf("[DEBUG] driver.exec: cgroups unavailable, disabling")
-		}
-		delete(node.Attributes, execDriverAttr)
-		return false, nil
-	} else if unix.Geteuid() != 0 {
-		if currentlyEnabled {
-			d.logger.Printf("[DEBUG] driver.exec: must run as root user, disabling")
-		}
-		delete(node.Attributes, execDriverAttr)
-		return false, nil
-	}
-
-	if !currentlyEnabled {
-		d.logger.Printf("[DEBUG] driver.exec: exec driver is enabled")
-	}
-	node.Attributes[execDriverAttr] = "1"
-	return true, nil
 }
 
 func (d *ExecDriver) Periodic() (bool, time.Duration) {
