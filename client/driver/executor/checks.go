b53183a332793fb96fa85a18cbc8f5b8a39872b6
Sean Chittenden <sean@chittenden.org>
2016-05-08 00:13:28 -0700

    Revert making UniversalExecutor.wait() platform specific

--- client/driver/executor/checks.go
+++ client/driver/executor/checks.go
@@ -1,8 +1,11 @@
 package executor
 
 import (
+	"fmt"
 	"log"
+	"os/exec"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/armon/circbuf"
@@ -147,6 +150,51 @@ func (e *ExecScriptCheck) ID() string {
 // Interval returns the interval at which the check has to run
 func (e *ExecScriptCheck) Interval() time.Duration {
 	return e.interval
+}
+
+// Run runs an exec script check
+func (e *ExecScriptCheck) Run() *cstructs.CheckResult {
+	buf, _ := circbuf.NewBuffer(int64(cstructs.CheckBufSize))
+	cmd := exec.Command(e.cmd, e.args...)
+	cmd.Stdout = buf
+	cmd.Stderr = buf
+	e.setChroot(cmd)
+	ts := time.Now()
+	if err := cmd.Start(); err != nil {
+		return &cstructs.CheckResult{Err: err}
+	}
+	errCh := make(chan error, 2)
+	go func() {
+		errCh <- cmd.Wait()
+	}()
+	for {
+		select {
+		case err := <-errCh:
+			endTime := time.Now()
+			if err == nil {
+				return &cstructs.CheckResult{
+					ExitCode:  0,
+					Output:    string(buf.Bytes()),
+					Timestamp: ts,
+				}
+			}
+			exitCode := 1
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+					exitCode = status.ExitStatus()
+				}
+			}
+			return &cstructs.CheckResult{
+				ExitCode:  exitCode,
+				Output:    string(buf.Bytes()),
+				Timestamp: ts,
+				Duration:  endTime.Sub(ts),
+			}
+		case <-time.After(e.Timeout()):
+			errCh <- fmt.Errorf("timed out after waiting 30s")
+		}
+	}
+	return nil
 }
 
 // Timeout returns the duration after which a check is timed out.
