a91f41ce1430618c3d8bc8452e1ec40139f312f9
Sean Chittenden <sean@chittenden.org>
2016-05-09 12:22:40 -0700

    Establish a floor of one core for the number of cores.
    In most cases the upstream library [shirou/gopsutil](https://github.com/shirou/gopsutil)
    needs to be fixed.

--- client/fingerprint/cpu.go
+++ client/fingerprint/cpu.go
@@ -51,6 +51,12 @@ func (f *CPUFingerprint) Fingerprint(cfg *config.Config, node *structs.Node) (bo
 		f.logger.Printf("[DEBUG] fingerprint.cpu: frequency: %02.1fMHz", mhz)
 	}
 
+	if numCores <= 0 {
+		const defaultCPUCoreCount = 1
+		f.logger.Printf("[DEBUG] fingerprint.cpu: unable to find core count, defaulting to %d", defaultCPUCoreCount)
+		numCores = defaultCPUCoreCount
+	}
+
 	if numCores > 0 {
 		node.Attributes["cpu.numcores"] = fmt.Sprintf("%d", numCores)
 		f.logger.Printf("[DEBUG] fingerprint.cpu: core count: %d", numCores)
