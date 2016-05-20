3f336f46ad7287527a8f45ec03a24505e4803363
Diptanu Choudhury <diptanuc@gmail.com>
2016-05-20 13:01:09 -0700

    Added missing vendored dependencies

--- vendor/github.com/docker/docker/pkg/system/meminfo_unsupported.go
+++ vendor/github.com/docker/docker/pkg/system/meminfo_unsupported.go
@@ -1,3 +1,4 @@
+<<<<<<< df68129e5afd485c281a9b7a3cd36d3ed32ffd83
 // +build !linux,!windows,!solaris
 
 package system
@@ -6,3 +7,14 @@ package system
 func ReadMemInfo() (*MemInfo, error) {
 	return nil, ErrNotSupportedPlatform
 }
+||||||| merged common ancestors
+=======
+// +build !linux,!windows
+
+package system
+
+// ReadMemInfo is not supported on platforms other than linux and windows.
+func ReadMemInfo() (*MemInfo, error) {
+	return nil, ErrNotSupportedPlatform
+}
+>>>>>>> Added missing vendored dependencies
