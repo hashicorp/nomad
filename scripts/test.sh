e00d4cae6a1e5b8c9c1db6f5687e2f305a1cc915
Sean Chittenden <sean@chittenden.org>
2016-07-11 12:41:14 -0700

    Add debugging to figure out why Travis can't find `go(1)` when sudo'ing but xargs(1) could.

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,4 +10,9 @@ go build -o $TEMPDIR/nomad || exit 1
 
 # Run the tests
 echo "--> Running tests"
-go list ./... | grep -v '/vendor/' | sudo -E PATH=$TEMPDIR:$PATH go test ${GOTEST_FLAGS:--cover -timeout=900s -v}
+printf "go(1) path: %s\n" "`which go`"
+go list ./... | grep -v '/vendor/' | \
+    sudo \
+        -E PATH=$TEMPDIR:$PATH \
+        -E GOPATH=$GOPATH \
+        go test ${GOTEST_FLAGS:--cover -timeout=900s -v}
