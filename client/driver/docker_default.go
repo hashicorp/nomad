d5ae1cb93ed0968c33ff8d39dffffe344d18ef4e
Michal Wieczorek <wieczorek-michal@wp.pl>
2016-08-04 23:11:06 +0200

    Set windows containers default network mode to 'nat'

--- client/driver/docker_default.go
+++ client/driver/docker_default.go
@@ -4,6 +4,11 @@ package driver
 
 import docker "github.com/fsouza/go-dockerclient"
 
+const (
+	//Setting default network mode for non-windows OS as bridge
+	defaultNetworkMode = "bridge"
+)
+
 func getPortBinding(ip string, port string) []docker.PortBinding {
 	return []docker.PortBinding{docker.PortBinding{HostIP: ip, HostPort: port}}
 }
