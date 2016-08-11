5b707f7e719edf52382e728269c16962ef0fac0d
Ivo Verberk <iverberk@users.noreply.github.com>
2016-08-11 09:20:53 +0200

    Disk resource monitoring and enforcement

--- command/agent/fs_endpoint_test.go
+++ command/agent/fs_endpoint_test.go
@@ -16,6 +16,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/nomad/client/allocdir"
+	"github.com/hashicorp/nomad/nomad/structs"
 	"github.com/hashicorp/nomad/testutil"
 	"github.com/ugorji/go/codec"
 )
@@ -448,7 +449,7 @@ func tempAllocDir(t *testing.T) *allocdir.AllocDir {
 		t.Fatalf("failed to chmod dir: %v", err)
 	}
 
-	return allocdir.NewAllocDir(dir)
+	return allocdir.NewAllocDir(dir, int64(structs.DefaultResources().DiskMB))
 }
 
 type nopWriteCloser struct {
