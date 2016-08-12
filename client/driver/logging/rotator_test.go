ddfae93941bb50e00f6084650ef4087807c2f635
Alex Dadgar <alex.dadgar@gmail.com>
2016-08-11 19:23:03 -0700

    Wrap file rotator tests in wait for

--- client/driver/logging/rotator_test.go
+++ client/driver/logging/rotator_test.go
@@ -1,12 +1,14 @@
 package logging
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"testing"
-	"time"
+
+	"github.com/hashicorp/nomad/testutil"
 )
 
 var (
@@ -85,14 +87,22 @@ func TestFileRotator_WriteToCurrentFile(t *testing.T) {
 	}
 
 	fr.Write([]byte("abcde"))
-	time.Sleep(200 * time.Millisecond)
-	fi, err := os.Stat(fname1)
-	if err != nil {
-		t.Fatalf("error getting the file info: %v", err)
-	}
-	if fi.Size() != 5 {
-		t.Fatalf("expected size: %v, actual: %v", 5, fi.Size())
-	}
+
+	var actual int64
+	testutil.WaitForResult(func() (bool, error) {
+		fi, err := os.Stat(fname1)
+		if err != nil {
+			return false, err
+		}
+		actual = fi.Size()
+		if actual != 5 {
+			return false, nil
+		}
+
+		return true, nil
+	}, func(err error) {
+		t.Fatalf("expected size: %v, actual: %v", 5, actual)
+	})
 }
 
 func TestFileRotator_RotateFiles(t *testing.T) {
@@ -110,34 +120,47 @@ func TestFileRotator_RotateFiles(t *testing.T) {
 
 	str := "abcdefgh"
 	nw, err := fr.Write([]byte(str))
-	time.Sleep(200 * time.Millisecond)
 	if err != nil {
 		t.Fatalf("got error while writing: %v", err)
 	}
+
 	if nw != len(str) {
 		t.Fatalf("expected %v, got %v", len(str), nw)
 	}
-	fname1 := filepath.Join(path, "redis.stdout.0")
-	fi, err := os.Stat(fname1)
-	if err != nil {
-		t.Fatalf("error getting the file info: %v", err)
-	}
-	if fi.Size() != 5 {
-		t.Fatalf("expected size: %v, actual: %v", 5, fi.Size())
-	}
 
-	fname2 := filepath.Join(path, "redis.stdout.1")
-	if _, err := os.Stat(fname2); err != nil {
-		t.Fatalf("expected file %v to exist", fname2)
-	}
-
-	if fi2, err := os.Stat(fname2); err == nil {
-		if fi2.Size() != 3 {
-			t.Fatalf("expected size: %v, actual: %v", 3, fi2.Size())
+	var lastErr error
+	testutil.WaitForResult(func() (bool, error) {
+		fname1 := filepath.Join(path, "redis.stdout.0")
+		fi, err := os.Stat(fname1)
+		if err != nil {
+			lastErr = err
+			return false, nil
 		}
-	} else {
-		t.Fatalf("error getting the file info: %v", err)
-	}
+		if fi.Size() != 5 {
+			lastErr = fmt.Errorf("expected size: %v, actual: %v", 5, fi.Size())
+			return false, nil
+		}
+
+		fname2 := filepath.Join(path, "redis.stdout.1")
+		if _, err := os.Stat(fname2); err != nil {
+			lastErr = fmt.Errorf("expected file %v to exist", fname2)
+			return false, nil
+		}
+
+		if fi2, err := os.Stat(fname2); err == nil {
+			if fi2.Size() != 3 {
+				lastErr = fmt.Errorf("expected size: %v, actual: %v", 3, fi2.Size())
+				return false, nil
+			}
+		} else {
+			lastErr = fmt.Errorf("error getting the file info: %v", err)
+			return false, nil
+		}
+
+		return true, nil
+	}, func(err error) {
+		t.Fatalf("%v", lastErr)
+	})
 }
 
 func TestFileRotator_WriteRemaining(t *testing.T) {
@@ -165,43 +188,57 @@ func TestFileRotator_WriteRemaining(t *testing.T) {
 	if err != nil {
 		t.Fatalf("got error while writing: %v", err)
 	}
-	time.Sleep(200 * time.Millisecond)
 	if nw != len(str) {
 		t.Fatalf("expected %v, got %v", len(str), nw)
 	}
-	fi, err := os.Stat(fname1)
-	if err != nil {
-		t.Fatalf("error getting the file info: %v", err)
-	}
-	if fi.Size() != 5 {
-		t.Fatalf("expected size: %v, actual: %v", 5, fi.Size())
-	}
-
-	fname2 := filepath.Join(path, "redis.stdout.1")
-	if _, err := os.Stat(fname2); err != nil {
-		t.Fatalf("expected file %v to exist", fname2)
-	}
-
-	if fi2, err := os.Stat(fname2); err == nil {
-		if fi2.Size() != 5 {
-			t.Fatalf("expected size: %v, actual: %v", 5, fi2.Size())
+	var lastErr error
+	testutil.WaitForResult(func() (bool, error) {
+		fi, err := os.Stat(fname1)
+		if err != nil {
+			lastErr = fmt.Errorf("error getting the file info: %v", err)
+			return false, nil
 		}
-	} else {
-		t.Fatalf("error getting the file info: %v", err)
-	}
-
-	fname3 := filepath.Join(path, "redis.stdout.2")
-	if _, err := os.Stat(fname3); err != nil {
-		t.Fatalf("expected file %v to exist", fname3)
-	}
-
-	if fi3, err := os.Stat(fname3); err == nil {
-		if fi3.Size() != 2 {
-			t.Fatalf("expected size: %v, actual: %v", 2, fi3.Size())
+		if fi.Size() != 5 {
+			lastErr = fmt.Errorf("expected size: %v, actual: %v", 5, fi.Size())
+			return false, nil
 		}
-	} else {
-		t.Fatalf("error getting the file info: %v", err)
-	}
+
+		fname2 := filepath.Join(path, "redis.stdout.1")
+		if _, err := os.Stat(fname2); err != nil {
+			lastErr = fmt.Errorf("expected file %v to exist", fname2)
+			return false, nil
+		}
+
+		if fi2, err := os.Stat(fname2); err == nil {
+			if fi2.Size() != 5 {
+				lastErr = fmt.Errorf("expected size: %v, actual: %v", 5, fi2.Size())
+				return false, nil
+			}
+		} else {
+			lastErr = fmt.Errorf("error getting the file info: %v", err)
+			return false, nil
+		}
+
+		fname3 := filepath.Join(path, "redis.stdout.2")
+		if _, err := os.Stat(fname3); err != nil {
+			lastErr = fmt.Errorf("expected file %v to exist", fname3)
+			return false, nil
+		}
+
+		if fi3, err := os.Stat(fname3); err == nil {
+			if fi3.Size() != 2 {
+				lastErr = fmt.Errorf("expected size: %v, actual: %v", 2, fi3.Size())
+				return false, nil
+			}
+		} else {
+			lastErr = fmt.Errorf("error getting the file info: %v", err)
+			return false, nil
+		}
+
+		return true, nil
+	}, func(err error) {
+		t.Fatalf("%v", lastErr)
+	})
 
 }
 
@@ -227,13 +264,21 @@ func TestFileRotator_PurgeOldFiles(t *testing.T) {
 		t.Fatalf("expected %v, got %v", len(str), nw)
 	}
 
-	time.Sleep(1 * time.Second)
-	f, err := ioutil.ReadDir(path)
-	if err != nil {
-		t.Fatalf("test error: %v", err)
-	}
+	var lastErr error
+	testutil.WaitForResult(func() (bool, error) {
+		f, err := ioutil.ReadDir(path)
+		if err != nil {
+			lastErr = fmt.Errorf("test error: %v", err)
+			return false, nil
+		}
 
-	if len(f) != 2 {
-		t.Fatalf("expected number of files: %v, got: %v", 2, len(f))
-	}
+		if len(f) != 2 {
+			lastErr = fmt.Errorf("expected number of files: %v, got: %v", 2, len(f))
+			return false, nil
+		}
+
+		return true, nil
+	}, func(err error) {
+		t.Fatalf("%v", lastErr)
+	})
 }
