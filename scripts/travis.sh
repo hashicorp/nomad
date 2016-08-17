1d94641692dede90ce258621217cbfd0d069d717
Diptanu Choudhury <diptanuc@gmail.com>
2016-08-17 15:18:37 -0700

    Changed build script

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,6 +4,8 @@ export PING_SLEEP=30
 bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
+trap "kill $PING_LOOP_PID" EXIT HUP INT QUIT TERM
+
 make test
 TEST_OUTPUT=$?
 
