6705ab8e09940974e0aa764cb037e4d60639e617
Diptanu Choudhury <diptanuc@gmail.com>
2016-10-03 11:27:12 -0700

    Added the keygen command

--- commands.go
+++ commands.go
@@ -79,6 +79,11 @@ func Commands(metaPtr *command.Meta) map[string]cli.CommandFactory {
 				Meta: meta,
 			}, nil
 		},
+		"keygen": func() (cli.Command, error) {
+			return &command.KeygenCommand{
+				Meta: meta,
+			}, nil
+		},
 		"logs": func() (cli.Command, error) {
 			return &command.LogsCommand{
 				Meta: meta,
