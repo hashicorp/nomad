aeb410c07c77704ef941ae12b9fb63b06ddf85a7
Michael Schurter <schmichael@hashicorp.com>
2016-10-26 14:55:54 -0700

    Put lxc support behind a flag
    Since lxc support requires linking to a C lib at compile and runtime
    I'm putting it behind a build flag to avoid forcing all nomad users to
    install liblxc (lxc-dev for development).

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,11 +7,11 @@ trap "rm -rf $TEMPDIR" EXIT HUP INT QUIT TERM
 
 # Build the Nomad binary for the API tests
 echo "--> Building nomad"
-go build -tags "nomad_test" -o $TEMPDIR/nomad || exit 1
+go build -tags "nomad_test lxc" -o $TEMPDIR/nomad || exit 1
 
 # Run the tests
 echo "--> Running tests"
 GOBIN="`which go`"
 sudo -E PATH=$TEMPDIR:$PATH  -E GOPATH=$GOPATH   -E NOMAD_TEST_RKT=1 \
-    $GOBIN test -tags "nomad_test" ${GOTEST_FLAGS:--cover -timeout=900s} $($GOBIN list ./... | grep -v /vendor/)
+    $GOBIN test -tags "nomad_test lxc" ${GOTEST_FLAGS:--cover -timeout=900s} $($GOBIN list ./... | grep -v /vendor/)
 
