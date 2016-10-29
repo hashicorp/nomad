40d0a4e074b15ee056c5d5cc73d4a338cd19db72
Seth Vargo <sethvargo@gmail.com>
2016-10-29 16:44:48 -0400

    Move helper into config (faster loading)

--- website/config.rb
+++ website/config.rb
@@ -29,6 +29,17 @@ helpers do
     return escape_html(page.data.description || "")
   end
 
+  # This helps by setting the "active" class for sidebar nav elements
+  # if the YAML frontmatter matches the expected value.
+  def sidebar_current(expected)
+    current = current_page.data.sidebar_current || ""
+    if current.start_with?(expected)
+      return " class=\"active\""
+    else
+      return ""
+    end
+  end
+
   # Returns the id for this page.
   # @return [String]
   def body_id_for(page)
