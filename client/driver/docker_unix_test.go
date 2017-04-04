1dbd63ad2a135893c4d444fa0b8c16c9527a213d
Alex Dadgar <alex.dadgar@gmail.com>
2017-04-04 15:12:14 -0700

    Split driver tests

---
+++ client/driver/docker_unix_test.go
@@ -0,0 +1,105 @@
+// +build !windows
+
+package driver
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/nomad/client/testutil"
+	"github.com/hashicorp/nomad/nomad/structs"
+	tu "github.com/hashicorp/nomad/testutil"
+)
+
+func TestDockerDriver_Signal(t *testing.T) {
+	if !testutil.DockerIsConnected(t) {
+		t.SkipNow()
+	}
+
+	task := &structs.Task{
+		Name:   "redis-demo",
+		Driver: "docker",
+		Config: map[string]interface{}{
+			"image":   "busybox",
+			"load":    "busybox.tar",
+			"command": "/bin/sh",
+			"args":    []string{"local/test.sh"},
+		},
+		Resources: &structs.Resources{
+			MemoryMB: 256,
+			CPU:      512,
+		},
+		LogConfig: &structs.LogConfig{
+			MaxFiles:      10,
+			MaxFileSizeMB: 10,
+		},
+	}
+
+	ctx := testDockerDriverContexts(t, task)
+	//ctx.DriverCtx.config.Options = map[string]string{"docker.cleanup.image": "false"}
+	defer ctx.AllocDir.Destroy()
+	d := NewDockerDriver(ctx.DriverCtx)
+
+	// Copy the image into the task's directory
+	copyImage(t, ctx.ExecCtx.TaskDir, "busybox.tar")
+
+	testFile := filepath.Join(ctx.ExecCtx.TaskDir.LocalDir, "test.sh")
+	testData := []byte(`
+at_term() {
+    echo 'Terminated.'
+    exit 3
+}
+trap at_term USR1
+while true; do
+    sleep 1
+done
+	`)
+	if err := ioutil.WriteFile(testFile, testData, 0777); err != nil {
+		t.Fatalf("Failed to write data: %v", err)
+	}
+
+	_, err := d.Prestart(ctx.ExecCtx, task)
+	if err != nil {
+		t.Fatalf("error in prestart: %v", err)
+	}
+	handle, err := d.Start(ctx.ExecCtx, task)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if handle == nil {
+		t.Fatalf("missing handle")
+	}
+	defer handle.Kill()
+
+	waitForExist(t, handle.(*DockerHandle).client, handle.(*DockerHandle))
+
+	time.Sleep(1 * time.Second)
+	if err := handle.Signal(syscall.SIGUSR1); err != nil {
+		t.Fatalf("Signal returned an error: %v", err)
+	}
+
+	select {
+	case res := <-handle.WaitCh():
+		if res.Successful() {
+			t.Fatalf("should err: %v", res)
+		}
+	case <-time.After(time.Duration(tu.TestMultiplier()*5) * time.Second):
+		t.Fatalf("timeout")
+	}
+
+	// Check the log file to see it exited because of the signal
+	outputFile := filepath.Join(ctx.ExecCtx.TaskDir.LogDir, "redis-demo.stdout.0")
+	act, err := ioutil.ReadFile(outputFile)
+	if err != nil {
+		t.Fatalf("Couldn't read expected output: %v", err)
+	}
+
+	exp := "Terminated."
+	if strings.TrimSpace(string(act)) != exp {
+		t.Fatalf("Command outputted %v; want %v", act, exp)
+	}
+}
--- client/driver/raw_exec_test.go
+++ client/driver/raw_exec_test.go
@@ -6,7 +6,6 @@ import (
 	"path/filepath"
 	"reflect"
 	"strings"
-	"syscall"
 	"testing"
 	"time"
 
@@ -297,83 +296,5 @@ func TestRawExecDriverUser(t *testing.T) {
 	msg := "unknown user alice"
 	if !strings.Contains(err.Error(), msg) {
 		t.Fatalf("Expecting '%v' in '%v'", msg, err)
-	}
-}
-
-func TestRawExecDriver_Signal(t *testing.T) {
-	task := &structs.Task{
-		Name:   "signal",
-		Driver: "raw_exec",
-		Config: map[string]interface{}{
-			"command": "/bin/bash",
-			"args":    []string{"test.sh"},
-		},
-		LogConfig: &structs.LogConfig{
-			MaxFiles:      10,
-			MaxFileSizeMB: 10,
-		},
-		Resources:   basicResources,
-		KillTimeout: 10 * time.Second,
-	}
-
-	ctx := testDriverContexts(t, task)
-	defer ctx.AllocDir.Destroy()
-	d := NewRawExecDriver(ctx.DriverCtx)
-
-	testFile := filepath.Join(ctx.ExecCtx.TaskDir.Dir, "test.sh")
-	testData := []byte(`
-at_term() {
-    echo 'Terminated.'
-    exit 3
-}
-trap at_term USR1
-while true; do
-    sleep 1
-done
-	`)
-	if err := ioutil.WriteFile(testFile, testData, 0777); err != nil {
-		t.Fatalf("Failed to write data: %v", err)
-	}
-
-	if _, err := d.Prestart(ctx.ExecCtx, task); err != nil {
-		t.Fatalf("prestart err: %v", err)
-	}
-	handle, err := d.Start(ctx.ExecCtx, task)
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	if handle == nil {
-		t.Fatalf("missing handle")
-	}
-
-	go func() {
-		time.Sleep(100 * time.Millisecond)
-		err := handle.Signal(syscall.SIGUSR1)
-		if err != nil {
-			t.Fatalf("err: %v", err)
-		}
-	}()
-
-	// Task should terminate quickly
-	select {
-	case res := <-handle.WaitCh():
-		if res.Successful() {
-			t.Fatal("should err")
-		}
-	case <-time.After(time.Duration(testutil.TestMultiplier()*6) * time.Second):
-		t.Fatalf("timeout")
-	}
-
-	// Check the log file to see it exited because of the signal
-	outputFile := filepath.Join(ctx.ExecCtx.TaskDir.LogDir, "signal.stdout.0")
-	act, err := ioutil.ReadFile(outputFile)
-	if err != nil {
-		t.Fatalf("Couldn't read expected output: %v", err)
-	}
-
-	exp := "Terminated."
-	if strings.TrimSpace(string(act)) != exp {
-		t.Logf("Read from %v", outputFile)
-		t.Fatalf("Command outputted %v; want %v", act, exp)
 	}
 }
