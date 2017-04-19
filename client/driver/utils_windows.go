fb3b30bba98140aeb72fe7a5ff8e413a644caac1
Michael Schurter <schmichael@hashicorp.com>
2017-04-19 13:16:48 -0700

    Fix Windows build.

--- client/driver/utils_windows.go
+++ client/driver/utils_windows.go
@@ -7,3 +7,7 @@ import (
 // TODO Figure out if this is needed in Wondows
 func isolateCommand(cmd *exec.Cmd) {
 }
+
+// setChroot is a noop on Windows
+func setChroot(cmd *exec.Cmd, chroot string) {
+}
