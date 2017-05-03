e07235e883c0b8e89b4355121232640973801f53
Michael Schurter <schmichael@hashicorp.com>
2017-05-03 15:18:48 -0700

    Adding logging for Travis

--- command/agent/http_test.go
+++ command/agent/http_test.go
@@ -440,9 +440,10 @@ func TestHTTP_VerifyHTTPSClient(t *testing.T) {
 	// PASS: Requests that specify a valid hostname, CA cert, and client
 	// certificate succeed.
 	tlsConf.GetClientCertificate = func(*tls.CertificateRequestInfo) (*tls.Certificate, error) {
+		s.Agent.logger.Printf("Loading certificate")
 		c, err := tls.LoadX509KeyPair(foocert, fookey)
 		if err != nil {
-			t.Logf("error loading client certificate: %v", err)
+			s.Agent.logger.Printf("error loading client certificate: %v", err)
 			return nil, err
 		}
 		return &c, nil
