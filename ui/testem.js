534ceaaea6f6413e81ec4a6340d18a2f6a98e09c
Michael Lange <dingoeatingfuzz@gmail.com>
2017-06-07 18:35:09 -0700

    Initialize the ember app

---
+++ ui/testem.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "PhantomJS"
+  ],
+  "launch_in_dev": [
+    "PhantomJS",
+    "Chrome"
+  ]
+};
