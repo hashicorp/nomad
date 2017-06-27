bba5645049b395f712aea256761d924390ee8d68
Alex Dadgar <alex.dadgar@gmail.com>
2017-06-27 11:15:07 -0700

    Deployments list

--- nomad/deployment_endpoint_test.go
+++ nomad/deployment_endpoint_test.go
@@ -10,6 +10,127 @@ import (
 	"github.com/hashicorp/nomad/testutil"
 )
 
+func TestDeploymentEndpoint_List(t *testing.T) {
+	s1 := testServer(t, nil)
+	defer s1.Shutdown()
+	codec := rpcClient(t, s1)
+	testutil.WaitForLeader(t, s1.RPC)
+
+	// Create the register request
+	deployment := mock.Deployment()
+	state := s1.fsm.State()
+
+	if err := state.UpsertDeployment(1000, deployment, false); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Lookup the deployments
+	get := &structs.DeploymentListRequest{
+		QueryOptions: structs.QueryOptions{Region: "global"},
+	}
+	var resp structs.DeploymentListResponse
+	if err := msgpackrpc.CallWithCodec(codec, "Deployment.List", get, &resp); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if resp.Index != 1000 {
+		t.Fatalf("Bad index: %d %d", resp.Index, 1000)
+	}
+
+	if len(resp.Deployments) != 1 {
+		t.Fatalf("bad: %#v", resp.Deployments)
+	}
+	if resp.Deployments[0].ID != deployment.ID {
+		t.Fatalf("bad: %#v", resp.Deployments[0])
+	}
+
+	// Lookup the deploys by prefix
+	get = &structs.DeploymentListRequest{
+		QueryOptions: structs.QueryOptions{Region: "global", Prefix: deployment.ID[:4]},
+	}
+
+	var resp2 structs.DeploymentListResponse
+	if err := msgpackrpc.CallWithCodec(codec, "Deployment.List", get, &resp2); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if resp2.Index != 1000 {
+		t.Fatalf("Bad index: %d %d", resp2.Index, 1000)
+	}
+
+	if len(resp2.Deployments) != 1 {
+		t.Fatalf("bad: %#v", resp2.Deployments)
+	}
+	if resp2.Deployments[0].ID != deployment.ID {
+		t.Fatalf("bad: %#v", resp2.Deployments[0])
+	}
+}
+
+func TestDeploymentEndpoint_List_Blocking(t *testing.T) {
+	s1 := testServer(t, nil)
+	defer s1.Shutdown()
+	state := s1.fsm.State()
+	codec := rpcClient(t, s1)
+	testutil.WaitForLeader(t, s1.RPC)
+
+	// Create the deployment
+	deployment := mock.Deployment()
+
+	// Upsert alloc triggers watches
+	time.AfterFunc(100*time.Millisecond, func() {
+		if err := state.UpsertDeployment(3, deployment, false); err != nil {
+			t.Fatalf("err: %v", err)
+		}
+	})
+
+	req := &structs.DeploymentListRequest{
+		QueryOptions: structs.QueryOptions{
+			Region:        "global",
+			MinQueryIndex: 1,
+		},
+	}
+	start := time.Now()
+	var resp structs.DeploymentListResponse
+	if err := msgpackrpc.CallWithCodec(codec, "Deployment.List", req, &resp); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	if elapsed := time.Since(start); elapsed < 100*time.Millisecond {
+		t.Fatalf("should block (returned in %s) %#v", elapsed, resp)
+	}
+	if resp.Index != 3 {
+		t.Fatalf("Bad index: %d %d", resp.Index, 3)
+	}
+	if len(resp.Deployments) != 1 || resp.Deployments[0].ID != deployment.ID {
+		t.Fatalf("bad: %#v", resp.Deployments)
+	}
+
+	// Deployment updates trigger watches
+	deployment2 := deployment.Copy()
+	deployment2.Status = structs.DeploymentStatusPaused
+	time.AfterFunc(100*time.Millisecond, func() {
+		if err := state.UpsertDeployment(5, deployment2, false); err != nil {
+			t.Fatalf("err: %v", err)
+		}
+	})
+
+	req.MinQueryIndex = 3
+	start = time.Now()
+	var resp2 structs.DeploymentListResponse
+	if err := msgpackrpc.CallWithCodec(codec, "Deployment.List", req, &resp2); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	if elapsed := time.Since(start); elapsed < 100*time.Millisecond {
+		t.Fatalf("should block (returned in %s) %#v", elapsed, resp2)
+	}
+	if resp2.Index != 5 {
+		t.Fatalf("Bad index: %d %d", resp2.Index, 5)
+	}
+	if len(resp2.Deployments) != 1 || resp.Deployments[0].ID != deployment2.ID ||
+		resp2.Deployments[0].Status != structs.DeploymentStatusPaused {
+		t.Fatalf("bad: %#v", resp2.Deployments)
+	}
+}
+
 func TestDeploymentEndpoint_Allocations(t *testing.T) {
 	s1 := testServer(t, nil)
 	defer s1.Shutdown()
