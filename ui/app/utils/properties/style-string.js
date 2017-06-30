bf14b6c12ec97e8e0fd7e2fab9c969db0c0b64a5
Michael Lange <dingoeatingfuzz@gmail.com>
2017-06-29 17:35:34 -0700

    Finishes job detail page

--- ui/app/utils/properties/style-string.js
+++ ui/app/utils/properties/style-string.js
@@ -4,6 +4,9 @@ const { computed } = Ember;
 
 // An Ember.Computed property for transforming an object into an
 // html compatible style attribute
+//
+// ex. styleProps: { color: '#FF0', border-width: '1px' }
+//     styleStr: styleStringProperty('styleProps') // color:#FF0;border-width:1px
 export default function styleStringProperty(prop) {
   return computed(prop, function() {
     const styles = this.get(prop);
@@ -13,9 +16,7 @@ export default function styleStringProperty(prop) {
       str = Object.keys(styles)
         .reduce(function(arr, key) {
           const val = styles[key];
-          arr.push(
-            key + ':' + (typeof val === 'number' ? val.toFixed(2) + 'px' : val)
-          );
+          arr.push(key + ':' + (typeof val === 'number' ? val.toFixed(2) + 'px' : val));
           return arr;
         }, [])
         .join(';');
