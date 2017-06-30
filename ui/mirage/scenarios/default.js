8da03ff3603cf8ce6f1d0432588e4b3e361a9545
Michael Lange <dingoeatingfuzz@gmail.com>
2017-06-30 16:03:21 -0700

    Add mirage support for tasks and task groups

--- ui/mirage/scenarios/default.js
+++ ui/mirage/scenarios/default.js
@@ -1,11 +1,3 @@
 export default function(server) {
-  /*
-    Seed your development database using your factories.
-    This data will not be loaded in your tests.
-
-    Make sure to define a factory for each model you want to create.
-  */
-
-  // server.createList('post', 10);
   server.createList('job', 10);
 }
