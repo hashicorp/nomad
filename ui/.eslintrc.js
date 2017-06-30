dbb4f160c29c2128f6a075fbe0f8de1a06404d3e
Michael Lange <dingoeatingfuzz@gmail.com>
2017-06-30 16:02:23 -0700

    Add avoidEscape rule for eslint
    Prettier does this by default, so eslint needs to match. It's also
    just a good rule.

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
+    quotes: ['error', 'single', 'avoid-escape'],
     semi: ['error', 'always'],
   },
 };
