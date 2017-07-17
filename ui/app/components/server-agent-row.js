dd548aa99e85376d95f34a2d28440e805a0655bb
Michael Lange <dingoeatingfuzz@gmail.com>
2017-07-17 14:57:23 -0700

    Show active server selection
    The best approach meant enabling the router service feature

--- ui/app/components/server-agent-row.js
+++ ui/app/components/server-agent-row.js
@@ -1,12 +1,22 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, inject, computed } = Ember;
 
 export default Component.extend({
+  router: inject.service(),
+
   tagName: 'tr',
-  classNames: ['server-agent-row'],
-  classNameBindings: ['isActive'],
+  classNames: ['server-agent-row', 'is-interactive'],
+  classNameBindings: ['isActive:is-active'],
 
   agent: null,
-  isActive: false,
+  isActive: computed('agent', 'router.currentURL', function() {
+    const targetURL = this.get('router').urlFor('nodes.servers.server', this.get('agent'));
+    const currentURL = `${this.get('router.rootURL').slice(0, -1)}${this.get('router.currentURL')}`;
+    return currentURL === targetURL;
+  }),
+
+  click() {
+    this.get('router').transitionTo('nodes.servers.server', this.get('agent'));
+  },
 });
--- ui/config/environment.js
+++ ui/config/environment.js
@@ -10,6 +10,7 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+        'ember-routing-router-service': true,
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
