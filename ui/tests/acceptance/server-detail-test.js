eb480d2a27a0068bb92616d268ad1f0cf2d3834c
Michael Lange <dingoeatingfuzz@gmail.com>
2017-07-18 13:14:53 -0700

    Test coverage for the server detail page

---
+++ ui/tests/acceptance/server-detail-test.js
@@ -0,0 +1,40 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'nomad-ui/tests/helpers/module-for-acceptance';
+
+let agent;
+
+moduleForAcceptance('Acceptance | server detail', {
+  beforeEach() {
+    server.createList('agent', 3);
+    agent = server.db.agents[0];
+    visit(`/nodes/servers/${agent.name}`);
+  },
+});
+
+test('visiting /nodes/servers/:server_name', function(assert) {
+  assert.equal(currentURL(), `/nodes/servers/${agent.name}`);
+});
+
+test('the server detail page should list all tags for the server', function(assert) {
+  const tags = agent.tags;
+
+  assert.equal(find('.server-tags tbody tr').length, Object.keys(tags).length, '# of tags');
+  Object.keys(tags).forEach((key, index) => {
+    const row = find(`.server-tags tbody tr:eq(${index})`);
+    assert.equal(row.find('td:eq(0)').text(), key, `Label: ${key}`);
+    assert.equal(row.find('td:eq(1)').text(), tags[key], `Value: ${tags[key]}`);
+  });
+});
+
+test('the list of servers from /nodes/servers should still be present', function(assert) {
+  assert.equal(find('.server-agent-row').length, server.db.agents.length, '# of servers');
+});
+
+test('the active server should be denoted in the table', function(assert) {
+  assert.equal(find('.server-agent-row.is-active').length, 1, 'Only one active server');
+  assert.equal(
+    find('.server-agent-row.is-active td:eq(0)').text(),
+    agent.name,
+    'Active server matches current route'
+  );
+});
