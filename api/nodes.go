8494af781be189a2af3b3db2c709355edd6ad8bb
Karel Malec <km@karelmalec.cz>
2017-07-21 03:57:10 +0200

    One more formatting

--- api/nodes.go
+++ api/nodes.go
@@ -19,10 +19,10 @@ func (c *Client) Nodes() *Nodes {
 // Looks up Node Name by Node ID, returns N/A in case of failure
 func (c *Client) GetNodeNameByID(NodeID string) string {
 	node, _, err := c.Nodes().Info(NodeID, nil)
-		if err != nil {
-			return "N/A"
-		} else {
-			return node.Name
+	if err != nil {
+		return "N/A"
+	} else {
+		return node.Name
 	}
 }
 
