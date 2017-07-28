eea5b98af05a9fec9cfd1271845274aba7391b7f
Michael Lange <dingoeatingfuzz@gmail.com>
2017-07-27 19:07:25 -0700

    Make allocation stats accessible from an allocation
    This is extra weird since the URL needs to be constructed using the
    node's http address. It was achieved in a way that sidesteps ED. That
    may change in the future.

--- ui/app/models/allocation.js
+++ ui/app/models/allocation.js
@@ -3,8 +3,11 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 import { fragment, fragmentArray } from 'ember-data-model-fragments/attributes';
+import fetch from 'fetch';
+import PromiseObject from '../utils/classes/promise-object';
+import timeout from '../utils/timeout';
 
-const { computed } = Ember;
+const { computed, RSVP } = Ember;
 
 export default Model.extend({
   job: belongsTo('job'),
@@ -15,6 +18,24 @@ export default Model.extend({
 
   taskGroup: computed('taskGroupName', 'job.taskGroups.[]', function() {
     return this.get('job.taskGroups').findBy('name', this.get('taskGroupName'));
+  }),
+
+  stats: computed('node.{isPartial,httpAddr}', function() {
+    const nodeIsPartial = this.get('node.isPartial');
+
+    // If the node doesn't have an httpAddr, it's a partial record.
+    // Once it reloads, this property will be dirtied and stats will load.
+    if (nodeIsPartial) {
+      return PromiseObject.create({
+        // Never resolve, so the promise object is always in a state of pending
+        promise: new RSVP.Promise(() => {}),
+      });
+    }
+
+    const url = `//${this.get('node.httpAddr')}/v1/client/allocation/${this.get('id')}/stats`;
+    return PromiseObject.create({
+      promise: RSVP.Promise.race([fetch(url).then(res => res.json()), timeout(2000)]),
+    });
   }),
 
   states: fragmentArray('task-state'),
