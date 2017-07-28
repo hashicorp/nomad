3c85ef78ae0f8dfb6db801eaf63abdaee126ad40
Michael Lange <dingoeatingfuzz@gmail.com>
2017-07-27 19:03:26 -0700

    Mirage modelig for client and client alloc stats
    Weird things needed to be done to model data that comes from dynamic
    hosts.

--- ui/mirage/common.js
+++ ui/mirage/common.js
@@ -5,3 +5,8 @@ export const DATACENTERS = provide(
   15,
   (n, i) => `${faker.address.countryCode().toLowerCase()}${i}`
 );
+
+export const HOSTS = provide(
+  100,
+  () => `${faker.internet.ip()}:${faker.random.number({ min: 4000, max: 4999 })}`
+);
