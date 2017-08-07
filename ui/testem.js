d12a4816bd1b49d65f4ef363449995726c578b99
Michael Lange <dingoeatingfuzz@gmail.com>
2017-08-07 11:16:19 -0700

    Simple pagination component

--- ui/testem.js
+++ ui/testem.js
@@ -3,5 +3,5 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: ['PhantomJS'],
-  launch_in_dev: ['PhantomJS', 'Chrome'],
+  launch_in_dev: ['Chrome'],
 };
