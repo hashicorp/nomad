8eb765bc1c5ce314b518c1cf3bbf382f8d36a7a6
Armon Dadgar <armon.dadgar@gmail.com>
2017-08-07 15:01:25 -0700

    nomad: Upsert and Delete ACL policies can take a list

--- nomad/state/state_store_test.go
+++ nomad/state/state_store_test.go
@@ -5719,14 +5719,8 @@ func TestStateStore_UpsertACLPolicy(t *testing.T) {
 		t.Fatalf("err: %v", err)
 	}
 
-	if err := state.UpsertACLPolicy(1000, policy); err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	if !watchFired(ws) {
-		t.Fatalf("bad")
-	}
-
-	if err := state.UpsertACLPolicy(1001, policy2); err != nil {
+	if err := state.UpsertACLPolicies(1000,
+		[]*structs.ACLPolicy{policy, policy2}); err != nil {
 		t.Fatalf("err: %v", err)
 	}
 	if !watchFired(ws) {
@@ -5776,9 +5770,11 @@ func TestStateStore_UpsertACLPolicy(t *testing.T) {
 func TestStateStore_DeleteACLPolicy(t *testing.T) {
 	state := testStateStore(t)
 	policy := mock.ACLPolicy()
+	policy2 := mock.ACLPolicy()
 
 	// Create the policy
-	if err := state.UpsertACLPolicy(1000, policy); err != nil {
+	if err := state.UpsertACLPolicies(1000,
+		[]*structs.ACLPolicy{policy, policy2}); err != nil {
 		t.Fatalf("err: %v", err)
 	}
 
@@ -5789,7 +5785,8 @@ func TestStateStore_DeleteACLPolicy(t *testing.T) {
 	}
 
 	// Delete the policy
-	if err := state.DeleteACLPolicy(1001, policy.Name); err != nil {
+	if err := state.DeleteACLPolicies(1001,
+		[]string{policy.Name, policy2.Name}); err != nil {
 		t.Fatalf("err: %v", err)
 	}
 
