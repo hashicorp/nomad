1368fd290240860a0c03674fb02be4b955da89ef
Michael Lange <dingoeatingfuzz@gmail.com>
2017-08-10 18:02:17 -0700

    Simple search box component for encapsulating debounce logic

---
+++ ui/app/components/search-box.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+const { Component, computed, run } = Ember;
+
+export default Component.extend({
+  // Passed to the component (mutable)
+  searchTerm: null,
+
+  // Used as a debounce buffer
+  _searchTerm: computed.reads('searchTerm'),
+
+  // Used to throttle sets to searchTerm
+  debounce: 150,
+
+  actions: {
+    setSearchTerm(e) {
+      this.set('_searchTerm', e.target.value);
+      run.debounce(this, updateSearch, this.get('debounce'));
+    },
+  },
+});
+
+function updateSearch() {
+  this.set('searchTerm', this.get('_searchTerm'));
+}
