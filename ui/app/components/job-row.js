161e34b94052d3f61ed332d70ef62841ad176b3f
Michael Lange <dingoeatingfuzz@gmail.com>
2017-08-18 15:56:25 -0700

    Move common allocation distribution bar chart into specialized component

--- ui/app/components/job-row.js
+++ ui/app/components/job-row.js
@@ -1,43 +1,12 @@
 import Ember from 'ember';
 
-const { Component, computed } = Ember;
+const { Component } = Ember;
 
 export default Component.extend({
   tagName: 'tr',
   classNames: ['job-row', 'is-interactive'],
 
   job: null,
-
-  allocDistribution: computed(
-    'job.{queuedAllocs,completeAllocs,failedAllocs,runningAllocs,startingAllocs}',
-    function() {
-      const allocs = this.get('job').getProperties(
-        'queuedAllocs',
-        'completeAllocs',
-        'failedAllocs',
-        'runningAllocs',
-        'startingAllocs',
-        'lostAllocs'
-      );
-      return [
-        { label: 'Queued', value: allocs.queuedAllocs, className: 'queued' },
-        {
-          label: 'Starting',
-          value: allocs.startingAllocs,
-          className: 'starting',
-          layers: 2,
-        },
-        { label: 'Running', value: allocs.runningAllocs, className: 'running' },
-        {
-          label: 'Complete',
-          value: allocs.completeAllocs,
-          className: 'complete',
-        },
-        { label: 'Failed', value: allocs.failedAllocs, className: 'failed' },
-        { label: 'Lost', value: allocs.lostAllocs, className: 'lost' },
-      ];
-    }
-  ),
 
   didReceiveAttrs() {
     // Reload the job in order to get detail information
--- ui/app/components/task-group-row.js
+++ ui/app/components/task-group-row.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { Component, computed } = Ember;
+const { Component } = Ember;
 
 export default Component.extend({
   tagName: 'tr',
@@ -8,26 +8,4 @@ export default Component.extend({
   classNames: ['task-group-row'],
 
   taskGroup: null,
-
-  allocDistribution: computed(
-    'taskGroup.summary.{queuedAllocs,completeAllocs,failedAllocs,runningAllocs,startingAllocs}',
-    function() {
-      const allocs = this.get('taskGroup.summary').getProperties(
-        'queuedAllocs',
-        'completeAllocs',
-        'failedAllocs',
-        'runningAllocs',
-        'startingAllocs',
-        'lostAllocs'
-      );
-      return [
-        { label: 'Queued', value: allocs.queuedAllocs, className: 'queued' },
-        { label: 'Starting', value: allocs.startingAllocs, className: 'starting', layers: 2 },
-        { label: 'Running', value: allocs.runningAllocs, className: 'running' },
-        { label: 'Complete', value: allocs.completeAllocs, className: 'complete' },
-        { label: 'Failed', value: allocs.failedAllocs, className: 'failed' },
-        { label: 'Lost', value: allocs.lostAllocs, className: 'lost' },
-      ];
-    }
-  ),
 });
