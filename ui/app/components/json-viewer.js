4c3743343a05779d2d417df1a5bc1eb1fd37cc37
Michael Lange <dingoeatingfuzz@gmail.com>
2017-08-24 14:56:47 -0700

    Simple component to wrap the json viewer lib

---
+++ ui/app/components/json-viewer.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+import JSONFormatterPkg from 'npm:json-formatter-js';
+
+const { Component, computed, run } = Ember;
+
+// json-formatter-js is packaged in a funny way that ember-cli-browserify
+// doesn't unwrap properly.
+const { default: JSONFormatter } = JSONFormatterPkg;
+
+export default Component.extend({
+  json: null,
+  expandDepth: 2,
+
+  formatter: computed('json', 'expandDepth', function() {
+    return new JSONFormatter(this.get('json'), this.get('expandDepth'), {
+      theme: 'nomad',
+    });
+  }),
+
+  didReceiveAttrs() {
+    const json = this.get('json');
+    if (!json) {
+      return;
+    }
+
+    run.scheduleOnce('afterRender', this, embedViewer);
+  },
+});
+
+function embedViewer() {
+  this.$().empty().append(this.get('formatter').render());
+}
