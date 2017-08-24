e70eb4cc7b7ddc46830f234a716a9f0bd7616732
Michael Lange <dingoeatingfuzz@gmail.com>
2017-08-24 14:57:30 -0700

    Add the definition page to the navigation structures

---
+++ ui/app/controllers/jobs/job/definition.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+const { Controller, computed, inject } = Ember;
+
+export default Controller.extend({
+  jobController: inject.controller('jobs.job'),
+
+  breadcrumbs: computed.alias('jobController.breadcrumbs'),
+});
