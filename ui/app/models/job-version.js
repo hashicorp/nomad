533f28347f8ce6ce3e461dbfee8a5ebd1bd74564
Michael Lange <dingoeatingfuzz@gmail.com>
2017-08-29 11:11:38 -0700

    Initial modeling of job versions

---
+++ ui/app/models/job-version.js
@@ -0,0 +1,10 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  job: belongsTo('job'),
+  stable: attr('boolean'),
+  submitTime: attr('date'),
+  diff: attr(),
+});
