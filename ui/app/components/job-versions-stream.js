8a8b758206a6400ad550a18ca0806d0f6cc45256
Michael Lange <dingoeatingfuzz@gmail.com>
2017-08-30 13:53:47 -0700

    Styles the job versions page with new timeline design

--- ui/app/components/job-versions-stream.js
+++ ui/app/components/job-versions-stream.js
@@ -4,6 +4,9 @@ import moment from 'moment';
 const { Component, computed } = Ember;
 
 export default Component.extend({
+  tagName: 'ol',
+  classNames: ['timeline'],
+
   versions: computed(() => []),
 
   annotatedVersions: computed('versions.[]', function() {
@@ -16,10 +19,9 @@ export default Component.extend({
       } else {
         const previousVersion = versions.objectAt(index - 1);
         if (
-          moment(previousVersion.get('submitTime')).diff(
-            moment(version.get('submitTime')),
-            'days'
-          ) > 0
+          moment(previousVersion.get('submitTime'))
+            .startOf('day')
+            .diff(moment(version.get('submitTime')).startOf('day'), 'days') > 0
         ) {
           meta.showDate = true;
         }
