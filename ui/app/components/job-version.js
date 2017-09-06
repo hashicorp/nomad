0677888351af85bc73c3aac91fc3b822d10f1ddf
Michael Lange <dingoeatingfuzz@gmail.com>
2017-09-06 12:35:45 -0700

    Algorithm for counting the changes in a job diff.

--- ui/app/components/job-version.js
+++ ui/app/components/job-version.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed } = Ember;
+
+const changeTypes = ['Added', 'Deleted', 'Edited'];
 
 export default Component.extend({
   classNames: ['job-version', 'boxed-section'],
@@ -8,9 +10,42 @@ export default Component.extend({
   version: null,
   isOpen: false,
 
+  changeCount: computed('version.diff', function() {
+    const diff = this.get('version.diff');
+    const taskGroups = diff.TaskGroups || [];
+
+    if (!diff) {
+      return 0;
+    }
+
+    return (
+      fieldChanges(diff) +
+      taskGroups.reduce(arrayOfFieldChanges, 0) +
+      (taskGroups.mapBy('Tasks') || []).reduce(flatten, []).reduce(arrayOfFieldChanges, 0)
+    );
+  }),
+
   actions: {
     toggleDiff() {
       this.toggleProperty('isOpen');
     },
   },
 });
+
+const flatten = (accumulator, array) => accumulator.concat(array);
+const countChanges = (total, field) => (changeTypes.includes(field.Type) ? total + 1 : total);
+
+function fieldChanges(diff) {
+  return (
+    (diff.Fields || []).reduce(countChanges, 0) +
+    (diff.Objects || []).reduce(arrayOfFieldChanges, 0)
+  );
+}
+
+function arrayOfFieldChanges(count, diff) {
+  if (!diff) {
+    return count;
+  }
+
+  return count + fieldChanges(diff);
+}
