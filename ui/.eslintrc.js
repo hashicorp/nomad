e2b9a5d523f8fa8ee85e62ad50a95a0f83183f27
Michael Lange <dingoeatingfuzz@gmail.com>
2017-09-06 15:58:26 -0700

    Match eslint and prettier for switch case

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single', 'avoid-escape'],
     semi: ['error', 'always'],
