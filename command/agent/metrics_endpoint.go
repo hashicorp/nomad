f25cbdb0d1fae96e9697cb3b83942b8bcab22c8e
Chelsea Holland Komlo <me@chelseakomlo.com>
2017-09-11 16:10:09 +0000

    updated documentation

--- command/agent/metrics_endpoint.go
+++ command/agent/metrics_endpoint.go
@@ -7,7 +7,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-// MetricsRequest returns metrics in JSON format
+// MetricsRequest returns metrics for the agent. Metrics are JSON by default
+// but Prometheus is an optional format.
 func (s *HTTPServer) MetricsRequest(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
 	if req.Method != "GET" {
 		return nil, CodedError(405, ErrInvalidMethod)
