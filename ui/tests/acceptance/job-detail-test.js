b2b636f560219c92bfb0231130f42f25df6fbf2e
Michael Lange <dingoeatingfuzz@gmail.com>
2017-09-11 20:55:38 -0700

    New acceptance tests for job-detail

--- ui/tests/acceptance/job-detail-test.js
+++ ui/tests/acceptance/job-detail-test.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import moment from 'moment';
 import { test } from 'qunit';
 import moduleForAcceptance from 'nomad-ui/tests/helpers/module-for-acceptance';
 
 const { get } = Ember;
+const sum = (list, key) => list.reduce((sum, item) => sum + get(item, key), 0);
 
 let job;
 
@@ -30,9 +32,24 @@ test('breadcrumbs includes job name and link back to the jobs list', function(as
 });
 
 test('the job detail page should contain basic information about the job', function(assert) {
-  assert.ok(find('.title .tag:eq(0)').text().includes(job.status), 'Status');
-  assert.ok(find('.job-stats span:eq(0)').text().includes(job.type), 'Type');
-  assert.ok(find('.job-stats span:eq(1)').text().includes(job.priority), 'Priority');
+  assert.ok(
+    find('.title .tag:eq(0)')
+      .text()
+      .includes(job.status),
+    'Status'
+  );
+  assert.ok(
+    find('.job-stats span:eq(0)')
+      .text()
+      .includes(job.type),
+    'Type'
+  );
+  assert.ok(
+    find('.job-stats span:eq(1)')
+      .text()
+      .includes(job.priority),
+    'Priority'
+  );
 });
 
 test('the job detail page should list all task groups', function(assert) {
@@ -121,4 +138,133 @@ test('the allocations diagram lists all allocation status figures', function(ass
     statusCounts.lost,
     `${statusCounts.lost} are lost`
   );
+});
+
+test('there is no active deployment section when the job has no active deployment', function(
+  assert
+) {
+  // TODO: it would be better to not visit two different job pages in one test, but this
+  // way is much more convenient.
+  job = server.create('job', { noActiveDeployment: true });
+  visit(`/jobs/${job.id}`);
+
+  andThen(() => {
+    assert.ok(find('.active-deployment').length === 0, 'No active deployment');
+  });
+});
+
+test('the active deployment section shows up for the currently running deployment', function(
+  assert
+) {
+  job = server.create('job', { activeDeployment: true });
+  const deployment = server.db.deployments.where({ jobId: job.id })[0];
+  const taskGroupSummaries = server.db.deploymentTaskGroupSummaries.where({
+    deploymentId: deployment.id,
+  });
+  const version = server.db.jobVersions.findBy({
+    jobId: job.id,
+    version: deployment.versionNumber,
+  });
+  visit(`/jobs/${job.id}`);
+
+  andThen(() => {
+    assert.ok(find('.active-deployment').length === 1, 'Active deployment');
+    assert.equal(
+      find('.active-deployment > .boxed-section-head .badge')
+        .text()
+        .trim(),
+      deployment.id.split('-')[0],
+      'The active deployment is the most recent running deployment'
+    );
+
+    assert.equal(
+      find('.active-deployment > .boxed-section-head .submit-time')
+        .text()
+        .trim(),
+      moment(version.submitTime / 1000000).fromNow(),
+      'Time since the job was submitted is in the active deployment header'
+    );
+
+    assert.equal(
+      find('.deployment-metrics .label:contains("Canaries") + .value')
+        .text()
+        .trim(),
+      `${sum(taskGroupSummaries, 'placedCanaries')} / ${sum(
+        taskGroupSummaries,
+        'desiredCanaries'
+      )}`,
+      'Canaries, both places and desired, are in the metrics'
+    );
+
+    assert.equal(
+      find('.deployment-metrics .label:contains("Placed") + .value')
+        .text()
+        .trim(),
+      sum(taskGroupSummaries, 'placedAllocs'),
+      'Placed allocs aggregates across task groups'
+    );
+
+    assert.equal(
+      find('.deployment-metrics .label:contains("Desired") + .value')
+        .text()
+        .trim(),
+      sum(taskGroupSummaries, 'desiredTotal'),
+      'Desired allocs aggregates across task groups'
+    );
+
+    assert.equal(
+      find('.deployment-metrics .label:contains("Healthy") + .value')
+        .text()
+        .trim(),
+      sum(taskGroupSummaries, 'healthyAllocs'),
+      'Healthy allocs aggregates across task groups'
+    );
+
+    assert.equal(
+      find('.deployment-metrics .label:contains("Unhealthy") + .value')
+        .text()
+        .trim(),
+      sum(taskGroupSummaries, 'unhealthyAllocs'),
+      'Unhealthy allocs aggregates across task groups'
+    );
+
+    assert.equal(
+      find('.deployment-metrics .notification')
+        .text()
+        .trim(),
+      deployment.statusDescription,
+      'Status description is in the metrics block'
+    );
+  });
+});
+
+test('the active deployment section can be expanded to show task groups and allocations', function(
+  assert
+) {
+  job = server.create('job', { activeDeployment: true });
+  visit(`/jobs/${job.id}`);
+
+  andThen(() => {
+    assert.ok(
+      find('.active-deployment .boxed-section-head:contains("Task Groups")').length === 0,
+      'Task groups not found'
+    );
+    assert.ok(
+      find('.active-deployment .boxed-section-head:contains("Allocations")').length === 0,
+      'Allocations not found'
+    );
+  });
+
+  click('.active-deployment-details-toggle');
+
+  andThen(() => {
+    assert.ok(
+      find('.active-deployment .boxed-section-head:contains("Task Groups")').length === 1,
+      'Task groups found'
+    );
+    assert.ok(
+      find('.active-deployment .boxed-section-head:contains("Allocations")').length === 1,
+      'Allocations found'
+    );
+  });
 });
