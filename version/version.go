71fdfe2081dd62356a638683830d247ad8b3041e
Alex Dadgar <alex.dadgar@gmail.com>
2017-09-13 09:54:55 -0700

    bump version

--- version/version.go
+++ version/version.go
@@ -12,7 +12,7 @@ var (
 	GitDescribe string
 
 	// The main version number that is being run at the moment.
-	Version = "0.6.3"
+	Version = "0.7.0"
 
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
