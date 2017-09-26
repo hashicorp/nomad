2536812403142f7062bf9ab0a741d325fafaf577
Michael Lange <dingoeatingfuzz@gmail.com>
2017-09-26 16:37:08 -0700

    Scroll to the top of the page on transition. Like a normal website.

--- ui/app/routes/application.js
+++ ui/app/routes/application.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 const { Route } = Ember;
 
-export default Route.extend({});
+export default Route.extend({
+  actions: {
+    didTransition() {
+      window.scrollTo(0, 0);
+    },
+  },
+});
