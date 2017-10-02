36a543bd3e72076b089fb8272011725a2e7e9bb6
Michael Schurter <schmichael@hashicorp.com>
2017-10-02 14:19:35 -0700

    Deployment.Pause ACL enforcement

--- nomad/deployment_endpoint.go
+++ nomad/deployment_endpoint.go
@@ -116,6 +116,13 @@ func (d *Deployment) Pause(args *structs.DeploymentPauseRequest, reply *structs.
 	}
 	defer metrics.MeasureSince([]string{"nomad", "deployment", "pause"}, time.Now())
 
+	// Check namespace submit-job permissions
+	if aclObj, err := d.srv.resolveToken(args.SecretID); err != nil {
+		return err
+	} else if aclObj != nil && !aclObj.AllowNsOp(args.RequestNamespace(), acl.NamespaceCapabilitySubmitJob) {
+		return structs.ErrPermissionDenied
+	}
+
 	// Validate the arguments
 	if args.DeploymentID == "" {
 		return fmt.Errorf("missing deployment ID")
