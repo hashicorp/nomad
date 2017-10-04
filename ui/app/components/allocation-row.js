bc3c59c3e009f2572ac51f041d9b5c333fcc785c
Michael Lange <dingoeatingfuzz@gmail.com>
2017-10-03 17:18:33 -0700

    Add a loading state to job information on allocation row.
    This information is already loaded when traversing to an alloc row from
    a job, but not when traversing to an alloc row from a node.

--- ui/app/components/allocation-row.js
+++ ui/app/components/allocation-row.js
@@ -18,4 +18,19 @@ export default Component.extend({
   click(event) {
     lazyClick([this.get('onClick'), event]);
   },
+
+  didReceiveAttrs() {
+    // If the job for this allocation is incomplete, reload it to get
+    // detailed information.
+    const allocation = this.get('allocation');
+    if (
+      allocation &&
+      allocation.get('job') &&
+      !allocation.get('job.isPending') &&
+      !allocation.get('taskGroup')
+    ) {
+      const job = allocation.get('job.content');
+      job && job.reload();
+    }
+  },
 });
