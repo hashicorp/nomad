432d95a72a6397002deb31f24e57a63b3759480d
Michael Schurter <schmichael@hashicorp.com>
2017-10-09 11:19:14 -0700

    Test all fs endpoints in a loop

--- command/agent/fs_endpoint_test.go
+++ command/agent/fs_endpoint_test.go
@@ -113,53 +113,55 @@ func TestAllocDirFS_ACL(t *testing.T) {
 	t.Parallel()
 	assert := assert.New(t)
 
-	httpACLTest(t, nil, func(s *TestAgent) {
-		state := s.Agent.server.State()
+	for _, endpoint := range []string{"ls", "stat", "readat", "cat", "stream"} {
+		httpACLTest(t, nil, func(s *TestAgent) {
+			state := s.Agent.server.State()
 
-		req, err := http.NewRequest("GET", "/v1/client/fs/ls/", nil)
-		assert.Nil(err)
+			req, err := http.NewRequest("GET", fmt.Sprintf("/v1/client/fs/%s/", endpoint), nil)
+			assert.Nil(err)
 
-		// Try request without a token and expect failure
-		{
-			respW := httptest.NewRecorder()
-			_, err := s.Server.FsRequest(respW, req)
-			assert.NotNil(err)
-			assert.Equal(err.Error(), structs.ErrPermissionDenied.Error())
-		}
+			// Try request without a token and expect failure
+			{
+				respW := httptest.NewRecorder()
+				_, err := s.Server.FsRequest(respW, req)
+				assert.NotNil(err)
+				assert.Equal(err.Error(), structs.ErrPermissionDenied.Error())
+			}
 
-		// Try request with an invalid token and expect failure
-		{
-			respW := httptest.NewRecorder()
-			policy := mock.NamespacePolicy(structs.DefaultNamespace, "", []string{acl.NamespaceCapabilityReadLogs})
-			token := mock.CreatePolicyAndToken(t, state, 1005, "invalid", policy)
-			setToken(req, token)
-			_, err := s.Server.FsRequest(respW, req)
-			assert.NotNil(err)
-			assert.Equal(err.Error(), structs.ErrPermissionDenied.Error())
-		}
+			// Try request with an invalid token and expect failure
+			{
+				respW := httptest.NewRecorder()
+				policy := mock.NamespacePolicy(structs.DefaultNamespace, "", []string{acl.NamespaceCapabilityReadLogs})
+				token := mock.CreatePolicyAndToken(t, state, 1005, "invalid", policy)
+				setToken(req, token)
+				_, err := s.Server.FsRequest(respW, req)
+				assert.NotNil(err)
+				assert.Equal(err.Error(), structs.ErrPermissionDenied.Error())
+			}
 
-		// Try request with a valid token
-		// No alloc id set, so expect an error - just not a permissions error
-		{
-			respW := httptest.NewRecorder()
-			policy := mock.NamespacePolicy(structs.DefaultNamespace, "", []string{acl.NamespaceCapabilityReadFS})
-			token := mock.CreatePolicyAndToken(t, state, 1007, "valid", policy)
-			setToken(req, token)
-			_, err := s.Server.FsRequest(respW, req)
-			assert.NotNil(err)
-			assert.Equal(allocIDNotPresentErr, err)
-		}
+			// Try request with a valid token
+			// No alloc id set, so expect an error - just not a permissions error
+			{
+				respW := httptest.NewRecorder()
+				policy := mock.NamespacePolicy(structs.DefaultNamespace, "", []string{acl.NamespaceCapabilityReadFS})
+				token := mock.CreatePolicyAndToken(t, state, 1007, "valid", policy)
+				setToken(req, token)
+				_, err := s.Server.FsRequest(respW, req)
+				assert.NotNil(err)
+				assert.Equal(allocIDNotPresentErr, err)
+			}
 
-		// Try request with a management token
-		// No alloc id set, so expect an error - just not a permissions error
-		{
-			respW := httptest.NewRecorder()
-			setToken(req, s.RootToken)
-			_, err := s.Server.FsRequest(respW, req)
-			assert.NotNil(err)
-			assert.Equal(allocIDNotPresentErr, err)
-		}
-	})
+			// Try request with a management token
+			// No alloc id set, so expect an error - just not a permissions error
+			{
+				respW := httptest.NewRecorder()
+				setToken(req, s.RootToken)
+				_, err := s.Server.FsRequest(respW, req)
+				assert.NotNil(err)
+				assert.Equal(allocIDNotPresentErr, err)
+			}
+		})
+	}
 }
 
 func TestAllocDirFS_Logs_ACL(t *testing.T) {
