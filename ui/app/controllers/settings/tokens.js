2d92d7ba3c4a01521ab78894b375bb6bae314c61
Michael Lange <dingoeatingfuzz@gmail.com>
2017-10-13 15:18:05 -0700

    Redo the tokens page to be a form that validates a token and lists policies

--- ui/app/controllers/settings/tokens.js
+++ ui/app/controllers/settings/tokens.js
@@ -1,20 +1,53 @@
 import Ember from 'ember';
 
-const { Controller, inject } = Ember;
+const { Controller, inject, computed } = Ember;
 
 export default Controller.extend({
   token: inject.service(),
 
-  actions: {
-    setTokenProperty(property, event) {
-      this.get('token').set(property, event.currentTarget.value);
-    },
+  tokenRecord: null,
+  secret: computed.reads('token.secret'),
+  accessor: computed.reads('token.accessor'),
 
+  tokenIsValid: false,
+  tokenIsInvalid: false,
+
+  actions: {
     clearTokenProperties() {
       this.get('token').setProperties({
         secret: undefined,
         accessor: undefined,
       });
+      this.setProperties({
+        tokenIsValid: false,
+        tokenIsInvalid: false,
+      });
+    },
+
+    verifyToken() {
+      const { secret, accessor } = this.getProperties('secret', 'accessor');
+
+      this.set('token.secret', secret);
+      this.get('store')
+        .findRecord('token', accessor)
+        .then(
+          token => {
+            this.set('token.accessor', accessor);
+            this.setProperties({
+              tokenIsValid: true,
+              tokenIsInvalid: false,
+              tokenRecord: token,
+            });
+          },
+          () => {
+            this.set('token.secret', null);
+            this.setProperties({
+              tokenIsInvalid: true,
+              tokenIsValid: false,
+              tokenRecord: null,
+            });
+          }
+        );
     },
   },
 });
