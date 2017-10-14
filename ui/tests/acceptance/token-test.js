560dd3f88ef203c1716e1721fc693def8bd9d8a1
Michael Lange <dingoeatingfuzz@gmail.com>
2017-10-14 11:53:48 -0700

    Redo acceptance testing for ACL tokens

--- ui/tests/acceptance/token-test.js
+++ ui/tests/acceptance/token-test.js
@@ -1,21 +1,27 @@
-import { fillIn, visit } from 'ember-native-dom-helpers';
+import { find, findAll, fillIn, click, visit } from 'ember-native-dom-helpers';
+import Ember from 'ember';
 import { test } from 'qunit';
 import moduleForAcceptance from 'nomad-ui/tests/helpers/module-for-acceptance';
 
+const { $ } = Ember;
+
 let job;
 let node;
+let managementToken;
+let clientToken;
 
 moduleForAcceptance('Acceptance | tokens', {
   beforeEach() {
     server.create('agent');
     node = server.create('node');
     job = server.create('job');
+    managementToken = server.create('token');
+    clientToken = server.create('token');
   },
 });
 
 test('the token form sets the token in session storage', function(assert) {
-  const secret = 'this-is-the-secret';
-  const accessor = 'this-is-the-accessor';
+  const { secretId, accessorId } = managementToken;
 
   visit('/settings/tokens');
 
@@ -23,27 +29,19 @@ test('the token form sets the token in session storage', function(assert) {
     assert.ok(window.sessionStorage.nomadTokenSecret == null, 'No token secret set');
     assert.ok(window.sessionStorage.nomadTokenAccessor == null, 'No token accessor set');
 
-    andThen(() => {
-      fillIn('.token-secret', secret);
-    });
+    fillIn('.token-secret', secretId);
+    fillIn('.token-accessor', accessorId);
+    click('.token-submit');
 
     andThen(() => {
-      assert.equal(window.sessionStorage.nomadTokenSecret, secret, 'Token secret was set');
-      assert.ok(window.sessionStorage.nomadTokenAccessor == null, 'Token accessor was not set');
-    });
-
-    andThen(() => {
-      fillIn('.token-accessor', accessor);
-    });
-
-    andThen(() => {
-      assert.equal(window.sessionStorage.nomadTokenAccessor, accessor, 'Token accessor was set');
+      assert.equal(window.sessionStorage.nomadTokenSecret, secretId, 'Token secret was set');
+      assert.equal(window.sessionStorage.nomadTokenAccessor, accessorId, 'Token accessor was set');
     });
   });
 });
 
 test('the X-Nomad-Token header gets sent with requests once it is set', function(assert) {
-  const secret = 'this-is-the-secret';
+  const { secretId, accessorId } = managementToken;
   let requestPosition = 0;
 
   visit(`/jobs/${job.id}`);
@@ -61,7 +59,9 @@ test('the X-Nomad-Token header gets sent with requests once it is set', function
 
   visit('/settings/tokens');
   andThen(() => {
-    fillIn('.token-secret', secret);
+    fillIn('.token-secret', secretId);
+    fillIn('.token-accessor', accessorId);
+    click('.token-submit');
   });
 
   visit(`/jobs/${job.id}`);
@@ -72,7 +72,111 @@ test('the X-Nomad-Token header gets sent with requests once it is set', function
     assert.ok(newRequests.length > 1, 'New requests have been made');
 
     newRequests.forEach(req => {
-      assert.equal(req.requestHeaders['X-Nomad-Token'], secret, `Token set for ${req.url}`);
+      assert.equal(req.requestHeaders['X-Nomad-Token'], secretId, `Token set for ${req.url}`);
+    });
+  });
+});
+
+test('an error message is shown when authenticating a token fails', function(assert) {
+  const { secretId, accessorId } = managementToken;
+  const bogusSecret = 'this-is-not-the-secret';
+  assert.notEqual(
+    secretId,
+    bogusSecret,
+    'bogus secret is not somehow coincidentally equal to the real secret'
+  );
+
+  visit('/settings/tokens');
+
+  andThen(() => {
+    fillIn('.token-secret', bogusSecret);
+    fillIn('.token-accessor', accessorId);
+    click('.token-submit');
+
+    andThen(() => {
+      assert.ok(
+        window.sessionStorage.nomadTokenSecret == null,
+        'Token secret is discarded on failure'
+      );
+      assert.ok(
+        window.sessionStorage.nomadTokenAccessor == null,
+        'Token accessor is discarded on failure'
+      );
+      assert.ok(find('.token-error'), 'Token error message is shown');
+      assert.notOk(find('.token-success'), 'Token success message is not shown');
+      assert.notOk(find('.token-policy'), 'No token policies are shown');
+    });
+  });
+});
+
+test('a success message and a special management token message are shown when authenticating succeeds', function(
+  assert
+) {
+  const { secretId, accessorId } = managementToken;
+
+  visit('/settings/tokens');
+
+  andThen(() => {
+    fillIn('.token-secret', secretId);
+    fillIn('.token-accessor', accessorId);
+    click('.token-submit');
+
+    andThen(() => {
+      assert.ok(find('.token-success'), 'Token success message is shown');
+      assert.notOk(find('.token-error'), 'Token error message is not shown');
+      assert.ok(find('.token-management-message'), 'Token management message is shown');
+      assert.notOk(find('.token-policy'), 'No token policies are shown');
+    });
+  });
+});
+
+test('a success message and associated policies are shown when authenticating succeeds', function(
+  assert
+) {
+  const { secretId, accessorId } = clientToken;
+  const policy = clientToken.policies.models[0];
+  policy.update('description', 'Make sure there is a description');
+
+  visit('/settings/tokens');
+
+  andThen(() => {
+    fillIn('.token-secret', secretId);
+    fillIn('.token-accessor', accessorId);
+    click('.token-submit');
+
+    andThen(() => {
+      assert.ok(find('.token-success'), 'Token success message is shown');
+      assert.notOk(find('.token-error'), 'Token error message is not shown');
+      assert.notOk(find('.token-management-message'), 'Token management message is not shown');
+      assert.equal(
+        findAll('.token-policy').length,
+        clientToken.policies.length,
+        'Each policy associated with the token is listed'
+      );
+
+      const policyElement = $(find('.token-policy'));
+
+      assert.equal(
+        policyElement
+          .find('.boxed-section-head')
+          .text()
+          .trim(),
+        policy.name,
+        'Policy Name'
+      );
+      assert.equal(
+        policyElement
+          .find('.boxed-section-body p.content')
+          .text()
+          .trim(),
+        policy.description,
+        'Policy Description'
+      );
+      assert.equal(
+        policyElement.find('.boxed-section-body pre code').text(),
+        policy.rules,
+        'Policy Rules'
+      );
     });
   });
 });
