275af4836bc0339aabe59d8f04765c7c291ca8e8
Michael Lange <dingoeatingfuzz@gmail.com>
2017-10-14 11:01:28 -0700

    Mirage factories for tokens and policies

--- ui/mirage/scenarios/default.js
+++ ui/mirage/scenarios/default.js
@@ -5,4 +5,21 @@ export default function(server) {
   server.createList('namespace', 3);
 
   server.createList('job', 15);
+
+  server.createList('token', 3);
+  logTokens(server);
 }
+
+/* eslint-disable */
+function logTokens(server) {
+  console.log('TOKENS:');
+  server.db.tokens.forEach(token => {
+    console.log(`
+Name: ${token.name}
+Secret: ${token.secretId}
+Accessor: ${token.accessorId}
+
+`);
+  });
+}
+/* eslint-enable */
