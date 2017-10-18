c20b9c5a82aabd4dbf8afb7f5513a852f2b9565c
Michael Lange <dingoeatingfuzz@gmail.com>
2017-10-18 10:36:24 -0700

    Make sure to treat the namespace Name as the ID

--- ui/app/models/namespace.js
+++ ui/app/models/namespace.js
@@ -1,8 +1,11 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
+const { computed } = Ember;
+
 export default Model.extend({
-  name: attr('string'),
+  name: computed.readOnly('id'),
   hash: attr('string'),
   description: attr('string'),
 });
