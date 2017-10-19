015fe02116eead711ac84841cc604cc31ca5be55
Alex Dadgar <alex.dadgar@gmail.com>
2017-10-19 10:03:56 -0700

    split privilidged calls to two scripts

---
+++ scripts/travis-mac-priv.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+bash ./scripts/travis-consul.sh
+bash ./scripts/travis-vault.sh
--- scripts/travis-mac.sh
+++ scripts/travis-mac.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 brew upgrade golang
-bash ./scripts/travis-consul.sh
-bash ./scripts/travis-vault.sh
