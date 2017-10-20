de7e9a89e1627cd8440db293b4e0e9332aec411d
Chelsea Holland Komlo <me@chelseakomlo.com>
2017-10-20 15:28:49 -0400

    clean up unused dependencies

--- e2e/migrations/docker-run.sh
+++ e2e/migrations/docker-run.sh
@@ -1,3 +1,3 @@
-CURRENT_DIRECTORY=` pwd`
+CURRENT_DIRECTORY=`pwd`
 
 docker run --privileged -v $CURRENT_DIRECTORY:/gopkg/src/migrations -it nomad-e2e /bin/bash -c "cd gopkg/src/migrations && go test"
