828543b18edc7e4ab746ea78a74b5537e35a92fb
Chelsea Holland Komlo <me@chelseakomlo.com>
2017-10-24 13:22:48 -0400

    Add E2E test for migrations with acls

--- e2e/migrations/docker-run.sh
+++ e2e/migrations/docker-run.sh
@@ -1,7 +1,16 @@
 CURRENT_DIRECTORY=`pwd`
 ROOT_DIRECTORY="$( dirname "$(dirname "$CURRENT_DIRECTORY")")"
 
-docker run --privileged -v \
-$ROOT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
+testOne= docker run --privileged -v \
+$CURRENT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
 -it nomad-e2e /bin/bash \
--c "cd gopkg/src/github.com/hashicorp/nomad/e2e/migrations && go test -integration"
+-c "cd gopkg/src/github.com/hashicorp/nomad/e2e/migrations && go test --run \
+TestJobMigrations -integration"
+
+echo $testOne
+
+docker run --privileged \
+-v $CURRENT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
+-it nomad-e2e /bin/bash \
+-c "cd gopkg/src/github.com/hashicorp/nomad/e2e/migrations && go test --run \
+TestMigrations_WithACLs"
