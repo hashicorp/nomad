5321c9f81d433545435ff5053bfe485093a60cae
Chelsea Holland Komlo <me@chelseakomlo.com>
2017-10-25 16:12:30 -0400

    run acl migration test only with integration flag

--- e2e/migrations/docker-run.sh
+++ e2e/migrations/docker-run.sh
@@ -13,4 +13,4 @@ docker run --privileged \
 -v $CURRENT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
 -it nomad-e2e /bin/bash \
 -c "cd gopkg/src/github.com/hashicorp/nomad/e2e/migrations && go test --run \
-TestMigrations_WithACLs"
+TestMigrations_WithACLs -integration"
