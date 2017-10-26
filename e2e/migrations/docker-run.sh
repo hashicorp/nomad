68e15a33d43cc9a6af688905bc5077ad25281134
Chelsea Holland Komlo <me@chelseakomlo.com>
2017-10-26 14:09:37 -0400

    run e2e tests from e2e/migrations folder

--- e2e/migrations/docker-run.sh
+++ e2e/migrations/docker-run.sh
@@ -2,7 +2,7 @@ CURRENT_DIRECTORY=`pwd`
 ROOT_DIRECTORY="$( dirname "$(dirname "$CURRENT_DIRECTORY")")"
 
 testOne= docker run --privileged -v \
-$CURRENT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
+$ROOT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
 -it nomad-e2e /bin/bash \
 -c "cd gopkg/src/github.com/hashicorp/nomad/e2e/migrations && go test --run \
 TestJobMigrations -integration"
@@ -10,7 +10,7 @@ TestJobMigrations -integration"
 echo $testOne
 
 docker run --privileged \
--v $CURRENT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
+-v $ROOT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
 -it nomad-e2e /bin/bash \
 -c "cd gopkg/src/github.com/hashicorp/nomad/e2e/migrations && go test --run \
 TestMigrations_WithACLs -integration"
