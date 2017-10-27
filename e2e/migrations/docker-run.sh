44f5373f800fbee646885409214a168f36632af9
Chelsea Holland Komlo <me@chelseakomlo.com>
2017-10-27 16:26:49 -0400

    remove unnecessary test output print

--- e2e/migrations/docker-run.sh
+++ e2e/migrations/docker-run.sh
@@ -1,13 +1,11 @@
 CURRENT_DIRECTORY=`pwd`
 ROOT_DIRECTORY="$( dirname "$(dirname "$CURRENT_DIRECTORY")")"
 
-testOne= docker run --privileged -v \
+docker run --privileged -v \
 $ROOT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
 -it nomad-e2e /bin/bash \
 -c "cd gopkg/src/github.com/hashicorp/nomad/e2e/migrations && go test --run \
 TestJobMigrations -integration"
-
-echo $testOne
 
 docker run --privileged \
 -v $ROOT_DIRECTORY:/gopkg/src/github.com/hashicorp/nomad \
