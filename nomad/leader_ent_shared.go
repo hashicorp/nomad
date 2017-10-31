bd5dab98c494e62245fce22dc4bbd514bd5dd19e
Alex Dadgar <alex.dadgar@gmail.com>
2017-10-31 12:53:08 -0700

    remove old style bootstrap

--- nomad/leader_ent_shared.go
+++ nomad/leader_ent_shared.go
@@ -5,12 +5,10 @@ package nomad
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"time"
 
 	"golang.org/x/time/rate"
 
-	version "github.com/hashicorp/go-version"
 	"github.com/hashicorp/nomad/nomad/state"
 	"github.com/hashicorp/nomad/nomad/structs"
 )
@@ -18,10 +16,6 @@ import (
 // establishProLeadership is used to establish Nomad Pro systems upon acquiring
 // leadership.
 func (s *Server) establishProLeadership(stopCh chan struct{}) error {
-	if err := s.initializeNamespaces(); err != nil {
-		return err
-	}
-
 	// Start replication of Namespaces if we are not the authoritative region.
 	if s.config.Region != s.config.AuthoritativeRegion {
 		go s.replicateNamespaces(stopCh)
@@ -33,47 +27,6 @@ func (s *Server) establishProLeadership(stopCh chan struct{}) error {
 // revokeProLeadership is used to disable Nomad Pro systems upon losing
 // leadership.
 func (s *Server) revokeProLeadership() error {
-	return nil
-}
-
-// initializeNamespaces is used to setup of Namespaces. If the cluster is just
-// being formed or being upgraded to enteprise, the "default" namespace will not
-// exist and has to be created.
-func (s *Server) initializeNamespaces() error {
-	state := s.fsm.State()
-	ns, err := state.NamespaceByName(nil, structs.DefaultNamespace)
-	if err != nil {
-		s.logger.Printf("[ERR] nomad: failed to lookup default namespace when initializing: %v", err)
-		return err
-	}
-
-	// The default namespace already exists so there is nothing to do.
-	if ns != nil {
-		return nil
-	}
-
-	// Check if the servers are all at a version where they would accept the
-	// Raft log.
-	// TODO(alex): bump before releasing
-	minVersion := version.Must(version.NewVersion("0.7.0-dev"))
-	if !ServersMeetMinimumVersion(s.Members(), minVersion) {
-		s.logger.Printf("[WARN] nomad: Can't initialize default namespace until all servers are >= %s", minVersion.String())
-		return nil
-	}
-
-	// Create the default namespace
-	req := structs.NamespaceUpsertRequest{
-		Namespaces: []*structs.Namespace{
-			{
-				Name:        structs.DefaultNamespace,
-				Description: structs.DefaultNamespaceDescription,
-			},
-		},
-	}
-	if _, _, err := s.raftApply(structs.NamespaceUpsertRequestType, &req); err != nil {
-		return fmt.Errorf("failed to create default namespace: %v", err)
-	}
-
 	return nil
 }
 
