51a407f2bc8659d08d601560af10535057d30e94
Thomas Bartelmess <tbartelmess@marketcircle.com>
2017-11-28 23:15:32 -0500

    Changed Superset to only return the resource name
    The Superset method on Resources used to return a string in the format of “[resource name] exhausted”.
    This was leading to the output in plan/create job API DimensionExhausted to return keys like
    ```
    "DimensionExhausted": {"cpu exhausted": 1}
    ```
    This was not anywhere documented, however, one of the examples on the website showed it like this.
    
    The other side effect of this is that the CLI formats the strings from the name of the key leading to output like
    ```
    * Dimension "cpu exhausted" exhausted on 1 nodes
    ```

--- nomad/structs/structs.go
+++ nomad/structs/structs.go
@@ -1339,16 +1339,16 @@ func (r *Resources) NetIndex(n *NetworkResource) int {
 // should be used for that.
 func (r *Resources) Superset(other *Resources) (bool, string) {
 	if r.CPU < other.CPU {
-		return false, "cpu exhausted"
+		return false, "cpu"
 	}
 	if r.MemoryMB < other.MemoryMB {
-		return false, "memory exhausted"
+		return false, "memory"
 	}
 	if r.DiskMB < other.DiskMB {
-		return false, "disk exhausted"
+		return false, "disk"
 	}
 	if r.IOPS < other.IOPS {
-		return false, "iops exhausted"
+		return false, "iops"
 	}
 	return true, ""
 }
