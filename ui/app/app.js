0222d92274e74e1a614bb83bd374f85c77dd37ce
Michael Lange <dingoeatingfuzz@gmail.com>
2017-12-15 13:59:52 -0800

    Remove the MODEL_FACTORY_INJECTIONS cruft

--- ui/app/app.js
+++ ui/app/app.js
@@ -1,12 +1,9 @@
 import Application from '@ember/application';
-import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 let App;
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Application.extend({
   modulePrefix: config.modulePrefix,
