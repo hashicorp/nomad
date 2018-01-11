7d53083ccb1b6a66d8cb86c27dfe00434730f366
Alex Dadgar <alex.dadgar@gmail.com>
2018-01-11 15:58:59 -0800

    Server can forward ClientStats.Stats

--- command/agent/stats_endpoint.go
+++ command/agent/stats_endpoint.go
@@ -2,23 +2,58 @@ package agent
 
 import (
 	"net/http"
+	"strings"
 
 	cstructs "github.com/hashicorp/nomad/client/structs"
+	"github.com/hashicorp/nomad/nomad"
 )
 
 func (s *HTTPServer) ClientStatsRequest(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
-	if s.agent.client == nil {
-		return nil, clientNotRunning
-	}
+	// Get the requested Node ID
+	requestedNode := req.URL.Query().Get("node_id")
 
-	// Parse the ACL token
-	var args cstructs.ClientStatsRequest
-	s.parseToken(req, &args.AuthToken)
+	localClient := s.agent.Client()
+	localServer := s.agent.Server()
+
+	// See if the local client can handle the request.
+	useLocalClient := localClient != nil && // Must have a client
+		(requestedNode == "" || // If no node ID is given
+			requestedNode == localClient.NodeID()) // Requested node is the local node.
+
+	// Only use the client RPC to server if we don't have a server and the local
+	// client can't handle the call.
+	useClientRPC := localClient != nil && !useLocalClient && localServer == nil
+
+	// Use the server as a last case.
+	useServerRPC := localServer != nil && requestedNode != ""
+
+	// Build the request and parse the ACL token
+	args := cstructs.ClientStatsRequest{
+		NodeID: requestedNode,
+	}
+	s.parse(resp, req, &args.QueryOptions.Region, &args.QueryOptions)
 
 	// Make the RPC
 	var reply cstructs.ClientStatsResponse
-	if err := s.agent.Client().ClientRPC("ClientStats.Stats", &args, &reply); err != nil {
-		return nil, err
+	var rpcErr error
+	if useLocalClient {
+		rpcErr = s.agent.Client().ClientRPC("ClientStats.Stats", &args, &reply)
+	} else if useClientRPC {
+		rpcErr = s.agent.Client().RPC("ClientStats.Stats", &args, &reply)
+	} else if useServerRPC {
+		rpcErr = s.agent.Server().RPC("ClientStats.Stats", &args, &reply)
+	} else {
+		rpcErr = CodedError(400, "No local Node and node_id not provided")
+	}
+
+	if rpcErr != nil {
+		if nomad.IsErrNoNodeConn(rpcErr) {
+			rpcErr = CodedError(404, rpcErr.Error())
+		} else if strings.Contains(rpcErr.Error(), "Unknown node") {
+			rpcErr = CodedError(404, rpcErr.Error())
+		}
+
+		return nil, rpcErr
 	}
 
 	return reply.HostStats, nil
