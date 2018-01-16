4d0967960ccf4bd0b3aac8947a22466b60a778d7
John Cowen <jcowen@hashicorp.com>
2018-01-16 14:42:36 +0000

    Pass `breadcrumbs` in via the `Controllers` as opposed to in the template

--- ui/app/controllers/allocations/allocation.js
+++ ui/app/controllers/allocations/allocation.js
@@ -1,3 +1,25 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
-export default Controller.extend({});
+export default Controller.extend({
+  breadcrumbs: computed('model.{job,taskGroupName,shortId}', 'model.job.name', function() {
+    return [
+      {
+        label: 'Jobs',
+        args: ['jobs'],
+      },
+      {
+        label: this.get('model.job.name'),
+        args: ['jobs.job', this.get('model.job')],
+      },
+      {
+        label: this.get('model.taskGroupName'),
+        args: ['jobs.job.task-group', this.get('model.job'), this.get('model.taskGroupName')],
+      },
+      {
+        label: this.get('model.shortId'),
+        args: ['allocations.allocation', this.get('model')],
+      },
+    ];
+  }),
+});
--- ui/app/controllers/allocations/allocation/task/index.js
+++ ui/app/controllers/allocations/allocation/task/index.js
@@ -3,6 +3,39 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
 export default Controller.extend({
+  breadcrumbs: computed(
+    'model.{name,allocation}',
+    'model.allocation.{job,taskGroupName}',
+    'model.allocation.job.name',
+    function() {
+      return [
+        {
+          label: 'Jobs',
+          args: ['jobs'],
+        },
+        {
+          label: this.get('model.allocation.job.name'),
+          args: ['jobs.job', this.get('model.allocation.job')],
+        },
+        {
+          label: this.get('model.allocation.taskGroupName'),
+          args: [
+            'jobs.job.task-group',
+            this.get('model.allocation.job'),
+            this.get('model.allocation.taskGroupName'),
+          ],
+        },
+        {
+          label: this.get('model.allocation.shortId'),
+          args: ['allocations.allocation', this.get('model.allocation')],
+        },
+        {
+          label: this.get('model.name'),
+          args: ['allocations.allocation.task', this.get('model.allocation'), this.get('model')],
+        },
+      ];
+    }
+  ),
   network: alias('model.resources.networks.firstObject'),
   ports: computed('network.reservedPorts.[]', 'network.dynamicPorts.[]', function() {
     return (this.get('network.reservedPorts') || [])
