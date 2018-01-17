ee2ac206cbd208a13ad9ca975eaeb779701795f1
Michael Lange <dingoeatingfuzz@gmail.com>
2018-01-16 16:13:47 -0800

    Reorganize the styleguide using components for sections

--- ui/app/controllers/freestyle.js
+++ ui/app/controllers/freestyle.js
@@ -1,46 +1,6 @@
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 import FreestyleController from 'ember-freestyle/controllers/freestyle';
 
 export default FreestyleController.extend({
   emberFreestyle: service(),
-
-  timerTicks: 0,
-
-  startTimer: function() {
-    this.set(
-      'timer',
-      setInterval(() => {
-        this.incrementProperty('timerTicks');
-      }, 500)
-    );
-  }.on('init'),
-
-  stopTimer: function() {
-    clearInterval(this.get('timer'));
-  }.on('willDestroy'),
-
-  distributionBarData: computed(() => {
-    return [
-      { label: 'one', value: 10 },
-      { label: 'two', value: 20 },
-      { label: 'three', value: 30 },
-    ];
-  }),
-
-  distributionBarDataWithClasses: computed(() => {
-    return [
-      { label: 'Queued', value: 10, className: 'queued' },
-      { label: 'Complete', value: 20, className: 'complete' },
-      { label: 'Failed', value: 30, className: 'failed' },
-    ];
-  }),
-
-  distributionBarDataRotating: computed('timerTicks', () => {
-    return [
-      { label: 'one', value: Math.round(Math.random() * 50) },
-      { label: 'two', value: Math.round(Math.random() * 50) },
-      { label: 'three', value: Math.round(Math.random() * 50) },
-    ];
-  }),
 });
