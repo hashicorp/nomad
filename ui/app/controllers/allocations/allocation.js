62fc3ae26f0479e412a20306e5d800b94810726a
John Cowen <jcowen@hashicorp.com>
2018-01-19 11:32:16 +0000

    Add temporary fix for refreshing empty job breadcrumb problem

--- ui/app/controllers/allocations/allocation.js
+++ ui/app/controllers/allocations/allocation.js
@@ -1,13 +1,29 @@
 import Controller, { inject as controller } from '@ember/controller';
 import { computed } from '@ember/object';
+import queryParams from 'nomad-ui/utils/queryParams';
 
 export default Controller.extend({
   jobController: controller('jobs.job'),
   breadcrumbs: computed(
     'jobController.breadcrumbs.[]',
     'model.{name,job,taskGroupName,shortId}',
+    'model.job.name',
+    'model.job.namespace.name',
     function() {
-      return this.get('jobController.breadcrumbs').concat([
+      const jobBreadcrumbs = this.get('jobController.breadcrumbs');
+      if(typeof jobBreadcrumbs[1].label === "undefined") {
+        jobBreadcrumbs[1] = {
+          label: this.get('model.job.name'),
+          params: [
+            "jobs.job",
+            this.get('model.job'),
+            queryParams({
+              jobNamespace: this.get('model.job.namespace.name'),
+            }),
+          ]
+        };
+      }
+      return jobBreadcrumbs.concat([
         {
           label: this.get('model.taskGroupName'),
           params: ['jobs.job.task-group', this.get('model.job'), this.get('model.taskGroupName')],
