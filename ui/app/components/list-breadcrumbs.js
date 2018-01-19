a4826439c8928b6991ec011ca33d2168b99267e4
John Cowen <jcowen@hashicorp.com>
2018-01-19 11:09:31 +0000

    First draft js based auto active highlighting

--- ui/app/components/list-breadcrumbs.js
+++ ui/app/components/list-breadcrumbs.js
@@ -1,4 +1,32 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 
-export default Component.extend({});
+// externalise and unit test
+function activate(possible, $, pathname, cls)
+{
+  if(pathname == "") {
+    return;
+  }
+  const items = possible.filter(
+    item => item.attributes['href'].value == pathname
+  );
+  if(items.length == 0) {
+    return activate(possible, $, pathname.split("/").slice(0, -1).join("/"), cls);
+  }
+  return items.forEach(
+    item => $(item).closest('li').addClass(cls)
+  );
+};
+function activateAnchors($, pathname = document.location.pathname, cls = 'is-active')
+{
+  const possible = $('a[href]').get();
+  return activate(possible, $, pathname, cls);
+}
+export default Component.extend(
+  {
+    didRender() {
+      this._super(...arguments);
+      activateAnchors(this.$.bind(this));
+    }
+  }
+);
