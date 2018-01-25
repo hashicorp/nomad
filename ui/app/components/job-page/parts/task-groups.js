ec35b5a02f33be92b9d02e7d2bc36563c27087ef
Michael Lange <dingoeatingfuzz@gmail.com>
2018-01-25 09:31:48 -0800

    Deconstruct the existing job detail page into common parts
    This is to later compose job detail page variations

---
+++ ui/app/components/job-page/parts/task-groups.js
@@ -0,0 +1,23 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import Sortable from 'nomad-ui/mixins/sortable';
+
+export default Component.extend(Sortable, {
+  job: null,
+
+  classNames: ['boxed-section'],
+
+  // Provide a value that is bound to a query param
+  sortProperty: null,
+  sortDescending: null,
+
+  gotoTaskGroup() {},
+
+  taskGroups: computed('job.taskGroups.[]', function() {
+    return this.get('job.taskGroups') || [];
+  }),
+
+  listToSort: alias('taskGroups'),
+  sortedTaskGroups: alias('listSorted'),
+});
