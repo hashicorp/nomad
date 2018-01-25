bb1e0d66b69373b10954677b0dcdc737fb21ff7b
Michael Lange <dingoeatingfuzz@gmail.com>
2018-01-25 09:32:34 -0800

    Recreate the service job detail page using job part components

---
+++ ui/app/components/job-page/abstract.js
@@ -0,0 +1,25 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  job: null,
+
+  // Provide a value that is bound to a query param
+  sortProperty: null,
+  sortDescending: null,
+
+  // Provide actions that require routing
+  onNamespaceChange() {},
+  gotoTaskGroup() {},
+
+  breadcrumbs: computed('job.{name,id}', function() {
+    const job = this.get('job');
+    return [
+      { label: 'Jobs', args: ['jobs'] },
+      {
+        label: job.get('name'),
+        args: ['jobs.job', job],
+      },
+    ];
+  }),
+});
