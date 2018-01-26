3fc0910ee7f2cf3c0b914e3db0e21182bb7017fe
Michael Lange <dingoeatingfuzz@gmail.com>
2018-01-25 16:57:58 -0800

    Payload details for the parameterized child job detail page

--- ui/app/components/job-page/parameterized-child.js
+++ ui/app/components/job-page/parameterized-child.js
@@ -1,3 +1,16 @@
-import AbstractJobPage from './abstract';
+import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import PeriodicChildJobPage from './periodic-child';
 
-export default AbstractJobPage.extend();
+export default PeriodicChildJobPage.extend({
+  payload: alias('job.decodedPayload'),
+  payloadJSON: computed('payload', function() {
+    let json;
+    try {
+      json = JSON.parse(this.get('payload'));
+    } catch (e) {
+      // Swallow error and fall back to plain text rendering
+    }
+    return json;
+  }),
+});
