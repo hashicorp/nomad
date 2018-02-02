b7f57ec3b6f81229128d5cc915227dd4bf4ab165
Michael Lange <dingoeatingfuzz@gmail.com>
2018-02-01 18:54:23 -0800

    Integration test for periodic job force launch

---
+++ ui/tests/integration/job-page/periodic-test.js
@@ -0,0 +1,89 @@
+import { run } from '@ember/runloop';
+import { getOwner } from '@ember/application';
+import { test, moduleForComponent } from 'ember-qunit';
+import { click, find, findAll } from 'ember-native-dom-helpers';
+import wait from 'ember-test-helpers/wait';
+import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
+import { clickTrigger } from 'ember-power-select/test-support/helpers';
+import { startMirage } from 'nomad-ui/initializers/ember-cli-mirage';
+
+moduleForComponent('job-page/periodic', 'Integration | Component | job-page/periodic', {
+  integration: true,
+  beforeEach() {
+    window.localStorage.clear();
+    this.store = getOwner(this).lookup('service:store');
+    this.server = startMirage();
+    this.server.create('namespace');
+  },
+  afterEach() {
+    this.server.shutdown();
+    window.localStorage.clear();
+  },
+});
+
+test('Clicking Force Launch launches a new periodic child job', function(assert) {
+  const childrenCount = 3;
+
+  this.server.create('job', 'periodic', {
+    id: 'parent',
+    childrenCount,
+    createAllocations: false,
+  });
+
+  this.store.findAll('job');
+
+  return wait().then(() => {
+    const job = this.store.peekAll('job').findBy('plainId', 'parent');
+    this.setProperties({
+      job,
+      sortProperty: 'name',
+      sortDescending: true,
+      currentPage: 1,
+      gotoJob: () => {},
+    });
+
+    this.render(hbs`
+      {{job-page/periodic
+        job=job
+        sortProperty=sortProperty
+        sortDescending=sortDescending
+        currentPage=currentPage
+        gotoJob=gotoJob}}
+    `);
+
+    return wait().then(() => {
+      const currentJobCount = server.db.jobs.length;
+
+      assert.equal(
+        findAll('[data-test-job-name]').length,
+        childrenCount,
+        'The new periodic job launch is in the children list'
+      );
+
+      click('[data-test-force-launch]');
+
+      return wait().then(() => {
+        const id = job.get('plainId');
+        const namespace = job.get('namespace.name') || 'default';
+
+        assert.ok(
+          server.pretender.handledRequests
+            .filterBy('method', 'POST')
+            .find(req => req.url === `/v1/job/${id}/periodic/force?namespace=${namespace}`),
+          'POST URL was correct'
+        );
+
+        assert.ok(server.db.jobs.length, currentJobCount + 1, 'POST request was made');
+
+        return wait().then(() => {
+          assert.equal(
+            findAll('[data-test-job-name]').length,
+            childrenCount + 1,
+            'The new periodic job launch is in the children list'
+          );
+        });
+      });
+    });
+  });
+});
