b675d9740034d41b5f0bf52c65d13567034eecf9
Michael Lange <dingoeatingfuzz@gmail.com>
2018-02-12 15:27:19 -0800

    Watch for summary changes in job-row

--- ui/app/components/job-row.js
+++ ui/app/components/job-row.js
@@ -1,7 +1,11 @@
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import { lazyClick } from '../helpers/lazy-click';
+import { watchRelationship } from 'nomad-ui/utils/properties/watch';
 
 export default Component.extend({
+  store: service(),
+
   tagName: 'tr',
   classNames: ['job-row', 'is-interactive'],
 
@@ -17,7 +21,11 @@ export default Component.extend({
     // Reload the job in order to get detail information
     const job = this.get('job');
     if (job && !job.get('isLoading')) {
-      job.reload();
+      job.reload().then(() => {
+        this.get('watch').perform(job, 100);
+      });
     }
   },
+
+  watch: watchRelationship('summary').drop(),
 });
