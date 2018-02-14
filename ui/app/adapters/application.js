8a119d99ce6cd1ea37c43a96624808f485f77d8a
Michael Lange <dingoeatingfuzz@gmail.com>
2018-02-14 15:31:27 -0800

    Remove stale records from the store for findHasMany requests

--- ui/app/adapters/application.js
+++ ui/app/adapters/application.js
@@ -34,6 +34,24 @@ export default RESTAdapter.extend({
     });
   },
 
+  // In order to remove stale records from the store, findHasMany has to unload
+  // all records related to the request in question.
+  findHasMany(store, snapshot, link, relationship) {
+    return this._super(...arguments).then(payload => {
+      const ownerType = snapshot.modelName;
+      const relationshipType = relationship.type;
+      // Naively assume that the inverse relationship is named the same as the
+      // owner type. In the event it isn't, findHasMany should be overridden.
+      store
+        .peekAll(relationshipType)
+        .filter(record => record.get(`${ownerType}.id` === snapshot.id))
+        .forEach(record => {
+          store.unloadRecord(record);
+        });
+      return payload;
+    });
+  },
+
   // Single record requests deviate from REST practice by using
   // the singular form of the resource name.
   //
