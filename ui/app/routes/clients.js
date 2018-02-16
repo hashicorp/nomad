a2976dae854e37ddeb378780e1d93ccc78dac75d
Michael Lange <dingoeatingfuzz@gmail.com>
2018-02-16 11:02:43 -0800

    Watch nodes and allocs on the nodes list page

--- ui/app/routes/clients.js
+++ ui/app/routes/clients.js
@@ -3,6 +3,7 @@ import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import WithForbiddenState from 'nomad-ui/mixins/with-forbidden-state';
 import notifyForbidden from 'nomad-ui/utils/notify-forbidden';
+import { watchAll } from 'nomad-ui/utils/properties/watch';
 
 export default Route.extend(WithForbiddenState, {
   store: service(),
@@ -18,4 +19,16 @@ export default Route.extend(WithForbiddenState, {
       agents: this.get('store').findAll('agent'),
     }).catch(notifyForbidden(this));
   },
+
+  setupController(controller) {
+    controller.set('modelWatch', this.get('watch').perform());
+    return this._super(...arguments);
+  },
+
+  deactivate() {
+    this.get('watch').cancelAll();
+    this._super(...arguments);
+  },
+
+  watch: watchAll('node'),
 });
