34c712d5c5caa759b35f241343aeeaeef8a062be
Michael Lange <dingoeatingfuzz@gmail.com>
2018-02-27 14:57:57 -0800

    Watch job versions

--- ui/app/routes/jobs/job/versions.js
+++ ui/app/routes/jobs/job/versions.js
@@ -1,8 +1,21 @@
 import Route from '@ember/routing/route';
+import { watchRelationship } from 'nomad-ui/utils/properties/watch';
 
 export default Route.extend({
   model() {
     const job = this.modelFor('jobs.job');
     return job.get('versions').then(() => job);
   },
+
+  setupController(controller, model) {
+    controller.set('watcher', this.get('watchVersions').perform(model));
+    return this._super(...arguments);
+  },
+
+  deactivate() {
+    this.get('watchVersions').cancelAll();
+    return this._super(...arguments);
+  },
+
+  watchVersions: watchRelationship('versions'),
 });
