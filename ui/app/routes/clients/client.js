8fe4f76a17484b704aa64bb14a934a6a3c18bcd6
Michael Lange <dingoeatingfuzz@gmail.com>
2018-02-28 12:15:39 -0800

    Watch node and related allocations on the client detail page

--- ui/app/routes/clients/client.js
+++ ui/app/routes/clients/client.js
@@ -1,6 +1,7 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import notifyError from 'nomad-ui/utils/notify-error';
+import { watchRecord, watchRelationship } from 'nomad-ui/utils/properties/watch';
 
 export default Route.extend({
   store: service(),
@@ -15,4 +16,19 @@ export default Route.extend({
     }
     return model && model.get('allocations');
   },
+
+  setupController(controller, model) {
+    controller.set('watchModel', this.get('watch').perform(model));
+    controller.set('watchAllocations', this.get('watchAllocations').perform(model));
+    return this._super(...arguments);
+  },
+
+  deactivate() {
+    this.get('watch').cancelAll();
+    this.get('watchAllocations').cancelAll();
+    return this._super(...arguments);
+  },
+
+  watch: watchRecord('node'),
+  watchAllocations: watchRelationship('allocations'),
 });
