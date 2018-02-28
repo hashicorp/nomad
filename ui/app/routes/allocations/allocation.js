e7a26ce8d16bacaa2b9a7f18311a8eea876e6c85
Michael Lange <dingoeatingfuzz@gmail.com>
2018-02-28 11:48:06 -0800

    Watch the allocation on the allocation and task pages

--- ui/app/routes/allocations/allocation.js
+++ ui/app/routes/allocations/allocation.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 import WithModelErrorHandling from 'nomad-ui/mixins/with-model-error-handling';
+import { watchRecord } from 'nomad-ui/utils/properties/watch';
 
-export default Route.extend(WithModelErrorHandling);
+export default Route.extend(WithModelErrorHandling, {
+  setupController(controller, model) {
+    controller.set('watcher', this.get('watch').perform(model));
+    return this._super(...arguments);
+  },
+
+  deactivate() {
+    this.get('watch').cancelAll();
+    return this._super(...arguments);
+  },
+
+  watch: watchRecord('allocation'),
+});
