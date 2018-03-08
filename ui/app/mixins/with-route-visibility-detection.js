27ec2c23dfc5e50ab28203805c8c13b7c91566b0
Michael Lange <dingoeatingfuzz@gmail.com>
2018-03-08 11:15:43 -0800

    Better define mixin contracts

--- ui/app/mixins/with-route-visibility-detection.js
+++ ui/app/mixins/with-route-visibility-detection.js
@@ -1,6 +1,11 @@
 import Mixin from '@ember/object/mixin';
+import { assert } from '@ember/debug';
 
 export default Mixin.create({
+  visibilityHandler() {
+    assert('visibilityHandler needs to be overridden in the Route', false);
+  },
+
   setupDocumentVisibility: function() {
     this.set('_visibilityHandler', this.get('visibilityHandler').bind(this));
     document.addEventListener('visibilitychange', this.get('_visibilityHandler'));
