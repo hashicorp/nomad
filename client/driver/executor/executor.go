31651d124d8631bd3761bf7bc80b80cb1f0021fb
Josh Soref <jsoref@users.noreply.github.com>
2018-03-11 18:41:26 +0000

    spelling: remnants

--- client/driver/executor/executor.go
+++ client/driver/executor/executor.go
@@ -438,7 +438,7 @@ var (
 )
 
 // ClientCleanup is the cleanup routine that a Nomad Client uses to remove the
-// reminants of a child UniversalExecutor.
+// remnants of a child UniversalExecutor.
 func ClientCleanup(ic *dstructs.IsolationConfig, pid int) error {
 	return clientCleanup(ic, pid)
 }
