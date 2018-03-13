c8693ba5250d542752e8fec46b8f1f006f4124c1
Michael Lange <dingoeatingfuzz@gmail.com>
2018-03-13 14:59:03 -0700

    Make task page breadcrumbs extend the allocation breadcrumbs

--- ui/app/controllers/allocations/allocation/task/index.js
+++ ui/app/controllers/allocations/allocation/task/index.js
@@ -1,8 +1,12 @@
-import { alias } from '@ember/object/computed';
-import Controller from '@ember/controller';
+import Controller, { inject as controller } from '@ember/controller';
 import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
+  taskController: controller('allocations.allocation.task'),
+
+  breadcrumbs: alias('taskController.breadcrumbs'),
+
   network: alias('model.resources.networks.firstObject'),
   ports: computed('network.reservedPorts.[]', 'network.dynamicPorts.[]', function() {
     return (this.get('network.reservedPorts') || [])
