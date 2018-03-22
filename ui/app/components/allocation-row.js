fc85a81a903cf25f90f4f566633ec2b29590d559
Michael Lange <dingoeatingfuzz@gmail.com>
2018-03-22 14:34:57 -0700

    Poll for allocation stats

--- ui/app/components/allocation-row.js
+++ ui/app/components/allocation-row.js
@@ -1,7 +1,9 @@
+import Ember from 'ember';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import { run } from '@ember/runloop';
 import { lazyClick } from '../helpers/lazy-click';
+import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
   store: service(),
@@ -14,6 +16,10 @@ export default Component.extend({
 
   // Used to determine whether the row should mention the node or the job
   context: null,
+
+  // Internal state
+  stats: null,
+  statsError: false,
 
   onClick() {},
 
@@ -45,8 +51,32 @@ export default Component.extend({
     // being resolved through the store (store.findAll('job')). The
     // workaround is to persist the jobID as a string on the allocation
     // and manually re-link the two records here.
+    const allocation = this.get('allocation');
+
+    if (allocation) {
+      this.get('fetchStats').perform(allocation);
+    } else {
+      this.get('fetchStats').cancelAll();
+      this.set('stats', null);
+    }
     run.scheduleOnce('afterRender', this, qualifyJob);
   },
+
+  fetchStats: task(function*(allocation) {
+    const maxTiming = 5500;
+    const backoffSequence = [500, 800, 1300, 2100, 3400];
+
+    do {
+      try {
+        const stats = yield allocation.fetchStats();
+        this.set('stats', stats);
+      } catch (error) {
+        this.set('statsError', true);
+        break;
+      }
+      yield timeout(backoffSequence.shift() || maxTiming);
+    } while (!Ember.testing);
+  }).drop(),
 });
 
 function qualifyJob() {
