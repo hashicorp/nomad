edc5e1eec4a8cdd16de7bdfabf3f32e5d2fd9e23
Jozef Slezak <jozef.slezak@innovatrics.com>
2018-05-25 17:51:02 +0200

    unit test

--- client/driver/custom.go
+++ client/driver/custom.go
@@ -9,43 +9,75 @@ import (
 	"strings"
 )
 
-const customDriversDir = "drivers"
+const (
+	customDriversDir = "drivers"
+	customDriverConstructorName = "NewDriver"
+)
 
 func init() {
-	if _, err := os.Stat(customDriversDir); os.IsNotExist(err) {
-		return
-	}
-
-	files, err := ioutil.ReadDir(customDriversDir)
+	files, err := findCustomDrivers(customDriversDir)
 	if err != nil {
 		fmt.Println(err)
-		return
+	}
+
+	err := loadCustomDrivers(files, goPluginNewDriver)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func goPluginNewDriver(file string) (interface{}, error) {
+	plug, err := plugin.Open(file)
+	if err != nil {
+		return err
+	}
+
+	constructorLookup, err := plug.Lookup(customDriverConstructorName)
+	if err != nil {
+		return err
+	}
+}
+
+func findCustomDrivers(dir string) (files []string, err error) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return nil
+	}
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
 	}
 
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), ".so") {
 			pluginName := strings.TrimSuffix(file.Name(), ".so")
+		}
 
-			plug, err := plugin.Open(path.Join(customDriversDir, file.Name()))
+		files = append(files, path.Join(dir, file.Name()))
+	}
+
+	return files, nil
+}
+
+
+func loadCustomDrivers(files []string, openPlugin func(string) (interface{}, error) {
+	for _, file := range files {
+			plug, err := openPlugin(file)
 			if err != nil {
 				fmt.Println(err)
-				continue
-			}
-
-			constructorLookup, err := plug.Lookup("NewDriver")
-			if err != nil {
-				fmt.Println(err)
-				continue
+				return err
 			}
 
 			var factory Factory
 			factory, ok := constructorLookup.(Factory)
 			if !ok {
 				fmt.Println("unexpected type from module symbol ", factory)
-				continue
+				return err
 			}
 
 			BuiltinDrivers[pluginName] = factory
 		}
 	}
+
+	return nil
 }
