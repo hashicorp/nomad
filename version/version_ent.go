1b2a0c6907fb219da004b80f9dba46a3a422c4bc
Alex Dadgar <alex.dadgar@gmail.com>
2018-06-11 15:21:12 -0700

    version + remove generated

--- version/version_ent.go
+++ version/version_ent.go
@@ -3,14 +3,6 @@
 package version
 
 func init() {
-	// The main version number that is being run at the moment.
-	Version = "0.8.4"
-
-	// A pre-release marker for the version. If this is "" (empty string)
-	// then it means that it is a final release. Otherwise, this is a pre-release
-	// such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
-
 	// Metadata specifies the type of binary other than the default open-source
 	// version, such as "ent", "pro", etc.
 	VersionMetadata = "ent"
