0e5954765245fe214e10fe0d36ed45be11e14a35
Michael Lange <dingoeatingfuzz@gmail.com>
2018-06-13 15:05:18 -0700

    Remove hacky code that worked around an Ember Data bug
    The bug is fixed in 2.18, so this can be written in the normal
    less surprising way.

--- ui/app/components/allocation-row.js
+++ ui/app/components/allocation-row.js
@@ -33,29 +33,6 @@ export default Component.extend({
   },
 
   didReceiveAttrs() {
-    // TODO: Use this code again once the temporary workaround below
-    // is resolved.
-
-    // If the job for this allocation is incomplete, reload it to get
-    // detailed information.
-    // const allocation = this.get('allocation');
-    // if (
-    //   allocation &&
-    //   allocation.get('job') &&
-    //   !allocation.get('job.isPending') &&
-    //   !allocation.get('taskGroup')
-    // ) {
-    //   const job = allocation.get('job.content');
-    //   job && job.reload();
-    // }
-
-    // TEMPORARY: https://github.com/emberjs/data/issues/5209
-    // Ember Data doesn't like it when relationships aren't reflective,
-    // which means the allocation's job will be null if it hasn't been
-    // resolved through the allocation (allocation.get('job')) before
-    // being resolved through the store (store.findAll('job')). The
-    // workaround is to persist the jobID as a string on the allocation
-    // and manually re-link the two records here.
     const allocation = this.get('allocation');
 
     if (allocation) {
@@ -87,28 +64,17 @@ function qualifyAllocation() {
   const allocation = this.get('allocation');
   return allocation.reload().then(() => {
     this.get('fetchStats').perform(allocation);
-    run.scheduleOnce('afterRender', this, qualifyJob);
-  });
-}
 
-function qualifyJob() {
-  const allocation = this.get('allocation');
-  if (allocation.get('originalJobId')) {
-    const job = this.get('store').peekRecord('job', allocation.get('originalJobId'));
-    if (job) {
-      allocation.setProperties({
-        job,
-        originalJobId: null,
-      });
-      if (job.get('isPartial')) {
-        job.reload();
-      }
-    } else {
-      this.get('store')
-        .findRecord('job', allocation.get('originalJobId'))
-        .then(job => {
-          allocation.set('job', job);
-        });
+    // Make sure that the job record in the store for this allocation
+    // is complete and not a partial from the list endpoint
+    if (
+      allocation &&
+      allocation.get('job') &&
+      !allocation.get('job.isPending') &&
+      !allocation.get('taskGroup')
+    ) {
+      const job = allocation.get('job.content');
+      job && job.reload();
     }
-  }
+  });
 }
