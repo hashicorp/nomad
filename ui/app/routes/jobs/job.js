b04f34b0ca50ab49dd4839cf52e43a2458df85d0
Michael Lange <dingoeatingfuzz@gmail.com>
2018-06-27 12:48:26 -0700

    Use the new breadcrumbs pattern for allocations

--- ui/app/routes/jobs/job.js
+++ ui/app/routes/jobs/job.js
@@ -2,38 +2,13 @@ import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import notifyError from 'nomad-ui/utils/notify-error';
-import PromiseObject from 'nomad-ui/utils/classes/promise-object';
-import { qpBuilder } from 'nomad-ui/utils/classes/query-params';
-
-const jobCrumb = job => ({
-  label: job.get('trimmedName'),
-  args: [
-    'jobs.job.index',
-    job.get('plainId'),
-    qpBuilder({
-      jobNamespace: job.get('namespace.name') || 'default',
-    }),
-  ],
-});
+import { jobCrumbs } from 'nomad-ui/utils/breadcrumb-utils';
 
 export default Route.extend({
   store: service(),
   token: service(),
 
-  breadcrumbs(model) {
-    if (!model) return [];
-
-    if (model.get('parent.content')) {
-      return [
-        PromiseObject.create({
-          promise: model.get('parent').then(parent => jobCrumb(parent)),
-        }),
-        jobCrumb(model),
-      ];
-    } else {
-      return [jobCrumb(model)];
-    }
-  },
+  breadcrumbs: jobCrumbs,
 
   serialize(model) {
     return { job_name: model.get('plainId') };
