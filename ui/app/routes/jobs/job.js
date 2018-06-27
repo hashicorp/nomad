5ed3f274bb5a4983840b60c3cbfc15c3dc1090c9
Michael Lange <dingoeatingfuzz@gmail.com>
2018-06-27 11:29:04 -0700

    Add breadcrumbs to jobs routes

--- ui/app/routes/jobs/job.js
+++ ui/app/routes/jobs/job.js
@@ -2,10 +2,31 @@ import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import notifyError from 'nomad-ui/utils/notify-error';
+import PromiseObject from 'nomad-ui/utils/classes/promise-object';
+
+const jobCrumb = job => ({
+  label: job.get('trimmedName'),
+  args: ['jobs.job.index', job],
+});
 
 export default Route.extend({
   store: service(),
   token: service(),
+
+  breadcrumbs(model) {
+    if (!model) return [];
+
+    if (model.get('parent.content')) {
+      return [
+        PromiseObject.create({
+          promise: model.get('parent').then(parent => jobCrumb(parent)),
+        }),
+        jobCrumb(model),
+      ];
+    } else {
+      return [jobCrumb(model)];
+    }
+  },
 
   serialize(model) {
     return { job_name: model.get('plainId') };
