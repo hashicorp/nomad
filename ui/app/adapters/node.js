566c5843dec3dd048a9ddb540ca22fa061cf94f7
Michael Lange <dingoeatingfuzz@gmail.com>
2018-07-05 11:24:14 -0700

    Remove unused findAllocations method in both job and node
    This was replaced with a relationship at some point.

--- ui/app/adapters/node.js
+++ ui/app/adapters/node.js
@@ -1,12 +1,3 @@
 import Watchable from './watchable';
 
-export default Watchable.extend({
-  findAllocations(node) {
-    const url = `${this.buildURL('node', node.get('id'), node, 'findRecord')}/allocations`;
-    return this.ajax(url, 'GET').then(allocs => {
-      return this.store.pushPayload('allocation', {
-        allocations: allocs,
-      });
-    });
-  },
-});
+export default Watchable.extend();
