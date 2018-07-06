6c1006ffdf005e9c3c651d7d1dc53af9b8a29d97
Michael Lange <dingoeatingfuzz@gmail.com>
2018-07-06 16:50:41 -0700

    Adds sorting to evaluations

--- ui/app/controllers/jobs/job/evaluations.js
+++ ui/app/controllers/jobs/job/evaluations.js
@@ -1,12 +1,24 @@
 import { alias } from '@ember/object/computed';
 import Controller, { inject as controller } from '@ember/controller';
 import WithNamespaceResetting from 'nomad-ui/mixins/with-namespace-resetting';
+import Sortable from 'nomad-ui/mixins/sortable';
 
-export default Controller.extend(WithNamespaceResetting, {
+export default Controller.extend(WithNamespaceResetting, Sortable, {
   jobController: controller('jobs.job'),
+
+  queryParams: {
+    sortProperty: 'sort',
+    sortDescending: 'desc',
+  },
+
+  sortProperty: 'modifyIndex',
+  sortDescending: true,
 
   job: alias('model'),
   evaluations: alias('model.evaluations'),
 
   breadcrumbs: alias('jobController.breadcrumbs'),
+
+  listToSort: alias('evaluations'),
+  sortedEvaluations: alias('listSorted'),
 });
