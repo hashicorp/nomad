38371b05881ae524cac81476d9e662842e65aca0
Michael Lange <dingoeatingfuzz@gmail.com>
2018-07-10 18:42:19 -0700

    Use PageObject for jobs list tests

---
+++ ui/tests/pages/jobs/list.js
@@ -0,0 +1,42 @@
+import {
+  attribute,
+  create,
+  collection,
+  clickable,
+  fillable,
+  isPresent,
+  text,
+  visitable,
+} from 'ember-cli-page-object';
+
+export default create({
+  pageSize: 10,
+
+  visit: visitable('/jobs'),
+
+  search: fillable('[data-test-jobs-search] input'),
+
+  jobs: collection('[data-test-job-row]', {
+    name: text('[data-test-job-name]'),
+    link: attribute('href', '[data-test-job-name] a'),
+    status: text('[data-test-job-status]'),
+    type: text('[data-test-job-type]'),
+    priority: text('[data-test-job-priority]'),
+    taskGroups: text('[data-test-job-task-groups]'),
+
+    clickRow: clickable(),
+    clickName: clickable('[data-test-job-name] a'),
+  }),
+
+  isEmpty: isPresent('[data-test-empty-jobs-list]'),
+
+  emptyState: {
+    headline: text('[data-test-empty-jobs-list-headline]'),
+  },
+
+  error: {
+    title: text('[data-test-error-title]'),
+    message: text('[data-test-error-message]'),
+    seekHelp: clickable('[data-test-error-message] a'),
+  },
+});
