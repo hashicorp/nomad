bdf8c635a0ee316531c6e79425d33fbcf6ffd2e4
Michael Lange <dingoeatingfuzz@gmail.com>
2018-07-11 09:28:19 -0700

    Update breadcrumbs on job evaluations

--- ui/app/controllers/jobs/job/evaluations.js
+++ ui/app/controllers/jobs/job/evaluations.js
@@ -1,11 +1,9 @@
 import { alias } from '@ember/object/computed';
-import Controller, { inject as controller } from '@ember/controller';
+import Controller from '@ember/controller';
 import WithNamespaceResetting from 'nomad-ui/mixins/with-namespace-resetting';
 import Sortable from 'nomad-ui/mixins/sortable';
 
 export default Controller.extend(WithNamespaceResetting, Sortable, {
-  jobController: controller('jobs.job'),
-
   queryParams: {
     sortProperty: 'sort',
     sortDescending: 'desc',
@@ -16,8 +14,6 @@ export default Controller.extend(WithNamespaceResetting, Sortable, {
 
   job: alias('model'),
   evaluations: alias('model.evaluations'),
-
-  breadcrumbs: alias('jobController.breadcrumbs'),
 
   listToSort: alias('evaluations'),
   sortedEvaluations: alias('listSorted'),
