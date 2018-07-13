294fd9aa11699eaf7a475778cb9fc7f9f1b65e25
Alex Dadgar <alex.dadgar@gmail.com>
2018-07-13 13:45:57 -0700

    Template hook

--- client/allocrunnerv2/taskrunner/lifecycle.go
+++ client/allocrunnerv2/taskrunner/lifecycle.go
@@ -2,14 +2,6 @@ package taskrunner
 
 import "os"
 
-// XXX These should probably all return an error and we should have predefined
-// error types for the task not currently running
-type TaskLifecycle interface {
-	Restart(source, reason string, failure bool)
-	Signal(source, reason string, s os.Signal) error
-	Kill(source, reason string, fail bool)
-}
-
 func (tr *TaskRunner) Restart(source, reason string, failure bool) {
 	// TODO
 }
