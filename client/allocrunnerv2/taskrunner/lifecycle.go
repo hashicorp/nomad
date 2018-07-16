4fe5a0d61ae24f21ca349b54afb80646a3f42dc9
Alex Dadgar <alex.dadgar@gmail.com>
2018-07-16 14:37:27 -0700

    Implement lifecycle hooks on the task runner

--- client/allocrunnerv2/taskrunner/lifecycle.go
+++ client/allocrunnerv2/taskrunner/lifecycle.go
@@ -1,16 +1,99 @@
 package taskrunner
 
-import "os"
+import (
+	"context"
+	"os"
 
-func (tr *TaskRunner) Restart(source, reason string, failure bool) {
-	// TODO
-}
+	"github.com/hashicorp/nomad/nomad/structs"
+)
 
-func (tr *TaskRunner) Signal(source, reason string, s os.Signal) error {
-	// TODO
+func (tr *TaskRunner) Restart(ctx context.Context, event *structs.TaskEvent, failure bool) error {
+	// Grab the handle
+	handle := tr.getDriverHandle()
+
+	// Check it is running
+	if handle == nil {
+		return ErrTaskNotRunning
+	}
+
+	// Emit the event
+	tr.EmitEvent(event)
+
+	// Tell the restart tracker that a restart triggered the exit
+	tr.restartTracker.SetRestartTriggered(failure)
+
+	// Kill the task using an exponential backoff in-case of failures.
+	destroySuccess, err := tr.handleDestroy(handle)
+	if !destroySuccess {
+		// We couldn't successfully destroy the resource created.
+		tr.logger.Error("failed to kill task. Resources may have been leaked", "error", err)
+	}
+
+	// Drain the wait channel or wait for the request context to be cancelled
+	select {
+	case <-handle.WaitCh():
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
 	return nil
 }
 
-func (tr *TaskRunner) Kill(source, reason string, fail bool) {
-	// TODO
+func (tr *TaskRunner) Signal(event *structs.TaskEvent, s os.Signal) error {
+	// Grab the handle
+	handle := tr.getDriverHandle()
+
+	// Check it is running
+	if handle == nil {
+		return ErrTaskNotRunning
+	}
+
+	// Emit the event
+	tr.EmitEvent(event)
+
+	// Send the signal
+	return handle.Signal(s)
+}
+
+func (tr *TaskRunner) Kill(ctx context.Context, event *structs.TaskEvent) error {
+	// Grab the handle
+	handle := tr.getDriverHandle()
+
+	// Check if the handle is running
+	if handle == nil {
+		return ErrTaskNotRunning
+	}
+
+	// Emit the event
+	tr.EmitEvent(event)
+
+	// Run the hooks prior to killing the task
+	tr.kill()
+
+	// Tell the restart tracker that the task has been killed
+	tr.restartTracker.SetKilled()
+
+	// Kill the task using an exponential backoff in-case of failures.
+	destroySuccess, destroyErr := tr.handleDestroy(handle)
+	if !destroySuccess {
+		// We couldn't successfully destroy the resource created.
+		tr.logger.Error("failed to kill task. Resources may have been leaked", "error", destroyErr)
+	}
+
+	// Drain the wait channel or wait for the request context to be cancelled
+	select {
+	case <-handle.WaitCh():
+	case <-ctx.Done():
+	}
+
+	// Store that the task has been destroyed and any associated error.
+	tr.SetState(structs.TaskStateDead, structs.NewTaskEvent(structs.TaskKilled).SetKillError(destroyErr))
+
+	if destroyErr != nil {
+		return destroyErr
+	} else if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
