b7d084b7d16c592b54e76835c27bbb295080feb9
Chelsea Holland Komlo <me@chelseakomlo.com>
2018-07-17 13:30:41 -0400

    Example GRPC plugins

---
+++ plugins/drivers/raw_exec_driver/plugin-go-grpc/raw_exec/raw_exec_test.go
@@ -0,0 +1,21 @@
+package raw_exec
+
+import (
+	"testing"
+
+	"github.com/hashicorp/nomad/plugins/drivers/raw_exec_driver/proto"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRawExecDriver_StartOpen_Wait(t *testing.T) {
+	t.Parallel()
+	require := require.New(t)
+
+	execCtx := &proto.ExecContext{}
+	taskInfo := &proto.TaskInfo{}
+
+	d := NewRawExecDriver()
+	resp, err := d.Start(execCtx, taskInfo)
+	require.Nil(err)
+	require.NotNil(resp)
+}
