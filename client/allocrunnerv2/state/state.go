52ae83d4d5a3a44a3286e32a431c859a5ad0993b
Alex Dadgar <alex.dadgar@gmail.com>
2018-07-18 17:06:44 -0700

    Update state with server

--- client/allocrunnerv2/state/state.go
+++ client/allocrunnerv2/state/state.go
@@ -1,34 +1,19 @@
 package state
 
 import (
-	"sync"
-
 	"github.com/hashicorp/nomad/nomad/structs"
 )
 
+// XXX Why its own package?
 // State captures the state of the allocation runner.
 type State struct {
-	sync.RWMutex
+	// ClientStatus captures the overall state of the allocation
+	ClientStatus string
 
-	// ClientState captures the overall state of the allocation
-	ClientState string
-
-	// ClientDesc is an optional human readable description of the
+	// ClientDescription is an optional human readable description of the
 	// allocations client state
-	ClientDesc string
+	ClientDescription string
 
 	// DeploymentStatus captures the status of the deployment
 	DeploymentStatus *structs.AllocDeploymentStatus
-}
-
-type PersistentState struct {
-	ClientState string
-}
-
-func (s *State) PersistentState() *PersistentState {
-	s.RLock()
-	defer s.RUnlock()
-	return &PersistentState{
-		ClientState: s.ClientState,
-	}
 }
