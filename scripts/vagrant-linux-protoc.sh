cd1b45311020f54048eb52f3366dda952908f9eb
Chelsea Holland Komlo <me@chelseakomlo.com>
2018-07-31 15:25:48 -0400

    bootstrap vagrant development environment with protoc

---
+++ scripts/vagrant-linux-protoc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# set up protoc so that we can use it for protobuf generation
+PROTOC_ZIP=protoc-3.3.0-osx-x86_64.zip
+curl -OL https://github.com/google/protobuf/releases/download/v3.3.0/$PROTOC_ZIP
+sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+rm -f $PROTOC_ZIP
+
