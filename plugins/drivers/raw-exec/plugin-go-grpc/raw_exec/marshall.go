3105e023fb0981f1eb7e6b3069f4812106c35a7c
Chelsea Holland Komlo <me@chelseakomlo.com>
2018-08-02 16:12:23 -0400

    extract methods for marshalling/unmarshalling

---
+++ plugins/drivers/raw-exec/plugin-go-grpc/raw_exec/marshall.go
@@ -0,0 +1,65 @@
+package raw_exec
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/hashicorp/nomad/plugins/drivers/raw-exec/proto"
+)
+
+func unmarshallExecContext(ctx *proto.ExecContext) *ExecContext {
+	return &ExecContext{
+		TaskEnv: &TaskEnv{},
+		TaskDir: &TaskDir{
+			Dir:       ctx.TaskDir.Directory,
+			LogDir:    ctx.TaskDir.LogDir,
+			LogLevel:  ctx.TaskDir.LogLevel,
+			LogOutput: os.Stdout,
+		},
+		MaxPort:        5000,
+		MinPort:        2000,
+		MaxKillTimeout: time.Duration(5),
+		Version:        "1.0", // TODO was d.DriverContext.Config.Version.VersionNumber()
+	}
+}
+
+func unmarshallTaskInfo(tInfo *proto.TaskInfo) (*TaskInfo, error) {
+	marshaller := jsonpb.Marshaler{EnumsAsInts: true, EmitDefaults: true, OrigName: false}
+
+	configString, err := marshaller.MarshalToString(tInfo.Config)
+	if err != nil {
+		// TODO  should log to a logger here
+		fmt.Println("Error decoding json config struct", err)
+		return nil, err
+	}
+	rawExecTaskConfig := &RawExecTaskConfig{}
+	if err := json.Unmarshal([]byte(configString), rawExecTaskConfig); err != nil {
+		return nil, fmt.Errorf("Failed to parse config json '%s': %v", configString, err)
+	}
+
+	taskInfo := &TaskInfo{
+		Resources: &Resources{
+			CPU:      int(tInfo.Resources.Cpu),
+			MemoryMB: int(tInfo.Resources.MemoryMb),
+			DiskMB:   int(tInfo.Resources.DiskMb),
+		},
+		LogConfig: &LogConfig{
+			MaxFiles:      int(tInfo.LogConfig.MaxFiles),
+			MaxFileSizeMB: int(tInfo.LogConfig.MaxFileSizeMb),
+		},
+		Name: "taskName",
+		Config: &Config{
+			Command: rawExecTaskConfig.Command,
+			Args:    rawExecTaskConfig.Args,
+		},
+	}
+	command := taskInfo.Config.Command
+	if err := validateCommand(command, "args"); err != nil {
+		return nil, err
+	}
+
+	return taskInfo, nil
+}
--- plugins/drivers/raw-exec/plugin-go-grpc/raw_exec/raw_exec.go
+++ plugins/drivers/raw-exec/plugin-go-grpc/raw_exec/raw_exec.go
@@ -11,7 +11,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/golang/protobuf/jsonpb"
 	multierror "github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/nomad/client/allocdir"
@@ -139,63 +138,9 @@ func (d *RawExecDriver) Prestart(*driver.ExecContext, *structs.Task) (*driver.Pr
 	return nil, nil
 }
 
-func getExecContextFromProto(ctx *proto.ExecContext) *ExecContext {
-	return &ExecContext{
-		TaskEnv: &TaskEnv{},
-		TaskDir: &TaskDir{
-			Dir:       ctx.TaskDir.Directory,
-			LogDir:    ctx.TaskDir.LogDir,
-			LogLevel:  ctx.TaskDir.LogLevel,
-			LogOutput: os.Stdout,
-		},
-		MaxPort:        5000,
-		MinPort:        2000,
-		MaxKillTimeout: time.Duration(5),
-		Version:        "1.0", // TODO was d.DriverContext.Config.Version.VersionNumber()
-	}
-}
-
-func getTaskInfoFromProto(tInfo *proto.TaskInfo) (*TaskInfo, error) {
-	marshaller := jsonpb.Marshaler{EnumsAsInts: true, EmitDefaults: true, OrigName: false}
-
-	configString, err := marshaller.MarshalToString(tInfo.Config)
-	if err != nil {
-		// TODO  should log to a logger here
-		fmt.Println("Error decoding json config struct", err)
-		return nil, err
-	}
-	rawExecTaskConfig := &RawExecTaskConfig{}
-	if err := json.Unmarshal([]byte(configString), rawExecTaskConfig); err != nil {
-		return nil, fmt.Errorf("Failed to parse config json '%s': %v", configString, err)
-	}
-
-	taskInfo := &TaskInfo{
-		Resources: &Resources{
-			CPU:      int(tInfo.Resources.Cpu),
-			MemoryMB: int(tInfo.Resources.MemoryMb),
-			DiskMB:   int(tInfo.Resources.DiskMb),
-		},
-		LogConfig: &LogConfig{
-			MaxFiles:      int(tInfo.LogConfig.MaxFiles),
-			MaxFileSizeMB: int(tInfo.LogConfig.MaxFileSizeMb),
-		},
-		Name: "taskName",
-		Config: &Config{
-			Command: rawExecTaskConfig.Command,
-			Args:    rawExecTaskConfig.Args,
-		},
-	}
-	command := taskInfo.Config.Command
-	if err := validateCommand(command, "args"); err != nil {
-		return nil, err
-	}
-
-	return taskInfo, nil
-}
-
 func (d *RawExecDriver) Start(ctx *proto.ExecContext, tInfo *proto.TaskInfo) (*proto.StartResponse, error) {
-	execCtx := getExecContextFromProto(ctx)
-	taskInfo, err := getTaskInfoFromProto(tInfo)
+	execCtx := unmarshallExecContext(ctx)
+	taskInfo, err := unmarshallTaskInfo(tInfo)
 	if err != nil {
 		return nil, err
 	}
