04b49ecab10910b4f2aaec040e19328af9970a7b
Michael Lange <dingoeatingfuzz@gmail.com>
2018-08-02 15:56:11 -0700

    Add three-way region property (query param, service, localStorage)

--- ui/app/routes/application.js
+++ ui/app/routes/application.js
@@ -1,14 +1,36 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import { next } from '@ember/runloop';
 import { AbortError } from 'ember-data/adapters/errors';
 
 export default Route.extend({
   config: service(),
+  system: service(),
 
   resetController(controller, isExiting) {
     if (isExiting) {
       controller.set('error', null);
     }
+  },
+
+  beforeModel() {
+    return this.get('system.regions');
+  },
+
+  syncToController(controller) {
+    const region = this.get('system.activeRegion');
+
+    // The run next is necessary to let the controller figure
+    // itself out before updating QPs.
+    // See: https://github.com/emberjs/ember.js/issues/5465
+    next(() => {
+      controller.set('region', region || 'global');
+    });
+  },
+
+  setupController(controller) {
+    this.syncToController(controller);
+    return this._super(...arguments);
   },
 
   actions: {
