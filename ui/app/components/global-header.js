082513982d03b75dad9358e185bb58ee9599c854
Michael Lange <dingoeatingfuzz@gmail.com>
2018-08-06 13:42:07 -0700

    Add region switcher to the global header

--- ui/app/components/global-header.js
+++ ui/app/components/global-header.js
@@ -1,5 +1,28 @@
 import Component from '@ember/component';
+import { next } from '@ember/runloop';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  system: service(),
+  router: service(),
+  store: service(),
+
   onHamburgerClick() {},
+
+  sortedRegions: computed('system.regions', function() {
+    return this.get('system.regions')
+      .toArray()
+      .sort();
+  }),
+
+  gotoRegion(region) {
+    this.get('system').reset();
+    this.get('store').unloadAll();
+    next(() => {
+      this.get('router').transitionTo('jobs', {
+        queryParams: { region },
+      });
+    });
+  },
 });
