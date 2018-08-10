189e7a865f784d378755f9e9d2c08f0363b08f09
Chelsea Holland Komlo <me@chelseakomlo.com>
2018-08-10 13:08:13 -0400

    skip update checking if DisableUpdateCheck is set to true

--- command/agent/command.go
+++ command/agent/command.go
@@ -393,8 +393,9 @@ func (c *Command) setupAgent(config *Config, logOutput io.Writer, inmem *metrics
 	}
 	c.httpServer = http
 
-	// Setup update checking
-	if config.DisableUpdateCheck != nil && *config.DisableUpdateCheck {
+	// If DisableUpdateCheck is not enabled, set up update checking
+	// (DisableUpdateCheck is false by default)
+	if config.DisableUpdateCheck != nil && !*config.DisableUpdateCheck {
 		version := config.Version.Version
 		if config.Version.VersionPrerelease != "" {
 			version += fmt.Sprintf("-%s", config.Version.VersionPrerelease)
