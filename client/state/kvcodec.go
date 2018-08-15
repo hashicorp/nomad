ea23462adc1b927330556216b313e9c3acd02f63
Michael Schurter <schmichael@hashicorp.com>
2018-08-15 14:03:18 -0700

    wip wrap boltdb to get path information
    finished but doesn't handle deleting deeply nested buckets

--- client/state/kvcodec.go
+++ client/state/kvcodec.go
@@ -12,38 +12,36 @@ import (
 )
 
 type kvStore interface {
+	Path() []byte
+	Bucket(name []byte) kvStore
+	CreateBucket(key []byte) (kvStore, error)
+	CreateBucketIfNotExists(key []byte) (kvStore, error)
+	DeleteBucket(key []byte) error
 	Get(key []byte) (val []byte)
 	Put(key, val []byte) error
-	Writable() bool
 }
 
 // keyValueCodec handles encoding and decoding values from a key/value store
 // such as boltdb.
 type keyValueCodec struct {
-	// hashes maps keys to the hash of the last content written
-	hashes     map[string][]byte
+	// hashes maps buckets to keys to the hash of the last content written:
+	//   bucket -> key -> hash  for example:
+	//   allocations/1234       -> alloc      -> abcd
+	//   allocations/1234/redis -> task_state -> efff
+	hashes     map[string]map[string][]byte
 	hashesLock sync.Mutex
 }
 
 func newKeyValueCodec() *keyValueCodec {
 	return &keyValueCodec{
-		hashes: make(map[string][]byte),
+		hashes: make(map[string]map[string][]byte),
 	}
-}
-
-// hashKey returns a unique key for each hashed boltdb value
-func (c *keyValueCodec) hashKey(path string, key []byte) string {
-	return path + "-" + string(key)
 }
 
 // Put into kv store iff it has changed since the last write. A globally
 // unique key is constructed for each value by concatinating the path and key
 // passed in.
-func (c *keyValueCodec) Put(bkt kvStore, path string, key []byte, val interface{}) error {
-	if !bkt.Writable() {
-		return fmt.Errorf("bucket must be writable")
-	}
-
+func (c *keyValueCodec) Put(bkt kvStore, key []byte, val interface{}) error {
 	// buffer for writing serialized state to
 	var buf bytes.Buffer
 
@@ -63,24 +61,34 @@ func (c *keyValueCodec) Put(bkt kvStore, path string, key []byte, val interface{
 	}
 
 	// If the hashes are equal, skip the write
+	hashPath := string(bkt.Path())
+	hashKey := string(key)
 	hashVal := h.Sum(nil)
-	hashKey := c.hashKey(path, key)
+
+	// lastHash value or nil if it hasn't been hashed yet
+	var lastHash []byte
 
 	c.hashesLock.Lock()
-	persistedHash := c.hashes[hashKey]
+	if hashBkt, ok := c.hashes[hashPath]; ok {
+		lastHash = hashBkt[hashKey]
+	} else {
+		// Create hash bucket
+		c.hashes[hashPath] = make(map[string][]byte, 2)
+	}
 	c.hashesLock.Unlock()
 
-	if bytes.Equal(hashVal, persistedHash) {
+	if bytes.Equal(hashVal, lastHash) {
 		return nil
 	}
 
+	// New value: write it to the underlying store
 	if err := bkt.Put(key, buf.Bytes()); err != nil {
 		return fmt.Errorf("failed to write data at key %s: %v", key, err)
 	}
 
-	// New value written, store hash
+	// New value written, store hash (bucket path map was created above)
 	c.hashesLock.Lock()
-	c.hashes[hashKey] = hashVal
+	c.hashes[hashPath][hashKey] = hashVal
 	c.hashesLock.Unlock()
 
 	return nil
@@ -101,4 +109,24 @@ func (c *keyValueCodec) Get(bkt kvStore, key []byte, obj interface{}) error {
 	}
 
 	return nil
+}
+
+// DeleteBucket or do nothing if bucket doesn't exist.
+func (c *keyValueCodec) DeleteBucket(parent kvStore, bktName []byte) error {
+	// Get the path of the bucket being deleted
+	bkt := parent.Bucket(bktName)
+	if bkt == nil {
+		// Doesn't exist! Nothing to delete
+		return nil
+	}
+
+	// Delete the bucket
+	err := parent.DeleteBucket(bktName)
+
+	// Always purge all corresponding hashes to prevent memory leaks
+	c.hashesLock.Lock()
+	delete(c.hashes, string(bkt.Path()))
+	c.hashesLock.Unlock()
+
+	return err
 }
