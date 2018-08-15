258ee818de8ea2562f2fa03af454fe094fde0264
Michael Lange <dingoeatingfuzz@gmail.com>
2018-08-15 15:18:38 -0700

    Error messages for job submit

--- ui/app/controllers/jobs/run.js
+++ ui/app/controllers/jobs/run.js
@@ -1,46 +1,61 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { task } from 'ember-concurrency';
-import { qpBuilder } from 'nomad-ui/utils/classes/query-params';
-import { next } from '@ember/runloop';
+import messageFromAdapterError from 'nomad-ui/utils/message-from-adapter-error';
+import localStorageProperty from 'nomad-ui/utils/properties/local-storage';
 
 export default Controller.extend({
+  parseError: null,
+  planError: null,
+  runError: null,
+
+  showPlanMessage: localStorageProperty('nomadMessageJobPlan', true),
+  showEditorMessage: localStorageProperty('nomadMessageJobEditor', true),
+
   stage: computed('planOutput', function() {
     return this.get('planOutput') ? 'plan' : 'editor';
   }),
 
   plan: task(function*() {
-    this.cancel();
+    this.reset();
 
     try {
       yield this.get('model').parse();
     } catch (err) {
-      this.set('parseError', err);
+      const error = messageFromAdapterError(err) || 'Could not parse input';
+      this.set('parseError', error);
+      return;
     }
 
     try {
       const planOutput = yield this.get('model').plan();
       this.set('planOutput', planOutput);
     } catch (err) {
-      this.set('planError', err);
+      const error = messageFromAdapterError(err) || 'Could not plan job';
+      this.set('planError', error);
     }
   }).drop(),
 
   submit: task(function*() {
     try {
       yield this.get('model').run();
+
       const id = this.get('model.plainId');
       const namespace = this.get('model.namespace.name') || 'default';
+
+      this.reset();
+
       // navigate to the new job page
       this.transitionToRoute('jobs.job', id, {
         queryParams: { jobNamespace: namespace },
       });
     } catch (err) {
-      this.set('runError', err);
+      const error = messageFromAdapterError(err) || 'Could not submit job';
+      this.set('runError', error);
     }
   }),
 
-  cancel() {
+  reset() {
     this.set('planOutput', null);
     this.set('planError', null);
     this.set('parseError', null);
