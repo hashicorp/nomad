6463efe2ceef0d3a5c217d5592fe01e0cd07a7e5
Michael Lange <dingoeatingfuzz@gmail.com>
2018-08-22 17:36:04 -0700

    Test coverage for the job-editor component
    Most of this was ported over from the existing job run acceptance tests

--- ui/tests/pages/jobs/run.js
+++ ui/tests/pages/jobs/run.js
@@ -1,46 +1,8 @@
-import { clickable, create, hasClass, isPresent, text, visitable } from 'ember-cli-page-object';
-import { codeFillable, code } from 'nomad-ui/tests/pages/helpers/codemirror';
+import { create, visitable } from 'ember-cli-page-object';
 
-import error from 'nomad-ui/tests/pages/components/error';
+import jobEditor from 'nomad-ui/tests/pages/components/job-editor';
 
 export default create({
   visit: visitable('/jobs/run'),
-
-  planError: error('data-test-plan-error'),
-  parseError: error('data-test-parse-error'),
-  runError: error('data-test-run-error'),
-
-  plan: clickable('[data-test-plan]'),
-  cancel: clickable('[data-test-cancel]'),
-  run: clickable('[data-test-run]'),
-
-  planOutput: text('[data-test-plan-output]'),
-
-  planHelp: {
-    isPresent: isPresent('[data-test-plan-help-title]'),
-    title: text('[data-test-plan-help-title]'),
-    message: text('[data-test-plan-help-message]'),
-    dismiss: clickable('[data-test-plan-help-dismiss]'),
-  },
-
-  editorHelp: {
-    isPresent: isPresent('[data-test-editor-help-title]'),
-    title: text('[data-test-editor-help-title]'),
-    message: text('[data-test-editor-help-message]'),
-    dismiss: clickable('[data-test-editor-help-dismiss]'),
-  },
-
-  editor: {
-    isPresent: isPresent('[data-test-editor]'),
-    contents: code('[data-test-editor]'),
-    fillIn: codeFillable('[data-test-editor]'),
-  },
-
-  dryRunMessage: {
-    scope: '[data-test-dry-run-message]',
-    title: text('[data-test-dry-run-title]'),
-    body: text('[data-test-dry-run-body]'),
-    errored: hasClass('is-warning'),
-    succeeded: hasClass('is-primary'),
-  },
+  editor: jobEditor(),
 });
