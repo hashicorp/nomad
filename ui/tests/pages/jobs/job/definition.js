1c5bc0420e43ae7bcdcf6117b73f386c7bbe5019
Michael Lange <dingoeatingfuzz@gmail.com>
2018-08-23 10:26:20 -0700

    Acceptance tests for the edit behaviors on the job definition page

--- ui/tests/pages/jobs/job/definition.js
+++ ui/tests/pages/jobs/job/definition.js
@@ -1,7 +1,12 @@
-import { create, isPresent, visitable } from 'ember-cli-page-object';
+import { create, isPresent, visitable, clickable } from 'ember-cli-page-object';
+
+import jobEditor from 'nomad-ui/tests/pages/components/job-editor';
 
 export default create({
   visit: visitable('/jobs/:id/definition'),
 
   jsonViewer: isPresent('[data-test-definition-view]'),
+  editor: jobEditor(),
+
+  edit: clickable('[data-test-edit-job]'),
 });
