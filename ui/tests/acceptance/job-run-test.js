7b6130013d08be0d89c5e7aae6803bd75eea649e
Michael Lange <dingoeatingfuzz@gmail.com>
2018-08-23 09:00:47 -0700

    Rewrite the job run acceptance tests to be about routing

--- ui/tests/acceptance/job-run-test.js
+++ ui/tests/acceptance/job-run-test.js
@@ -35,17 +35,6 @@ const jsonJob = overrides => {
   );
 };
 
-const hclJob = () => `
-job "${newJobName}" {
-  namespace = "default"
-  datacenters = ["dc1"]
-
-  task "${newJobTaskGroupName}" {
-    driver = "docker"
-  }
-}
-`;
-
 moduleForAcceptance('Acceptance | job run', {
   beforeEach() {
     // Required for placing allocations (a result of creating jobs)
@@ -61,233 +50,24 @@ test('visiting /jobs/run', function(assert) {
   });
 });
 
-test('the page has an editor and an explanation popup', function(assert) {
-  JobRun.visit();
-
-  andThen(() => {
-    assert.ok(JobRun.editorHelp.isPresent, 'Editor explanation popup is present');
-    assert.ok(JobRun.editor.isPresent, 'Editor is present');
-  });
-});
-
-test('the explanation popup can be dismissed', function(assert) {
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editorHelp.dismiss();
-  });
-
-  andThen(() => {
-    assert.notOk(JobRun.editorHelp.isPresent, 'Editor explanation popup is gone');
-    assert.equal(
-      window.localStorage.nomadMessageJobEditor,
-      'false',
-      'Dismissal is persisted in localStorage'
-    );
-  });
-});
-
-test('the explanation popup is not shown once the dismissal state is set in localStorage', function(assert) {
-  window.localStorage.nomadMessageJobEditor = 'false';
-
-  JobRun.visit();
-
-  andThen(() => {
-    assert.notOk(JobRun.editorHelp.isPresent, 'Editor explanation popup is gone');
-  });
-});
-
-test('submitting a json job skips the parse endpoint', function(assert) {
+test('when submitting a job, the site redirects to the new job overview page', function(assert) {
   const spec = jsonJob();
 
   JobRun.visit();
 
   andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
+    JobRun.editor.editor.fillIn(spec);
+    JobRun.editor.plan();
   });
 
   andThen(() => {
-    const requests = server.pretender.handledRequests.mapBy('url');
-    assert.notOk(requests.includes('/v1/jobs/parse'), 'JSON job spec is not parsed');
-    assert.ok(requests.includes(`/v1/job/${newJobName}/plan`), 'JSON job spec is still planned');
+    JobRun.editor.run();
   });
-});
-
-test('submitting an hcl job requires the parse endpoint', function(assert) {
-  const spec = hclJob();
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    const requests = server.pretender.handledRequests.mapBy('url');
-    assert.ok(requests.includes('/v1/jobs/parse'), 'HCL job spec is parsed first');
-    assert.ok(requests.includes(`/v1/job/${newJobName}/plan`), 'HCL job spec is planned');
-    assert.ok(
-      requests.indexOf('/v1/jobs/parse') < requests.indexOf(`/v1/job/${newJobName}/plan`),
-      'Parse comes before Plan'
-    );
-  });
-});
-
-test('when a job is successfully parsed and planned, the plan is shown to the user', function(assert) {
-  const spec = hclJob();
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    assert.ok(JobRun.planOutput, 'The plan is outputted');
-    assert.notOk(JobRun.editor.isPresent, 'The editor is replaced with the plan output');
-    assert.ok(JobRun.planHelp.isPresent, 'The plan explanation popup is shown');
-  });
-});
-
-test('from the plan screen, the cancel button goes back to the editor with the job still in tact', function(assert) {
-  const spec = hclJob();
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    JobRun.cancel();
-  });
-
-  andThen(() => {
-    assert.ok(JobRun.editor.isPresent, 'The editor is shown again');
-    assert.notOk(JobRun.planOutpu, 'The plan is gone');
-    assert.equal(JobRun.editor.contents, spec, 'The spec that was planned is still in the editor');
-  });
-});
-
-test('from the plan screen, the submit button submits the job and redirects to the job overview page', function(assert) {
-  const spec = hclJob();
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    JobRun.run();
-  });
-
   andThen(() => {
     assert.equal(
       currentURL(),
       `/jobs/${newJobName}`,
       `Redirected to the job overview page for ${newJobName}`
-    );
-
-    const runRequest = server.pretender.handledRequests.find(
-      req => req.method === 'POST' && req.url === '/v1/jobs'
-    );
-    const planRequest = server.pretender.handledRequests.find(
-      req => req.method === 'POST' && req.url === '/v1/jobs/parse'
-    );
-
-    assert.ok(runRequest, 'A POST request was made to run the new job');
-    assert.deepEqual(
-      JSON.parse(runRequest.requestBody).Job,
-      JSON.parse(planRequest.responseText),
-      'The Job payload parameter is equivalent to the result of the parse request'
-    );
-  });
-});
-
-test('when parse fails, the parse error message is shown', function(assert) {
-  const spec = hclJob();
-
-  const errorMessage = 'Parse Failed!! :o';
-  server.pretender.post('/v1/jobs/parse', () => [400, {}, errorMessage]);
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    assert.notOk(JobRun.planError.isPresent, 'Plan error is not shown');
-    assert.notOk(JobRun.runError.isPresent, 'Run error is not shown');
-
-    assert.ok(JobRun.parseError.isPresent, 'Parse error is shown');
-    assert.equal(
-      JobRun.parseError.message,
-      errorMessage,
-      'The error message from the server is shown in the error in the UI'
-    );
-  });
-});
-
-test('when plan fails, the plan error message is shown', function(assert) {
-  const spec = hclJob();
-
-  const errorMessage = 'Parse Failed!! :o';
-  server.pretender.post(`/v1/job/${newJobName}/plan`, () => [400, {}, errorMessage]);
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    assert.notOk(JobRun.parseError.isPresent, 'Parse error is not shown');
-    assert.notOk(JobRun.runError.isPresent, 'Run error is not shown');
-
-    assert.ok(JobRun.planError.isPresent, 'Plan error is shown');
-    assert.equal(
-      JobRun.planError.message,
-      errorMessage,
-      'The error message from the server is shown in the error in the UI'
-    );
-  });
-});
-
-test('when run fails, the run error message is shown', function(assert) {
-  const spec = hclJob();
-
-  const errorMessage = 'Parse Failed!! :o';
-  server.pretender.post('/v1/jobs', () => [400, {}, errorMessage]);
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    JobRun.run();
-  });
-
-  andThen(() => {
-    assert.notOk(JobRun.planError.isPresent, 'Plan error is not shown');
-    assert.notOk(JobRun.parseError.isPresent, 'Parse error is not shown');
-
-    assert.ok(JobRun.runError.isPresent, 'Run error is shown');
-    assert.equal(
-      JobRun.runError.message,
-      errorMessage,
-      'The error message from the server is shown in the error in the UI'
     );
   });
 });
@@ -301,67 +81,18 @@ test('when submitting a job to a different namespace, the redirect to the job ov
   JobRun.visit();
 
   andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
+    JobRun.editor.editor.fillIn(spec);
+    JobRun.editor.plan();
   });
 
   andThen(() => {
-    JobRun.run();
+    JobRun.editor.run();
   });
   andThen(() => {
     assert.equal(
       currentURL(),
       `/jobs/${newJobName}?namespace=${newNamespace}`,
       `Redirected to the job overview page for ${newJobName} and switched the namespace to ${newNamespace}`
-    );
-  });
-});
-
-test('when the scheduler dry-run has warnings, the warnings are shown to the user', function(assert) {
-  // Unschedulable is a hint to Mirage to respond with warnings from the plan endpoint
-  const spec = jsonJob({ Unschedulable: true });
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    assert.ok(
-      JobRun.dryRunMessage.errored,
-      'The scheduler dry-run message is in the warning state'
-    );
-    assert.notOk(
-      JobRun.dryRunMessage.succeeded,
-      'The success message is not shown in addition to the warning message'
-    );
-    assert.ok(
-      JobRun.dryRunMessage.body.includes(newJobTaskGroupName),
-      'The scheduler dry-run message includes the warning from send back by the API'
-    );
-  });
-});
-
-test('when the scheduler dry-run has no warnings, a success message is shown to the user', function(assert) {
-  const spec = hclJob();
-
-  JobRun.visit();
-
-  andThen(() => {
-    JobRun.editor.fillIn(spec);
-    JobRun.plan();
-  });
-
-  andThen(() => {
-    assert.ok(
-      JobRun.dryRunMessage.succeeded,
-      'The scheduler dry-run message is in the success state'
-    );
-    assert.notOk(
-      JobRun.dryRunMessage.errored,
-      'The warning message is not shown in addition to the success message'
     );
   });
 });
