80264e93d0da2997e28de0a5ef1656613e5fdeda
Michael Lange <dingoeatingfuzz@gmail.com>
2018-08-24 13:17:45 -0700

    Add Start Job action on the job overview page for when a job is dead

--- ui/app/components/job-page/parts/title.js
+++ ui/app/components/job-page/parts/title.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import messageFromAdapterError from 'nomad-ui/utils/message-from-adapter-error';
 
 export default Component.extend({
   tagName: '',
@@ -16,6 +17,36 @@ export default Component.extend({
           this.get('handleError')({
             title: 'Could Not Stop Job',
             description: 'Your ACL token does not grant permission to stop jobs.',
+          });
+        });
+    },
+
+    startJob() {
+      const job = this.get('job');
+      job
+        .fetchRawDefinition()
+        .then(definition => {
+          // A stopped job will have this Stop = true metadata
+          // If Stop is true when submitted to the cluster, the job
+          // won't transition from the Dead to Running state.
+          delete definition.Stop;
+          job.set('_newDefinition', JSON.stringify(definition));
+        })
+        .then(() => {
+          return job.parse();
+        })
+        .then(() => {
+          return job.update();
+        })
+        .catch(err => {
+          let message = messageFromAdapterError(err);
+          if (!message || message === 'Forbidden') {
+            message = 'Your ACL token does not grant permission to stop jobs.';
+          }
+
+          this.get('handleError')({
+            title: 'Could Not Start Job',
+            description: message,
           });
         });
     },
