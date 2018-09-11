bf7192c58d8503ac4123660f33a6f989e0c28242
Preetha Appan <preetha@hashicorp.com>
2018-09-11 11:36:15 -0500

    Add number of evictions to DesiredUpdates struct to use in CLI/API

--- nomad/structs/structs.go
+++ nomad/structs/structs.go
@@ -8299,6 +8299,7 @@ type DesiredUpdates struct {
 	InPlaceUpdate     uint64
 	DestructiveUpdate uint64
 	Canary            uint64
+	Evict             uint64
 }
 
 func (d *DesiredUpdates) GoString() string {
