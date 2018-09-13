fa0128c99450a386a7f8e19020bebcff2ee9d638
Nick Ethier <nethier@hashicorp.com>
2018-09-13 12:19:37 -0400

    fifo: use containerd/fifo lib on unix

--- client/lib/fifo/fifo_unix.go
+++ client/lib/fifo/fifo_unix.go
@@ -3,33 +3,23 @@
 package fifo
 
 import (
+	"context"
 	"io"
 	"os"
 	"syscall"
+
+	cfifo "github.com/containerd/fifo"
 )
 
 // New creates a fifo at the given path and returns a reader for it
 // The fifo must not already exist
 func New(path string) (io.ReadCloser, error) {
-	err := syscall.Mkfifo(path, 0600)
-	if err != nil {
-		return nil, err
-	}
-
-	return open(path)
-}
-
-func open(path string) (*os.File, error) {
-	f, err := os.OpenFile(path, os.O_RDWR, 0600)
-	if err != nil {
-		return nil, err
-	}
-	return f, nil
+	return cfifo.OpenFifo(context.Background(), path, syscall.O_RDONLY|syscall.O_CREAT|syscall.O_NONBLOCK, 0600)
 }
 
 // OpenWriter opens a fifo that already exists and returns a writer for it
 func OpenWriter(path string) (io.WriteCloser, error) {
-	return open(path)
+	return cfifo.OpenFifo(context.Background(), path, syscall.O_WRONLY|syscall.O_NONBLOCK, 0600)
 }
 
 // Remove a fifo that already exists at a given path
