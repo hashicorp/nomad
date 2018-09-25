c322ccc6f024b1307f3e13f656fe653de3811ce0
Michael Lange <dingoeatingfuzz@gmail.com>
2018-09-24 20:01:42 -0700

    Add a longForm option to format-duration

--- ui/app/utils/format-duration.js
+++ ui/app/utils/format-duration.js
@@ -4,15 +4,26 @@ const allUnits = [
   { name: 'years', suffix: 'year', inMoment: true, pluralizable: true },
   { name: 'months', suffix: 'month', inMoment: true, pluralizable: true },
   { name: 'days', suffix: 'day', inMoment: true, pluralizable: true },
-  { name: 'hours', suffix: 'h', inMoment: true, pluralizable: false },
-  { name: 'minutes', suffix: 'm', inMoment: true, pluralizable: false },
-  { name: 'seconds', suffix: 's', inMoment: true, pluralizable: false },
+  { name: 'hours', suffix: 'h', longSuffix: 'hour', inMoment: true, pluralizable: false },
+  { name: 'minutes', suffix: 'm', longSuffix: 'minute', inMoment: true, pluralizable: false },
+  { name: 'seconds', suffix: 's', longSuffix: 'second', inMoment: true, pluralizable: false },
   { name: 'milliseconds', suffix: 'ms', inMoment: true, pluralizable: false },
   { name: 'microseconds', suffix: 'µs', inMoment: false, pluralizable: false },
   { name: 'nanoseconds', suffix: 'ns', inMoment: false, pluralizable: false },
 ];
 
-export default function formatDuration(duration = 0, units = 'ns') {
+const pluralizeUnits = (amount, unit, longForm) => {
+  let suffix;
+  if (longForm && unit.longSuffix) {
+    suffix = amount === 1 ? unit.longSuffix : unit.longSuffix.pluralize();
+  } else {
+    suffix = amount === 1 || !unit.pluralizable ? unit.suffix : unit.suffix.pluralize();
+  }
+  const addSpace = unit.pluralizable || (longForm && unit.longSuffix);
+  return `${amount}${addSpace ? ' ' : ''}${suffix}`;
+};
+
+export default function formatDuration(duration = 0, units = 'ns', longForm = false) {
   const durationParts = {};
 
   // Moment only handles up to millisecond precision.
@@ -46,9 +57,7 @@ export default function formatDuration(duration = 0, units = 'ns') {
   const displayParts = allUnits.reduce((parts, unitType) => {
     if (durationParts[unitType.name]) {
       const count = durationParts[unitType.name];
-      const suffix =
-        count === 1 || !unitType.pluralizable ? unitType.suffix : unitType.suffix.pluralize();
-      parts.push(`${count}${unitType.pluralizable ? ' ' : ''}${suffix}`);
+      parts.push(pluralizeUnits(count, unitType, longForm));
     }
     return parts;
   }, []);
@@ -58,7 +67,5 @@ export default function formatDuration(duration = 0, units = 'ns') {
   }
 
   // When the duration is 0, show 0 in terms of `units`
-  const unitTypeForUnits = allUnits.findBy('suffix', units);
-  const suffix = unitTypeForUnits.pluralizable ? units.pluralize() : units;
-  return `0${unitTypeForUnits.pluralizable ? ' ' : ''}${suffix}`;
+  return pluralizeUnits(0, allUnits.findBy('suffix', units), longForm);
 }
