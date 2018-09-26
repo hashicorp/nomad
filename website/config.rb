069d45e6e0f094d296682804e2dc19d7bd3a4039
Preetha Appan <preetha@hashicorp.com>
2018-09-26 16:13:04 -0500

    Update website version

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.nomadproject.io/"
 
 activate :hashicorp do |h|
   h.name        = "nomad"
-  h.version     = "0.8.5"
+  h.version     = "0.8.6"
   h.github_slug = "hashicorp/nomad"
 end
 
