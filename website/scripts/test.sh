2566a81f12ca082ac56a42e602ade9791c1a1041
Chris Baker <cgbaker@hashicorp.com>
2018-10-08 15:02:15 -0400

    docs: modified docs wget testing to be a little less verbose

--- website/scripts/test.sh
+++ website/scripts/test.sh
@@ -3,6 +3,7 @@ set -e
 
 MMVERSION=${1-0.3.35}
 
+echo Running website in container...
 WEBSITE_CID=$(docker run \
     --detach \
     --rm \
@@ -10,8 +11,9 @@ WEBSITE_CID=$(docker run \
     --publish "35729:35729" \
     --volume "$PWD:/website" \
     hashicorp/middleman-hashicorp:${MMVERSION})
+echo Website running in container ${WEBSITE_CID}.
 
 trap 'echo Killing docker website container... && docker kill ${WEBSITE_CID}' EXIT HUP INT QUIT TERM
 
-echo Checking website for dead links...
-wget --recursive --delete-after http://localhost:4567
+echo Checking website for dead links using recursive wget...
+wget -nv --recursive --level=10 --delete-after http://localhost:4567
