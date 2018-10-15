7848acbea475c2cd2a8c91019a94f124fde4a0b1
Michael Schurter <schmichael@hashicorp.com>
2018-10-15 10:58:09 -0700

    register drivers by default
    Do not register mock_driver on release builds.

---
+++ plugins/shared/catalog/register.go
@@ -0,0 +1,10 @@
+package catalog
+
+import "github.com/hashicorp/nomad/drivers/rawexec"
+
+// This file is where all builtin plugins should be registered in the catalog.
+// Plugins with build restrictions should be placed in the appropriate
+// register_XXX.go file.
+func init() {
+	RegisterDeferredConfig(rawexec.PluginID, rawexec.PluginConfig, rawexec.PluginLoader)
+}
