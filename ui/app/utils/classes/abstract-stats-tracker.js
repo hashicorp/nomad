0a453c916408b63f1b9c326c5b868a6f37e6472e
Michael Lange <dingoeatingfuzz@gmail.com>
2018-11-01 22:08:57 -0700

    Gracefully handle response errors in stat trackers
    1. Check if the response is a 4xx/5xx
    2. If it is, skip the append step and track a frame miss
    3. If enough frame misses occur in a row, treat it as a pause
    
    A "pause" is when a null data frame is added, which shows up as a
    gap in line charts.

--- ui/app/utils/classes/abstract-stats-tracker.js
+++ ui/app/utils/classes/abstract-stats-tracker.js
@@ -1,11 +1,19 @@
 import Mixin from '@ember/object/mixin';
 import { assert } from '@ember/debug';
 import { task, timeout } from 'ember-concurrency';
+import jsonWithDefault from 'nomad-ui/utils/json-with-default';
 
 export default Mixin.create({
   url: '',
 
+  // The max number of data points tracked. Once the max is reached,
+  // data points at the head of the list are removed in favor of new
+  // data appended at the tail
   bufferSize: 500,
+
+  // The number of consecutive request failures that can occur before an
+  // empty frame is appended
+  maxFrameMisses: 5,
 
   fetch() {
     assert('StatsTrackers need a fetch method, which should have an interface like window.fetch');
@@ -23,6 +31,25 @@ export default Mixin.create({
     );
   },
 
+  frameMisses: 0,
+
+  handleResponse(frame) {
+    if (frame.error) {
+      this.incrementProperty('frameMisses');
+      if (this.get('frameMisses') >= this.get('maxFrameMisses')) {
+        // Missing enough data consecutively is effectively a pause
+        this.pause();
+        this.set('frameMisses', 0);
+      }
+      return;
+    } else {
+      this.set('frameMisses', 0);
+
+      // Only append non-error frames
+      this.append(frame);
+    }
+  },
+
   // Uses EC as a form of debounce to prevent multiple
   // references to the same tracker from flooding the tracker,
   // but also avoiding the issue where different places where the
@@ -36,8 +63,8 @@ export default Mixin.create({
       assert('Url must be defined', url);
 
       yield this.get('fetch')(url)
-        .then(res => res.json())
-        .then(frame => this.append(frame));
+        .then(jsonWithDefault({ error: true }))
+        .then(frame => this.handleResponse(frame));
     } catch (error) {
       throw new Error(error);
     }
