1226a079f91c193b68805ba3cf3e86a5c5fbf730
Michael Lange <dingoeatingfuzz@gmail.com>
2018-11-05 15:20:31 -0800

    Handle the job 404 case in the job definition route

--- ui/tests/pages/jobs/job/definition.js
+++ ui/tests/pages/jobs/job/definition.js
@@ -1,4 +1,4 @@
-import { create, isPresent, visitable, clickable } from 'ember-cli-page-object';
+import { create, isPresent, visitable, clickable, text } from 'ember-cli-page-object';
 
 import jobEditor from 'nomad-ui/tests/pages/components/job-editor';
 
@@ -9,4 +9,11 @@ export default create({
   editor: jobEditor(),
 
   edit: clickable('[data-test-edit-job]'),
+
+  error: {
+    isPresent: isPresent('[data-test-error]'),
+    title: text('[data-test-error-title]'),
+    message: text('[data-test-error-message]'),
+    seekHelp: clickable('[data-test-error-message] a'),
+  },
 });
