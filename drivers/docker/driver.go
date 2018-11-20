2c7bea7190aea1b0770732cdccce819979f54811
Mahmood Ali <mahmood@hashicorp.com>
2018-11-20 09:49:50 -0500

    Add support for storage opt

--- drivers/docker/driver.go
+++ drivers/docker/driver.go
@@ -627,6 +627,7 @@ func (d *Driver) createContainerConfig(task *drivers.TaskConfig, driverConfig *T
 		// used to share data between different tasks in the same task group.
 		Binds: binds,
 
+		StorageOpt:   driverConfig.StorageOpt,
 		VolumeDriver: driverConfig.VolumeDriver,
 
 		PidsLimit: driverConfig.PidsLimit,
