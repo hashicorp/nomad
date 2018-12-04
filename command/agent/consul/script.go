hashicorp/nomad

commit d335a82859ca2177bc6deda0c2c85b559daf2db3

client: begin driver plugin integration
client: fingerprint driver plugins

command/agent/consul/script.go
@@ -8,7 +8,7 @@ import (
 	log "github.com/hashicorp/go-hclog"
 
 	"github.com/hashicorp/consul/api"
-	"github.com/hashicorp/nomad/client/driver"
+	"github.com/hashicorp/nomad/client/allocrunnerv2/taskrunner/interfaces"
 	"github.com/hashicorp/nomad/nomad/structs"
 )
 
@@ -39,7 +39,7 @@ type scriptCheck struct {
 
 	id    string
 	check *structs.ServiceCheck
-	exec  driver.ScriptExecutor
+	exec  interfaces.ScriptExecutor
 	agent heartbeater
 
 	// lastCheckOk is true if the last check was ok; otherwise false
@@ -52,7 +52,7 @@ type scriptCheck struct {
 // newScriptCheck creates a new scriptCheck. run() should be called once the
 // initial check is registered with Consul.
 func newScriptCheck(allocID, taskName, checkID string, check *structs.ServiceCheck,
-	exec driver.ScriptExecutor, agent heartbeater, logger log.Logger,
+	exec interfaces.ScriptExecutor, agent heartbeater, logger log.Logger,
 	shutdownCh <-chan struct{}) *scriptCheck {
 
 	logger = logger.ResetNamed("consul.checks").With("task", taskName, "alloc_id", allocID, "check", check.Name)
@@ -93,9 +93,8 @@ func (s *scriptCheck) run() *scriptHandle {
 			metrics.IncrCounter([]string{"client", "consul", "script_runs"}, 1)
 
 			// Execute check script with timeout
-			execctx, cancel := context.WithTimeout(ctx, s.check.Timeout)
-			output, code, err := s.exec.Exec(execctx, s.check.Command, s.check.Args)
-			switch execctx.Err() {
+			output, code, err := s.exec.Exec(s.check.Timeout, s.check.Command, s.check.Args)
+			switch err {
 			case context.Canceled:
 				// check removed during execution; exit
 				cancel()

client/allocdir/task_dir.go
@@ -13,6 +13,9 @@ import (
 // TaskDir contains all of the paths relevant to a task. All paths are on the
 // host system so drivers should mount/link into task containers as necessary.
 type TaskDir struct {
+	// AllocDir is the path to the alloc directory on the host
+	AllocDir string
+
 	// Dir is the path to Task directory on the host
 	Dir string
 
@@ -50,6 +53,7 @@ func newTaskDir(logger hclog.Logger, allocDir, taskName string) *TaskDir {
 	logger = logger.Named("task_dir").With("task_name", taskName)
 
 	return &TaskDir{
+		AllocDir:       allocDir,
 		Dir:            taskDir,
 		SharedAllocDir: filepath.Join(allocDir, SharedAllocName),
 		LogDir:         filepath.Join(allocDir, SharedAllocName, LogDirName),

client/allocrunner/config.go
@@ -9,6 +9,7 @@ import (
 	cstate "github.com/hashicorp/nomad/client/state"
 	"github.com/hashicorp/nomad/client/vaultclient"
 	"github.com/hashicorp/nomad/nomad/structs"
+	"github.com/hashicorp/nomad/plugins/shared/loader"
 )
 
 // Config holds the configuration for creating an allocation runner.
@@ -37,4 +38,11 @@ type Config struct {
 	// PrevAllocWatcher handles waiting on previous allocations and
 	// migrating their ephemeral disk when necessary.
 	PrevAllocWatcher allocwatcher.PrevAllocWatcher
+
+	// PluginLoader is used to load plugins.
+	PluginLoader loader.PluginCatalog
+
+	// PluginSingletonLoader is a plugin loader that will returns singleton
+	// instances of the plugins.
+	PluginSingletonLoader loader.PluginCatalog
 }

client/allocrunner/interfaces/task_lifecycle.go
@@ -3,7 +3,7 @@ package interfaces
 import (
 	"context"
 
-	"github.com/hashicorp/nomad/client/driver"
+	"github.com/hashicorp/nomad/client/allocrunnerv2/taskrunner/interfaces"
 	"github.com/hashicorp/nomad/client/driver/env"
 	cstructs "github.com/hashicorp/nomad/client/structs"
 	"github.com/hashicorp/nomad/nomad/structs"
@@ -82,7 +82,7 @@ type DriverStats interface {
 
 type TaskPoststartRequest struct {
 	// Exec hook (may be nil)
-	DriverExec driver.ScriptExecutor
+	DriverExec interfaces.ScriptExecutor
 
 	// Network info (may be nil)
 	DriverNetwork *cstructs.DriverNetwork

client/allocrunner/taskrunner/driver_handle.go
@@ -1,0 +1,64 @@
+package taskrunner
+
+import (
+	"context"
+	"time"
+
+	"github.com/hashicorp/nomad/client/allocrunnerv2/taskrunner/interfaces"
+	cstructs "github.com/hashicorp/nomad/client/structs"
+	"github.com/hashicorp/nomad/nomad/structs"
+	"github.com/hashicorp/nomad/plugins/drivers"
+)
+
+func NewDriverHandle(driver drivers.DriverPlugin, taskID string, task *structs.Task, net *cstructs.DriverNetwork) interfaces.DriverHandle {
+	return &driverHandleImpl{
+		driver: driver,
+		net:    net,
+		taskID: taskID,
+		task:   task,
+	}
+}
+
+type driverHandleImpl struct {
+	driver drivers.DriverPlugin
+	net    *cstructs.DriverNetwork
+	task   *structs.Task
+	taskID string
+}
+
+func (h *driverHandleImpl) ID() string {
+	return h.taskID
+}
+
+func (h *driverHandleImpl) WaitCh(ctx context.Context) (<-chan *drivers.ExitResult, error) {
+	return h.driver.WaitTask(ctx, h.taskID)
+}
+
+func (h *driverHandleImpl) Update(task *structs.Task) error {
+	return nil
+}
+
+func (h *driverHandleImpl) Kill() error {
+	return h.driver.StopTask(h.taskID, h.task.KillTimeout, h.task.KillSignal)
+}
+
+func (h *driverHandleImpl) Stats() (*cstructs.TaskResourceUsage, error) {
+	return h.driver.TaskStats(h.taskID)
+}
+
+func (h *driverHandleImpl) Signal(s string) error {
+	return h.driver.SignalTask(h.taskID, s)
+}
+
+func (h *driverHandleImpl) Exec(timeout time.Duration, cmd string, args []string) ([]byte, int, error) {
+	command := append([]string{cmd}, args...)
+	res, err := h.driver.ExecTask(h.taskID, command, timeout)
+	if err != nil {
+		return nil, 0, err
+	}
+	return res.Stdout, res.ExitResult.ExitCode, res.ExitResult.Err
+}
+
+func (h *driverHandleImpl) Network() *cstructs.DriverNetwork {
+	return h.net
+}

client/allocrunner/taskrunner/interfaces/handle.go
@@ -1,0 +1,47 @@
+package interfaces
+
+import (
+	"context"
+	"time"
+
+	cstructs "github.com/hashicorp/nomad/client/structs"
+	"github.com/hashicorp/nomad/nomad/structs"
+	"github.com/hashicorp/nomad/plugins/drivers"
+)
+
+// DriverHandle wraps operations to a driver such that they are operated on a specific
+// task
+type DriverHandle interface {
+	// ID returns the task ID
+	ID() string
+
+	// WaitCh is used to return a channel used to wait for task completion
+	WaitCh(context.Context) (<-chan *drivers.ExitResult, error)
+
+	// Update is used to update the task if possible and update task related
+	// configurations.
+	Update(task *structs.Task) error
+
+	// Kill is used to stop the task
+	Kill() error
+
+	// Stats returns aggregated stats of the driver
+	Stats() (*cstructs.TaskResourceUsage, error)
+
+	// Signal is used to send a signal to the task
+	Signal(s string) error
+
+	// ScriptExecutor is an interface used to execute commands such as
+	// health check scripts in the a DriverHandle's context.
+	ScriptExecutor
+
+	// Network returns the driver's network or nil if the driver did not
+	// create a network.
+	Network() *cstructs.DriverNetwork
+}
+
+// ScriptExecutor is an interface that supports Exec()ing commands in the
+// driver's context. Split out of DriverHandle to ease testing.
+type ScriptExecutor interface {
+	Exec(timeout time.Duration, cmd string, args []string) ([]byte, int, error)
+}

client/allocrunner/taskrunner/interfaces/lifecycle.go
@@ -2,13 +2,12 @@ package interfaces
 
 import (
 	"context"
-	"os"
 
 	"github.com/hashicorp/nomad/nomad/structs"
 )
 
 type TaskLifecycle interface {
 	Restart(ctx context.Context, event *structs.TaskEvent, failure bool) error
-	Signal(event *structs.TaskEvent, s os.Signal) error
+	Signal(event *structs.TaskEvent, signal string) error
 	Kill(ctx context.Context, event *structs.TaskEvent) error
 }

client/allocrunner/taskrunner/lifecycle.go
@@ -2,17 +2,16 @@ package taskrunner
 
 import (
 	"context"
-	"os"
 
 	"github.com/hashicorp/nomad/nomad/structs"
+	"github.com/hashicorp/nomad/plugins/drivers"
 )
 
 // Restart a task. Returns immediately if no task is running. Blocks until
 // existing task exits or passed-in context is canceled.
 func (tr *TaskRunner) Restart(ctx context.Context, event *structs.TaskEvent, failure bool) error {
 	// Grab the handle
-	handle, result := tr.getDriverHandle()
-
+	handle := tr.getDriverHandle()
 	// Check it is running
 	if handle == nil {
 		return ErrTaskNotRunning
@@ -32,13 +31,17 @@ func (tr *TaskRunner) Restart(ctx context.Context, event *structs.TaskEvent, fai
 	}
 
 	// Drain the wait channel or wait for the request context to be canceled
-	result.Wait(ctx)
+	waitCh, err := handle.WaitCh(ctx)
+	if err != nil {
+		tr.logger.Error("failed to kill task. Resources may have been leaked", "error", err)
+	}
+	<-waitCh
 	return nil
 }
 
-func (tr *TaskRunner) Signal(event *structs.TaskEvent, s os.Signal) error {
+func (tr *TaskRunner) Signal(event *structs.TaskEvent, s string) error {
 	// Grab the handle
-	handle, _ := tr.getDriverHandle()
+	handle := tr.getDriverHandle()
 
 	// Check it is running
 	if handle == nil {
@@ -49,6 +52,7 @@ func (tr *TaskRunner) Signal(event *structs.TaskEvent, s os.Signal) error {
 	tr.EmitEvent(event)
 
 	// Send the signal
+
 	return handle.Signal(s)
 }
 
@@ -60,9 +64,9 @@ func (tr *TaskRunner) Kill(ctx context.Context, event *structs.TaskEvent) error 
 	tr.ctxCancel()
 
 	// Grab the handle
-	handle, result := tr.getDriverHandle()
+	handle := tr.getDriverHandle()
 
-	// Check if the handle is running
+	// Check it is running
 	if handle == nil {
 		return ErrTaskNotRunning
 	}
@@ -84,7 +88,17 @@ func (tr *TaskRunner) Kill(ctx context.Context, event *structs.TaskEvent) error 
 	}
 
 	// Block until task has exited.
-	result.Wait(ctx)
+	waitCh, err := handle.WaitCh(ctx)
+
+	// The task may have already been cleaned up
+	if err != nil && err != drivers.ErrTaskNotFound {
+		tr.logger.Error("failed to wait on task. Resources may have been leaked", "error", err)
+		return err
+	}
+
+	if waitCh != nil {
+		<-waitCh
+	}
 
 	// Store that the task has been destroyed and any associated error.
 	tr.UpdateState(structs.TaskStateDead, structs.NewTaskEvent(structs.TaskKilled).SetKillError(destroyErr))

commit 104bbf78d97617562370e435e1f8b2dea15eb4af

consul: fix script checks exiting after 1 run
Fixes a regression caused in d335a82859ca2177bc6deda0c2c85b559daf2db3

The removal of the inner context made the remaining cancels cancel the
outer context and cause script checks to exit prematurely.

command/agent/consul/script.go
@@ -97,7 +97,6 @@ func (s *scriptCheck) run() *scriptHandle {
 			switch err {
 			case context.Canceled:
 				// check removed during execution; exit
-				cancel()
 				return
 			case context.DeadlineExceeded:
 				metrics.IncrCounter([]string{"client", "consul", "script_timeouts"}, 1)
@@ -111,9 +110,6 @@ func (s *scriptCheck) run() *scriptHandle {
 				// failures
 				s.logger.Warn("check timed out", "timeout", s.check.Timeout)
 			}
-
-			// cleanup context
-			cancel()
 
 			state := api.HealthCritical
 			switch code {

command/agent/consul/unit_test.go
@@ -777,10 +777,6 @@ func TestConsul_RegServices(t *testing.T) {
 // TestConsul_ShutdownOK tests the ok path for the shutdown logic in
 // ServiceClient.
 func TestConsul_ShutdownOK(t *testing.T) {
-	// FIXME: This test is failing now because checks are called once only
-	// not sure what changed
-	t.Skip("FIXME: unexpected failing test")
-
 	require := require.New(t)
 	ctx := setupFake(t)
 
@@ -832,7 +828,7 @@ func TestConsul_ShutdownOK(t *testing.T) {
 		t.Fatalf("expected 1 checkTTL entry but found: %d", n)
 	}
 	for _, v := range ctx.FakeConsul.checkTTLs {
-		require.Equalf(2, v, "expected 2 updates but foud %d", v)
+		require.Equalf(2, v, "expected 2 updates but found %d", v)
 	}
 	for _, v := range ctx.FakeConsul.checks {
 		if v.Status != "passing" {