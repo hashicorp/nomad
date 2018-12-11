8f2454029acc9d991f121580107957e85fc1dcb3
Mahmood Ali <mahmood@hashicorp.com>
2018-12-11 15:13:43 -0500

    tests: skip checking rdma cgroup
    rdma was added in most recent kernels and libcontainer/docker
    don't isolate them by default.

--- drivers/exec/driver_test.go
+++ drivers/exec/driver_test.go
@@ -466,8 +466,9 @@ func TestExecDriver_HandlerExec(t *testing.T) {
 		if line == "" {
 			continue
 		}
-		// Skip systemd cgroup
-		if strings.HasPrefix(line, "1:name=systemd") {
+		// Skip systemd and rdma cgroups; rdma was added in most recent kernels and libcontainer/docker
+		// don't isolate them by default.
+		if strings.HasPrefix(line, "1:name=systemd") || strings.Contains(line, ":rdma:") {
 			continue
 		}
 		if !strings.Contains(line, ":/nomad/") {
