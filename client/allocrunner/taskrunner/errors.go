4d4201331c041cc1f6af32abe9495878b5a0ef9b
Danielle Tomlinson <dani@hashicorp.com>
2018-12-13 18:20:18 +0100

    taskrunner: Emit task events when a hook fails

--- client/allocrunner/taskrunner/errors.go
+++ client/allocrunner/taskrunner/errors.go
@@ -1,6 +1,10 @@
 package taskrunner
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/hashicorp/nomad/nomad/structs"
+)
 
 const (
 	errTaskNotRunning = "Task not running"
@@ -9,3 +13,32 @@ const (
 var (
 	ErrTaskNotRunning = errors.New(errTaskNotRunning)
 )
+
+// HookError is an error interface that can be used by a TaskRunner hook to
+// emit custom task events when an error occurs.
+type HookError interface {
+	TaskEvent() *structs.TaskEvent
+	Error() string
+}
+
+// NewHookError returns an implementation of a HookError with an underlying err
+// and a pre-formatted task event.
+func NewHookError(err error, taskEvent *structs.TaskEvent) error {
+	return &hookError{
+		err:       err,
+		taskEvent: taskEvent,
+	}
+}
+
+type hookError struct {
+	taskEvent *structs.TaskEvent
+	err       error
+}
+
+func (h *hookError) TaskEvent() *structs.TaskEvent {
+	return h.taskEvent
+}
+
+func (h *hookError) Error() string {
+	return h.err.Error()
+}
