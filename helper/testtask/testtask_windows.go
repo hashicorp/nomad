9e4e2d812d4d0982b87c66afd7cbc364d4d733b9
Michael Schurter <michael.schurter@gmail.com>
2019-01-15 22:01:59 +0100

    Update helper/testtask/testtask_windows.go
    Co-Authored-By: dantoml <dani@tomlinson.io>

--- helper/testtask/testtask_windows.go
+++ helper/testtask/testtask_windows.go
@@ -8,6 +8,7 @@ import (
 )
 
 func executeProcessGroup(gid string) {
-	fmt.Fprintf(os.Stderr, "process groups are not supported on windows\n")
+	fmt.Fprintf(os.Stderr, "TODO: implement process groups are on windows\n")
+	fmt.Fprintf(os.Stderr, "TODO: see https://github.com/hashicorp/nomad/blob/109c5ef650206fc62334d202002cda92ceb67399/drivers/shared/executor/executor_windows.go#L9-L17\n")
 	os.Exit(1)
 }
