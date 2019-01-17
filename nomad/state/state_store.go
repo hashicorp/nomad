946d47712bfc2ac8da04465fdcaed846790751b2
Preetha Appan <preetha@hashicorp.com>
2019-01-17 15:36:33 -0600

    fix linting

--- nomad/state/state_store.go
+++ nomad/state/state_store.go
@@ -6,12 +6,13 @@ import (
 	"sort"
 	"time"
 
+	"reflect"
+
 	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-memdb"
 	multierror "github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/nomad/helper"
 	"github.com/hashicorp/nomad/nomad/structs"
-	"reflect"
 )
 
 // Txn is a transaction against a state store.
