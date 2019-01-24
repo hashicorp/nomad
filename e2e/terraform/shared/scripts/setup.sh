de84a2e4637360270cf96dbcb734e9135403c54b
Preetha Appan <preetha@hashicorp.com>
2019-01-24 09:52:15 -0600

    update to Consul 1.4.0 for e2e tests

--- e2e/terraform/shared/scripts/setup.sh
+++ e2e/terraform/shared/scripts/setup.sh
@@ -9,7 +9,7 @@ cd /ops
 
 CONFIGDIR=/ops/shared/config
 
-CONSULVERSION=1.3.1
+CONSULVERSION=1.4.0
 CONSULDOWNLOAD=https://releases.hashicorp.com/consul/${CONSULVERSION}/consul_${CONSULVERSION}_linux_amd64.zip
 CONSULCONFIGDIR=/etc/consul.d
 CONSULDIR=/opt/consul
