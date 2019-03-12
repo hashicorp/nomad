fe165a5d1cb9163f26d1f66c42aefcdd3e71df24
Michael Lange <dingoeatingfuzz@gmail.com>
2019-03-12 16:42:47 -0700

    Upgrade to Ember 3.0

--- ui/tests/helpers/destroy-app.js
+++
@@ -1,8 +0,0 @@
-import { run } from '@ember/runloop';
-
-export default function destroyApp(application) {
-  run(application, 'destroy');
-  if (window.server) {
-    window.server.shutdown();
-  }
-}
