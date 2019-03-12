71bcfaa0aafc21b64590e4b82633e2986604c32d
Michael Lange <dingoeatingfuzz@gmail.com>
2019-03-12 16:42:47 -0700

    Upgrade to Ember 3.0

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -26,4 +26,23 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    // node files
+    {
+      files: [
+        'ember-cli-build.js',
+        'testem.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
+  ]
 };
