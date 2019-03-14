2f81f3b713751c71fac58a193249b610494597d3
Michael Lange <dingoeatingfuzz@gmail.com>
2019-03-13 19:17:12 -0700

    Update serializer unit tests to use module instead of custom code

--- ui/tests/unit/serializers/job-plan-test.js
+++ ui/tests/unit/serializers/job-plan-test.js
@@ -1,94 +1,92 @@
-import { test } from 'qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 import JobPlanModel from 'nomad-ui/models/job-plan';
-import moduleForSerializer from '../../helpers/module-for-serializer';
 
-moduleForSerializer('job-plan', 'Unit | Serializer | JobPlan', {
-  needs: [
-    'service:token',
-    'service:system',
-    'serializer:job-plan',
-    'transform:fragment-array',
-    'model:placement-failure',
-  ],
-});
+module('Unit | Serializer | JobPlan', function(hooks) {
+  setupTest(hooks);
+  hooks.beforeEach(function() {
+    this.store = this.owner.lookup('service:store');
+    this.subject = () => this.store.serializerFor('job-plan');
+  });
 
-const normalizationTestCases = [
-  {
-    name: 'Normal',
-    in: {
-      ID: 'test-plan',
-      Diff: {
-        Arbitrary: 'Value',
-      },
-      FailedTGAllocs: {
-        taskGroup: {
-          NodesAvailable: 10,
+  const normalizationTestCases = [
+    {
+      name: 'Normal',
+      in: {
+        ID: 'test-plan',
+        Diff: {
+          Arbitrary: 'Value',
         },
-      },
-    },
-    out: {
-      data: {
-        id: 'test-plan',
-        type: 'job-plan',
-        attributes: {
-          diff: {
-            Arbitrary: 'Value',
+        FailedTGAllocs: {
+          taskGroup: {
+            NodesAvailable: 10,
           },
-          failedTGAllocs: [
-            {
-              name: 'taskGroup',
-              nodesAvailable: 10,
+        },
+      },
+      out: {
+        data: {
+          id: 'test-plan',
+          type: 'job-plan',
+          attributes: {
+            diff: {
+              Arbitrary: 'Value',
             },
-          ],
-        },
-        relationships: {},
-      },
-    },
-  },
-
-  {
-    name: 'Dots in task names',
-    in: {
-      ID: 'test-plan',
-      Diff: {
-        Arbitrary: 'Value',
-      },
-      FailedTGAllocs: {
-        'one.two': {
-          NodesAvailable: 10,
-        },
-        'three.four': {
-          NodesAvailable: 25,
-        },
-      },
-    },
-    out: {
-      data: {
-        id: 'test-plan',
-        type: 'job-plan',
-        attributes: {
-          diff: {
-            Arbitrary: 'Value',
+            failedTGAllocs: [
+              {
+                name: 'taskGroup',
+                nodesAvailable: 10,
+              },
+            ],
           },
-          failedTGAllocs: [
-            {
-              name: 'one.two',
-              nodesAvailable: 10,
-            },
-            {
-              name: 'three.four',
-              nodesAvailable: 25,
-            },
-          ],
+          relationships: {},
         },
-        relationships: {},
       },
     },
-  },
-];
 
-normalizationTestCases.forEach(testCase => {
-  test(`normalization: ${testCase.name}`, function(assert) {
-    assert.deepEqual(this.subject().normalize(JobPlanModel, testCase.in), testCase.out);
+    {
+      name: 'Dots in task names',
+      in: {
+        ID: 'test-plan',
+        Diff: {
+          Arbitrary: 'Value',
+        },
+        FailedTGAllocs: {
+          'one.two': {
+            NodesAvailable: 10,
+          },
+          'three.four': {
+            NodesAvailable: 25,
+          },
+        },
+      },
+      out: {
+        data: {
+          id: 'test-plan',
+          type: 'job-plan',
+          attributes: {
+            diff: {
+              Arbitrary: 'Value',
+            },
+            failedTGAllocs: [
+              {
+                name: 'one.two',
+                nodesAvailable: 10,
+              },
+              {
+                name: 'three.four',
+                nodesAvailable: 25,
+              },
+            ],
+          },
+          relationships: {},
+        },
+      },
+    },
+  ];
+
+  normalizationTestCases.forEach(testCase => {
+    test(`normalization: ${testCase.name}`, function(assert) {
+      assert.deepEqual(this.subject().normalize(JobPlanModel, testCase.in), testCase.out);
+    });
   });
 });
