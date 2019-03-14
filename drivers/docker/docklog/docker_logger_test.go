a1f73909642888a1904cea96474da41e25e190e8
Mahmood Ali <mahmood@hashicorp.com>
2019-03-14 09:58:01 -0400

    Handle when cannot fetch docker logs
    Fix #5418
    
    When using a docker logger that doesn't support log streaming through
    API, currently docker logger runs a tight loop of Docker API calls
    unexpectedly.  This change ensures we stop fetching logs early.
    
    Also, this adds some basic backoff strategy when Docker API logging
    fails unexpectedly, to avoid accidentally DoSing the docker daemon.

--- drivers/docker/docklog/docker_logger_test.go
+++ drivers/docker/docklog/docker_logger_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"runtime"
 	"testing"
+	"time"
 
 	docker "github.com/fsouza/go-dockerclient"
 	ctu "github.com/hashicorp/nomad/client/testutil"
@@ -24,7 +25,7 @@ func testContainerDetails() (image string, imageName string, imageTag string) {
 	return "busybox:1", "busybox", "1"
 }
 
-func TestDockerLogger(t *testing.T) {
+func TestDockerLogger_Success(t *testing.T) {
 	ctu.DockerCompatible(t)
 
 	t.Parallel()
@@ -43,9 +44,7 @@ func TestDockerLogger(t *testing.T) {
 			Repository: containerImageName,
 			Tag:        containerImageTag,
 		}, docker.AuthConfiguration{})
-		if err != nil {
-			t.Fatalf("failed to pull image: %v", err)
-		}
+		require.NoError(err, "failed to pull image")
 	}
 
 	containerConf := docker.CreateContainerOptions{
@@ -111,7 +110,7 @@ func echoToContainer(t *testing.T, client *docker.Client, id string, line string
 	op := docker.CreateExecOptions{
 		Container: id,
 		Cmd: []string{
-			"ash", "-c",
+			"/bin/sh", "-c",
 			fmt.Sprintf("echo %s >>~/docklog", line),
 		},
 	}
@@ -121,10 +120,115 @@ func echoToContainer(t *testing.T, client *docker.Client, id string, line string
 	require.NoError(t, client.StartExec(exec.ID, docker.StartExecOptions{Detach: true}))
 }
 
+func TestDockerLogger_LoggingNotSupported(t *testing.T) {
+	ctu.DockerCompatible(t)
+
+	t.Parallel()
+	require := require.New(t)
+
+	containerImage, containerImageName, containerImageTag := testContainerDetails()
+
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		t.Skip("docker unavailable:", err)
+	}
+
+	if img, err := client.InspectImage(containerImage); err != nil || img == nil {
+		t.Log("image not found locally, downloading...")
+		err = client.PullImage(docker.PullImageOptions{
+			Repository: containerImageName,
+			Tag:        containerImageTag,
+		}, docker.AuthConfiguration{})
+		require.NoError(err, "failed to pull image")
+	}
+
+	containerConf := docker.CreateContainerOptions{
+		Config: &docker.Config{
+			Cmd: []string{
+				"sh", "-c", "touch ~/docklog; tail -f ~/docklog",
+			},
+			Image: containerImage,
+		},
+		HostConfig: &docker.HostConfig{
+			LogConfig: docker.LogConfig{
+				Type: "gelf",
+				Config: map[string]string{
+					"gelf-address":    "udp://localhost:12201",
+					"mode":            "non-blocking",
+					"max-buffer-size": "4m",
+				},
+			},
+		},
+		Context: context.Background(),
+	}
+
+	container, err := client.CreateContainer(containerConf)
+	require.NoError(err)
+
+	defer client.RemoveContainer(docker.RemoveContainerOptions{
+		ID:    container.ID,
+		Force: true,
+	})
+
+	err = client.StartContainer(container.ID, nil)
+	require.NoError(err)
+
+	testutil.WaitForResult(func() (bool, error) {
+		container, err = client.InspectContainer(container.ID)
+		if err != nil {
+			return false, err
+		}
+		if !container.State.Running {
+			return false, fmt.Errorf("container not running")
+		}
+		return true, nil
+	}, func(err error) {
+		require.NoError(err)
+	})
+
+	stdout := &noopCloser{bytes.NewBuffer(nil)}
+	stderr := &noopCloser{bytes.NewBuffer(nil)}
+
+	dl := NewDockerLogger(testlog.HCLogger(t)).(*dockerLogger)
+	dl.stdout = stdout
+	dl.stderr = stderr
+	require.NoError(dl.Start(&StartOpts{
+		ContainerID: container.ID,
+	}))
+
+	select {
+	case <-dl.doneCh:
+	case <-time.After(10 * time.Second):
+		require.Fail("timedout while waiting for docker_logging to terminate")
+	}
+}
+
 type noopCloser struct {
 	*bytes.Buffer
 }
 
 func (*noopCloser) Close() error {
 	return nil
+}
+
+func TestNextBackoff(t *testing.T) {
+	cases := []struct {
+		currentBackoff float64
+		min            float64
+		max            float64
+	}{
+		{0.0, 0.5, 1.15},
+		{5.0, 5.0, 16},
+		{120, 120, 120},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("case %v", c.currentBackoff), func(t *testing.T) {
+			next := nextBackoff(c.currentBackoff)
+			t.Logf("computed backoff(%v) = %v", c.currentBackoff, next)
+
+			require.True(t, next >= c.min, "next backoff is smaller than expected")
+			require.True(t, next <= c.max, "next backoff is larger than expected")
+		})
+	}
 }
