43a9b6d86509baa516943e49ca0195ba65421cad
Michael Lange <dingoeatingfuzz@gmail.com>
2019-03-13 19:15:25 -0700

    Update environment to 3.0 conventions

--- ui/config/environment.js
+++ ui/config/environment.js
@@ -51,6 +51,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.autoboot = false;
 
     ENV.browserify = {
       tests: true,
