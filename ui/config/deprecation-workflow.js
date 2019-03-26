ad13270e215f0238cb6fc3ede5a3132448a53811
Michael Lange <dingoeatingfuzz@gmail.com>
2019-03-25 22:27:47 -0700

    Address the Ember.Logger deprecations

--- ui/config/deprecation-workflow.js
+++ ui/config/deprecation-workflow.js
@@ -4,7 +4,7 @@ self.deprecationWorkflow.config = {
   workflow: [
     { handler: 'throw', matchId: 'ember-inflector.globals' },
     { handler: 'throw', matchId: 'ember-runtime.deprecate-copy-copyable' },
-    { handler: 'silence', matchId: 'ember-console.deprecate-logger' },
+    { handler: 'throw', matchId: 'ember-console.deprecate-logger' },
     { handler: 'silence', matchId: 'ember-component.send-action' },
   ],
 };
