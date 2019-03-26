23729ad5a7d0cb1c1383d8329303433bed5ddfe4
Michael Lange <dingoeatingfuzz@gmail.com>
2019-03-25 21:55:06 -0700

    Address the copy/copyable deprecation

--- ui/config/deprecation-workflow.js
+++ ui/config/deprecation-workflow.js
@@ -3,7 +3,7 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
     { handler: 'throw', matchId: 'ember-inflector.globals' },
-    { handler: 'silence', matchId: 'ember-runtime.deprecate-copy-copyable' },
+    { handler: 'throw', matchId: 'ember-runtime.deprecate-copy-copyable' },
     { handler: 'silence', matchId: 'ember-console.deprecate-logger' },
     { handler: 'silence', matchId: 'ember-component.send-action' },
   ],
