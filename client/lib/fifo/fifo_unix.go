0b82a4a708bc53271ca42545fb3cbbea2d3e7900
Mahmood Ali <mahmood@hashicorp.com>
2019-03-31 12:50:06 -0400

    basic fifo file

--- client/lib/fifo/fifo_unix.go
+++ client/lib/fifo/fifo_unix.go
@@ -3,23 +3,36 @@
 package fifo
 
 import (
-	"context"
 	"io"
 	"os"
 	"syscall"
 
-	cfifo "github.com/containerd/fifo"
+	"github.com/pkg/errors"
+	"golang.org/x/sys/unix"
 )
 
-// New creates a fifo at the given path and returns an io.ReadWriteCloser for it
-// The fifo must not already exist
-func New(path string) (io.ReadWriteCloser, error) {
-	return cfifo.OpenFifo(context.Background(), path, syscall.O_RDONLY|syscall.O_CREAT|syscall.O_NONBLOCK, 0600)
+// New creates a fifo at the given path and returns an os.File for it
+// The fifo must not exist already, or that it's already a fifo file
+//
+// It returns a file open file that may block until a writer opens
+// so it's advised to run it in a goroutine different from reader goroutine
+func New(path string) (func() (io.ReadCloser, error), error) {
+	// create first
+	if err := mkfifo(path, 0600); err != nil && !os.IsExist(err) {
+		return nil, errors.Wrapf(err, "error creating fifo %v", path)
+	}
+
+	openFn := func() (io.ReadCloser, error) {
+		return os.OpenFile(path, syscall.O_RDONLY, os.ModeNamedPipe)
+	}
+
+	return openFn, nil
 }
 
-// Open opens a fifo that already exists and returns an io.ReadWriteCloser for it
-func Open(path string) (io.ReadWriteCloser, error) {
-	return cfifo.OpenFifo(context.Background(), path, syscall.O_WRONLY|syscall.O_NONBLOCK, 0600)
+// Open opens a fifo as a reader that already exists and returns an io.ReadWriteCloser for it
+func Open(path string) (io.WriteCloser, error) {
+	return os.OpenFile(path, syscall.O_WRONLY, os.ModeNamedPipe)
+	io.Copy(nil, nil)
 }
 
 // Remove a fifo that already exists at a given path
@@ -33,4 +46,8 @@ func IsClosedErr(err error) bool {
 		return err2.Err == os.ErrClosed
 	}
 	return false
+}
+
+func mkfifo(path string, mode uint32) (err error) {
+	return unix.Mkfifo(path, mode)
 }
