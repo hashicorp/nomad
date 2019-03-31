5ca9b6eb372e0ff20428b0595376f7ea04a3b227
Mahmood Ali <mahmood@hashicorp.com>
2019-03-31 12:50:06 -0400

    fifo: Use plain fifo file in Unix
    This PR switches to using plain fifo files instead of golang structs
    managed by containerd/fifo library.
    
    The library main benefit is management of opening fifo files.  In Linux,
    a reader `open()` request would block until a writer opens the file (and
    vice-versa).  The library uses goroutines so that it's the first IO
    operation that blocks.
    
    This benefit isn't really useful for us: Given that logmon simply
    streams output in a separate process, blocking of opening or first read
    is effectively the same.
    
    The library additionally makes further complications for managing state
    and tracking read/write permission that seems overhead for our use,
    compared to using a file directly.
    
    Looking here, I made the following incidental changes:
    * document that we do handle if fifo files are already created, as we
    rely on that behavior for logmon restarts
    * use type system to lock read vs write: currently, fifo library returns
    `io.ReadWriteCloser` even if fifo is opened for writing only!

--- client/logmon/logmon.go
+++ client/logmon/logmon.go
@@ -177,10 +177,12 @@ func NewTaskLogger(cfg *LogConfig, logger hclog.Logger) (*TaskLogger, error) {
 // data will be copied from the reader to the rotator.
 type logRotatorWrapper struct {
 	fifoPath          string
-	processOutReader  io.ReadCloser
 	rotatorWriter     *logging.FileRotator
 	hasFinishedCopied chan struct{}
 	logger            hclog.Logger
+
+	processOutReader io.ReadCloser
+	opened           chan struct{}
 }
 
 // isRunning will return true until the reader is closed
@@ -197,29 +199,38 @@ func (l *logRotatorWrapper) isRunning() bool {
 // processOutWriter to attach to the stdout or stderr of a process.
 func newLogRotatorWrapper(path string, logger hclog.Logger, rotator *logging.FileRotator) (*logRotatorWrapper, error) {
 	logger.Info("opening fifo", "path", path)
-	f, err := fifo.New(path)
+	fifoOpenFn, err := fifo.New(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create fifo for extracting logs: %v", err)
 	}
 
 	wrap := &logRotatorWrapper{
 		fifoPath:          path,
-		processOutReader:  f,
 		rotatorWriter:     rotator,
 		hasFinishedCopied: make(chan struct{}),
+		opened:            make(chan struct{}),
 		logger:            logger,
 	}
-	wrap.start()
+	wrap.start(fifoOpenFn)
 	return wrap, nil
 }
 
 // start starts a goroutine that copies from the pipe into the rotator. This is
 // called by the constructor and not the user of the wrapper.
-func (l *logRotatorWrapper) start() {
+func (l *logRotatorWrapper) start(readerOpenFn func() (io.ReadCloser, error)) {
 	go func() {
 		defer close(l.hasFinishedCopied)
-		_, err := io.Copy(l.rotatorWriter, l.processOutReader)
+
+		reader, err := readerOpenFn()
 		if err != nil {
+			return
+		}
+		l.processOutReader = reader
+		close(l.opened)
+
+		_, err = io.Copy(l.rotatorWriter, reader)
+		if err != nil {
+			l.logger.Error("copying got an error", "error", err)
 			// Close reader to propagate io error across pipe.
 			// Note that this may block until the process exits on
 			// Windows due to
@@ -227,7 +238,7 @@ func (l *logRotatorWrapper) start() {
 			// or similar issues. Since this is already running in
 			// a goroutine its safe to block until the process is
 			// force-killed.
-			l.processOutReader.Close()
+			reader.Close()
 		}
 	}()
 	return
@@ -249,6 +260,7 @@ func (l *logRotatorWrapper) Close() {
 	closeDone := make(chan struct{})
 	go func() {
 		defer close(closeDone)
+		<-l.opened
 		err := l.processOutReader.Close()
 		if err != nil && !strings.Contains(err.Error(), "file already closed") {
 			l.logger.Warn("error closing read-side of process output pipe", "err", err)
