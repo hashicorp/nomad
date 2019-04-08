6dc8dad570042bb90b07cc445a57d842d1f2521a
Mahmood Ali <mahmood@hashicorp.com>
2019-04-08 10:08:36 -0400

    reuse some test cases

--- e2e/nomadexec/exec.go
+++ e2e/nomadexec/exec.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/nomad/e2e/e2eutil"
 	"github.com/hashicorp/nomad/e2e/framework"
 	"github.com/hashicorp/nomad/helper/uuid"
-	"github.com/hashicorp/nomad/plugins/drivers"
+	dtestutils "github.com/hashicorp/nomad/plugins/drivers/testutils"
 	"github.com/stretchr/testify/require"
 )
 
@@ -68,81 +68,23 @@ func (tc *NomadExecE2ETest) BeforeAll(f *framework.F) {
 }
 
 func (tc *NomadExecE2ETest) TestExecBasicResponses(f *framework.F) {
-	cases := []struct {
-		name        string
-		command     string
-		tty         bool
-		stdin       string
-		stdout      string
-		stderr      string
-		exitCode    int
-		customizeFn func(*drivers.ExecOptions, chan drivers.TerminalSize)
-	}{
-		{
-			name:     "notty: basic",
-			command:  "echo hello stdout; echo hello stderr >&2; exit 43",
-			tty:      false,
-			stdout:   "hello stdout\n",
-			stderr:   "hello stderr\n",
-			exitCode: 43,
-		},
-		{
-			name:     "notty: streaming",
-			command:  "for n in 1 2 3; do echo $n; sleep 1; done",
-			tty:      false,
-			stdout:   "1\n2\n3\n",
-			exitCode: 0,
-		},
-		{
-			name:     "ntty: stty check",
-			command:  "stty size",
-			tty:      false,
-			stderr:   "stty: standard input: Inappropriate ioctl for device\n",
-			exitCode: 1,
-		},
-		{
-			name:     "notty: stdin passing",
-			command:  "echo hello from command; cat",
-			tty:      false,
-			stdin:    "hello from stdin\n",
-			stdout:   "hello from command\nhello from stdin\n",
-			exitCode: 0,
-		},
-		{
-			name:     "notty: stdin passing",
-			command:  "echo hello from command; cat",
-			tty:      false,
-			stdin:    "hello from stdin\n",
-			stdout:   "hello from command\nhello from stdin\n",
-			exitCode: 0,
-		},
-		{
-			name:    "notty: children processes",
-			command: "(( sleep 3; echo from background ) & ); echo from main; exec sleep 1",
-			tty:     false,
-			// when not using tty; wait for all processes to exit matching behavior of `docker exec`
-			stdout:   "from main\nfrom background\n",
-			exitCode: 0,
-		},
-	}
-
-	for _, c := range cases {
-		f.T().Run(c.name, func(t *testing.T) {
+	for _, c := range dtestutils.ExecTaskStreamingBasicCases {
+		f.T().Run(c.Name, func(t *testing.T) {
 			var stdout, stderr bytes.Buffer
 			resize := make(chan api.TerminalSize)
 
-			stdin := ioutil.NopCloser(strings.NewReader(c.stdin))
+			stdin := ioutil.NopCloser(strings.NewReader(c.Stdin))
 
 			exitCode, err := tc.Nomad().Allocations().Exec(context.Background(),
-				&tc.alloc, "task", c.tty,
-				[]string{"/bin/sh", "-c", c.command},
+				&tc.alloc, "task", c.Tty,
+				[]string{"/bin/sh", "-c", c.Command},
 				stdin, &stdout, &stderr,
 				resize, nil)
 
 			require.NoError(t, err)
-			require.Equal(t, c.exitCode, exitCode)
-			require.Equal(t, c.stdout, stdout.String())
-			require.Equal(t, c.stderr, stderr.String())
+			require.Equal(t, c.ExitCode, exitCode)
+			require.Equal(t, c.Stdout, stdout.String())
+			require.Equal(t, c.Stderr, stderr.String())
 		})
 	}
 }
