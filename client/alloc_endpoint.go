9c74f2f41b63389d924930d856d376489c7b77d4
Mahmood Ali <mahmood@hashicorp.com>
2019-04-11 20:37:44 -0400

    remove encoder helper

--- client/alloc_endpoint.go
+++ client/alloc_endpoint.go
@@ -113,7 +113,7 @@ func (a *Allocations) exec(conn io.ReadWriteCloser) {
 	// Decode the arguments
 	var req cstructs.AllocExecRequest
 	decoder := codec.NewDecoder(conn, structs.MsgpackHandle)
-	encoder := newSyncEncoder(codec.NewEncoder(conn, structs.MsgpackHandle))
+	encoder := codec.NewEncoder(conn, structs.MsgpackHandle)
 
 	if err := decoder.Decode(&req); err != nil {
 		handleStreamResultError(err, helper.Int64ToPtr(500), encoder)
@@ -262,10 +262,17 @@ func (a *Allocations) exec(conn io.ReadWriteCloser) {
 	}()
 
 	var outWg sync.WaitGroup
-	outWg.Add(2)
+	var encoderLock sync.Locker
+	encodeFn := func(v interface{}) error {
+		encoderLock.Lock()
+		defer encoderLock.Unlock()
 
-	go a.forwardOutput(encoder, outReader, "stdout", &outWg)
-	go a.forwardOutput(encoder, errReader, "stderr", &outWg)
+		return encoder.Encode(v)
+	}
+
+	outWg.Add(2)
+	go a.forwardOutput(encodeFn, outReader, "stdout", &outWg)
+	go a.forwardOutput(encodeFn, errReader, "stderr", &outWg)
 
 	h := ar.GetTaskExecHandler(req.Task)
 	r, err := h(ctx, drivers.ExecOptions{
@@ -279,6 +286,9 @@ func (a *Allocations) exec(conn io.ReadWriteCloser) {
 		ResizeCh: resizeCh,
 	})
 
+	// ensure that exit messages are the last messages sent
+	outWg.Wait()
+
 	sendFrame := func(frame *sframer.StreamFrame) {
 		buf := new(bytes.Buffer)
 		frameCodec := codec.NewEncoder(buf, structs.JsonHandle)
@@ -288,9 +298,6 @@ func (a *Allocations) exec(conn io.ReadWriteCloser) {
 			Payload: buf.Bytes(),
 		})
 	}
-
-	// ensure that exit messages are the last messages sent
-	outWg.Wait()
 
 	if err != nil {
 		sendFrame(&sframer.StreamFrame{
@@ -308,7 +315,7 @@ func (a *Allocations) exec(conn io.ReadWriteCloser) {
 	return
 }
 
-func (a *Allocations) forwardOutput(encoder encoder, reader io.Reader, source string, wg *sync.WaitGroup) error {
+func (a *Allocations) forwardOutput(encodeFn func(v interface{}) error, reader io.Reader, source string, wg *sync.WaitGroup) error {
 	defer wg.Done()
 
 	buf := new(bytes.Buffer)
@@ -327,7 +334,7 @@ func (a *Allocations) forwardOutput(encoder encoder, reader io.Reader, source st
 				FileEvent: "close",
 			}
 			frameCodec.MustEncode(frame)
-			encoder.Encode(cstructs.StreamErrWrapper{
+			encodeFn(cstructs.StreamErrWrapper{
 				Payload: buf.Bytes(),
 			})
 
@@ -339,7 +346,7 @@ func (a *Allocations) forwardOutput(encoder encoder, reader io.Reader, source st
 		frame.Data = bytes[:n]
 		frameCodec.MustEncode(frame)
 
-		err = encoder.Encode(cstructs.StreamErrWrapper{
+		err = encodeFn(cstructs.StreamErrWrapper{
 			Payload: buf.Bytes(),
 		})
 		if err == io.ErrClosedPipe {
