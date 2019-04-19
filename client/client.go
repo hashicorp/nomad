290f0992e94c81e206a91ce258e49e90127276d7
Mahmood Ali <mahmood@hashicorp.com>
2019-04-19 09:12:50 -0400

    client: avoid registering node twice right away
    I noticed that `watchNodeUpdates()` almost immediately after
    `registerAndHeartbeat()` calls `retryRegisterNode()`, well after 5
    seconds.
    
    This call is unnecessary and made debugging a bit harder.  So here, we
    ensure that we only re-register node for new node events, not for
    initial registration.

--- client/client.go
+++ client/client.go
@@ -1449,13 +1449,7 @@ func (c *Client) watchNodeEvents() {
 	// batchEvents stores events that have yet to be published
 	var batchEvents []*structs.NodeEvent
 
-	// Create and drain the timer
-	timer := time.NewTimer(0)
-	timer.Stop()
-	select {
-	case <-timer.C:
-	default:
-	}
+	timer := stoppedTimer()
 	defer timer.Stop()
 
 	for {
@@ -1911,7 +1905,8 @@ func (c *Client) updateNodeLocked() {
 // it will update the client node copy and re-register the node.
 func (c *Client) watchNodeUpdates() {
 	var hasChanged bool
-	timer := time.NewTimer(c.retryIntv(nodeUpdateRetryIntv))
+
+	timer := stoppedTimer()
 	defer timer.Stop()
 
 	for {
