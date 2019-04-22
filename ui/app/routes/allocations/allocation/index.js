5aa938e121d32aca262908ae9f2e965d91dcef96
Michael Lange <dingoeatingfuzz@gmail.com>
2019-04-22 16:10:21 -0700

    Test coverage for preemption on the allocation detail page

--- ui/app/routes/allocations/allocation/index.js
+++ ui/app/routes/allocations/allocation/index.js
@@ -3,8 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   setupController(controller, model) {
     // Suppress the preemptedByAllocation fetch error in the event it's a 404
-    const setPreempter = () => controller.set('preempter', model.preemptedByAllocation);
-    model.preemptedByAllocation.then(setPreempter, setPreempter);
+    if (model) {
+      const setPreempter = () => controller.set('preempter', model.preemptedByAllocation);
+      model.preemptedByAllocation.then(setPreempter, setPreempter);
+    }
 
     return this._super(...arguments);
   },
