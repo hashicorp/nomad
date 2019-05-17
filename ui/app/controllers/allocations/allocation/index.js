64b4bf654645ed410c33231fb46f2192f13be92a
Michael Lange <dingoeatingfuzz@gmail.com>
2019-05-16 17:43:12 -0700

    Add stop and restart buttons to the allocation index page

--- ui/app/controllers/allocations/allocation/index.js
+++ ui/app/controllers/allocations/allocation/index.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
+import { task } from 'ember-concurrency';
 import Sortable from 'nomad-ui/mixins/sortable';
 import { lazyClick } from 'nomad-ui/helpers/lazy-click';
 
@@ -20,6 +22,40 @@ export default Controller.extend(Sortable, {
 
   // Set in the route
   preempter: null,
+
+  error: computed(() => {
+    // { title, description }
+    return null;
+  }),
+
+  onDismiss() {
+    this.set('error', null);
+  },
+
+  stopAllocation: task(function*() {
+    try {
+      yield this.model.stop();
+      // Eagerly update the allocation clientStatus to avoid flickering
+      this.model.set('clientStatus', 'complete');
+    } catch (err) {
+      this.set('error', {
+        title: 'Could Not Stop Allocation',
+        description: 'Your ACL token does not grant allocation lifecyle permissions.',
+      });
+    }
+  }),
+
+  restartAllocation: task(function*() {
+    try {
+      yield this.model.restart();
+    } catch (err) {
+      console.log('oops', err);
+      this.set('error', {
+        title: 'Could Not Stop Allocation',
+        description: 'Your ACL token does not grant allocation lifecyle permissions.',
+      });
+    }
+  }),
 
   actions: {
     gotoTask(allocation, task) {
