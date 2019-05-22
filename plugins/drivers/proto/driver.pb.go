fe38d0c9244f84085c336260b0d20b32d01fb7db
Nick Ethier <nethier@hashicorp.com>
2019-05-21 22:17:15 -0400

    ar: fix lint errors

--- plugins/drivers/proto/driver.pb.go
+++ plugins/drivers/proto/driver.pb.go
@@ -1583,7 +1583,7 @@ func (m *ExecTaskStreamingResponse) GetResult() *ExitResult {
 }
 
 type CreateNetworkRequest struct {
-	// AllodID of the allocation the network is associated with
+	// AllocID of the allocation the network is associated with
 	AllocId              string   `protobuf:"bytes,1,opt,name=alloc_id,json=allocId,proto3" json:"alloc_id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -1660,7 +1660,7 @@ func (m *CreateNetworkResponse) GetIsolationSpec() *NetworkIsolationSpec {
 }
 
 type DestroyNetworkRequest struct {
-	// AllodID of the allocation the network is associated with
+	// AllocID of the allocation the network is associated with
 	AllocId              string                `protobuf:"bytes,1,opt,name=alloc_id,json=allocId,proto3" json:"alloc_id,omitempty"`
 	IsolationSpec        *NetworkIsolationSpec `protobuf:"bytes,2,opt,name=isolation_spec,json=isolationSpec,proto3" json:"isolation_spec,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}              `json:"-"`
