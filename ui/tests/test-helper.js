3cb5138efe276fb97181fc4618f9d3e88e56c402
Buck Doyle <buck@hashicorp.com>
2019-05-24 09:29:06 -0500

    Add ember-cli-flash with hardcoded template use
    The element structure and CSS is taken from Vault; it’s
    a hack-job inclusion that still needs to be generalised.

--- ui/tests/test-helper.js
+++
@@ -1,9 +0,0 @@
-import 'core-js';
-import Application from '../app';
-import config from '../config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-
-setApplication(Application.create(config.APP));
-
-start();
