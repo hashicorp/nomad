acc76e02f53da2c9152664bcd1c327bbbd18150b
Buck Doyle <buck@hashicorp.com>
2019-05-27 13:59:44 -0500

    Restore mistakenly-deleted file

---
+++ ui/tests/test-helper.js
@@ -0,0 +1,9 @@
+import 'core-js';
+import Application from '../app';
+import config from '../config/environment';
+import { setApplication } from '@ember/test-helpers';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+start();
