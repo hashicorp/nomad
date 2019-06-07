bd53f76ce8bb1e782a10ed2549f2dabfec33de36
Buck Doyle <buck@hashicorp.com>
2019-06-07 08:44:50 -0500

    Add attempt to deploy from Travis
    If I can get this to work, it’ll mean deployments won’t show
    up until after the tests have run, which is slower than the
    current setup where Netlify runs in parallel. But it also will
    allow me to not deploy for non-UI PRs, etc.

---
+++ .netlify/deploy.sh
@@ -0,0 +1,12 @@
+mkdir -p ui-dist/ui
+mv ui/dist/* ui-dist/ui
+
+netlify deploy --dir=ui-dist > netlify.log
+cat netlify.log
+
+NETLIFY_DEPLOYMENT_URL=$(echo netlify.log | awk '{ print $NF }')
+
+curl -X POST \
+    --data "{\"state\": \"success\", \"target_url\": \"$NETLIFY_DEPLOYMENT_URL\", \"description\": \"Visit a deployment for this PR\", \"context\": \"deployments\"}" \
+    -H "Authorization: token $GITHUB_STATUS_TOKEN" \
+    https://api.github.com/repos/hashicorp/nomad/statuses/$TRAVIS_PULL_REQUEST_SHA
