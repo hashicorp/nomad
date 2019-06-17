5bfd6877a7e061791ce1a1467ab6985e9937475b
Nick Ethier <nethier@hashicorp.com>
2019-06-16 23:56:20 -0400

    executor: support network namespacing on universal executor

--- drivers/shared/executor/executor.go
+++ drivers/shared/executor/executor.go
@@ -14,7 +14,6 @@ import (
 	"time"
 
 	"github.com/armon/circbuf"
-	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/hashicorp/consul-template/signals"
 	hclog "github.com/hashicorp/go-hclog"
 	multierror "github.com/hashicorp/go-multierror"
@@ -306,30 +305,8 @@ func (e *UniversalExecutor) Launch(command *ExecCommand) (*ProcessState, error) 
 	e.childCmd.Env = e.commandCfg.Env
 
 	// Start the process
-	if command.NetworkIsolation != nil && command.NetworkIsolation.Path != "" {
-		// Lock to the thread we're changing the network namespace of
-		runtime.LockOSThread()
-		netns, err := ns.GetNS(command.NetworkIsolation.Path)
-		if err != nil {
-			return nil, err
-		}
-
-		// Start the container in the network namespace
-		err = netns.Do(func(ns.NetNS) error {
-
-			if err := e.childCmd.Start(); err != nil {
-				return fmt.Errorf("failed to start command path=%q --- args=%q: %v", path, e.childCmd.Args, err)
-			}
-			return nil
-		})
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		if err := e.childCmd.Start(); err != nil {
-			return nil, fmt.Errorf("failed to start command path=%q --- args=%q: %v", path, e.childCmd.Args, err)
-		}
-
+	if err = e.start(command); err != nil {
+		return nil, fmt.Errorf("failed to start command path=%q --- args=%q: %v", path, e.childCmd.Args, err)
 	}
 
 	go e.pidCollector.collectPids(e.processExited, getAllPids)
