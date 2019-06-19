290482a5bc4e1c88cb39e9ebe288c63b72dd2c13
Buck Doyle <buck@hashicorp.com>
2019-06-19 12:45:09 -0700

    Replace error-determination with existing util

--- ui/app/utils/properties/watch.js
+++ ui/app/utils/properties/watch.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
-import { get, getWithDefault } from '@ember/object';
+import { get } from '@ember/object';
 import RSVP from 'rsvp';
 import { task } from 'ember-concurrency';
 import wait from 'nomad-ui/utils/wait';
 import XHRToken from 'nomad-ui/utils/classes/xhr-token';
 import config from 'nomad-ui/config/environment';
+import codesForError from 'nomad-ui/utils/codes-for-error';
 
 const isEnabled = config.APP.blockingQueries !== false;
 
@@ -26,10 +27,9 @@ export function watchRecord(modelName, { ignore404 } = {}) {
         ]);
       } catch (e) {
         if (!ignore404) {
-          const statusIs404 = getWithDefault(e, 'errors', [])
-            .mapBy('status')
-            .includes('404');
+          const statusIs404 = codesForError(e).includes('404');
           const errorIsEmptyResponse = e.message.includes('response did not have any data');
+
           if (statusIs404 || errorIsEmptyResponse) {
             const message = this.flashMessages
               .warning(`This ${modelName} no longer exists`, { sticky: true })
