f3c944aaf585ca2abc630e4be00e170af9b6759c
Mahmood Ali <mahmood@hashicorp.com>
2019-06-26 23:39:10 +0800

    task runner to avoid running task if terminal
    This change fixes a bug where nomad would avoid running alloc tasks if
    the alloc is client terminal but the server copy on the client isn't
    marked as running.
    
    Here, we fix the case by having task runner uses the
    allocRunner.shouldRun() instead of only checking the server updated
    alloc.
    
    Here, we preserve much of the invariants such that `tr.Run()` is always
    run, and don't change the overall alloc runner and task runner
    lifecycles.
    
    Fixes https://github.com/hashicorp/nomad/issues/5883

--- client/allocrunner/taskrunner/task_runner.go
+++ client/allocrunner/taskrunner/task_runner.go
@@ -394,6 +394,22 @@ func (tr *TaskRunner) Run() {
 	defer close(tr.waitCh)
 	var result *drivers.ExitResult
 
+	tr.stateLock.RLock()
+	dead := tr.state.State == structs.TaskStateDead
+	tr.stateLock.RUnlock()
+
+	// if restoring a dead task, ensure that task is cleared and all post hooks
+	// are called without additional state updates
+	if dead {
+		// clear driver handle if it was successfully restored on
+		// already dead task
+		tr.clearDriverHandle()
+		if err := tr.stop(); err != nil {
+			tr.logger.Error("stop failed on terminal task", "error", err)
+		}
+		return
+	}
+
 	// Updates are handled asynchronously with the other hooks but each
 	// triggered update - whether due to alloc updates or a new vault token
 	// - should be handled serially.
@@ -899,7 +915,7 @@ func (tr *TaskRunner) Restore() error {
 		}
 
 		alloc := tr.Alloc()
-		if alloc.TerminalStatus() || alloc.Job.Type == structs.JobTypeSystem {
+		if tr.state.State == structs.TaskStateDead || alloc.TerminalStatus() || alloc.Job.Type == structs.JobTypeSystem {
 			return nil
 		}
 
