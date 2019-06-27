caf48ee3e6e599c1f2b2fe0a879d4a58e1abfd1b
Michael Lange <dingoeatingfuzz@gmail.com>
2019-06-27 12:10:34 -0700

    Extract a streaming-file component from the task-log component
    The new streaming-file component takes an arbitrary logger component
    along with some mode flags and handles things like polling, DOM updates,
    and scroll position.

--- ui/app/components/task-log.js
+++ ui/app/components/task-log.js
@@ -1,9 +1,7 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { run } from '@ember/runloop';
 import RSVP from 'rsvp';
-import { task } from 'ember-concurrency';
 import { logger } from 'nomad-ui/utils/classes/log';
 import WindowResizable from 'nomad-ui/mixins/window-resizable';
 import timeout from 'nomad-ui/utils/timeout';
@@ -25,27 +23,8 @@ export default Component.extend(WindowResizable, {
   clientTimeout: 1000,
   serverTimeout: 5000,
 
-  didReceiveAttrs() {
-    if (this.allocation && this.task) {
-      this.send('toggleStream');
-    }
-  },
-
-  didInsertElement() {
-    this.fillAvailableHeight();
-  },
-
-  windowResizeHandler() {
-    run.once(this, this.fillAvailableHeight);
-  },
-
-  fillAvailableHeight() {
-    // This math is arbitrary and far from bulletproof, but the UX
-    // of having the log window fill available height is worth the hack.
-    const margins = 30 + 30; // Account for padding and margin on either side of the CLI
-    const cliWindow = this.$('.cli-window');
-    cliWindow.height(window.innerHeight - cliWindow.offset().top - margins);
-  },
+  isStreaming: true,
+  streamMode: 'streaming',
 
   mode: 'stdout',
 
@@ -76,56 +55,28 @@ export default Component.extend(WindowResizable, {
             this.set('noConnection', true);
           } else {
             this.send('failoverToServer');
-            this.stream.perform();
           }
           throw error;
         }
       );
   }),
 
-  head: task(function*() {
-    yield this.get('logger.gotoHead').perform();
-    run.scheduleOnce('afterRender', () => {
-      this.$('.cli-window').scrollTop(0);
-    });
-  }),
-
-  tail: task(function*() {
-    yield this.get('logger.gotoTail').perform();
-    run.scheduleOnce('afterRender', () => {
-      const cliWindow = this.$('.cli-window');
-      cliWindow.scrollTop(cliWindow[0].scrollHeight);
-    });
-  }),
-
-  stream: task(function*() {
-    this.logger.on('tick', () => {
-      run.scheduleOnce('afterRender', () => {
-        const cliWindow = this.$('.cli-window');
-        cliWindow.scrollTop(cliWindow[0].scrollHeight);
-      });
-    });
-
-    yield this.logger.startStreaming();
-    this.logger.off('tick');
-  }),
-
-  willDestroy() {
-    this.logger.stop();
-  },
-
   actions: {
     setMode(mode) {
       this.logger.stop();
       this.set('mode', mode);
-      this.stream.perform();
     },
     toggleStream() {
-      if (this.get('logger.isStreaming')) {
-        this.logger.stop();
-      } else {
-        this.stream.perform();
-      }
+      this.set('streamMode', 'streaming');
+      this.toggleProperty('isStreaming');
+    },
+    gotoHead() {
+      this.set('streamMode', 'head');
+      this.set('isStreaming', false);
+    },
+    gotoTail() {
+      this.set('streamMode', 'tail');
+      this.set('isStreaming', false);
     },
     failoverToServer() {
       this.set('useServer', true);
