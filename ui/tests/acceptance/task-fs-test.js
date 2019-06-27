d7232faa20c0650b053303b281f605c20f2ca93f
Buck Doyle <buck@hashicorp.com>
2019-06-27 12:14:01 -0500

    Combine path tests into root FS file

--- ui/tests/acceptance/task-fs-test.js
+++ ui/tests/acceptance/task-fs-test.js
@@ -1,4 +1,5 @@
 import { currentURL } from '@ember/test-helpers';
+import { Promise } from 'rsvp';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
@@ -36,5 +37,90 @@ module('Acceptance | task fs', function(hooks) {
       FS.emptyState.headline.includes('Task is not Running'),
       'Empty state explains the condition'
     );
+  });
+
+  test('visiting /allocations/:allocation_id/:task_name/fs/:path', async function(assert) {
+    const paths = ['some-file.log', 'a/deep/path/to/a/file.log', '/', 'Unicode™®'];
+
+    const testPath = async filePath => {
+      await FS.visitPath({ id: allocation.id, name: task.name, path: filePath });
+      assert.equal(
+        currentURL(),
+        `/allocations/${allocation.id}/${task.name}/fs/${encodeURIComponent(filePath)}`,
+        'No redirect'
+      );
+      assert.equal(FS.breadcrumbsText, `${task.name} ${filePath.replace(/\//g, ' ')}`.trim());
+    };
+
+    await paths.reduce(async (prev, filePath) => {
+      await prev;
+      return testPath(filePath);
+    }, Promise.resolve());
+  });
+
+  test('navigating allocation filesystem', async function(assert) {
+    await FS.visitPath({ id: allocation.id, name: task.name, path: '/' });
+
+    assert.ok(FS.fileViewer.isHidden);
+
+    assert.equal(FS.directoryEntries.length, 4);
+
+    assert.equal(FS.breadcrumbsText, task.name);
+
+    assert.equal(FS.breadcrumbs.length, 1);
+    assert.ok(FS.breadcrumbs[0].isActive);
+    assert.equal(FS.breadcrumbs[0].text, task.name);
+
+    assert.equal(FS.directoryEntries[0].name, 'directory', 'directories should come first');
+    assert.ok(FS.directoryEntries[0].isDirectory);
+    assert.equal(FS.directoryEntries[0].size, '', 'directory sizes are hidden');
+    assert.equal(FS.directoryEntries[0].lastModified, 'a year ago');
+
+    assert.equal(FS.directoryEntries[2].name, '🤩.txt');
+    assert.ok(FS.directoryEntries[2].isFile);
+    assert.equal(FS.directoryEntries[2].size, '1 KiB');
+    assert.equal(FS.directoryEntries[2].lastModified, '2 days ago');
+
+    assert.equal(FS.directoryEntries[3].name, '🙌🏿.txt');
+
+    await FS.directoryEntries[0].visit();
+
+    assert.equal(FS.directoryEntries.length, 1);
+
+    assert.equal(FS.breadcrumbs.length, 2);
+    assert.equal(FS.breadcrumbsText, `${task.name} directory`);
+
+    assert.notOk(FS.breadcrumbs[0].isActive);
+
+    assert.equal(FS.breadcrumbs[1].text, 'directory');
+    assert.ok(FS.breadcrumbs[1].isActive);
+
+    await FS.directoryEntries[0].visit();
+
+    assert.equal(FS.directoryEntries.length, 1);
+
+    assert.equal(FS.breadcrumbs.length, 3);
+    assert.equal(FS.breadcrumbsText, `${task.name} directory another`);
+    assert.equal(FS.breadcrumbs[2].text, 'another');
+
+    await FS.breadcrumbs[1].visit();
+    assert.equal(FS.breadcrumbsText, `${task.name} directory`);
+    assert.equal(FS.breadcrumbs.length, 2);
+  });
+
+  test('viewing a file', async function(assert) {
+    await FS.visitPath({ id: allocation.id, name: task.name, path: '/' });
+    await FS.directoryEntries[2].visit();
+
+    assert.equal(FS.breadcrumbsText, `${task.name} 🤩.txt`);
+
+    assert.ok(FS.fileViewer.isPresent);
+  });
+
+  test('viewing an empty directory', async function(assert) {
+    await FS.visitPath({ id: allocation.id, name: task.name, path: '/empty-directory' });
+
+    assert.equal(FS.directoryEntries.length, 1);
+    assert.ok(FS.directoryEntries[0].isEmpty);
   });
 });
--- ui/tests/pages/allocations/task/fs.js
+++ ui/tests/pages/allocations/task/fs.js
@@ -1,9 +1,40 @@
-import { create, isPresent, text, visitable } from 'ember-cli-page-object';
+import {
+  collection,
+  clickable,
+  create,
+  hasClass,
+  isPresent,
+  text,
+  visitable,
+} from 'ember-cli-page-object';
 
 export default create({
   visit: visitable('/allocations/:id/:name/fs'),
+  visitPath: visitable('/allocations/:id/:name/fs/:path'),
 
-  hasFiles: isPresent('[data-test-file-explorer]'),
+  fileViewer: {
+    scope: '[data-test-file-viewer]',
+  },
+
+  breadcrumbsText: text('[data-test-fs-breadcrumbs]'),
+
+  breadcrumbs: collection('[data-test-fs-breadcrumbs] li', {
+    visit: clickable('a'),
+    isActive: hasClass('is-active'),
+  }),
+
+  directoryEntries: collection('[data-test-entry]', {
+    name: text('[data-test-name]'),
+
+    isFile: isPresent('[data-test-file-icon]'),
+    isDirectory: isPresent('[data-test-directory-icon]'),
+    isEmpty: isPresent('[data-test-empty-icon]'),
+
+    size: text('[data-test-size]'),
+    lastModified: text('[data-test-last-modified]'),
+
+    visit: clickable('a'),
+  }),
 
   hasEmptyState: isPresent('[data-test-not-running]'),
   emptyState: {
