45c8f37157cdef3a7a5420a3e2c1dc6927062bf5
Michael Lange <dingoeatingfuzz@gmail.com>
2019-07-02 07:52:47 -0700

    image-file component for showing an image and image metadata

---
+++ ui/app/components/image-file.js
@@ -0,0 +1,28 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  tagName: 'figure',
+  classNames: 'image-file',
+
+  src: null,
+  alt: null,
+  size: null,
+
+  // Set by updateImageMeta
+  width: 0,
+  height: 0,
+
+  fileName: computed('src', function() {
+    if (!this.src) return;
+    return this.src.includes('/') ? this.src.match(/^.*\/(.*)$/)[1] : this.src;
+  }),
+
+  updateImageMeta(event) {
+    const img = event.target;
+    this.setProperties({
+      width: img.naturalWidth,
+      height: img.naturalHeight,
+    });
+  },
+});
