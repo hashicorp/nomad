0eb74d2cac8873aa71c74066394aa6acca834a1f
Michael Lange <dingoeatingfuzz@gmail.com>
2019-07-07 10:23:21 +0200

    Expose SharedResources and networking mode to models

--- ui/app/models/allocation.js
+++ ui/app/models/allocation.js
@@ -25,6 +25,7 @@ export default Model.extend({
   name: attr('string'),
   taskGroupName: attr('string'),
   resources: fragment('resources'),
+  sharedResources: fragment('resources'),
   jobVersion: attr('number'),
 
   modifyIndex: attr('number'),
