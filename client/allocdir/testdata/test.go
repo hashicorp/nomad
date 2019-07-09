a5a41d90b083730d8829f6a35548d66f6b41dc1d
Michael Schurter <mschurter@hashicorp.com>
2019-07-09 15:20:50 +0200

    make linter happy

--- client/allocdir/testdata/test.go
+++ client/allocdir/testdata/test.go
@@ -5,22 +5,22 @@ import (
 	"syscall"
 )
 
-// linkDir hardlinks src to dst. The src and dst must be on the same filesystem.
-func linkDir(src, dst string) error {
+// LinkDir hardlinks src to dst. The src and dst must be on the same filesystem.
+func LinkDir(src, dst string) error {
 	return syscall.Link(src, dst)
 }
 
-// unlinkDir removes a directory link.
-func unlinkDir(dir string) error {
+// UnlinkDir removes a directory link.
+func UnlinkDir(dir string) error {
 	return syscall.Unlink(dir)
 }
 
-// createSecretDir creates the secrets dir folder at the given path
-func createSecretDir(dir string) error {
+// CreateSecretDir creates the secrets dir folder at the given path
+func CreateSecretDir(dir string) error {
 	return os.MkdirAll(dir, 0777)
 }
 
-// removeSecretDir removes the secrets dir folder
-func removeSecretDir(dir string) error {
+// RemoveSecretDir removes the secrets dir folder
+func RemoveSecretDir(dir string) error {
 	return os.RemoveAll(dir)
 }
