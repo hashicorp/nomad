26652d7a6b112c920656028533e5c7e25da923f2
Preetha Appan <preetha@hashicorp.com>
2019-07-09 09:37:09 -0500

    Populate task event struct with kill timeout
    This makes for a nicer task event message

--- client/allocrunner/alloc_runner_test.go
+++ client/allocrunner/alloc_runner_test.go
@@ -107,10 +107,19 @@ func TestAllocRunner_TaskLeader_KillTG(t *testing.T) {
 		}
 
 		found := false
+		killingMsg := ""
 		for _, e := range state1.Events {
 			if e.Type != structs.TaskLeaderDead {
 				found = true
 			}
+			if e.Type == structs.TaskKilling {
+				killingMsg = e.DisplayMessage
+			}
+		}
+		expectedKillingMsg := "Sent interrupt. Waiting 10ms before force killing"
+
+		if killingMsg != expectedKillingMsg {
+			return false, fmt.Errorf("Unexpected task event message - wanted %q. got %q", killingMsg, expectedKillingMsg)
 		}
 
 		if !found {
