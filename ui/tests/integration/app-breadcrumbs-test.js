4c5da74019ad3e957ec058b1a06d8b46ce13d933
Buck Doyle <buck@hashicorp.com>
2019-07-15 11:29:39 -0500

    Remove duplicate import origin

--- ui/tests/integration/app-breadcrumbs-test.js
+++ ui/tests/integration/app-breadcrumbs-test.js
@@ -2,8 +2,7 @@ import Service from '@ember/service';
 import RSVP from 'rsvp';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled } from '@ember/test-helpers';
-import { findAll } from '@ember/test-helpers';
+import { findAll, render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import PromiseObject from 'nomad-ui/utils/classes/promise-object';
 
