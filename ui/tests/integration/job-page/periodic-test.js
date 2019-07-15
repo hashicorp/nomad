64f46c802bce5d6865589a29c003edb1c192c697
Buck Doyle <buck@hashicorp.com>
2019-07-15 10:17:00 -0500

    Remove unused import

--- ui/tests/integration/job-page/periodic-test.js
+++ ui/tests/integration/job-page/periodic-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { click, find, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { startMirage } from 'nomad-ui/initializers/ember-cli-mirage';
