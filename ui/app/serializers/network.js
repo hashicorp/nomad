e0746efe09e7e533a9da958ce64f473a7d2812ca
Buck Doyle <buck@hashicorp.com>
2019-07-24 09:55:35 -0500

    Add IPv6 bracket-wrapping to network serializer

--- ui/app/serializers/network.js
+++ ui/app/serializers/network.js
@@ -1,9 +1,20 @@
 import ApplicationSerializer from './application';
+import isIp from 'is-ip';
 
 export default ApplicationSerializer.extend({
   attrs: {
     cidr: 'CIDR',
     ip: 'IP',
     mbits: 'MBits',
+  },
+
+  normalize(typeHash, hash) {
+    const ip = hash.IP;
+
+    if (isIp.v6(ip)) {
+      hash.IP = `[${ip}]`;
+    }
+
+    return this._super(...arguments);
   },
 });
