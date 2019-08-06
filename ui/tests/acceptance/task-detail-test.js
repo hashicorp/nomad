66f97c25f5c17cacfcc14f98882bb500a50a67f1
Buck Doyle <buck@hashicorp.com>
2019-08-06 15:06:23 -0500

    Add assertion for proxy badge
    The `nomad_envoy` thing is outdated, but I’ll change that next.

--- ui/tests/acceptance/task-detail-test.js
+++ ui/tests/acceptance/task-detail-test.js
@@ -23,7 +23,7 @@ module('Acceptance | task detail', function(hooks) {
   });
 
   test('/allocation/:id/:task_name should name the task and list high-level task information', async function(assert) {
-    assert.ok(Task.title.includes(task.name), 'Task name');
+    assert.ok(Task.title.text.includes(task.name), 'Task name');
     assert.ok(Task.state.includes(task.state), 'Task state');
 
     assert.ok(
@@ -305,5 +305,29 @@ module('Acceptance | task detail (not running)', function(hooks) {
   test('when the allocation for a task is not running, the resource utilization graphs are replaced by an empty message', async function(assert) {
     assert.equal(Task.resourceCharts.length, 0, 'No resource charts');
     assert.equal(Task.resourceEmptyMessage, "Task isn't running", 'Empty message is appropriate');
+  });
+});
+
+module('Acceptance | proxy task detail', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function() {
+    server.create('agent');
+    server.create('node');
+    server.create('job', { createAllocations: false });
+    // FIXME when this is `running`, the graphs fail to render, despite this being copied from the first module 🧐
+    allocation = server.create('allocation', 'withTaskWithPorts', { clientStatus: 'complete' });
+    task = allocation.task_states.models[0];
+
+    task.name = 'nomad_envoy';
+    task.save();
+
+    await Task.visit({ id: allocation.id, name: task.name });
+  });
+
+  test('a proxy badge is shown', async function(assert) {
+    assert.ok(Task.title.badge.isPresent);
+    assert.equal(Task.title.badge.text, 'Proxy');
   });
 });
