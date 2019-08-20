3db9a865807591160ba66e3be96addfb327facba
Buck Doyle <buck@hashicorp.com>
2019-08-20 09:24:38 -0500

    Change factory-generated properties to be dynamic

--- ui/mirage/factories/service.js
+++ ui/mirage/factories/service.js
@@ -3,8 +3,9 @@ import { provide } from '../utils';
 
 export default Factory.extend({
   name: id => `${faker.hacker.noun().dasherize()}-${id}-service`,
-  portLabel: faker.random.number({ min: 5000, max: 60000 }),
-  tags: provide(faker.random.number({ min: 0, max: 2 }), faker.hacker.noun.bind(faker.hacker.noun)),
+  portLabel: () => faker.random.number({ min: 5000, max: 60000 }),
+  tags: () =>
+    provide(faker.random.number({ min: 0, max: 2 }), faker.hacker.noun.bind(faker.hacker.noun)),
 
   afterCreate(service, server) {
     // FIXME if extending to put services in tasks vs task groups, make conditional
