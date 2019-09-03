6863772d007342f17c61c41965952cbb4f336ee8
Buck Doyle <buck@hashicorp.com>
2019-09-03 15:46:54 -0500

    Combine tag and tooltip elements

--- ui/app/components/proxy-tag.js
+++ ui/app/components/proxy-tag.js
@@ -1,8 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  tagName: 'span',
-  classNames: ['badge', 'is-light'],
-
-  'data-test-proxy-tag': true,
+  tagName: '',
 });
