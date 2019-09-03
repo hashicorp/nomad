ee87889450e2a3ce67d29d519d76aa1075e28261
Buck Doyle <buck@hashicorp.com>
2019-09-03 16:19:52 -0500

    Add intermediary Connect upstreams model fragments

--- ui/app/models/sidecar-service.js
+++ ui/app/models/sidecar-service.js
@@ -1,10 +1,6 @@
-import attr from 'ember-data/attr';
 import Fragment from 'ember-data-model-fragments/fragment';
 import { fragment } from 'ember-data-model-fragments/attributes';
 
 export default Fragment.extend({
-  name: attr('string'),
-  portLabel: attr('string'),
-  tags: attr(),
-  connect: fragment('consul-connect'),
+  proxy: fragment('sidecar-proxy'),
 });
