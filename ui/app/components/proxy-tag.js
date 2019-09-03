e73637d6ab10a8cc6bc8a59c416145aa1db131d5
Buck Doyle <buck@hashicorp.com>
2019-09-03 15:38:09 -0500

    Change proxy tag to use badge styles

--- ui/app/components/proxy-tag.js
+++ ui/app/components/proxy-tag.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'span',
-  classNames: ['tag', 'is-secondary', 'is-structure'],
+  classNames: ['badge', 'is-light'],
 
   'data-test-proxy-tag': true,
 });
