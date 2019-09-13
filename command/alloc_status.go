48034051d48db9eabf0c6073387cb24ae7809fbd
Mahmood Ali <mahmood@hashicorp.com>
2019-09-13 19:01:34 -0400

    cli: emit job version number proper
    We must emit alloc job number rather than its the field address.

--- command/alloc_status.go
+++ command/alloc_status.go
@@ -240,7 +240,7 @@ func formatAllocBasicInfo(alloc *api.Allocation, client *api.Client, uuidLength 
 		fmt.Sprintf("Node ID|%s", limit(alloc.NodeID, uuidLength)),
 		fmt.Sprintf("Node Name|%s", alloc.NodeName),
 		fmt.Sprintf("Job ID|%s", alloc.JobID),
-		fmt.Sprintf("Job Version|%d", alloc.Job.Version),
+		fmt.Sprintf("Job Version|%d", *alloc.Job.Version),
 		fmt.Sprintf("Client Status|%s", alloc.ClientStatus),
 		fmt.Sprintf("Client Description|%s", alloc.ClientDescription),
 		fmt.Sprintf("Desired Status|%s", alloc.DesiredStatus),
