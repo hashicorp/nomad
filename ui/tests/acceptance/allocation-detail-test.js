5c7244fed9cdb39caf3a74bca11c7ada2523126a
Buck Doyle <buck@hashicorp.com>
2019-09-24 13:03:17 -0500

    Change test to make every task state a proxy
    It appears the problem is that despite my attempts to force
    the first task in the list to be the one with the tag so
    it can be asserted against, it’s not always the first. So
    this should make every task be of kind proxy, so asserting
    against the first will work.

--- ui/tests/acceptance/allocation-detail-test.js
+++ ui/tests/acceptance/allocation-detail-test.js
@@ -154,10 +154,11 @@ module('Acceptance | allocation detail', function(hooks) {
       jobId: job.id,
     });
 
-    const taskState = allocation.task_states.models[0];
-    const task = server.schema.tasks.findBy({ name: taskState.name });
-    task.update('kind', 'connect-proxy:task');
-    task.save();
+    allocation.task_states.models.forEach(taskState => {
+      const task = server.schema.tasks.findBy({ name: taskState.name });
+      task.update('kind', 'connect-proxy:task');
+      task.save();
+    });
 
     await Allocation.visit({ id: allocation.id });
 
