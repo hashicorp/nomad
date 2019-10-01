58f2ce403803eee84dd12de05d9061f3ee984c69
Buck Doyle <buck@hashicorp.com>
2019-10-01 16:20:18 -0500

    Update to Ember 3.12 LTS via ember-cli-update

--- ui/config/environment.js
+++ ui/config/environment.js
@@ -15,7 +15,7 @@ module.exports = function(environment) {
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
         'ember-routing-router-service': true,
       },
       EXTEND_PROTOTYPES: {
