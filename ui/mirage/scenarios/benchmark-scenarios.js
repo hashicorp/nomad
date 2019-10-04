c52b29225605f82ca3f9cacb00ef2421cae0be97
Michael Lange <dingoeatingfuzz@gmail.com>
2019-10-04 14:38:50 -0700

    UXR Task 1a

--- ui/mirage/scenarios/benchmark-scenarios.js
+++ ui/mirage/scenarios/benchmark-scenarios.js
@@ -1,9 +1,41 @@
 export function uxrTask0(server) {
+  // !! Needs a corresponding job file
   server.createList('agent', 3);
   server.createList('node', 5);
-  server.create('job', { ratio: '10: R 1' });
-  server.create('job', { ratio: '5: R 1 C 4' });
-  server.create('job', { ratio: '10: R 1 F 5 L 1' });
-  server.create('job', { ratio: '8: Q 3 S 5' });
-  server.create('job', { ratio: '10: C 1' });
+  server.createList('job', 5, { status: 'running', ratio: '10: R 1' });
+  server.create('job', { status: 'running', ratio: '5: R 1 C 4' });
+  server.create('job', { status: 'running', ratio: '10: R 5 F 1 L 1' });
+  server.create('job', { status: 'dead', ratio: '10: C 1' });
+  server.create('job', {
+    id: 'web-server',
+    status: 'running',
+    ratio: '5: R 1',
+  });
+}
+
+export function uxrTask1a(server) {
+  server.createList('agent', 3);
+  server.createList('node', 5, 'forceAllDrivers');
+
+  server.create('job', {
+    id: 'web-server',
+    status: 'running',
+    groupsCount: 1,
+    ratio: '50: S 1 R 4 C 1',
+    createAllocations: false,
+    noFailedPlacements: true,
+  });
+
+  server.createList('allocation', 4, {
+    jobId: 'web-server',
+    clientStatus: 'running',
+    jobVersion: 1,
+  });
+
+  server.create('allocation', 'rescheduled', {
+    jobId: 'web-server',
+    clientStatus: 'completed',
+    rescheduleStatus: 'starting',
+    jobVersion: 1,
+  });
 }
