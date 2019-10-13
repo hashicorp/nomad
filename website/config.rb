62c72d768bf1521c26b96f8a784e161bcd28950e
Alvin Huang <17609145+alvin-huang@users.noreply.github.com>
2019-10-12 23:17:03 -0400

    generate a _redirects for netlify previews

--- website/config.rb
+++ website/config.rb
@@ -6,6 +6,9 @@ activate :hashicorp do |h|
   h.github_slug = "hashicorp/nomad"
 end
 
+# Netlify redirects/headers
+proxy '_redirects', 'redirects.txt', ignore: true
+
 helpers do
   # Returns a segment tracking ID such that local development is not
   # tracked to production systems.
