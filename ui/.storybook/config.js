de400a7c764572ea3f52a825e6dc20bed210a9d1
Buck Doyle <buck@hashicorp.com>
2019-10-16 13:33:12 -0500

    Add margin around canvas
    This is a copy-paste from Vault, again.

--- ui/.storybook/config.js
+++ ui/.storybook/config.js
@@ -1,4 +1,35 @@
 /* eslint-env node */
-import { configure } from '@storybook/ember';
+import { addDecorator, configure } from '@storybook/ember';
+
+addDecorator(storyFn => {
+  const { template, context } = storyFn();
+
+  // This adds styling to the Canvas tab.
+  const styles = {
+    style: {
+      margin: '20px',
+    },
+  };
+
+  // Create a div to wrap the Canvas tab with the applied styles.
+  const element = document.createElement('div');
+  Object.assign(element.style, styles.style);
+
+  const innerElement = document.createElement('div');
+  const wormhole = document.createElement('div');
+  wormhole.setAttribute('id', 'ember-basic-dropdown-wormhole');
+  innerElement.appendChild(wormhole);
+
+  element.appendChild(innerElement);
+  innerElement.appendTo = function appendTo(el) {
+    el.appendChild(element);
+  };
+
+  return {
+    template,
+    context,
+    element: innerElement,
+  };
+});
 
 configure(require.context('../stories', true, /\.stories\.js$/), module);
