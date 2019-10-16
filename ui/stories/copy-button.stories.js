cca35ce2d680be00a0013417959c850056b30b3b
Buck Doyle <buck@hashicorp.com>
2019-10-16 14:49:02 -0500

    Add demonstration knob
    I’m not sure whether this is that useful…???

--- ui/stories/copy-button.stories.js
+++ ui/stories/copy-button.stories.js
@@ -2,20 +2,24 @@
 // FIXME Vault has an entry in .eslintignore to skip Storybook altogether…???
 import hbs from 'htmlbars-inline-precompile';
 import { storiesOf } from '@storybook/ember';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import notes from './copy-button.md';
 
 
 storiesOf('CopyButton/', module)
   .addParameters({ options: { showPanel: true } })
+  .addDecorator(withKnobs())
   .add('CopyButton', () => ({
     template: hbs`
       <h5 class="title is-5">Copy Button</h5>
       <span class="tag is-hollow is-small no-text-transform">
-        e8c898a0-794b-9063-7a7f-bf0c4a405f83
-        {{copy-button clipboardText="e8c898a0-794b-9063-7a7f-bf0c4a405f83"}}
+        {{clipboardText}}
+        {{copy-button clipboardText=clipboardText}}
       </span>
     `,
-    context: {},
+    context: {
+      clipboardText: text('Clipboard Text', 'e8c898a0-794b-9063-7a7f-bf0c4a405f83'),
+    },
   }),
   {notes}
   );
