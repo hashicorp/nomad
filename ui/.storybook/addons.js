af0bb0d949ae272555f34222e7e52e174c4252a0
Buck Doyle <buck@hashicorp.com>
2019-10-24 09:59:16 -0500

    Add storysource addon
    Not sure whether this is the best approach…

--- ui/.storybook/addons.js
+++ ui/.storybook/addons.js
@@ -1,3 +1,4 @@
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-notes/register';
+import '@storybook/addon-storysource/register';
 import '@storybook/addon-viewport/register';
---
+++ ui/.storybook/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = function({ config }) {
+  config.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre',
+  });
+
+  return config;
+};
