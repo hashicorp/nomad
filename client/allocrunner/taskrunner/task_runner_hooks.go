bc90c92c398148158d64b66c8ddd30d804ba3dfa
Danielle Lancashire <dani@hashicorp.com>
2019-10-25 14:08:46 +0200

    tr: Setup plugin supervisor loop
    This commit introduces the initial implementation of the reconciler loop
    for plugin supervision inside nomad. Setting up mounts, and then
    fingerprinting the configured socket path for the existence of a plugin.

--- client/allocrunner/taskrunner/task_runner_hooks.go
+++ client/allocrunner/taskrunner/task_runner_hooks.go
@@ -70,6 +70,11 @@ func (tr *TaskRunner) initHooks() {
 		newEnvoyBootstrapHook(alloc, tr.clientConfig.ConsulConfig.Addr, hookLogger),
 	}
 
+	// If the task has a CSI stanza, add the hook.
+	if task.CSIPluginConfig != nil {
+		tr.runnerHooks = append(tr.runnerHooks, newCSIPluginSupervisorHook("/tmp/csiroot", tr, tr, hookLogger))
+	}
+
 	// If Vault is enabled, add the hook
 	if task.Vault != nil {
 		tr.runnerHooks = append(tr.runnerHooks, newVaultHook(&vaultHookConfig{
