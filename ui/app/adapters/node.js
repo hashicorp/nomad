fd29271c8cec44b2ab6d8bbbd6d2ac95984b44eb
Michael Lange <dingoeatingfuzz@gmail.com>
2019-10-24 17:52:34 -0700

    Implement eligibility toggling in the data layer

--- ui/app/adapters/node.js
+++ ui/app/adapters/node.js
@@ -1,3 +1,22 @@
 import Watchable from './watchable';
+import addToPath from 'nomad-ui/utils/add-to-path';
 
-export default Watchable.extend();
+export default Watchable.extend({
+  setEligible(node) {
+    return this.setEligibility(node, true);
+  },
+
+  setIneligible(node) {
+    return this.setEligibility(node, false);
+  },
+
+  setEligibility(node, isEligible) {
+    const url = addToPath(this.urlForFindRecord(node.id, 'node'), '/eligibility');
+    return this.ajax(url, 'POST', {
+      data: {
+        NodeID: node.id,
+        Eligibility: isEligible ? 'eligible' : 'ineligible',
+      },
+    });
+  },
+});
