dc5ee22e273b5efd45462b3490036cd75aaaf378
Danielle Lancashire <dani@hashicorp.com>
2019-10-28 13:16:32 +0100

    csi: Implement base.BasePlugin in Client

--- plugins/csi/client.go
+++ plugins/csi/client.go
@@ -7,28 +7,10 @@ import (
 	"time"
 
 	csipbv1 "github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/hashicorp/nomad/plugins/base"
+	"github.com/hashicorp/nomad/plugins/shared/hclspec"
 	"google.golang.org/grpc"
 )
-
-// Client implements a lightweight abstraction layer around a CSI Plugin.
-// It validates that responses from storage providers (SP's), correctly conform
-// to the specification before returning response data or erroring.
-type Client interface {
-	// PluginProbe is used to verify that the plugin is in a healthy state
-	PluginProbe(ctx context.Context) (bool, error)
-
-	// PluginGetInfo is used to return semantic data about the plugin.
-	// Response:
-	//  - string: name, the name of the plugin in domain notation format.
-	PluginGetInfo(ctx context.Context) (string, error)
-
-	// NodeGetInfo is used to return semantic data about the current node in
-	// respect to the SP.
-	NodeGetInfo(ctx context.Context) (*NodeGetInfoResponse, error)
-
-	// Shutdown the client and ensure any connections are cleaned up.
-	Close() error
-}
 
 type NodeGetInfoResponse struct {
 	NodeID     string
@@ -49,7 +31,7 @@ func (c *client) Close() error {
 	return nil
 }
 
-func NewClient(addr string) (Client, error) {
+func NewClient(addr string) (CSIPlugin, error) {
 	if addr == "" {
 		return nil, fmt.Errorf("address is empty")
 	}
@@ -81,6 +63,34 @@ func newGrpcConn(addr string) (*grpc.ClientConn, error) {
 	}
 
 	return conn, nil
+}
+
+// PluginInfo describes the type and version of a plugin as required by the nomad
+// base.BasePlugin interface.
+func (c *client) PluginInfo() (*base.PluginInfoResponse, error) {
+	name, err := c.PluginGetInfo(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	return &base.PluginInfoResponse{
+		Type:              "csi",
+		PluginApiVersions: []string{"1.0.0"}, // TODO: fingerprint csi version
+		PluginVersion:     "1.0.0",           // TODO: get plugin version from somewhere?!
+		Name:              name,
+	}, nil
+}
+
+// ConfigSchema returns the schema for parsing the plugins configuration as
+// required by the base.BasePlugin interface. It will always return nil.
+func (c *client) ConfigSchema() (*hclspec.Spec, error) {
+	return nil, nil
+}
+
+// SetConfig is used to set the configuration by passing a MessagePack
+// encoding of it.
+func (c *client) SetConfig(_ *base.Config) error {
+	return fmt.Errorf("unsupported")
 }
 
 func (c *client) PluginProbe(ctx context.Context) (bool, error) {
