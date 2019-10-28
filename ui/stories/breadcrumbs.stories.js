d9ca89e553e311ed1f62d1c8fe8e6cdd702e97d3
Buck Doyle <buck@hashicorp.com>
2019-10-28 15:46:23 -0500

    Add breadcrumbs stories

---
+++ ui/stories/breadcrumbs.stories.js
@@ -0,0 +1,43 @@
+/* eslint-env node */
+import hbs from 'htmlbars-inline-precompile';
+import { storiesOf } from '@storybook/ember';
+
+storiesOf('Breadcrumbs/', module)
+  .addParameters({ options: { showPanel: true } })
+  .add(`Standard breadcrumbs`, () => ({
+    template: hbs`
+    <h5 class="title is-5">Standard breadcrumbs</h5>
+      <div class="navbar is-secondary">
+      <div class="navbar-item"></div>
+      <nav class="breadcrumb is-large">
+        <li>
+          <a href="#">Topic</a>
+        </li>
+        <li>
+          <a href="#">Sub-topic</a>
+        </li>
+        <li class="is-active">
+          <a href="#">Active Topic</a>
+        </li>
+      </nav>
+    </div>
+    <p>Breadcrumbs are only ever used in the secondary nav of the primary header.</p>
+    `,
+  }),
+  {})
+  .add(`Single breadcrumb`, () => ({
+    template: hbs`
+      <h5 class="title is-5">Single breadcrumb</h5>
+      <div class="navbar is-secondary">
+        <div class="navbar-item"></div>
+        <nav class="breadcrumb is-large">
+          <li>
+            <a href="#">Topic</a>
+          </li>
+        </nav>
+      </div>
+      <p>Breadcrumbs are given a lot of emphasis and often double as a page title. Since they are also global state, they are important for helping a user keep their bearings.</p>
+  `,
+  }),
+  {}
+);
