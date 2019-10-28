5e3fa90ff5d80c53edf45a0852a1fb890d84f848
Danielle Lancashire <dani@hashicorp.com>
2019-10-28 13:16:32 +0100

    csi: Implement base.BasePlugin in Client

---
+++ plugins/csi/plugin.go
@@ -0,0 +1,29 @@
+package csi
+
+import (
+	"context"
+
+	"github.com/hashicorp/nomad/plugins/base"
+)
+
+// CSIPlugin implements a lightweight abstraction layer around a CSI Plugin.
+// It validates that responses from storage providers (SP's), correctly conform
+// to the specification before returning response data or erroring.
+type CSIPlugin interface {
+	base.BasePlugin
+
+	// PluginProbe is used to verify that the plugin is in a healthy state
+	PluginProbe(ctx context.Context) (bool, error)
+
+	// PluginGetInfo is used to return semantic data about the plugin.
+	// Response:
+	//  - string: name, the name of the plugin in domain notation format.
+	PluginGetInfo(ctx context.Context) (string, error)
+
+	// NodeGetInfo is used to return semantic data about the current node in
+	// respect to the SP.
+	NodeGetInfo(ctx context.Context) (*NodeGetInfoResponse, error)
+
+	// Shutdown the client and ensure any connections are cleaned up.
+	Close() error
+}
