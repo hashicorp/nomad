8c4e46b41cf97fe344371b7c09fd436b658da090
Buck Doyle <buck@hashicorp.com>
2019-10-30 12:12:38 -0500

    Add theme stories

---
+++ ui/stories/theme/font-stacks.stories.js
@@ -0,0 +1,34 @@
+import hbs from 'htmlbars-inline-precompile';
+
+export default {
+  title: 'Theme|Font Stacks',
+};
+
+export const FontStacks = () => {
+  return {
+    template: hbs`
+      <h5 class="title is-5">Font Stacks</h5>
+
+      {{#each fontFamilies as |fontFamily|}}
+        <h6 class="title is-6 with-headroom">{{fontFamily}}</h6>
+        {{!- FIXME replace with a non-Freestyle solution? --}}
+        <FreestyleTypeface @fontFamily={{fontFamily}} />
+
+      {{/each}}
+    `,
+    context: {
+      fontFamilies: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen-Sans',
+        'Ubuntu',
+        'Cantarell',
+        'Helvetica Neue',
+        'sans-serif',
+        'monospace',
+      ],
+    },
+  };
+};
