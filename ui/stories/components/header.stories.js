e552065a7dcd8f0eb40268a5eff49ce0ac42a5b9
Buck Doyle <buck@hashicorp.com>
2019-10-30 14:10:50 -0500

    Add header story

---
+++ ui/stories/components/header.stories.js
@@ -0,0 +1,29 @@
+/* eslint-env node */
+import hbs from 'htmlbars-inline-precompile';
+
+export default {
+  title: 'Components|Header',
+};
+
+export const Header = () => {
+  return {
+    template: hbs`
+      <h5 class="title is-5">Global Header</h5>
+      <nav class="navbar is-primary">
+        <div class="navbar-brand">
+          <span class="gutter-toggle" aria-label="menu">
+            {{partial "partials/hamburger-menu"}}
+          </span>
+          <span class="navbar-item is-logo">
+            {{partial "partials/nomad-logo"}}
+          </span>
+        </div>
+        <div class="navbar-end">
+          <a class="navbar-item">Secondary</a>
+          <a class="navbar-item">Links</a>
+          <a class="navbar-item">Here</a>
+        </div>
+      </nav>
+      `,
+  };
+};
