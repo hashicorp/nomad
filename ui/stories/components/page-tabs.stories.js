8788716337bb317cfc70026b6f5c6b923459b309
Buck Doyle <buck@hashicorp.com>
2019-10-30 15:39:13 -0500

    Add page tabs stories

---
+++ ui/stories/components/page-tabs.stories.js
@@ -0,0 +1,36 @@
+/* eslint-env node */
+import hbs from 'htmlbars-inline-precompile';
+
+export default {
+  title: 'Components|Page Tabs',
+};
+
+export const Tabs = () => {
+  return {
+    template: hbs`
+      <h5 class="title is-5">Page Tabs</h5>
+      <div class="tabs">
+        <ul>
+          <li><a href="#">Overview</a></li>
+          <li><a href="#" class="is-active">Definition</a></li>
+          <li><a href="#">Versions</a></li>
+          <li><a href="#">Deployments</a></li>
+        </ul>
+      </div>
+      <!-- FIXME clicking navigates to the iframe! -->
+          `,
+  };
+};
+
+export const Single = () => {
+  return {
+    template: hbs`
+      <h5 class="title is-5">Single Page Tab</h5>
+      <div class="tabs">
+        <ul>
+          <li><a href="#" class="is-active">Overview</a></li>
+        </ul>
+      </div>
+          `,
+  };
+};
