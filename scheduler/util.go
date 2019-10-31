7a5640874a4c7b3a1d7ed1ac94ec0de84f0cf6ac
Michael Schurter <mschurter@hashicorp.com>
2019-10-31 08:46:34 -0700

    Merge pull request #6541 from hashicorp/release-0100-net-panic
    core: fix panic when AllocatedResources is nil

--- scheduler/util.go
+++ scheduler/util.go
@@ -838,11 +838,17 @@ func genericAllocUpdateFn(ctx Context, stack Stack, evalID string) allocUpdateTy
 			Tasks: option.TaskResources,
 			Shared: structs.AllocatedSharedResources{
 				DiskMB: int64(newTG.EphemeralDisk.SizeMB),
-				// Since this is an inplace update, we should copy network
-				// information from the original alloc. This is similar to
-				// how we copy network info for task level networks above.
-				Networks: existing.AllocatedResources.Shared.Networks,
 			},
+		}
+
+		// Since this is an inplace update, we should copy network
+		// information from the original alloc. This is similar to how
+		// we copy network info for task level networks above.
+		//
+		// existing.AllocatedResources is nil on Allocations created by
+		// Nomad v0.8 or earlier.
+		if existing.AllocatedResources != nil {
+			newAlloc.AllocatedResources.Shared.Networks = existing.AllocatedResources.Shared.Networks
 		}
 
 		// Use metrics from existing alloc for in place upgrade
