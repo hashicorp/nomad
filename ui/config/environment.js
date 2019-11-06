cadbd6a125cc8e6b7ea1cc6d1d6060cd0b60fcb3
Buck Doyle <buck@hashicorp.com>
2019-11-06 15:32:44 -0600

    Restore root URL
    This will break the Netlify deployment, hopefully I can figure
    out another way to have it work.

--- ui/config/environment.js
+++ ui/config/environment.js
@@ -10,7 +10,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'nomad-ui',
     environment: environment,
-    rootURL: '/', // FIXME
+    rootURL: '/ui',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
