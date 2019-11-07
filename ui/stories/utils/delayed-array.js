788baae63a6c10964c2c878e8fb00998c5c096c8
Buck Doyle <buck@hashicorp.com>
2019-11-07 11:19:46 -0600

    Change delay utilities to use next vs later

--- ui/stories/utils/delayed-array.js
+++ ui/stories/utils/delayed-array.js
@@ -1,18 +1,14 @@
 import { A } from '@ember/array';
 import ArrayProxy from '@ember/array/proxy';
-import { later } from '@ember/runloop';
+import { next } from '@ember/runloop';
 
 export default ArrayProxy.extend({
   init(array) {
     this.set('content', A([]));
     this._super(...arguments);
 
-    later(
-      this,
-      () => {
-        this.set('content', A(array));
-      },
-      100
-    );
+    next(this, () => {
+      this.set('content', A(array));
+    });
   },
 });
--- ui/stories/utils/delayed-truth.js
+++ ui/stories/utils/delayed-truth.js
@@ -1,17 +1,13 @@
 import EmberObject from '@ember/object';
-import { later } from '@ember/runloop';
+import { next } from '@ember/runloop';
 
 export default EmberObject.extend({
   init() {
     this._super(...arguments);
     this.set('complete', false);
 
-    later(
-      this,
-      () => {
-        this.set('complete', true);
-      },
-      100
-    );
+    next(this, () => {
+      this.set('complete', true);
+    });
   },
 });
