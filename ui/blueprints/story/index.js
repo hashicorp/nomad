93fb49f1b1cbe3dd2c1d7a12443f258090a99637
Buck Doyle <buck@hashicorp.com>
2019-11-07 10:12:49 -0600

    Remove in-repo-addon-related code from blueprint

--- ui/blueprints/story/index.js
+++ ui/blueprints/story/index.js
@@ -2,16 +2,6 @@ const getPathOption = require('ember-cli-get-component-path-option');
 const stringUtil = require('ember-cli-string-utils');
 const path = require('path');
 
-function findAddonByName(addonOrProject, name) {
-  let addon = addonOrProject.addons.find(addon => addon.name === name);
-
-  if (addon) {
-    return addon;
-  }
-
-  return addonOrProject.addons.find(addon => findAddonByName(addon, name));
-}
-
 module.exports = {
   description: 'generates a story for storybook',
 
@@ -19,10 +9,6 @@ module.exports = {
     let { project } = this;
     return {
       __path__: function(options) {
-        if (options.inRepoAddon) {
-          let addon = findAddonByName(project, options.inRepoAddon);
-          return path.relative(project.root, addon.root);
-        }
         return path.relative(project.root, project.root);
       },
       __markdownname__: function(options) {
