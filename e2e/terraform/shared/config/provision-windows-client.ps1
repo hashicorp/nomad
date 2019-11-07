ca61e5c43078b71b5ff7f2859caa4a643bfbb129
Tim Gross <tim@0x74696d.com>
2019-11-07 15:59:30 -0500

    corrected a bunch of path issues with sc.exe

--- e2e/terraform/shared/config/provision-windows-client.ps1
+++ e2e/terraform/shared/config/provision-windows-client.ps1
@@ -3,10 +3,13 @@ param([string]$cloud="aws", [string]$nomad_sha="")
 # TODO: index is currently unused
 #[string]$index=0)
 
-# Consul
+# Force TLS1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-cp "C:\ops\shared\consul\consul_client_$cloud.json" C:\opt\consul.d\config.json
-sc.exe create "Consul" binPath= "C:\opt\consul.exe" agent -config-dir "C:\opt\consul.d" start= auto
+# Consul
+cp "C:\ops\shared\consul\base.json" "C:\opt\consul.d\base.json"
+cp "C:\ops\shared\consul\retry_$cloud.json" "C:\opt\consul.d\retry_$cloud.json"
+sc.exe create "Consul" binPath= "C:\opt\consul.exe agent -config-dir C:\opt\consul.d" start= auto
 sc.exe start "Consul"
 
 # Vault
@@ -20,23 +23,26 @@ md C:\opt\nomad
 
 Read-S3Object `
   -BucketName nomad-team-test-binary `
-  -Key "builds-oss/nomad_windows_amd64_$nomad_sha.tar.gz" `
-  -File .\nomad.tar.gz
+  -Key "builds-oss/nomad_windows_amd64_$nomad_sha.zip" `
+  -File .\nomad.zip
+
+Expand-Archive .\nomad.zip .\
 rm C:\opt\nomad.exe
-Expand-Archive .\nomad.tar.gz C:\opt\nomad.exe
+mv nomad.exe C:\opt\nomad.exe
 
 # install config file
-cp "C:\ops\shared\nomad\client-windows.hcl" C:\opt\nomad.d\nomad.hcl
+cp "C:\ops\shared\nomad\client-windows.hcl" "C:\opt\nomad.d\nomad.hcl"
 
 # Setup Host Volumes
 md C:\tmp\data
 
-Write-Output "Install CNI"
-md C:\opt\cni\bin
-$cni_url = "https://github.com/containernetworking/plugins/releases/download/v0.8.2/cni-plugins-windows-amd64-v0.8.2.tgz"
-Invoke-WebRequest -Uri "$cni_url" -Outfile cni.tgz
-Expand-Archive .\cni.tgz C:\opt\cni\bin
+# TODO(tgross): not sure we even support this for Windows?
+# Write-Output "Install CNI"
+# md C:\opt\cni\bin
+# $cni_url = "https://github.com/containernetworking/plugins/releases/download/v0.8.2/cni-plugins-windows-amd64-v0.8.2.tgz"
+# Invoke-WebRequest -Uri "$cni_url" -Outfile cni.tgz
+# Expand-7Zip -ArchiveFileName .\cni.tgz -TargetPath C:\opt\cni\bin\
 
 # enable as a service
-sc.exe create "Nomad" binPath= "C:\opt\nomad.exe" agent -config-dir "C:\opt\nomad.d" start= auto
+sc.exe create "Nomad" binPath= "C:\opt\nomad.exe agent -config-dir C:\opt\nomad.d" start= auto
 sc.exe start "Nomad"
