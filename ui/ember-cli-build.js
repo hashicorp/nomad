11b0da4aa64e0c0bceb3e1ef2f7e6f2025314191
Buck Doyle <buck@hashicorp.com>
2019-11-13 15:41:34 -0600

    Add xterm.js

--- ui/ember-cli-build.js
+++ ui/ember-cli-build.js
@@ -52,5 +52,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('node_modules/xterm/css/xterm.css');
+
   return app.toTree();
 };
