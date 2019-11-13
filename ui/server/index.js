7256769437a61350a69846213960ba8cc11b735c
Buck Doyle <buck@hashicorp.com>
2019-11-13 14:59:49 -0600

    Add proxy that properly handles websockets
    This is adapted from helpful posts in this thread:
    https://github.com/ember-cli/ember-cli/issues/2508
    
    Without this, the origin header wasn’t being rewritten
    so I was unable to open a websocket to the API running
    on a different port.

---
+++ ui/server/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// To use it create some files under `mocks/`
+// e.g. `server/mocks/ember-hamsters.js`
+//
+// module.exports = function(app) {
+//   app.get('/ember-hamsters', function(req, res) {
+//     res.send('hello');
+//   });
+// };
+
+module.exports = function(app, options) {
+  const globSync   = require('glob').sync;
+  const mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
+  const proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
+
+  // Log proxy requests
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+
+  mocks.forEach(route => route(app, options));
+  proxies.forEach(route => route(app, options));
+};
---
+++ ui/server/proxies/api.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const proxyPath = '/v1';
+
+module.exports = function(app, options) {
+  // For options, see:
+  // https://github.com/nodejitsu/node-http-proxy
+
+  let server = options.httpServer;
+  let proxy = require('http-proxy').createProxyServer({
+    target: 'http://localhost:4646',
+    ws: true,
+    changeOrigin: true
+  });
+
+  proxy.on('error', function(err, req) {
+    console.error(err, req.url);
+  });
+
+  app.use(proxyPath, function(req, res, next){
+    // include root path in proxied request
+    req.url = proxyPath + req.url;
+    proxy.web(req, res, { target: 'http://localhost:4646'});
+  });
+
+  server.on('upgrade', function (req, socket, head) {
+    if (req.url.startsWith('/v1/client/allocation') && req.url.includes('exec?')) {
+      req.headers.origin = 'http://localhost:4646';
+      proxy.ws(req, socket, head, { target: 'http://localhost:4646'});
+    }
+  });
+};
