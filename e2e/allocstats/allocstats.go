5333bf7f283bfc083444bbe82e06243323e294f0
Tim Gross <tgross@hashicorp.com>
2019-11-25 16:06:56 -0500

    e2e: add allocstats test for Windows
    Extends the BasicAllocStats test to include a test for Windows
    clients, exercising stats via a powershell `raw_exec` job.
    
    Adds `ListLinuxClientNodes` and `ListWindowsClientNodes` utils so that
    we can scope tests to run only when Linux or Windows clients are
    available. This prevents waiting on timeouts when running a subset of
    the tests against a development cluster (vs our nightly test
    cluster).

--- e2e/allocstats/allocstats.go
+++ e2e/allocstats/allocstats.go
@@ -1,17 +1,16 @@
 package allocstats
 
 import (
-	"github.com/hashicorp/nomad/e2e/framework"
-	"github.com/stretchr/testify/require"
-
 	"fmt"
-
 	"time"
 
 	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/nomad/e2e/e2eutil"
+	"github.com/hashicorp/nomad/e2e/framework"
 	"github.com/hashicorp/nomad/helper/uuid"
 	"github.com/hashicorp/nomad/testutil"
+
+	"github.com/stretchr/testify/require"
 )
 
 type BasicAllocStatsTest struct {
@@ -32,7 +31,7 @@ func init() {
 func (tc *BasicAllocStatsTest) BeforeAll(f *framework.F) {
 	// Ensure cluster has leader before running tests
 	e2eutil.WaitForLeader(f.T(), tc.Nomad())
-	// Ensure that we have four client nodes in ready state
+	// Ensure that we have at least one client node in ready state
 	e2eutil.WaitForNodesReady(f.T(), tc.Nomad(), 1)
 }
 
@@ -41,10 +40,45 @@ func (tc *BasicAllocStatsTest) BeforeAll(f *framework.F) {
 // TODO(preetha) - add more test cases with more realistic resource utilization
 func (tc *BasicAllocStatsTest) TestResourceStats(f *framework.F) {
 	nomadClient := tc.Nomad()
+
+	clientNodes, err := e2eutil.ListLinuxClientNodes(nomadClient)
+	if err != nil {
+		f.T().Fatalf("could not list client nodes: %v", err)
+	}
+	if len(clientNodes) == 0 {
+		f.T().Skip("no Linux clients")
+	}
+
 	uuid := uuid.Generate()
-	jobId := "allocstats" + uuid[0:8]
-	tc.jobIds = append(tc.jobIds, jobId)
-	allocs := e2eutil.RegisterAndWaitForAllocs(f.T(), nomadClient, "allocstats/input/raw_exec.nomad", jobId)
+	jobID := "allocstats" + uuid[0:8]
+	tc.jobIds = append(tc.jobIds, jobID)
+	runResourceStatsTest(f, nomadClient, jobID,
+		"allocstats/input/raw_exec.nomad")
+}
+
+// TestResourceStatsWindows is an end to end test for resource utilization.
+// This runs a raw exec job.
+func (tc *BasicAllocStatsTest) TestResourceStatsWindows(f *framework.F) {
+	nomadClient := tc.Nomad()
+
+	clientNodes, err := e2eutil.ListWindowsClientNodes(nomadClient)
+	if err != nil {
+		f.T().Fatalf("could not list client nodes: %v", err)
+	}
+	if len(clientNodes) == 0 {
+		f.T().Skip("no Windows clients")
+	}
+
+	uuid := uuid.Generate()
+	jobID := "allocstats_windows" + uuid[0:8]
+	tc.jobIds = append(tc.jobIds, jobID)
+	runResourceStatsTest(f, nomadClient, jobID,
+		"allocstats/input/raw_exec_windows.nomad")
+}
+
+func runResourceStatsTest(f *framework.F, nomadClient *api.Client, jobID, jobSpec string) {
+
+	allocs := e2eutil.RegisterAndWaitForAllocs(f.T(), nomadClient, jobSpec, jobID)
 
 	require := require.New(f.T())
 	require.Len(allocs, 1)
@@ -70,7 +104,6 @@ func (tc *BasicAllocStatsTest) TestResourceStats(f *framework.F) {
 	}, func(err error) {
 		f.T().Fatalf("invalid resource usage : %v", err)
 	})
-
 }
 
 func (tc *BasicAllocStatsTest) AfterEach(f *framework.F) {
