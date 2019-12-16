65133c6c5d8f7e4643e6be16087303dc044a289c
Danielle Lancashire <dani@hashicorp.com>
2019-12-16 14:19:59 +0100

    client: Setup CSI RPC Endpoint
    This commit introduces a new set of endpoints to a Nomad Client:
    ClientCSI.
    
    ClientCSI is responsible for mediating requests from a Nomad Server to
    a CSI Plugin running on a Nomad Client. It should only really be used to
    make controller RPCs.

---
+++ client/client_csi_endpoint.go
@@ -0,0 +1,81 @@
+package client
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	metrics "github.com/armon/go-metrics"
+	"github.com/hashicorp/nomad/client/dynamicplugins"
+	"github.com/hashicorp/nomad/client/structs"
+	"github.com/hashicorp/nomad/plugins/csi"
+)
+
+// ClientCSI endpoint is used for interacting with CSI plugins on a client.
+// TODO: Submit metrics with labels to allow debugging per plugin perf problems.
+type ClientCSI struct {
+	c *Client
+}
+
+const (
+	// CSIPluginRequestTimeout is the timeout that should be used when making reqs
+	// against CSI Plugins. It is copied from Kubernetes as an initial seed value.
+	// https://github.com/kubernetes/kubernetes/blob/e680ad7156f263a6d8129cc0117fda58602e50ad/pkg/volume/csi/csi_plugin.go#L52
+	CSIPluginRequestTimeout = 2 * time.Minute
+)
+
+var (
+	ErrPluginTypeError = errors.New("CSI Plugin loaded incorrectly")
+)
+
+// CSIControllerPublishVolume is used to attach a volume from a CSI Cluster to
+// the storage node provided in the request.
+func (c *ClientCSI) CSIControllerPublishVolume(req *structs.ClientCSIControllerPublishVolumeRequest, resp *structs.ClientCSIControllerPublishVolumeResponse) error {
+	defer metrics.MeasureSince([]string{"client", "csi_controller", "publish_volume"}, time.Now())
+	client, err := c.findControllerPlugin(req.PluginName)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	if req.VolumeID == "" {
+		return errors.New("VolumeID is required")
+	}
+
+	if req.NodeID == "" {
+		return errors.New("NodeID is required")
+	}
+
+	ctx, cancelFn := c.requestContext()
+	defer cancelFn()
+	cresp, err := client.ControllerPublishVolume(ctx, req.ToCSIRequest())
+	if err != nil {
+		return err
+	}
+
+	resp.PublishContext = cresp.PublishContext
+	return nil
+}
+
+func (c *ClientCSI) findControllerPlugin(name string) (csi.CSIPlugin, error) {
+	return c.findPlugin(dynamicplugins.PluginTypeCSIController, name)
+}
+
+// TODO: Cache Plugin Clients?
+func (c *ClientCSI) findPlugin(ptype, name string) (csi.CSIPlugin, error) {
+	pIface, err := c.c.dynamicRegistry.DispensePlugin(ptype, name)
+	if err != nil {
+		return nil, err
+	}
+
+	plugin, ok := pIface.(csi.CSIPlugin)
+	if !ok {
+		return nil, ErrPluginTypeError
+	}
+
+	return plugin, nil
+}
+
+func (c *ClientCSI) requestContext() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), CSIPluginRequestTimeout)
+}
