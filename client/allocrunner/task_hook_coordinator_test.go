c2ab4c9c90e0bce2c6d271dee581c5bc6c37030b
Jasmine Dahilig <jasmine@dahilig.com>
2019-12-16 11:08:36 -0800

    add test for lifecycle coordinator

--- client/allocrunner/task_hook_coordinator_test.go
+++ client/allocrunner/task_hook_coordinator_test.go
@@ -24,3 +24,34 @@ func TestTaskHookCoordinator_OnlyMainApp(t *testing.T) {
 		require.Fail(t, "channel wasn't closed")
 	}
 }
+
+func TestTaskHookCoordinator_Prestart(t *testing.T) {
+	alloc := mock.Alloc()
+	tasks := alloc.Job.TaskGroups[0].Tasks
+	logger := testlog.HCLogger(t)
+
+	tasks = append(tasks, mock.InitTask())
+	tasks = append(tasks, mock.SidecarTask())
+	coord := newTaskHookCoordinator(logger, tasks)
+
+	mainCh := coord.startConditionForTask(tasks[0])
+	initCh := coord.startConditionForTask(tasks[1])
+	sideCh := coord.startConditionForTask(tasks[2])
+
+	select {
+	case _, ok := <-initCh:
+		require.False(t, ok)
+	case _, ok := <-sideCh:
+		require.False(t, ok)
+	default:
+		require.Fail(t, "prestart channels weren't closed")
+	}
+
+	select {
+	case <-mainCh:
+		require.Fail(t, "channel was closed, should be open")
+	default:
+		// channel for main task is open, which is correct: coordinator should
+		// block all other tasks until prestart tasks are completed
+	}
+}
