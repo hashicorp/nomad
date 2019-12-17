b15b2cfe3395fe68abb96907db919e9023758678
Lang Martin <lang@hashicorp.com>
2019-12-17 12:44:03 -0500

    test: build quota_apply_test, remove the tests that require ent

--- command/quota_apply_test.go
+++ command/quota_apply_test.go
@@ -1,16 +1,11 @@
-// +build ent
-
 package command
 
 import (
-	"io/ioutil"
-	"os"
 	"strings"
 	"testing"
 
 	"github.com/hashicorp/nomad/api"
 	"github.com/mitchellh/cli"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -40,64 +35,6 @@ func TestQuotaApplyCommand_Fails(t *testing.T) {
 		t.Fatalf("name required error, got: %s", out)
 	}
 	ui.ErrorWriter.Reset()
-}
-
-func TestQuotaApplyCommand_Good_HCL(t *testing.T) {
-	t.Parallel()
-
-	// Create a server
-	srv, client, url := testServer(t, true, nil)
-	defer srv.Shutdown()
-
-	ui := new(cli.MockUi)
-	cmd := &QuotaApplyCommand{Meta: Meta{Ui: ui}}
-
-	fh1, err := ioutil.TempFile("", "nomad")
-	if err != nil {
-		t.Fatalf("err: %s", err)
-	}
-	defer os.Remove(fh1.Name())
-	if _, err := fh1.WriteString(defaultHclQuotaSpec); err != nil {
-		t.Fatalf("err: %s", err)
-	}
-
-	// Create a quota spec
-	if code := cmd.Run([]string{"-address=" + url, fh1.Name()}); code != 0 {
-		t.Fatalf("expected exit 0, got: %d; %v", code, ui.ErrorWriter.String())
-	}
-
-	quotas, _, err := client.Quotas().List(nil)
-	assert.Nil(t, err)
-	assert.Len(t, quotas, 1)
-}
-
-func TestQuotaApplyCommand_Good_JSON(t *testing.T) {
-	t.Parallel()
-
-	// Create a server
-	srv, client, url := testServer(t, true, nil)
-	defer srv.Shutdown()
-
-	ui := new(cli.MockUi)
-	cmd := &QuotaApplyCommand{Meta: Meta{Ui: ui}}
-
-	fh1, err := ioutil.TempFile("", "nomad")
-	if err != nil {
-		t.Fatalf("err: %s", err)
-	}
-	defer os.Remove(fh1.Name())
-	if _, err := fh1.WriteString(defaultJsonQuotaSpec); err != nil {
-		t.Fatalf("err: %s", err)
-	}
-
-	// Create a quota spec
-	if code := cmd.Run([]string{"-address=" + url, "-json", fh1.Name()}); code != 0 {
-		t.Fatalf("expected exit 0, got: %d; %v", code, ui.ErrorWriter.String())
-	}
-
-	quotas, _, err := client.Quotas().List(nil)
-	assert.Nil(t, err)
-	assert.Len(t, quotas, 1)
 }
 
 func TestQuotaApplyNetwork(t *testing.T) {
