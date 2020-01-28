cd4d3d1f40f81539952215d786580707a39e58b0
Danielle Lancashire <dani@hashicorp.com>
2020-01-28 13:19:56 +0100

    volume_manager: Introduce helpers for staging
    This commit adds helpers that create and validate the staging directory
    for a given volume. It is currently missing usage options as the
    interfaces are not yet in place for those.
    
    The staging directory is only required when a volume has the
    STAGE_UNSTAGE Volume capability and has to live within the plugin root
    as the plugin needs to be able to create mounts inside it from within
    the container.

---
+++ client/pluginmanager/csimanager/volume_test.go
@@ -0,0 +1,108 @@
+package csimanager
+
+import (
+	"io/ioutil"
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/hashicorp/nomad/helper/testlog"
+	"github.com/hashicorp/nomad/nomad/structs"
+	csifake "github.com/hashicorp/nomad/plugins/csi/fake"
+	"github.com/stretchr/testify/require"
+)
+
+func tmpDir(t testing.TB) string {
+	t.Helper()
+	dir, err := ioutil.TempDir("", "nomad")
+	require.NoError(t, err)
+	return dir
+}
+
+func TestVolumeManager_ensureStagingDir(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		Name                 string
+		Volume               *structs.CSIVolume
+		CreateDirAheadOfTime bool
+		MountDirAheadOfTime  bool
+
+		ExpectedErr        error
+		ExpectedMountState bool
+	}{
+		{
+			Name:   "Creates a directory when one does not exist",
+			Volume: &structs.CSIVolume{ID: "foo"},
+		},
+		{
+			Name:                 "Does not fail because of a pre-existing directory",
+			Volume:               &structs.CSIVolume{ID: "foo"},
+			CreateDirAheadOfTime: true,
+		},
+		{
+			Name:   "Returns negative mount info",
+			Volume: &structs.CSIVolume{ID: "foo"},
+		},
+		{
+			Name:                 "Returns positive mount info",
+			Volume:               &structs.CSIVolume{ID: "foo"},
+			CreateDirAheadOfTime: true,
+			MountDirAheadOfTime:  true,
+			ExpectedMountState:   true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			// Step 1: Validate that the test case makes sense
+			if !tc.CreateDirAheadOfTime && tc.MountDirAheadOfTime {
+				require.Fail(t, "Cannot Mount without creating a dir")
+			}
+
+			if tc.MountDirAheadOfTime {
+				// We can enable these tests by either mounting a fake device on linux
+				// e.g shipping a small ext4 image file and using that as a loopback
+				//     device, but there's no convenient way to implement this.
+				t.Skip("TODO: Skipped because we don't detect bind mounts")
+			}
+
+			// Step 2: Test Setup
+			tmpPath := tmpDir(t)
+			defer os.RemoveAll(tmpPath)
+
+			csiFake := &csifake.Client{}
+			manager := newVolumeManager(testlog.HCLogger(t), csiFake, tmpPath, true)
+			expectedStagingPath := manager.stagingDirForVolume(tc.Volume)
+
+			if tc.CreateDirAheadOfTime {
+				err := os.MkdirAll(expectedStagingPath, 0700)
+				require.NoError(t, err)
+			}
+
+			// Step 3: Now we can do some testing
+
+			detectedMount, path, testErr := manager.ensureStagingDir(tc.Volume)
+			if tc.ExpectedErr != nil {
+				require.EqualError(t, testErr, tc.ExpectedErr.Error())
+				return // We don't perform extra validation if an error was detected.
+			}
+
+			require.NoError(t, testErr)
+			require.Equal(t, tc.ExpectedMountState, detectedMount)
+
+			// If the ensureStagingDir call had to create a directory itself, then here
+			// we validate that the directory exists and its permissions
+			if !tc.CreateDirAheadOfTime {
+				file, err := os.Lstat(path)
+				require.NoError(t, err)
+				require.True(t, file.IsDir())
+
+				// TODO: Figure out a windows equivalent of this test
+				if runtime.GOOS != "windows" {
+					require.Equal(t, os.FileMode(0700), file.Mode().Perm())
+				}
+			}
+		})
+	}
+}
