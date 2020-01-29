3367be941fcec9d4f16070c019ea33d7ce66f37b
Danielle Lancashire <dani@hashicorp.com>
2020-01-29 12:35:35 +0100

    helper/mount: Add mount helper package
    This package introduces some basic abstractions around mount utilties
    for various platforms. Initially it only supports linux, but the plan is
    to expand this as CSI expands across to other platforms.

---
+++ helper/mount/mount.go
@@ -0,0 +1,16 @@
+package mount
+
+// Mounter defines the set of methods to allow for mount operations on a system.
+type Mounter interface {
+	// IsNotAMountPoint detects if a provided directory is not a mountpoint.
+	IsNotAMountPoint(file string) (bool, error)
+
+	// Mount will mount filesystem according to the specified configuration, on
+	// the condition that the target path is *not* already mounted. Options must
+	// be specified like the mount or fstab unix commands: "opt1=val1,opt2=val2".
+	Mount(device, target, mountType, options string) error
+}
+
+// Compile-time check to ensure all Mounter implementations satisfy
+// the mount interface.
+var _ Mounter = &mounter{}
