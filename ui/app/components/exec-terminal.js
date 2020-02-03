75c2c5764e85c4e0ccd9eac87225affae8e25e47
Buck Doyle <buck@hashicorp.com>
2020-02-03 10:11:46 -0600

    Add auto-fit for terminal
    This isn’t responsive to resize events etc yet but at least
    looks better.

--- ui/app/components/exec-terminal.js
+++ ui/app/components/exec-terminal.js
@@ -1,7 +1,15 @@
 import Component from '@ember/component';
+import { FitAddon } from 'xterm-addon-fit';
 
 export default Component.extend({
+  classNames: ['terminal-container'],
+
   didInsertElement() {
+    const fitAddon = new FitAddon();
+    this.terminal.loadAddon(fitAddon);
+
     this.terminal.open(this.element.querySelector('.terminal'));
+
+    fitAddon.fit();
   },
 });
