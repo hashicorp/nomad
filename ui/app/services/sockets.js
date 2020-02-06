a1960d25ffabd7079e5b19c6922518414be60308
Buck Doyle <buck@hashicorp.com>
2020-02-06 16:00:58 -0600

    Add placeholder socket for Mirage context

--- ui/app/services/sockets.js
+++ ui/app/services/sockets.js
@@ -1,11 +1,30 @@
 import Service from '@ember/service';
+import config from 'nomad-ui/config/environment';
 
 export default Service.extend({
   getTaskStateSocket(taskState, command) {
-    return new WebSocket(
-      `ws://localhost:4200/v1/client/allocation/${taskState.allocation.id}` +
-        `/exec?task=${taskState.name}&tty=true` +
-        `&command=${encodeURIComponent(`["${command}"]`)}`
-    );
+    const mirageEnabled =
+      config['ember-cli-mirage'] && config['ember-cli-mirage'].enabled !== false;
+
+    if (mirageEnabled) {
+      return new Object({
+        messageDisplayed: false,
+
+        send(e) {
+          if (!this.messageDisplayed) {
+            this.messageDisplayed = true;
+            this.onmessage({ data: `{"stdout":{"data":"${btoa('unsupported in Mirage\n\r')}"}}` });
+          } else {
+            this.onmessage({ data: e.replace('stdin', 'stdout') });
+          }
+        },
+      });
+    } else {
+      return new WebSocket(
+        `ws://localhost:4200/v1/client/allocation/${taskState.allocation.id}` +
+          `/exec?task=${taskState.name}&tty=true` +
+          `&command=${encodeURIComponent(`["${command}"]`)}`
+      );
+    }
   },
 });
