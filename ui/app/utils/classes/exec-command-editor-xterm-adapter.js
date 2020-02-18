c87c365e5b5eac04e06031d878dc16d2a1cb771d
Buck Doyle <buck@hashicorp.com>
2020-02-18 10:14:19 -0500

    Add wraparound backspacing for command-editing
    This seems to work…?!

--- ui/app/utils/classes/exec-command-editor-xterm-adapter.js
+++ ui/app/utils/classes/exec-command-editor-xterm-adapter.js
@@ -1,3 +1,9 @@
+const ANSI_MOVE_CURSOR_UP_ONE = '\x1b[1A';
+
+function ansi_move_cursor_right_n(n) {
+  return `\x1b[${n}C`;
+}
+
 export default class ExecCommandEditorXtermAdapter {
   constructor(terminal, setCommandCallback, command) {
     this.terminal = terminal;
@@ -19,7 +25,16 @@ export default class ExecCommandEditorXtermAdapter {
       this.keyListener.dispose();
     } else if (e.domEvent.key === 'Backspace') {
       if (this.command.length > 0) {
-        this.terminal.write('\b \b');
+        const cursorX = this.terminal.buffer.cursorX;
+
+        if (cursorX === 0) {
+          this.terminal.write(ANSI_MOVE_CURSOR_UP_ONE);
+          this.terminal.write(ansi_move_cursor_right_n(this.terminal.cols));
+          this.terminal.write(' ');
+        } else {
+          this.terminal.write('\b \b');
+        }
+
         this.command = this.command.slice(0, -1);
       }
     } else if (e.key.length > 0) {
