c4c86ce2a949e63fdd72a0f9d629850c533be72c
Jeff Escalante <jescalan@users.noreply.github.com>
2020-03-02 14:53:43 -0500

    fix subnav css overlap, inconsistent subnav data (#7230)

--- website/components/subnav/index.jsx
+++ website/components/subnav/index.jsx
@@ -1,4 +1,5 @@
 import Subnav from '@hashicorp/react-subnav'
+import subnavItems from '../../data/subnav'
 import { useRouter } from 'next/router'
 
 export default function NomadSubnav() {
@@ -15,52 +16,7 @@ export default function NomadSubnav() {
       ]}
       currentPath={router.pathname}
       menuItemsAlign="right"
-      menuItems={[
-        { text: 'Overview', url: '/', type: 'inbound' },
-        {
-          text: 'Use Cases',
-          submenu: [
-            {
-              text: 'Simple Container Orchestration',
-              url: '/use-cases/simple-container-orchestration'
-            },
-            {
-              text: 'Non-Containerized Application Orchestration',
-              url: '/use-cases/non-containerized-application-orchestration'
-            },
-            {
-              text: 'Automated Service Networking with Consul',
-              url: '/use-cases/automated-service-networking-with-consul'
-            }
-          ]
-        },
-        {
-          text: 'Enterprise',
-          url: 'https://www.hashicorp.com/products/nomad/',
-          type: 'outbound'
-        },
-        'divider',
-        {
-          text: 'Learn',
-          url: 'https://learn.hashicorp.com/nomad',
-          type: 'outbound'
-        },
-        {
-          text: 'Docs',
-          url: '/docs',
-          type: 'inbound'
-        },
-        {
-          text: 'API',
-          url: '/api-docs',
-          type: 'inbound'
-        },
-        {
-          text: 'Resources',
-          url: '/resources',
-          type: 'inbound'
-        }
-      ]}
+      menuItems={subnavItems}
     />
   )
 }
