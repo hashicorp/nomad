7515da6231695a323f931dd8d8be22b1b2d3d421
Mahmood Ali <mahmood@hashicorp.com>
2020-03-02 13:55:02 -0500

    use golang 1.14

--- scripts/update_golang_version.sh
+++ scripts/update_golang_version.sh
@@ -3,6 +3,7 @@
 golang_version="$1"
 
 current_version=$(grep -o -e 'golang:[.0-9]*' .circleci/config.yml | head -n1 | cut -d: -f2)
+echo "--> Replacing Go ${current_version} with Go ${golang_version} ..."
 
 # To support both GNU and BSD sed, the regex is looser than it needs to be.
 # Specifically, we use "* instead of "?, which relies on GNU extension without much loss of
@@ -21,8 +22,8 @@ sed -i'' -e "s|go[.0-9]*.windows-amd64.zip|go${golang_version}.windows-amd64.zip
 sed -i'' -e "s|go_version=\"*[^\"]*\"*$|go_version=\"${golang_version}\"|g" \
 	scripts/vagrant-linux-priv-go.sh scripts/release/mac-remote-build
 
-# check if there is any remaining references to old versions
-if git grep -I --fixed-strings "${current_version}" | grep -v -e CHANGELOG.md -e vendor/
+echo "--> Checking if there is any remaining references to old versions..."
+if git grep -I --fixed-strings "${current_version}" | grep -v -e CHANGELOG.md -e vendor/ -e website/
 then
 	echo "  ^^ files contain references to old golang version" >&2
 	echo "  update script and run again" >&2
