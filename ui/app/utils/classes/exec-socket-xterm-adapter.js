e138e7115912c36d6d14a9d28648021ae0bb3db2
Buck Doyle <buck@hashicorp.com>
2020-03-03 10:17:32 -0600

    Change var to let

--- ui/app/utils/classes/exec-socket-xterm-adapter.js
+++ ui/app/utils/classes/exec-socket-xterm-adapter.js
@@ -50,11 +50,11 @@ export default class ExecSocketXtermAdapter {
 }
 
 function encodeString(string) {
-  var encoded = new TextEncoderLite('utf-8').encode(string);
+  let encoded = new TextEncoderLite('utf-8').encode(string);
   return base64js.fromByteArray(encoded);
 }
 
 function decodeString(b64String) {
-  var uint8array = base64js.toByteArray(b64String);
+  let uint8array = base64js.toByteArray(b64String);
   return new TextDecoderLite('utf-8').decode(uint8array);
 }
