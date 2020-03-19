2fa79b5ccb87fd9dfab02f3d1bc39102d8286242
Lang Martin <lang@hashicorp.com>
2020-03-19 15:24:25 -0400

    client/pluginmanager/csimanager/interface: UsageOptions.MountOptions

--- client/pluginmanager/csimanager/interface.go
+++ client/pluginmanager/csimanager/interface.go
@@ -22,6 +22,7 @@ type UsageOptions struct {
 	ReadOnly       bool
 	AttachmentMode string
 	AccessMode     string
+	MountOptions   *structs.CSIOptions
 }
 
 // ToFS is used by a VolumeManager to construct the path to where a volume
