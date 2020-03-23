4656300b42a8d81017c2103d13f748e92dc66b91
Michael Lange <dingoeatingfuzz@gmail.com>
2020-03-23 08:18:17 -0700

    Set up watchers for volume detail

--- ui/app/routes/csi/volumes/volume.js
+++ ui/app/routes/csi/volumes/volume.js
@@ -1,9 +1,12 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import { collect } from '@ember/object/computed';
 import notifyError from 'nomad-ui/utils/notify-error';
 import { qpBuilder } from 'nomad-ui/utils/classes/query-params';
+import { watchRecord } from 'nomad-ui/utils/properties/watch';
+import WithWatchers from 'nomad-ui/mixins/with-watchers';
 
-export default Route.extend({
+export default Route.extend(WithWatchers, {
   store: service(),
   system: service(),
 
@@ -25,6 +28,14 @@ export default Route.extend({
     },
   ],
 
+  startWatchers(controller, model) {
+    if (!model) return;
+
+    controller.set('watchers', {
+      model: this.watch.perform(model),
+    });
+  },
+
   serialize(model) {
     return { volume_name: model.get('plainId') };
   },
@@ -35,4 +46,10 @@ export default Route.extend({
     const fullId = JSON.stringify([`csi/${name}`, namespace || 'default']);
     return this.store.findRecord('volume', fullId, { reload: true }).catch(notifyError(this));
   },
+
+  // Since volume includes embedded records for allocations,
+  // it's possible that allocations that are server-side deleted may
+  // not be removed from the UI while sitting on the volume detail page.
+  watch: watchRecord('volume'),
+  watchers: collect('watch'),
 });
