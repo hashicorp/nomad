253a83e2457590cbb11054f76f0935483f41b6ba
Tim Gross <tgross@hashicorp.com>
2020-04-02 16:05:26 -0400

    csi: internals docs (#7522)
    Summarizes the internal RFCs for plugins and volume lifecycles.

--- website/data/docs-navigation.js
+++ website/data/docs-navigation.js
@@ -32,7 +32,7 @@ export default [
     category: 'internals',
     content: [
       'architecture',
-      { category: 'plugins', content: ['base', 'task-drivers', 'devices'] },
+      { category: 'plugins', content: ['base', 'task-drivers', 'devices', 'csi'] },
       {
         category: 'scheduling',
         content: ['scheduling', 'preemption']
