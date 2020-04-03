1444e2d94125ff002f1bd2599ba6522dba709250
Buck Doyle <buck@hashicorp.com>
2020-04-03 16:53:34 -0500

    Change task state to be computed
    This is unfortunately not covered by automated tests because
    it seems too difficult to create the object hierarchy that
    represents a new set of allocations and task states that are
    created when a job is updated. This does however fix the problem
    where the wrong task has the active marker in the sidebar when
    the job is updated.

--- ui/app/controllers/exec.js
+++ ui/app/controllers/exec.js
@@ -1,7 +1,7 @@
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
-import { mapBy, sort, uniq } from '@ember/object/computed';
+import { alias, mapBy, sort, uniq } from '@ember/object/computed';
 import escapeTaskName from 'nomad-ui/utils/escape-task-name';
 import ExecCommandEditorXtermAdapter from 'nomad-ui/utils/classes/exec-command-editor-xterm-adapter';
 import ExecSocketXtermAdapter from 'nomad-ui/utils/classes/exec-socket-xterm-adapter';
@@ -44,14 +44,53 @@ export default Controller.extend({
     this.terminal.writeln('Select a task to start your session.');
   },
 
+  allocations: alias('model.allocations'),
+
+  taskState: computed(
+    'allocations.[]',
+    'allocationShortId',
+    'allocations.@each.isActive',
+    'taskName',
+    'taskGroupName',
+    'allocation',
+    'allocation.states.@each.name',
+    'allocation.states.@each.isRunning',
+    function() {
+      if (!this.allocations) {
+        return false;
+      }
+
+      let allocation;
+
+      if (this.allocationShortId) {
+        allocation = this.allocations.findBy('shortId', this.allocationShortId);
+      } else {
+        allocation = this.allocations.find(allocation =>
+          allocation.states
+            .filterBy('isActive')
+            .mapBy('name')
+            .includes(this.taskName)
+        );
+      }
+
+      if (allocation) {
+        return allocation.states.find(state => state.name === this.taskName);
+      }
+    }
+  ),
+
   actions: {
-    setTaskState({ allocationSpecified, taskState }) {
-      this.set('taskState', taskState);
+    setTaskProperties({ allocationShortId, taskName, taskGroupName }) {
+      this.setProperties({
+        allocationShortId,
+        taskName,
+        taskGroupName,
+      });
 
       this.terminal.write(ANSI_UI_GRAY_400);
       this.terminal.writeln('');
 
-      if (!allocationSpecified) {
+      if (!allocationShortId) {
         this.terminal.writeln(
           'Multiple instances of this task are running. The allocation below was selected by random draw.'
         );
@@ -61,8 +100,8 @@ export default Controller.extend({
       this.terminal.writeln('Customize your command, then hit ‘return’ to run.');
       this.terminal.writeln('');
       this.terminal.write(
-        `$ nomad alloc exec -i -t -task ${escapeTaskName(taskState.name)} ${
-          taskState.allocation.shortId
+        `$ nomad alloc exec -i -t -task ${escapeTaskName(taskName)} ${
+          this.taskState.allocation.shortId
         } `
       );
 
