eabfa3b3ed987746d335d4e0eb04688a6033b100
Michael Lange <dingoeatingfuzz@gmail.com>
2020-04-04 17:11:58 -0700

    Sort allocation tables by modify index

--- ui/app/controllers/csi/volumes/volume.js
+++ ui/app/controllers/csi/volumes/volume.js
@@ -1,6 +1,18 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+  system: service(),
+
+  sortedReadAllocations: computed('model.readAllocations.@each.modifyIndex', function() {
+    return this.model.readAllocations.sortBy('modifyIndex').reverse();
+  }),
+
+  sortedWriteAllocations: computed('model.writeAllocations.@each.modifyIndex', function() {
+    return this.model.writeAllocations.sortBy('modifyIndex').reverse();
+  }),
+
   actions: {
     gotoAllocation(allocation) {
       this.transitionToRoute('allocations.allocation', allocation);
