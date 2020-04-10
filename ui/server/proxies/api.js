1e4a3cf9a1db9c124f3b44aaf9e55f40e9073ee5
Michael Lange <dingoeatingfuzz@gmail.com>
2020-04-10 14:13:04 -0700

    Disable the proxy when Mirage is enabled
    This is to prevent max socket connection errors that can stop
    the live reload server from responding.

--- ui/server/proxies/api.js
+++ ui/server/proxies/api.js
@@ -7,6 +7,20 @@ module.exports = function(app, options) {
   // For options, see:
   // https://github.com/nodejitsu/node-http-proxy
 
+  // This is probably not safe to do, but it works for now.
+  let cacheKey = `${options.project.configPath()}|${options.environment}`;
+  let config = options.project.configCache.get(cacheKey);
+
+  // Disable the proxy completely when Mirage is enabled. No requests to the API
+  // will be being made, and having the proxy attempt to connect to Nomad when it
+  // is not running can result in socket max connections that block the livereload
+  // server from reloading.
+  if (config['ember-cli-mirage'].enabled !== false) {
+    options.ui.writeInfoLine('Mirage is enabled. Not starting proxy');
+    delete options.proxy;
+    return;
+  }
+
   let proxyAddress = options.proxy;
 
   let server = options.httpServer;
