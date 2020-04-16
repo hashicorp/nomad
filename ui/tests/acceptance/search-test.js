38fcf1b3e4d3371b6304dd9b85c51d1d1f442fa4
Buck Doyle <buck@hashicorp.com>
2020-04-16 16:54:33 -0500

    Remove unused import

--- ui/tests/acceptance/search-test.js
+++ ui/tests/acceptance/search-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { currentURL, triggerKeyEvent, visit } from '@ember/test-helpers';
+import { currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import PageLayout from 'nomad-ui/tests/pages/layout';
