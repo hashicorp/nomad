5b5143b900568bdf19b3ad259e75b906982feb14
Yoan Blanc <yoan@dosimple.ch>
2020-04-26 08:55:45 +0200

    docker: use constant from nvidia
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- drivers/docker/driver.go
+++ drivers/docker/driver.go
@@ -21,6 +21,7 @@ import (
 	multierror "github.com/hashicorp/go-multierror"
 	plugin "github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/nomad/client/taskenv"
+	"github.com/hashicorp/nomad/devices/gpu/nvidia"
 	"github.com/hashicorp/nomad/drivers/docker/docklog"
 	"github.com/hashicorp/nomad/drivers/shared/eventer"
 	nstructs "github.com/hashicorp/nomad/nomad/structs"
@@ -64,9 +65,6 @@ var (
 	// taskHandleVersion is the version of task handle which this driver sets
 	// and understands how to decode driver state
 	taskHandleVersion = 1
-
-	// Nvidia-container-runtime environment variable names
-	nvidiaVisibleDevices = "NVIDIA_VISIBLE_DEVICES"
 )
 
 const (
@@ -759,7 +757,7 @@ func (d *Driver) createContainerConfig(task *drivers.TaskConfig, driverConfig *T
 		PidsLimit: &driverConfig.PidsLimit,
 	}
 
-	if _, ok := task.DeviceEnv[nvidiaVisibleDevices]; ok {
+	if _, ok := task.DeviceEnv[nvidia.NvidiaVisibleDevices]; ok {
 		if !d.gpuRuntime {
 			return c, fmt.Errorf("requested docker-runtime %q was not found", d.config.GPURuntimeName)
 		}
