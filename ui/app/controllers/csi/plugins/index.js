f07a44d84458a73a7d4a06d96c7d532803295d68
Michael Lange <dingoeatingfuzz@gmail.com>
2020-05-01 23:39:58 -0700

    Set up routes, controllers, and template basics for the plugins page

---
+++ ui/app/controllers/csi/plugins/index.js
@@ -0,0 +1,40 @@
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { alias, readOnly } from '@ember/object/computed';
+import Controller, { inject as controller } from '@ember/controller';
+import SortableFactory from 'nomad-ui/mixins/sortable-factory';
+
+export default Controller.extend(SortableFactory([]), {
+  userSettings: service(),
+  pluginsController: controller('csi/plugins'),
+
+  isForbidden: alias('pluginsController.isForbidden'),
+
+  queryParams: {
+    currentPage: 'page',
+    sortProperty: 'sort',
+    sortDescending: 'desc',
+  },
+
+  currentPage: 1,
+  pageSize: readOnly('userSettings.pageSize'),
+
+  sortProperty: 'id',
+  sortDescending: false,
+
+  listToSort: alias('model'),
+  sortedPlugins: alias('listSorted'),
+
+  // TODO: Remove once this page gets search capability
+  resetPagination() {
+    if (this.currentPage != null) {
+      this.set('currentPage', 1);
+    }
+  },
+
+  actions: {
+    gotoPlugin(plugin) {
+      this.transitionToRoute('csi.plugins.plugin', plugin.id);
+    },
+  },
+});
