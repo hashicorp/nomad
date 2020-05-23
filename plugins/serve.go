195f15da84bb43bc21be85ffa29ad3705f914df5
Mahmood Ali <mahmood@hashicorp.com>
2020-05-23 07:48:44 -0400

    serve with context

--- plugins/serve.go
+++ plugins/serve.go
@@ -12,6 +12,7 @@ import (
 // PluginFactory returns a new plugin instance
 type PluginFactory func(log log.Logger) interface{}
 
+// PluginFactory returns a new plugin instance, that takes in a context
 type PluginCtxFactory func(ctx context.Context, log log.Logger) interface{}
 
 // Serve is used to serve a new Nomad plugin
@@ -22,6 +23,23 @@ func Serve(f PluginFactory) {
 	})
 
 	plugin := f(logger)
+	serve(plugin, logger)
+}
+
+// Serve is used to serve a new Nomad plugin
+func ServeCtx(f PluginCtxFactory) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	logger := log.New(&log.LoggerOptions{
+		Level:      log.Trace,
+		JSONFormat: true,
+	})
+
+	plugin := f(ctx, logger)
+	serve(plugin, logger)
+}
+func serve(plugin interface{}, logger log.Logger) {
 	switch p := plugin.(type) {
 	case device.DevicePlugin:
 		device.Serve(p, logger)
