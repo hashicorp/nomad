f1746a75cc8e8ab43a0fcaf9a8f5804478923ab9
changli0617 <39781496+changli0617@users.noreply.github.com>
2020-05-29 10:09:43 -0700

    Update alert-banner.js

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -3,8 +3,8 @@ export const ALERT_BANNER_ACTIVE = true
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
   url:
-    'https://www.hashicorp.com/webinars/nomad-virtual-day-cloud-bursting-with-nomad-and-consul-betterhelp-customer-story/',
-  tag: 'Livestream',
+    'https://www.nomadproject.io/downloads/',
+  tag: 'NEW',
   text:
-    'Nomad Virtual Day: cloud bursting use case, customer talk from BetterHelp and more. Register now.',
+    'Nomad 0.11 is now generally available, with CSI integration, autoscaling, and more. Download now',
 }
