11321b269b878d0d8d22fa2bd4fe2c1d124975c6
Buck Doyle <buck@hashicorp.com>
2020-06-02 09:21:15 -0500

    Fix self-cancel loop warnings
    This addresses these warnings which I believe were causing
    the tests to hang:
    
    ember-concurrency detected a potentially hazardous "self-cancel loop" between parent task `fetchStats` and child task `poll`. If you want child task `poll` to be canceled when parent task `fetchStats` is canceled, please change `.perform()` to `.linked().perform()`. If you want child task `poll` to keep running after parent task `fetchStats` is canceled, change it to `.unlinked().perform()

--- ui/app/components/allocation-row.js
+++ ui/app/components/allocation-row.js
@@ -63,7 +63,9 @@ export default Component.extend({
     do {
       if (this.stats) {
         try {
-          yield this.get('stats.poll').perform();
+          yield this.get('stats.poll')
+            .linked()
+            .perform();
           this.set('statsError', false);
         } catch (error) {
           this.set('statsError', true);
