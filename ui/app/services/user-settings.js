2a781333308cda60696057dc5797634dc602328a
Michael Lange <dingoeatingfuzz@gmail.com>
2020-06-30 17:09:52 -0700

    Persist the log stream/mode setting to localStorage

--- ui/app/services/user-settings.js
+++ ui/app/services/user-settings.js
@@ -3,4 +3,5 @@ import localStorageProperty from 'nomad-ui/utils/properties/local-storage';
 
 export default class UserSettingsService extends Service {
   @localStorageProperty('nomadPageSize', 25) pageSize;
+  @localStorageProperty('nomadLogMode', 'stdout') logMode;
 }
