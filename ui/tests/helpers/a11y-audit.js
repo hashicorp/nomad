c1fde7ff3b7b99d1ffc486a9a9345ecfc4c6b5aa
Buck Doyle <buck@hashicorp.com>
2020-07-21 13:38:22 -0500

    Add rule exclusions
    Some of these (color-contrast and heading-order) will be
    harder to fix than others, but I will hopefully be able
    to address the majority in subsequent commits.

--- ui/tests/helpers/a11y-audit.js
+++ ui/tests/helpers/a11y-audit.js
@@ -3,6 +3,30 @@ import a11yAudit from 'ember-a11y-testing/test-support/audit';
 export default async function defaultA11yAudit() {
   await a11yAudit({
     rules: {
+      'button-name': {
+        enabled: false
+      },
+      'color-contrast': {
+        enabled: false
+      },
+      'heading-order': {
+        enabled: false
+      },
+      'label': {
+        enabled: false
+      },
+      'landmark-unique': {
+        enabled: false
+      },
+      'link-name': {
+        enabled: false
+      },
+      'scrollable-region-focusable': {
+        enabled: false
+      },
+      'svg-img-alt': {
+        enabled: false
+      }
     }
   });
 }
