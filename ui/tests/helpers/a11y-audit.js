cdbab701fc768dda182a3ad3b75e8e8e1f9ed8a2
Buck Doyle <buck@hashicorp.com>
2020-07-23 11:25:24 -0500

    Fix button-name failures
    This rule is described here:
    https://dequeuniversity.com/rules/axe/3.5/button-name
    
    For the log streaming toggles, normally I would assert against
    conditionals like these, but I couldn’t find any assertions
    on the icons 😯

--- ui/tests/helpers/a11y-audit.js
+++ ui/tests/helpers/a11y-audit.js
@@ -3,9 +3,6 @@ import a11yAudit from 'ember-a11y-testing/test-support/audit';
 export default async function defaultA11yAudit() {
   await a11yAudit({
     rules: {
-      'button-name': {
-        enabled: false
-      },
       'color-contrast': {
         enabled: false
       },
