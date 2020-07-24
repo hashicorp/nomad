5f2cdd17c17d4f3aa31d2f653a10b7c937c50c2a
Michael Lange <dingoeatingfuzz@gmail.com>
2020-07-24 16:37:29 -0700

    Refactor job summary adapter to use the namespace ID base class

--- ui/app/adapters/job-summary.js
+++ ui/app/adapters/job-summary.js
@@ -1,12 +1,3 @@
-import Watchable from './watchable';
+import WatchableNamespaceIDs from './watchable-namespace-ids';
 
-export default class JobSummaryAdapter extends Watchable {
-  urlForFindRecord(id, type, hash) {
-    const [name, namespace] = JSON.parse(id);
-    let url = super.urlForFindRecord(name, 'job', hash) + '/summary';
-    if (namespace && namespace !== 'default') {
-      url += `?namespace=${namespace}`;
-    }
-    return url;
-  }
-}
+export default class JobSummaryAdapter extends WatchableNamespaceIDs {}
