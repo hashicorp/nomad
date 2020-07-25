5f2dee8225ef340ea6887eed8bce76478b70ab0d
Michael Lange <dingoeatingfuzz@gmail.com>
2020-07-24 21:36:43 -0700

    Model job scaling and events in Mirage

--- ui/mirage/models/job.js
+++ ui/mirage/models/job.js
@@ -3,4 +3,5 @@ import { Model, hasMany, belongsTo } from 'ember-cli-mirage';
 export default Model.extend({
   task_groups: hasMany('task-group'),
   job_summary: belongsTo('job-summary'),
+  job_scale: belongsTo('job-scale'),
 });
