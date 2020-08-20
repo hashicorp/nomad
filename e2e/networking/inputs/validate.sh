f90249281623c944a65e3def1bb61abf2b7be92a
Nick Ethier <nethier@hashicorp.com>
2020-08-20 11:47:59 -0400

    e2e: remove debug lines

--- e2e/networking/inputs/validate.sh
+++ e2e/networking/inputs/validate.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo ${NOMAD_ADDR}/v1/allocation/${NOMAD_ALLOC_ID} >> /tmp/out
-curl ${NOMAD_ADDR}/v1/allocation/${NOMAD_ALLOC_ID} >> /tmp/out2
 test $(curl --silent ${NOMAD_ADDR}/v1/allocation/${NOMAD_ALLOC_ID} | jq '.NetworkStatus.Address | length') -ne 0
