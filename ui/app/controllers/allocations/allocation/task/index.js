0d885bec09d0d6765fbf33eb72435798fa5617de
Buck Doyle <buck@hashicorp.com>
2020-08-26 09:46:10 -0500

    Remove prestart tasks table
    My suggestion is that this table isn’t sufficiently useful to
    keep around with the combinatoric explosion of other lifecycle
    phases. The logic was that someone might wonder “why isn’t my
    main task starting?” and this table would show that the prestart
    tasks hadn’t yet completed. One might wonder the same about
    any task that has prerequisites, so should a poststart task have
    a table that shows main tasks? And so on.
    
    Since the route hierarchy guarantees that one has already passed
    through a template that shows the lifecycle chart before one
    can reach the template where this table is displayed, I believe
    this table is redundant. It also conveys information in a more
    abstract way than the chart, which is dense and more easily
    understood, to me.

--- ui/app/controllers/allocations/allocation/task/index.js
+++ ui/app/controllers/allocations/allocation/task/index.js
@@ -1,22 +1,10 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
 import { computed as overridable } from 'ember-overridable-computed';
 import { task } from 'ember-concurrency';
 import classic from 'ember-classic-decorator';
 
 @classic
 export default class IndexController extends Controller {
-  @computed('model.task.taskGroup.tasks.@each.name')
-  get otherTaskStates() {
-    const taskName = this.model.task.name;
-    return this.model.allocation.states.rejectBy('name', taskName);
-  }
-
-  @computed('otherTaskStates.@each.lifecycle')
-  get prestartTaskStates() {
-    return this.otherTaskStates.filterBy('task.lifecycle');
-  }
-
   @overridable(() => {
     // { title, description }
     return null;
