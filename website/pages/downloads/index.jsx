759dab4608acb0cd48f345f698b41fa12fff0339
Michael Schurter <mschurter@hashicorp.com>
2020-09-02 21:18:56 -0700

    docs: add download link to 0.12.4-rc1

--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -12,6 +12,11 @@ export default function DownloadsPage({ releaseData }) {
         version={VERSION}
         releaseData={releaseData}
         community="/resources"
+        prerelease={{
+          type: 'Release Candidate',
+          name: 'v0.12.4',
+          version: '0.12.4-rc1'
+        }}
       />
     </div>
   )
