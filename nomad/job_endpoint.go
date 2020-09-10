hashicorp/nomad

commit 8976261f4af49e7e8d13a6b7705ba052928eb091

fix: panic in test introduced by #8453 (#8834)
Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

nomad/job_endpoint_oss.go
@@ -3,12 +3,16 @@
 package nomad
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
 	"github.com/hashicorp/nomad/nomad/structs"
 	vapi "github.com/hashicorp/vault/api"
 )
+
+// ErrMultipleNamespaces is send when multiple namespaces are used in the OSS setup
+var ErrMultipleNamespaces = errors.New("multiple vault namespaces requires Nomad Enterprise")
 
 // enforceSubmitJob is used to check any Sentinel policies for the submit-job scope
 func (j *Job) enforceSubmitJob(override bool, job *structs.Job) (error, error) {
@@ -51,7 +55,7 @@ func (j *Job) multiVaultNamespaceValidation(
 ) error {
 	requestedNamespaces := structs.VaultNamespaceSet(policies)
 	if len(requestedNamespaces) > 0 {
-		return fmt.Errorf("multiple vault namespaces requires Nomad Enterprise, Namespaces: %s", strings.Join(requestedNamespaces, ", "))
+		return fmt.Errorf("%w, Namespaces: %s", ErrMultipleNamespaces, strings.Join(requestedNamespaces, ", "))
 	}
 	return nil
 }

nomad/job_endpoint_test.go
@@ -1736,8 +1736,9 @@ func TestJobEndpoint_Register_Vault_MultiNamespaces(t *testing.T) {
 	var resp structs.JobRegisterResponse
 	err := msgpackrpc.CallWithCodec(codec, "Job.Register", req, &resp)
 	// OSS or Ent check
-	if s1.EnterpriseState.Features() == 0 {
-		require.Contains(t, err.Error(), "multiple vault namespaces requires Nomad Enterprise")
+	if err != nil && s1.EnterpriseState.Features() == 0 {
+		// errors.Is cannot be used because the RPC call break error wrapping.
+		require.Contains(t, err.Error(), ErrMultipleNamespaces.Error())
 	} else {
 		require.NoError(t, err)
 	}

commit c8e48105f18fa50456014c5bdccdc7424f34660f

move variable out of oss-only build into shared file, fixes ent compile error introduced by #8834

nomad/job_endpoint.go
@@ -34,6 +34,9 @@ const (
 	DispatchPayloadSizeLimit = 16 * 1024
 )
 
+// ErrMultipleNamespaces is send when multiple namespaces are used in the OSS setup
+var ErrMultipleNamespaces = errors.New("multiple vault namespaces requires Nomad Enterprise")
+
 var (
 	// allowRescheduleTransition is the transition that allows failed
 	// allocations to be force rescheduled. We create a one off

nomad/job_endpoint_oss.go
@@ -3,16 +3,12 @@
 package nomad
 
 import (
-	"errors"
 	"fmt"
 	"strings"
 
 	"github.com/hashicorp/nomad/nomad/structs"
 	vapi "github.com/hashicorp/vault/api"
 )
-
-// ErrMultipleNamespaces is send when multiple namespaces are used in the OSS setup
-var ErrMultipleNamespaces = errors.New("multiple vault namespaces requires Nomad Enterprise")
 
 // enforceSubmitJob is used to check any Sentinel policies for the submit-job scope
 func (j *Job) enforceSubmitJob(override bool, job *structs.Job) (error, error) {