2ec1eb4ec614b1ef6b13ddf3b8c2850fe0d34e90
Tim Gross <tgross@hashicorp.com>
2020-09-16 16:10:06 -0400

    e2e: refactor CLI utils out of rescheduling test (#8905)
    The CLI helpers in the rescheduling test were intended for shared use, but
    until some other tests were written we didn't want to waste time making them
    generic. This changeset refactors them and adds some new helpers associated
    with the node drain tests (under separate PR).

---
+++ e2e/e2eutil/wait.go
@@ -0,0 +1,25 @@
+package e2eutil
+
+import "time"
+
+// WaitConfig is an interval and wait time that can be passed to a waiter
+// function, but with a default value that comes from the OrDefault method
+// if the config is nil
+type WaitConfig struct {
+	Interval time.Duration
+	Retries  int64
+}
+
+// Return a default wait config of 10s
+func (wc *WaitConfig) OrDefault() (time.Duration, int64) {
+	if wc == nil {
+		return time.Millisecond * 100, 100
+	}
+	if wc.Interval == 0 {
+		wc.Interval = time.Millisecond * 100
+	}
+	if wc.Retries == 0 {
+		wc.Retries = 100
+	}
+	return wc.Interval, wc.Retries
+}
