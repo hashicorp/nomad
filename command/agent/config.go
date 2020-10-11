53b948fe4ad062f13364d10683d4229c07aa18a0
Yoan Blanc <yoan@dosimple.ch>
2020-10-11 08:16:19 +0200

    feat: test the legacy names
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- command/agent/config.go
+++ command/agent/config.go
@@ -311,7 +311,7 @@ type ClientTemplateConfig struct {
 	// are unsafe because they expose information from the client host.
 	FunctionDenylist []string `hcl:"function_denylist"`
 
-	// COMPAT(0.13) consul-template uses inclusive language from v0.25.0 - function_blacklist is kept for compatibility
+	// Deprecated: COMPAT(0.13) consul-template uses inclusive language from v0.25.0 - function_blacklist is kept for compatibility
 	FunctionBlacklist []string `hcl:"function_blacklist"`
 
 	// DisableSandbox allows templates to access arbitrary files on the
