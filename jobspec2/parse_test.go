afd0f738168904a76a887a9bc3c893e8c45fe624
Mahmood Ali <mahmood@hashicorp.com>
2020-10-20 13:52:24 -0400

    add some expression tests

--- jobspec2/parse_test.go
+++ jobspec2/parse_test.go
@@ -3,6 +3,7 @@ package jobspec2
 import (
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/nomad/jobspec"
@@ -35,4 +36,52 @@ func TestEquivalentToHCL1(t *testing.T) {
 			require.Equal(t, job1, job2)
 		})
 	}
+}
+
+func TestParse_Variables(t *testing.T) {
+	hcl := `
+job "example" {
+  datacenters = [for s in ["dc1", "dc2"] : upper(s)]
+  region      = vars.region_var
+}
+`
+
+	out, err := ParseWithArgs("input.hcl", strings.NewReader(hcl), map[string]string{"region_var": "aug"}, true)
+	require.NoError(t, err)
+
+	require.Equal(t, []string{"DC1", "DC2"}, out.Datacenters)
+	require.Equal(t, "aug", *out.Region)
+}
+
+func TestParse_VarsAndFunctions(t *testing.T) {
+	hcl := `
+job "example" {
+  datacenters = [for s in ["dc1", "dc2"] : upper(s)]
+  region      = vars.region_var
+}
+`
+
+	out, err := ParseWithArgs("input.hcl", strings.NewReader(hcl), map[string]string{"region_var": "aug"}, true)
+	require.NoError(t, err)
+
+	require.Equal(t, []string{"DC1", "DC2"}, out.Datacenters)
+	require.NotNil(t, out.Region)
+	require.Equal(t, "aug", *out.Region)
+}
+
+func TestParse_FileOperators(t *testing.T) {
+	hcl := `
+job "example" {
+  region      = file("parse_test.go")
+}
+`
+
+	out, err := ParseWithArgs("input.hcl", strings.NewReader(hcl), nil, true)
+	require.NoError(t, err)
+
+	expected, err := ioutil.ReadFile("parse_test.go")
+	require.NoError(t, err)
+
+	require.NotNil(t, out.Region)
+	require.Equal(t, string(expected), *out.Region)
 }
