cebcca19ad32e03a089ae8195e638eafa6857a59
Kevin Pruett <pruett.kevin@gmail.com>
2020-10-22 16:49:37 -0400

    Document new `expirationDate` prop for AlertBanner

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,9 +2,12 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  url:
-    'https://www.hashicorp.com/blog/announcing-general-availability-of-hashicorp-nomad-0-12',
-  tag: 'ANNOUNCING',
+  linkText: 'Register now',
+  url: 'https://www.hashicorp.com/events/webinars/introducing-nomad-1-0',
+  tag: 'ANNOUNCEMENT',
   text:
-    'Nomad 0.12 is now generally available, which includes 15+ new features and our breakthrough Multi-Cluster Deployment. Learn more!',
+    'Join us on Oct 27th for the Nomad 1.0 Product Announcement with Armon Dadgar',
+  // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
+  // if you'd like the component to stop showing at or after a certain date
+  expirationDate: '2020-10-27T09:00:00-07:00',
 }
