94f3d79ebe71ca65499b80ac3fbf84031d711740
Michael Lange <dingoeatingfuzz@gmail.com>
2020-10-26 12:26:42 -0700

    List all available icons in a story.
    This is like the same as using svg-jar, right?

---
+++ ui/stories/theme/icons.stories.js
@@ -0,0 +1,18 @@
+import hbs from 'htmlbars-inline-precompile';
+
+export default {
+  title: 'Theme|Icons',
+};
+
+export let Icons = () => ({
+  template: hbs`
+    <ul class="tile-list">
+      {{#each (all-icons) as |icon|}}
+        <li class="icon-tile">
+          {{x-icon icon}}
+          <code>{{icon}}</code>
+        </li>
+      {{/each}}
+    </ul>
+  `,
+});
