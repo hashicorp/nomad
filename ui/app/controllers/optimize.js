7e458e0b3433432ea00143b1e297ce7141374d70
Buck Doyle <buck@hashicorp.com>
2020-10-30 09:07:59 -0500

    Remove route check before fetching child model
    This was a defensive move because it felt unpleasant to be
    reaching into a child controller without being sure it was
    on the active route. But removing it doesn’t break any
    tests and I didn’t see any difference visually either so
    it seems okay!

--- ui/app/controllers/optimize.js
+++ ui/app/controllers/optimize.js
@@ -1,21 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import { inject as controller } from '@ember/controller';
 import { task, timeout } from 'ember-concurrency';
 
 export default class OptimizeController extends Controller {
-  @service router;
   @controller('optimize/summary') summaryController;
 
   get activeRecommendationSummary() {
-    const currentRoute = this.router.currentRoute;
-
-    if (currentRoute.name === 'optimize.summary') {
-      return this.summaryController.model;
-    } else {
-      return undefined;
-    }
+    return this.summaryController.model;
   }
 
   @(task(function*() {
