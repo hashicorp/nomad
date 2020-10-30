b6fff730f18889429c527ce327924f71f86f2ee2
Buck Doyle <buck@hashicorp.com>
2020-10-30 08:59:54 -0500

    Fix name of child controller
    This is a copypaste error that I frequently make 😐

--- ui/app/controllers/optimize/summary.js
+++ ui/app/controllers/optimize/summary.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 import { inject as controller } from '@ember/controller';
 
-export default class OptimizeController extends Controller {
+export default class OptimizeSummaryController extends Controller {
   @controller('optimize') optimizeController;
 
   queryParams = [
