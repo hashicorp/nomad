a664e4afc05a155ca948c670f2947b07075a1e21
Tim Gross <tgross@hashicorp.com>
2020-11-10 10:06:30 -0500

    version bump for 0.10.8

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ var (
 	GitDescribe string
 
 	// The main version number that is being run at the moment.
-	Version = "0.10.7"
+	Version = "0.10.8"
 
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
