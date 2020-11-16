3e6f737fa533a470c64dd39ce89064dde90e557c
Kris Hicks <khicks@hashicorp.com>
2020-11-16 06:42:59 -0800

    Add buf to tools

--- tools/tools.go
+++ tools/tools.go
@@ -6,6 +6,7 @@ package tools
 
 import (
 	_ "github.com/a8m/tree/cmd/tree"
+	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/elazarl/go-bindata-assetfs/go-bindata-assetfs"
 	_ "github.com/golang/protobuf/protoc-gen-go"
