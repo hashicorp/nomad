cac152b59bacc0b205b0e3ad0f695e55805ef5e2
Daniel Rossbach <daniel.rossbach@simplesurance.de>
2020-11-22 22:48:54 +0100

    HCL2: Add vault function

---
+++ jobspec2/vault/functions.go
@@ -0,0 +1,70 @@
+package vault
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	vaultapi "github.com/hashicorp/vault/api"
+	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/function"
+)
+
+// GenVaultFunc returns generated vault func
+var GenVaultFunc = function.New(&function.Spec{
+	Params: []function.Parameter{
+		{
+			Name: "path",
+			Type: cty.String,
+		},
+		{
+			Name: "key",
+			Type: cty.String,
+		},
+	},
+	Type: function.StaticReturnType(cty.String),
+	Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
+		path := args[0].AsString()
+		key := args[1].AsString()
+
+		return vault(path, key)
+	},
+})
+
+func vault(path string, key string) (cty.Value, error) {
+
+	if token := os.Getenv("VAULT_TOKEN"); token == "" {
+		return cty.StringVal(""), errors.New("Must set VAULT_TOKEN env var in order to use vault template function")
+	}
+
+	vaultConfig := vaultapi.DefaultConfig()
+	cli, err := vaultapi.NewClient(vaultConfig)
+	if err != nil {
+		return cty.StringVal(""), fmt.Errorf("Error getting Vault client: %s", err)
+	}
+	secret, err := cli.Logical().Read(path)
+	if err != nil {
+		return cty.StringVal(""), fmt.Errorf("Error reading vault secret: %s", err)
+	}
+	if secret == nil {
+		return cty.StringVal(""), errors.New("Vault Secret does not exist at the given path")
+	}
+
+	data, ok := secret.Data["data"]
+	if !ok {
+		// maybe ths is v1, not v2 kv store
+		value, ok := secret.Data[key]
+		if ok {
+			return cty.StringVal(value.(string)), nil
+		}
+
+		// neither v1 nor v2 proudced a valid value
+		return cty.StringVal(""), fmt.Errorf("Vault data was empty at the given path. Warnings: %s", strings.Join(secret.Warnings, "; "))
+	}
+
+	if val, ok := data.(map[string]interface{})[key]; ok {
+		return cty.StringVal(val.(string)), nil
+	}
+	return cty.StringVal(""), errors.New("Vault path does not contain the requested key")
+}
