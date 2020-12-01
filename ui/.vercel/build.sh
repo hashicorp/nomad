69e6d4d1a58aa8d31a6a6821e302ff596dca48f6
Buck Doyle <buck@hashicorp.com>
2020-12-01 09:09:33 -0600

    Remove landing page for UI header link

--- ui/.vercel/build.sh
+++ ui/.vercel/build.sh
@@ -1,8 +1,6 @@
-ember build
+STORYBOOK_LINK=true ember build
 mkdir -p ui-dist/ui
 mv dist/* ui-dist/ui/
 
 yarn build-storybook
 mv storybook-static ui-dist/storybook/
-
-cp .vercel/index.html ui-dist/index.html
