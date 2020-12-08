1d2520cecc6cb7d40d6632c899df342ca55ba471
Mike Wickett <mike@wickett.ca>
2020-12-07 20:11:50 -0500

    website: add k8s vs callout to homepage

--- website/components/features-list/index.jsx
+++ website/components/features-list/index.jsx
@@ -1,7 +1,11 @@
-export default function FeaturesList({ title, items }) {
+export default function FeaturesList({ title, items, intro }) {
   return (
     <div className="g-features-list g-grid-container">
       <h2 className="g-type-display-2">{title}</h2>
+      <div
+        className="intro-container"
+        dangerouslySetInnerHTML={{ __html: intro }}
+      />
       <div className="items-container">
         {items.map(({ title, content, icon }) => (
           <div key={title} className="item">
