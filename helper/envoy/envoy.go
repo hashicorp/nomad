f0f6f3a18f1f88fce33d92642583f175cf3a103a
Seth Hoenig <shoenig@hashicorp.com>
2020-12-11 09:57:13 -0600

    consul/connect: fix regression where client connect images ignored
    Nomad v1.0.0 introduced a regression where the client configurations
    for `connect.sidecar_image` and `connect.gateway_image` would be
    ignored despite being set. This PR restores that functionality.
    
    There was a missing layer of interpolation that needs to occur for
    these parameters. Since Nomad 1.0 now supports dynamic envoy versioning
    through the ${NOMAD_envoy_version} psuedo variable, we basically need
    to first interpolate
    
      ${connect.sidecar_image} => envoyproxy/envoy:v${NOMAD_envoy_version}
    
    then use Consul at runtime to resolve to a real image, e.g.
    
      envoyproxy/envoy:v${NOMAD_envoy_version} => envoyproxy/envoy:v1.16.0
    
    Of course, if the version of Consul is too old to provide an envoy
    version preference, we then need to know to fallback to the old
    version of envoy that we used before.
    
      envoyproxy/envoy:v${NOMAD_envoy_version} => envoyproxy/envoy:v1.11.2@sha256:a7769160c9c1a55bb8d07a3b71ce5d64f72b1f665f10d81aa1581bc3cf850d09
    
    Beyond that, we also need to continue to support jobs that set the
    sidecar task themselves, e.g.
    
      sidecar_task { config { image: "custom/envoy" } }
    
    which itself could include teh pseudo envoy version variable.

---
+++ helper/envoy/envoy.go
@@ -0,0 +1,49 @@
+// Package envoy provides a high level view of the variables that go into
+// selecting an envoy version.
+package envoy
+
+const (
+	// SidecarMetaParam is the parameter name used to configure the connect sidecar
+	// at the client level. Setting this option in client configuration takes the
+	// lowest precedence.
+	//
+	// If this meta option is not set in client configuration, it defaults to
+	// ImageFormat, so that Nomad will defer envoy version selection to Consul.
+	SidecarMetaParam = "connect.sidecar_image"
+
+	// SidecarConfigVar is used as the default config.image value for connect
+	// sidecar proxies, when they are injected in the job connect mutator.
+	SidecarConfigVar = "${meta." + SidecarMetaParam + "}"
+
+	// GatewayMetaParam is the parameter name used to configure the connect gateway
+	// at the client level. Setting this option in client configuration takes the
+	// lowest precedence.
+	//
+	// If this meta option is not set in client configuration, it defaults to
+	// ImageFormat, so that Nomad will defer envoy version selection to Consul.
+	GatewayMetaParam = "connect.gateway_image"
+
+	// GatewayConfigVar is used as the default config.image value for connect
+	// gateway proxies, when they are injected in the job connect mutator.
+	GatewayConfigVar = "${meta." + GatewayMetaParam + "}"
+
+	// envoyImageFormat is the default format string used for official envoy Docker
+	// images with the tag being the semver of the version of envoy. Nomad fakes
+	// interpolation of ${NOMAD_envoy_version} by replacing it with the version
+	// string for envoy that Consul reports as preferred.
+	//
+	// Folks wanting to build and use custom images while still having Nomad refer
+	// to specific versions as preferred by Consul would set meta.connect.sidecar_image
+	// to something like: "custom/envoy:${NOMAD_envoy_version}".
+	ImageFormat = "envoyproxy/envoy:v" + VersionVar
+
+	// envoyVersionVar will be replaced with the Envoy version string when
+	// used in the meta.connect.sidecar_image variable.
+	VersionVar = "${NOMAD_envoy_version}"
+
+	// FallbackImage is the image set in the node meta by default
+	// to be used by Consul Connect sidecar tasks. As of Nomad 1.0, this value
+	// is only used as a fallback when the version of Consul does not yet support
+	// dynamic envoy versions.
+	FallbackImage = "envoyproxy/envoy:v1.11.2@sha256:a7769160c9c1a55bb8d07a3b71ce5d64f72b1f665f10d81aa1581bc3cf850d09"
+)
