c6bbb9247844569a03c45c4ea0d6cb12d3c4c85f
Chris Baker <1675087+cgbaker@users.noreply.github.com>
2021-01-29 13:53:17 -0600

    Update website/data/alert-banner.js

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -6,7 +6,7 @@ export default {
   url: 'https://docs.google.com/forms/d/e/1FAIpQLSeTre9Uvsfohl3yKLdTf0jUACT2GQgvGBsbp4fZvARpFwdv-g/viewform',
   tag: 'FREE SWAG',
   text:
-    'Share your Nomad success story for a chance to win a special swag.',
+    'Share your Nomad success story for a chance to win special swag.',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
   expirationDate: '2021-02-08T09:00:00-07:00',
