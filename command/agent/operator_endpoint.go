f5b33da02db8dbafb435fbe5ddb3e28f76de186c
Buck Doyle <buck@hashicorp.com>
2021-02-05 14:48:12 -0600

    Add 501 error for license PUT

--- command/agent/operator_endpoint.go
+++ command/agent/operator_endpoint.go
@@ -294,6 +294,8 @@ func (s *HTTPServer) LicenseRequest(resp http.ResponseWriter, req *http.Request)
 	case "GET":
 		resp.WriteHeader(http.StatusNoContent)
 		return nil, nil
+	case "PUT":
+		return nil, CodedError(501, ErrEntOnly)
 	default:
 		return nil, CodedError(405, ErrInvalidMethod)
 	}
