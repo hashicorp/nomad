20111b09c4b4e7a54beb39236007e5c6688b13fd
Drew Bailey <drewbailey5@gmail.com>
2021-02-18 11:34:50 -0500

    update periodic to not use template
    nomad job inspect no longer returns an apiliststub so the required fields to query job summary are no longer there, parse cli output instead

--- e2e/periodic/periodic.go
+++ e2e/periodic/periodic.go
@@ -29,16 +29,16 @@ func (tc *PeriodicTest) BeforeAll(f *framework.F) {
 	e2eutil.WaitForLeader(f.T(), tc.Nomad())
 }
 
-// func (tc *PeriodicTest) AfterEach(f *framework.F) {
-// 	nomadClient := tc.Nomad()
-// 	j := nomadClient.Jobs()
+func (tc *PeriodicTest) AfterEach(f *framework.F) {
+	nomadClient := tc.Nomad()
+	j := nomadClient.Jobs()
 
-// 	for _, id := range tc.jobIDs {
-// 		j.Deregister(id, true, nil)
-// 	}
-// 	_, err := e2eutil.Command("nomad", "system", "gc")
-// 	f.NoError(err)
-// }
+	for _, id := range tc.jobIDs {
+		j.Deregister(id, true, nil)
+	}
+	_, err := e2eutil.Command("nomad", "system", "gc")
+	f.NoError(err)
+}
 
 func (tc *PeriodicTest) TestPeriodicDispatch_Basic(f *framework.F) {
 	t := f.T()
@@ -53,39 +53,26 @@ func (tc *PeriodicTest) TestPeriodicDispatch_Basic(f *framework.F) {
 	// force dispatch
 	require.NoError(t, e2eutil.PeriodicForce(jobID))
 
-	// Get the child job ID
 	testutil.WaitForResult(func() (bool, error) {
-		childID, err := e2eutil.JobInspectTemplate(jobID, `{{with index . 0}}{{printf "%s" .ID}}{{end}}`)
+		children, err := e2eutil.PreviouslyLaunced(jobID)
 		if err != nil {
 			return false, err
 		}
-		if childID != "" {
-			return true, nil
-		}
-		return false, fmt.Errorf("expected non-empty periodic child jobID for job %s", jobID)
-	}, func(err error) {
-		require.NoError(t, err)
-	})
 
-	testutil.WaitForResult(func() (bool, error) {
-		status, err := e2eutil.JobInspectTemplate(jobID, `{{with index . 1}}{{printf "%s" .Status}}{{end}}`)
-		require.NoError(t, err)
-		require.NotEmpty(t, status)
-		if status == "dead" {
-			return true, nil
+		for _, c := range children {
+			if c["Status"] == "dead" {
+				return true, nil
+			}
 		}
-		return false, fmt.Errorf("expected periodic job to be dead, got %s", status)
+		return false, fmt.Errorf("expected periodic job to be dead")
 	}, func(err error) {
 		require.NoError(t, err)
 	})
 
 	// Assert there are no pending children
-	pending, err := e2eutil.JobInspectTemplate(jobID, `{{with index . 0}}{{printf "%d" .JobSummary.Children.Pending}}{{end}}`)
+	summary, err := e2eutil.ChildrenJobSummary(jobID)
 	require.NoError(t, err)
-	require.Equal(t, "0", pending)
-
-	// Assert there are no pending children
-	dead, err := e2eutil.JobInspectTemplate(jobID, `{{with index . 0}}{{printf "%d" .JobSummary.Children.Dead}}{{end}}`)
-	require.NoError(t, err)
-	require.Equal(t, "1", dead)
+	require.Len(t, summary, 1)
+	require.Equal(t, summary[0]["Pending"], "0")
+	require.Equal(t, summary[0]["Dead"], "1")
 }
