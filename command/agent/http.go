126b9f2836d07ec857d17ba072a231295d8a9be5
Tim Gross <tgross@hashicorp.com>
2021-02-25 16:41:00 -0500

    HTTP API support for 'nomad ui -login'
    Endpoints for requesting and exchanging one-time tokens via the HTTP
    API. Includes documentation updates.

--- command/agent/http.go
+++ command/agent/http.go
@@ -272,6 +272,8 @@ func (s *HTTPServer) registerHandlers(enableDebug bool) {
 	s.mux.HandleFunc("/v1/acl/policies", s.wrap(s.ACLPoliciesRequest))
 	s.mux.HandleFunc("/v1/acl/policy/", s.wrap(s.ACLPolicySpecificRequest))
 
+	s.mux.HandleFunc("/v1/acl/token/onetime", s.wrap(s.UpsertOneTimeToken))
+	s.mux.HandleFunc("/v1/acl/token/onetime/exchange", s.wrap(s.ExchangeOneTimeToken))
 	s.mux.HandleFunc("/v1/acl/bootstrap", s.wrap(s.ACLTokenBootstrap))
 	s.mux.HandleFunc("/v1/acl/tokens", s.wrap(s.ACLTokensRequest))
 	s.mux.HandleFunc("/v1/acl/token", s.wrap(s.ACLTokenSpecificRequest))
