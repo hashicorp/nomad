0ac0349e9f9f92dd37f0cfa7327fb60782d970ba
Jeff Escalante <jescalan@users.noreply.github.com>
2021-03-30 14:52:44 -0400

    downloads page upgrade

--- website/data/version.js
+++ website/data/version.js
@@ -1,1 +1,47 @@
 export default '1.0.4'
+
+// HashiCorp officially supported package managers
+// NOTE: This information will be moving into the releases API
+export const packageManagers = [
+  {
+    label: 'Homebrew',
+    commands: ['brew tap hashicorp/tap', 'brew install hashicorp/tap/nomad'],
+    os: 'darwin',
+  },
+  {
+    label: 'Ubuntu/Debian',
+    commands: [
+      'curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -',
+      'sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"',
+      'sudo apt-get update && sudo apt-get install nomad',
+    ],
+    os: 'linux',
+  },
+  {
+    label: 'CentOS/RHEL',
+    commands: [
+      'sudo yum install -y yum-utils',
+      'sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo',
+      'sudo yum -y install nomad',
+    ],
+    os: 'linux',
+  },
+  {
+    label: 'Fedora',
+    commands: [
+      'sudo dnf install -y dnf-plugins-core',
+      'sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo',
+      'sudo dnf -y install nomad',
+    ],
+    os: 'linux',
+  },
+  {
+    label: 'Amazon Linux',
+    commands: [
+      'sudo yum install -y yum-utils',
+      'sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo',
+      'sudo yum -y install nomad',
+    ],
+    os: 'linux',
+  },
+]
--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -1,31 +1,75 @@
-import VERSION from 'data/version.js'
+import VERSION, { packageManagers } from 'data/version.js'
 import ProductDownloader from '@hashicorp/react-product-downloader'
 import Head from 'next/head'
 import HashiHead from '@hashicorp/react-head'
+import { productName, productSlug } from 'data/metadata'
+import s from './style.module.css'
 
-export default function DownloadsPage({ releaseData }) {
+export default function DownloadsPage({ releases }) {
   return (
-    <div id="p-downloads" className="g-container">
+    <div className={s.root}>
       <HashiHead is={Head} title="Downloads | Nomad by HashiCorp" />
       <ProductDownloader
-        product="Nomad"
-        version={VERSION}
-        releaseData={releaseData}
-        community="/resources"
+        releases={releases}
+        packageManagers={packageManagers}
+        productName={productName}
+        productId={productSlug}
+        latestVersion={VERSION}
+        getStartedDescription="Follow step-by-step tutorials on the essentials of Nomad."
+        getStartedLinks={[
+          {
+            label: 'Get Started With Nomad',
+            href: 'https://learn.hashicorp.com/collections/nomad/get-started',
+          },
+          {
+            label: 'Deploy and Manage Nomad Jobs',
+            href: 'https://learn.hashicorp.com/collections/nomad/manage-jobs',
+          },
+          {
+            label: 'Explore the Nomad Web UI',
+            href: 'https://learn.hashicorp.com/collections/nomad/web-ui',
+          },
+          {
+            label: 'View all Nomad tutorials',
+            href: 'https://learn.hashicorp.com/nomad',
+          },
+        ]}
+        logo={
+          <img
+            className={s.logo}
+            alt="Nomad"
+            src={require('./img/nomad-logo.svg')}
+          />
+        }
+        product="nomad"
+        tutorialLink={{
+          href: 'https://learn.hashicorp.com/nomad',
+          label: 'View Tutorials at HashiCorp Learn',
+        }}
       />
     </div>
   )
 }
 
 export async function getStaticProps() {
-  return fetch(`https://releases.hashicorp.com/nomad/${VERSION}/index.json`)
+  return fetch(`https://releases.hashicorp.com/nomad/index.json`, {
+    headers: {
+      'Cache-Control': 'no-cache',
+    },
+  })
     .then((res) => res.json())
-    .then((releaseData) => ({ props: { releaseData } }))
+    .then((result) => {
+      return {
+        props: {
+          releases: result,
+        },
+      }
+    })
     .catch(() => {
       throw new Error(
         `--------------------------------------------------------
         Unable to resolve version ${VERSION} on releases.hashicorp.com from link
-        <https://releases.hashicorp.com/nomad/${VERSION}/index.json>. Usually this
+        <https://releases.hashicorp.com/${productSlug}/${VERSION}/index.json>. Usually this
         means that the specified version has not yet been released. The downloads page
         version can only be updated after the new version has been released, to ensure
         that it works for all users.
