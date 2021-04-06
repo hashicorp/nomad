7d09eee89d89af3ee9e6d15bf75681a1cb710c53
Tim Gross <tgross@hashicorp.com>
2021-04-06 14:29:28 -0400

    docs: update CSI create/register fields
    Add new `access_mode`/`attachment_mode` fields. Make it more clear which set
    of fields belong to create vs register. Update the example spec that's
    generated by `volume init`.

--- command/volume_init.go
+++ command/volume_init.go
@@ -125,10 +125,11 @@ snapshot_id = "snap-12345"
 capacity_min = "10GiB"
 capacity_max = "20G"
 
-# Optional: for 'nomad volume create', specify one or more capabilities to
-# validate. Registering an existing volume will record but ignore these fields.
+# Required (at least one): for 'nomad volume create', specify one or more
+# capabilities to validate. Registering an existing volume will record but
+# ignore these fields.
 capability {
-  access_mode     = "single-node-writer"
+  access_mode = "single-node-writer"
   attachment_mode = "file-system"
 }
 
@@ -137,9 +138,9 @@ capability {
   attachment_mode = "block-device"
 }
 
-# Optional: for 'nomad volume create', specify mount options to
-# validate. Registering an existing volume will record but ignore these
-# fields.
+# Optional: for 'nomad volume create', specify mount options to validate for
+# 'attachment_mode = "file-system". Registering an existing volume will record
+# but ignore these fields.
 mount_options {
   fs_type     = "ext4"
   mount_flags = ["ro"]
