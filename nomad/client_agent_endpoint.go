edda9a8ead2d1d2ab45e3a6d6cfcb1370922c2f3
davemay99 <dmay@hashicorp.com>
2021-04-10 17:41:49 -0400

    Avoid error - import cycle not allowed

--- nomad/client_agent_endpoint.go
+++ nomad/client_agent_endpoint.go
@@ -456,8 +456,28 @@ func (a *Agent) Host(args *structs.HostDataRequest, reply *structs.HostDataRespo
 	return nil
 }
 
-// Self returns the agent's configuration and statistics
+// AgentSelfRequest is used by /agent/self to retrieve the agent's
+// configuration and statistics. If ServerID or NodeID is specified, the
+// request is forwarded to the remote agent
+type AgentSelfRequest struct {
+	ServerID string
+	NodeID   string
+	QueryOptions
+}
 
+// AgentSelfResponse contains the HostData content
+// Stats can be either server.Stats or client.Stats
+//   the other option is to formally declarea struct for each
+//   but that that may be more static than desired
+// We also could return both client and server when they are both running
+type AgentSelfResponse struct {
+	AgentID string
+	Config  *agent.Config
+	Member  serf.Member
+	Stats   map[string]map[string]string
+}
+
+// Self returns the agent's configuration and statistics
 func (a *Agent) Self(args *structs.AgentSelfRequest, reply *structs.AgentSelfResponse) error {
 
 	aclObj, err := a.srv.ResolveToken(args.AuthToken)
