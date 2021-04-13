00cf1a8fcb58e344728bb7996411033600d64440
Nick Ethier <nethier@hashicorp.com>
2021-04-12 23:59:49 -0400

    docker: add support for cpuset cgroup management

--- drivers/docker/driver_default.go
+++ drivers/docker/driver_default.go
@@ -5,6 +5,7 @@ package docker
 import (
 	"github.com/docker/docker/oci/caps"
 	docker "github.com/fsouza/go-dockerclient"
+	"github.com/opencontainers/runc/libcontainer/cgroups"
 )
 
 func getPortBinding(ip string, port string) docker.PortBinding {
@@ -28,4 +29,8 @@ func tweakCapabilities(basics, adds, drops []string) ([]string, error) {
 		effectiveCaps[i] = cap[len("CAP_"):]
 	}
 	return effectiveCaps, nil
+}
+
+func setCPUSetCgroup(path string, pid int) error {
+	return cgroups.WriteCgroupProc(path, pid)
 }
