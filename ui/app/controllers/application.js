83164f21d20681b4004076fce1907d144b4ee658
Buck Doyle <buck@hashicorp.com>
2021-04-13 11:56:59 -0500

    Add removal of OTT query parameter with delay (#10319)
    This followup to #10066 adds a step to clear the one-time token
    from the URL after the application has loaded. The delay is
    required for it to actually clear, but only when the OTT is present
    to avoid slowing down the entire test suite.

--- ui/app/controllers/application.js
+++ ui/app/controllers/application.js
@@ -19,9 +19,14 @@ export default class ApplicationController extends Controller {
     {
       region: 'region',
     },
+    {
+      oneTimeToken: 'ott',
+    },
   ];
 
   region = null;
+
+  oneTimeToken = '';
 
   error = null;
 
--- ui/app/routes/application.js
+++ ui/app/routes/application.js
@@ -1,5 +1,5 @@
 import { inject as service } from '@ember/service';
-import { next } from '@ember/runloop';
+import { later, next } from '@ember/runloop';
 import Route from '@ember/routing/route';
 import { AbortError } from '@ember-data/adapter/error';
 import RSVP from 'rsvp';
@@ -75,22 +75,30 @@ export default class ApplicationRoute extends Route {
     return promises;
   }
 
-  // Model is being used as a way to transfer the provided region
-  // query param to update the controller state.
-  model(params) {
-    return params.region;
+  // Model is being used as a way to propagate the region and
+  // one time token query parameters for use in setupController.
+  model({ region }, { to: { queryParams: { ott }}}) {
+    return {
+      region,
+      hasOneTimeToken: ott,
+    };
   }
 
-  setupController(controller, model) {
-    const queryParam = model;
-
-    if (queryParam === this.get('system.defaultRegion.region')) {
+  setupController(controller, { region, hasOneTimeToken }) {
+    if (region === this.get('system.defaultRegion.region')) {
       next(() => {
         controller.set('region', null);
       });
     }
 
-    return super.setupController(...arguments);
+    super.setupController(...arguments);
+
+    if (hasOneTimeToken) {
+      // Hack to force clear the OTT query parameter
+      later(() => {
+        controller.set('oneTimeToken', '');
+      }, 500);
+    }
   }
 
   @action
