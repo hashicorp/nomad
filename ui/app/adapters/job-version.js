d0ecfec15c37e56faa1ea7a684049adee7cc9989
Buck Doyle <buck@hashicorp.com>
2021-04-14 09:43:52 -0500

    Fix whitespace

--- ui/app/adapters/job-version.js
+++ ui/app/adapters/job-version.js
@@ -2,12 +2,10 @@ import ApplicationAdapter from './application';
 import addToPath from 'nomad-ui/utils/add-to-path';
 
 export default class JobVersionAdapter extends ApplicationAdapter {
-
   revertTo(jobVersion) {
     const jobAdapter = this.store.adapterFor('job');
 
     const url = addToPath(jobAdapter.urlForFindRecord(jobVersion.get('job.id'), 'job'), '/revert');
-
     const [jobName] = JSON.parse(jobVersion.get('job.id'));
 
     return this.ajax(url, 'POST', {
