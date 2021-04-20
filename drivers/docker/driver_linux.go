aba23b427c640f6222f744ed507568aa91c3a5a6
Nick Ethier <nethier@hashicorp.com>
2021-04-19 23:38:35 -0400

    driver/docker: ignore error if container exists before cgroup can be written

--- drivers/docker/driver_linux.go
+++ drivers/docker/driver_linux.go
@@ -1,9 +1,19 @@
 package docker
 
 import (
+	"strings"
+
 	"github.com/opencontainers/runc/libcontainer/cgroups"
 )
 
 func setCPUSetCgroup(path string, pid int) error {
-	return cgroups.WriteCgroupProc(path, pid)
+	// Sometimes the container exists before we can write the
+	// cgroup resulting in an error which can be ignored.
+	if err := cgroups.WriteCgroupProc(path, pid); err != nil {
+		if strings.Contains(err.Error(), "no such process") {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
