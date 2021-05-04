e01bb3fb80a13465f5982e72db372e86e263a2d0
Buck Doyle <buck@hashicorp.com>
2021-05-04 13:03:48 -0500

    Add soft limit annotation to allocation chart

--- ui/app/components/primary-metric/allocation.js
+++ ui/app/components/primary-metric/allocation.js
@@ -5,6 +5,7 @@ import { assert } from '@ember/debug';
 import { inject as service } from '@ember/service';
 import { action, get, computed } from '@ember/object';
 import { dependentKeyCompat } from '@ember/object/compat';
+import { formatScheduledBytes } from 'nomad-ui/utils/units';
 
 export default class AllocationPrimaryMetric extends Component {
   @service('stats-trackers-registry') statsTrackersRegistry;
@@ -62,6 +63,21 @@ export default class AllocationPrimaryMetric extends Component {
     if (this.metric === 'cpu') return 'blues';
     if (this.metric === 'memory') return 'reds';
     return 'ordinal';
+  }
+
+  get softLimitAnnotations() {
+    if (this.metric === 'memory' && this.allocation && (this.allocation.allocatedResources.memoryMax > this.allocation.allocatedResources.memory)) {
+      const memory = this.allocation.allocatedResources.memory;
+
+      return [
+        {
+          label: `${formatScheduledBytes(memory, 'MiB')} soft limit`,
+          percent: memory / this.allocation.allocatedResources.memoryMax,
+        },
+      ];
+    }
+
+    return [];
   }
 
   @task(function*() {
