34dfdb055d4c846a894cbed360e8ab0056b303c5
changli0617 <39781496+changli0617@users.noreply.github.com>
2021-05-13 15:26:49 -0700

    Update alert-banner.js

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,11 +2,11 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  linkText: 'Read the blog',
-  url: 'https://www.hashicorp.com/blog/announcing-hashicorp-nomad-1-1-beta',
-  tag: 'New Release',
+  linkText: 'Register Now',
+  url: 'https://www.hashicorp.com/events/webinars/nomad-1-1-oversubscription-reserved-cpu-cores-autoscaler-and-more',
+  tag: 'Live Webinar',
   text:
-    'Announcing HashiCorp Nomad 1.1 release candidate with 10+ new features for greater scheduling flexibility and simplified operator experience.',
+    'Join us on May 19th to watch HashiCorp Nomad 1.1 features in action',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
   expirationDate: '2021-05-20T09:00:00-07:00',
