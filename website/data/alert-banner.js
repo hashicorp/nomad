c2709e7c3cc147801788411158c3fe3ca38c1462
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2021-05-17 15:36:23 -0500

    Add HashiConf alert banner data (#10606)

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -1,13 +1,13 @@
 export const ALERT_BANNER_ACTIVE = true
 
-// https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
+// https://github.com/hashicorp/react-components/tree/main/packages/alert-banner
 export default {
-  linkText: 'Register Now',
-  url: 'https://www.hashicorp.com/events/webinars/nomad-1-1-oversubscription-reserved-cpu-cores-autoscaler-and-more',
-  tag: 'Live Webinar',
+  linkText: 'View Schedule',
+  url: 'https://hashiconf.com/europe/?utm_source=DocsBanner',
+  tag: 'June 8-11',
   text:
-    'Join us on May 19th to watch HashiCorp Nomad 1.1 features in action',
+    'The countdown to HashiConf Europe is on, and the full schedule is now live.',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-05-20T09:00:00-07:00',
+  expirationDate: null,
 }
