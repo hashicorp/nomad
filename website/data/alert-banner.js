b32da80e78c9113d838b1622276b214d950ca7b0
changli0617 <39781496+changli0617@users.noreply.github.com>
2021-05-18 10:10:27 -0700

    Update alert-banner.js

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,11 +2,11 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/react-components/tree/main/packages/alert-banner
 export default {
-  linkText: 'View Schedule',
-  url: 'https://hashiconf.com/europe/?utm_source=DocsBanner',
-  tag: 'June 8-11',
+  linkText: 'Read the Blog',
+  url: 'https://www.hashicorp.com/blog/announcing-general-availability-of-hashicorp-nomad-1-1',
+  tag: 'New Release',
   text:
-    'The countdown to HashiConf Europe is on, and the full schedule is now live.',
+    'HashiCorp Nomad 1.1 is now Generally Available.',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
   expirationDate: null,
