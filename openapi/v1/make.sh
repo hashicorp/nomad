6fbbcbf0252a64c20b901181412744176f915ed1
DerekStrickland <dstrickland@hashicorp.com>
2021-06-14 17:22:17 -0400

    html transform test

--- openapi/v1/make.sh
+++ openapi/v1/make.sh
@@ -8,10 +8,9 @@
 # Sync docs with spec
 # TBD
 
-docker run -it \
-  -v "${PWD}/website/content/api-docs:/local" \
-  sean0x42/markdown-extract \
-  -i -f "List Jobs" /local/jobs.mdx
+markdown=$(docker run -it -v "${PWD}/website/content/api-docs:/local" sean0x42/markdown-extract -i -f "List Jobs" /local/jobs.mdx)
+html=$(node ./openapi/v1/sync-doc.js "$markdown")
+echo "$html"
 
 # Generate test client from spec
 # docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli batch /local/config.yaml
--- openapi/v1/sync-doc.js
+++ openapi/v1/sync-doc.js
@@ -1,20 +1,58 @@
-var markedpp = require('markedpp'),
-    md = '!include(jobs.mdx)',
-    options = { dirname: '../../website/content/api-docs' };
+// const md = require('markdown-it')();
+const args = process.argv.splice(2);
+//
+const docMap = [{
+    source: "jobs.mdx",
+    target: "jobs-api.yaml",
+    header: "List Jobs",
+    path: "/jobs/get"
+}]
+//
+// console.log(md.render(args[0]));
 
-markedpp(md, function(err, result){
-    console.log(result);
-    /* Outputs
-    <!-- !numberedheadings -->
+const marked = require("marked");
+const fs = require("fs");
 
-    <!-- !toc (level=1) -->
+// __dirname means relative to script. Use "./data.txt" if you want it relative to execution path.
+// fs.readFile(__dirname + "/data.txt", (error, data) => {
+//     if(error) {
+//         throw error;
+//     }
+//     console.log(data.toString());
+// });
 
-    * [1\. hello](#1-hello)
+md = args[0]
+// console.log(markdown)
 
-    <!-- toc! -->
+const tokens = marked.lexer(md);
 
-    # 1\. hello
+const headerVisitor = {};
 
-    ## 1.1\. hello again
-    */
+let currentHeader = '';
+tokens.forEach((token, index, array) => {
+    // Will refactor this to use docMap
+    // aggregate until next
+    if (token.type === "heading" && token.depth === 2 && token.text !== currentHeader) {
+        currentHeader = token.text;
+        headerVisitor[currentHeader] =  [];
+    } else if (currentHeader.length > 0) {
+        headerVisitor[currentHeader] = headerVisitor[currentHeader].concat([token]);
+    }
 });
+
+docMap.forEach((item) =>{
+    let tokens = headerVisitor[item.header];
+
+    if (tokens) {
+        let html = marked.parser(tokens);
+        console.log(html);
+    }
+});
+
+
+// console.log(headerVisitor)
+
+
+
+// const html = marked(args[0]);
+// console.log(html)
