2c09a1e5d313eb36ea2a80e5b035f836f94e2799
DerekStrickland <dstrickland@hashicorp.com>
2021-06-15 10:04:46 -0400

    Api Doc syncing working

--- openapi/v1/sync-doc.js
+++ openapi/v1/sync-doc.js
@@ -1,58 +1,64 @@
-// const md = require('markdown-it')();
-const args = process.argv.splice(2);
-//
+const beautify = require('js-beautify');
+const fs = require("fs");
+const marked = require("marked");
+const yaml = require('js-yaml');
+
 const docMap = [{
     source: "jobs.mdx",
     target: "jobs-api.yaml",
     header: "List Jobs",
     path: "/jobs/get"
-}]
-//
-// console.log(md.render(args[0]));
-
-const marked = require("marked");
-const fs = require("fs");
-
-// __dirname means relative to script. Use "./data.txt" if you want it relative to execution path.
-// fs.readFile(__dirname + "/data.txt", (error, data) => {
-//     if(error) {
-//         throw error;
-//     }
-//     console.log(data.toString());
-// });
-
-md = args[0]
-// console.log(markdown)
-
-const tokens = marked.lexer(md);
-
-const headerVisitor = {};
-
-let currentHeader = '';
-tokens.forEach((token, index, array) => {
-    // Will refactor this to use docMap
-    // aggregate until next
-    if (token.type === "heading" && token.depth === 2 && token.text !== currentHeader) {
-        currentHeader = token.text;
-        headerVisitor[currentHeader] =  [];
-    } else if (currentHeader.length > 0) {
-        headerVisitor[currentHeader] = headerVisitor[currentHeader].concat([token]);
-    }
-});
+}];
 
 docMap.forEach((item) =>{
-    let tokens = headerVisitor[item.header];
+    let md = fs.readFileSync(`./website/content/api-docs/${item.source}`, (error, data) => {
+        if(error) {
+            throw error;
+        }
+    }).toString();
 
-    if (tokens) {
-        let html = marked.parser(tokens);
-        console.log(html);
+    const tokens = marked.lexer(md);
+    const headerVisitor = {};
+    let currentHeader = '';
+
+    tokens.forEach((token, index, array) => {
+        // Will refactor this to use docMap
+        // aggregate until next
+        if (token.type === "heading" && token.depth === 2 && token.text !== currentHeader) {
+            currentHeader = token.text;
+            headerVisitor[currentHeader] =  [];
+        } else if (currentHeader.length > 0) {
+            headerVisitor[currentHeader] = headerVisitor[currentHeader].concat([token]);
+        }
+    });
+
+    if (headerVisitor[item.header]) {
+        try {
+            let html = beautify.html(marked.parser(headerVisitor[item.header]));
+            if (html.length < 1) {
+                console.log(`error generating html for ${item.header}`);
+                return;
+            }
+
+            let targetFile = `./openapi/v1/${item.target}`;
+            let targetDoc = yaml.load(fs.readFileSync(targetFile, 'utf8'));
+
+            let lastIndex = item.path.lastIndexOf('/');
+            let path = item.path.substr(0, lastIndex);
+            let method = item.path.substr(lastIndex, item.path.length - 1).replace('/', '');
+            targetDoc.paths[path][method].description = '';
+
+            let yamlStr = yaml.dump(targetDoc, {
+                noRefs: true,
+                replacer(key, value) {
+                    if (key !== "description") return value;
+                    return html;
+                }
+            });
+
+            fs.writeFileSync(targetFile, yamlStr, 'utf8');
+        } catch (e) {
+            console.log(`error syncing ${item.path}: ${e}`);
+        }
     }
 });
-
-
-// console.log(headerVisitor)
-
-
-
-// const html = marked(args[0]);
-// console.log(html)
