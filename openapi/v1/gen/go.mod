3d4e3046a77bc984da90879626cec4b629f31177
DerekStrickland <dstrickland@hashicorp.com>
2021-06-22 12:31:07 -0400

    safety checkin

--- openapi/v1/gen/go.mod
+++ openapi/v1/gen/go.mod
@@ -6,5 +6,7 @@ replace github.com/hashicorp/nomad/api => ../../../api
 
 require (
 	github.com/hashicorp/nomad/api v0.0.0
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	golang.org/x/tools v0.1.3
 )
--- openapi/v1/gen/main.go
+++ openapi/v1/gen/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/hashicorp/nomad/api"
+	"github.com/modern-go/reflect2"
 	"go/ast"
 	"go/build"
 	"go/parser"
 	"go/token"
-
-	_ "github.com/hashicorp/nomad/api"
 	"golang.org/x/tools/go/ast/inspector"
 	"golang.org/x/tools/go/packages"
+	"log"
+	"os"
+	"reflect"
+	"strings"
 )
 
 func main() {
@@ -58,7 +62,20 @@ func buildSchema(n ast.Node) string {
 }
 
 func loadPackages() error {
-	cfg := &packages.Config{Mode: packages.NeedFiles | packages.NeedSyntax}
+	const loadMode = packages.NeedName |
+		packages.NeedFiles |
+		packages.NeedCompiledGoFiles |
+		packages.NeedImports |
+		packages.NeedDeps |
+		packages.NeedExportsFile |
+		packages.NeedTypes |
+		packages.NeedSyntax |
+		packages.NeedTypesInfo |
+		packages.NeedTypesSizes |
+		packages.NeedModule
+
+	cfg := &packages.Config{Mode: loadMode}
+
 	pkgs, err := packages.Load(cfg, "../../../api/")
 	if err != nil {
 		return err
@@ -67,9 +84,88 @@ func loadPackages() error {
 		return errors.New("error loading packages - see stdout for details")
 	}
 
+	var inputBuilder strings.Builder
+
 	for _, pkg := range pkgs {
-		fmt.Println(pkg.ID, pkg.GoFiles)
+		for _, goFile := range pkg.GoFiles {
+			// Create the AST by parsing src.
+			fset := token.NewFileSet() // positions are relative to fset
+			f, err := parser.ParseFile(fset, goFile, nil, 0)
+			if err != nil {
+				panic(err)
+			}
+
+			for _, node := range f.Decls {
+				switch node.(type) {
+
+				case *ast.GenDecl:
+					genDecl := node.(*ast.GenDecl)
+					for _, spec := range genDecl.Specs {
+						switch spec.(type) {
+						case *ast.TypeSpec:
+							typeSpec := spec.(*ast.TypeSpec)
+
+							switch typeSpec.Type.(type) {
+							case *ast.StructType:
+								if hasClient(typeSpec) {
+									continue
+								} else {
+									// fmt.Println(fmt.Sprintf("%+v", typeSpec))
+									inputBuilder.WriteString(fmt.Sprintf("\t&api.%s,\n", typeSpec.Name.Name))
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	f, err := os.OpenFile("input.go", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = f.WriteString(strings.Replace(inputTmpl, "{{REPLACE}}", inputBuilder.String(), 1))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
 	}
 
 	return nil
 }
+
+func hasClient(typeSpec *ast.TypeSpec) bool {
+	structType := typeSpec.Type.(*ast.StructType)
+	for _, field := range structType.Fields.List {
+		for _, name := range field.Names {
+			if name.Name == "client" {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func createInstance(name string) interface{} {
+	test := api.ACLPolicies{}
+	fmt.Println(fmt.Sprintf("force package include %+v", test))
+	t := reflect2.TypeByPackageName("github.com/hashicorp/nomad/api", name)
+	return reflect.New(t.Type1()).Elem().Interface()
+}
+
+var inputTmpl = `
+package main
+
+import (
+	"github.com/hashicorp/nomad/api"
+)
+
+inputs := [
+{{REPLACE}}
+]
+`
+
