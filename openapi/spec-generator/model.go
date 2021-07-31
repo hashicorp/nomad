8d73ef62e7a919dfe8770d34d096d093a477941b
DerekStrickland <dstrickland@hashicorp.com>
2021-07-31 07:36:26 -0400

    stub out the api surface by tags based functions

--- openapi/spec-generator/model.go
+++ openapi/spec-generator/model.go
@@ -250,6 +250,32 @@ type Path struct {
 type V1API struct{}
 
 func (v *V1API) GetPaths() []*Path {
+
+	paths := v.getJobPaths()
+	paths = append(paths, v.getACLPaths()...)
+	paths = append(paths, v.getAgentPaths()...)
+	paths = append(paths, v.getAllocationPaths()...)
+	paths = append(paths, v.getClientPaths()...)
+	paths = append(paths, v.getDeploymentsPaths()...)
+	paths = append(paths, v.getEnterprisePaths()...)
+	paths = append(paths, v.getEvaluationsPaths()...)
+	paths = append(paths, v.getMiscPaths()...)
+	paths = append(paths, v.getNamespacePaths()...)
+	paths = append(paths, v.getNodePaths()...)
+	paths = append(paths, v.getOperatorPaths()...)
+	paths = append(paths, v.getPPROFPaths()...)
+	paths = append(paths, v.getPluginsPaths()...)
+	paths = append(paths, v.getVolumesPaths()...)
+
+	return paths
+}
+
+func (v *V1API) getJobPaths() []*Path {
+	//s.mux.HandleFunc("/v1/jobs", s.wrap(s.JobsRequest))
+	//s.mux.HandleFunc("/v1/jobs/parse", s.wrap(s.JobsParseRequest))
+	//s.mux.HandleFunc("/v1/job/", s.wrap(s.JobSpecificRequest))
+	//s.mux.HandleFunc("/v1/validate/job", s.wrap(s.ValidateJobRequest))
+
 	return []*Path{
 		{
 			Key: "/jobs/{jobName}",
@@ -282,6 +308,156 @@ func (v *V1API) GetPaths() []*Path {
 			},
 		},
 	}
+}
+
+func (v *V1API) getAllocationPaths() []*Path {
+	//s.mux.HandleFunc("/v1/allocations", s.wrap(s.AllocsRequest))
+	//s.mux.HandleFunc("/v1/allocation/", s.wrap(s.AllocSpecificRequest))
+
+	return nil
+}
+
+func (v *V1API) getNodePaths() []*Path {
+	//s.mux.HandleFunc("/v1/nodes", s.wrap(s.NodesRequest))
+	//s.mux.HandleFunc("/v1/node/", s.wrap(s.NodeSpecificRequest))
+
+	return nil
+}
+
+func (v *V1API) getEvaluationsPaths() []*Path {
+	//s.mux.HandleFunc("/v1/evaluations", s.wrap(s.EvalsRequest))
+	//s.mux.HandleFunc("/v1/evaluation/", s.wrap(s.EvalSpecificRequest))
+
+	return nil
+}
+
+func (v *V1API) getDeploymentsPaths() []*Path {
+	//s.mux.HandleFunc("/v1/deployments", s.wrap(s.DeploymentsRequest))
+	//s.mux.HandleFunc("/v1/deployment/", s.wrap(s.DeploymentSpecificRequest))
+
+	return nil
+}
+
+func (v *V1API) getVolumesPaths() []*Path {
+	//s.mux.HandleFunc("/v1/volumes", s.wrap(s.CSIVolumesRequest))
+	//s.mux.HandleFunc("/v1/volumes/external", s.wrap(s.CSIExternalVolumesRequest))
+	//s.mux.HandleFunc("/v1/volumes/snapshot", s.wrap(s.CSISnapshotsRequest))
+	//s.mux.HandleFunc("/v1/volume/csi/", s.wrap(s.CSIVolumeSpecificRequest))
+
+	return nil
+}
+
+func (v *V1API) getPluginsPaths() []*Path {
+	//s.mux.HandleFunc("/v1/plugins", s.wrap(s.CSIPluginsRequest))
+	//s.mux.HandleFunc("/v1/plugin/csi/", s.wrap(s.CSIPluginSpecificRequest))
+
+	return nil
+}
+
+func (v *V1API) getACLPaths() []*Path {
+	//s.mux.HandleFunc("/v1/acl/policies", s.wrap(s.ACLPoliciesRequest))
+	//s.mux.HandleFunc("/v1/acl/policy/", s.wrap(s.ACLPolicySpecificRequest))
+	//s.mux.HandleFunc("/v1/acl/token/onetime", s.wrap(s.UpsertOneTimeToken))
+	//s.mux.HandleFunc("/v1/acl/token/onetime/exchange", s.wrap(s.ExchangeOneTimeToken))
+	//s.mux.HandleFunc("/v1/acl/bootstrap", s.wrap(s.ACLTokenBootstrap))
+	//s.mux.HandleFunc("/v1/acl/tokens", s.wrap(s.ACLTokensRequest))
+	//s.mux.HandleFunc("/v1/acl/token", s.wrap(s.ACLTokenSpecificRequest))
+	//s.mux.HandleFunc("/v1/acl/token/", s.wrap(s.ACLTokenSpecificRequest))
+
+	return nil
+}
+
+func (v *V1API) getClientPaths() []*Path {
+	//s.mux.Handle("/v1/client/fs/", wrapCORS(s.wrap(s.FsRequest)))
+	//s.mux.HandleFunc("/v1/client/gc", s.wrap(s.ClientGCRequest))
+	//s.mux.Handle("/v1/client/stats", wrapCORS(s.wrap(s.ClientStatsRequest)))
+	//s.mux.Handle("/v1/client/allocation/", wrapCORS(s.wrap(s.ClientAllocRequest)))
+
+	return nil
+}
+
+func (v *V1API) getAgentPaths() []*Path {
+	//s.mux.HandleFunc("/v1/agent/self", s.wrap(s.AgentSelfRequest))
+	//s.mux.HandleFunc("/v1/agent/join", s.wrap(s.AgentJoinRequest))
+	//s.mux.HandleFunc("/v1/agent/members", s.wrap(s.AgentMembersRequest))
+	//s.mux.HandleFunc("/v1/agent/force-leave", s.wrap(s.AgentForceLeaveRequest))
+	//s.mux.HandleFunc("/v1/agent/servers", s.wrap(s.AgentServersRequest))
+	//s.mux.HandleFunc("/v1/agent/keyring/", s.wrap(s.KeyringOperationRequest))
+	//s.mux.HandleFunc("/v1/agent/health", s.wrap(s.HealthRequest))
+	//s.mux.HandleFunc("/v1/agent/host", s.wrap(s.AgentHostRequest))
+	//s.mux.HandleFunc("/v1/agent/monitor", s.wrap(s.AgentMonitor))
+	//s.mux.HandleFunc("/v1/agent/pprof/", s.wrapNonJSON(s.AgentPprofRequest))
+
+	return nil
+}
+
+func (v *V1API) getMiscPaths() []*Path {
+
+	//s.mux.HandleFunc("/v1/metrics", s.wrap(s.MetricsRequest))
+	//s.mux.HandleFunc("/v1/regions", s.wrap(s.RegionListRequest))
+	//
+	//s.mux.HandleFunc("/v1/scaling/policies", s.wrap(s.ScalingPoliciesRequest))
+	//s.mux.HandleFunc("/v1/scaling/policy/", s.wrap(s.ScalingPolicySpecificRequest))
+	//
+	//s.mux.HandleFunc("/v1/status/leader", s.wrap(s.StatusLeaderRequest))
+	//s.mux.HandleFunc("/v1/status/peers", s.wrap(s.StatusPeersRequest))
+	//
+	//s.mux.HandleFunc("/v1/search/fuzzy", s.wrap(s.FuzzySearchRequest))
+	//s.mux.HandleFunc("/v1/search", s.wrap(s.SearchRequest))
+
+	//s.mux.HandleFunc("/v1/system/gc", s.wrap(s.GarbageCollectRequest))
+	//s.mux.HandleFunc("/v1/system/reconcile/summaries", s.wrap(s.ReconcileJobSummaries))
+
+	//s.mux.HandleFunc("/v1/event/stream", s.wrap(s.EventStream))
+
+	return nil
+}
+
+func (v *V1API) getOperatorPaths() []*Path {
+	//s.mux.HandleFunc("/v1/operator/license", s.wrap(s.LicenseRequest))
+	//s.mux.HandleFunc("/v1/operator/raft/", s.wrap(s.OperatorRequest))
+	//s.mux.HandleFunc("/v1/operator/autopilot/configuration", s.wrap(s.OperatorAutopilotConfiguration))
+	//s.mux.HandleFunc("/v1/operator/autopilot/health", s.wrap(s.OperatorServerHealth))
+	//s.mux.HandleFunc("/v1/operator/snapshot", s.wrap(s.SnapshotRequest))
+	//s.mux.HandleFunc("/v1/operator/scheduler/configuration", s.wrap(s.OperatorSchedulerConfiguration))
+
+	return nil
+}
+
+func (v *V1API) getNamespacePaths() []*Path {
+
+	//s.mux.HandleFunc("/v1/namespaces", s.wrap(s.NamespacesRequest))
+	//s.mux.HandleFunc("/v1/namespace", s.wrap(s.NamespaceCreateRequest))
+	//s.mux.HandleFunc("/v1/namespace/", s.wrap(s.NamespaceSpecificRequest))
+
+	return nil
+}
+
+func (v *V1API) getPPROFPaths() []*Path {
+	//s.mux.HandleFunc("/debug/pprof/", pprof.Index)
+	//s.mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	//s.mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	//s.mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	//s.mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+	return nil
+}
+
+func (v *V1API) getEnterprisePaths() []*Path {
+	//s.mux.HandleFunc("/v1/sentinel/policies", s.wrap(s.entOnly))
+	//s.mux.HandleFunc("/v1/sentinel/policy/", s.wrap(s.entOnly))
+	//
+	//s.mux.HandleFunc("/v1/quotas", s.wrap(s.entOnly))
+	//s.mux.HandleFunc("/v1/quota-usages", s.wrap(s.entOnly))
+	//s.mux.HandleFunc("/v1/quota/", s.wrap(s.entOnly))
+	//s.mux.HandleFunc("/v1/quota", s.wrap(s.entOnly))
+	//
+	//s.mux.HandleFunc("/v1/recommendation", s.wrap(s.entOnly))
+	//s.mux.HandleFunc("/v1/recommendations", s.wrap(s.entOnly))
+	//s.mux.HandleFunc("/v1/recommendations/apply", s.wrap(s.entOnly))
+	//s.mux.HandleFunc("/v1/recommendation/", s.wrap(s.entOnly))
+
+	return nil
 }
 
 func NewOperation(method string, tags []string, operationId string, requestBody *RequestBody, params []*Parameter, responses ...*ResponseConfig) *Operation {
