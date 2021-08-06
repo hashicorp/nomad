067747155f4de86554af781ca3e51c94a36e4b6e
DerekStrickland <dstrickland@hashicorp.com>
2021-08-06 08:42:06 -0400

    Add OpenAPI config for regions endpoint

--- openapi/spec-generator/regions.go
+++ openapi/spec-generator/regions.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"net/http"
+)
+
 func (v *v1api) getRegionsPaths() []*Path {
-	// tags := []string{"Regions"}
+	tags := []string{"Regions"}
 
 	return []*Path{
 		//s.mux.HandleFunc("/v1/regions", s.wrap(s.RegionListRequest))
+		{
+			Template: "/regions",
+			Operations: []*Operation{
+				newOperation(http.MethodGet, "regionListRequest", tags, "GetRegions",
+					nil,
+					queryOptions,
+					newResponseConfig(200, arraySchema, "string", queryMeta, "GetJobsResponse"),
+				),
+			},
+		},
 	}
 }
