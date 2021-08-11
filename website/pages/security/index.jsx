85fcad4db2370843934e7abf2bd7d7e1e5f61e28
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-11 15:57:14 -0400

    website: fix unsupported prop

--- website/pages/security/index.jsx
+++ website/pages/security/index.jsx
@@ -2,7 +2,7 @@ import MarkdownPage from '@hashicorp/react-markdown-page'
 import generateStaticProps from '@hashicorp/react-markdown-page/server'
 
 export default function SecurityPage(staticProps) {
-  return <MarkdownPage {...staticProps} product="nomad" />
+  return <MarkdownPage {...staticProps} />
 }
 
 export const getStaticProps = generateStaticProps({
