2904a0710a0962ed66efdc505856c4f222e5e3ff
Luiz Aoqui <luiz@hashicorp.com>
2021-09-22 11:17:47 -0400

    Disable PowerShell profile and simplify fingerprinting link speed on Windows (#11183)

--- client/fingerprint/network_windows.go
+++ client/fingerprint/network_windows.go
@@ -9,44 +9,22 @@ import (
 
 // linkSpeed returns link speed in Mb/s, or 0 when unable to determine it.
 func (f *NetworkFingerprint) linkSpeed(device string) int {
-	command := fmt.Sprintf("Get-NetAdapter -IncludeHidden | Where name -eq '%s' | Select -ExpandProperty LinkSpeed", device)
+	command := fmt.Sprintf("Get-NetAdapter -Name '%s' -ErrorAction Ignore | Select-Object -ExpandProperty 'Speed'", device)
 	path := "powershell.exe"
-	outBytes, err := exec.Command(path, command).Output()
+	powershellParams := "-NoProfile"
 
+	outBytes, err := exec.Command(path, powershellParams, command).Output()
 	if err != nil {
 		f.logger.Warn("failed to detect link speed", "device", device, "path", path, "command", command, "error", err)
 		return 0
 	}
-
 	output := strings.TrimSpace(string(outBytes))
 
-	return f.parseLinkSpeed(device, output)
-}
-
-func (f *NetworkFingerprint) parseLinkSpeed(device, commandOutput string) int {
-	args := strings.Split(commandOutput, " ")
-	if len(args) != 2 {
-		f.logger.Warn("couldn't split LinkSpeed output", "device", device, "output", commandOutput)
-		return 0
-	}
-
-	unit := strings.Replace(args[1], "\r\n", "", -1)
-	value, err := strconv.Atoi(args[0])
+	value, err := strconv.Atoi(output)
 	if err != nil {
-		f.logger.Warn("unable to parse LinkSpeed value", "device", device, "value", commandOutput, "error", err)
+		f.logger.Warn("unable to parse Speed value", "device", device, "value", output, "error", err)
 		return 0
 	}
 
-	switch unit {
-	case "Mbps":
-		return value
-	case "Kbps":
-		return value / 1000
-	case "Gbps":
-		return value * 1000
-	case "bps":
-		return value / 1000000
-	}
-
-	return 0
+	return value / 1000000
 }
