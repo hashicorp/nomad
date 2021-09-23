358a8a91adb5df2d0b2e0d22a59cdaf0b430761c
Luiz Aoqui <luiz@hashicorp.com>
2021-09-23 18:29:51 -0400

    ui: add system and service jobs to sysbatchSmall mirage scenario

--- ui/mirage/scenarios/sysbatch.js
+++ ui/mirage/scenarios/sysbatch.js
@@ -11,55 +11,65 @@ export function sysbatchSmall(server) {
     status: 'ready',
   });
 
-  // Job with 1 task group.
-  const job1 = server.create('job', {
+  // Generate non-system/sysbatch job as counter-example.
+  server.create('job', {
     status: 'running',
-    datacenters: ['dc1', 'dc2'],
-    type: 'sysbatch',
+    type: 'service',
     resourceSpec: ['M: 256, C: 500'],
-    createAllocations: false,
-  });
-  clients.forEach(c => {
-    server.create('allocation', { jobId: job1.id, nodeId: c.id });
+    createAllocations: true,
   });
 
-  // Job with 2 task groups.
-  const job2 = server.create('job', {
-    status: 'running',
-    datacenters: ['dc1'],
-    type: 'sysbatch',
-    resourceSpec: ['M: 256, C: 500', 'M: 256, C: 500'],
-    createAllocations: false,
-  });
-  clients.forEach(c => {
-    server.create('allocation', { jobId: job2.id, nodeId: c.id });
-    server.create('allocation', { jobId: job2.id, nodeId: c.id });
-  });
+  ['system', 'sysbatch'].forEach(type => {
+    // Job with 1 task group.
+    const job1 = server.create('job', {
+      status: 'running',
+      datacenters: ['dc1', 'dc2'],
+      type,
+      resourceSpec: ['M: 256, C: 500'],
+      createAllocations: false,
+    });
+    clients.forEach(c => {
+      server.create('allocation', { jobId: job1.id, nodeId: c.id });
+    });
 
-  // Job with 3 task groups.
-  const job3 = server.create('job', {
-    status: 'running',
-    datacenters: ['dc1'],
-    type: 'sysbatch',
-    resourceSpec: ['M: 256, C: 500', 'M: 256, C: 500', 'M: 256, C: 500'],
-    createAllocations: false,
-  });
-  clients.forEach(c => {
-    server.create('allocation', { jobId: job3.id, nodeId: c.id });
-    server.create('allocation', { jobId: job3.id, nodeId: c.id });
-    server.create('allocation', { jobId: job3.id, nodeId: c.id });
-  });
+    // Job with 2 task groups.
+    const job2 = server.create('job', {
+      status: 'running',
+      datacenters: ['dc1'],
+      type,
+      resourceSpec: ['M: 256, C: 500', 'M: 256, C: 500'],
+      createAllocations: false,
+    });
+    clients.forEach(c => {
+      server.create('allocation', { jobId: job2.id, nodeId: c.id });
+      server.create('allocation', { jobId: job2.id, nodeId: c.id });
+    });
 
-  // Job with client not scheduled.
-  const jobNotScheduled = server.create('job', {
-    status: 'running',
-    datacenters: ['dc1'],
-    type: 'sysbatch',
-    resourceSpec: ['M: 256, C: 500'],
-    createAllocations: false,
-  });
-  clients.forEach((c, i) => {
-    if (i > clients.length - 3) return;
-    server.create('allocation', { jobId: jobNotScheduled.id, nodeId: c.id });
+    // Job with 3 task groups.
+    const job3 = server.create('job', {
+      status: 'running',
+      datacenters: ['dc1'],
+      type,
+      resourceSpec: ['M: 256, C: 500', 'M: 256, C: 500', 'M: 256, C: 500'],
+      createAllocations: false,
+    });
+    clients.forEach(c => {
+      server.create('allocation', { jobId: job3.id, nodeId: c.id });
+      server.create('allocation', { jobId: job3.id, nodeId: c.id });
+      server.create('allocation', { jobId: job3.id, nodeId: c.id });
+    });
+
+    // Job with client not scheduled.
+    const jobNotScheduled = server.create('job', {
+      status: 'running',
+      datacenters: ['dc1'],
+      type,
+      resourceSpec: ['M: 256, C: 500'],
+      createAllocations: false,
+    });
+    clients.forEach((c, i) => {
+      if (i > clients.length - 3) return;
+      server.create('allocation', { jobId: jobNotScheduled.id, nodeId: c.id });
+    });
   });
 }
