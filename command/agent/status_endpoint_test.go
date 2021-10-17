e61c8c2d05f1223529da1bfe328fff294e18ea51
Drew Gonzales <drewgonzales360@gmail.com>
2021-10-16 21:49:49 -0700

    add test for version endpoint

--- command/agent/status_endpoint_test.go
+++ command/agent/status_endpoint_test.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/hashicorp/nomad/version"
 )
 
 func TestHTTP_StatusLeader(t *testing.T) {
@@ -49,6 +51,31 @@ func TestHTTP_StatusPeers(t *testing.T) {
 		peers := obj.([]string)
 		if len(peers) == 0 {
 			t.Fatalf("bad: %#v", peers)
+		}
+	})
+}
+
+func TestHTTP_StatusVersion(t *testing.T) {
+	t.Parallel()
+	httpTest(t, nil, func(s *TestAgent) {
+		// Make the HTTP request
+		req, err := http.NewRequest("GET", "/v1/status/version", nil)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+		respW := httptest.NewRecorder()
+
+		// Make the request
+		obj, err := s.Server.StatusVersionRequest(respW, req)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+		}
+
+		// Check the version
+		serverVersion := obj.(*string)
+		clientVersion := version.GetVersion().FullVersionNumber(true)
+		if *serverVersion != clientVersion {
+			t.Fatalf("bad: version mismatch %s vs %s", *serverVersion, clientVersion)
 		}
 	})
 }
