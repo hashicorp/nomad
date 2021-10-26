d2c730d032b761cea7c455d6132e1475cf9e9ee7
Luiz Aoqui <luiz@hashicorp.com>
2021-10-26 17:34:37 -0400

    add nolint to arch-specific int64 conversion

--- command/agent/log_file_linux.go
+++ command/agent/log_file_linux.go
@@ -13,5 +13,5 @@ func (l *logFile) createTime(stat os.FileInfo) time.Time {
 	stat_t := stat.Sys().(*syscall.Stat_t)
 	createTime := stat_t.Ctim
 	// Sec and Nsec are int32 in 32-bit architectures.
-	return time.Unix(int64(createTime.Sec), int64(createTime.Nsec))
+	return time.Unix(int64(createTime.Sec), int64(createTime.Nsec)) //nolint:unconvert
 }
