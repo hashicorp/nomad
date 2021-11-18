9cbea5b3c0ea187080d331765a7f076920e148e4
Gowtham <gts@gowtham.dev>
2021-11-18 18:38:01 +0530

    add concurrent download support - resolves #11244

--- client/allocrunner/taskrunner/artifact_hook.go
+++ client/allocrunner/taskrunner/artifact_hook.go
@@ -3,12 +3,12 @@ package taskrunner
 import (
 	"context"
 	"fmt"
-
 	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/nomad/client/allocrunner/interfaces"
 	"github.com/hashicorp/nomad/client/allocrunner/taskrunner/getter"
 	ti "github.com/hashicorp/nomad/client/allocrunner/taskrunner/interfaces"
 	"github.com/hashicorp/nomad/nomad/structs"
+	"sync"
 )
 
 // artifactHook downloads artifacts for a task.
@@ -23,6 +23,47 @@ func newArtifactHook(e ti.EventEmitter, logger log.Logger) *artifactHook {
 	}
 	h.logger = logger.Named(h.Name())
 	return h
+}
+
+func (h *artifactHook) createWorkers(req *interfaces.TaskPrestartRequest, resp *interfaces.TaskPrestartResponse, noOfWorkers int, jobsChannel chan *structs.TaskArtifact, errorChannel chan error) {
+	var wg sync.WaitGroup
+	for i := 0; i < noOfWorkers; i++ {
+		wg.Add(1)
+		go h.doWork(req, resp, jobsChannel, errorChannel, &wg)
+	}
+	wg.Wait()
+	close(errorChannel)
+}
+
+func (h *artifactHook) doWork(req *interfaces.TaskPrestartRequest, resp *interfaces.TaskPrestartResponse, jobs chan *structs.TaskArtifact, errorChannel chan error, wg *sync.WaitGroup) {
+	for artifact := range jobs {
+		aid := artifact.Hash()
+		if req.PreviousState[aid] != "" {
+			h.logger.Trace("skipping already downloaded artifact", "artifact", artifact.GetterSource)
+			resp.State[aid] = req.PreviousState[aid]
+			continue
+		}
+
+		h.logger.Debug("downloading artifact", "artifact", artifact.GetterSource)
+		//XXX add ctx to GetArtifact to allow cancelling long downloads
+		if err := getter.GetArtifact(req.TaskEnv, artifact); err != nil {
+
+			wrapped := structs.NewRecoverableError(
+				fmt.Errorf("failed to download artifact %q: %v", artifact.GetterSource, err),
+				true,
+			)
+			herr := NewHookError(wrapped, structs.NewTaskEvent(structs.TaskArtifactDownloadFailed).SetDownloadError(wrapped))
+
+			errorChannel <- herr
+			continue
+		}
+
+		// Mark artifact as downloaded to avoid re-downloading due to
+		// retries caused by subsequent artifacts failing. Any
+		// non-empty value works.
+		resp.State[aid] = "1"
+	}
+	wg.Done()
 }
 
 func (*artifactHook) Name() string {
@@ -42,31 +83,28 @@ func (h *artifactHook) Prestart(ctx context.Context, req *interfaces.TaskPrestar
 
 	h.eventEmitter.EmitEvent(structs.NewTaskEvent(structs.TaskDownloadingArtifacts))
 
-	for _, artifact := range req.Task.Artifacts {
-		aid := artifact.Hash()
-		if req.PreviousState[aid] != "" {
-			h.logger.Trace("skipping already downloaded artifact", "artifact", artifact.GetterSource)
-			resp.State[aid] = req.PreviousState[aid]
-			continue
+	// maxConcurrency denotes the number of workers that will download artifacts in parallel
+	maxConcurrency := 3
+
+	// jobsChannel is a buffered channel which will have all the artifacts that needs to be processed
+	jobsChannel := make(chan *structs.TaskArtifact, maxConcurrency)
+	// Push all artifact requests to job channel
+	go func() {
+		for _, artifact := range req.Task.Artifacts {
+			jobsChannel <- artifact
 		}
+		close(jobsChannel)
+	}()
 
-		h.logger.Debug("downloading artifact", "artifact", artifact.GetterSource)
-		//XXX add ctx to GetArtifact to allow cancelling long downloads
-		if err := getter.GetArtifact(req.TaskEnv, artifact); err != nil {
+	errorChannel := make(chan error, maxConcurrency)
+	// create workers and process artifacts
+	h.createWorkers(req, resp, maxConcurrency, jobsChannel, errorChannel)
 
-			wrapped := structs.NewRecoverableError(
-				fmt.Errorf("failed to download artifact %q: %v", artifact.GetterSource, err),
-				true,
-			)
-			herr := NewHookError(wrapped, structs.NewTaskEvent(structs.TaskArtifactDownloadFailed).SetDownloadError(wrapped))
-
-			return herr
+	// Iterate over the errorChannel and if there is an error, return it
+	for err := range errorChannel {
+		if err != nil {
+			return err
 		}
-
-		// Mark artifact as downloaded to avoid re-downloading due to
-		// retries caused by subsequent artifacts failing. Any
-		// non-empty value works.
-		resp.State[aid] = "1"
 	}
 
 	resp.Done = true
