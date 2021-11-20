dc3c0b134ac097aa8c5b8ddf7c472f5e59967863
Jai Bhagat <jaybhagat841@gmail.com>
2021-11-20 10:04:15 -0500

    feat:  add status filter to allocations

--- ui/app/controllers/jobs/job/allocations.js
+++ ui/app/controllers/jobs/job/allocations.js
@@ -4,14 +4,15 @@ import { action, computed } from '@ember/object';
 import Sortable from 'nomad-ui/mixins/sortable';
 import Searchable from 'nomad-ui/mixins/searchable';
 import WithNamespaceResetting from 'nomad-ui/mixins/with-namespace-resetting';
+import { serialize, deserializedQueryParam as selection } from 'nomad-ui/utils/qp-serialize';
 import classic from 'ember-classic-decorator';
 
 @classic
 export default class AllocationsController extends Controller.extend(
-    Sortable,
-    Searchable,
-    WithNamespaceResetting
-  ) {
+  Sortable,
+  Searchable,
+  WithNamespaceResetting
+) {
   queryParams = [
     {
       currentPage: 'page',
@@ -25,8 +26,12 @@ export default class AllocationsController extends Controller.extend(
     {
       sortDescending: 'desc',
     },
+    {
+      qpStatus: 'status',
+    },
   ];
 
+  qpStatus = '';
   currentPage = 1;
   pageSize = 25;
 
@@ -40,10 +45,23 @@ export default class AllocationsController extends Controller.extend(
     return ['shortId', 'name', 'taskGroupName'];
   }
 
-  @computed('model.allocations.[]')
+  @computed('model.allocations.[]', 'selectionStatus')
   get allocations() {
-    return this.get('model.allocations') || [];
+    const allocations = this.get('model.allocations') || [];
+    const { selectionStatus } = this;
+
+    if (!allocations.length) return allocations;
+
+    return allocations.filter(alloc => {
+      if (selectionStatus.length && !selectionStatus.includes(alloc.status)) {
+        return false;
+      }
+
+      return true;
+    });
   }
+
+  @selection('qpStatus') selectionStatus;
 
   @alias('allocations') listToSort;
   @alias('listSorted') listToSearch;
@@ -52,5 +70,20 @@ export default class AllocationsController extends Controller.extend(
   @action
   gotoAllocation(allocation) {
     this.transitionToRoute('allocations.allocation', allocation);
+  }
+
+  get optionsAllocationStatus() {
+    return [
+      { key: 'queued', label: 'Queued' },
+      { key: 'starting', label: 'Starting' },
+      { key: 'running', label: 'Running' },
+      { key: 'complete', label: 'Complete' },
+      { key: 'failed', label: 'Failed' },
+      { key: 'lost', label: 'Lost' },
+    ];
+  }
+
+  setFacetQueryParam(queryParam, selection) {
+    this.set(queryParam, serialize(selection));
   }
 }
