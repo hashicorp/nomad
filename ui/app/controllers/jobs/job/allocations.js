c3f14395f8b6a8c0530d58b542e7d1ddac111b8e
Jai Bhagat <jaybhagat841@gmail.com>
2021-11-20 10:49:31 -0500

    disable eslint for indentation

--- ui/app/controllers/jobs/job/allocations.js
+++ ui/app/controllers/jobs/job/allocations.js
@@ -12,6 +12,7 @@ import classic from 'ember-classic-decorator';
 
 @classic
 export default class AllocationsController extends Controller.extend(
+  /*eslint-disable  indent */
   Sortable,
   Searchable,
   WithNamespaceResetting
