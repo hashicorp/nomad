74a5af53bee3081f605ab47f81805def06ed7290
Tim Gross <tim@0x74696d.com>
2021-11-30 09:54:57 -0500

    ui: change Consul/Vault base URL field name
    Give ourselves some room for extension in the UI configuration block
    by naming the field `ui_url`, which will let us have an `api_url`.
    Fix the template path to ensure we're getting the right value from the
    API.

--- nomad/structs/config/ui.go
+++ nomad/structs/config/ui.go
@@ -15,19 +15,19 @@ type UIConfig struct {
 	Vault *VaultUIConfig `hcl:"vault"`
 }
 
-// ConsulUIConfig configures deep links to this cluster's Consul UI
+// ConsulUIConfig configures deep links to this cluster's Consul
 type ConsulUIConfig struct {
 
-	// BaseURL provides the full base URL, ex:
+	// BaseUIURL provides the full base URL to the UI, ex:
 	// https://consul.example.com:8500/ui/
-	BaseURL string `hcl:"base_url"`
+	BaseUIURL string `hcl:"ui_url"`
 }
 
-// VaultUIConfig configures deep links to this cluster's Vault UI
+// VaultUIConfig configures deep links to this cluster's Vault
 type VaultUIConfig struct {
-	// BaseURL provides the full base URL, ex:
+	// BaseUIURL provides the full base URL to the UI, ex:
 	// https://vault.example.com:8200/ui/
-	BaseURL string `hcl:"base_url"`
+	BaseUIURL string `hcl:"ui_url"`
 }
 
 // DefaultUIConfig returns the canonical defaults for the Nomad
@@ -95,8 +95,8 @@ func (this *ConsulUIConfig) Merge(other *ConsulUIConfig) *ConsulUIConfig {
 		return result
 	}
 
-	if other.BaseURL != "" {
-		result.BaseURL = other.BaseURL
+	if other.BaseUIURL != "" {
+		result.BaseUIURL = other.BaseUIURL
 	}
 	return result
 }
@@ -123,8 +123,8 @@ func (this *VaultUIConfig) Merge(other *VaultUIConfig) *VaultUIConfig {
 		return result
 	}
 
-	if other.BaseURL != "" {
-		result.BaseURL = other.BaseURL
+	if other.BaseUIURL != "" {
+		result.BaseUIURL = other.BaseUIURL
 	}
 	return result
 }
