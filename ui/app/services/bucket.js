67e6cec66b06d4706922700435035c831aedbc91
Jai Bhagat <jaybhagat841@gmail.com>
2021-11-30 15:09:16 -0500

    feat:  create bucket service to register, deregister and store breadcrumbs

---
+++ ui/app/services/bucket.js
@@ -0,0 +1,21 @@
+import Service from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { schedule } from '@ember/runloop';
+
+export default class BucketService extends Service {
+  @tracked crumbs = [];
+
+  @action registerBreadcrumb(crumb) {
+    schedule('actions', this, () => {
+      console.log('register crumb:  ', crumb);
+      this.crumbs = [...this.crumbs, crumb];
+    });
+  }
+
+  @action deregisterBreadcrumb(crumb) {
+    const newCrumbs = this.crumbs.filter(c => c !== crumb);
+
+    this.crumbs = newCrumbs;
+  }
+}
