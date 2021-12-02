f68bd91e849d7fd427d3ee8a5e3a56c5fe617b2f
Kevin Wang <kwangsan@gmail.com>
2021-12-02 11:57:31 -0500

    chore: use getStaticGenerationFunctions

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -1,9 +1,6 @@
 import { productName, productSlug } from 'data/metadata'
 import DocsPage from '@hashicorp/react-docs-page'
-import {
-  generateStaticPaths,
-  generateStaticProps,
-} from '@hashicorp/react-docs-page/server'
+import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server'
 import Placement from 'components/placement-table'
 
 const NAV_DATA_FILE = 'data/docs-nav-data.json'
@@ -18,36 +15,28 @@ export default function DocsLayout(props) {
       baseRoute={basePath}
       staticProps={props}
       additionalComponents={additionalComponents}
-      showVersionSelect={true}
+      showVersionSelect={process.env.ENABLE_VERSIONED_DOCS === 'true'}
     />
   )
 }
 
-export async function getStaticPaths() {
-  return {
-    fallback: 'blocking',
-    paths: await generateStaticPaths({
-      navDataFile: NAV_DATA_FILE,
-      localContentDir: CONTENT_DIR,
-      product: { name: productName, slug: productSlug },
-      basePath: basePath,
-    }),
-  }
-}
-export async function getStaticProps({ params }) {
-  try {
-    return {
-      revalidate: 10,
-      props: await generateStaticProps({
-        params,
-        navDataFile: NAV_DATA_FILE,
-        localContentDir: CONTENT_DIR,
-        product: { name: productName, slug: productSlug },
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
+  process.env.ENABLE_VERSIONED_DOCS === 'true'
+    ? {
+        strategy: 'remote',
         basePath: basePath,
-      }),
-    }
-  } catch (err) {
-    console.warn(err)
-    return { notFound: true }
-  }
-}
+        fallback: 'blocking',
+        revalidate: 360, // 1 hour
+        product: productSlug,
+      }
+    : {
+        strategy: 'fs',
+        basePath: basePath,
+        localContentDir: CONTENT_DIR,
+        navDataFile: NAV_DATA_FILE,
+        product: productSlug,
+        revalidate: false,
+      }
+)
+
+export { getStaticPaths, getStaticProps }
