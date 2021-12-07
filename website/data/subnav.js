64aeef14c1a698dd9d121280483739f7ae25d6ec
Luiz Aoqui <luiz@hashicorp.com>
2021-12-07 18:42:12 -0500

    website: remove "Use Cases" and split "Plugins" and "Tools" into separate items

--- website/data/subnav.js
+++ website/data/subnav.js
@@ -1,23 +1,6 @@
 const subnavData = [
   { text: 'Overview', url: '/', type: 'inbound' },
   {
-    text: 'Use Cases',
-    submenu: [
-      {
-        text: 'Simple Container Orchestration',
-        url: '/use-cases/simple-container-orchestration',
-      },
-      {
-        text: 'Non-Containerized Application Orchestration',
-        url: '/use-cases/non-containerized-application-orchestration',
-      },
-      {
-        text: 'Automated Service Networking with Consul',
-        url: '/use-cases/automated-service-networking-with-consul',
-      },
-    ],
-  },
-  {
     text: 'Enterprise',
     url: 'https://www.hashicorp.com/products/nomad/',
     type: 'outbound',
@@ -39,8 +22,13 @@ const subnavData = [
     type: 'inbound',
   },
   {
-    text: 'Plugins and Tools',
+    text: 'Plugins',
     url: '/plugins',
+    type: 'inbound',
+  },
+  {
+    text: 'Tools',
+    url: '/tools',
     type: 'inbound',
   },
   {
