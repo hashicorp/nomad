19fe384f88b26637796a54b3833ab139e2122a34
Mikael Sikora <mikael.sikora@hashicorp.com>
2021-12-08 10:28:43 -0800

    Revert "Update Ubuntu that is compatible"
    This reverts commit 9ccc73d011342ade9b263298f078ba2f615bddfd.

--- terraform/shared/scripts/setup.sh
+++ terraform/shared/scripts/setup.sh
@@ -111,8 +111,8 @@ sudo apt-get install -y docker-ce
 # Needs testing, updating and fixing
 if [[ ! -z ${INSTALL_NVIDIA_DOCKER+x} ]]; then 
   # Install official NVIDIA driver package
-  sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-  sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+  sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+  sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
   sudo apt-get update && sudo apt-get install -y --no-install-recommends linux-headers-generic dkms cuda-drivers
 
   # Install nvidia-docker and nvidia-docker-plugin
