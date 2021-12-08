079702249ed0440c62fce1d05e67216899e5b183
Luiz Aoqui <luiz@hashicorp.com>
2021-12-08 16:15:14 -0500

    docs: split external plugins and tools into the new section (#11624)
    * docs: split external plugins and tools into the new section
    
    * website: remove "Use Cases" and split "Plugins" and "Tools" into separate items
    
    * Adding tools
    
    * docs: fix autoscaling tool link
    
    Co-authored-by: Mike Nomitch <mnomitch@hashicorp.com>

--- website/data/subnav.js
+++ website/data/subnav.js
@@ -1,23 +1,6 @@
 const subnavData = [
   { text: 'Overview', url: '/', type: 'inbound' },
   {
-    text: 'Use Cases',
-    submenu: [
-      {
-        text: 'Simple Container Orchestration',
-        url: '/use-cases/simple-container-orchestration',
-      },
-      {
-        text: 'Non-Containerized Application Orchestration',
-        url: '/use-cases/non-containerized-application-orchestration',
-      },
-      {
-        text: 'Automated Service Networking with Consul',
-        url: '/use-cases/automated-service-networking-with-consul',
-      },
-    ],
-  },
-  {
     text: 'Enterprise',
     url: 'https://www.hashicorp.com/products/nomad/',
     type: 'outbound',
@@ -41,6 +24,11 @@ const subnavData = [
   {
     text: 'Plugins',
     url: '/plugins',
+    type: 'inbound',
+  },
+  {
+    text: 'Tools',
+    url: '/tools',
     type: 'inbound',
   },
   {
