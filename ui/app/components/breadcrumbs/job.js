e94a97906626304c1601bb05776a60f39f1e195e
Jai Bhagat <jaybhagat841@gmail.com>
2021-12-09 11:32:28 -0500

    fix:  resolve parent on job still issue with alloc job async relationship

--- ui/app/components/breadcrumbs/job.js
+++ ui/app/components/breadcrumbs/job.js
@@ -1,36 +1,16 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { qpBuilder } from 'nomad-ui/utils/classes/query-params';
 
 export default class BreadcrumbsJob extends Component {
   get job() {
     return this.args.crumb.job;
   }
 
-  @tracked parent = null;
-
-  generateCrumb(job) {
-    return {
-      label: job.get('trimmedName') || job.trimmedName,
-      args: [
-        'jobs.job.index',
-        job.get('plainId') || job.plainId,
-        qpBuilder({
-          jobNamespace: job.get('namespace.name') || 'default',
-        }),
-      ],
-    };
-  }
-
-  get crumb() {
-    if (!this.job) return null;
-    return this.generateCrumb(this.job);
-  }
-
   @action
   fetchParent() {
-    this.parent = this.job.parent || this.job.get('parent');
-    return this.generateCrumb(this.parent);
+    const hasParent = !!this.job.belongsTo('parent').id();
+    if (hasParent) {
+      return this.job.get('parent');
+    }
   }
 }
