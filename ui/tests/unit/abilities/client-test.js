eefcfe4a8aeb7f645e48e284749bf13fd24a49a7
Luiz Aoqui <luiz@hashicorp.com>
2021-12-13 16:56:58 -0500

    ui: add "read client" ability

--- ui/tests/unit/abilities/client-test.js
+++ ui/tests/unit/abilities/client-test.js
@@ -8,26 +8,28 @@ module('Unit | Ability | client', function(hooks) {
   setupTest(hooks);
   setupAbility('client')(hooks);
 
-  test('it permits client write when ACLs are disabled', function(assert) {
+  test('it permits client read and write when ACLs are disabled', function(assert) {
     const mockToken = Service.extend({
       aclEnabled: false,
     });
     this.owner.register('service:token', mockToken);
 
+    assert.ok(this.ability.canRead);
     assert.ok(this.ability.canWrite);
   });
 
-  test('it permits client write for management tokens', function(assert) {
+  test('it permits client read and write for management tokens', function(assert) {
     const mockToken = Service.extend({
       aclEnabled: true,
       selfToken: { type: 'management' },
     });
     this.owner.register('service:token', mockToken);
 
+    assert.ok(this.ability.canRead);
     assert.ok(this.ability.canWrite);
   });
 
-  test('it permits client write for tokens with a policy that has node-write', function(assert) {
+  test('it permits client read and write for tokens with a policy that has node-write', function(assert) {
     const mockToken = Service.extend({
       aclEnabled: true,
       selfToken: { type: 'client' },
@@ -43,10 +45,11 @@ module('Unit | Ability | client', function(hooks) {
     });
     this.owner.register('service:token', mockToken);
 
+    assert.ok(this.ability.canRead);
     assert.ok(this.ability.canWrite);
   });
 
-  test('it permits client write for tokens with a policy that allows write and another policy that disallows it', function(assert) {
+  test('it permits client read and write for tokens with a policy that allows write and another policy that disallows it', function(assert) {
     const mockToken = Service.extend({
       aclEnabled: true,
       selfToken: { type: 'client' },
@@ -69,10 +72,11 @@ module('Unit | Ability | client', function(hooks) {
     });
     this.owner.register('service:token', mockToken);
 
+    assert.ok(this.ability.canRead);
     assert.ok(this.ability.canWrite);
   });
 
-  test('it blocks client write for tokens with a policy that does not allow node-write', function(assert) {
+  test('it permits client read and blocks client write for tokens with a policy that does not allow node-write', function(assert) {
     const mockToken = Service.extend({
       aclEnabled: true,
       selfToken: { type: 'client' },
@@ -88,6 +92,23 @@ module('Unit | Ability | client', function(hooks) {
     });
     this.owner.register('service:token', mockToken);
 
+    assert.ok(this.ability.canRead);
+    assert.notOk(this.ability.canWrite);
+  });
+
+  test('it blocks client read and write for tokens without a node policy', function(assert) {
+    const mockToken = Service.extend({
+      aclEnabled: true,
+      selfToken: { type: 'client' },
+      selfTokenPolicies: [
+        {
+          rulesJSON: {},
+        },
+      ],
+    });
+    this.owner.register('service:token', mockToken);
+
+    assert.notOk(this.ability.canRead);
     assert.notOk(this.ability.canWrite);
   });
 });
