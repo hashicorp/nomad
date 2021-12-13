77f20e0946caa2703056b2bc0844e250af4e3224
Kevin Wang <kwangsan@gmail.com>
2021-12-13 16:21:57 -0500

    feat: versioned docs (#11407)

--- website/next.config.js
+++ website/next.config.js
@@ -23,5 +23,6 @@ module.exports = withHashicorp({
     SEGMENT_WRITE_KEY: 'qW11yxgipKMsKFKQUCpTVgQUYftYsJj0',
     BUGSNAG_CLIENT_KEY: '4fa712dfcabddd05da29fd1f5ea5a4c0',
     BUGSNAG_SERVER_KEY: '61141296f1ba00a95a8788b7871e1184',
+    ENABLE_VERSIONED_DOCS: process.env.ENABLE_VERSIONED_DOCS || false,
   },
 })
