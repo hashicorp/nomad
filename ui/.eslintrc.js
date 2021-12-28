1975f43d35ae169afe765abd3c991006734d7334
Jai Bhagat <jaybhagat841@gmail.com>
2021-12-28 10:46:53 -0500

    ui:  turn off no-component-lifecycle-hooks
    Classic components use lifecycle hooks.

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'ember/no-classic-classes': 'off',
     'ember/no-computed-properties-in-native-classes': 'off',
     'ember/no-classic-components': 'off',
+    'ember/no-component-lifecycle-hooks': 'off',
   },
   overrides: [
     // node files
