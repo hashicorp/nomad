d64df8ba6813a8b7a422da571adf277e37bde16b
Jai Bhagat <jaybhagat841@gmail.com>
2022-01-03 12:34:02 -0500

    fix:  router expect identifier instead of model for trans to job page

--- ui/app/components/job-row.js
+++ ui/app/components/job-row.js
@@ -26,7 +26,7 @@ export default class JobRow extends Component {
   @action
   gotoJob() {
     const { job } = this;
-    this.router.transitionTo('jobs.job', job, {
+    this.router.transitionTo('jobs.job', job.plainId, {
       queryParams: { namespace: job.get('namespace.name') },
     });
   }
