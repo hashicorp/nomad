7c7347a535bbd8ea8affc1a65d1153ae79336c19
Tim Gross <tgross@hashicorp.com>
2022-01-06 11:29:01 -0500

    benchmark closely inserted vs over time

--- nomad/state/ulid_test.go
+++ nomad/state/ulid_test.go
@@ -12,12 +12,181 @@ import (
 	"github.com/hashicorp/nomad/nomad/structs"
 )
 
-func generateEvals(count int, idFn func() string) []*structs.Evaluation {
+func Benchmark_SortableIDs_Iteration(b *testing.B) {
+
+	const evalCount = 10000
+
+	b.Run("uuid iterate", func(b *testing.B) {
+		state := setupPopulatedState(b, evalCount, uuidGenerator())
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			iter, _ := state.EvalsByNamespace(nil, structs.DefaultNamespace)
+			var lastSeen string
+			var countSeen int
+			for {
+				raw := iter.Next()
+				if raw == nil {
+					break
+				}
+				eval := raw.(*structs.Evaluation)
+				lastSeen = eval.ID
+				countSeen++
+			}
+			if countSeen < evalCount {
+				b.Fatalf("failed: %d evals seen, lastSeen=%s", countSeen, lastSeen)
+			}
+		}
+	})
+
+	b.Run("ulid iterate inserted closely", func(b *testing.B) {
+		entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+		state := setupPopulatedState(b, evalCount,
+			ulidGenerator(entropy))
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			iter, _ := state.EvalsByNamespace(nil, structs.DefaultNamespace)
+			var lastSeen string
+			var countSeen int
+			for {
+				raw := iter.Next()
+				if raw == nil {
+					break
+				}
+				eval := raw.(*structs.Evaluation)
+				lastSeen = eval.ID
+				countSeen++
+			}
+			if countSeen < evalCount {
+				b.Fatalf("failed: %d evals seen, lastSeen=%s", countSeen, lastSeen)
+			}
+		}
+	})
+
+	b.Run("ulid iterate inserted over hours", func(b *testing.B) {
+		entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+		state := setupPopulatedState(b, evalCount,
+			ulidGeneratorForPast(entropy, 4, evalCount))
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			iter, _ := state.EvalsByNamespace(nil, structs.DefaultNamespace)
+			var lastSeen string
+			var countSeen int
+			for {
+				raw := iter.Next()
+				if raw == nil {
+					break
+				}
+				eval := raw.(*structs.Evaluation)
+				lastSeen = eval.ID
+				countSeen++
+			}
+			if countSeen < evalCount {
+				b.Fatalf("failed: %d evals seen, lastSeen=%s", countSeen, lastSeen)
+			}
+		}
+	})
+
+}
+
+func Benchmark_SortableIDs_Upserts(b *testing.B) {
+
+	const evalCount = 10000
+
+	b.Run("uuid upsert eval", func(b *testing.B) {
+		state := setupPopulatedState(b, evalCount, uuidGenerator())
+		evals := generateEvals(evalCount, uuidGenerator())
+
+		index := uint64(0)
+		var err error
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			snap, _ := state.Snapshot()
+			for _, eval := range evals {
+				index++
+				err = snap.UpsertEvals(
+					structs.MsgTypeTestSetup, index, []*structs.Evaluation{eval})
+			}
+		}
+		if err != nil {
+			b.Fatalf("failed: %v", err)
+		}
+	})
+
+	b.Run("ulid upsert eval closely", func(b *testing.B) {
+		entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+		state := setupPopulatedState(b, evalCount,
+			ulidGeneratorForPast(entropy, 4, evalCount))
+		evals := generateEvals(evalCount, ulidGenerator(entropy))
+
+		index := uint64(0)
+		var err error
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			snap, _ := state.Snapshot()
+			for _, eval := range evals {
+				index++
+				err = snap.UpsertEvals(
+					structs.MsgTypeTestSetup, index, []*structs.Evaluation{eval})
+			}
+		}
+		if err != nil {
+			b.Fatalf("failed: %v", err)
+		}
+	})
+
+	b.Run("ulid upsert eval over hours", func(b *testing.B) {
+		entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+		state := setupPopulatedState(b, evalCount,
+			ulidGeneratorForPast(entropy, 4, evalCount))
+		evals := generateEvals(evalCount, ulidGeneratorForPast(entropy, 4, evalCount))
+
+		index := uint64(0)
+		var err error
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			snap, _ := state.Snapshot()
+			for _, eval := range evals {
+				index++
+				err = snap.UpsertEvals(
+					structs.MsgTypeTestSetup, index, []*structs.Evaluation{eval})
+			}
+		}
+		if err != nil {
+			b.Fatalf("failed: %v", err)
+		}
+	})
+
+}
+
+// -----------------
+// BENCHMARK HELPER FUNCTIONS
+
+func setupPopulatedState(b *testing.B, evalCount int, idFn func(int) string) *StateStore {
+	evals := generateEvals(evalCount, idFn)
+
+	index := uint64(0)
+	var err error
+	state := TestStateStore(b)
+	for _, eval := range evals {
+		index++
+		err = state.UpsertEvals(
+			structs.MsgTypeTestSetup, index, []*structs.Evaluation{eval})
+	}
+	if err != nil {
+		b.Fatalf("failed: %v", err)
+	}
+	return state
+}
+
+func generateEvals(count int, idFn func(int) string) []*structs.Evaluation {
 	evals := []*structs.Evaluation{}
 	for i := 0; i < count; i++ {
 		now := time.Now().UTC().UnixNano()
 		eval := &structs.Evaluation{
-			ID:         idFn(),
+			ID:         idFn(i),
 			Namespace:  structs.DefaultNamespace,
 			Priority:   50,
 			Type:       structs.JobTypeService,
@@ -32,9 +201,24 @@ func generateEvals(count int, idFn func() string) []*structs.Evaluation {
 	return evals
 }
 
-func generateULID(entropy *ulid.MonotonicEntropy) string {
+func ulidGenerator(entropy *ulid.MonotonicEntropy) func(int) string {
+	return func(_ int) string {
+		return generateULIDAt(entropy, ulid.Timestamp(time.Now()))
+	}
+}
 
-	id := ulid.MustNew(ulid.Timestamp(time.Now()), entropy)
+// generates ULIDs so that they're spread evenly out over the past several hours
+func ulidGeneratorForPast(entropy *ulid.MonotonicEntropy, hoursPast int, totalCount int) func(int) string {
+	base := time.Now().Add(time.Duration(-hoursPast) * time.Hour)
+	multiplier := uint64(hoursPast * totalCount)
+	return func(idx int) string {
+		ts := ulid.Timestamp(base) + uint64(idx)*multiplier
+		return generateULIDAt(entropy, ts)
+	}
+}
+
+func generateULIDAt(entropy *ulid.MonotonicEntropy, ts uint64) string {
+	id := ulid.MustNew(ts, entropy)
 	buf, _ := id.MarshalBinary()
 	return fmt.Sprintf("%08x-%04x-%04x-%04x-%12x",
 		buf[0:4],
@@ -44,125 +228,8 @@ func generateULID(entropy *ulid.MonotonicEntropy) string {
 		buf[10:16])
 }
 
-func Benchmark_EvalsIDGeneration(b *testing.B) {
-
-	const evalCount = 10000
-
-	b.Run("uuid insert", func(b *testing.B) {
-		evals := generateEvals(evalCount, uuid.Generate)
-
-		index := uint64(0)
-		var err error
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			state := TestStateStore(b)
-			for _, eval := range evals {
-				index++
-				err = state.UpsertEvals(
-					structs.MsgTypeTestSetup, index, []*structs.Evaluation{eval})
-			}
-		}
-		if err != nil {
-			b.Fatalf("failed: %v", err)
-		}
-	})
-
-	b.Run("uuid iterate", func(b *testing.B) {
-		evals := generateEvals(evalCount, uuid.Generate)
-
-		index := uint64(0)
-		var err error
-		state := TestStateStore(b)
-		for _, eval := range evals {
-			index++
-			err = state.UpsertEvals(
-				structs.MsgTypeTestSetup, index, []*structs.Evaluation{eval})
-		}
-		if err != nil {
-			b.Fatalf("failed: %v", err)
-		}
-
-		b.ResetTimer()
-
-		for i := 0; i < b.N; i++ {
-			iter, _ := state.EvalsByNamespace(nil, structs.DefaultNamespace)
-			var lastSeen string
-			var countSeen int
-			for {
-				raw := iter.Next()
-				if raw == nil {
-					break
-				}
-				eval := raw.(*structs.Evaluation)
-				lastSeen = eval.ID
-				countSeen++
-			}
-			if countSeen < evalCount {
-				b.Fatalf("failed: %d evals seen, lastSeen=%s", countSeen, lastSeen)
-			}
-		}
-	})
-
-	b.Run("ulid insert", func(b *testing.B) {
-
-		entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
-		evals := generateEvals(evalCount, func() string {
-			return generateULID(entropy)
-		})
-
-		index := uint64(0)
-		var err error
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			state := TestStateStore(b)
-			for _, eval := range evals {
-				index++
-				err = state.UpsertEvals(
-					structs.MsgTypeTestSetup, index, []*structs.Evaluation{eval})
-			}
-		}
-		if err != nil {
-			b.Fatalf("failed: %v", err)
-		}
-	})
-
-	b.Run("ulid iterate", func(b *testing.B) {
-		entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
-		evals := generateEvals(evalCount, func() string {
-			return generateULID(entropy)
-		})
-
-		index := uint64(0)
-		var err error
-		state := TestStateStore(b)
-		for _, eval := range evals {
-			index++
-			err = state.UpsertEvals(
-				structs.MsgTypeTestSetup, index, []*structs.Evaluation{eval})
-		}
-		if err != nil {
-			b.Fatalf("failed: %v", err)
-		}
-
-		b.ResetTimer()
-
-		for i := 0; i < b.N; i++ {
-			iter, _ := state.EvalsByNamespace(nil, structs.DefaultNamespace)
-			var lastSeen string
-			var countSeen int
-			for {
-				raw := iter.Next()
-				if raw == nil {
-					break
-				}
-				eval := raw.(*structs.Evaluation)
-				lastSeen = eval.ID
-				countSeen++
-			}
-			if countSeen < evalCount {
-				b.Fatalf("failed: %d evals seen, lastSeen=%s", countSeen, lastSeen)
-			}
-		}
-	})
-
+func uuidGenerator() func(int) string {
+	return func(_ int) string {
+		return uuid.Generate()
+	}
 }
