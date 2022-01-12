e644aa7c0a01452b971feeb5ffa65376e0ef48da
sara-gawlinski <56051524+sara-gawlinski@users.noreply.github.com>
2022-01-12 10:28:17 -0600

    Update alert-banner (#11817)
    Updating banner for edge survey

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -1,11 +1,11 @@
-export const ALERT_BANNER_ACTIVE = false
+export const ALERT_BANNER_ACTIVE = true
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'Blog post',
-  url: 'https://www.hashicorp.com/blog/a-new-chapter-for-hashicorp',
-  text: 'HashiCorp shares have begun trading on the Nasdaq. Read the blog from our founders, Mitchell Hashimoto and Armon Dadgar.',
-  linkText: 'Read the post',
+  tag: 'Survey',
+  url: 'https://docs.google.com/forms/d/e/1FAIpQLSeyDEyQXzkijZnkXjj8qVb_5IydajRkFnOrPjDNoysFs-6jDQ/viewform',
+  text: 'Using Nomad for edge workloads? We want to hear about it!',
+  linkText: 'Fill out our user survey',
   // Set the expirationDate prop with a datetime string (e.g. '2020-01-31T12:00:00-07:00')
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-12-17T23:00:00-07:00',
+  expirationDate: '2022-02-08T23:00:00-07:00',
 }
