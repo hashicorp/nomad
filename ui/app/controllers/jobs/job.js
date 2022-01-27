d024e7ae5e4235c9c22abcfb597895f1a5acfb46
Jai Bhagat <jaybhagat841@gmail.com>
2022-01-27 16:25:49 -0500

    chore:  remove jobNamespace query parameter

--- ui/app/controllers/jobs/job.js
+++ ui/app/controllers/jobs/job.js
@@ -1,13 +1,6 @@
 import Controller from '@ember/controller';
 
 export default class JobController extends Controller {
-  queryParams = [
-    {
-      jobNamespace: 'namespace',
-    },
-  ];
-  jobNamespace = 'default';
-
   get job() {
     return this.model;
   }
