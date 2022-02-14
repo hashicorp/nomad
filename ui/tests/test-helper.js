f03a8d656f19538225121226abacbea6fa13ed48
Jai Bhagat <jaybhagat841@gmail.com>
2022-02-14 09:53:46 -0500

    temp:  upgrade to ember-cli 3.24

--- ui/tests/test-helper.js
+++ ui/tests/test-helper.js
@@ -1,12 +1,16 @@
 import 'core-js';
 import Application from 'nomad-ui/app';
 import config from 'nomad-ui/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import start from 'ember-exam/test-support/start';
 import { useNativeEvents } from 'ember-cli-page-object/extend';
 
 useNativeEvents();
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
