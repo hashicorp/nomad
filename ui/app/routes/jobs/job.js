4c387574361b9c5978ee4f5e34bd68a6849d4ace
Michael Klein <michael@firstiwaslike.com>
2022-02-16 17:42:26 +0100

    refact: use `idWithNamespace` in serialize hook jobs.job
    This will give us 'correct' URLs for free when we only pass a `job`-model
    to a `LinkTo` that links to the `jobs.job.*`-routes.

--- ui/app/routes/jobs/job.js
+++ ui/app/routes/jobs/job.js
@@ -11,7 +11,7 @@ export default class JobRoute extends Route {
   @service token;
 
   serialize(model) {
-    return { job_name: JSON.parse(model.get('id')).join('@') };
+    return { job_name: model.get('idWithNamespace') };
   }
 
   model(params) {
