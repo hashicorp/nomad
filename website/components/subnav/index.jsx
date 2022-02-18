ee37484245f39a1fcd0eec6ddc469ff9f55e795a
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-02-18 08:33:57 -0500

    chore: bump subnav

--- website/components/subnav/index.jsx
+++ website/components/subnav/index.jsx
@@ -7,12 +7,18 @@ export default function NomadSubnav() {
   return (
     <Subnav
       titleLink={{
-        text: 'nomad',
+        text: 'HashiCorp Nomad',
         url: '/',
       }}
       ctaLinks={[
         { text: 'GitHub', url: 'https://www.github.com/hashicorp/nomad' },
-        { text: 'Download', url: '/downloads' },
+        {
+          text: 'Download',
+          url: '/downloads',
+          theme: {
+            brand: 'nomad',
+          },
+        },
       ]}
       currentPath={router.asPath}
       menuItemsAlign="right"
