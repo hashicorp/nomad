5b9f41d7fd6673963ffa26304c4752ad820df7ba
Michael Klein <michael@firstiwaslike.com>
2022-03-03 17:04:28 +0100

    fix: remove unused useNativeEvents
    ember-cli-page-objects doesn't provide this method anymore

--- ui/tests/test-helper.js
+++ ui/tests/test-helper.js
@@ -4,10 +4,7 @@ import config from 'nomad-ui/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import start from 'ember-exam/test-support/start';
-import { useNativeEvents } from 'ember-cli-page-object/extend';
 import { setup } from 'qunit-dom';
-
-useNativeEvents();
 
 setApplication(Application.create(config.APP));
 
