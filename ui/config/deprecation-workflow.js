6b1995afb58ae636666c31c535ddde83015cb032
Michael Klein <michael@firstiwaslike.com>
2022-03-04 11:42:41 +0100

    fix: `implicit-injection`-deprecation
    All routes that add watchers will need to inject the store-service
    as the store service is internally used in watchers.

--- ui/config/deprecation-workflow.js
+++ ui/config/deprecation-workflow.js
@@ -17,7 +17,7 @@ self.deprecationWorkflow.config = {
       matchId: 'ember-glimmer.link-to.positional-arguments',
     },
     {
-      handler: 'silence',
+      handler: 'throw',
       matchId: 'implicit-injections',
     },
   ],
