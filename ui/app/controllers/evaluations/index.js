8957ed97616f0c2a590bffe50d2c80e949237b7a
Jai Bhagat <jaybhagat841@gmail.com>
2022-03-10 09:14:18 -0500

    ui:  create and implement statechart for evals

--- ui/app/controllers/evaluations/index.js
+++ ui/app/controllers/evaluations/index.js
@@ -2,11 +2,71 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { matchesState, useMachine } from 'ember-statecharts';
+import { use } from 'ember-usable';
+import evaluationsMachine from '../../machines/evaluations';
 
 export default class EvaluationsController extends Controller {
+  @service store;
   @service userSettings;
 
-  queryParams = ['nextToken', 'pageSize', 'status'];
+  @matchesState({ sidebar: 'open' })
+  isSideBarOpen;
+
+  @use statechart = useMachine(evaluationsMachine).withConfig({
+    services: {
+      loadEvaluation: this.loadEvaluation,
+    },
+    actions: {
+      updateEvaluationQueryParameter: this.updateEvaluationQueryParameter,
+      removeCurrentEvaluationQueryParameter:
+        this.removeCurrentEvaluationQueryParameter,
+    },
+    guards: {
+      sidebarIsOpen: this._sidebarIsOpen,
+    },
+  });
+
+  queryParams = ['nextToken', 'currentEval', 'pageSize', 'status'];
+  @tracked currentEval = null;
+
+  @action
+  _sidebarIsOpen() {
+    return !!this.currentEval;
+  }
+
+  @action
+  async loadEvaluation(context, { evaluation }) {
+    let evaluationId;
+    if (evaluation?.id) {
+      evaluationId = evaluation.id;
+    } else {
+      evaluationId = this.currentEval;
+    }
+    return this.store.findRecord('evaluation', evaluationId, {
+      reload: true,
+    });
+  }
+
+  @action
+  closeSidebar() {
+    return this.statechart.send('MODAL_CLOSE');
+  }
+
+  @action
+  async handleEvaluationClick(evaluation) {
+    this.statechart.send('LOAD_EVALUATION', { evaluation });
+  }
+
+  @action
+  updateEvaluationQueryParameter(context, { evaluation }) {
+    this.currentEval = evaluation.id;
+  }
+
+  @action
+  removeCurrentEvaluationQueryParameter() {
+    this.currentEval = null;
+  }
 
   get shouldDisableNext() {
     return !this.model.meta?.nextToken;
@@ -25,6 +85,116 @@ export default class EvaluationsController extends Controller {
       { key: 'failed', label: 'Failed' },
       { key: 'canceled', label: 'Canceled' },
     ];
+  }
+
+  get currentEvalDetail() {
+    return {
+      ID: this.currentEval,
+      Priority: 50,
+      Type: 'service',
+      TriggeredBy: 'job-register',
+      JobID: 'example',
+      JobModifyIndex: 52,
+      NodeID: '',
+      NodeModifyIndex: 0,
+      Status: 'complete',
+      StatusDescription: '',
+      Wait: 0,
+      NextEval: '',
+      PreviousEval: '',
+      BlockedEval: '',
+      FailedTGAllocs: null,
+      ClassEligibility: null,
+      EscapedComputedClass: false,
+      AnnotatePlan: false,
+      SnapshotIndex: 53,
+      QueuedAllocations: {
+        cache: 0,
+      },
+      CreateIndex: 53,
+      ModifyIndex: 55,
+      RelatedEvals: [
+        {
+          CreateIndex: 82,
+          CreateTime: 1646071501471847000,
+          ID: 'eef1147c-3396-928d-b0f3-ce411bd5d550',
+          JobID: 'system-job',
+          ModifyIndex: 88,
+          ModifyTime: 1646071501474137000,
+          Namespace: 'default',
+          NodeID: '462aad3f-bfa5-00be-d1d4-aa713adb4dbe',
+          NodeModifyIndex: 81,
+          Priority: 50,
+          QueuedAllocations: {
+            cache: 0,
+          },
+          SnapshotIndex: 82,
+          Status: 'complete',
+          TriggeredBy: 'node-update',
+          Type: 'system',
+        },
+        {
+          CreateIndex: 82,
+          CreateTime: 1646071501471847000,
+          DeploymentID: '61adb5d0-4bb2-fe37-f504-21e23377e291',
+          ID: '55d51a54-f7af-f978-2390-c5688a17fba8',
+          JobID: 'example2',
+          ModifyIndex: 90,
+          ModifyTime: 1646071501476093000,
+          Namespace: 'default',
+          NodeID: '462aad3f-bfa5-00be-d1d4-aa713adb4dbe',
+          NodeModifyIndex: 81,
+          Priority: 50,
+          QueuedAllocations: {
+            cache: 0,
+          },
+          SnapshotIndex: 89,
+          Status: 'complete',
+          TriggeredBy: 'node-update',
+          Type: 'service',
+        },
+        {
+          CreateIndex: 82,
+          CreateTime: 1646071501471847000,
+          DeploymentID: 'c60e9405-86ff-5de6-9d3e-ab4ab2409d28',
+          ID: '2b33210a-9471-b523-0ae4-e8fcc9d094fe',
+          JobID: 'example',
+          ModifyIndex: 86,
+          ModifyTime: 1646071501473893000,
+          Namespace: 'default',
+          NodeID: '462aad3f-bfa5-00be-d1d4-aa713adb4dbe',
+          NodeModifyIndex: 81,
+          Priority: 50,
+          QueuedAllocations: {
+            cache: 0,
+          },
+          SnapshotIndex: 84,
+          Status: 'complete',
+          TriggeredBy: 'node-update',
+          Type: 'service',
+        },
+        {
+          CreateIndex: 43,
+          CreateTime: 1646071122930603000,
+          DeploymentID: '61adb5d0-4bb2-fe37-f504-21e23377e291',
+          ID: 'f8915b6d-294b-a60d-172d-f0b5d4e6df1a',
+          JobID: 'example2',
+          ModifyIndex: 46,
+          ModifyTime: 1646071122936311000,
+          Namespace: 'default',
+          NodeID: '462aad3f-bfa5-00be-d1d4-aa713adb4dbe',
+          NodeModifyIndex: 42,
+          Priority: 50,
+          QueuedAllocations: {
+            cache: 0,
+          },
+          SnapshotIndex: 45,
+          Status: 'complete',
+          TriggeredBy: 'node-update',
+          Type: 'service',
+        },
+      ],
+    };
   }
 
   @tracked pageSize = this.userSettings.pageSize;
---
+++ ui/app/machines/evaluations.js
@@ -0,0 +1,126 @@
+import { assign, createMachine, send } from 'xstate';
+
+export default createMachine(
+  {
+    id: 'evaluations_ui',
+    context: { evaluation: null },
+    type: 'parallel',
+    states: {
+      table: {
+        initial: 'unknown',
+        on: {
+          NEXT: {
+            actions: ['requestNextPage', send('MODAL_CLOSE')],
+          },
+          PREV: {
+            actions: ['requestPrevPage', send('MODAL_CLOSE')],
+          },
+          CHANGE_PAGES_SIZE: {
+            actions: ['changePageSize', send('MODAL_CLOSE')],
+          },
+          MODEL_UPDATED: '#unknown',
+        },
+        states: {
+          unknown: {
+            id: 'unknown',
+            always: [{ target: 'data', cond: 'hasData' }, { target: 'empty' }],
+          },
+          data: {},
+          empty: {},
+        },
+      },
+      sidebar: {
+        initial: 'unknown',
+        states: {
+          unknown: {
+            always: [
+              { target: 'open', cond: 'sidebarIsOpen' },
+              { target: 'close' },
+            ],
+          },
+          open: {
+            initial: 'busy',
+            exit: ['removeCurrentEvaluationQueryParameter'],
+            states: {
+              busy: {
+                invoke: {
+                  src: 'loadEvaluation',
+                  onDone: 'success',
+                  onError: 'error',
+                },
+              },
+              success: {
+                entry: assign({
+                  evaluation: (context, event) => {
+                    return event.data;
+                  },
+                }),
+                initial: 'busy',
+                on: {
+                  LOAD_EVALUATION: {
+                    target: 'busy',
+                    actions: ['updateEvaluationQueryParameter'],
+                  },
+                },
+                states: {
+                  busy: {
+                    invoke: {
+                      src: 'loadRelatedEvaluations',
+                      onDone: 'successRelatedEvaluations',
+                      onError: 'errorRelatedEvaluations',
+                    },
+                  },
+                  successRelatedEvaluations: {},
+                  errorRelatedEvaluations: {
+                    on: {
+                      RETRY: 'busy',
+                    },
+                  },
+                },
+              },
+              error: {
+                on: {
+                  RETRY: 'busy',
+                },
+              },
+            },
+            on: {
+              MODAL_CLOSE: 'close',
+            },
+          },
+          close: {
+            on: {
+              LOAD_EVALUATION: {
+                target: 'open',
+                actions: ['updateEvaluationQueryParameter'],
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    services: {
+      async loadEvaluations() {
+        return;
+      },
+      async loadEvaluation() {},
+      async loadRelatedEvaluations() {
+        return;
+      },
+    },
+    guards: {
+      sidebarIsOpen() {
+        return false;
+      },
+      hasData() {
+        return true;
+      },
+    },
+    actions: {
+      updateEvaluationQueryParameter() {},
+      removeCurrentEvaluationQueryParameter() {},
+    },
+  }
+);
