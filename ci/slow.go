bca014b2cb4b8f7587641dfd81b07abd9fc79e09
Seth Hoenig <seth.a.hoenig@gmail.com>
2022-03-16 08:38:42 -0500

    ci: explain why ci runs tests in serial now

--- ci/slow.go
+++ ci/slow.go
@@ -16,6 +16,9 @@ func SkipSlow(t *testing.T, reason string) {
 }
 
 // Parallel runs t in parallel, unless CI is set to a true value.
+//
+// In CI (CircleCI / GitHub Actions) we get better performance by running tests
+// in serial while not restricting GOMAXPROCS.
 func Parallel(t *testing.T) {
 	value := os.Getenv("CI")
 	isCI, err := strconv.ParseBool(value)
