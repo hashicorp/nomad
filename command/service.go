9944777919885a0c3dce33430151a2b2f9d86c22
James Rasell <jrasell@hashicorp.com>
2022-03-21 11:59:03 +0100

    cli: add service commands for list, info, and delete.

---
+++ command/service.go
@@ -0,0 +1,40 @@
+package command
+
+import (
+	"strings"
+
+	"github.com/mitchellh/cli"
+)
+
+type ServiceCommand struct {
+	Meta
+}
+
+func (c *ServiceCommand) Help() string {
+	helpText := `
+Usage: nomad service <subcommand> [options]
+
+  This command groups subcommands for interacting with the services API.
+
+  List services:
+
+      $ nomad service list
+
+  Detail an individual service:
+
+      $ nomad service info <service_name>
+
+  Delete an individual service registration:
+
+      $ nomad service delete <service_name> <service_id>
+
+  Please see the individual subcommand help for detailed usage information.
+`
+	return strings.TrimSpace(helpText)
+}
+
+func (c *ServiceCommand) Name() string { return "service" }
+
+func (c *ServiceCommand) Synopsis() string { return "Interact with registered services" }
+
+func (c *ServiceCommand) Run(_ []string) int { return cli.RunResultHelp }
