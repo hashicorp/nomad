4066321ac14b299363683c5f140eeb3c3efac091
Luiz Aoqui <luiz@hashicorp.com>
2022-04-08 12:04:14 -0400

    add Nomad v1.3.0-beta.1 download box (#12517)

--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -3,9 +3,23 @@ import { productSlug } from 'data/metadata'
 import ProductDownloadsPage from '@hashicorp/react-product-downloads-page'
 import { generateStaticProps } from '@hashicorp/react-product-downloads-page/server'
 import baseProps from 'components/downloads-props'
+import s from './style.module.css'
 
 export default function DownloadsPage(staticProps) {
-  return <ProductDownloadsPage {...baseProps()} {...staticProps} />
+  return <ProductDownloadsPage
+    {...baseProps()}
+    merchandisingSlot={
+      <div className={s.releaseCandidate}>
+        <p>
+          A beta for Nomad v1.3.0 is available! The release can be{' '}
+          <a href="https://releases.hashicorp.com/nomad/1.3.0-beta.1/">
+          downloaded here.
+          </a>
+        </p>
+      </div>
+    }
+    {...staticProps}
+  />
 }
 
 export async function getStaticProps() {
