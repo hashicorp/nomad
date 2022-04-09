ba3924164fa85dc0add4346ed4251092ffa00108
DerekStrickland <dstrickland@hashicorp.com>
2022-04-08 22:15:42 -0400

    initial checkin

---
+++ scheduler/task_group_reconciler.go
@@ -0,0 +1,239 @@
+package scheduler
+
+import (
+	"time"
+
+	log "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/nomad/nomad/structs"
+)
+
+type TaskGroupReconciler struct {
+	// logger is used to log debug information. Logging should be kept at a
+	// minimal here
+	logger        log.Logger
+	allocUpdateFn allocUpdateType
+
+	// job is the job being operated on, it may be nil if the job is being
+	// stopped via a purge
+	job *structs.Job
+
+	// jobID is the ID of the job being operated on. The job may be nil if it is
+	// being stopped, so we require this separately.
+	jobID string
+
+	// isBatchJob indicates whether the job is a batch job. The job may be nil if it is
+	//	// being stopped, so we require this separately.
+	isBatchJob bool
+
+	// lastDeployment is the last deployment for the job
+	lastDeployment *structs.Deployment
+
+	// deployment is the current deployment for the job
+	deployment *structs.Deployment
+
+	// taintedNodes contains a map of nodes that are tainted
+	taintedNodes map[string]*structs.Node
+
+	// existingAllocs is non-terminal existing allocations
+	existingAllocs []*structs.Allocation
+
+	// evalID and evalPriority is the ID and Priority of the evaluation that
+	// triggered the reconciler.
+	evalID       string
+	evalPriority int
+
+	// supportsDisconnectedClients indicates whether all servers meet the required
+	// minimum version to allow application of max_client_disconnect configuration.
+	supportsDisconnectedClients bool
+
+	// now is the time used when determining rescheduling eligibility
+	// defaults to time.Now, and overridden in unit tests
+	now time.Time
+
+	// result is the results of the reconciliation.
+	result *reconcileResults
+
+	// allocSlots holds the set of available slots equal to the task group count
+	// and are responsible for determining how to fill the slot.
+	allocSlots map[string]*allocSlot
+}
+
+func NewTaskGroupReconciler(logger log.Logger, allocUpdateFn allocUpdateType, isBatchJob bool,
+	jobID string, job *structs.Job, deployment *structs.Deployment, existingAllocs []*structs.Allocation, taintedNodes map[string]*structs.Node, evalID string,
+	evalPriority int, supportsDisconnectedClients bool) *TaskGroupReconciler {
+	tgr := &TaskGroupReconciler{
+		logger:                      logger.Named("task_group_reconciler"),
+		allocUpdateFn:               allocUpdateFn,
+		job:                         job,
+		jobID:                       jobID,
+		isBatchJob:                  isBatchJob,
+		deployment:                  deployment.Copy(),
+		existingAllocs:              existingAllocs,
+		taintedNodes:                taintedNodes,
+		evalID:                      evalID,
+		evalPriority:                evalPriority,
+		supportsDisconnectedClients: supportsDisconnectedClients,
+		now:                         time.Now(),
+		allocSlots: map[string]*allocSlot{},
+		result: &reconcileResults{
+			attributeUpdates:     make(map[string]*structs.Allocation),
+			disconnectUpdates:    make(map[string]*structs.Allocation),
+			reconnectUpdates:     make(map[string]*structs.Allocation),
+			desiredTGUpdates:     make(map[string]*structs.DesiredUpdates),
+			desiredFollowupEvals: make(map[string][]*structs.Evaluation),
+		},
+	}
+
+	for _, taskGroup := range tgr.job.TaskGroups {
+		allocs := make(allocSet)
+		for _, alloc := range existingAllocs {
+			if alloc.TaskGroup == taskGroup.Name {
+				allocs[alloc.ID] = alloc
+			}
+		}
+		tgr.allocSlots = newAllocSlots(tgr.jobID, taskGroup, allocs)
+	}
+
+	return tgr
+}
+
+func (tgr *TaskGroupReconciler) DeploymentPaused() bool {
+	if tgr.deployment != nil {
+		return tgr.deployment.Status == structs.DeploymentStatusPaused ||
+			tgr.deployment.Status == structs.DeploymentStatusPending
+	}
+	return true
+}
+
+func (tgr *TaskGroupReconciler) DeploymentFailed() bool {
+	if tgr.deployment != nil {
+		return tgr.deployment.Status == structs.DeploymentStatusFailed
+	}
+	// TODO (derek): what should it be if deployment nil?
+	return false
+}
+
+func (tgr *TaskGroupReconciler) BuildResult() {
+	tgr.result = &reconcileResults{
+		deployment:           tgr.deployment,
+		deploymentUpdates:    []*structs.DeploymentStatusUpdate{},
+		place:                []allocPlaceResult{},
+		destructiveUpdate:    []allocDestructiveResult{},
+		inplaceUpdate:        []*structs.Allocation{},
+		stop:                 []allocStopResult{},
+		attributeUpdates:     map[string]*structs.Allocation{},
+		disconnectUpdates:    map[string]*structs.Allocation{},
+		reconnectUpdates:     map[string]*structs.Allocation{},
+		desiredTGUpdates:     map[string]*structs.DesiredUpdates{},
+		desiredFollowupEvals: map[string][]*structs.Evaluation{},
+	}
+
+	for _, slot := range tgr.allocSlots {
+		tgr.result.deploymentUpdates = append(tgr.result.deploymentUpdates, slot.DeploymentUpdates()...)
+		tgr.result.place = append(tgr.result.place, slot.Place()...)
+		tgr.result.destructiveUpdate = append(tgr.result.destructiveUpdate, slot.DestructiveUpdates()...)
+		tgr.result.inplaceUpdate = append(tgr.result.inplaceUpdate, slot.InplaceUpdates()...)
+		tgr.result.stop = append(tgr.result.stop, slot.Stop()...)
+		// TODO (derek): replace all this boilerplate with generics once we update go version.
+		tgr.result.attributeUpdates = mergeAllocMaps(tgr.result.attributeUpdates, slot.AttributeUpdates())
+		tgr.result.disconnectUpdates = mergeAllocMaps(tgr.result.disconnectUpdates, slot.DisconnectUpdates())
+		tgr.result.reconnectUpdates = mergeAllocMaps(tgr.result.reconnectUpdates, slot.ReconnectUpdates())
+	}
+}
+
+type allocSlot struct {
+	Name       string
+	Index      int
+	TaskGroup  *structs.TaskGroup
+	Candidates allocSet
+}
+
+func newAllocSlots(jobID string, taskGroup *structs.TaskGroup, allocs allocSet) map[string]*allocSlot {
+	slots := map[string]*allocSlot{}
+	nameIndex := newAllocNameIndex(jobID, taskGroup.Name, taskGroup.Count, allocs)
+
+	for index, name := range nameIndex.Next(uint(taskGroup.Count)) {
+		slot := &allocSlot{
+			Name:       name,
+			Index:      index,
+			TaskGroup:  taskGroup,
+			Candidates: allocs.filterByName(name),
+		}
+		slots[slot.Name] = slot
+	}
+
+	return slots
+}
+
+func (as *allocSlot) Place() []allocPlaceResult {
+	return nil
+}
+
+func (as *allocSlot) Stop() []allocStopResult {
+	return nil
+}
+
+func (as *allocSlot) DeploymentUpdates() []*structs.DeploymentStatusUpdate {
+	return nil
+}
+
+func (as *allocSlot) DestructiveUpdates() []allocDestructiveResult {
+	return nil
+}
+
+func (as *allocSlot) InplaceUpdates() []*structs.Allocation {
+	return nil
+}
+
+func (as *allocSlot) AttributeUpdates() map[string]*structs.Allocation {
+	return nil
+}
+
+func (as *allocSlot) DisconnectUpdates() map[string]*structs.Allocation {
+	return nil
+}
+
+func (as *allocSlot) ReconnectUpdates() map[string]*structs.Allocation {
+	return nil
+}
+
+func (as *allocSlot) DesiredTGUpdates() map[string]*structs.DesiredUpdates {
+	return nil
+}
+
+func (as *allocSlot) DesiredFollowupEvals() map[string][]*structs.Evaluation {
+	return nil
+}
+
+func mergeAllocMaps(m map[string]*structs.Allocation, n map[string]*structs.Allocation) map[string]*structs.Allocation {
+	if len(m) == 0 && len(n) == 0 {
+		return map[string]*structs.Allocation{}
+	}
+	if len(m) == 0 {
+		return n
+	}
+	if len(n) == 0 {
+		return m
+	}
+
+	result := copyMapStringAlloc(m)
+
+	for k, v := range n {
+		result[k] = v
+	}
+
+	return result
+}
+
+func copyMapStringAlloc(m map[string]*structs.Allocation) map[string]*structs.Allocation {
+	l := len(m)
+	if l == 0 {
+		return nil
+	}
+
+	c := make(map[string]*structs.Allocation, l)
+	for k, v := range m {
+		c[k] = v
+	}
+	return c
+}
---
+++ scheduler/task_group_reconciler_test.go
@@ -0,0 +1,135 @@
+package scheduler
+
+import (
+	"testing"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/nomad/helper/uuid"
+	"github.com/hashicorp/nomad/nomad/mock"
+	"github.com/hashicorp/nomad/nomad/structs"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTaskGroupReconciler_BuildsCandidates_ByAllocName(t *testing.T) {
+	logger := hclog.NewInterceptLogger(hclog.DefaultOptions)
+	ready1 := mock.Node()
+	ready1.Name = "ready1"
+	ready1.Status = structs.NodeStatusReady
+
+	ready2 := mock.Node()
+	ready2.Name = "ready2"
+	ready2.Status = structs.NodeStatusReady
+
+	ready3 := mock.Node()
+	ready3.Name = "ready3"
+	ready3.Status = structs.NodeStatusReady
+
+	disconnected := mock.Node()
+	disconnected.Name = "disconnected"
+	disconnected.Status = structs.NodeStatusDisconnected
+
+	// nodes := map[string]*structs.Node{
+	// 	ready1.ID: ready1,
+	// 	ready2.ID: ready2,
+	// 	ready3.ID: ready3,
+	//  disconnected.ID: disconnected,
+	// }
+
+	running := structs.AllocClientStatusRunning
+	complete := structs.AllocClientStatusComplete
+	failed := structs.AllocClientStatusFailed
+	unknown := structs.AllocClientStatusUnknown
+	pending := structs.AllocClientStatusPending
+	run := structs.AllocDesiredStatusRun
+	stop := structs.AllocDesiredStatusStop
+
+	job := mock.Job()
+	job.TaskGroups[0].Count = 3
+	updatedJob := job.Copy()
+	updatedJob.Version = updatedJob.Version + 1
+
+	// jobs := map[string]*structs.Job{
+	// 	job.ID:        job,
+	// 	updatedJob.ID: updatedJob,
+	// }
+
+	type testCase struct {
+		name   string
+		allocs []*structs.Allocation
+	}
+
+	testCases := []testCase{
+		{
+			name: "single-job-version",
+			allocs: []*structs.Allocation{
+				{Name: "my-job.web[0]", NodeID: ready1.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[1]", NodeID: ready2.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[2]", NodeID: ready3.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+			},
+		},
+		{
+			name: "multiple-job-versions",
+			allocs: []*structs.Allocation{
+				{Name: "my-job.web[0]", NodeID: ready1.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[1]", NodeID: ready2.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[2]", NodeID: ready3.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[0]", NodeID: ready1.ID, Job: updatedJob, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[1]", NodeID: ready2.ID, Job: updatedJob, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[2]", NodeID: ready3.ID, Job: updatedJob, ClientStatus: running, DesiredStatus: run},
+			},
+		},
+		{
+			name: "complete-and-failed",
+			allocs: []*structs.Allocation{
+				{Name: "my-job.web[0]", NodeID: ready1.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[1]", NodeID: ready2.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[2]", NodeID: ready3.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[0]", NodeID: ready1.ID, Job: job, ClientStatus: complete, DesiredStatus: stop},
+				{Name: "my-job.web[1]", NodeID: ready2.ID, Job: job, ClientStatus: complete, DesiredStatus: stop},
+				{Name: "my-job.web[2]", NodeID: ready3.ID, Job: job, ClientStatus: complete, DesiredStatus: stop},
+				{Name: "my-job.web[0]", NodeID: ready1.ID, Job: job, ClientStatus: failed, DesiredStatus: run},
+				{Name: "my-job.web[1]", NodeID: ready2.ID, Job: job, ClientStatus: failed, DesiredStatus: run},
+				{Name: "my-job.web[2]", NodeID: ready3.ID, Job: job, ClientStatus: failed, DesiredStatus: run},
+			},
+		},
+		{
+			name: "unknown-and-pending",
+			allocs: []*structs.Allocation{
+				{Name: "my-job.web[0]", NodeID: ready1.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[1]", NodeID: ready2.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[2]", NodeID: ready3.ID, Job: job, ClientStatus: running, DesiredStatus: run},
+				{Name: "my-job.web[0]", NodeID: disconnected.ID, Job: job, ClientStatus: unknown, DesiredStatus: run},
+				{Name: "my-job.web[1]", NodeID: disconnected.ID, Job: job, ClientStatus: unknown, DesiredStatus: run},
+				{Name: "my-job.web[2]", NodeID: disconnected.ID, Job: job, ClientStatus: unknown, DesiredStatus: run},
+				{Name: "my-job.web[0]", NodeID: ready1.ID, Job: job, ClientStatus: pending, DesiredStatus: run},
+				{Name: "my-job.web[1]", NodeID: ready2.ID, Job: job, ClientStatus: pending, DesiredStatus: run},
+				{Name: "my-job.web[2]", NodeID: ready3.ID, Job: job, ClientStatus: pending, DesiredStatus: run},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			for _, alloc := range tc.allocs {
+				alloc.ID = uuid.Generate()
+				alloc.JobID = alloc.Job.ID
+			}
+
+			reconciler := NewTaskGroupReconciler(logger, allocUpdateFnDestructive, false, updatedJob.ID, updatedJob,
+				structs.NewDeployment(updatedJob, 50), tc.allocs, nil, uuid.Generate(), 50, true)
+
+			slots := reconciler.allocSlots[updatedJob.TaskGroups[0].Name]
+			require.Len(t, slots, 3)
+
+			for name, slot := range slots {
+				allocCountByName := 0
+				for _, alloc := range tc.allocs {
+					if alloc.Name == name {
+						allocCountByName++
+					}
+				}
+				require.Len(t, slot.Candidates, allocCountByName)
+			}
+		})
+	}
+}
