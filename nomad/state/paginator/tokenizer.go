c1a73bcadbd03245653f218775d67f2123b19dc8
James Rasell <jrasell@hashicorp.com>
2022-04-12 10:08:56 +0100

    paginator: minor updates to comment and func signature.

--- nomad/state/paginator/tokenizer.go
+++ nomad/state/paginator/tokenizer.go
@@ -45,7 +45,7 @@ type StructsTokenizer struct {
 }
 
 // NewStructsTokenizer returns a new StructsTokenizer.
-func NewStructsTokenizer(it Iterator, opts StructsTokenizerOptions) StructsTokenizer {
+func NewStructsTokenizer(_ Iterator, opts StructsTokenizerOptions) StructsTokenizer {
 	return StructsTokenizer{
 		opts: opts,
 	}
