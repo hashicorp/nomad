e863322b7a6458e3a9f3b5c96e2261587f4f110b
Jai Bhagat <jaybhagat841@gmail.com>
2022-04-15 18:41:06 -0400

    refact:  use filter expression dsl instead of named params

--- ui/app/routes/evaluations/index.js
+++ ui/app/routes/evaluations/index.js
@@ -37,20 +37,51 @@ export default class EvaluationsIndexRoute extends Route {
     type,
     qpNamespace: namespace,
   }) {
-    const generateFilter = () => {
+    const generateFilterExpression = () => {
       const searchFilter = searchTerm
         ? `ID contains "${searchTerm}" or JobID contains "${searchTerm}" or NodeID contains "${searchTerm}" or TriggeredBy contains "${searchTerm}"`
         : null;
       const typeFilter =
         type === 'client' ? `NodeID is not empty` : `NodeID is empty`;
       const triggeredByFilter = `TriggeredBy contains "${triggeredBy}"`;
+      const statusFilter = `Status contains "${status}"`;
 
-      if (searchTerm && type) return `${searchFilter} ${typeFilter}`;
-      if (searchTerm && triggeredBy)
-        return `${searchFilter} ${triggeredByFilter}`;
-      if (searchTerm) return searchFilter;
-      if (type) return typeFilter;
-      if (triggeredBy) return triggeredByFilter;
+      let filterExp;
+      if (searchTerm) {
+        if (!type && !status && !triggeredBy) {
+          return searchFilter;
+        }
+        filterExp = `(${searchFilter})`;
+        if (type) {
+          filterExp = `${filterExp} and ${typeFilter}`;
+        }
+        if (triggeredBy) {
+          filterExp = `${filterExp} and ${triggeredByFilter}`;
+        }
+        if (status) {
+          filterExp = `${filterExp} and ${statusFilter}`;
+        }
+        return filterExp;
+      }
+
+      if (type || status || triggeredBy) {
+        const lookup = {
+          [type]: typeFilter,
+          [status]: statusFilter,
+          [triggeredBy]: triggeredByFilter,
+        };
+
+        filterExp = [type, status, triggeredBy].reduce((result, filter) => {
+          const expression = lookup[filter];
+          if (!!filter && result !== '') {
+            result = result.concat(` and ${expression}`);
+          } else if (filter) {
+            result = expression;
+          }
+          return result;
+        }, '');
+        return filterExp;
+      }
 
       return null;
     };
@@ -61,8 +92,7 @@ export default class EvaluationsIndexRoute extends Route {
       namespace,
       per_page: pageSize,
       next_token: nextToken,
-      status,
-      filter: generateFilter(),
+      filter: generateFilterExpression(),
     });
   }
 }
