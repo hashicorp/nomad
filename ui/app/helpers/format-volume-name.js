a977577e445bfd6d5c5ff76c97953cb49c441276
Phil Renaud <phil.renaud@hashicorp.com>
2022-04-21 13:57:18 -0400

    [ui, bugfix] Link fix for volumes where per_alloc=true (#12713)
    * Allocation page linkfix
    
    * fix added to task page and computed prop moved to allocation model
    
    * Fallback query added to task group when specific volume isnt knowable
    
    * Delog
    
    * link text reflects alloc suffix
    
    * Helper instead of in-template conditionals
    
    * formatVolumeName unit test
    
    * Removing unused helper import

---
+++ ui/app/helpers/format-volume-name.js
@@ -0,0 +1,18 @@
+import { helper } from '@ember/component/helper';
+
+/**
+ * Volume Name Formatter
+ *
+ * Usage: {{format-volume-name source=string isPerAlloc=boolean volumeExtension=string}}
+ *
+ * Outputs a title/link for volumes that are per_alloc-aware.
+ * (when a volume is per_alloc, its route location requires an additional extension)
+ */
+export function formatVolumeName(
+  _,
+  { source = '', isPerAlloc, volumeExtension }
+) {
+  return `${source}${isPerAlloc ? volumeExtension : ''}`;
+}
+
+export default helper(formatVolumeName);
---
+++ ui/tests/unit/helpers/format-volume-name-test.js
@@ -0,0 +1,39 @@
+import { module, test } from 'qunit';
+import { formatVolumeName } from 'nomad-ui/helpers/format-volume-name';
+
+module('Unit | Helper | formatVolumeName', function () {
+  test('Returns source as string when isPerAlloc is false', function (assert) {
+    const expectation = 'my-volume-source';
+    assert.equal(
+      formatVolumeName(null, {
+        source: 'my-volume-source',
+        isPerAlloc: false,
+        volumeExtension: '[arbitrary]',
+      }),
+      expectation,
+      'false perAlloc'
+    );
+    assert.equal(
+      formatVolumeName(null, {
+        source: 'my-volume-source',
+        isPerAlloc: null,
+        volumeExtension: '[arbitrary]',
+      }),
+      expectation,
+      'null perAlloc'
+    );
+  });
+
+  test('Returns concatonated name when isPerAlloc is true', function (assert) {
+    const expectation = 'my-volume-source[1]';
+    assert.equal(
+      formatVolumeName(null, {
+        source: 'my-volume-source',
+        isPerAlloc: true,
+        volumeExtension: '[1]',
+      }),
+      expectation,
+      expectation
+    );
+  });
+});
