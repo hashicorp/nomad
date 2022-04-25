fc2c21423d666ee73e7014bc38a6c8533e1bbe2d
Tim Gross <tgross@hashicorp.com>
2022-04-25 11:50:31 -0400

    CSI: plugin config updates should always be destructive

--- scheduler/util.go
+++ scheduler/util.go
@@ -551,6 +551,9 @@ func tasksUpdated(jobA, jobB *structs.Job, taskGroup string) bool {
 		if !reflect.DeepEqual(at.Templates, bt.Templates) {
 			return true
 		}
+		if !reflect.DeepEqual(at.CSIPluginConfig, bt.CSIPluginConfig) {
+			return true
+		}
 
 		// Check the metadata
 		if !reflect.DeepEqual(
