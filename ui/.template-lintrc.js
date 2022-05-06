d9bc67600c803e8aa8b753e7f7ff7e968910f293
Phil Renaud <phil@riotindustries.com>
2022-05-06 15:48:52 -0400

    helper-based initializer and teardown for new contextual commands

--- ui/.template-lintrc.js
+++ ui/.template-lintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'no-action': 'off',
     'no-invalid-interactive': 'off',
     'no-inline-styles': 'off',
-    'no-curly-component-invocation': { allow: ['format-volume-name'] },
+    'no-curly-component-invocation': { allow: ['format-volume-name', 'keyboard-commands'] },
+    'no-implicit-this': { allow: ['keyboard-commands'] },
   },
 };
