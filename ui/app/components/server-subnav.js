4e17c6960843b67707a95600a0b6e4b86cfa4fc7
Phil Renaud <phil@riotindustries.com>
2022-05-09 13:31:49 -0400

    Register and unregister methods for subnav plus new subnavs for volumes and volume

--- ui/app/components/server-subnav.js
+++ ui/app/components/server-subnav.js
@@ -1,5 +1,8 @@
 import Component from '@ember/component';
 import { tagName } from '@ember-decorators/component';
+import { inject as service } from '@ember/service';
 
 @tagName('')
-export default class ServerSubnav extends Component {}
+export default class ServerSubnav extends Component {
+  @service keyboard;
+}
---
+++ ui/app/components/storage-subnav.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+export default class StorageSubnavComponent extends Component {
+  @service keyboard;
+}
