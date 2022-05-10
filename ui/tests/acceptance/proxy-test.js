76af28aa02a131bfdb4c86e660769e6f1908e1e0
Georges-Etienne Legendre <georges-etienne.legendre@bell.ca>
2022-05-10 11:37:07 -0400

    Fix lint errors

--- ui/tests/acceptance/proxy-test.js
+++ ui/tests/acceptance/proxy-test.js
@@ -18,7 +18,8 @@ module('Acceptance | reverse proxy', function (hooks) {
     managementToken = server.create('token');
 
     // Prepare a setRequestHeader that accumulate headers already set. This is to avoid double setting X-Nomad-Token
-    this._originalXMLHttpRequestSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
+    this._originalXMLHttpRequestSetRequestHeader =
+      XMLHttpRequest.prototype.setRequestHeader;
     (function (setRequestHeader) {
       XMLHttpRequest.prototype.setRequestHeader = function (header, value) {
         if (!this.headers) {
@@ -39,14 +40,15 @@ module('Acceptance | reverse proxy', function (hooks) {
       XMLHttpRequest.prototype.send = function (data) {
         if (!this.headers || !('X-Nomad-Token' in this.headers)) {
           this.setRequestHeader('X-Nomad-Token', managementToken.secretId);
-        } 
+        }
         send.call(this, data);
       };
     })(this._originalXMLHttpRequestSend);
   });
 
   hooks.afterEach(function () {
-    XMLHttpRequest.prototype.setRequestHeader = this._originalXMLHttpRequestSetRequestHeader;
+    XMLHttpRequest.prototype.setRequestHeader =
+      this._originalXMLHttpRequestSetRequestHeader;
     XMLHttpRequest.prototype.send = this._originalXMLHttpRequestSend;
   });
 
