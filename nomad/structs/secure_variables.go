ff7c517f486efb01e7d61a91809e3382104676d2
Tim Gross <tgross@hashicorp.com>
2022-05-10 10:29:40 -0400

    sv: core SecureVariable and RootKey structs

---
+++ nomad/structs/secure_variables.go
@@ -0,0 +1,68 @@
+package structs
+
+import "time"
+
+// SecureVariable is the metadata envelope for a Secure Variable
+type SecureVariable struct {
+	Namespace   string
+	Path        string
+	CreatedAt   time.Time
+	CreateIndex uint64
+	ModifyIndex uint64
+	ModifyTime  time.Time
+
+	// reserved for post-1.4.0 work
+	// LockIndex      uint64
+	// Session        string
+	// DeletedAt      time.Time
+	// Version        uint64
+	// CustomMetaData map[string]string
+
+	EncryptedData   *SecureVariableData // removed during serialization
+	UnencryptedData map[string]string   // empty until serialized
+}
+
+// SecureVariableData is the secret data for a Secure Variable
+type SecureVariableData struct {
+	Data  []byte // includes nonce
+	KeyID string // ID of root key used to encrypt this entry
+}
+
+// SecureVariablesQuota is used to track the total size of secure
+// variables entries per namespace. The total length of
+// SecureVariable.EncryptedData will be added to the SecureVariablesQuota
+// table in the same transaction as a write, update, or delete.
+type SecureVariablesQuota struct {
+	Namespace   string
+	Size        uint64
+	CreateIndex uint64
+	ModifyIndex uint64
+}
+
+// RootKey is used to encrypt and decrypt secure variables. It is
+// never stored in raft.
+type RootKey struct {
+	Meta RootKeyMeta
+	Key  []byte // serialized to keystore as base64 blob
+}
+
+// RootKeyMeta is the metadata used to refer to a RootKey. It is
+// stored in raft.
+type RootKeyMeta struct {
+	Active           bool
+	KeyID            string // UUID
+	Algorithm        EncryptionAlgorithm
+	EncryptionsCount uint64
+	CreatedAt        time.Time
+	CreateIndex      uint64
+	ModifyIndex      uint64
+}
+
+// EncryptionAlgorithm chooses which algorithm is used for
+// encrypting / decrypting entries with this key
+type EncryptionAlgorithm string
+
+const (
+	EncryptionAlgorithmXChaCha20 EncryptionAlgorithm = "xchacha20"
+	EncryptionAlgorithmAES256GCM EncryptionAlgorithm = "aes256-gcm"
+)
