9db49c17f7266385b836e62f9b936888bbd30256
Georges-Etienne Legendre <georges-etienne.legendre@bell.ca>
2022-05-10 13:42:12 -0400

    Fix Exec not working with reverse proxy X-Nomad-Token (#12925)
    * Capture token secret on fetch
    
    * Fix tests
    
    * Fix lint errors

--- ui/tests/acceptance/proxy-test.js
+++ ui/tests/acceptance/proxy-test.js
@@ -17,17 +17,36 @@ module('Acceptance | reverse proxy', function(hooks) {
     server.create('agent');
     managementToken = server.create('token');
 
+    // Prepare a setRequestHeader that accumulate headers already set. This is to avoid double setting X-Nomad-Token
+    this._originalXMLHttpRequestSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
+    (function(setRequestHeader) {
+      XMLHttpRequest.prototype.setRequestHeader = function(header, value) {
+        if (!this.headers) {
+          this.headers = {};
+        }
+        if (!this.headers[header]) {
+          this.headers[header] = [];
+        }
+        // Add the value to the header
+        this.headers[header].push(value);
+        setRequestHeader.call(this, header, value);
+      };
+    })(this._originalXMLHttpRequestSetRequestHeader);
+
     // Simulate a reverse proxy injecting X-Nomad-Token header for all requests
     this._originalXMLHttpRequestSend = XMLHttpRequest.prototype.send;
     (function(send) {
       XMLHttpRequest.prototype.send = function(data) {
-        this.setRequestHeader('X-Nomad-Token', managementToken.secretId);
+        if (!this.headers || !('X-Nomad-Token' in this.headers)) {
+          this.setRequestHeader('X-Nomad-Token', managementToken.secretId);
+        }
         send.call(this, data);
       };
     })(this._originalXMLHttpRequestSend);
   });
 
   hooks.afterEach(function() {
+    XMLHttpRequest.prototype.setRequestHeader = this._originalXMLHttpRequestSetRequestHeader;
     XMLHttpRequest.prototype.send = this._originalXMLHttpRequestSend;
   });
 
@@ -36,7 +55,7 @@ module('Acceptance | reverse proxy', function(hooks) {
     const { secretId } = managementToken;
 
     await Jobs.visit();
-    assert.ok(window.localStorage.nomadTokenSecret == null, 'No token secret set');
+    assert.equal(window.localStorage.nomadTokenSecret, secretId, 'Token secret was set');
 
     // Make sure that server received the header
     assert.ok(
