c7349b2455670c9e446729e06ffd2097ea1a57bc
Jorge Marey <jorge.marey.oton.next@bbva.com>
2022-05-12 08:46:06 +0200

    Add nomad env to envoy bootstrap

--- client/allocrunner/taskrunner/envoy_bootstrap_hook.go
+++ client/allocrunner/taskrunner/envoy_bootstrap_hook.go
@@ -292,6 +292,8 @@ func (h *envoyBootstrapHook) Prestart(ctx context.Context, req *ifs.TaskPrestart
 
 	// Create environment
 	bootstrapEnv := bootstrap.env(os.Environ())
+	// append nomad environment variables to the bootstrap environment
+	bootstrapEnv = append(bootstrapEnv, h.groupEnv()...)
 
 	// Write env to file for debugging
 	envFile, err := os.Create(bootstrapEnvPath)
@@ -375,6 +377,19 @@ func (h *envoyBootstrapHook) Prestart(ctx context.Context, req *ifs.TaskPrestart
 	}
 
 	return nil
+}
+
+func (h *envoyBootstrapHook) groupEnv() []string {
+	return []string{
+		fmt.Sprintf("%s=%s", taskenv.AllocID, h.alloc.ID),
+		fmt.Sprintf("%s=%s", taskenv.ShortAllocID, h.alloc.ID[:8]),
+		fmt.Sprintf("%s=%s", taskenv.AllocName, h.alloc.Name),
+		fmt.Sprintf("%s=%s", taskenv.GroupName, h.alloc.TaskGroup),
+		fmt.Sprintf("%s=%s", taskenv.JobName, h.alloc.Job.Name),
+		fmt.Sprintf("%s=%s", taskenv.JobID, h.alloc.Job.ID),
+		fmt.Sprintf("%s=%s", taskenv.Namespace, h.alloc.Namespace),
+		fmt.Sprintf("%s=%s", taskenv.Region, h.alloc.Job.Region),
+	}
 }
 
 // buildEnvoyAdminBind determines a unique port for use by the envoy admin listener.
