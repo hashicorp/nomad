73c10f0c5d7d56787fffbe18176f289d153ba378
Phil Renaud <phil@riotindustries.com>
2022-05-13 13:51:36 -0400

    Confirmed the volume equivalent and simplified idWIthNamespace logic

--- ui/app/models/job.js
+++ ui/app/models/job.js
@@ -37,13 +37,7 @@ export default class Job extends Model {
 
   @computed('plainId')
   get idWithNamespace() {
-    const namespaceId = this.belongsTo('namespace').id();
-
-    if (!namespaceId) {
-      return this.plainId;
-    } else {
-      return `${this.plainId}@${namespaceId}`;
-    }
+    return `${this.plainId}@${this.belongsTo('namespace').id()}`;
   }
 
   @computed('periodic', 'parameterized', 'dispatched')
