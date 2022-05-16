c8b86d0597b5515aa7e79b4e126db4bb6b70ca43
Phil Renaud <phil@riotindustries.com>
2022-05-16 12:26:46 -0400

    Page guard tests for secure variables

--- ui/tests/acceptance/secure-variables-test.js
+++ ui/tests/acceptance/secure-variables-test.js
@@ -2,28 +2,50 @@ import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import Variables from 'nomad-ui/tests/pages/variables';
 import a11yAudit from 'nomad-ui/tests/helpers/a11y-audit';
+import defaultScenario from '../../mirage/scenarios/default';
+
+import Variables from 'nomad-ui/tests/pages/variables';
+import Layout from 'nomad-ui/tests/pages/layout';
+
+const SECURE_TOKEN_ID = '53cur3-v4r14bl35';
 
 module('Acceptance | secure variables', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('it redirects to jobs and hides the gutter link when the token lacks permissions', async function (assert) {
-    await Variables.visit();
-    assert.equal(currentURL(), '/jobs');
+  module('Guarding page access', function () {
+    test('it redirects to jobs and hides the gutter link when the token lacks permissions', async function (assert) {
+      await Variables.visit();
+      assert.equal(currentURL(), '/jobs');
+      assert.ok(Layout.gutter.variables.isHidden);
+    });
+
+    test('it allows access for management level tokens', async function (assert) {
+      defaultScenario(server);
+      window.localStorage.nomadTokenSecret = server.db.tokens[0].secretId;
+
+      await Variables.visit();
+      assert.equal(currentURL(), '/variables');
+      assert.ok(Layout.gutter.variables.isVisible);
+    });
+
+    test('it allows access for list-variables allowed ACL rules', async function (assert) {
+      defaultScenario(server);
+      const variablesToken = server.db.tokens.find(SECURE_TOKEN_ID);
+      window.localStorage.nomadTokenSecret = variablesToken.secretId;
+
+      await Variables.visit();
+      assert.equal(currentURL(), '/variables');
+      assert.ok(Layout.gutter.variables.isVisible);
+    });
+
+    test('it passes an accessibility audit', async function (assert) {
+      defaultScenario(server);
+      const variablesToken = server.db.tokens.find(SECURE_TOKEN_ID);
+      window.localStorage.nomadTokenSecret = variablesToken.secretId;
+      await Variables.visit();
+      await a11yAudit(assert);
+    });
   });
-
-  // test('it passes an accessibility audit', async function (assert) {
-  //   await Variables.visit();
-  //   await a11yAudit(assert);
-  // });
-
-  // test('it redirects to jobs and hides the gutter link when the token lacks permissions', async function (assert) {
-  //   window.localStorage.nomadTokenSecret = clientToken.secretId;
-  //   await Optimize.visit();
-
-  //   assert.equal(currentURL(), '/jobs?namespace=*');
-  //   assert.ok(Layout.gutter.optimize.isHidden);
-  // });
 });
