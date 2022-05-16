607ce11582e2a165c59aecbc6319d2d535037765
Ivo <bonsi@users.noreply.github.com>
2022-05-16 12:49:01 +0200

    [terraform/aws] Fix NVidia GPG key error (#12985)
    * Fix NVidia GPG key error - NVidia rotated their repo keys, see https://forums.developer.nvidia.com/t/notice-cuda-linux-repository-key-rotation/212771

--- terraform/shared/scripts/setup.sh
+++ terraform/shared/scripts/setup.sh
@@ -32,8 +32,7 @@ CONSULTEMPLATEDIR=/opt/consul-template
 # Dependencies
 sudo apt-get install -y software-properties-common
 sudo apt-get update
-sudo apt-get install -y unzip tree redis-tools jq curl tmux
-
+sudo apt-get install -y unzip tree redis-tools jq curl tmux gnupg-curl
 
 # Disable the firewall
 
@@ -110,8 +109,10 @@ sudo apt-get install -y docker-ce
 
 # Needs testing, updating and fixing
 if [[ ! -z ${INSTALL_NVIDIA_DOCKER+x} ]]; then 
+  
   # Install official NVIDIA driver package
-  sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+  # This is why we added gnupg-curl, otherwise, the following fails with "gpgkeys: protocol `https' not supported"
+  sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/3bf863cc.pub
   sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
   sudo apt-get update && sudo apt-get install -y --no-install-recommends linux-headers-generic dkms cuda-drivers
 
