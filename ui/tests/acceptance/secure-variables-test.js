a9a18267f03030c002ab4cdb2b9967653e58d2a7
Phil Renaud <phil@riotindustries.com>
2022-05-16 13:36:09 -0400

    Set expectations for test assertions

--- ui/tests/acceptance/secure-variables-test.js
+++ ui/tests/acceptance/secure-variables-test.js
@@ -42,6 +42,7 @@ module('Acceptance | secure variables', function (hooks) {
     });
 
     test('it passes an accessibility audit', async function (assert) {
+      assert.expect(2);
       defaultScenario(server);
       const variablesToken = server.db.tokens.find(SECURE_TOKEN_ID);
       window.localStorage.nomadTokenSecret = variablesToken.secretId;
