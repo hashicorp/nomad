6d913a1e97645b2cb3f29fa702646c6055f03055
Phil Renaud <phil.renaud@hashicorp.com>
2022-05-17 14:52:14 -0400

    Secure Variables UI: Router setup and /variables/index route + guards (#12967)
    * Route init
    
    * Bones of a mirage-mocked secure variables policy
    
    * Functinoing policy for list vars
    
    * Delog and transition on route
    
    * Basic guard test
    
    * Page guard tests for secure variables
    
    * Cleanup and unit tests for variables ability
    
    * Linter cleanup
    
    * Set expectations for test assertions
    
    * PR feedback addressed
    
    * Read label changed to View per suggestion

---
+++ ui/app/abilities/variable.js
@@ -0,0 +1,19 @@
+import AbstractAbility from './abstract';
+import { computed, get } from '@ember/object';
+import { or } from '@ember/object/computed';
+
+export default class extends AbstractAbility {
+  @or(
+    'bypassAuthorization',
+    'selfTokenIsManagement',
+    'policiesSupportVariableView'
+  )
+  canList;
+
+  @computed('rulesForNamespace.@each.capabilities')
+  get policiesSupportVariableView() {
+    return this.rulesForNamespace.some((rules) => {
+      return get(rules, 'SecureVariables');
+    });
+  }
+}
