7d034cb9a0e3f785d94bd579f2178c9543fd464b
Phil Renaud <phil.renaud@hashicorp.com>
2022-05-17 11:45:28 -0400

    12762 table keynav (#12975)
    * Experimental feature: shortcut visual hints
    
    * Long way around to a custom modifier for keyboard shortcuts
    
    * dynamic table and list iterative shortcuts
    
    * Progress with regular old tether
    
    * Delogging
    
    * Table Keynav tether fix, server and client navs, and fix to shiftless on modified arrow keys

--- ui/app/components/keyboard-shortcuts-modal.js
+++ ui/app/components/keyboard-shortcuts-modal.js
@@ -1,16 +1,53 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { htmlSafe } from '@ember/template';
+import { computed } from '@ember/object';
+import Tether from 'tether';
 
 export default class KeyboardShortcutsModalComponent extends Component {
   @service keyboard;
 
-  keyCommands = [
-    {
-      label: 'Hide Keyboard Shortcuts',
-      pattern: ['Escape'],
-      action: () => {
-        this.keyboard.shortcutsVisible = false;
-      },
+  escapeCommand = {
+    label: 'Hide Keyboard Shortcuts',
+    pattern: ['Escape'],
+    action: () => {
+      this.keyboard.shortcutsVisible = false;
     },
-  ];
+  };
+
+  /**
+   * commands: filter keyCommands to those that have an action and a label,
+   * to distinguish between those that are just visual hints of existing commands
+   */
+  @computed('keyboard.keyCommands.length')
+  get commands() {
+    return this.keyboard.keyCommands.filter((c) => c.label && c.action);
+  }
+
+  /**
+   * hints: filter keyCommands to those that have an element property,
+   * and then compute a position on screen to place the hint.
+   */
+  @computed('keyboard.keyCommands.length', 'keyboard.displayHints')
+  get hints() {
+    if (this.keyboard.displayHints) {
+      return this.keyboard.keyCommands.filter((c) => c.element);
+    } else {
+      return [];
+    }
+  }
+
+  tetherToElement(self, _, { element, hint }) {
+    let binder = new Tether({
+      element: self,
+      target: element,
+      attachment: 'top left',
+      targetAttachment: 'top left',
+      targetModifier: 'visible',
+    });
+    hint.binder = binder;
+  }
+  untetherFromElement(self, _, { element, hint }) {
+    hint.binder.destroy();
+  }
 }
