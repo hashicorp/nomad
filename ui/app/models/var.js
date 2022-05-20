33ba92f847100cbf39e48f1ec09b9953b0d341f6
Phil Renaud <phil@riotindustries.com>
2022-05-19 22:58:11 -0400

    multiple KVs per variable

--- ui/app/models/var.js
+++ ui/app/models/var.js
@@ -5,8 +5,6 @@ import classic from 'ember-classic-decorator';
 @classic
 export default class VarModel extends Model {
   @attr('string') path;
-  // @attr() key;
-  // @attr('string') value;
   @attr('string') namespace;
   @attr() items;
 }
