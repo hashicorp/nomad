658e5dcf064c3e53b26bfca0f0279c7554740eb9
Phil Renaud <phil@riotindustries.com>
2022-05-26 14:56:12 -0400

    Autofocus modifier tests

--- ui/tests/integration/modifiers/autofocus-test.js
+++ ui/tests/integration/modifiers/autofocus-test.js
@@ -6,10 +6,63 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Modifier | autofocus', function (hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function (assert) {
-    await render(hbs`<div {{autofocus}}></div>`);
+  test('Basic Usage', async function (assert) {
+    await render(hbs`
+    <form>
+      <label>
+        <input data-test-input-1 {{autofocus}} />
+      </label>
+    </form>`);
 
-    assert.ok(true);
+    assert
+      .dom('[data-test-input-1]')
+      .isFocused('Autofocus on an element works');
+  });
+
+  test('Multiple foci', async function (assert) {
+    await render(hbs`
+    <form>
+      <label>
+        <input data-test-input-1 {{autofocus}} />
+      </label>
+      <label>
+        <input data-test-input-2 {{autofocus}} />
+      </label>
+    </form>`);
+
+    assert
+      .dom('[data-test-input-1]')
+      .isNotFocused('With multiple autofocus elements, priors are unfocused');
+    assert
+      .dom('[data-test-input-2]')
+      .isFocused('With multiple autofocus elements, posteriors are focused');
+  });
+
+  test('Ignore parameter', async function (assert) {
+    await render(hbs`
+    <form>
+    <label>
+        <input data-test-input-1 {{autofocus}} />
+      </label>
+      <label>
+        <input data-test-input-2 {{autofocus}} />
+      </label>
+      <label>
+        <input data-test-input-3 {{autofocus ignore=true}} />
+      </label>
+      <label>
+        <input data-test-input-4 {{autofocus ignore=true}} />
+      </label>
+    </form>`);
+
+    assert
+      .dom('[data-test-input-2]')
+      .isFocused('The last autofocus element without ignore is focused');
+    assert
+      .dom('[data-test-input-3]')
+      .isNotFocused('Ignore parameter is observed, prior');
+    assert
+      .dom('[data-test-input-4]')
+      .isNotFocused('Ignore parameter is observed, posterior');
   });
 });
