d25a5728fe7af1a96ba069e8bddcc301b3f6a788
Phil Renaud <phil@riotindustries.com>
2022-06-06 16:13:07 -0400

    Trim Path tests

--- ui/tests/integration/helpers/trim-path-test.js
+++ ui/tests/integration/helpers/trim-path-test.js
@@ -6,12 +6,24 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Helper | trim-path', function (hooks) {
   setupRenderingTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it renders', async function (assert) {
-    this.set('inputValue', '1234');
-
+  test('it doesnt mess with internal slashes', async function (assert) {
+    this.set('inputValue', 'a/b/c/d');
     await render(hbs`{{trim-path this.inputValue}}`);
-
-    assert.dom(this.element).hasText('1234');
+    assert.dom(this.element).hasText('a/b/c/d');
+  });
+  test('it will remove a prefix slash', async function (assert) {
+    this.set('inputValue', '/a/b/c/d');
+    await render(hbs`{{trim-path this.inputValue}}`);
+    assert.dom(this.element).hasText('a/b/c/d');
+  });
+  test('it will remove a suffix slash', async function (assert) {
+    this.set('inputValue', 'a/b/c/d/');
+    await render(hbs`{{trim-path this.inputValue}}`);
+    assert.dom(this.element).hasText('a/b/c/d');
+  });
+  test('it will remove both at once', async function (assert) {
+    this.set('inputValue', '/a/b/c/d/');
+    await render(hbs`{{trim-path this.inputValue}}`);
+    assert.dom(this.element).hasText('a/b/c/d');
   });
 });
