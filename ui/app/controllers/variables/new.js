3440835153a471035fc90c58d6732dfc1a8834b7
Phil Renaud <phil@riotindustries.com>
2022-06-06 13:29:49 -0400

    Slightly simpler path QP mods

--- ui/app/controllers/variables/new.js
+++ ui/app/controllers/variables/new.js
@@ -1,8 +1,4 @@
 import Controller from '@ember/controller';
 export default class VariablesNewController extends Controller {
-  queryParams = [
-    {
-      path: '',
-    },
-  ];
+  queryParams = ['path'];
 }
--- ui/app/routes/variables/new.js
+++ ui/app/routes/variables/new.js
@@ -5,6 +5,8 @@ export default class VariablesNewRoute extends Route {
     return this.store.createRecord('variable', { path: params.path });
   }
   resetController(controller, isExiting) {
+    // If the user navigates away from /new, clear the path
+    controller.set('path', null);
     if (isExiting) {
       // If user didn't save, delete the freshly created model
       if (controller.model.isNew) {
