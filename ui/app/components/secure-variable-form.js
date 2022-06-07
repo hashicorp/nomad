a708eff6ac165696cba4506b88d33b56475e49f6
Phil Renaud <phil@riotindustries.com>
2022-06-07 16:01:55 -0400

    Notify a user if they try to make a variable with an existing path

--- ui/app/components/secure-variable-form.js
+++ ui/app/components/secure-variable-form.js
@@ -2,11 +2,16 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { trimPath } from '../helpers/trim-path';
+
 export default class SecureVariableFormComponent extends Component {
   @service router;
+  @service store;
 
   @tracked
   shouldHideValues = true;
+
+  @tracked duplicatePathError = false;
 
   get valueFieldType() {
     return this.shouldHideValues ? 'password' : 'text';
@@ -14,6 +19,19 @@ export default class SecureVariableFormComponent extends Component {
 
   get shouldDisableSave() {
     return !this.args.model?.path;
+  }
+
+  @action
+  validatePath(e) {
+    const value = trimPath([e.target.value]);
+    let existingVariable = this.store
+      .peekAll('variable')
+      .find((v) => v.path === value);
+    if (existingVariable) {
+      this.duplicatePathError = { variable: existingVariable };
+    } else {
+      this.duplicatePathError = false;
+    }
   }
 
   @action
