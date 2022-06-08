680dd0edd6a316afe722be6d48ad2cf334625f25
Phil Renaud <phil.renaud@hashicorp.com>
2022-06-08 10:31:22 -0400

    Notify a user if they try to make a variable with an existing path (#13278)
    * Notify a user if they try to make a variable with an existing path
    
    * Stylize error fade
    
    * Bugfix: if you click the dupe link to a variable you havent previously loaded, you lack its keyvalues
    
    * rename and typefix for duplicate path warning

--- ui/app/routes/variables/variable/edit.js
+++ ui/app/routes/variables/variable/edit.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class VariablesVariableEditRoute extends Route {}
+export default class VariablesVariableEditRoute extends Route {
+  model() {
+    return this.modelFor('variables.variable');
+  }
+}
