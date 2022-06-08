017de4ae3f22f34cb9dde0bde2239d7912450132
Phil Renaud <phil@riotindustries.com>
2022-06-08 16:52:12 -0400

    variable index header and deletion hook

--- ui/app/controllers/variables/variable/index.js
+++ ui/app/controllers/variables/variable/index.js
@@ -1,3 +1,31 @@
 import Controller from '@ember/controller';
+import { task } from 'ember-concurrency';
+import messageForError from '../../../utils/message-from-adapter-error';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-export default class VariablesVariableIndexController extends Controller {}
+export default class VariablesVariableIndexController extends Controller {
+  @service router;
+
+  @tracked
+  error = null;
+
+  @task(function* () {
+    try {
+      yield this.model.deleteRecord();
+      yield this.model.save();
+      this.router.transitionTo('variables.index');
+      // TODO: alert the user that the variable was successfully deleted
+    } catch (err) {
+      this.error = {
+        title: 'Could Not Delete Variable',
+        description: messageForError(err, 'delete secure variables'),
+      };
+    }
+  })
+  deleteVariableFile;
+
+  onDismissError() {
+    this.error = null;
+  }
+}
