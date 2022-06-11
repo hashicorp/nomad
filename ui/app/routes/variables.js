cc102d40d618b1eef08690ad9b8a4e881c788c92
Phil Renaud <phil@riotindustries.com>
2022-06-11 12:40:00 -0400

    pre-PR cleanup

--- ui/app/routes/variables.js
+++ ui/app/routes/variables.js
@@ -17,7 +17,6 @@ export default class VariablesRoute extends Route.extend(WithForbiddenState) {
   async model() {
     try {
       const variables = await this.store.findAll('variable');
-      console.log('pathtree', new PathTree(variables));
       return {
         variables,
         pathTree: new PathTree(variables),
