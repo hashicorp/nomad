1d2950570e2c7c35e976b0fb1b065622f9672989
Luiz Aoqui <luiz@hashicorp.com>
2022-06-14 14:21:50 +0000

    backport of commit ac7d14bc85ada5e52c65e709c53deafe91f179a2

--- website/redirects.js
+++ website/redirects.js
@@ -1,6 +1,6 @@
 module.exports = [
   // Define your custom redirects within this file.
-  // Vercel's redirect documentation: https://vercel.com/docs/configuration#project/redirects
+  // Vercel's redirect documentation: https://nextjs.org/docs/api-reference/next.config.js/redirects
   // Relative paths with fragments (#) are not supported. For destinations with fragments use an absolute URL.
   // Playground for testing url pattern matching: https://npm.runkit.com/path-to-regexp
   {
