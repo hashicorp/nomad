678105486fb71a3aadbc2d5073488cecb847cffb
Phil Renaud <phil@riotindustries.com>
2022-06-17 13:26:08 -0400

    Path compaction tests

--- ui/tests/unit/utils/compact-path-test.js
+++ ui/tests/unit/utils/compact-path-test.js
@@ -1,10 +1,43 @@
 import compactPath from 'nomad-ui/utils/compact-path';
+import pathTree from 'nomad-ui/utils/path-tree';
 import { module, test } from 'qunit';
 
+const PATHSTRINGS = [
+  { path: 'a/b/c/d/e/foo0' },
+  { path: 'a/b/c/d/e/bar1' },
+  { path: 'a/b/c/d/e/baz2' },
+  { path: 'a/b/c/d/e/z/z/z/z/z/z/z/z/z/z/foo3' },
+  { path: 'z/y/x/dalmation/index' },
+  { path: 'z/y/x/doberman/index' },
+  { path: 'z/y/x/dachshund/index' },
+  { path: 'z/y/x/dachshund/poppy' },
+];
+
 module('Unit | Utility | compact-path', function () {
-  // TODO: Replace this with your real tests.
-  test('it works', function (assert) {
-    let result = compactPath();
-    assert.ok(result);
+  test('it compacts empty folders correctly', function (assert) {
+    const tree = new pathTree(PATHSTRINGS);
+    console.log('tree', tree, compactPath(tree.root.children['a'], 'a'));
+    assert.ok(
+      'a' in tree.paths.root.children,
+      'root.a exists in the path tree despite having no files and only a single path'
+    );
+    assert.ok(
+      compactPath(tree.root.children['a'], 'a').name === 'a/b/c/d/e',
+      'but root.a is displayed compacted down to /e from its root level folder'
+    );
+    assert.ok(
+      compactPath(tree.findPath('z/y'), 'y').name === 'y/x',
+      'Path z/y is compacted to y/x, since it has a single child'
+    );
+    assert.ok(
+      compactPath(tree.findPath('z/y/x'), 'x').name === 'x',
+      'Path z/y/x is uncompacted, since it has multiple children'
+    );
+    console.log('ah fuck', compactPath(tree.findPath('a/b/c/d/e/z'), 'z'));
+    assert.ok(
+      compactPath(tree.findPath('a/b/c/d/e/z'), 'z').name ===
+        'z/z/z/z/z/z/z/z/z/z',
+      'Long path is recursively compacted'
+    );
   });
 });
