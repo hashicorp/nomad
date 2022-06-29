8cc14cd5e1aa691e08b07b218674096653b5e6bd
Phil Renaud <phil@riotindustries.com>
2022-06-29 12:49:25 -0400

    Trying a simpler codemirror test helper

--- ui/tests/helpers/codemirror.js
+++ ui/tests/helpers/codemirror.js
@@ -4,15 +4,7 @@ const invariant = (truthy, error) => {
 
 export function getCodeMirrorInstance(container) {
   return function (selector) {
-    const cmService = container.lookup('service:code-mirror');
-
-    const element = document.querySelector(selector);
-    invariant(element, `Selector ${selector} matched no elements`);
-
-    const cm = cmService.instanceFor(element.id);
-    invariant(cm, `No registered CodeMirror instance for ${selector}`);
-
-    return cm;
+    return document.querySelector('.CodeMirror').CodeMirror;
   };
 }
 
