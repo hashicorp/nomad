8b29702df2f839a0b11b9105e59e1aedcaf138c0
Jai <41024828+ChaiWithJai@users.noreply.github.com>
2022-06-30 15:29:04 -0400

    ui:  glob matching algorithm (#13533)

--- ui/app/abilities/variable.js
+++ ui/app/abilities/variable.js
@@ -134,4 +134,40 @@ export default class Variable extends AbstractAbility {
       return matchingPath;
     });
   }
+
+  _doesMatchPattern(pattern, path) {
+    const parts = pattern?.split(WILDCARD_GLOB);
+    const hasLeadingGlob = pattern?.startsWith(WILDCARD_GLOB);
+    const hasTrailingGlob = pattern?.endsWith(WILDCARD_GLOB);
+    const lastPartOfPattern = parts[parts.length - 1];
+    const isPatternWithoutGlob = parts.length === 1 && !hasLeadingGlob;
+
+    if (!pattern || !path || isPatternWithoutGlob) {
+      return pattern === path;
+    }
+
+    if (pattern === WILDCARD_GLOB) {
+      return true;
+    }
+
+    let subPathToMatchOn = path;
+    for (let i = 0; i < parts.length; i++) {
+      const part = parts[i];
+      const idx = subPathToMatchOn?.indexOf(part);
+      const doesPathIncludeSubPattern = idx > -1;
+      const doesMatchOnFirstSubpattern = idx === 0;
+
+      if (i === 0 && !hasLeadingGlob && !doesMatchOnFirstSubpattern) {
+        return false;
+      }
+
+      if (!doesPathIncludeSubPattern) {
+        return false;
+      }
+
+      subPathToMatchOn = subPathToMatchOn.slice(0, idx + path.length);
+    }
+
+    return hasTrailingGlob || path.endsWith(lastPartOfPattern);
+  }
 }
