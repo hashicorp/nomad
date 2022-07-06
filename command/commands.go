6eea7f9f9170f88b3bf2fd373eec68001edd8e64
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2022-07-06 14:17:44 -0400

    SV CLI: var list

--- command/commands.go
+++ command/commands.go
@@ -901,6 +901,16 @@ func Commands(metaPtr *Meta, agentUi cli.Ui) map[string]cli.CommandFactory {
 				Meta: meta,
 			}, nil
 		},
+		"var": func() (cli.Command, error) {
+			return &VarCommand{
+				Meta: meta,
+			}, nil
+		},
+		"var list": func() (cli.Command, error) {
+			return &VarListCommand{
+				Meta: meta,
+			}, nil
+		},
 		"version": func() (cli.Command, error) {
 			return &VersionCommand{
 				Version: version.GetVersion(),
