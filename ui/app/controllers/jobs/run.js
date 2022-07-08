d03b99cf86b138f47fabff9cfeaf612bc78442ec
Phil Renaud <phil@riotindustries.com>
2022-07-08 16:33:55 -0400

    Allow running jobs from a namespace-limited token

--- ui/app/controllers/jobs/run.js
+++ ui/app/controllers/jobs/run.js
@@ -1,9 +1,46 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { scheduleOnce } from '@ember/runloop';
+import { serialize } from 'nomad-ui/utils/qp-serialize';
 
 export default class RunController extends Controller {
   @service router;
+  @service system;
+  @service store;
   onSubmit(id, namespace) {
     this.router.transitionTo('jobs.job', `${id}@${namespace || 'default'}`);
+  }
+  @computed('qpNamespace')
+  get optionsNamespaces() {
+    const availableNamespaces = this.store
+      .peekAll('namespace')
+      .map((namespace) => ({
+        key: namespace.name,
+        label: namespace.name,
+      }));
+
+    availableNamespaces.unshift({
+      key: '*',
+      label: 'All (*)',
+    });
+
+    // Unset the namespace selection if it was server-side deleted
+    if (!availableNamespaces.mapBy('key').includes(this.qpNamespace)) {
+      scheduleOnce('actions', () => {
+        // eslint-disable-next-line ember/no-side-effects
+        this.set('qpNamespace', '*');
+      });
+    }
+
+    return availableNamespaces;
+  }
+
+  setFacetQueryParam(queryParam, selection) {
+    this.set(queryParam, serialize(selection));
+    this.set(
+      'model.namespace',
+      this.store.peekAll('namespace').find((ns) => ns.id === this.qpNamespace)
+    );
   }
 }
