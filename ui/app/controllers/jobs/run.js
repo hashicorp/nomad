61f1dfb7254e87832b94e14714dcb0d46c235fa5
Phil Renaud <phil@riotindustries.com>
2022-07-09 03:13:02 +0000

    backport of commit b6ea5da10105fc0be0b38f09c57f30989bbaa461

--- ui/app/controllers/jobs/run.js
+++ ui/app/controllers/jobs/run.js
@@ -3,7 +3,6 @@ import { inject as service } from '@ember/service';
 
 export default class RunController extends Controller {
   @service router;
-
   onSubmit(id, namespace) {
     this.router.transitionTo('jobs.job', `${id}@${namespace || 'default'}`);
   }
