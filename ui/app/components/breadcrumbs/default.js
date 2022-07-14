5cc1b5991fc610e1b026cdc5206629af86d5ac20
Phil Renaud <phil@riotindustries.com>
2022-07-13 23:06:11 -0400

    U to go up a level

---
+++ ui/app/components/breadcrumbs/default.js
@@ -0,0 +1,18 @@
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import KeyboardShortcutModifier from 'nomad-ui/modifiers/keyboard-shortcut';
+import { inject as service } from '@ember/service';
+
+export default class BreadcrumbsTemplate extends Component {
+  @service router;
+
+  @action
+  traverseUpALevel(args) {
+    const [path, ...rest] = args;
+    this.router.transitionTo(path, ...rest);
+  }
+
+  get keyboardShortcut() {
+    return this.args.isOneCrumbUp ? KeyboardShortcutModifier : null;
+  }
+}
