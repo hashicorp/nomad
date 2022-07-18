65f71bb6a7a302380ec0dc018e19139229483b74
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2022-07-18 14:19:57 -0400

    Backport of fsm: one-time token expiration should be deterministic into release/1.2.x (#13822)
    This pull request was automerged via backport-assistant

--- nomad/structs/structs.go
+++ nomad/structs/structs.go
@@ -11557,6 +11557,7 @@ type OneTimeTokenDeleteRequest struct {
 
 // OneTimeTokenExpireRequest is a request to delete all expired one-time tokens
 type OneTimeTokenExpireRequest struct {
+	Timestamp time.Time
 	WriteRequest
 }
 
