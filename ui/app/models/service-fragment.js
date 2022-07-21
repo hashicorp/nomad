3cf40b711a3615384765e47e9fa847aa9d4755f2
Phil Renaud <phil@riotindustries.com>
2022-07-21 15:34:32 -0400

    Explicit service-fragment mirage model

--- ui/app/models/service-fragment.js
+++ ui/app/models/service-fragment.js
@@ -2,7 +2,7 @@ import { attr } from '@ember-data/model';
 import Fragment from 'ember-data-model-fragments/fragment';
 import { fragment } from 'ember-data-model-fragments/attributes';
 
-export default class Service extends Fragment {
+export default class ServiceFragment extends Fragment {
   @attr('string') name;
   @attr('string') portLabel;
   @attr() tags;
