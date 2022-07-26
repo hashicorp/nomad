554cf36834ab4bfdab5bde4e082edd94aa44ff41
Mike Nomitch <mail@mikenomitch.com>
2022-07-26 14:43:16 -0700

    Basic policies page

--- ui/app/controllers/policies/index.js
+++ ui/app/controllers/policies/index.js
@@ -2,8 +2,6 @@ import Controller, { inject as controller } from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
-const ALL_NAMESPACE_WILDCARD = '*';
-
 export default class PoliciesIndexController extends Controller {
   @service router;
   @service store;
@@ -17,36 +15,11 @@ export default class PoliciesIndexController extends Controller {
 
   @controller policies;
 
-  @action
-  setNamespace(namespace) {
-    this.policies.setNamespace(namespace);
-  }
-
-  get namespaceSelection() {
-    return this.policies.qpNamespace;
-  }
-
   get hasPolicies() {
     return this.model.policies.length;
   }
 
   get root() {
     return this.model.root;
-  }
-
-  get namespaceOptions() {
-    const namespaces = this.store
-      .peekAll('namespace')
-      .map(({ name }) => ({ key: name, label: name }));
-
-    if (namespaces.length <= 1) return null;
-
-    // Create default namespace selection
-    namespaces.unshift({
-      key: ALL_NAMESPACE_WILDCARD,
-      label: 'All (*)',
-    });
-
-    return namespaces;
   }
 }
--- ui/app/routes/policies.js
+++ ui/app/routes/policies.js
@@ -1,3 +1,35 @@
 import Route from '@ember/routing/route';
 
-export default class PoliciesRoute extends Route {}
+import notifyError from 'nomad-ui/utils/notify-error';
+import PathTree from 'nomad-ui/utils/path-tree';
+
+export default class PoliciesRoute extends Route {
+  async model() {
+    try {
+      // await this.store.findAll('policies');
+      // const policies = await this.store.query(
+      //   'policy',
+      //   { },
+      //   { reload: true }
+      // );
+
+      const policies = [
+        {
+          id: 1,
+          name: 'foo',
+          description: 'bar',
+          rules: `
+            foo = "bar"
+          `,
+        },
+      ];
+
+      return {
+        policies,
+        pathTree: new PathTree(policies),
+      };
+    } catch (e) {
+      notifyError(this)(e);
+    }
+  }
+}
