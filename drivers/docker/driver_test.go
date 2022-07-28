2415d72bb6d429b3ee92e7ab850a302bba06bc1f
Tim Gross <tgross@hashicorp.com>
2022-07-28 14:47:06 -0400

    test: disable docker OOM detection test on cgroups v2 (#13928)
    OOM detection under cgroups v2 is flaky under versions of `containerd` before
    v1.6.3, but our `containerd` dependency is transitive on `moby/moby`, who have
    not yet updated. Disable this test for cgroups v2 environments until we can
    update the dependency chain.

--- drivers/docker/driver_test.go
+++ drivers/docker/driver_test.go
@@ -2383,9 +2383,9 @@ func TestDockerDriver_OOMKilled(t *testing.T) {
 	ci.Parallel(t)
 	testutil.DockerCompatible(t)
 
-	if runtime.GOOS == "windows" {
-		t.Skip("Windows does not support OOM Killer")
-	}
+	// waiting on upstream fix for cgroups v2
+	// see https://github.com/hashicorp/nomad/issues/13119
+	testutil.CgroupsCompatibleV1(t)
 
 	taskCfg := newTaskConfig("", []string{"sh", "-c", `sleep 2 && x=a && while true; do x="$x$x"; done`})
 	task := &drivers.TaskConfig{
