36ced80963c5941dfc0857bc1c25759618f88a46
Derek Strickland <derek.strickland@gmail.com>
2022-08-17 20:13:05 +0000

    backport of commit b0ee4b4d7d467b7197242152da0dc3ce129c4825

--- nomad/job_endpoint_oss.go
+++ nomad/job_endpoint_oss.go
@@ -8,7 +8,7 @@ import (
 )
 
 // enforceSubmitJob is used to check any Sentinel policies for the submit-job scope
-func (j *Job) enforceSubmitJob(override bool, job *structs.Job) (error, error) {
+func (j *Job) enforceSubmitJob(override bool, job *structs.Job, nomadACLToken *structs.ACLToken, ns *structs.Namespace) (error, error) {
 	return nil, nil
 }
 
