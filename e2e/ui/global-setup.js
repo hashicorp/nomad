a8ae0465fd23fc1cd8bcda71d2c2ae3abf9d81e8
Jai Bhagat <jaybhagat841@gmail.com>
2022-08-18 10:11:27 -0400

    ui: add nomad commands to global set-up

--- e2e/ui/global-setup.js
+++ e2e/ui/global-setup.js
@@ -2,6 +2,15 @@ import { chromium } from '@playwright/test';
 import { execaSync } from 'execa';
 
 async function globalSetup() {
+    // Run Nomad Commands
+    execaSync('nomad namespace apply -description "Prod" prod')
+    execaSync('nomad namespace apply -description "Dev" dev')
+    execaSync('nomad acl policy apply -description "Operator" operator ./policies/operator.hcl')
+    execaSync('nomad acl policy apply -description "Power User" dev ./policies/developer.hcl')
+    execaSync('nomad acl policy apply -description "Anonymous" anon ./policies/anon.hcl')
+    execaSync('nomad acl token create -name="E2E Operator" -policy=operator -type=client | tee operator.token')
+    execaSync('nomad acl token create -name="E2E Power User" -policy=dev -type=client | tee dev.token')
+    execaSync('nomad acl token create -name="E2E Anonymous User" -policy=anon -type=client | tee anon.token')  
   
   var NOMAD_TOKEN = process.env.NOMAD_TOKEN;
   if (NOMAD_TOKEN === undefined || NOMAD_TOKEN === "") {
