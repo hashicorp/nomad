3940864c7c14ee7ec18a529ec0e652bc3cc99b9c
Phil Renaud <phil.renaud@hashicorp.com>
2022-08-22 11:45:12 -0400

    [ui] Allocation route services table: show task-level services (#14199)
    Adds service fragments to allocations and union taskGroup and task services

--- ui/tests/acceptance/regions-test.js
+++ ui/tests/acceptance/regions-test.js
@@ -155,6 +155,7 @@ module('Acceptance | regions (many)', function (hooks) {
     const newRegion = server.db.regions[1].id;
 
     await Allocation.visit({ id: server.db.allocations[0].id });
+    await this.pauseTest();
 
     await selectChoose('[data-test-region-switcher-parent]', newRegion);
 
