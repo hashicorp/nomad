3ee47277614350a77ee7345de26585bc56d9b5ba
Jai Bhagat <jaybhagat841@gmail.com>
2022-08-23 11:15:33 -0400

    chore: remove anon policy and token

--- e2e/ui/global-teardown.js
+++ e2e/ui/global-teardown.js
@@ -6,10 +6,8 @@ async function globalTeardown() {
       await client(`/namespace/dev`, {method: 'delete'});
       await client(`/acl/policy/operator`, {method: 'delete'});
       await client(`/acl/policy/dev`, {method: 'delete'});
-      await client(`/acl/policy/anon`, {method: 'delete'});
       await client(`/acl/token/operator`, {method: 'delete'});
       await client(`/acl/token/dev`, {method: 'delete'});
-      await client(`/acl/token/anon`, {method: 'delete'});
     } catch (e) {
       console.error('ERROR:  ', e)
     }  
