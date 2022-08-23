b835effd6e817b8f2a75380a3fe5230425154498
Luiz Aoqui <luiz@hashicorp.com>
2022-08-23 13:34:33 +0000

    backport of commit 434ee9941680b6519c2da9a3fe265772d4f24d75

--- ui/tests/acceptance/exec-test.js
+++ ui/tests/acceptance/exec-test.js
@@ -27,11 +27,15 @@ module('Acceptance | exec', function (hooks) {
     });
 
     this.job.taskGroups.models.forEach((taskGroup) => {
-      server.create('allocation', {
+      const alloc = server.create('allocation', {
         jobId: this.job.id,
         taskGroup: taskGroup.name,
         forceRunningClientStatus: true,
       });
+      server.db.taskStates.update(
+        { allocationId: alloc.id },
+        { state: 'running' }
+      );
     });
   });
 
@@ -135,12 +139,11 @@ module('Acceptance | exec', function (hooks) {
 
     let runningTaskGroup = this.job.taskGroups.models.sortBy('name')[1];
     runningTaskGroup.tasks.models.forEach((task, index) => {
+      let state = 'running';
       if (index > 0) {
-        this.server.db.taskStates.update(
-          { name: task.name },
-          { finishedAt: new Date() }
-        );
+        state = 'dead';
       }
+      this.server.db.taskStates.update({ name: task.name }, { state });
     });
 
     await Exec.visitJob({ job: this.job.id });
@@ -159,12 +162,11 @@ module('Acceptance | exec', function (hooks) {
     let runningTaskGroup = this.job.taskGroups.models.sortBy('name')[1];
     let changingTaskStateName;
     runningTaskGroup.tasks.models.sortBy('name').forEach((task, index) => {
+      let state = 'running';
       if (index > 0) {
-        this.server.db.taskStates.update(
-          { name: task.name },
-          { finishedAt: new Date() }
-        );
+        state = 'dead';
       }
+      this.server.db.taskStates.update({ name: task.name }, { state });
 
       if (index === 1) {
         changingTaskStateName = task.name;
@@ -187,7 +189,7 @@ module('Acceptance | exec', function (hooks) {
         );
 
         if (changingTaskState) {
-          changingTaskState.set('finishedAt', undefined);
+          changingTaskState.set('state', 'running');
         }
       });
 
