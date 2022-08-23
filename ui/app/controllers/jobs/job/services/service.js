caa0fd69f8fb25de707806c709d55468187fa2a9
Phil Renaud <phil@riotindustries.com>
2022-08-23 15:52:15 -0400

    Keyboard shortcuts on service page

--- ui/app/controllers/jobs/job/services/service.js
+++ ui/app/controllers/jobs/job/services/service.js
@@ -1,3 +1,14 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class JobsJobServicesServiceController extends Controller {}
+export default class JobsJobServicesServiceController extends Controller {
+  @service router;
+
+	@action
+  gotoAllocation(allocation) {
+		console.log('alloc', allocation);
+    this.router.transitionTo('allocations.allocation', allocation.get('id'));
+  }
+
+}
