826f1b0728397245201dc518b5e955ff7e983458
Michael Schurter <mschurter@hashicorp.com>
2022-08-23 20:39:01 +0000

    backport of commit e49786728c98c12b954c0a93c19b9d7a9f41d0ce

--- nomad/server_setup.go
+++ nomad/server_setup.go
@@ -1,6 +1,24 @@
 package nomad
 
-import "golang.org/x/exp/slices"
+import (
+	"github.com/hashicorp/go-hclog"
+	"golang.org/x/exp/slices"
+)
+
+// LicenseConfig allows for tunable licensing config
+// primarily used for enterprise testing
+type LicenseConfig struct {
+	// LicenseEnvBytes is the license bytes to use for the server's license
+	LicenseEnvBytes string
+
+	// LicensePath is the path to use for the server's license
+	LicensePath string
+
+	// AdditionalPubKeys is a set of public keys to
+	AdditionalPubKeys []string
+
+	Logger hclog.InterceptLogger
+}
 
 func (c *LicenseConfig) Copy() *LicenseConfig {
 	if c == nil {
