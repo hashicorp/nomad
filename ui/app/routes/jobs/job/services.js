a29c0a93da52147713ce0b424e91bbbab91af28a
Phil Renaud <phil@riotindustries.com>
2022-08-24 14:44:04 -0400

    Watch job for service name changes too

--- ui/app/routes/jobs/job/services.js
+++ ui/app/routes/jobs/job/services.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
 import WithWatchers from 'nomad-ui/mixins/with-watchers';
 import { collect } from '@ember/object/computed';
-import { watchRelationship } from 'nomad-ui/utils/properties/watch';
+import {
+  watchRecord,
+  watchRelationship,
+} from 'nomad-ui/utils/properties/watch';
 
 export default class JobsJobServicesRoute extends Route.extend(WithWatchers) {
   model() {
@@ -12,10 +15,12 @@ export default class JobsJobServicesRoute extends Route.extend(WithWatchers) {
   startWatchers(controller, model) {
     if (model) {
       controller.set('watchServices', this.watchServices.perform(model));
+      controller.set('watchJob', this.watchJob.perform(model));
     }
   }
 
   @watchRelationship('services', true) watchServices;
+  @watchRecord('job') watchJob;
 
-  @collect('watchServices') watchers;
+  @collect('watchServices', 'watchJob') watchers;
 }
