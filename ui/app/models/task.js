59e250adbb696f2fb9063067fff9ed09490c715f
Phil Renaud <phil.renaud@hashicorp.com>
2022-08-25 23:22:55 -0400

    Nomad Services: job routes, model, and serializer updates (#14226)
    * Added to subnav and basic table implemented
    
    * Existing services become service fragments, and services tab aggregated beneath job route
    
    * Index page within jobs/job/services
    
    * Watchable services
    
    * Lintfixes
    
    * Links to clients and individual services set up
    
    * Child service route
    
    * Keyboard shortcuts on service page
    
    * Model that shows consul services as well, plus level and provider cols
    
    * lintfix
    
    * Level as query param
    
    * Watch job for service name changes too
    
    * Lintfix
    
    * Testfixes
    
    * Placeholder mirage route

--- ui/app/models/task.js
+++ ui/app/models/task.js
@@ -48,7 +48,7 @@ export default class Task extends Fragment {
   @attr('number') reservedCPU;
   @attr('number') reservedDisk;
   @attr('number') reservedEphemeralDisk;
-  @fragmentArray('service') services;
+  @fragmentArray('service-fragment') services;
 
   @fragmentArray('volume-mount', { defaultValue: () => [] }) volumeMounts;
 
