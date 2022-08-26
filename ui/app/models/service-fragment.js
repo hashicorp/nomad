cc0ee6687996906330f242163911f0454f9a94ac
Phil Renaud <phil.renaud@hashicorp.com>
2022-08-25 23:22:55 -0400

    Nomad Services: job routes, model, and serializer updates (#14226)
    * Added to subnav and basic table implemented
    
    * Existing services become service fragments, and services tab aggregated beneath job route
    
    * Index page within jobs/job/services
    
    * Watchable services
    
    * Lintfixes
    
    * Links to clients and individual services set up
    
    * Child service route
    
    * Keyboard shortcuts on service page
    
    * Model that shows consul services as well, plus level and provider cols
    
    * lintfix
    
    * Level as query param
    
    * Watch job for service name changes too
    
    * Lintfix
    
    * Testfixes
    
    * Placeholder mirage route

---
+++ ui/app/models/service-fragment.js
@@ -0,0 +1,12 @@
+import { attr } from '@ember-data/model';
+import Fragment from 'ember-data-model-fragments/fragment';
+import { fragment } from 'ember-data-model-fragments/attributes';
+
+export default class Service extends Fragment {
+  @attr('string') name;
+  @attr('string') portLabel;
+  @attr() tags;
+  @attr('string') onUpdate;
+  @attr('string') provider;
+  @fragment('consul-connect') connect;
+}
---
+++ ui/app/routes/jobs/job/services.js
@@ -0,0 +1,26 @@
+import Route from '@ember/routing/route';
+import WithWatchers from 'nomad-ui/mixins/with-watchers';
+import { collect } from '@ember/object/computed';
+import {
+  watchRecord,
+  watchRelationship,
+} from 'nomad-ui/utils/properties/watch';
+
+export default class JobsJobServicesRoute extends Route.extend(WithWatchers) {
+  model() {
+    const job = this.modelFor('jobs.job');
+    return job && job.get('services').then(() => job);
+  }
+
+  startWatchers(controller, model) {
+    if (model) {
+      controller.set('watchServices', this.watchServices.perform(model));
+      controller.set('watchJob', this.watchJob.perform(model));
+    }
+  }
+
+  @watchRelationship('services', true) watchServices;
+  @watchRecord('job') watchJob;
+
+  @collect('watchServices', 'watchJob') watchers;
+}
