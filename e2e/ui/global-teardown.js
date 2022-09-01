b080131bed3bd9810c6ae0f342fff13deea38b47
Jai Bhagat <jaybhagat841@gmail.com>
2022-09-01 12:22:49 -0400

    chore: prettify files

--- e2e/ui/global-teardown.js
+++ e2e/ui/global-teardown.js
@@ -1,22 +1,24 @@
-import { client } from './api-client.js';
+import { client } from "./api-client.js";
 
 async function globalTeardown() {
   try {
-      await client(`/namespace/prod`, {method: 'delete'});
-      await client(`/namespace/dev`, {method: 'delete'});
-      await client(`/acl/policy/operator`, {method: 'delete'});
-      await client(`/acl/policy/dev`, {method: 'delete'});
-      
-      const {data: tokens} = await client(`/acl/tokens`);
-      
-      await Promise.all(tokens.map(async (token) => {
-        if (token.Type === 'client') {
-          await client(`/acl/token/${token.AccessorID}`, {method: 'delete'});
+    await client(`/namespace/prod`, { method: "delete" });
+    await client(`/namespace/dev`, { method: "delete" });
+    await client(`/acl/policy/operator`, { method: "delete" });
+    await client(`/acl/policy/dev`, { method: "delete" });
+
+    const { data: tokens } = await client(`/acl/tokens`);
+
+    await Promise.all(
+      tokens.map(async (token) => {
+        if (token.Type === "client") {
+          await client(`/acl/token/${token.AccessorID}`, { method: "delete" });
         }
-      }));
-    } catch (e) {
-      console.error('ERROR:  ', e)
-    }  
-};
+      })
+    );
+  } catch (e) {
+    console.error("ERROR:  ", e);
+  }
+}
 
 export default globalTeardown;
