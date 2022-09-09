bd7644e1321d60076ab3606d55d977316fbbe0ee
HashiDaveR <106760442+hashidave@users.noreply.github.com>
2022-09-09 15:26:22 -0500

    Update variables.tf

--- terraform/aws/env/us-east/variables.tf
+++ terraform/aws/env/us-east/variables.tf
@@ -16,7 +16,7 @@ variable "ami" {
 
 variable "server_instance_type" {
   description = "The AWS instance type to use for servers."
-  default     = "t2.small"
+  default     = "t2.micro"
 }
 
 variable "client_instance_type" {
