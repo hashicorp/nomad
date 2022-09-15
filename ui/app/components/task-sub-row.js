a7ef44788fda8ead31ba6de3fac895fb48b56a2f
Phil Renaud <phil@riotindustries.com>
2022-09-15 09:50:16 -0400

    Lintfix

--- ui/app/components/task-sub-row.js
+++ ui/app/components/task-sub-row.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
--- ui/tests/integration/components/task-sub-row-test.js
+++ ui/tests/integration/components/task-sub-row-test.js
@@ -2,25 +2,15 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { componentA11yAudit } from 'nomad-ui/tests/helpers/a11y-audit';
 
 module('Integration | Component | task-sub-row', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+    assert.expect(1);
     await render(hbs`<TaskSubRow />`);
-
     assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <TaskSubRow>
-        template block text
-      </TaskSubRow>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
+    await componentA11yAudit(this.element, assert);
   });
 });
