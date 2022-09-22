5ffc99de98bbb906ab3d0c1946d29df2a535f5df
Seth Hoenig <shoenig@duck.com>
2022-09-22 10:57:48 -0500

    cleanup: rearrange mocks package
    This PR splits up the nomad/mock package into more files. Specific features
    that have a lot of mocks get their own file (e.g. acl, variables, csi, connect, etc.).
    Above that, functions that return jobs/allocs/nodes are in the job/alloc/node file. And
    lastly other mocks/helpers are in mock.go

--- nomad/mock/acl.go
+++ nomad/mock/acl.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/hashicorp/nomad/helper/uuid"
 	testing "github.com/mitchellh/go-testing-interface"
 
 	"github.com/hashicorp/nomad/nomad/structs"
@@ -38,7 +40,7 @@ func NamespacePolicy(namespace string, policy string, capabilities []string) str
 	return policyHCL
 }
 
-// NamespacePolicy is a helper for generating the policy hcl for a given
+// NamespacePolicyWithVariables is a helper for generating the policy hcl for a given
 // namespace. Either policy or capabilities may be nil but not both.
 func NamespacePolicyWithVariables(namespace string, policy string, capabilities []string, svars map[string][]string) string {
 	policyHCL := fmt.Sprintf("namespace %q {", namespace)
@@ -148,4 +150,71 @@ func CreateToken(t testing.T, state StateStore, index uint64, policies []string)
 func CreatePolicyAndToken(t testing.T, state StateStore, index uint64, name, rule string) *structs.ACLToken {
 	CreatePolicy(t, state, index, name, rule)
 	return CreateToken(t, state, index+1, []string{name})
+}
+
+func ACLRole() *structs.ACLRole {
+	role := structs.ACLRole{
+		ID:          uuid.Generate(),
+		Name:        fmt.Sprintf("acl-role-%s", uuid.Short()),
+		Description: "mocked-test-acl-role",
+		Policies: []*structs.ACLRolePolicyLink{
+			{Name: "mocked-test-policy-1"},
+			{Name: "mocked-test-policy-2"},
+		},
+		CreateIndex: 10,
+		ModifyIndex: 10,
+	}
+	role.SetHash()
+	return &role
+}
+
+func ACLPolicy() *structs.ACLPolicy {
+	ap := &structs.ACLPolicy{
+		Name:        fmt.Sprintf("policy-%s", uuid.Generate()),
+		Description: "Super cool policy!",
+		Rules: `
+		namespace "default" {
+			policy = "write"
+		}
+		node {
+			policy = "read"
+		}
+		agent {
+			policy = "read"
+		}
+		`,
+		CreateIndex: 10,
+		ModifyIndex: 20,
+	}
+	ap.SetHash()
+	return ap
+}
+
+func ACLToken() *structs.ACLToken {
+	tk := &structs.ACLToken{
+		AccessorID:  uuid.Generate(),
+		SecretID:    uuid.Generate(),
+		Name:        "my cool token " + uuid.Generate(),
+		Type:        "client",
+		Policies:    []string{"foo", "bar"},
+		Global:      false,
+		CreateTime:  time.Now().UTC(),
+		CreateIndex: 10,
+		ModifyIndex: 20,
+	}
+	tk.SetHash()
+	return tk
+}
+
+func ACLManagementToken() *structs.ACLToken {
+	return &structs.ACLToken{
+		AccessorID:  uuid.Generate(),
+		SecretID:    uuid.Generate(),
+		Name:        "management " + uuid.Generate(),
+		Type:        "management",
+		Global:      true,
+		CreateTime:  time.Now().UTC(),
+		CreateIndex: 10,
+		ModifyIndex: 20,
+	}
 }
