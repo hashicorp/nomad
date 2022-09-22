74ff4e6d30fb8dae8d6ac155fc697e9017f20282
Phil Renaud <phil@riotindustries.com>
2022-09-22 10:40:43 -0400

    Lintfix

--- ui/tests/integration/components/task-sub-row-test.js
+++ ui/tests/integration/components/task-sub-row-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { componentA11yAudit } from 'nomad-ui/tests/helpers/a11y-audit';
 
