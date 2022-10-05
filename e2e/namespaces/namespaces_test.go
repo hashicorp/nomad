8867791818ca05c1829e4b6aa0239758f78506ef
James Rasell <jrasell@users.noreply.github.com>
2022-10-05 15:50:56 +0200

    e2e: fix incorrect must function usage in namespace suite. (#14805)

--- e2e/namespaces/namespaces_test.go
+++ e2e/namespaces/namespaces_test.go
@@ -147,7 +147,7 @@ func TestNamespacesFiltering(t *testing.T) {
 
 	out, err = e2eutil.Command("nomad", "job", "stop", jobA)
 	must.Eq(t, fmt.Sprintf("No job(s) with prefix or id %q found\n", jobA), out)
-	must.ContainsString(t, err.Error(), "exit status 1")
+	must.StrContains(t, err.Error(), "exit status 1")
 
 	err = e2eutil.StopJob(jobA, "-namespace", "NamespaceA")
 	require.NoError(t, err, "could not stop job in namespace")
