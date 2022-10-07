33c20bb3b02cd9daeea6eccf7418353c5edd1343
Tim Gross <tgross@hashicorp.com>
2022-10-07 16:36:23 -0400

    logmon: add profiling on SIGUSR2

--- client/logmon/z_logmon_cmd.go
+++ client/logmon/z_logmon_cmd.go
@@ -2,6 +2,11 @@ package logmon
 
 import (
 	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
+	"syscall"
+	"time"
 
 	hclog "github.com/hashicorp/go-hclog"
 	plugin "github.com/hashicorp/go-plugin"
@@ -15,11 +20,23 @@ import (
 // here. See eeaa95d commit message for more details.
 func init() {
 	if len(os.Args) > 1 && os.Args[1] == "logmon" {
+
 		logger := hclog.New(&hclog.LoggerOptions{
 			Level:      hclog.Trace,
 			JSONFormat: true,
 			Name:       "logmon",
 		})
+
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGUSR2)
+		go func() {
+			for {
+				// note: this will drop any other SIGUSR2 while this is running
+				<-sigCh
+				runProfile(logger)
+			}
+		}()
+
 		plugin.Serve(&plugin.ServeConfig{
 			HandshakeConfig: base.Handshake,
 			Plugins: map[string]plugin.Plugin{
@@ -30,4 +47,34 @@ func init() {
 		})
 		os.Exit(0)
 	}
+}
+
+func runProfile(logger hclog.Logger) {
+	logger.Info("starting logmon profile")
+	cpuprofile, err := os.CreateTemp("", "logmon-cpuprofile-*")
+	if err != nil {
+		logger.Error("could not create cpuprofile tempfile", "error", err)
+	}
+
+	memprofile, err := os.CreateTemp("", "logmon-memprofile-*")
+	if err != nil {
+		logger.Error("could not create cpuprofile tempfile", "error", err)
+	}
+
+	defer cpuprofile.Close()
+	defer memprofile.Close()
+
+	runtime.GC() // get up-to-date statistics
+	if err := pprof.WriteHeapProfile(memprofile); err != nil {
+		logger.Error("could not write memory profile", "error", err)
+	}
+
+	if err := pprof.StartCPUProfile(cpuprofile); err != nil {
+		logger.Error("could not start CPU profile", "error", err)
+	}
+	time.Sleep(10 * time.Second)
+	pprof.StopCPUProfile()
+
+	logger.Info("recorded cpuprofile", "path", cpuprofile.Name())
+	logger.Info("recorded memprofile", "path", memprofile.Name())
 }
