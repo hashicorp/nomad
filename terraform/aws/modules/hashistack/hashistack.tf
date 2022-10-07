f6bcd6b949fd88ef5822a5dad41d1dbef5c6d95a
david randolph <david.randolph@hashicorp.com>
2022-10-07 12:05:48 -0500

    Restored key_name variable.

--- terraform/aws/modules/hashistack/hashistack.tf
+++ terraform/aws/modules/hashistack/hashistack.tf
@@ -178,7 +178,7 @@ resource "aws_instance" "server" {
   ami                    = var.ami
   instance_type          = var.server_instance_type
   #key_name               = aws_key_pair.nomaddemo_server[count.index].key_name
-  key_name                ="DavesTestKeys"
+  key_name                = var.key_name
   vpc_security_group_ids = [aws_security_group.primary.id]
   count                  = var.server_count
 
