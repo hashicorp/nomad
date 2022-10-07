9d1531efc086bf95c5580d4ace0f5a9c66ebdab7
david randolph <david.randolph@hashicorp.com>
2022-10-07 10:20:53 -0500

    have to use underscores instead of -

--- terraform/aws/env/us-east/main.tf
+++ terraform/aws/env/us-east/main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 
-data "hcp_packer_iteration" "hashistack-image" {
+data "hcp_packer_iteration" "hashistack_image" {
     bucket_name = "hashistack"
     channel     = "production"
   }
  
-  data "hcp_packer_image" "hashistack-image" {
+  data "hcp_packer_image" "hashistack_image" {
     bucket_name    = "hashistack"
     cloud_provider = "aws"
-    iteration_id   = data.hcp_packer_iteration.hashistack-image.ulid
+    iteration_id   = data.hcp_packer_iteration.hashistack_image.ulid
     region         = "us-east-1"
   }
 
@@ -21,7 +21,7 @@ module "hashistack" {
 
   name                   = var.name
   region                 = var.region
-  ami                    = hcp_packer_image.hashistack-image.cloud_image_id
+  ami                    = hcp_packer_image.hashistack_image.cloud_image_id
   server_instance_type   = var.server_instance_type
   client_instance_type   = var.client_instance_type
   key_name               = var.key_name
