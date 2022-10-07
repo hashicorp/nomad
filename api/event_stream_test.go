d36f2337116d586a9496e7ef81f79c43f6662516
James Rasell <jrasell@hashicorp.com>
2022-10-07 11:56:50 +0100

    api: add convenience string func to Topic type.

--- api/event_stream_test.go
+++ api/event_stream_test.go
@@ -11,6 +11,51 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTopic_String(t *testing.T) {
+	testutil.Parallel(t)
+
+	testCases := []struct {
+		inputTopic     Topic
+		expectedOutput string
+	}{
+		{
+			inputTopic:     TopicDeployment,
+			expectedOutput: "Deployment",
+		},
+		{
+			inputTopic:     TopicEvaluation,
+			expectedOutput: "Evaluation",
+		},
+		{
+			inputTopic:     TopicAllocation,
+			expectedOutput: "Allocation",
+		},
+		{
+			inputTopic:     TopicJob,
+			expectedOutput: "Job",
+		},
+		{
+			inputTopic:     TopicNode,
+			expectedOutput: "Node",
+		},
+		{
+			inputTopic:     TopicService,
+			expectedOutput: "Service",
+		},
+		{
+			inputTopic:     TopicAll,
+			expectedOutput: "*",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.expectedOutput, func(t *testing.T) {
+			actualOutput := tc.inputTopic.String()
+			require.Equal(t, tc.expectedOutput, actualOutput)
+		})
+	}
+}
+
 func TestEvent_Stream(t *testing.T) {
 	testutil.Parallel(t)
 
