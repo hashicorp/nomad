9595a7e2f7b1cf65d95e18994c7d1702d0dca59d
Tim Gross <tgross@hashicorp.com>
2022-10-11 11:01:41 -0400

    docker_logger: add profiling on SIGUSR2

--- drivers/docker/docklog/z_docker_logger_cmd.go
+++ drivers/docker/docklog/z_docker_logger_cmd.go
@@ -2,7 +2,13 @@ package docklog
 
 import (
 	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
+	"syscall"
+	"time"
 
+	"github.com/hashicorp/go-hclog"
 	log "github.com/hashicorp/go-hclog"
 	plugin "github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/nomad/plugins/base"
@@ -21,6 +27,16 @@ func init() {
 			Name:       PluginName,
 		})
 
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGUSR2)
+		go func() {
+			for {
+				// note: this will drop any other SIGUSR2 while this is running
+				<-sigCh
+				runProfile(logger)
+			}
+		}()
+
 		plugin.Serve(&plugin.ServeConfig{
 			HandshakeConfig: base.Handshake,
 			Plugins: map[string]plugin.Plugin{
@@ -31,4 +47,34 @@ func init() {
 		})
 		os.Exit(0)
 	}
+}
+
+func runProfile(logger hclog.Logger) {
+	logger.Info("starting docker_logger profile")
+	cpuprofile, err := os.CreateTemp("", "docker_logger-cpuprofile-*")
+	if err != nil {
+		logger.Error("could not create cpuprofile tempfile", "error", err)
+	}
+
+	memprofile, err := os.CreateTemp("", "docker_logger-memprofile-*")
+	if err != nil {
+		logger.Error("could not create cpuprofile tempfile", "error", err)
+	}
+
+	defer cpuprofile.Close()
+	defer memprofile.Close()
+
+	runtime.GC() // get up-to-date statistics
+	if err := pprof.WriteHeapProfile(memprofile); err != nil {
+		logger.Error("could not write memory profile", "error", err)
+	}
+
+	if err := pprof.StartCPUProfile(cpuprofile); err != nil {
+		logger.Error("could not start CPU profile", "error", err)
+	}
+	time.Sleep(10 * time.Second)
+	pprof.StopCPUProfile()
+
+	logger.Info("recorded cpuprofile", "path", cpuprofile.Name())
+	logger.Info("recorded memprofile", "path", memprofile.Name())
 }
