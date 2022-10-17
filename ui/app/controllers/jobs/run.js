ab68f4ebea221cb35ca636b785cfdf5e0d111d64
Phil Renaud <phil@riotindustries.com>
2022-10-17 16:56:44 -0400

    Within the job-editor again tho

--- ui/app/controllers/jobs/run.js
+++ ui/app/controllers/jobs/run.js
@@ -1,20 +1,9 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class RunController extends Controller {
   @service router;
   onSubmit(id, namespace) {
     this.router.transitionTo('jobs.job', `${id}@${namespace || 'default'}`);
-  }
-
-  @action uploadJobSpec(event) {
-    const reader = new FileReader();
-    reader.onload = () => {
-      this.model._newDefinition = reader.result;
-    };
-
-    const [file] = event.target.files;
-    reader.readAsText(file);
   }
 }
