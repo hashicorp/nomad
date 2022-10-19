56816f2f936d290e86c85f2ff54f847586023c5a
Luiz Aoqui <luiz@hashicorp.com>
2022-10-19 17:57:08 -0400

    docs: expand Autoscaling documentation (#14937)
    Rename `Internals` section to `Concepts` to match core docs structure
    and expand on how policies are evaluated.
    
    Also include missing documentation for check grouping and fix examples
    to use the new feature.

--- website/redirects.js
+++ website/redirects.js
@@ -1,1 +1,18 @@
-module.exports = []
+module.exports = [
+  // Rename and re-arrange Autoscaling Internals section
+  {
+    source: '/nomad/tools/autoscaling/internals/:path*',
+    destination: '/nomad/tools/autoscaling/concepts/:path*',
+    permanent: true,
+  },
+  {
+    source: '/nomad/tools/autoscaling/concepts/checks',
+    destination: '/nomad/tools/autoscaling/concepts/policy-eval/checks',
+    permanent: true,
+  },
+  {
+    source: '/nomad/tools/autoscaling/concepts/node-selector-strategy',
+    destination: '/nomad/tools/autoscaling/concepts/policy-eval/node-selector-strategy',
+    permanent: true,
+  },
+]
