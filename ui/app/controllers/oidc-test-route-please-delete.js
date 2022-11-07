b57827169d924afe798cca013669e9b3f17a9ca0
Phil Renaud <phil@riotindustries.com>
2022-11-07 10:54:12 -0500

    dont recursively add queryparams on redirect

--- ui/app/controllers/oidc-test-route-please-delete.js
+++ ui/app/controllers/oidc-test-route-please-delete.js
@@ -1,16 +1,17 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import addToPath from 'nomad-ui/utils/add-to-path';
 
 export default class OidcTestRoutePleaseDeleteController extends Controller {
-  queryParams = ['auth_method', 'client_nonce', 'redirect_uri', 'meta']
+  queryParams = ['auth_method', 'client_nonce', 'redirect_uri', 'meta'];
 
   @action
   doGood() {
-    window.location = `${this.redirect_uri}?fakeRedirect=success`;
+    window.location = addToPath(this.redirect_uri, '?fakeRedirect=success');
   }
 
   @action
   doBad() {
-    window.location = `${this.redirect_uri}?fakeRedirect=failure`;
+    window.location = addToPath(this.redirect_uri, '?fakeRedirect=failure');
   }
 }
