89e258ee42257c850ab4a4bb6b25192d435e2529
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2022-11-09 16:18:38 +0100

    persist ACL auth methods in state store

--- nomad/fsm.go
+++ nomad/fsm.go
@@ -59,6 +59,7 @@ const (
 	VariablesQuotaSnapshot               SnapshotType = 23
 	RootKeyMetaSnapshot                  SnapshotType = 24
 	ACLRoleSnapshot                      SnapshotType = 25
+	ACLAuthMethodSnapshot                SnapshotType = 26
 
 	// Namespace appliers were moved from enterprise and therefore start at 64
 	NamespaceSnapshot SnapshotType = 64
@@ -1767,6 +1768,17 @@ func (n *nomadFSM) restoreImpl(old io.ReadCloser, filter *FSMFilter) error {
 			if err := restore.ACLRoleRestore(aclRole); err != nil {
 				return err
 			}
+		case ACLAuthMethodSnapshot:
+			authMethod := new(structs.ACLAuthMethod)
+
+			if err := dec.Decode(authMethod); err != nil {
+				return err
+			}
+
+			// Perform the restoration.
+			if err := restore.ACLAuthMethodRestore(authMethod); err != nil {
+				return err
+			}
 
 		default:
 			// Check if this is an enterprise only object being restored
@@ -2259,6 +2271,10 @@ func (s *nomadSnapshot) Persist(sink raft.SnapshotSink) error {
 		return err
 	}
 	if err := s.persistACLRoles(sink, encoder); err != nil {
+		sink.Cancel()
+		return err
+	}
+	if err := s.persistACLAuthMethods(sink, encoder); err != nil {
 		sink.Cancel()
 		return err
 	}
@@ -2909,6 +2925,30 @@ func (s *nomadSnapshot) persistACLRoles(sink raft.SnapshotSink,
 			// Write out an ACL role snapshot.
 			sink.Write([]byte{byte(ACLRoleSnapshot)})
 			if err := encoder.Encode(role); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+func (s *nomadSnapshot) persistACLAuthMethods(sink raft.SnapshotSink,
+	encoder *codec.Encoder) error {
+
+	// Get all the ACL Auth methods.
+	ws := memdb.NewWatchSet()
+	aclAuthMethodsIter, err := s.snap.GetACLAuthMethods(ws)
+	if err != nil {
+		return err
+	}
+
+	for {
+		for raw := aclAuthMethodsIter.Next(); raw != nil; raw = aclAuthMethodsIter.Next() {
+			method := raw.(*structs.ACLAuthMethod)
+
+			// write the snapshot
+			sink.Write([]byte{byte(ACLAuthMethodSnapshot)})
+			if err := encoder.Encode(method); err != nil {
 				return err
 			}
 		}
