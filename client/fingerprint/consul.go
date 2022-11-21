847c2cc528a534735b21dbdc0f08bc2ef813ed5c
James Rasell <jrasell@users.noreply.github.com>
2022-11-21 16:19:09 +0100

    client: accommodate Consul 1.14.0 gRPC and agent self changes. (#15309)
    * client: accommodate Consul 1.14.0 gRPC and agent self changes.
    
    Consul 1.14.0 changed the way in which gRPC listeners are
    configured, particularly when using TLS. Prior to the change, a
    single listener was responsible for handling plain-text and
    encrypted gRPC requests. In 1.14.0 and beyond, separate listeners
    will be used for each, defaulting to 8502 and 8503 for plain-text
    and TLS respectively.
    
    The change means that Nomad’s Consul Connect integration would not
    work when integrated with Consul clusters using TLS and running
    1.14.0 or greater.
    
    The Nomad Consul fingerprinter identifies the gRPC port Consul has
    exposed using the "DebugConfig.GRPCPort" value from Consul’s
    “/v1/agent/self” endpoint. In Consul 1.14.0 and greater, this only
    represents the plain-text gRPC port which is likely to be disbaled
    in clusters running TLS. In order to fix this issue, Nomad now
    takes into account the Consul version and configured scheme to
    optionally use “DebugConfig.GRPCTLSPort” value from Consul’s agent
    self return.
    
    The “consul_grcp_socket” allocrunner hook has also been updated so
    that the fingerprinted gRPC port attribute is passed in. This
    provides a better fallback method, when the operator does not
    configure the “consul.grpc_address” option.
    
    * docs: modify Consul Connect entries to detail 1.14.0 changes.
    
    * changelog: add entry for #15309
    
    * fixup: tidy tests and clean version match from review feedback.
    
    * fixup: use strings tolower func.

--- client/fingerprint/consul.go
+++ client/fingerprint/consul.go
@@ -3,16 +3,26 @@ package fingerprint
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	consulapi "github.com/hashicorp/consul/api"
 	log "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-version"
 	agentconsul "github.com/hashicorp/nomad/command/agent/consul"
 )
 
 const (
 	consulAvailable   = "available"
 	consulUnavailable = "unavailable"
+)
+
+var (
+	// consulGRPCPortChangeVersion is the Consul version which made a breaking
+	// change to the way gRPC API listeners are created. This means Nomad must
+	// perform different fingerprinting depending on which version of Consul it
+	// is communicating with.
+	consulGRPCPortChangeVersion = version.Must(version.NewVersion("1.14.0"))
 )
 
 // ConsulFingerprint is used to fingerprint for Consul
@@ -97,7 +107,7 @@ func (f *ConsulFingerprint) initialize(req *FingerprintRequest) error {
 			"consul.datacenter":    f.dc,
 			"consul.segment":       f.segment,
 			"consul.connect":       f.connect,
-			"consul.grpc":          f.grpc,
+			"consul.grpc":          f.grpc(consulConfig.Scheme),
 			"consul.ft.namespaces": f.namespaces,
 		}
 	}
@@ -173,8 +183,47 @@ func (f *ConsulFingerprint) connect(info agentconsul.Self) (string, bool) {
 	return strconv.FormatBool(c), ok
 }
 
-func (f *ConsulFingerprint) grpc(info agentconsul.Self) (string, bool) {
+func (f *ConsulFingerprint) grpc(scheme string) func(info agentconsul.Self) (string, bool) {
+	return func(info agentconsul.Self) (string, bool) {
+
+		// The version is needed in order to understand which config object to
+		// query. This is because Consul 1.14.0 added a new gRPC port which
+		// broke the previous behaviour.
+		v, ok := info["Config"]["Version"].(string)
+		if !ok {
+			return "", false
+		}
+
+		consulVersion, err := version.NewVersion(v)
+		if err != nil {
+			return "", false
+		}
+
+		// If the Consul agent being fingerprinted is running a version less
+		// than 1.14.0 we use the original single gRPC port.
+		if consulVersion.Core().LessThan(consulGRPCPortChangeVersion.Core()) {
+			return f.grpcPort(info)
+		}
+
+		// Now that we know we are querying a Consul agent running v1.14.0 or
+		// greater, we need to select the correct port parameter from the
+		// config depending on whether we have been asked to speak TLS or not.
+		switch strings.ToLower(scheme) {
+		case "https":
+			return f.grpcTLSPort(info)
+		default:
+			return f.grpcPort(info)
+		}
+	}
+}
+
+func (f *ConsulFingerprint) grpcPort(info agentconsul.Self) (string, bool) {
 	p, ok := info["DebugConfig"]["GRPCPort"].(float64)
+	return fmt.Sprintf("%d", int(p)), ok
+}
+
+func (f *ConsulFingerprint) grpcTLSPort(info agentconsul.Self) (string, bool) {
+	p, ok := info["DebugConfig"]["GRPCTLSPort"].(float64)
 	return fmt.Sprintf("%d", int(p)), ok
 }
 
