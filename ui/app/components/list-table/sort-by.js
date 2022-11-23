f9cd7f5cf78ecf12981c1a30f2eddc337a80f9df
Phil Renaud <phil@riotindustries.com>
2022-11-23 13:55:13 -0500

    Resizable table columns demo

--- ui/app/components/list-table/sort-by.js
+++ ui/app/components/list-table/sort-by.js
@@ -7,6 +7,8 @@ import {
   tagName,
 } from '@ember-decorators/component';
 import classic from 'ember-classic-decorator';
+import { action } from '@ember/object';
+// import localStorageProperty from 'nomad-ui/utils/properties/local-storage';
 
 @classic
 @tagName('th')
@@ -14,6 +16,19 @@ import classic from 'ember-classic-decorator';
 @classNames('is-selectable')
 @classNameBindings('isActive:is-active', 'sortDescending:desc:asc')
 export default class SortBy extends Component {
+
+  didInsertElement() {
+    super.didInsertElement(...arguments);
+    if (this.resizable) {
+      console.log('better check localstorage for', this.element.innerText);
+      const width = window.localStorage.getItem(`nomad.column.width.${this.element.innerText}`);
+      console.log('width set?', width);
+       if (width) {
+        this.element.style.width = width;
+       }
+    }
+  }
+
   // The prop that the table is currently sorted by
   currentProp = '';
 
@@ -29,4 +44,58 @@ export default class SortBy extends Component {
   get shouldSortDescending() {
     return !this.isActive || !this.sortDescending;
   }
+
+  get canResize() {
+    return this.resizable;
+  }
+
+  resizing = false;
+  startingWidth = 0;
+  startingPosition = 0;
+  td = null;
+
+  @action
+  startResize(e) {
+    console.log('start resize', e.target);
+    document.addEventListener('mousemove', this.resize);
+    document.addEventListener('mouseup', this.endResize);
+    this.resizing = true;
+    this.td = e.target.parentElement;
+    console.log('elly', this.td);
+    this.startingWidth = this.td.clientWidth;
+    this.startingPosition = e.clientX;
+  }
+  @action
+  resize(e) {
+    if (this.resizing) {
+      e.preventDefault();
+      // console.log('resizing', e.offsetX, e.clientX);
+      const newWidth = this.startingWidth + (e.clientX - this.startingPosition);
+      // console.log('NEW WIDTH IS', newWidth);
+      this.td.style.width = `${newWidth}px`;
+    }
+  }
+  @action
+  endResize(e) {
+    if (this.resizing) {
+      console.log('end resize', e);
+      // document.removeEventListener('mouseup', this.endResize);
+      document.removeEventListener('mousemove', this.resize);
+      console.log('STARTING WIDTH WAS', this.startingWidth, 'FINAL WIDTH IS', this.td.style.width);
+      window.localStorage.setItem(
+        `nomad.column.width.${this.td.innerText}`,
+        this.td.style.width
+      );
+      this.resizing = false;
+      this.startingWidth = 0;
+      this.startingPosition = 0;
+      this.td = null;
+    }
+  }
+
+  // @action resetSize(e) {
+  //   console.log('reset size', e);
+  //   window.localStorage.removeItem(`nomad.column.width.${this.element.innerText}`);
+  //   this.element.style.width = '';
+  // }
 }
