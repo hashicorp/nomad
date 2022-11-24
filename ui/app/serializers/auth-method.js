757cd9d5ec6f1782fbdcb4ede6455304f0daba9d
Phil Renaud <phil@riotindustries.com>
2022-11-24 12:21:53 -0500

    Use @classic because of class extension

--- ui/app/serializers/auth-method.js
+++ ui/app/serializers/auth-method.js
@@ -1,6 +1,7 @@
 import ApplicationSerializer from './application';
+import classic from 'ember-classic-decorator';
 
-// @classic
+@classic
 export default class AuthMethodSerializer extends ApplicationSerializer {
   primaryKey = 'Name';
 }
