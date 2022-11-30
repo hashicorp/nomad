c31730dcebba23bfaef57650ea2550af340dd1b2
Phil Renaud <phil@riotindustries.com>
2022-11-30 17:06:02 -0500

    A11y test

--- ui/tests/acceptance/policies-test.js
+++ ui/tests/acceptance/policies-test.js
@@ -4,18 +4,23 @@ import { setupApplicationTest } from 'ember-qunit';
 import { allScenarios } from '../../mirage/scenarios/default';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import percySnapshot from '@percy/ember';
+import a11yAudit from 'nomad-ui/tests/helpers/a11y-audit';
 
 module('Acceptance | policies', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
   test('Policies index route looks good', async function (assert) {
+    assert.expect(3);
     allScenarios.policiesTestCluster(server);
     window.localStorage.nomadTokenSecret = server.db.tokens[0].secretId;
     await visit('/policies');
     assert.dom('[data-test-gutter-link="policies"]').exists();
     assert.equal(currentURL(), '/policies');
-    assert.dom('[data-test-policy-row]').exists({ count: server.db.policies.length });
+    assert
+      .dom('[data-test-policy-row]')
+      .exists({ count: server.db.policies.length });
+    await a11yAudit(assert);
     await percySnapshot(assert);
   });
 
@@ -47,7 +52,7 @@ module('Acceptance | policies', function (hooks) {
     await click('[data-test-create-policy]');
     assert.equal(currentURL(), '/policies/new');
     await typeIn('[data-test-policy-name]', 'My Fun Policy');
-    await click('button[type="submit"]')
+    await click('button[type="submit"]');
     assert.dom('.flash-message.alert-error').exists();
     assert.equal(currentURL(), '/policies/new');
     document.querySelector('[data-test-policy-name]').value = ''; // clear
