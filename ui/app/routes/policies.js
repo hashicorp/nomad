d5d9c6db6f25bd502998e18131715934dba2f76a
Phil Renaud <phil@riotindustries.com>
2022-12-05 15:10:01 -0500

    Using rsvp hash to do concurrent fetches

--- ui/app/routes/policies.js
+++ ui/app/routes/policies.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import withForbiddenState from 'nomad-ui/mixins/with-forbidden-state';
 import WithModelErrorHandling from 'nomad-ui/mixins/with-model-error-handling';
 import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default class PoliciesRoute extends Route.extend(
   withForbiddenState,
@@ -18,8 +19,9 @@ export default class PoliciesRoute extends Route.extend(
   }
 
   async model() {
-    const policies = await this.store.query('policy', { reload: true });
-    const tokens = await this.store.query('token', { reload: true });
-    return { policies, tokens };
+    return await hash({
+      policies: this.store.query('policy', { reload: true }),
+      tokens: this.store.query('token', { reload: true })
+    });
   }
 }
