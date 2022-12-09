9526115b6ca8c9ca9ce68f4b7bdcf20432e6d910
Tim Gross <tgross@hashicorp.com>
2022-12-09 20:03:32 +0000

    backport of commit 0d3b86ad6aaea166508ee2d12728ef7f996c3555

--- client/pluginmanager/csimanager/manager.go
+++ client/pluginmanager/csimanager/manager.go
@@ -149,7 +149,7 @@ func (c *csiManager) resyncPluginsFromRegistry(ptype string) {
 
 // handlePluginEvent syncs a single event against the plugin registry
 func (c *csiManager) handlePluginEvent(event *dynamicplugins.PluginUpdateEvent) {
-	if event == nil {
+	if event == nil || event.Info == nil {
 		return
 	}
 	c.logger.Trace("dynamic plugin event",
