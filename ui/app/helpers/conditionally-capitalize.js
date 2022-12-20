59381ecbe273d1bc6d6dbb825c190550f66aee8c
Phil Renaud <phil@riotindustries.com>
2022-12-20 10:32:36 -0500

    new helper to conditionally capitalize handlebars strings

---
+++ ui/app/helpers/conditionally-capitalize.js
@@ -0,0 +1,13 @@
+import Helper from '@ember/component/helper';
+
+/**
+ * If the condition is true, capitalize the first letter of the term.
+ * Otherwise, return the term in lowercase.
+ */
+export function conditionallyCapitalize([term, condition]) {
+  return condition
+    ? `${term.charAt(0).toUpperCase()}${term.substring(1)}`
+    : term.toLowerCase();
+}
+
+export default Helper.helper(conditionallyCapitalize);
