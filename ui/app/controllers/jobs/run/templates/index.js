407191b235f834782e464aba4e7898a3cae3625d
Jai Bhagat <jaybhagat841@gmail.com>
2022-12-22 15:02:12 -0500

    ui: handle reading template variable

--- ui/app/controllers/jobs/run/templates/index.js
+++ ui/app/controllers/jobs/run/templates/index.js
@@ -11,21 +11,17 @@ export default class JobsRunTemplatesController extends Controller {
   get templates() {
     return this.model.map((templateVariable) => {
       // THIS LOGIC SHOULD LIKELY MOVE TO THE SERIALIZATION LAYER
-      const { value: label } = templateVariable.keyValues.find((el) => {
-        return el.key === 'label';
-      });
-
-      const { value: description } = templateVariable.keyValues.find((el) => {
+      const description = templateVariable?.keyValues?.find((el) => {
         return el.key === 'description';
-      });
+      })?.value;
 
-      const { value: json } = templateVariable.keyValues.find((el) => {
-        return el.key === 'json';
-      });
+      const json = templateVariable?.keyValues?.find((el) => {
+        return el.key === 'template';
+      })?.value;
 
       return {
         id: templateVariable.id,
-        label,
+        label: templateVariable.path.split('nomad/job-templates/')[1],
         description,
         json,
       };
@@ -37,18 +33,20 @@ export default class JobsRunTemplatesController extends Controller {
     this.selectedTemplate = e.target.id;
   }
 
+  @action
   navigateToJobsRun(plainId) {
     return this.router.transitionTo('jobs.run', { queryParams: { plainId } });
   }
 
   @action
-  async onApply() {
+  onApply() {
     const job = getOwner(this).lookup('controller:jobs.run.index').model;
-    const { json } = this.templates.find(
+    const { json } = this.templates?.find(
       (template) => template.id === this.selectedTemplate
     );
+
     job.set('_newDefinition', json);
-    await job.parse();
+    job.setIdByPayload(json);
     this.navigateToJobsRun(job.plainId);
   }
 }
