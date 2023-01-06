92d20b19df594c2c83a21d0fff9241a5667fbbf8
Jai Bhagat <jaybhagat841@gmail.com>
2023-01-06 14:11:02 -0500

    refact: model should query variables to populate editor

--- ui/app/routes/jobs/run/index.js
+++ ui/app/routes/jobs/run/index.js
@@ -1,12 +1,19 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import classic from 'ember-classic-decorator';
+import notifyForbidden from 'nomad-ui/utils/notify-forbidden';
 
 @classic
 export default class RunRoute extends Route {
   @service can;
   @service store;
   @service system;
+
+  queryParams = {
+    template: {
+      refreshModel: true,
+    },
+  };
 
   beforeModel(transition) {
     if (
@@ -18,26 +25,34 @@ export default class RunRoute extends Route {
     }
   }
 
-  async model() {
-    const transition = arguments[1];
+  async model({ template }) {
+    try {
+      // When jobs are created with a namespace attribute, it is verified against
+      // available namespaces to prevent redirecting to a non-existent namespace.
+      await this.store.findAll('namespace');
 
-    if (transition.from?.name === 'jobs.run.templates.index') {
-      const plainId = transition.to.queryParams.plainId;
-      return this.store.peekAll('job').findBy('plainId', plainId);
-    }
+      // If template is set in URL, create the job model and add the definition
+      if (template) {
+        const templateRecord = await this.store.findRecord(
+          'variable',
+          template
+        );
 
-    // When jobs are created with a namespace attribute, it is verified against
-    // available namespaces to prevent redirecting to a non-existent namespace.
-    return this.store.findAll('namespace').then(() => {
+        return this.store.createRecord('job', {
+          _newDefinition: templateRecord.keyValues?.find((el) => {
+            return el.key === 'template';
+          })?.value,
+        });
+      }
+
       return this.store.createRecord('job');
-    });
+    } catch (e) {
+      notifyForbidden(this)(e);
+    }
   }
 
-  resetController(controller, isExiting, transition) {
+  resetController(controller, isExiting) {
     if (isExiting) {
-      if (transition.to.name === 'jobs.run.templates.index') {
-        return;
-      }
       controller.model.deleteRecord();
     }
   }
