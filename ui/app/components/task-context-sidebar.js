17531ca612d01f4e2bf3847ce7eabab082ca5279
Phil Renaud <phil.renaud@hashicorp.com>
2023-01-10 16:35:53 -0500

    Basic sidebar expander (#15735)

--- ui/app/components/task-context-sidebar.js
+++ ui/app/components/task-context-sidebar.js
@@ -1,5 +1,7 @@
 // @ts-check
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class TaskContextSidebarComponent extends Component {
   get isSideBarOpen() {
@@ -13,4 +15,9 @@ export default class TaskContextSidebarComponent extends Component {
       action: () => this.args.fns.closeSidebar(),
     },
   ];
+
+  @tracked wide = false;
+  @action toggleWide() {
+    this.wide = !this.wide;
+  }
 }
