2bebc1011623a4336ed50fd3d211483eeba3a3bf
Jai Bhagat <jaybhagat841@gmail.com>
2023-01-10 09:09:18 -0500

    ui: create job template

---
+++ ui/app/controllers/jobs/run/templates/new.js
@@ -0,0 +1,71 @@
+import { A } from '@ember/array';
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+const ALL_NAMESPACE_WILDCARD = '*';
+
+export default class JobsRunTemplatesController extends Controller {
+  @service router;
+  @tracked templateName = null;
+  @tracked templateNamespace = 'default';
+
+  keyValues = A([]);
+
+  get namespaceOptions() {
+    const namespaces = this.store
+      .peekAll('namespace')
+      .map(({ name }) => ({ key: name, label: name }));
+
+    if (namespaces.length <= 1) return null;
+
+    // Create default namespace selection
+    namespaces.unshift({
+      key: ALL_NAMESPACE_WILDCARD,
+      label: 'All (*)',
+    });
+
+    return namespaces;
+  }
+
+  @action
+  updateKeyValue(key, value) {
+    if (this.keyValues.find((kv) => kv.key === key)) {
+      this.keyValues.find((kv) => kv.key === key).value = value;
+    } else {
+      this.keyValues.pushObject({ key, value });
+    }
+  }
+
+  @action
+  async save(e, overwrite = false) {
+    if (e.type === 'submit') {
+      e.preventDefault();
+    }
+
+    if (this.model?.isNew) {
+      if (this.namespaceOptions) {
+        this.model.set('namespace', this.templateNamespace);
+      } else {
+        const [namespace] = this.store.peekAll('namespace').toArray();
+        this.model.set('namespace', namespace.id);
+      }
+    }
+
+    this.model.set('keyValues', this.keyValues);
+    this.model.set('path', `nomad/job-templates/${this.templateName}`);
+    this.model.setAndTrimPath();
+    await this.model.save({ adapterOptions: { overwrite } });
+
+    this.flashMessages.add({
+      title: 'Job template saved',
+      message: `${this.templateName} successfully saved`,
+      type: 'success',
+      destroyOnClick: false,
+      timeout: 5000,
+    });
+
+    this.router.transitionTo('jobs.run.templates');
+  }
+}
---
+++ ui/app/routes/jobs/run/templates/new.js
@@ -0,0 +1,45 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import notifyForbidden from 'nomad-ui/utils/notify-forbidden';
+
+export default class RunRoute extends Route {
+  @service can;
+  @service router;
+  @service store;
+  @service system;
+
+  beforeModel(transition) {
+    if (
+      this.can.cannot('run job', null, {
+        namespace: transition.to.queryParams.namespace,
+      })
+    ) {
+      this.router.transitionTo('jobs');
+    }
+  }
+
+  async model() {
+    try {
+      // When variables are created with a namespace attribute, it is verified against
+      // available namespaces to prevent redirecting to a non-existent namespace.
+      await this.store.findAll('namespace');
+
+      return this.store.createRecord('variable');
+    } catch (e) {
+      notifyForbidden(this)(e);
+    }
+  }
+
+  resetController(controller, isExiting) {
+    if (
+      isExiting &&
+      controller.model.isNew &&
+      !controller.model.isDestroyed &&
+      !controller.model.isDestroying
+    ) {
+      controller.model?.deleteRecord();
+    }
+    controller.set('templateName', null);
+    controller.set('templateNamespace', 'default');
+  }
+}
