91abe9e27b2cc18113ac47b8351308341cddfe8b
Jai Bhagat <jaybhagat841@gmail.com>
2023-01-10 09:06:54 -0500

    chore: remove dead code

--- ui/app/controllers/jobs/run/templates/index.js
+++ ui/app/controllers/jobs/run/templates/index.js
@@ -1,10 +1,8 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class JobsRunTemplatesController extends Controller {
-  @service router;
   @tracked selectedTemplate = null;
 
   get templates() {
