4c1eb579193b888b374e5d8f2ebd193ea17ed14a
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2023-01-24 18:27:45 +0000

    Make arch flexible for CNI install script

--- scripts/vagrant-linux-priv-cni.sh
+++ scripts/vagrant-linux-priv-cni.sh
@@ -2,12 +2,18 @@
 
 set -o errexit
 
+ARCH="amd64"
+if ( which go > /dev/null ); then
+  ARCH=$(go env GOARCH)
+fi
+
 VERSION="v1.0.0"
-DOWNLOAD=https://github.com/containernetworking/plugins/releases/download/${VERSION}/cni-plugins-linux-amd64-${VERSION}.tgz
+DOWNLOAD=https://github.com/containernetworking/plugins/releases/download/${VERSION}/cni-plugins-linux-${ARCH}-${VERSION}.tgz
 TARGET_DIR=/opt/cni/bin
 CONFIG_DIR=/opt/cni/config
 
 function install_cni() {
+	echo "Installing CNI Plugins..."
 	mkdir -p ${TARGET_DIR} ${CONFIG_DIR}
 	if [[ -e ${TARGET_DIR}/${VERSION} ]] ; then
 		return
@@ -15,6 +21,7 @@ function install_cni() {
 
 	curl -sSL --fail -o /tmp/cni-plugins.tar.gz ${DOWNLOAD}
 	tar -xf /tmp/cni-plugins.tar.gz -C ${TARGET_DIR}
+	echo "  - Installed ${VERSION} with $(ls ${TARGET_DIR}|wc -l) plugins."
 	touch ${TARGET_DIR}/${VERSION}
 }
 
