4335576b8216fb0516e249e60256614d18747768
Tim Gross <tgross@hashicorp.com>
2023-01-25 11:18:07 -0500

    WI: allow workloads to use RPCs for Agent/Client APIs

--- nomad/client_stats_endpoint.go
+++ nomad/client_stats_endpoint.go
@@ -23,19 +23,24 @@ func NewClientStatsEndpoint(srv *Server) *ClientStats {
 }
 
 func (s *ClientStats) Stats(args *nstructs.NodeSpecificRequest, reply *structs.ClientStatsResponse) error {
+
 	// We only allow stale reads since the only potentially stale information is
 	// the Node registration and the cost is fairly high for adding another hope
 	// in the forwarding chain.
 	args.QueryOptions.AllowStale = true
+	authErr := s.srv.Authenticate(nil, args)
 
 	// Potentially forward to a different region.
 	if done, err := s.srv.forward("ClientStats.Stats", args, args, reply); done {
 		return err
 	}
+	if authErr != nil {
+		return nstructs.ErrPermissionDenied
+	}
 	defer metrics.MeasureSince([]string{"nomad", "client_stats", "stats"}, time.Now())
 
 	// Check node read permissions
-	if aclObj, err := s.srv.ResolveToken(args.AuthToken); err != nil {
+	if aclObj, err := s.srv.ResolveACL(args); err != nil {
 		return err
 	} else if aclObj != nil && !aclObj.AllowNodeRead() {
 		return nstructs.ErrPermissionDenied
