1e84a53c1b00a662ee1ccf2d7b19607a6e1821c9
Luiz Aoqui <luiz@hashicorp.com>
2023-01-26 17:08:13 -0500

    ui: fix alloc memory stats to match CLI output (#15909)

--- ui/app/utils/classes/allocation-stats-tracker.js
+++ ui/app/utils/classes/allocation-stats-tracker.js
@@ -29,6 +29,13 @@ const sortMap = [
 const taskPrioritySort = (a, b) =>
   sortMap[a.lifecycleName] - sortMap[b.lifecycleName];
 
+// Select the value for memory usage.
+// Must match logic in command/alloc_status.go.
+const memoryUsed = (frame) =>
+  frame.ResourceUsage.MemoryStats.RSS ||
+  frame.ResourceUsage.MemoryStats.Usage ||
+  0;
+
 @classic
 class AllocationStatsTracker extends EmberObject.extend(AbstractStatsTracker) {
   // Set via the stats computed property macro
@@ -49,15 +56,11 @@ class AllocationStatsTracker extends EmberObject.extend(AbstractStatsTracker) {
       percent: percent(cpuUsed, this.reservedCPU),
     });
 
-    const memoryUsed =
-      frame.ResourceUsage.MemoryStats.Usage ||
-      frame.ResourceUsage.MemoryStats.RSS ||
-      0;
-
+    const memUsed = memoryUsed(frame);
     this.memory.pushObject({
       timestamp,
-      used: memoryUsed,
-      percent: percent(memoryUsed / 1024 / 1024, this.reservedMemory),
+      used: memUsed,
+      percent: percent(memUsed / 1024 / 1024, this.reservedMemory),
     });
 
     let aggregateCpu = 0;
@@ -84,11 +87,7 @@ class AllocationStatsTracker extends EmberObject.extend(AbstractStatsTracker) {
         percentStack: percentCpuTotal + aggregateCpu,
       });
 
-      const taskMemoryUsed =
-        taskFrame.ResourceUsage.MemoryStats.Usage ||
-        taskFrame.ResourceUsage.MemoryStats.RSS ||
-        0;
-
+      const taskMemoryUsed = memoryUsed(taskFrame);
       const percentMemoryTotal = percent(
         taskMemoryUsed / 1024 / 1024,
         this.reservedMemory
