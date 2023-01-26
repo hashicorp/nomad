3daf71af5710f56288923f35e355b4c81696db07
Seth Hoenig <shoenig@duck.com>
2023-01-26 10:57:37 -0600

    artifact: enable reading system git/mercurial configuration
    This PR adjusts the artifact sandbox on Linux to enable reading from known
    system-wide git or mercurial configuration, if they exist.
    
    Folks doing something odd like specifying custom paths for global config will
    need to use the standard locations, or disable artifact filesystem isolation.

--- client/allocrunner/taskrunner/getter/util_linux.go
+++ client/allocrunner/taskrunner/getter/util_linux.go
@@ -3,6 +3,7 @@
 package getter
 
 import (
+	"os"
 	"path/filepath"
 	"syscall"
 
@@ -77,6 +78,34 @@ func lockdown(allocDir, taskDir string) error {
 		landlock.Dir(allocDir, "rwc"),
 		landlock.Dir(taskDir, "rwc"),
 	}
+	paths = append(paths, systemVersionControlGlobalConfigs()...)
 	locker := landlock.New(paths...)
 	return locker.Lock(landlock.Mandatory)
+}
+
+func systemVersionControlGlobalConfigs() []*landlock.Path {
+	const (
+		gitGlobalFile = "/etc/gitconfig"        // https://git-scm.com/docs/git-config#SCOPES
+		hgGlobalFile  = "/etc/mercurial/hgrc"   // https://www.mercurial-scm.org/doc/hgrc.5.html#files
+		hgGlobalDir   = "/etc/mercurial/hgrc.d" // https://www.mercurial-scm.org/doc/hgrc.5.html#files
+	)
+	return loadVersionControlGlobalConfigs(gitGlobalFile, hgGlobalFile, hgGlobalDir)
+}
+
+func loadVersionControlGlobalConfigs(gitGlobalFile, hgGlobalFile, hgGlobalDir string) []*landlock.Path {
+	exists := func(p string) bool {
+		_, err := os.Stat(p)
+		return err == nil
+	}
+	result := make([]*landlock.Path, 0, 3)
+	if exists(gitGlobalFile) {
+		result = append(result, landlock.File(gitGlobalFile, "r"))
+	}
+	if exists(hgGlobalFile) {
+		result = append(result, landlock.File(hgGlobalFile, "r"))
+	}
+	if exists(hgGlobalDir) {
+		result = append(result, landlock.Dir(hgGlobalDir, "r"))
+	}
+	return result
 }
