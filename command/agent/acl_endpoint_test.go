f63792d6628d8cdc64a2c77389b16e322cfe5f7d
Seth Hoenig <shoenig@duck.com>
2023-02-02 11:03:04 -0600

    test: add missing import

--- command/agent/acl_endpoint_test.go
+++ command/agent/acl_endpoint_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/nomad/helper/uuid"
 	"github.com/hashicorp/nomad/nomad/mock"
 	"github.com/hashicorp/nomad/nomad/structs"
+	"github.com/shoenig/test/must"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
