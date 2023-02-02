fad28e4265b4923d55538a169af44c3f51a34e00
Daniel Bennett <dbennett@hashicorp.com>
2023-02-02 14:20:26 -0600

    docs: how to troubleshoot consul connect envoy (#15908)
    * largely a doc-ification of this commit message:
      d47678074bf8ae9ff2da3c91d0729bf03aee8446
      this doesn't spell out all the possible failure modes,
      but should be a good starting point for folks.
    
    * connect: add doc link to envoy bootstrap error
    
    * add Unwrap() to RecoverableError
      mainly for easier testing

--- nomad/structs/structs.go
+++ nomad/structs/structs.go
@@ -11906,6 +11906,7 @@ type KeyringRequest struct {
 type RecoverableError struct {
 	Err         string
 	Recoverable bool
+	wrapped     error
 }
 
 // NewRecoverableError is used to wrap an error and mark it as recoverable or
@@ -11918,6 +11919,7 @@ func NewRecoverableError(e error, recoverable bool) error {
 	return &RecoverableError{
 		Err:         e.Error(),
 		Recoverable: recoverable,
+		wrapped:     e,
 	}
 }
 
@@ -11938,6 +11940,10 @@ func (r *RecoverableError) IsRecoverable() bool {
 
 func (r *RecoverableError) IsUnrecoverable() bool {
 	return !r.Recoverable
+}
+
+func (r *RecoverableError) Unwrap() error {
+	return r.wrapped
 }
 
 // Recoverable is an interface for errors to implement to indicate whether or
