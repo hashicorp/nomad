4b155df59c89b4419b3081b35600b00ff3392661
Phil Renaud <phil@riotindustries.com>
2023-02-06 13:59:02 -0500

    Handle prefix case

--- ui/app/components/metadata-kv.js
+++ ui/app/components/metadata-kv.js
@@ -4,4 +4,9 @@ import { tracked } from '@glimmer/tracking';
 export default class MetadataKvComponent extends Component {
   editing = false;
   @tracked value = this.args.value;
+  get prefixedKey() {
+    return this.args.prefix
+      ? `${this.args.prefix}.${this.args.key}`
+      : this.args.key;
+  }
 }
