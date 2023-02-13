0c1bbf98ccf06ca25113dfceb104812333b69177
Seth Hoenig <shoenig@duck.com>
2023-02-13 11:21:24 -0600

    artifact: protect against unbounded artifact decompression (1.4.x) (#16126)
    * artifact: protect against unbounded artifact decompression
    
    This PR enables mitigations provided by go-getter against payloads which
    decompress into an unbounded size or file count.
    
    There are two new client config options under the artifact block:
    
    artifact.decompression_size_limit (e.g. "10GB") - the maximum amount of
    data that will be decompressed before triggering an error and cancelling
    the operation
    
    artifact.decompression_file_count_limit (e.g. 1024) - the maximum number
    of files that will be decompressed before triggering ana error and
    cancelling the operation.
    
    * fixup CR comments
    
    * deps: update to go-getter 1.7.0

--- client/allocrunner/taskrunner/getter/testing.go
+++ client/allocrunner/taskrunner/getter/testing.go
@@ -1,5 +1,4 @@
 //go:build !release
-// +build !release
 
 package getter
 
@@ -8,12 +7,17 @@ import (
 
 	"github.com/hashicorp/go-hclog"
 	clientconfig "github.com/hashicorp/nomad/client/config"
+	"github.com/hashicorp/nomad/helper/pointer"
 	"github.com/hashicorp/nomad/nomad/structs/config"
-	"github.com/stretchr/testify/require"
+	"github.com/shoenig/test/must"
 )
 
+// TestDefaultGetter creates a Getter suitable for unit test cases.
 func TestDefaultGetter(t *testing.T) *Getter {
-	getterConf, err := clientconfig.ArtifactConfigFromAgent(config.DefaultArtifactConfig())
-	require.NoError(t, err)
+	defaultConfig := config.DefaultArtifactConfig()
+	defaultConfig.DecompressionSizeLimit = pointer.Of("1MB")
+	defaultConfig.DecompressionFileCountLimit = pointer.Of(10)
+	getterConf, err := clientconfig.ArtifactConfigFromAgent(defaultConfig)
+	must.NoError(t, err)
 	return NewGetter(hclog.NewNullLogger(), getterConf)
 }
