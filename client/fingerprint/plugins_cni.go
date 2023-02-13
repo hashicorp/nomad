c41ccc6cd38e572a5be32b3d34e540d4c1f7fe12
Seth Hoenig <shoenig@duck.com>
2023-02-13 14:55:56 -0600

    cni: handle multi-path cni_path when fingerprinting plugins (#16163)
    This PR fixes the CNI plugin fingerprinter to take into account the fact
    that the cni_path config can be a multi-path (e.g. `/foo:/bar:/baz`).
    
    Accumulate plugins from each of the possible path elements. If scanning
    any of the named directory fails, the fingerprinter fails.
    
    Fixes #16083
    
    No CL/BP - has not shipped yet.

--- client/fingerprint/plugins_cni.go
+++ client/fingerprint/plugins_cni.go
@@ -40,24 +40,28 @@ func (f *PluginsCNIFingerprint) Fingerprint(req *FingerprintRequest, resp *Finge
 		return nil
 	}
 
-	// list the cni_path directory
-	entries, err := f.lister(cniPath)
-	switch {
-	case err != nil:
-		f.logger.Warn("failed to read CNI plugins directory", "cni_path", cniPath, "error", err)
-		resp.Detected = false
-		return nil
-	case len(entries) == 0:
-		f.logger.Debug("no CNI plugins found", "cni_path", cniPath)
-		resp.Detected = true
-		return nil
-	}
+	// cniPath could be a multi-path, e.g. /opt/cni/bin:/custom/cni/bin
+	cniPathList := filepath.SplitList(cniPath)
+	for _, cniPath = range cniPathList {
+		// list the cni_path directory
+		entries, err := f.lister(cniPath)
+		switch {
+		case err != nil:
+			f.logger.Warn("failed to read CNI plugins directory", "cni_path", cniPath, "error", err)
+			resp.Detected = false
+			return nil
+		case len(entries) == 0:
+			f.logger.Debug("no CNI plugins found", "cni_path", cniPath)
+			resp.Detected = true
+			return nil
+		}
 
-	// for each file in cni_path, detect executables and try to get their version
-	for _, entry := range entries {
-		v, ok := f.detectOne(cniPath, entry)
-		if ok {
-			resp.AddAttribute(f.attribute(entry.Name()), v)
+		// for each file in cni_path, detect executables and try to get their version
+		for _, entry := range entries {
+			v, ok := f.detectOnePlugin(cniPath, entry)
+			if ok {
+				resp.AddAttribute(f.attribute(entry.Name()), v)
+			}
 		}
 	}
 
@@ -70,7 +74,7 @@ func (f *PluginsCNIFingerprint) attribute(filename string) string {
 	return fmt.Sprintf("%s.%s", cniPluginAttribute, filename)
 }
 
-func (f *PluginsCNIFingerprint) detectOne(cniPath string, entry os.DirEntry) (string, bool) {
+func (f *PluginsCNIFingerprint) detectOnePlugin(pluginPath string, entry os.DirEntry) (string, bool) {
 	fi, err := entry.Info()
 	if err != nil {
 		f.logger.Debug("failed to read cni directory entry", "error", err)
@@ -82,7 +86,7 @@ func (f *PluginsCNIFingerprint) detectOne(cniPath string, entry os.DirEntry) (st
 		return "", false // not executable
 	}
 
-	exePath := filepath.Join(cniPath, fi.Name())
+	exePath := filepath.Join(pluginPath, fi.Name())
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
