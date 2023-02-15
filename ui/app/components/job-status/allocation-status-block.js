2e74e75cbd17752af4cf1c01a46f58bd6d1813c8
Phil Renaud <phil@riotindustries.com>
2023-02-15 17:03:52 -0500

    Still very hacky but seems deece

--- ui/app/components/job-status/allocation-status-block.js
+++ ui/app/components/job-status/allocation-status-block.js
@@ -1,13 +1,33 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
 
 export default class JobStatusAllocationStatusBlockComponent extends Component {
   // Only show as much as can reasonably fit in the panel, given @cxount and @percentage
+
+  @tracked element = null;
+  @alias('element.clientWidth') width;
+
   get countToShow() {
-    console.log('CTS', this.args.status, this.args.count/this.args.percentage);
-    return 10;
+    // TODO: 60 is a magic number representing the rest element + 10px gap. Make less magic.
+    console.log('consider percentage', this.args.percentage, this.args.count, this.args.percentage * this.args.count)
+    // Show only as many as can fit within width, assuming each is 30px wide
+    // console.log('CTS', this.args.status, Math.floor((this.width - 60) / 30));
+    // Show all if there's room
+    // console.log('about to compare for', this.args.status, this.args.count, this.width / 30);
+    let cts = Math.floor((this.width-60) / 20);
+    return cts > 0 ? cts : 0;
   }
 
   get remaining() {
     return this.args.count - this.countToShow;
   }
+
+  @action
+  captureElement(element) {
+    this.element = element;
+    console.log('elly', element.clientWidth);
+  }
+
 }
--- ui/mirage/factories/job-summary.js
+++ ui/mirage/factories/job-summary.js
@@ -14,11 +14,11 @@ export default Factory.extend({
         summary[group] = {
           Queued: faker.random.number(10),
           Complete: faker.random.number(10),
-          Failed: 10,
-          Running: 10,
+          Failed: 15,
+          Running: 50,
           Starting: faker.random.number(10),
           Lost: faker.random.number(10),
-          Unknown: 7,
+          Unknown: 50,
         };
         return summary;
       }, {});
