07addac4aef4e3e65c67c1e2a17a6d0ec76637f1
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2023-02-16 14:19:32 -0500

    Create handler for streaming metrics

--- command/agent/metrics_endpoint.go
+++ command/agent/metrics_endpoint.go
@@ -1,9 +1,11 @@
 package agent
 
 import (
+	"encoding/json"
 	"net/http"
 	"sync"
 
+	hclog "github.com/hashicorp/go-hclog"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -46,4 +48,50 @@ func (s *HTTPServer) prometheusHandler() http.Handler {
 		promHandler = promhttp.HandlerFor(prometheus.DefaultGatherer, handlerOptions)
 	})
 	return promHandler
+}
+
+// MetricsStreamRequest streams metrics back to the caller.
+func (s *HTTPServer) MetricsStreamRequest(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
+	if req.Method != "GET" {
+		return nil, CodedError(http.StatusMethodNotAllowed, ErrInvalidMethod)
+	}
+
+	//TODO: Fetch the ACL token, if any, and enforce agent policy.
+
+	flusher, ok := resp.(http.Flusher)
+	if !ok {
+		return nil, CodedError(http.StatusForbidden, "streaming not supported")
+	}
+
+	resp.WriteHeader(http.StatusOK)
+
+	// 0 byte write is needed before the Flush call so that if we are using
+	// a gzip stream it will go ahead and write out the HTTP response header
+	resp.Write([]byte(""))
+	flusher.Flush()
+
+	enc := metricsEncoder{
+		logger:  s.logger,
+		encoder: json.NewEncoder(resp),
+		flusher: flusher,
+	}
+	enc.encoder.SetIndent("", "    ")
+	s.agent.GetMetricsSink().Stream(req.Context(), enc)
+	return nil, nil
+}
+
+type metricsEncoder struct {
+	logger  hclog.Logger
+	encoder *json.Encoder
+	flusher http.Flusher
+}
+
+func (m metricsEncoder) Encode(summary interface{}) error {
+	if err := m.encoder.Encode(summary); err != nil {
+
+		m.logger.Error("failed to encode metrics summary", "error", err)
+		return err
+	}
+	m.flusher.Flush()
+	return nil
 }
