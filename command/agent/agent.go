c51f8a7b8f85e135f6662392d4d835028a5940d6
Tim Gross <tgross@hashicorp.com>
2023-02-16 09:02:30 -0500

    add validation, changelog, docs

--- command/agent/agent.go
+++ command/agent/agent.go
@@ -408,6 +408,8 @@ func convertServerConfig(agentConfig *Config) (*nomad.Config, error) {
 		dur, err := time.ParseDuration(gcInterval)
 		if err != nil {
 			return nil, err
+		} else if dur <= time.Duration(0) {
+			return nil, fmt.Errorf("csi_volume_claim_gc_interval should be greater than 0s")
 		}
 		conf.CSIVolumeClaimGCInterval = dur
 	}
