5bbb8cc72a11a30e95e8126cbca4ddc295d7caf0
Phil Renaud <phil@riotindustries.com>
2023-02-16 09:41:32 -0500

    Width from on-high

--- ui/app/components/job-status/allocation-row.js
+++ ui/app/components/job-status/allocation-row.js
@@ -1,11 +1,26 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
 
 export default class JobStatusAllocationRowComponent extends Component {
+
+  @tracked element = null;
+
+  @alias('element.clientWidth') width;
+
   get showSummaries() {
     return this.args.totalAllocs > 60; // TODO: arbitrary
   }
 
   calcPerc(count) {
-    return count / this.args.totalAllocs * 100;
+    return count / this.args.totalAllocs * this.width;
   }
+
+  @action
+  captureElement(element) {
+    this.element = element;
+    console.log('elly', element.clientWidth);
+  }
+
 }
