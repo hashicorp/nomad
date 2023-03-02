273b76aa12d4d9c9640f4d3998fec29179a9cb55
Dao Thanh Tung <ttdao.2015@accountancy.smu.edu.sg>
2023-03-03 05:16:30 +0800

    cli: sort Node value in `nomad operator raft list-peers` command (#16221)
    Signed-off-by: dttung2905 <ttdao.2015@accountancy.smu.edu.sg>

--- command/operator_raft_list.go
+++ command/operator_raft_list.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/hashicorp/nomad/api"
@@ -84,6 +85,10 @@ func (c *OperatorRaftListCommand) Run(args []string) int {
 
 	// Format it as a nice table.
 	result := []string{"Node|ID|Address|State|Voter|RaftProtocol"}
+	sort.Slice(reply.Servers, func(i, j int) bool {
+		return reply.Servers[i].Node < reply.Servers[j].Node
+	})
+
 	for _, s := range reply.Servers {
 		state := "follower"
 		if s.Leader {
