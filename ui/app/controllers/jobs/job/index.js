041f31041e81b9ed2232b5f6a3011f6666155355
Phil Renaud <phil@riotindustries.com>
2023-03-07 10:06:36 -0500

    Adding @tracked to legacy controller

--- ui/app/controllers/jobs/job/index.js
+++ ui/app/controllers/jobs/job/index.js
@@ -5,6 +5,7 @@ import { inject as service } from '@ember/service';
 import WithNamespaceResetting from 'nomad-ui/mixins/with-namespace-resetting';
 import classic from 'ember-classic-decorator';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 @classic
 export default class IndexController extends Controller.extend(
   WithNamespaceResetting
@@ -31,19 +32,21 @@ export default class IndexController extends Controller.extend(
 
   sortProperty = 'name';
   sortDescending = false;
-  activeTask = null;
+
+  @tracked activeTask = null;
 
   /**
    * @type {('current'|'historical')}
    */
+  @tracked
   statusMode = 'current';
 
   @action
   setActiveTaskQueryParam(task) {
     if (task) {
-      this.set('activeTask', `${task.allocation.id}-${task.name}`);
+      this.activeTask = `${task.allocation.id}-${task.name}`;
     } else {
-      this.set('activeTask', null);
+      this.activeTask = null;
     }
   }
 
@@ -52,6 +55,6 @@ export default class IndexController extends Controller.extend(
    */
   @action
   setStatusMode(mode) {
-    this.set('statusMode', mode);
+    this.statusMode = mode;
   }
 }
