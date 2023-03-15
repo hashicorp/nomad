324c374d5d8da43e671c9907d4ed0b8ba8d469af
Phil Renaud <phil@riotindustries.com>
2023-03-15 13:36:54 -0400

    Refactored latest-deployment new logic back into panel.js

--- ui/app/components/job-status/panel.js
+++ ui/app/components/job-status/panel.js
@@ -1,7 +1,105 @@
 // @ts-check
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+import messageFromAdapterError from 'nomad-ui/utils/message-from-adapter-error';
+import { alias } from '@ember/object/computed';
 
 export default class JobStatusPanelComponent extends Component {
+  @alias('args.job') job;
+  @alias('args.handleError') handleError = () => {};
+
+  // #region deployment
+  @tracked oldVersionAllocBlockIDs = [];
+
+  // Called via did-insert; sets a static array of "outgoing"
+  // allocations we can track throughout a deployment
+  establishOldAllocBlockIDs() {
+    this.oldVersionAllocBlockIDs = this.job.allocations.filter(
+      (a) =>
+        a.clientStatus === 'running' &&
+        a.jobVersion !== this.deployment.get('versionNumber')
+    );
+  }
+
+  get isActivelyDeploying() {
+    return this.job.get('latestDeployment.isRunning');
+  }
+
+  @task(function* () {
+    try {
+      yield this.job.latestDeployment.content.promote();
+    } catch (err) {
+      this.handleError({
+        title: 'Could Not Promote Deployment',
+        description: messageFromAdapterError(err, 'promote deployments'),
+      });
+    }
+  })
+  promote;
+
+  @task(function* () {
+    try {
+      yield this.job.latestDeployment.content.fail();
+    } catch (err) {
+      this.handleError({
+        title: 'Could Not Fail Deployment',
+        description: messageFromAdapterError(err, 'fail deployments'),
+      });
+    }
+  })
+  fail;
+
+  @alias('job.latestDeployment') deployment;
+  @alias('deployment.desiredTotal') desiredTotal;
+
+  get oldVersionAllocBlocks() {
+    return this.job.allocations
+      .filter((allocation) => this.oldVersionAllocBlockIDs.includes(allocation))
+      .reduce((alloGroups, currentAlloc) => {
+        (alloGroups[currentAlloc.clientStatus] =
+          alloGroups[currentAlloc.clientStatus] || []).push(currentAlloc);
+        return alloGroups;
+      }, {});
+  }
+
+  get newVersionAllocBlocks() {
+    let availableSlotsToFill = this.desiredTotal;
+    let allocationsOfDeploymentVersion = this.job.allocations.filter(
+      (a) => a.jobVersion === this.deployment.get('versionNumber')
+    );
+    // Only fill up to 100% of desiredTotal. Once we've filled up, we can stop counting.
+    let allocationsOfShowableType = this.allocTypes.reduce((blocks, type) => {
+      const jobAllocsOfType = allocationsOfDeploymentVersion.filterBy(
+        'clientStatus',
+        type.label
+      );
+      if (availableSlotsToFill > 0) {
+        blocks[type.label] = Array(
+          Math.min(availableSlotsToFill, jobAllocsOfType.length)
+        )
+          .fill()
+          .map((_, i) => {
+            return jobAllocsOfType[i];
+          });
+        availableSlotsToFill -= blocks[type.label].length;
+      } else {
+        blocks[type.label] = [];
+      }
+      return blocks;
+    }, {});
+    if (availableSlotsToFill > 0) {
+      allocationsOfShowableType['unplaced'] = Array(availableSlotsToFill)
+        .fill()
+        .map(() => {
+          return { clientStatus: 'unplaced' };
+        });
+    }
+    return allocationsOfShowableType;
+  }
+
+  // #endregion deployment
+
   // Build note: allocTypes order matters! We will fill up to 100% of totalAllocs in this order.
   allocTypes = [
     'running',
@@ -48,7 +146,6 @@ export default class JobStatusPanelComponent extends Component {
           return { clientStatus: 'unplaced' };
         });
     }
-    console.log('panel about to return', allocationsOfShowableType);
     return allocationsOfShowableType;
   }
 
