01752d56f281a6409547f0b200cd7d3f550ea4d4
Phil Renaud <phil@riotindustries.com>
2023-03-20 09:59:10 -0400

    Types and groups for updateParams

--- ui/app/components/job-status/update-params.js
+++ ui/app/components/job-status/update-params.js
@@ -3,12 +3,47 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
+/**
+ * @typedef {Object} DefinitionUpdateStrategy
+ * @property {boolean} AutoPromote
+ * @property {boolean} AutoRevert
+ * @property {number} Canary
+ * @property {number} MaxParallel
+ * @property {string} HealthCheck
+ * @property {number} MinHealthyTime
+ * @property {number} HealthyDeadline
+ * @property {number} ProgressDeadline
+ * @property {number} Stagger
+ */
+
+/**
+ * @typedef {Object} DefinitionTaskGroup
+ * @property {string} Name
+ * @property {number} Count
+ * @property {DefinitionUpdateStrategy} Update
+ */
+
+/**
+ * @typedef {Object} JobDefinition
+ * @property {string} ID
+ * @property {DefinitionUpdateStrategy} Update
+ * @property {DefinitionTaskGroup[]} TaskGroups
+ */
+
 export default class JobStatusUpdateParamsComponent extends Component {
+  /**
+   * @type {JobDefinition}
+   */
   @tracked rawDefinition = null;
 
   get updateParams() {
     if (this.rawDefinition) {
-      return this.rawDefinition.Update;
+      return this.rawDefinition.TaskGroups.map((tg) => {
+        return {
+          name: tg.Name,
+          update: tg.Update,
+        };
+      });
     } else {
       return null;
     }
