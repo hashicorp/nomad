7de91c93938b0e9c576d0a2b7ac782121accb000
Jai <41024828+ChaiWithJai@users.noreply.github.com>
2023-03-20 15:21:14 -0400

    ui:  Display JSON view of variables associated to job specification (#16570)
    * chore: move fixture to util
    
    * chore: update tests:
    
    * ui: display variables table

--- ui/app/controllers/jobs/job/definition.js
+++ ui/app/controllers/jobs/job/definition.js
@@ -16,6 +16,7 @@ export default class DefinitionController extends Controller.extend(
   @alias('model.definition') definition;
   @alias('model.job') job;
   @alias('model.specification') specification;
+  @alias('model.variables') variables;
 
   @service router;
 
