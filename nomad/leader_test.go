dd05f719c2fc565171f843f5834193955fdf4ac8
Juana De La Cuesta <mcj@delacuesta.co>
2023-03-22 16:38:58 +0100

    Update nomad/leader_test.go
    Co-authored-by: James Rasell <jrasell@users.noreply.github.com>

--- nomad/leader_test.go
+++ nomad/leader_test.go
@@ -622,9 +622,7 @@ func TestLeader_PeriodicDispatcher_No_Overlaps_Running_Job(t *testing.T) {
 		},
 	}
 	_, _, err := s1.raftApply(structs.JobRegisterRequestType, req)
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
+	must.NoError(t, err)
 
 	// Create an eval for the past launch.
 	eval, err := s1.periodicDispatcher.createEval(job, past)
