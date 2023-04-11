88dfde714b2090a0fd21826e065fcdf0ee9e5c9d
Seth Hoenig <shoenig@duck.com>
2023-04-11 09:04:23 -0500

    e2e: fixup callers of AllocLogs

--- e2e/isolation/chroot_test.go
+++ e2e/isolation/chroot_test.go
@@ -39,7 +39,7 @@ func testExecUsesChroot(t *testing.T) {
 	e2eutil.WaitForAllocsStopped(t, nomad, []string{allocID})
 
 	// assert log contents
-	logs, err := e2eutil.AllocLogs(allocID, e2eutil.LogsStdOut)
+	logs, err := e2eutil.AllocLogs(allocID, "", e2eutil.LogsStdOut)
 	must.NoError(t, err)
 	must.RegexMatch(t, regexp.MustCompile(`(?m:^/alloc\b)`), logs)
 	must.RegexMatch(t, regexp.MustCompile(`(?m:^/local\b)`), logs)
@@ -63,7 +63,7 @@ func testImageUsesChroot(t *testing.T) {
 	e2eutil.WaitForAllocsStopped(t, nomad, []string{allocID})
 
 	// assert log contents
-	logs, err := e2eutil.AllocLogs(allocID, e2eutil.LogsStdOut)
+	logs, err := e2eutil.AllocLogs(allocID, "", e2eutil.LogsStdOut)
 	must.NoError(t, err)
 	must.RegexMatch(t, regexp.MustCompile(`(?m:^/alloc\b)`), logs)
 	must.RegexMatch(t, regexp.MustCompile(`(?m:^/local\b)`), logs)
