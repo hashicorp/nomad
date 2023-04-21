213bc9e3a8701a0da23a1922628d37d28949bbb4
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2023-04-20 22:54:02 -0400

    Backport of fix host port handling for ipv6, fixes #16550 into release/1.5.x (#16957)
    This pull request was automerged via backport-assistant

--- command/agent/agent_endpoint_test.go
+++ command/agent/agent_endpoint_test.go
@@ -12,6 +12,7 @@ import (
 	"net/url"
 	"os"
 	"reflect"
+	"strconv"
 	"strings"
 	"sync"
 	"syscall"
@@ -155,7 +156,7 @@ func TestHTTP_AgentJoin(t *testing.T) {
 	httpTest(t, nil, func(s *TestAgent) {
 		// Determine the join address
 		member := s.Agent.Server().LocalMember()
-		addr := fmt.Sprintf("%s:%d", member.Addr, member.Port)
+		addr := net.JoinHostPort(member.Addr.String(), strconv.Itoa(int(member.Port)))
 
 		// Make the HTTP request
 		req, err := http.NewRequest("PUT",
