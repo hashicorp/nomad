c2b7fb7222d9ef9b3cfab862b518b662ec4a843c
Phil Renaud <phil@riotindustries.com>
2023-05-03 16:45:32 -0400

    Bones of some new system jobs tests

--- ui/tests/acceptance/job-status-panel-test.js
+++ ui/tests/acceptance/job-status-panel-test.js
@@ -628,4 +628,131 @@ module('Acceptance | job status panel', function (hooks) {
         .exists('No match message is shown');
     });
   });
+
+  module('System jobs', function () {
+    // TODO
+    test('System jobs show restarted but not rescheduled allocs', async function (assert) {
+      this.store = this.owner.lookup('service:store');
+
+      let job = server.create('job', {
+        status: 'running',
+        datacenters: ['*'],
+        type: 'system',
+        createAllocations: true,
+        allocStatusDistribution: {
+          running: 0.5,
+          failed: 0.5,
+          unknown: 0,
+          lost: 0,
+        },
+        noActiveDeployment: true,
+        shallow: true,
+      });
+
+      let state = server.create('task-state');
+      state.events = server.schema.taskEvents.where({ taskStateId: state.id });
+      server.schema.allocations.where({ jobId: job.id }).update({
+        taskStateIds: [state.id],
+        jobVersion: 0,
+      });
+
+      await visit(`/jobs/${job.id}`);
+      assert.dom('.job-status-panel').exists();
+      assert.ok(true);
+      // await this.pauseTest();
+      // assert
+      //   .dom('.failed-or-lost')
+      //   .exists({ count: 2 }, 'Restarted and Rescheduled cells are both present');
+
+      // let rescheduledCell = [...findAll('.failed-or-lost')][0];
+      // let restartedCell = [...findAll('.failed-or-lost')][1];
+
+      // // Check that the title in each cell has the right text
+      // assert.dom(rescheduledCell.querySelector('h4')).hasText('Rescheduled');
+      // assert.dom(restartedCell.querySelector('h4')).hasText('Restarted');
+
+      // // Check that both values are zero and non-links
+      // assert
+      //   .dom(rescheduledCell.querySelector('a'))
+      //   .doesNotExist('Rescheduled cell is not a link');
+      // assert
+      //   .dom(rescheduledCell.querySelector('.failed-or-lost-link'))
+      //   .hasText('0', 'Rescheduled cell has zero value');
+      // assert
+      //   .dom(restartedCell.querySelector('a'))
+      //   .doesNotExist('Restarted cell is not a link');
+      // assert
+      //   .dom(restartedCell.querySelector('.failed-or-lost-link'))
+      //   .hasText('0', 'Restarted cell has zero value');
+
+      // // A wild event appears! Change a recent task event to type "Restarting" in a task state:
+      // this.store
+      //   .peekAll('job')
+      //   .objectAt(0)
+      //   .get('allocations')
+      //   .objectAt(0)
+      //   .get('states')
+      //   .objectAt(0)
+      //   .get('events')
+      //   .objectAt(0)
+      //   .set('type', 'Restarting');
+
+      // await settled();
+
+      // assert
+      //   .dom(restartedCell.querySelector('.failed-or-lost-link'))
+      //   .hasText(
+      //     '1',
+      //     'Restarted cell updates when a task event with type "Restarting" is added'
+      //   );
+
+      // this.store
+      //   .peekAll('job')
+      //   .objectAt(0)
+      //   .get('allocations')
+      //   .objectAt(1)
+      //   .get('states')
+      //   .objectAt(0)
+      //   .get('events')
+      //   .objectAt(0)
+      //   .set('type', 'Restarting');
+
+      // await settled();
+
+      // // Trigger a reschedule! Set up a desiredTransition object with a Reschedule property on one of the allocations.
+      // assert
+      //   .dom(restartedCell.querySelector('.failed-or-lost-link'))
+      //   .hasText(
+      //     '2',
+      //     'Restarted cell updates when a second task event with type "Restarting" is added'
+      //   );
+
+      // this.store
+      //   .peekAll('job')
+      //   .objectAt(0)
+      //   .get('allocations')
+      //   .objectAt(0)
+      //   .get('followUpEvaluation')
+      //   .set('content', { 'test-key': 'not-empty' });
+
+      // await settled();
+
+      // assert
+      //   .dom(rescheduledCell.querySelector('.failed-or-lost-link'))
+      //   .hasText('1', 'Rescheduled cell updates when desiredTransition is set');
+      // assert
+      //   .dom(rescheduledCell.querySelector('a'))
+      //   .exists('Rescheduled cell with a non-zero number is now a link');
+    });
+
+    // TODO
+    test("System jobs' totalAllocs count matches the number of nodes that exist", async function (assert) {
+      assert.ok(true);
+    });
+
+    // TODO, post-https://github.com/hashicorp/nomad/pull/17073 merge
+    test('System jobs handle node:read permissions', async function (assert) {
+      assert.ok(true);
+    });
+  });
 });
