4e5c2d653084e088b831cb3388d451c0959c0108
Daniel Bennett <dbennett@hashicorp.com>
2023-05-09 17:22:13 -0500

    testutil for test container image names
    func TestDockerImage
      uses our docker mirror if in CI,
      otherwise docker.io
    
    func TestBusyboxImage
      uses BUSYBOX_IMAGE env var if set,
      otherwise busybox:1 on linux,
      or a special windows image

---
+++ testutil/container_images.go
@@ -0,0 +1,41 @@
+package testutil
+
+import (
+	"os"
+	"runtime"
+)
+
+const (
+	// BusyboxImageEnvName is always used, if set, by TestBusyboxImage()
+	BusyboxImageEnvName = "BUSYBOX_IMAGE"
+)
+
+func TestDockerImage(name, tag string) string {
+	img := name + ":" + tag
+	if IsCI() {
+		// use our mirror to avoid rate-limiting in CI
+		img = "docker.mirror.hashicorp.services/" + img
+	} else {
+		// explicitly include docker.io for podman
+		img = "docker.io/" + img
+	}
+	return img
+}
+
+func TestBusyboxImage() string {
+	// if env is set, use it verbatim
+	if img, ok := os.LookupEnv(BusyboxImageEnvName); ok {
+		return img
+	}
+
+	// otherwise, figure it out
+	name := "busybox"
+	tag := "1"
+	if runtime.GOOS == "windows" {
+		// this image is maintained in https://github.com/hashicorp/windows-busybox
+		name = "hashicorpdev/busybox-windows"
+		// TODO: tag = "ltsc2022"
+		tag = "server2016-0.1"
+	}
+	return TestDockerImage(name, tag)
+}
