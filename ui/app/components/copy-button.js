c47c2c988f35736925ab8569e06c2ece39b80b43
Jai <41024828+ChaiWithJai@users.noreply.github.com>
2023-05-12 09:54:13 -0400

    16664/upgrade (#17158)
    * chore:  upgrade
    
    Upgrade @babel/helper-string-parserprop-types
    
    * chore: add resolution
    
    * chore: update component API for breaking changes
    
    * chore: update arguments
    
    * api: forgive user for pass wrong args
    
    * chore:  update tests
    
    * chore: update yarn lock
    
    * chore: upgrade to Glimmer component
    
    * styling: add properties to component invocation
    
    * chore: add inset styles

--- ui/app/components/copy-button.js
+++ ui/app/components/copy-button.js
@@ -3,23 +3,27 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { task, timeout } from 'ember-concurrency';
-import { classNames, classNameBindings } from '@ember-decorators/component';
-import classic from 'ember-classic-decorator';
 
-@classic
-@classNames('copy-button')
-@classNameBindings('inset')
 export default class CopyButton extends Component {
-  clipboardText = null;
-  state = null;
+  @tracked state = null;
+
+  get text() {
+    if (typeof this.args.clipboardText === 'function')
+      return this.args.clipboardText;
+    if (typeof this.args.clipboardText === 'string')
+      return this.args.clipboardText;
+
+    return String(this.args.clipboardText);
+  }
 
   @(task(function* () {
-    this.set('state', 'success');
+    this.state = 'success';
 
     yield timeout(2000);
-    this.set('state', null);
+    this.state = null;
   }).restartable())
   indicateSuccess;
 }
