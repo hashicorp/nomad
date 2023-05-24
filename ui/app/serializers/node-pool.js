fa12b48830b408d8c0d96ee5e8cd0bf91f7c1e9e
Jai Bhagat <jaybhagat841@gmail.com>
2023-05-24 11:38:37 -0400

    ui: node pool serializer

---
+++ ui/app/serializers/node-pool.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import ApplicationSerializer from './application';
+import classic from 'ember-classic-decorator';
+
+@classic
+export default class NodePool extends ApplicationSerializer {
+  primaryKey = 'Name';
+}
