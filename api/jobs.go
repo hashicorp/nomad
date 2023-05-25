2e7684753a798aaec8c03869c70a3e3f5f69cb51
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2023-05-25 16:17:01 +0000

    backport of commit 3b0070b0a4bd5a0659d0e8a42bb645ac07d49f07

--- api/jobs.go
+++ api/jobs.go
@@ -938,7 +938,7 @@ func (j *Job) Canonicalize() {
 		j.Namespace = pointerOf(DefaultNamespace)
 	}
 	if j.Priority == nil {
-		j.Priority = pointerOf(0)
+		j.Priority = pointerOf(1)
 	}
 	if j.Stop == nil {
 		j.Stop = pointerOf(false)
