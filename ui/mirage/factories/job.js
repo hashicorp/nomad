717c252311ffe006cde76da29f991d644f247f01
Jai <41024828+ChaiWithJai@users.noreply.github.com>
2023-05-31 09:38:49 -0400

    chore: setup mirage models + factory hooks (#17321)
    * chore: setup mirage models + factory hooks
    
    * Acceptance test hardening to avoid faker/mirage randomness
    
    ---------
    
    Co-authored-by: Phil Renaud <phil@riotindustries.com>

--- ui/mirage/factories/job.js
+++ ui/mirage/factories/job.js
@@ -214,6 +214,15 @@ export default Factory.extend({
       });
     }
 
+    if (!job.nodePool) {
+      const nodePool = server.db.nodePools.length
+        ? pickOne(server.db.nodePools).name
+        : server.create('node-pool').name;
+      job.update({
+        nodePool,
+      });
+    }
+
     const groupProps = {
       job,
       createAllocations: job.createAllocations,
@@ -368,6 +377,7 @@ export default Factory.extend({
         datacenters: job.datacenters,
         createAllocations: job.createAllocations,
         shallow: job.shallow,
+        noActiveDeployment: job.noActiveDeployment,
       });
     }
   },
