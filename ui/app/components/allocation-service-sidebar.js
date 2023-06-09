667d0026cde50db770979b0341ac918228cae9ae
Phil Renaud <phil.renaud@hashicorp.com>
2023-06-09 15:43:11 -0400

    [ui] Don't show a service as healthy when its parent alloc is not running (#17465)
    * Fix: dont show a service as healthy when its parent alloc is not running
    
    * Test for Health Unknown

--- ui/app/components/allocation-service-sidebar.js
+++ ui/app/components/allocation-service-sidebar.js
@@ -38,6 +38,7 @@ export default class AllocationServiceSidebarComponent extends Component {
   }
 
   get aggregateStatus() {
+    if (this.args.allocation?.clientStatus !== 'running') return 'Unknown';
     return this.checks.any((check) => check.Status === 'failure')
       ? 'Unhealthy'
       : 'Healthy';
