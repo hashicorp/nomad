cc582dec146f86d40d4401e933e494fcd234a453
Phil Renaud <phil@riotindustries.com>
2023-06-12 12:41:51 -0400

    Backburner override

--- ui/tests/test-helper.js
+++ ui/tests/test-helper.js
@@ -11,18 +11,15 @@ import { setApplication } from '@ember/test-helpers';
 import start from 'ember-exam/test-support/start';
 import { setup } from 'qunit-dom';
 import './helpers/flash-message';
-import Ember from 'ember';
+
+import { _backburner } from '@ember/runloop';
+import { RSVP } from '@ember/-internals/runtime';
+
+RSVP.configure('async', function (callback, promise) {
+  _backburner.schedule('actions', () => callback(promise));
+});
 
 setApplication(Application.create(config.APP));
-
-Ember.onerror = function (err) {
-  console.log('an onerror event has occurred and is being overridden');
-  console.error(err);
-  console.log('stringified', JSON.stringify(err));
-  console.log('end of onerror event');
-  QUnit.assert.ok(false, err);
-  return new Error(err);
-};
 
 setup(QUnit.assert);
 
