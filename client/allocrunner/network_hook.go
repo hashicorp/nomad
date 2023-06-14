3474f94b8e0e0bacbe51e13906a5b5ee14914ca8
Luiz Aoqui <luiz@hashicorp.com>
2023-06-14 09:36:02 -0400

    client: add no-op network manager implementation
    Provide a no-op implementation of the drivers.DriverNetoworkManager
    interface to be used by systems that don't support network isolation and
    prevent panics where a network manager is expected.

--- client/allocrunner/network_hook.go
+++ client/allocrunner/network_hook.go
@@ -186,10 +186,6 @@ func (h *networkHook) Postrun() error {
 		}
 	}
 
-	// h.manager is only set on Linux.
-	if h.manager != nil {
-		// issue driver destroy regardless if we have a spec (e.g. cleanup pause container)
-		return h.manager.DestroyNetwork(h.alloc.ID, h.spec)
-	}
-	return nil
+	// issue driver destroy regardless if we have a spec (e.g. cleanup pause container)
+	return h.manager.DestroyNetwork(h.alloc.ID, h.spec)
 }
