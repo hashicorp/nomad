3e56c37a051926de7fe847222519d15779b25b82
Luiz Aoqui <luiz@hashicorp.com>
2023-06-19 17:35:06 -0400

    ci: test failure

--- main_test.go
+++ main_test.go
@@ -3,5 +3,11 @@
 
 package main
 
+import "testing"
+
 // This file is intentionally empty to force early versions of Go
 // to test compilation for tests.
+
+func TestFail(t *testing.T) {
+	t.Fatal("Boom")
+}
