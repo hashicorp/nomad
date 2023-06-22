fe945845d8133b6a69909f1541f1240221365d9a
James Rasell <jrasell@users.noreply.github.com>
2023-06-22 08:57:59 +0200

    var: add initial locking struct to core package. (#17636)

--- nomad/structs/variables_test.go
+++ nomad/structs/variables_test.go
@@ -4,12 +4,190 @@
 package structs
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/hashicorp/nomad/ci"
-	"github.com/stretchr/testify/require"
+	"github.com/shoenig/test/must"
 )
+
+func TestVariableMetadata_Equal(t *testing.T) {
+	ci.Parallel(t)
+
+	testCases := []struct {
+		name            string
+		inputMetadata   VariableMetadata
+		inputMetadataFn VariableMetadata
+		expectedOutput  bool
+	}{
+		{
+			name: "no lock equal",
+			inputMetadata: VariableMetadata{
+				Namespace:   "default",
+				Path:        "custom/test/path",
+				Lock:        nil,
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251868,
+			},
+			inputMetadataFn: VariableMetadata{
+				Namespace:   "default",
+				Path:        "custom/test/path",
+				Lock:        nil,
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251868,
+			},
+			expectedOutput: true,
+		},
+		{
+			name: "no lock unequal",
+			inputMetadata: VariableMetadata{
+				Namespace:   "default",
+				Path:        "custom/test/path",
+				Lock:        nil,
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251876,
+			},
+			inputMetadataFn: VariableMetadata{
+				Namespace:   "default",
+				Path:        "custom/test/path",
+				Lock:        nil,
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 200,
+				ModifyTime:  1687251885,
+			},
+			expectedOutput: false,
+		},
+
+		{
+			name: "lock equal",
+			inputMetadata: VariableMetadata{
+				Namespace: "default",
+				Path:      "custom/test/path",
+				Lock: &VariableLock{
+					ID:        "896bdbef-8ce7-4b1d-9b4c-4e6c5639196d",
+					TTL:       20 * time.Second,
+					LockDelay: 5 * time.Second,
+				},
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251868,
+			},
+			inputMetadataFn: VariableMetadata{
+				Namespace: "default",
+				Path:      "custom/test/path",
+				Lock: &VariableLock{
+					ID:        "896bdbef-8ce7-4b1d-9b4c-4e6c5639196d",
+					TTL:       20 * time.Second,
+					LockDelay: 5 * time.Second,
+				},
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251868,
+			},
+			expectedOutput: true,
+		},
+		{
+			name: "lock unequal",
+			inputMetadata: VariableMetadata{
+				Namespace: "default",
+				Path:      "custom/test/path",
+				Lock: &VariableLock{
+					ID:        "896bdbef-8ce7-4b1d-9b4c-4e6c5639196d",
+					TTL:       20 * time.Second,
+					LockDelay: 5 * time.Second,
+				},
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251876,
+			},
+			inputMetadataFn: VariableMetadata{
+				Namespace: "default",
+				Path:      "custom/test/path",
+				Lock: &VariableLock{
+					ID:        "896bdbef-8ce7-4b1d-9b4c-4e6c5639196d",
+					TTL:       20 * time.Second,
+					LockDelay: 15 * time.Second,
+				},
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251876,
+			},
+			expectedOutput: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			must.Eq(t, tc.expectedOutput, tc.inputMetadata.Equal(tc.inputMetadataFn))
+		})
+	}
+}
+
+func TestVariableMetadata_Copy(t *testing.T) {
+	ci.Parallel(t)
+
+	testCases := []struct {
+		name                  string
+		inputVariableMetadata *VariableMetadata
+	}{
+		{
+			name: "no lock",
+			inputVariableMetadata: &VariableMetadata{
+				Namespace:   "default",
+				Path:        "custom/test/path",
+				Lock:        nil,
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251876,
+			},
+		},
+		{
+			name: "lock",
+			inputVariableMetadata: &VariableMetadata{
+				Namespace: "default",
+				Path:      "custom/test/path",
+				Lock: &VariableLock{
+					ID:        "896bdbef-8ce7-4b1d-9b4c-4e6c5639196d",
+					TTL:       20 * time.Second,
+					LockDelay: 15 * time.Second,
+				},
+				CreateIndex: 10,
+				CreateTime:  1687251815,
+				ModifyIndex: 100,
+				ModifyTime:  1687251876,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualOutput := tc.inputVariableMetadata.Copy()
+			must.Eq(t, tc.inputVariableMetadata, actualOutput)
+			must.NotEqOp(t,
+				fmt.Sprintf("%p", tc.inputVariableMetadata),
+				fmt.Sprintf("%p", actualOutput))
+
+			if tc.inputVariableMetadata.Lock != nil {
+				must.NotEqOp(t,
+					fmt.Sprintf("%p", tc.inputVariableMetadata.Lock),
+					fmt.Sprintf("%p", actualOutput.Lock))
+			}
+		})
+	}
+}
 
 func TestStructs_VariableDecrypted_Copy(t *testing.T) {
 	ci.Parallel(t)
@@ -30,9 +208,9 @@ func TestStructs_VariableDecrypted_Copy(t *testing.T) {
 		},
 	}
 	sv2 := sv.Copy()
-	require.True(t, sv.Equal(sv2), "sv and sv2 should be equal")
+	must.True(t, sv.Equal(sv2), must.Sprintf("sv and sv2 should be equal"))
 	sv2.Items["new"] = "new"
-	require.False(t, sv.Equal(sv2), "sv and sv2 should not be equal")
+	must.False(t, sv.Equal(sv2), must.Sprintf("sv and sv2 should not be equal"))
 }
 
 func TestStructs_VariableDecrypted_Validate(t *testing.T) {
@@ -65,9 +243,9 @@ func TestStructs_VariableDecrypted_Validate(t *testing.T) {
 		sv.Path = tc.path
 		err := sv.Validate()
 		if tc.ok {
-			require.NoError(t, err, "should not get error for: %s", tc.path)
+			must.NoError(t, err, must.Sprintf("should not get error for: %s", tc.path))
 		} else {
-			require.Error(t, err, "should get error for: %s", tc.path)
+			must.Error(t, err, must.Sprintf("should get error for: %s", tc.path))
 		}
 	}
 }
