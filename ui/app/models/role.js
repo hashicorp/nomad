72654952487423894bb2f52392d8e7998e310299
Phil Renaud <phil@riotindustries.com>
2023-06-29 11:08:48 -0400

    Models and adapters

---
+++ ui/app/models/role.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// @ts-check
+import Model from '@ember-data/model';
+import { attr, hasMany } from '@ember-data/model';
+
+export default class Role extends Model {
+  @attr('string') name;
+  @attr('string') description;
+  @hasMany('policy', { defaultValue: () => [] }) policies;
+}
