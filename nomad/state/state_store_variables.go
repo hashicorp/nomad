c829e2bd3d9c7595c9b190c91c2e2eb6804d76be
Juana De La Cuesta <mcj@delacuesta.co>
2023-07-05 10:05:35 -0400

    func: add happy path test for acquire and release flows on non existing variable

--- nomad/state/state_store_variables.go
+++ nomad/state/state_store_variables.go
@@ -560,7 +560,6 @@ func (s *StateStore) VarLockAcquire(idx uint64,
 	sv, ok := raw.(*structs.VariableEncrypted)
 	// If the variable exist, we must make sure it doesn't hold a lock already
 	if ok {
-
 		if isLocked(sv.VariableMetadata.Lock, req) {
 			zeroVal := &structs.VariableEncrypted{
 				VariableMetadata: structs.VariableMetadata{
@@ -605,12 +604,8 @@ func (s *StateStore) VarLockRelease(idx uint64,
 		return req.ErrorResponse(idx, errLockNotFound)
 	}
 
-<<<<<<< HEAD
 	if req.Var.Lock != nil && sv.Lock.ID != req.Var.Lock.ID {
 		// Avoid showing the variable data on a failed lock release
-=======
-	if sv.Lock == nil || sv.Lock.ID == "" || sv.Lock.ID != req.Var.Lock.ID {
->>>>>>> 54f7b750ed (func: fix the query for missing lock to avoid nil points in case of a missing lock)
 		zeroVal := &structs.VariableEncrypted{
 			VariableMetadata: structs.VariableMetadata{
 				Namespace: sv.Namespace,
