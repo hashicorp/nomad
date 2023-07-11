23907ad927b4faced31a372f7389fe9fc75a0337
Phil Renaud <phil@riotindustries.com>
2023-07-11 14:17:12 -0400

    Boot the user off the job if it gets deleted

--- ui/app/controllers/jobs/job.js
+++ ui/app/controllers/jobs/job.js
@@ -3,9 +3,14 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+// @ts-check
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class JobController extends Controller {
+  @service router;
+  @service notifications;
   queryParams = [
     {
       jobNamespace: 'namespace',
@@ -15,5 +20,25 @@ export default class JobController extends Controller {
 
   get job() {
     return this.model;
+  }
+
+  get jobHasBeenYoinked() {
+    return (
+      this.watchers.job.isError &&
+      this.watchers.job.error.errors.some((e) => e.status === '404')
+    );
+  }
+
+  @action yoinkedJobHandler() {
+    if (this.jobHasBeenYoinked) {
+      this.notifications.add({
+        title: `Job ${this.job.name} has been deleted`,
+        message:
+          'The job you were looking at has been deleted; this is usually because it was purged from elsewhere.',
+        color: 'critical',
+        destroyOnClick: false,
+      });
+      this.router.transitionTo('jobs');
+    }
   }
 }
