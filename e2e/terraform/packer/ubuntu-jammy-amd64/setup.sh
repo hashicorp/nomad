5e2d6be5d31fe1291cf94d491242b35fc246b75e
Seth Hoenig <shoenig@duck.com>
2023-07-19 20:55:01 +0000

    cr: no need to install nomad twice

--- e2e/terraform/packer/ubuntu-jammy-amd64/setup.sh
+++ e2e/terraform/packer/ubuntu-jammy-amd64/setup.sh
@@ -45,7 +45,6 @@ sudo ufw disable || echo "ufw not installed"
 echo "Install HashiCorp apt repositories"
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt-get update && sudo apt-get install nomad
 
 echo "Installing Docker apt repositories"
 sudo install -m 0755 -d /etc/apt/keyrings
