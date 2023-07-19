ab2e6bd7bf4a6b8e193689c6f6e9790c98e0c5b8
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2023-07-19 13:31:14 -0500

    backport of commit 615e76ef3c23497f768ebd175f0c624d32aeece8 (#17993)
    This pull request was automerged via backport-assistant

--- client/stats/cpu_test.go
+++ client/stats/cpu_test.go
@@ -7,32 +7,17 @@ import (
 	"math"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/hashicorp/nomad/ci"
-	shelpers "github.com/hashicorp/nomad/helper/stats"
 	"github.com/hashicorp/nomad/helper/testlog"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestCpuStatsPercent(t *testing.T) {
-	ci.Parallel(t)
-
-	cs := NewCpuStats()
-	cs.Percent(79.7)
-	time.Sleep(1 * time.Second)
-	percent := cs.Percent(80.69)
-	expectedPercent := 98.00
-	if percent < expectedPercent && percent > (expectedPercent+1.00) {
-		t.Fatalf("expected: %v, actual: %v", expectedPercent, percent)
-	}
-}
 
 func TestHostStats_CPU(t *testing.T) {
 	ci.Parallel(t)
 
 	assert := assert.New(t)
-	assert.Nil(shelpers.Init())
+	assert.Nil(Init(0))
 
 	logger := testlog.HCLogger(t)
 	cwd, err := os.Getwd()
