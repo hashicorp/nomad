2b3dd86dc5c5f1f0d333d97ff5465b90a34fa550
Luiz Aoqui <luiz@hashicorp.com>
2023-07-21 09:16:49 -0400

    ui: handle node pool requests to older regions (#18021)
    When accessing a region running a version of Nomad without node pools an
    error was thrown because the request is handled by the nodes endpoint
    which fails because it assumes `pools` is the node ID.

--- ui/app/adapters/node-pool.js
+++ ui/app/adapters/node-pool.js
@@ -14,4 +14,21 @@ export default class NodePoolAdapter extends ApplicationAdapter {
     resource = pluralize(resource);
     return `/v1/${relationshipResource}/${resource}`;
   }
+
+  findAll() {
+    return super.findAll(...arguments).catch((error) => {
+      // Handle the case where the node pool request is sent to a region that
+      // doesn't have node pools and the request is handled by the nodes
+      // endpoint.
+      const isNodeRequest = error.message.includes(
+        'node lookup failed: index error: UUID must be 36 characters'
+      );
+      if (isNodeRequest) {
+        return [];
+      }
+
+      // Rethrow to be handled downstream.
+      throw error;
+    });
+  }
 }
