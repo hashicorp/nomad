b28436beb410750addc86b5e0dc8658704830c1c
Michael Schurter <mschurter@hashicorp.com>
2023-07-25 17:22:04 -0700

    test retry logic

--- helper/retry.go
+++ helper/retry.go
@@ -3,18 +3,25 @@
 
 package helper
 
-import "time"
+import (
+	"time"
+)
 
 // ExpiryToRenewTime calculates how long until clients should try to renew
 // credentials based on their expiration time and now.
 //
 // Renewals will begin halfway between now and the expiry plus some jitter.
+//
+// If the expiration is in the past or less than the min wait, then the min
+// wait time will be used with jitter.
 func ExpiryToRenewTime(exp time.Time, now func() time.Time, minWait time.Duration) time.Duration {
 	left := exp.Sub(now())
 
-	if left < minWait {
-		left = minWait
+	renewAt := left / 2
+
+	if renewAt < minWait {
+		return minWait + RandomStagger(minWait/10)
 	}
 
-	return (left / 2) + RandomStagger(left/10)
+	return renewAt + RandomStagger(renewAt/10)
 }
