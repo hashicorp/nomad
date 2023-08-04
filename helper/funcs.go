73fb7d4e96be26a056a300a0c920cce36e843310
Esteban Barrios <esteban.barrios@schuettflix.com>
2023-08-04 17:56:47 +0200

    Removed unused function from helpers

--- helper/funcs.go
+++ helper/funcs.go
@@ -509,10 +509,3 @@ func Merge[T comparable](a, b T) T {
 	}
 	return a
 }
-
-func MergeListWithDefaults(list []string, defaults []string) []string {
-	if len(list) == 0 {
-		return defaults
-	}
-	return list
-}
