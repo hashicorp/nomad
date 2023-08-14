d6fe18d4c0993b22907da9f745b2144cbb1ed937
stswidwinski <stan.swidwinski@gmail.com>
2023-08-15 06:11:21 +0800

    Propose a more structured approach to computation of backoff.

---
+++ helper/backoff.go
@@ -0,0 +1,26 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package helper
+
+import (
+	"time"
+)
+
+const MaxUint = ^uint64(0)
+const MaxInt = int64(MaxUint >> 1)
+
+func Backoff(backoffBase time.Duration, backoffLimit time.Duration, attempt uint64) time.Duration {
+	// Ensure that a large attempt will not cause an overflow
+	if attempt > 62 || MaxInt/backoffBase.Nanoseconds() < (1<<attempt) {
+		return backoffLimit
+	}
+
+	// Compute deadline and clamp it to backoffLimit
+	deadline := 1 << attempt * backoffBase
+	if deadline > backoffLimit {
+		deadline = backoffLimit
+	}
+
+	return deadline
+}
