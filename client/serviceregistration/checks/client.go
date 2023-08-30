05c332221471d39053eaecafe4832ddd6e1b3b89
Seth Hoenig <shoenig@duck.com>
2023-08-30 08:22:28 -0500

    Revert "client: include response body in output for successful HTTP checks (#18345)" (#18362)
    * Revert "client: include response body in output for successful HTTP checks (#18345)"
    
    This reverts commit d0a93f12d1ec1e2b276f9958898c9a6fe4f6b077.
    
    * cr: add comment about dropping ok output
    
    Co-authored-by: Luiz Aoqui <luiz@hashicorp.com>
    
    ---------
    
    Co-authored-by: Luiz Aoqui <luiz@hashicorp.com>

--- client/serviceregistration/checks/client.go
+++ client/serviceregistration/checks/client.go
@@ -205,12 +205,19 @@ func (c *checker) checkHTTP(ctx context.Context, qc *QueryContext, q *Query) *st
 	switch {
 	case result.StatusCode == http.StatusOK:
 		qr.Status = structs.CheckSuccess
+		// The check output is ignored on success to prevent users from relying
+		// on their content since querying service check results is an
+		// expensive operation.
+		qr.Output = "nomad: http ok"
+		return qr
 	case result.StatusCode < http.StatusBadRequest:
 		qr.Status = structs.CheckSuccess
 	default:
 		qr.Status = structs.CheckFailure
 	}
 
+	// status code was not 200; read the response body and set that as the
+	// check result output content
 	qr.Output = limitRead(result.Body)
 
 	return qr
