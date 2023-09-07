4e4c859e56469a9b97b9539b9062af7cce864ee1
Phil Renaud <phil@riotindustries.com>
2023-09-07 11:59:17 -0400

    Overview page

--- ui/app/routes/access-control.js
+++ ui/app/routes/access-control.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default class AccessControlRoute extends Route {}
+export default class AccessControlRoute extends Route {
+  // Load our tokens, roles, and policies
+  async model() {
+    return {
+      tokens: await this.store.findAll('token'),
+      roles: await this.store.findAll('role'),
+      policies: await this.store.findAll('policy'),
+    };
+  }
+}
