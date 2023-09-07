d8c2cb74167ce3676da56eb02e5e7d9ecbe0b475
Juana De La Cuesta <juanita.delacuestamorales@hashicorp.com>
2023-09-07 18:53:59 +0200

    [f-gh-17449-lease] Add lease mechanism to SDK (#18268)
    * func: add lease functionality to locks
    
    * func: configure retry
    
    * func: configure retry
    
    * style: rearrange the way the LockLease is created based on the Locks
    
    * style: add comments
    
    * style: add comments
    
    * style: add comments
    
    * Update api/locks.go
    
    Co-authored-by: James Rasell <jrasell@users.noreply.github.com>
    
    * Update api/locks_test.go
    
    Co-authored-by: James Rasell <jrasell@users.noreply.github.com>
    
    * func: add start wrapper function to deal with lock release
    
    * func: add correct handling of 409 conflict error, remove the release from the test
    
    * func: add test for return when function fail
    
    * func: add visibility for renewal errors
    
    * func: change the lock errors on the variables rpc to rpc mapped errors
    
    * func: change the lock errors on the variables rpc canonicalize to rpc mapped errors
    
    * style: update error message
    
    * func: make protected func array
    
    * func: use funcCtx to cancel the maintainLease and make variable on locker private and an instance
    
    * style: typo
    
    * func: pass locker to LockLeaser
    
    * style: change http status codes for const
    
    * func: make error for conflict public
    
    * func: use the ttl from the variable
    
    * func: close channel to avoid leaks
    
    * func: make lock pointer so it can be differentiated from empty
    
    * func: add ttl to locker interface
    
    * func: remove caller ID from acquire signature
    
    * style: renew ttl func
    
    * func: improve error handling
    
    * Update api/locks.go
    
    Co-authored-by: James Rasell <jrasell@users.noreply.github.com>
    
    * func: add verification for the variable's path
    
    ---------
    
    Co-authored-by: James Rasell <jrasell@users.noreply.github.com>

--- api/retry.go
+++ api/retry.go
@@ -6,7 +6,6 @@ package api
 import (
 	"context"
 	"errors"
-	"math"
 	"net/http"
 	"time"
 )
@@ -17,28 +16,23 @@ const (
 	defaultMaxBackoffDelay = 5 * time.Minute
 )
 
-type client interface {
-	put(endpoint string, in, out any, q *WriteOptions) (*WriteMeta, error)
-}
-
-type retryClient struct {
-	c client
-	retryOptions
-}
-
-// LockOptions is used to parameterize the Lock behavior.
 type retryOptions struct {
 	maxRetries int64 // Optional, defaults to 5
 	// maxBackoffDelay  sets a capping value for the delay between calls, to avoid it growing infinitely
 	maxBackoffDelay time.Duration // Optional, defaults to 5 min
 	// maxToLastCall sets a capping value for all the retry process, in case there is a deadline to make the call.
-	maxToLastCall time.Duration // Optional, defaults to 5min, meaning no time cap
+	maxToLastCall time.Duration // Optional, defaults to 0, meaning no time cap
 	// fixedDelay is used in case an uniform distribution of the calls is preferred.
 	fixedDelay time.Duration // Optional, defaults to 0, meaning Delay is exponential, starting at 1sec
 	// delayBase is used to calculate the starting value at which the delay starts to grow,
 	// When left empty, a value of 1 sec will be used as base and then the delays will
 	// grow exponentially with every attempt: starting at 1s, then 2s, 4s, 8s...
 	delayBase time.Duration // Optional, defaults to 1sec
+
+	// maxValidAttempt is used to ensure that a big attempts number or a big delayBase number will not cause
+	// a negative delay by overflowing the delay increase. Every attempt after the
+	// maxValid will use the maxBackoffDelay if configured, or the defaultMaxBackoffDelay if not.
+	maxValidAttempt int64
 }
 
 func (c *Client) retryPut(ctx context.Context, endpoint string, in, out any, q *WriteOptions) (*WriteMeta, error) {
@@ -116,14 +110,7 @@ func (c *Client) calculateDelay(attempt int64) time.Duration {
 		return 0
 	}
 
-	// Ensure that a big attempt number or a big delayBase number will not cause
-	// a negative delay by overflowing the delay increase.
-	// TODO: This value should be calculated on configuration and set the
-	// to default if not provided.
-	maxValidAttempt := int64(math.Log2(float64(math.MaxInt64 /
-		c.config.retryOptions.delayBase.Nanoseconds())))
-
-	if attempt > maxValidAttempt {
+	if attempt > c.config.retryOptions.maxValidAttempt {
 		return c.config.retryOptions.maxBackoffDelay
 	}
 
