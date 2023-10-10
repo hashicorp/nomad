f1dba2031a06cddd1af4b7dd030abb82e61c23d4
Tim Gross <tgross@hashicorp.com>
2023-10-10 20:37:24 +0000

    backport of commit 635afee376f0057a8b12dcf55703c7d88b1953f7

--- scripts/linux-priv-go.sh
+++ scripts/linux-priv-go.sh
@@ -21,7 +21,7 @@ case $(arch) in
 esac
 
 function install_go() {
-	local go_version="1.21.0"
+	local go_version="1.21.3"
 	local download="https://storage.googleapis.com/golang/go${go_version}.linux-${ARCH}.tar.gz"
 
 	if go version 2>&1 | grep -q "${go_version}"; then
