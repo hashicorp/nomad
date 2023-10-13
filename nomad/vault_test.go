9ec7e249c470814c149244c1fb9e9de66c94c39a
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2023-10-13 02:11:54 -0500

    vault: use an importable const for Vault header string. (#18740) (#18750)
    Co-authored-by: James Rasell <jrasell@users.noreply.github.com>

--- nomad/vault_test.go
+++ nomad/vault_test.go
@@ -71,8 +71,6 @@ path "secret/*" {
 	capabilities = ["create", "read", "update", "delete", "list"]
 }
 `
-
-	vaultNamespaceHeaderName = "X-Vault-Namespace"
 )
 
 // defaultTestVaultAllowlistRoleAndToken creates a test Vault role and returns a token
@@ -202,8 +200,8 @@ func TestVaultClient_WithNamespaceSupport(t *testing.T) {
 		t.Fatalf("failed to build vault client: %v", err)
 	}
 
-	require.Equal(testNs, c.client.Headers().Get(vaultNamespaceHeaderName))
-	require.Equal("", c.clientSys.Headers().Get(vaultNamespaceHeaderName))
+	require.Equal(testNs, c.client.Headers().Get(structs.VaultNamespaceHeaderName))
+	require.Equal("", c.clientSys.Headers().Get(structs.VaultNamespaceHeaderName))
 	require.NotEqual(c.clientSys, c.client)
 }
 
@@ -227,8 +225,8 @@ func TestVaultClient_WithoutNamespaceSupport(t *testing.T) {
 		t.Fatalf("failed to build vault client: %v", err)
 	}
 
-	require.Equal("", c.client.Headers().Get(vaultNamespaceHeaderName))
-	require.Equal("", c.clientSys.Headers().Get(vaultNamespaceHeaderName))
+	require.Equal("", c.client.Headers().Get(structs.VaultNamespaceHeaderName))
+	require.Equal("", c.clientSys.Headers().Get(structs.VaultNamespaceHeaderName))
 	require.Equal(c.clientSys, c.client)
 }
 
