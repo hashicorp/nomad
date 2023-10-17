7d250c2071cd0b59cdc9f2d91e000e9d1eff05bb
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2023-10-17 16:43:53 +0200

    better consul client mock

--- client/consul/consul_testing.go
+++ client/consul/consul_testing.go
@@ -4,12 +4,28 @@
 package consul
 
 import (
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/nomad/helper/uuid"
+	"github.com/hashicorp/nomad/nomad/structs/config"
 )
 
-type MockConsulClient struct{}
+type MockConsulClient struct {
+	tokens map[string]string
+}
+
+func NewMockConsulClient(config *config.ConsulConfig, logger hclog.Logger) (Client, error) {
+	return &MockConsulClient{}, nil
+}
+
+func (mc *MockConsulClient) SetTokens(tokens map[string]string) {
+	mc.tokens = tokens
+}
 
 func (mc *MockConsulClient) DeriveSITokenWithJWT(reqs map[string]JWTLoginRequest) (map[string]string, error) {
+	if mc.tokens != nil && len(mc.tokens) > 0 {
+		return mc.tokens, nil
+	}
+
 	tokens := make(map[string]string, len(reqs))
 	for id := range reqs {
 		tokens[id] = uuid.Generate()
