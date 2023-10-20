793a75df786bc5b29a7eb5e23b4dd4c476af48d0
James Rasell <jrasell@users.noreply.github.com>
2023-10-20 06:55:22 +0000

    backport of commit ca9e08e6b5eee00d055b9429df5976a70cdcb2d6

--- command/agent_monitor_test.go
+++ command/agent_monitor_test.go
@@ -42,4 +42,13 @@ func TestMonitorCommand_Fails(t *testing.T) {
 
 	out = ui.ErrorWriter.String()
 	must.StrContains(t, out, "No node(s) with prefix")
+
+	ui.ErrorWriter.Reset()
+
+	// Fails on passing a log-include-location flag which cannot be parsed.
+	code = cmd.Run([]string{"-address=" + url, "-log-include-location=maybe"})
+	must.One(t, code)
+
+	out = ui.ErrorWriter.String()
+	must.StrContains(t, out, `invalid boolean value "maybe" for -log-include-location`)
 }
