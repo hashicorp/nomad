6c2d5a0fbbcaf6a180dbaf6a7e1857b680b7c18d
Tim Gross <tgross@hashicorp.com>
2023-10-24 16:03:53 -0400

    E2E: Consul compatibility matrix tests (#18799)
    Set up a new test suite that exercises Nomad's compatibility with Consul. This
    suite installs all currently supported versions of Consul, spins up a Consul
    agent with appropriate configuration, and a Nomad agent running in dev
    mode. Then it runs a Connect job against each pair.

---
+++ e2e/consulcompat/input/consul-policy-for-nomad.hcl
@@ -0,0 +1,42 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+
+# Policy for the Nomad agent. Note that with this policy we must use Workload
+# Identity for Connect jobs, or we'll get "failed to derive SI token" errors
+# from the client because the Nomad agent's token doesn't have "acl:write"
+
+# The operator:write permission is required for creating config entries for
+# connect ingress gateways. operator ACLs are not namespaced, though the
+# config entries they can generate are.
+operator = "write"
+
+agent_prefix "" {
+  policy = "read"
+}
+
+key_prefix "" {
+  policy = "read"
+}
+
+node_prefix "" {
+  policy = "read"
+}
+
+service_prefix "" {
+  policy = "write"
+}
+
+# for use with Consul ENT
+namespace_prefix "" {
+  key_prefix "" {
+    policy = "read"
+  }
+
+  node_prefix "" {
+    policy = "read"
+  }
+
+  service_prefix "" {
+    policy = "write"
+  }
+}
---
+++ e2e/consulcompat/shared_setup_test.go
@@ -0,0 +1,117 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package consulcompat
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	consulapi "github.com/hashicorp/consul/api"
+	consulTestUtil "github.com/hashicorp/consul/sdk/testutil"
+	nomadapi "github.com/hashicorp/nomad/api"
+	"github.com/hashicorp/nomad/helper/testlog"
+	"github.com/hashicorp/nomad/helper/uuid"
+	"github.com/hashicorp/nomad/testutil"
+	"github.com/shoenig/test/must"
+)
+
+const (
+	consulDataDir = "consul-data"
+)
+
+// startConsul runs a Consul agent with bootstrapped ACLs and returns a stop
+// function, the HTTP address, and a HTTP API client
+func startConsul(t *testing.T, b build, baseDir, ns string) (string, *consulapi.Client) {
+
+	path := filepath.Join(baseDir, binDir, b.Version)
+	cwd, _ := os.Getwd()
+	os.Chdir(path)      // so that we can launch Consul from the current directory
+	defer os.Chdir(cwd) // return to the test dir so we can find job files
+
+	oldpath := os.Getenv("PATH")
+	os.Setenv("PATH", path+":"+oldpath)
+	t.Cleanup(func() {
+		os.Setenv("PATH", oldpath)
+	})
+
+	consulDC1 := "dc1"
+	rootToken := uuid.Generate()
+
+	testconsul, err := consulTestUtil.NewTestServerConfigT(t,
+		func(c *consulTestUtil.TestServerConfig) {
+			c.ACL.Enabled = true
+			c.ACL.DefaultPolicy = "deny"
+			c.ACL.Tokens = consulTestUtil.TestTokens{
+				InitialManagement: rootToken,
+			}
+			c.Datacenter = consulDC1
+			c.DataDir = filepath.Join(baseDir, binDir, b.Version, consulDataDir)
+			c.LogLevel = "info"
+			c.Connect = map[string]any{"enabled": true}
+			c.Server = true
+
+			if !testing.Verbose() {
+				c.Stdout = io.Discard
+				c.Stderr = io.Discard
+			}
+		})
+	must.NoError(t, err, must.Sprint("error starting test consul server"))
+
+	t.Cleanup(func() {
+		testconsul.Stop()
+		os.RemoveAll(filepath.Join(baseDir, binDir, b.Version, consulDataDir))
+	})
+
+	testconsul.WaitForLeader(t)
+	testconsul.WaitForActiveCARoot(t)
+
+	// TODO: we should run this entire test suite with mTLS everywhere
+	consulClient, err := consulapi.NewClient(&consulapi.Config{
+		Address:    testconsul.HTTPAddr,
+		Scheme:     "http",
+		Datacenter: consulDC1,
+		HttpClient: consulapi.DefaultConfig().HttpClient,
+		Token:      rootToken,
+		Namespace:  ns,
+		TLSConfig:  consulapi.TLSConfig{},
+	})
+	must.NoError(t, err)
+
+	return testconsul.HTTPAddr, consulClient
+}
+
+// startNomad runs a Nomad agent in dev mode with bootstrapped ACLs
+func startNomad(t *testing.T, consulConfig *testutil.Consul) *nomadapi.Client {
+
+	rootToken := uuid.Generate()
+
+	ts := testutil.NewTestServer(t, func(c *testutil.TestServerConfig) {
+		c.DevMode = true
+		c.LogLevel = testlog.HCLoggerTestLevel().String()
+		c.Consul = consulConfig
+		c.ACL = &testutil.ACLConfig{
+			Enabled:        true,
+			BootstrapToken: rootToken,
+		}
+
+		if !testing.Verbose() {
+			c.Stdout = io.Discard
+			c.Stderr = io.Discard
+		}
+	})
+
+	t.Cleanup(ts.Stop)
+
+	// TODO: we should run this entire test suite with mTLS everywhere
+	nc, err := nomadapi.NewClient(&nomadapi.Config{
+		Address:   "http://" + ts.HTTPAddr,
+		TLSConfig: &nomadapi.TLSConfig{},
+	})
+	must.NoError(t, err, must.Sprint("unable to create nomad api client"))
+
+	nc.SetSecretID(rootToken)
+	return nc
+}
