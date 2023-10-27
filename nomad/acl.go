2daf49df9a10571664cbb851fc2f9f4000b7462e
James Rasell <jrasell@users.noreply.github.com>
2023-10-27 16:36:10 +0100

    server: use same receiver name for all server funcs. (#18896)

--- nomad/acl.go
+++ nomad/acl.go
@@ -8,34 +8,34 @@ import (
 	"github.com/hashicorp/nomad/nomad/structs"
 )
 
-func (srv *Server) Authenticate(ctx *RPCContext, args structs.RequestWithIdentity) error {
-	return srv.auth.Authenticate(ctx, args)
+func (s *Server) Authenticate(ctx *RPCContext, args structs.RequestWithIdentity) error {
+	return s.auth.Authenticate(ctx, args)
 }
 
-func (srv *Server) AuthenticateServerOnly(ctx *RPCContext, args structs.RequestWithIdentity) (*acl.ACL, error) {
-	return srv.auth.AuthenticateServerOnly(ctx, args)
+func (s *Server) AuthenticateServerOnly(ctx *RPCContext, args structs.RequestWithIdentity) (*acl.ACL, error) {
+	return s.auth.AuthenticateServerOnly(ctx, args)
 }
 
-func (srv *Server) AuthenticateClientOnly(ctx *RPCContext, args structs.RequestWithIdentity) (*acl.ACL, error) {
-	return srv.auth.AuthenticateClientOnly(ctx, args)
+func (s *Server) AuthenticateClientOnly(ctx *RPCContext, args structs.RequestWithIdentity) (*acl.ACL, error) {
+	return s.auth.AuthenticateClientOnly(ctx, args)
 }
 
-func (srv *Server) AuthenticateClientOnlyLegacy(ctx *RPCContext, args structs.RequestWithIdentity) (*acl.ACL, error) {
-	return srv.auth.AuthenticateClientOnlyLegacy(ctx, args)
+func (s *Server) AuthenticateClientOnlyLegacy(ctx *RPCContext, args structs.RequestWithIdentity) (*acl.ACL, error) {
+	return s.auth.AuthenticateClientOnlyLegacy(ctx, args)
 }
 
-func (srv *Server) ResolveACL(args structs.RequestWithIdentity) (*acl.ACL, error) {
-	return srv.auth.ResolveACL(args)
+func (s *Server) ResolveACL(args structs.RequestWithIdentity) (*acl.ACL, error) {
+	return s.auth.ResolveACL(args)
 }
 
-func (srv *Server) VerifyClaim(token string) (*structs.IdentityClaims, error) {
-	return srv.auth.VerifyClaim(token)
+func (s *Server) VerifyClaim(token string) (*structs.IdentityClaims, error) {
+	return s.auth.VerifyClaim(token)
 }
 
-func (srv *Server) ResolveToken(secretID string) (*acl.ACL, error) {
-	return srv.auth.ResolveToken(secretID)
+func (s *Server) ResolveToken(secretID string) (*acl.ACL, error) {
+	return s.auth.ResolveToken(secretID)
 }
 
-func (srv *Server) ResolvePoliciesForClaims(claims *structs.IdentityClaims) ([]*structs.ACLPolicy, error) {
-	return srv.auth.ResolvePoliciesForClaims(claims)
+func (s *Server) ResolvePoliciesForClaims(claims *structs.IdentityClaims) ([]*structs.ACLPolicy, error) {
+	return s.auth.ResolvePoliciesForClaims(claims)
 }
