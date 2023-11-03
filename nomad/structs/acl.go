ec823ae2238a9c2fbcb9f6ac268fa8c82e019f41
James Rasell <jrasell@users.noreply.github.com>
2023-11-03 07:22:54 +0000

    backport of commit 5f98e6473ccf2c5dd643fa6d79c43d113c56519b

--- nomad/structs/acl.go
+++ nomad/structs/acl.go
@@ -222,7 +222,7 @@ var (
 	// ValidACLAuthMethod is used to validate an ACL auth method name.
 	ValidACLAuthMethod = regexp.MustCompile("^[a-zA-Z0-9-]{1,128}$")
 
-	// ValitACLAuthMethodTypes lists supported auth method types.
+	// ValidACLAuthMethodTypes lists supported auth method types.
 	ValidACLAuthMethodTypes = []string{ACLAuthMethodTypeOIDC, ACLAuthMethodTypeJWT}
 )
 
@@ -924,7 +924,7 @@ func (a *ACLAuthMethod) Validate(minTTL, maxTTL time.Duration) error {
 		mErr.Errors = append(mErr.Errors, fmt.Errorf("invalid name '%s'", a.Name))
 	}
 
-	if !slices.Contains([]string{"local", "global"}, a.TokenLocality) {
+	if !slices.Contains([]string{ACLAuthMethodTokenLocalityLocal, ACLAuthMethodTokenLocalityGlobal}, a.TokenLocality) {
 		mErr.Errors = append(
 			mErr.Errors, fmt.Errorf("invalid token locality '%s'", a.TokenLocality))
 	}
@@ -945,7 +945,9 @@ func (a *ACLAuthMethod) Validate(minTTL, maxTTL time.Duration) error {
 
 // TokenLocalityIsGlobal returns whether the auth method creates global ACL
 // tokens or not.
-func (a *ACLAuthMethod) TokenLocalityIsGlobal() bool { return a.TokenLocality == "global" }
+func (a *ACLAuthMethod) TokenLocalityIsGlobal() bool {
+	return a.TokenLocality == ACLAuthMethodTokenLocalityGlobal
+}
 
 // ACLAuthMethodConfig is used to store configuration of an auth method
 type ACLAuthMethodConfig struct {
