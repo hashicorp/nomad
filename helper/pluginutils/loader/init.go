e0cea41e3772331d5cb814b58872eda7fe203706
Luiz Aoqui <luiz@hashicorp.com>
2023-11-27 21:36:42 -0500

    client: deprecate loading plugins without config (#19189)
    Nomad load all plugins from `plugin_dir` regardless if it is listed in
    the agent configuration file. This can cause unexpected binaries to be
    executed.
    
    This commit begins the deprecation process of this behaviour. The Nomad
    agent will emit a warning log for every plugin binary found without a
    corresponding agent configuration block.
    
    ---------
    
    Co-authored-by: Michael Schurter <mschurter@hashicorp.com>

--- helper/pluginutils/loader/init.go
+++ helper/pluginutils/loader/init.go
@@ -264,7 +264,11 @@ func (l *PluginLoader) fingerprintPlugins(plugins []os.FileInfo, configs map[str
 	fingerprinted := make(map[PluginID]*pluginInfo, len(plugins))
 	for _, p := range plugins {
 		name := cleanPluginExecutable(p.Name())
-		c := configs[name]
+		c, ok := configs[name]
+		if !ok {
+			// COMPAT(1.7): Skip executing unconfigured plugins in 1.8 or later.
+			l.logger.Warn("plugin not referenced in the agent configuration file, future versions of Nomad will not load this plugin until the agent configuration is updated", "plugin", name)
+		}
 		info, err := l.fingerprintPlugin(p, c)
 		if err != nil {
 			l.logger.Error("failed to fingerprint plugin", "plugin", name, "error", err)
