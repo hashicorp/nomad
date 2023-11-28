ddb060d8b3e5ee79bed5c49998a7e5de4a355150
Luiz Aoqui <luiz@hashicorp.com>
2023-11-28 12:37:57 -0500

    deps: update go-metrics to v0.5.3 (#19190)
    Update `go-metrics` to v0.5.3 to pick
    https://github.com/hashicorp/go-metrics/pull/146.

--- nomad/node_pool_endpoint.go
+++ nomad/node_pool_endpoint.go
@@ -9,8 +9,8 @@ import (
 	"net/http"
 	"time"
 
-	metrics "github.com/armon/go-metrics"
 	"github.com/hashicorp/go-memdb"
+	metrics "github.com/hashicorp/go-metrics"
 	multierror "github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/nomad/acl"
 	"github.com/hashicorp/nomad/nomad/state"
