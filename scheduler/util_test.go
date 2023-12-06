926774852231602280094c178199a74a7bac2d3f
Juana De La Cuesta <juanita.delacuestamorales@hashicorp.com>
2023-12-06 11:02:55 +0100

    Update scheduler/util_test.go
    Co-authored-by: Luiz Aoqui <luiz@hashicorp.com>

--- scheduler/util_test.go
+++ scheduler/util_test.go
@@ -547,7 +547,6 @@ func TestTasksUpdated(t *testing.T) {
 	// Compare changed Template ErrMissingKey
 	j30.TaskGroups[0].Tasks[0].Templates[0].ErrMissingKey = true
 	must.True(t, tasksUpdated(j29, j30, name).modified)
-
 }
 
 func TestTasksUpdated_connectServiceUpdated(t *testing.T) {
