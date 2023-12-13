1565de949c9e9f7910017caada4ccf5ea9995b12
hc-github-team-nomad-core <github-team-nomad-core@hashicorp.com>
2023-12-13 19:59:46 +0000

    Prepare for next release

--- version/version.go
+++ version/version.go
@@ -19,12 +19,12 @@ var (
 	GitDescribe string
 
 	// The main version number that is being run at the moment.
-	Version = "1.7.2"
+	Version = "1.7.3"
 
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
 	// such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
+	VersionPrerelease = "dev"
 
 	// VersionMetadata is metadata further describing the build type.
 	VersionMetadata = ""
