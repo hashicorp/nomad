e3ed96689b49026700f0a4f6ca47e62ab0afcbc7
Nick Wales <588472+nickwales@users.noreply.github.com>
2023-12-13 13:41:32 -0600

    Updates for 22.04, uses packages.

--- terraform/shared/scripts/setup.sh
+++ terraform/shared/scripts/setup.sh
@@ -3,173 +3,92 @@
 # SPDX-License-Identifier: BUSL-1.1
 
 
-set -e
+#set -e
 
-# Disable interactive apt prompts
+# Disable interactive apt-get prompts
 export DEBIAN_FRONTEND=noninteractive
 
 cd /ops
 
 CONFIGDIR=/ops/shared/config
+sudo apt-get install -yq  apt-utils
 
-CONSULVERSION=1.12.2
-CONSULDOWNLOAD=https://releases.hashicorp.com/consul/${CONSULVERSION}/consul_${CONSULVERSION}_linux_amd64.zip
-CONSULCONFIGDIR=/etc/consul.d
-CONSULDIR=/opt/consul
+# Install HashiCorp products
+CONSULVERSION=1.17.0
+VAULTVERSION=1.15.4
+NOMADVERSION=1.7.1
+CONSULTEMPLATEVERSION=0.35.0
 
-VAULTVERSION=1.11.0
-VAULTDOWNLOAD=https://releases.hashicorp.com/vault/${VAULTVERSION}/vault_${VAULTVERSION}_linux_amd64.zip
-VAULTCONFIGDIR=/etc/vault.d
-VAULTDIR=/opt/vault
-
-NOMADVERSION=1.3.1
-NOMADDOWNLOAD=https://releases.hashicorp.com/nomad/${NOMADVERSION}/nomad_${NOMADVERSION}_linux_amd64.zip
-NOMADCONFIGDIR=/etc/nomad.d
-NOMADDIR=/opt/nomad
-
-CONSULTEMPLATEVERSION=0.29.1
-CONSULTEMPLATEDOWNLOAD=https://releases.hashicorp.com/consul-template/${CONSULTEMPLATEVERSION}/consul-template_${CONSULTEMPLATEVERSION}_linux_amd64.zip
-CONSULTEMPLATECONFIGDIR=/etc/consul-template.d
-CONSULTEMPLATEDIR=/opt/consul-template
+sudo apt-get update && sudo apt-get install gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update
+sudo apt-get install -yq consul="${CONSULVERSION}*" \
+                    vault="${VAULTVERSION}*" \
+                    nomad="${NOMADVERSION}*" \
+                    consul-template="${CONSULTEMPLATEVERSION}*"
 
 # Dependencies
-sudo apt-get install -y software-properties-common
+sudo apt-get install -yq software-properties-common
 sudo apt-get update
-sudo apt-get install -y unzip tree redis-tools jq curl tmux gnupg-curl
+sudo apt-get install -yq unzip tree redis jq curl tmux openjdk-8-jdk
 
 # Disable the firewall
-
 sudo ufw disable || echo "ufw not installed"
-
-# Consul
-
-curl -L $CONSULDOWNLOAD > consul.zip
-
-## Install
-sudo unzip consul.zip -d /usr/local/bin
-sudo chmod 0755 /usr/local/bin/consul
-sudo chown root:root /usr/local/bin/consul
-
-## Configure
-sudo mkdir -p $CONSULCONFIGDIR
-sudo chmod 755 $CONSULCONFIGDIR
-sudo mkdir -p $CONSULDIR
-sudo chmod 755 $CONSULDIR
-
-# Vault
-
-curl -L $VAULTDOWNLOAD > vault.zip
-
-## Install
-sudo unzip vault.zip -d /usr/local/bin
-sudo chmod 0755 /usr/local/bin/vault
-sudo chown root:root /usr/local/bin/vault
-
-## Configure
-sudo mkdir -p $VAULTCONFIGDIR
-sudo chmod 755 $VAULTCONFIGDIR
-sudo mkdir -p $VAULTDIR
-sudo chmod 755 $VAULTDIR
-
-# Nomad
-
-curl -L $NOMADDOWNLOAD > nomad.zip
-
-## Install
-sudo unzip nomad.zip -d /usr/local/bin
-sudo chmod 0755 /usr/local/bin/nomad
-sudo chown root:root /usr/local/bin/nomad
-
-## Configure
-sudo mkdir -p $NOMADCONFIGDIR
-sudo chmod 755 $NOMADCONFIGDIR
-sudo mkdir -p $NOMADDIR
-sudo chmod 755 $NOMADDIR
-
-# Consul Template 
-
-curl -L $CONSULTEMPLATEDOWNLOAD > consul-template.zip
-
-## Install
-sudo unzip consul-template.zip -d /usr/local/bin
-sudo chmod 0755 /usr/local/bin/consul-template
-sudo chown root:root /usr/local/bin/consul-template
-
-## Configure
-sudo mkdir -p $CONSULTEMPLATECONFIGDIR
-sudo chmod 755 $CONSULTEMPLATECONFIGDIR
-sudo mkdir -p $CONSULTEMPLATEDIR
-sudo chmod 755 $CONSULTEMPLATEDIR
-
 
 # Docker
 distro=$(lsb_release -si | tr '[:upper:]' '[:lower:]')
-sudo apt-get install -y apt-transport-https ca-certificates gnupg2 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${distro} $(lsb_release -cs) stable"
+sudo apt-get install -yq apt-transport-https ca-certificates gnupg2 
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Needs testing, updating and fixing
+# Add the repository to apt-get sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -yq docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+
+
+# # Needs testing, updating and fixing
 if [[ ! -z ${INSTALL_NVIDIA_DOCKER+x} ]]; then 
-  
-  # Install official NVIDIA driver package
-  # This is why we added gnupg-curl, otherwise, the following fails with "gpgkeys: protocol `https' not supported"
-  sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/3bf863cc.pub
-  sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
-  sudo apt-get update && sudo apt-get install -y --no-install-recommends --allow-unauthenticated linux-headers-generic dkms cuda-drivers
 
-  # Install nvidia-docker and nvidia-docker-plugin
-  # from: https://github.com/NVIDIA/nvidia-docker#ubuntu-140416041804-debian-jessiestretch
-  wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
-  sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
-  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-    sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+  # Install official NVIDIA driver package
+  ### Needs updating for 2204
+
+  # Install nvidia container support
+  curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+      sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+      sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
   sudo apt-get update
-  sudo apt-get install -y --allow-unauthenticated nvidia-docker2
-fi
+  sudo apt-get install -y nvidia-container-toolkit
 
-# rkt
-# Note: rkt has been ended and archived. This should likely be removed. 
-# See https://github.com/rkt/rkt/issues/4024
-VERSION=1.30.0
-DOWNLOAD=https://github.com/rkt/rkt/releases/download/v${VERSION}/rkt-v${VERSION}.tar.gz
+  sudo nvidia-ctk runtime configure --runtime=docker
+  sudo systemctl restart docker
 
-function install_rkt() {
-	wget -q -O /tmp/rkt.tar.gz "${DOWNLOAD}"
-	tar -C /tmp -xvf /tmp/rkt.tar.gz
-	sudo mv /tmp/rkt-v${VERSION}/rkt /usr/local/bin
-	sudo mv /tmp/rkt-v${VERSION}/*.aci /usr/local/bin
-}
+#   # Install official NVIDIA driver package
+#   # This is why we added gnupg-curl, otherwise, the following fails with "gpgkeys: protocol `https' not supported"
+#   sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/3bf863cc.pub
+#   sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+#   sudo apt-get update && sudo apt-get install -yq --no-install-recommends --allow-unauthenticated linux-headers-generic dkms cuda-drivers
 
-function configure_rkt_networking() {
-	sudo mkdir -p /etc/rkt/net.d
-    sudo bash -c 'cat << EOT > /etc/rkt/net.d/99-network.conf
-{
-  "name": "default",
-  "type": "ptp",
-  "ipMasq": false,
-  "ipam": {
-    "type": "host-local",
-    "subnet": "172.16.28.0/24",
-    "routes": [
-      {
-        "dst": "0.0.0.0/0"
-      }
-    ]
-  }
-}
-EOT'
-}
+#   # Install nvidia-docker and nvidia-docker-plugin
+#   # from: https://github.com/NVIDIA/nvidia-docker#ubuntu-140416041804-debian-jessiestretch
+#   wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
+#   sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+#   curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+#   distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+#   curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+#     sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
-install_rkt
-configure_rkt_networking
+#   sudo apt-get update
+#   sudo apt-get install -yq --allow-unauthenticated nvidia-docker2
+# fi
 
-# Java
-sudo add-apt-repository -y ppa:openjdk-r/ppa
-sudo apt-get update 
-sudo apt-get install -y openjdk-8-jdk
-JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
