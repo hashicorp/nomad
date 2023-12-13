65f41df858f0a80efc9f257bbdb901dedcb80c94
Seth Hoenig <shoenig@duck.com>
2023-12-13 19:06:45 +0000

    backport of commit 7e43317e3771935aad88fbdee623649faaf63e53

--- nomad/structs/cpucompat_default.go
+++ nomad/structs/cpucompat_default.go
@@ -6,9 +6,7 @@
 package structs
 
 import (
-	"github.com/hashicorp/nomad/client/lib/idset"
 	"github.com/hashicorp/nomad/client/lib/numalib"
-	"github.com/hashicorp/nomad/client/lib/numalib/hw"
 )
 
 // Compatibility will translate the LegacyNodeCpuResources into NodeProcessor
@@ -40,41 +38,5 @@ func (n *NodeResources) Compatibility() {
 }
 
 func topologyFromLegacy(old LegacyNodeCpuResources) *numalib.Topology {
-	coreCount := old.TotalCpuCores
-
-	// interpret per-core frequency given total compute and total core count
-	frequency := hw.MHz(old.CpuShares / (int64(coreCount)))
-
-	// synthesize a set of cores that abstractly matches the legacy cpu specs
-	cores := make([]numalib.Core, 0, coreCount)
-
-	for i := 0; i < int(coreCount); i++ {
-		cores = append(cores, numalib.Core{
-			ID:         hw.CoreID(i),
-			SocketID:   0,                   // no numa support on non-linux
-			NodeID:     0,                   // no numa support on non-linux
-			Grade:      numalib.Performance, // assume P-cores
-			Disable:    false,               // no reservable cores on non-linux
-			GuessSpeed: frequency,
-		})
-	}
-
-	withheld := (frequency * hw.MHz(coreCount)) - hw.MHz(old.CpuShares)
-
-	return &numalib.Topology{
-		// legacy: assume one node with id 0
-		NodeIDs: idset.From[hw.NodeID]([]hw.NodeID{0}),
-
-		// legacy: with one node the distance matrix is 1-D
-		Distances: numalib.SLIT{{10}},
-
-		// legacy: a pseudo representation of each actual core profile
-		Cores: cores,
-
-		// legacy: set since we have the value
-		OverrideTotalCompute: hw.MHz(old.CpuShares),
-
-		// legacy: set since we can compute the value
-		OverrideWitholdCompute: withheld,
-	}
+	return topologyFromLegacyGeneric(old)
 }
