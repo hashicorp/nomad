49b4996d46feae03f101d72138dca8a382963b28
Phil Renaud <phil.renaud@hashicorp.com>
2023-12-13 15:19:22 -0500

    overflow-anchor to hold view to bottom of scrollable action output (#19452)

--- ui/app/components/action-card.js
+++ ui/app/components/action-card.js
@@ -48,4 +48,30 @@ export default class ActionCardComponent extends Component {
     // Either the passed instance, or the peer-selected instance
     return this.selectedPeer || this.args.instance;
   }
+
+  @tracked hasBeenAnchored = false;
+
+  /**
+   * Runs from the action-card template whenever instance.messages updates,
+   * and serves to keep the user's view anchored to the bottom of the messages.
+   * This uses a hidden element and the overflow-anchor css attribute, which
+   * keeps the element visible within the scrollable <code> block parent.
+   * A trick here is that, if the user scrolls up from the bottom of the block,
+   * we don't want to force them down to the bottom again on update, but we do
+   * want to keep them there by default (so they have the latest output).
+   * The hasBeenAnchored flag is used to track this state, and we do a little
+   * trick when the messages get long enough to cause a scroll to start the
+   * anchoring process here.
+   *
+   * @param {HTMLElement} element
+   */
+  @action anchorToBottom(element) {
+    if (this.hasBeenAnchored) return;
+    const parentHeight = element.parentElement.clientHeight;
+    const elementHeight = element.clientHeight;
+    if (elementHeight > parentHeight) {
+      this.hasBeenAnchored = true;
+      element.parentElement.scroll(0, elementHeight);
+    }
+  }
 }
