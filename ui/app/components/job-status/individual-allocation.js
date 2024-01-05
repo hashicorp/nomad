4ab8cb63bc6da2c133121d85b9235b33f50438fc
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2024-01-04 21:00:42 -0600

    backport of commit 16876697a1f6db15ac5b1f84759d13beb482c1de (#19610)
    Co-authored-by: Phil Renaud <phil.renaud@hashicorp.com>

--- ui/app/components/job-status/individual-allocation.js
+++ ui/app/components/job-status/individual-allocation.js
@@ -10,8 +10,21 @@ import { alias } from '@ember/object/computed';
 export default class JobStatusIndividualAllocationComponent extends Component {
   @alias('args.allocation.job.type') jobType;
   @alias('args.allocation.node.name') nodeName;
+  @alias('args.allocation.taskGroup.name') groupName;
+  @alias('args.allocation.job.taskGroups') taskGroups;
+  @alias('args.allocation.shortId') shortId;
 
   get showClient() {
     return this.jobType === 'system' || this.jobType === 'sysbatch';
+  }
+
+  get tooltipText() {
+    if (this.showClient) {
+      return `${this.nodeName} - ${this.shortId}`;
+    } else if (this.groupName && this.taskGroups?.length > 1) {
+      return `${this.groupName} - ${this.shortId}`;
+    } else {
+      return this.shortId;
+    }
   }
 }
