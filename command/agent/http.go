8514c10de1c6cf48c41d4e8588d424e2f0ae53be
Daniel Bennett <dbennett@hashicorp.com>
2024-01-10 17:12:08 -0600

    /jobs/statuses2 endpoint for the whole /ui/jobs index table

--- command/agent/http.go
+++ command/agent/http.go
@@ -383,6 +383,7 @@ func (s *HTTPServer) registerHandlers(enableDebug bool) {
 	s.mux.HandleFunc("/v1/jobs", s.wrap(s.JobsRequest))
 	s.mux.HandleFunc("/v1/jobs/parse", s.wrap(s.JobsParseRequest))
 	s.mux.HandleFunc("/v1/jobs/statuses", s.wrap(s.JobsStatusesRequest))
+	s.mux.HandleFunc("/v1/jobs/statuses2", s.wrap(s.JobsStatuses2Request))
 	s.mux.HandleFunc("/v1/job/", s.wrap(s.JobSpecificRequest))
 
 	s.mux.HandleFunc("/v1/nodes", s.wrap(s.NodesRequest))
