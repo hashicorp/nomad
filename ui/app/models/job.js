12babe472a59c429f71144b766653c0ed3a65aaf
Phil Renaud <phil@riotindustries.com>
2024-01-21 22:13:54 -0500

    allocBlocks reimplemented at job model level

--- ui/app/models/job.js
+++ ui/app/models/job.js
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
+// @ts-check
+
 import { alias, equal, or, and, mapBy } from '@ember/object/computed';
 import { computed } from '@ember/object';
 import Model from '@ember-data/model';
@@ -11,6 +13,7 @@ import { fragment, fragmentArray } from 'ember-data-model-fragments/attributes';
 import RSVP from 'rsvp';
 import { assert } from '@ember/debug';
 import classic from 'ember-classic-decorator';
+import { jobAllocStatuses } from '../utils/allocation-client-statuses';
 
 const JOB_TYPES = ['service', 'batch', 'system', 'sysbatch'];
 
@@ -30,6 +33,208 @@ export default class Job extends Model {
   @attr('date') submitTime;
   @attr('string') nodePool; // Jobs are related to Node Pools either directly or via its Namespace, but no relationship.
 
+  @attr('number') groupCountSum;
+  @attr('string') deploymentID;
+
+  get allocTypes() {
+    return jobAllocStatuses[this.type].map((type) => {
+      return {
+        label: type,
+      };
+    });
+  }
+
+  /**
+   * @typedef {Object} CurrentStatus
+   * @property {"Healthy"|"Failed"|"Degraded"|"Recovering"|"Complete"|"Running"} label - The current status of the job
+   * @property {"highlight"|"success"|"warning"|"critical"} state -
+   */
+
+  /**
+   * @typedef {Object} HealthStatus
+   * @property {Array} nonCanary
+   * @property {Array} canary
+   */
+
+  /**
+   * @typedef {Object} AllocationStatus
+   * @property {HealthStatus} healthy
+   * @property {HealthStatus} unhealthy
+   * @property {HealthStatus} health unknown
+   */
+
+  /**
+   * @typedef {Object} AllocationBlock
+   * @property {AllocationStatus} [running]
+   * @property {AllocationStatus} [pending]
+   * @property {AllocationStatus} [failed]
+   * @property {AllocationStatus} [lost]
+   * @property {AllocationStatus} [unplaced]
+   * @property {AllocationStatus} [complete]
+   */
+
+  /**
+   * Looks through running/pending allocations with the aim of filling up your desired number of allocations.
+   * If any desired remain, it will walk backwards through job versions and other allocation types to build
+   * a picture of the job's overall status.
+   *
+   * @returns {AllocationBlock} An object containing healthy non-canary allocations
+   *                            for each clientStatus.
+   */
+  get allocBlocks() {
+    let availableSlotsToFill = this.totalAllocs;
+
+    // Initialize allocationsOfShowableType with empty arrays for each clientStatus
+    /**
+     * @type {AllocationBlock}
+     */
+    let allocationsOfShowableType = this.allocTypes.reduce(
+      (accumulator, type) => {
+        accumulator[type.label] = { healthy: { nonCanary: [] } };
+        return accumulator;
+      },
+      {}
+    );
+
+    // First accumulate the Running/Pending allocations
+    for (const alloc of this.allocations.filter(
+      (a) => a.clientStatus === 'running' || a.clientStatus === 'pending'
+    )) {
+      if (availableSlotsToFill === 0) {
+        break;
+      }
+
+      const status = alloc.clientStatus;
+      allocationsOfShowableType[status].healthy.nonCanary.push(alloc);
+      availableSlotsToFill--;
+    }
+
+    // Sort all allocs by jobVersion in descending order
+    const sortedAllocs = this.allocations
+      .filter(
+        (a) => a.clientStatus !== 'running' && a.clientStatus !== 'pending'
+      )
+      .sort((a, b) => {
+        // First sort by jobVersion
+        if (a.jobVersion > b.jobVersion) return 1;
+        if (a.jobVersion < b.jobVersion) return -1;
+
+        // If jobVersion is the same, sort by status order
+        if (a.jobVersion === b.jobVersion) {
+          return (
+            jobAllocStatuses[this.type].indexOf(b.clientStatus) -
+            jobAllocStatuses[this.type].indexOf(a.clientStatus)
+          );
+        } else {
+          return 0;
+        }
+      })
+      .reverse();
+
+    // Iterate over the sorted allocs
+    for (const alloc of sortedAllocs) {
+      if (availableSlotsToFill === 0) {
+        break;
+      }
+
+      const status = alloc.clientStatus;
+      // If the alloc has another clientStatus, add it to the corresponding list
+      // as long as we haven't reached the totalAllocs limit for that clientStatus
+      if (
+        this.allocTypes.map(({ label }) => label).includes(status) &&
+        allocationsOfShowableType[status].healthy.nonCanary.length <
+          this.totalAllocs
+      ) {
+        allocationsOfShowableType[status].healthy.nonCanary.push(alloc);
+        availableSlotsToFill--;
+      }
+    }
+
+    // Handle unplaced allocs
+    if (availableSlotsToFill > 0) {
+      allocationsOfShowableType['unplaced'] = {
+        healthy: {
+          nonCanary: Array(availableSlotsToFill)
+            .fill()
+            .map(() => {
+              return { clientStatus: 'unplaced' };
+            }),
+        },
+      };
+    }
+
+    console.log('allocBlocks for', this.name, 'is', allocationsOfShowableType);
+
+    return allocationsOfShowableType;
+  }
+
+  /**
+   * A single status to indicate how a job is doing, based on running/healthy allocations vs desired.
+   * Possible statuses are:
+   * - Deploying: A deployment is actively taking place
+   * - Complete: (Batch/Sysbatch only) All expected allocations are complete
+   * - Running: (Batch/Sysbatch only) All expected allocations are running
+   * - Healthy: All expected allocations are running and healthy
+   * - Recovering: Some allocations are pending
+   * - Degraded: A deployment is not taking place, and some allocations are failed, lost, or unplaced
+   * - Failed: All allocations are failed, lost, or unplaced
+   * @returns {CurrentStatus}
+   */
+  /**
+   * A general assessment for how a job is going, in a non-deployment state
+   * @returns {CurrentStatus}
+   */
+  get aggregateAllocStatus() {
+    // If all allocs are running, the job is Healthy
+    const totalAllocs = this.totalAllocs;
+    console.log('groupCountSum is', totalAllocs);
+    console.log('ablocks are', this.allocBlocks);
+
+    // If deploying:
+    if (this.deploymentID) {
+      return { label: 'Deploying', state: 'highlight' };
+    }
+
+    if (this.type === 'batch' || this.type === 'sysbatch') {
+      // If all the allocs are complete, the job is Complete
+      const completeAllocs = this.allocBlocks.complete?.healthy?.nonCanary;
+      if (completeAllocs?.length === totalAllocs) {
+        return { label: 'Complete', state: 'success' };
+      }
+
+      // If any allocations are running the job is "Running"
+      const healthyAllocs = this.allocBlocks.running?.healthy?.nonCanary;
+      if (healthyAllocs?.length + completeAllocs?.length === totalAllocs) {
+        return { label: 'Running', state: 'success' };
+      }
+    }
+
+    const healthyAllocs = this.allocBlocks.running?.healthy?.nonCanary;
+    if (healthyAllocs?.length === totalAllocs) {
+      return { label: 'Healthy', state: 'success' };
+    }
+
+    // If any allocations are pending the job is "Recovering"
+    const pendingAllocs = this.allocBlocks.pending?.healthy?.nonCanary;
+    if (pendingAllocs?.length > 0) {
+      return { label: 'Recovering', state: 'highlight' };
+    }
+
+    // If any allocations are failed, lost, or unplaced in a steady state, the job is "Degraded"
+    const failedOrLostAllocs = [
+      ...this.allocBlocks.failed?.healthy?.nonCanary,
+      ...this.allocBlocks.lost?.healthy?.nonCanary,
+      ...this.allocBlocks.unplaced?.healthy?.nonCanary,
+    ];
+    console.log('numFailedAllocs', failedOrLostAllocs.length);
+    // if (failedOrLostAllocs.length === totalAllocs) {
+    if (failedOrLostAllocs.length >= totalAllocs) {
+      // TODO: when totalAllocs only cares about latest version, change back to ===
+      return { label: 'Failed', state: 'critical' };
+    } else {
+      return { label: 'Degraded', state: 'warning' };
+    }
+  }
   @fragment('structured-attributes') meta;
 
   get isPack() {
