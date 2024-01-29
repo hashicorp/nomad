6de7ec0e53bcc27b47e3cc64b0d6f1c0058292b9
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2024-01-29 11:26:22 -0600

    license: fix some imports of BUSL-1.1 in MPL-2.0 (#19832) (#19842)
    Some packages licensed under MPL-2.0 were incorrectly importing code
    from packages licensed under BUSL-1.1.
    
    Not all imports are fixed here as they will require additional work to
    untangle them. To help track progress this commit adds a Semgrep rule
    that detects incorrect BUSL-1.1 imports in MPL-2.0 packages.
    
    Co-authored-by: Luiz Aoqui <luiz@hashicorp.com>

--- helper/pointer/pointer.go
+++ helper/pointer/pointer.go
@@ -1,5 +1,5 @@
 // Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MPL-2.0
 
 // Package pointer provides helper functions related to Go pointers.
 package pointer
