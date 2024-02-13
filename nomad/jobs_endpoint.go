2c2ba597d8b847d1ccfd7546e535f15fabf330c7
Daniel Bennett <dbennett@hashicorp.com>
2024-02-13 12:00:50 -0500

    misc tidying and rearranging

--- nomad/jobs_endpoint.go
+++ nomad/jobs_endpoint.go
@@ -4,6 +4,7 @@
 package nomad
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
@@ -29,6 +30,163 @@ type Jobs struct {
 	srv    *Server
 	ctx    *RPCContext
 	logger hclog.Logger
+}
+
+func (j *Jobs) Statuses(
+	args *structs.JobsStatusesRequest,
+	reply *structs.JobsStatusesResponse) error {
+
+	authErr := j.srv.Authenticate(j.ctx, args)
+	if done, err := j.srv.forward("Jobs.Statuses", args, args, reply); done {
+		return err
+	}
+	j.srv.MeasureRPCRate("jobs", structs.RateMetricList, args)
+	if authErr != nil {
+		return structs.ErrPermissionDenied
+	}
+	defer metrics.MeasureSince([]string{"nomad", "jobs", "statuses"}, time.Now())
+
+	namespace := args.RequestNamespace()
+	// The ns from the UI by default is "*" which scans the whole "jobs" table.
+	// If specific jobs are requested, all with the same namespace,
+	// we may get some extra efficiency, especially for non-contiguous job IDs.
+	if len(args.Jobs) > 0 {
+		nses := set.New[string](0)
+		for _, j := range args.Jobs {
+			nses.Insert(j.Namespace)
+		}
+		if nses.Size() == 1 {
+			namespace = nses.Slice()[0]
+		}
+	}
+
+	// Check for list-job permissions
+	aclObj, err := j.srv.ResolveACL(args)
+	if err != nil {
+		return err
+	}
+	if !aclObj.AllowNsOp(namespace, acl.NamespaceCapabilityListJobs) {
+		return structs.ErrPermissionDenied
+	}
+	allow := aclObj.AllowNsOpFunc(acl.NamespaceCapabilityListJobs)
+
+	// compare between state run() unblocks to see if the RPC should unblock.
+	// i.e. if new job(s) shift the page, or when job(s) go away.
+	prevJobs := set.New[structs.NamespacedID](0)
+
+	sort := state.QueryOptionSort(args.QueryOptions)
+
+	// Setup the blocking query
+	opts := blockingOptions{
+		queryOpts: &args.QueryOptions,
+		queryMeta: &reply.QueryMeta,
+		run: func(ws memdb.WatchSet, state *state.StateStore) error {
+			var err error
+			var iter memdb.ResultIterator
+
+			// Get the namespaces the user is allowed to access.
+			allowableNamespaces, err := allowedNSes(aclObj, state, allow)
+			if errors.Is(err, structs.ErrPermissionDenied) {
+				// return empty jobs if token isn't authorized for any
+				// namespace, matching other endpoints
+				reply.Jobs = make([]structs.UIJob, 0)
+				return nil
+			} else if err != nil {
+				return err
+			}
+
+			if prefix := args.QueryOptions.Prefix; prefix != "" {
+				iter, err = state.JobsByIDPrefix(ws, namespace, prefix, sort)
+			} else if namespace != structs.AllNamespacesSentinel {
+				iter, err = state.JobsByNamespace(ws, namespace, sort)
+			} else {
+				iter, err = state.Jobs(ws, sort)
+			}
+			if err != nil {
+				return err
+			}
+
+			tokenizer := paginator.NewStructsTokenizer(
+				iter,
+				paginator.StructsTokenizerOptions{
+					WithNamespace: true,
+					WithID:        true,
+				},
+			)
+			filters := []paginator.Filter{
+				paginator.NamespaceFilter{
+					AllowableNamespaces: allowableNamespaces,
+				},
+				// don't include child jobs; we'll look them up later, per parent.
+				paginator.GenericFilter{Allow: func(i interface{}) (bool, error) {
+					job := i.(*structs.Job)
+					return job.ParentID == "", nil
+				}},
+			}
+			// only provide specific jobs if requested.
+			if len(args.Jobs) > 0 {
+				// set per-page to avoid iterating the whole table
+				args.QueryOptions.PerPage = int32(len(args.Jobs))
+				// filter in the requested jobs
+				jobSet := set.From[structs.NamespacedID](args.Jobs)
+				filters = append(filters, paginator.GenericFilter{
+					Allow: func(i interface{}) (bool, error) {
+						job := i.(*structs.Job)
+						return jobSet.Contains(job.NamespacedID()), nil
+					},
+				})
+			}
+
+			jobs := make([]structs.UIJob, 0)
+			newJobs := set.New[structs.NamespacedID](0)
+			pager, err := paginator.NewPaginator(iter, tokenizer, filters, args.QueryOptions,
+				func(raw interface{}) error {
+					job := raw.(*structs.Job)
+					if job == nil {
+						return nil
+					}
+
+					// this is where the sausage is made
+					uiJob, idx, err := UIJobFromJob(ws, state, job, args.SmartOnly)
+					if err != nil {
+						return err
+					}
+
+					jobs = append(jobs, uiJob)
+					newJobs.Insert(job.NamespacedID())
+
+					if idx > reply.Index {
+						reply.Index = idx
+					}
+					return nil
+				})
+			if err != nil {
+				return structs.NewErrRPCCodedf(
+					http.StatusBadRequest, "failed to create result paginator: %v", err)
+			}
+
+			nextToken, err := pager.Page()
+			if err != nil {
+				return structs.NewErrRPCCodedf(
+					http.StatusBadRequest, "failed to read result page: %v", err)
+			}
+
+			// if the page has updated, or a job has gone away,
+			// bump the index to latest jobs entry.
+			if !newJobs.Equal(prevJobs) {
+				reply.Index, err = state.Index("jobs")
+				if err != nil {
+					return err
+				}
+			}
+			prevJobs = newJobs
+
+			reply.QueryMeta.NextToken = nextToken
+			reply.Jobs = jobs
+
+			return nil
+		}}
+	return j.srv.blockingRPC(&opts)
 }
 
 func UIJobFromJob(ws memdb.WatchSet, store *state.StateStore, job *structs.Job, smartOnly bool) (structs.UIJob, uint64, error) {
@@ -107,7 +265,7 @@ func UIJobFromJob(ws memdb.WatchSet, store *state.StateStore, job *structs.Job, 
 			}
 		}
 		uiJob.Allocs = append(uiJob.Allocs, alloc)
-		if a.ModifyIndex > idx { // TODO: unblock if an alloc goes away (like GC)
+		if a.ModifyIndex > idx {
 			idx = a.ModifyIndex
 		}
 	}
@@ -125,155 +283,4 @@ func UIJobFromJob(ws memdb.WatchSet, store *state.StateStore, job *structs.Job, 
 		}
 	}
 	return uiJob, idx, nil
-}
-
-func (j *Jobs) Statuses(
-	args *structs.JobsStatusesRequest,
-	reply *structs.JobsStatusesResponse) error {
-
-	// totally lifted from Job.List
-	authErr := j.srv.Authenticate(j.ctx, args)
-	if done, err := j.srv.forward("Jobs.Statuses", args, args, reply); done {
-		return err
-	}
-	j.srv.MeasureRPCRate("jobs", structs.RateMetricList, args)
-	if authErr != nil {
-		return structs.ErrPermissionDenied
-	}
-	defer metrics.MeasureSince([]string{"nomad", "jobs", "statuses"}, time.Now())
-
-	namespace := args.RequestNamespace()
-	// The ns from the UI by default is "*" which scans the whole "jobs" table.
-	// If specific jobs are requested, all with the same namespace,
-	// we may get some extra efficiency, especially for non-contiguous job IDs.
-	if len(args.Jobs) > 0 {
-		nses := set.New[string](0)
-		for _, j := range args.Jobs {
-			nses.Insert(j.Namespace)
-		}
-		if nses.Size() == 1 {
-			namespace = nses.Slice()[0]
-		}
-	}
-
-	// Check for list-job permissions
-	aclObj, err := j.srv.ResolveACL(args)
-	if err != nil {
-		return err
-	}
-	if !aclObj.AllowNsOp(namespace, acl.NamespaceCapabilityListJobs) {
-		return structs.ErrPermissionDenied
-	}
-	allow := aclObj.AllowNsOpFunc(acl.NamespaceCapabilityListJobs)
-
-	// compare between state run() unblocks to see if the RPC should unblock.
-	// i.e. if new job(s) shift the page, or when job(s) go away.
-	prevJobs := set.New[structs.NamespacedID](0)
-
-	sort := state.QueryOptionSort(args.QueryOptions)
-
-	// Setup the blocking query
-	opts := blockingOptions{
-		queryOpts: &args.QueryOptions,
-		queryMeta: &reply.QueryMeta,
-		run: func(ws memdb.WatchSet, state *state.StateStore) error {
-			var err error
-			var iter memdb.ResultIterator
-
-			// Get the namespaces the user is allowed to access.
-			allowableNamespaces, err := allowedNSes(aclObj, state, allow)
-			if err == structs.ErrPermissionDenied {
-				// return empty jobs if token isn't authorized for any
-				// namespace, matching other endpoints
-				reply.Jobs = make([]structs.UIJob, 0)
-			} else if err != nil {
-				return err
-			} else {
-				if prefix := args.QueryOptions.Prefix; prefix != "" {
-					iter, err = state.JobsByIDPrefix(ws, namespace, prefix, sort)
-				} else if namespace != structs.AllNamespacesSentinel {
-					iter, err = state.JobsByNamespace(ws, namespace, sort)
-				} else {
-					iter, err = state.Jobs(ws, sort)
-				}
-				if err != nil {
-					return err
-				}
-
-				tokenizer := paginator.NewStructsTokenizer(
-					iter,
-					paginator.StructsTokenizerOptions{
-						WithNamespace: true,
-						WithID:        true,
-					},
-				)
-				filters := []paginator.Filter{
-					paginator.NamespaceFilter{
-						AllowableNamespaces: allowableNamespaces,
-					},
-					// don't include child jobs; we'll look them up later, per parent.
-					paginator.GenericFilter{Allow: func(i interface{}) (bool, error) {
-						job := i.(*structs.Job)
-						return job.ParentID == "", nil
-					}},
-				}
-				// only provide specific jobs if requested.
-				if len(args.Jobs) > 0 {
-					jobSet := set.From[structs.NamespacedID](args.Jobs)
-					filters = append(filters, paginator.GenericFilter{
-						Allow: func(i interface{}) (bool, error) {
-							job := i.(*structs.Job)
-							return jobSet.Contains(job.NamespacedID()), nil
-						},
-					})
-				}
-
-				jobs := make([]structs.UIJob, 0)
-				newJobs := set.New[structs.NamespacedID](0)
-				pager, err := paginator.NewPaginator(iter, tokenizer, filters, args.QueryOptions,
-					func(raw interface{}) error {
-						job := raw.(*structs.Job)
-						if job == nil {
-							return nil
-						}
-
-						uiJob, idx, err := UIJobFromJob(ws, state, job, args.SmartOnly)
-						if err != nil {
-							return err
-						}
-
-						jobs = append(jobs, uiJob)
-						newJobs.Insert(job.NamespacedID())
-
-						if idx > reply.Index {
-							reply.Index = idx
-						}
-						return nil
-					})
-				if err != nil {
-					return structs.NewErrRPCCodedf(
-						http.StatusBadRequest, "failed to create result paginator: %v", err)
-				}
-
-				nextToken, err := pager.Page()
-				if err != nil {
-					return structs.NewErrRPCCodedf(
-						http.StatusBadRequest, "failed to read result page: %v", err)
-				}
-
-				// if the page has updated, or a job has gone away, bump the index to latest jobs entry.
-				if !newJobs.Equal(prevJobs) {
-					reply.Index, err = state.Index("jobs")
-					if err != nil {
-						return err
-					}
-				}
-				prevJobs = newJobs
-
-				reply.QueryMeta.NextToken = nextToken
-				reply.Jobs = jobs
-			}
-			return nil
-		}}
-	return j.srv.blockingRPC(&opts)
 }
