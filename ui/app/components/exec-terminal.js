f918170c14c85facf72c839cd7f29491f9c953c0
Phil Renaud <phil@riotindustries.com>
2024-02-14 11:18:45 -0500

    Confirmation on exit from exec as long as socket has been opened

--- ui/app/components/exec-terminal.js
+++ ui/app/components/exec-terminal.js
@@ -3,15 +3,21 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
+// @ts-check
+
 import Component from '@ember/component';
 import { FitAddon } from 'xterm-addon-fit';
 import WindowResizable from '../mixins/window-resizable';
 import { classNames } from '@ember-decorators/component';
 import classic from 'ember-classic-decorator';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 @classic
 @classNames('terminal-container')
 export default class ExecTerminal extends Component.extend(WindowResizable) {
+  @service router;
+
   didInsertElement() {
     super.didInsertElement(...arguments);
     let fitAddon = new FitAddon();
@@ -21,6 +27,38 @@ export default class ExecTerminal extends Component.extend(WindowResizable) {
     this.terminal.open(this.element.querySelector('.terminal'));
 
     fitAddon.fit();
+    this.addExitHandler();
+  }
+
+  socketOpen = false;
+  hasRemovedExitHandler = false;
+
+  @action
+  addExitHandler() {
+    window.addEventListener('beforeunload', this.confirmExit.bind(this));
+  }
+  removeExitHandler() {
+    if (!this.hasRemovedExitHandler) {
+      window.removeEventListener('beforeunload', this.confirmExit.bind(this));
+      this.hasRemovedExitHandler = true;
+    }
+  }
+
+  /**
+   *
+   * @param {BeforeUnloadEvent} event
+   * @returns {string}
+   */
+  confirmExit(event) {
+    if (this.socketOpen) {
+      event.preventDefault();
+      return (event.returnValue = 'Are you sure you want to exit?');
+    }
+  }
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+    this.removeExitHandler();
   }
 
   windowResizeHandler(e) {
