1b20f198e501c4b33f0eee40622717983ae65e93
Phil Renaud <phil@riotindustries.com>
2024-02-16 10:24:15 -0500

    Nomad logo svg component added

--- ui/app/helpers/x-icon.js
+++ ui/app/helpers/x-icon.js
@@ -1,30 +1,30 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: BUSL-1.1
- */
+// /**
+//  * Copyright (c) HashiCorp, Inc.
+//  * SPDX-License-Identifier: BUSL-1.1
+//  */
 
-import { helper } from '@ember/component/helper';
-import { inlineSvg } from 'ember-inline-svg/helpers/inline-svg';
+// import { helper } from '@ember/component/helper';
+// import { inlineSvg } from 'ember-inline-svg/helpers/inline-svg';
 
-// Generated at compile-time by ember-inline-svg
-import SVGs from '../svgs';
+// // Generated at compile-time by ember-inline-svg
+// import SVGs from '../svgs';
 
-/**
- * Icon Helper
- *
- * Usage: {{x-icon name}}
- *
- * Renders an inline svg element by looking it up at `/public/images/icons/${name}.svg`
- */
-export function xIcon(params, options) {
-  const name = params[0];
-  const classes = [options.class, 'icon', `icon-is-${name}`]
-    .compact()
-    .join(' ');
+// /**
+//  * Icon Helper
+//  *
+//  * Usage: {{x-icon name}}
+//  *
+//  * Renders an inline svg element by looking it up at `/public/images/icons/${name}.svg`
+//  */
+// export function xIcon(params, options) {
+//   const name = params[0];
+//   const classes = [options.class, 'icon', `icon-is-${name}`]
+//     .compact()
+//     .join(' ');
 
-  return inlineSvg(SVGs, name, { class: classes });
-}
+//   return inlineSvg(SVGs, name, { class: classes });
+// }
 
-export default helper(xIcon);
+// export default helper(xIcon);
 
-// TODO: delete file
+// // TODO: delete file
