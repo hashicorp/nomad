7f8503b04ec9153e3ac7c611b086990e4b248a69
Tim Gross <tgross@hashicorp.com>
2024-02-20 10:17:27 -0500

    cni: add DNS set by CNI plugins to task configuration (#20007)
    CNI plugins may set DNS configuration, but this isn't threaded through to the
    task configuration so that we can write it to the `/etc/resolv.conf` file as
    needed. Add the `AllocNetworkStatus` to the alloc hook resources so they're
    accessible from the taskrunner. Any DNS entries provided by the user will
    override these values.
    
    Fixes: https://github.com/hashicorp/nomad/issues/11102

--- client/structs/allochook.go
+++ client/structs/allochook.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hashicorp/nomad/client/pluginmanager/csimanager"
 	"github.com/hashicorp/nomad/helper"
+	"github.com/hashicorp/nomad/nomad/structs"
 )
 
 // AllocHookResources contains data that is provided by AllocRunner Hooks for
@@ -15,7 +16,8 @@ import (
 // AllocRunner and then only accessed via getters and setters that hold the
 // lock.
 type AllocHookResources struct {
-	csiMounts map[string]*csimanager.MountInfo
+	csiMounts     map[string]*csimanager.MountInfo
+	networkStatus *structs.AllocNetworkStatus
 
 	mu sync.RWMutex
 }
@@ -42,4 +44,22 @@ func (a *AllocHookResources) SetCSIMounts(m map[string]*csimanager.MountInfo) {
 	defer a.mu.Unlock()
 
 	a.csiMounts = m
+}
+
+// GetAllocNetworkStatus returns a copy of the AllocNetworkStatus previously
+// written the group's network_hook
+func (a *AllocHookResources) GetAllocNetworkStatus() *structs.AllocNetworkStatus {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+
+	return a.networkStatus.Copy()
+}
+
+// SetAllocNetworkStatus stores the AllocNetworkStatus for later use by the
+// taskrunner's buildTaskConfig() method
+func (a *AllocHookResources) SetAllocNetworkStatus(ans *structs.AllocNetworkStatus) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	a.networkStatus = ans
 }
