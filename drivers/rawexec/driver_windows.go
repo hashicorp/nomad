d1cdbacbb38da17ac05cc5762757b8a6549f9c01
Mike Nomitch <mail@mikenomitch.com>
2024-03-05 15:47:41 -0800

    Fixed windows build error

--- drivers/rawexec/driver_windows.go
+++ drivers/rawexec/driver_windows.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/nomad/plugins/drivers"
 )
 
-func (tc *TaskConfig) Validate(cfg *drivers.TaskConfig, driverCofig Config) error {
+func (tc *TaskConfig) Validate(driverCofig Config, cfg drivers.TaskConfig) error {
 	// This is a noop on windows since the uid and gid cannot be checked against a range easily
 	// We could eventually extend this functionality to check for individual users IDs strings
 	// but that is not currently supported. See driverValidators.UserInRange for
