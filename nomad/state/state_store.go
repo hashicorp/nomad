b798e46afdec806087d4feb28fcf02dc9d10fb01
Daniel Bennett <dbennett@hashicorp.com>
2024-03-13 16:01:39 -0500

    revert "add JobSummary.Children.Desired"
    This reverts commit 6a8b132408a19e2f46eb2757107e5c7471df93db.

--- nomad/state/state_store.go
+++ nomad/state/state_store.go
@@ -5492,9 +5492,7 @@ func (s *StateStore) updateSummaryWithJob(index uint64, job *structs.Job,
 		hasSummaryChanged = true
 	}
 
-	var totalCount int64
 	for _, tg := range job.TaskGroups {
-		totalCount += int64(tg.Count)
 		if _, ok := summary.Summary[tg.Name]; !ok {
 			newSummary := structs.TaskGroupSummary{
 				Complete: 0,
@@ -5506,7 +5504,6 @@ func (s *StateStore) updateSummaryWithJob(index uint64, job *structs.Job,
 			hasSummaryChanged = true
 		}
 	}
-	summary.Children.Desired = totalCount
 
 	// The job summary has changed, so update the modify index.
 	if hasSummaryChanged {
@@ -5864,12 +5861,6 @@ func (s *StateStore) updateSummaryWithAlloc(index uint64, alloc *structs.Allocat
 	if jobSummary.CreateIndex != alloc.Job.CreateIndex {
 		return nil
 	}
-
-	var totalCount int64
-	for _, tg := range alloc.Job.TaskGroups {
-		totalCount += int64(tg.Count)
-	}
-	jobSummary.Children.Desired = totalCount
 
 	tgSummary, ok := jobSummary.Summary[alloc.TaskGroup]
 	if !ok {
