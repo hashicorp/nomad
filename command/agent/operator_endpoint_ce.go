559efd086c134ca9c991f359bf8ae5bfc0eff9ed
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2024-03-18 10:55:03 -0500

    Backport of autopilot: add Enterprise health information to API endpoint into release/1.7.x (#20155)
    Co-authored-by: Tim Gross <tgross@hashicorp.com>

--- command/agent/operator_endpoint_ce.go
+++ command/agent/operator_endpoint_ce.go
@@ -8,6 +8,9 @@ package agent
 
 import (
 	"net/http"
+
+	"github.com/hashicorp/nomad/api"
+	"github.com/hashicorp/nomad/nomad/structs"
 )
 
 func (s *HTTPServer) LicenseRequest(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
@@ -20,5 +23,8 @@ func (s *HTTPServer) LicenseRequest(resp http.ResponseWriter, req *http.Request)
 	default:
 		return nil, CodedError(405, ErrInvalidMethod)
 	}
+}
 
+func autopilotToAPIEntState(_ structs.OperatorHealthReply, _ *api.OperatorHealthReply) interface{} {
+	return nil
 }
