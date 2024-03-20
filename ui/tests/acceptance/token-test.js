e5e532f1f6632679afd1942f7cbc9232848c486c
Phil Renaud <phil@riotindustries.com>
2024-03-20 12:35:42 -0400

    Job row class for test purposes

--- ui/tests/acceptance/token-test.js
+++ ui/tests/acceptance/token-test.js
@@ -194,12 +194,11 @@ module('Acceptance | tokens', function (hooks) {
     await Tokens.visit();
     await Tokens.secret(secretId).submit();
 
-    server.pretender.get('/v1/jobs', function () {
+    server.pretender.get('/v1/jobs/statuses', function () {
       return [200, {}, '[]'];
     });
 
     await Jobs.visit();
-
     // If jobs are lingering in the store, they would show up
     assert.notOk(find('[data-test-job-row]'), 'No jobs found');
   });
@@ -272,7 +271,7 @@ module('Acceptance | tokens', function (hooks) {
         },
       ],
     };
-    server.pretender.get('/v1/jobs', function () {
+    server.pretender.get('/v1/jobs/statuses', function () {
       return [500, {}, JSON.stringify(expiredServerError)];
     });
 
@@ -298,7 +297,7 @@ module('Acceptance | tokens', function (hooks) {
         },
       ],
     };
-    server.pretender.get('/v1/jobs', function () {
+    server.pretender.get('/v1/jobs/statuses', function () {
       return [500, {}, JSON.stringify(notFoundServerError)];
     });
 
@@ -843,8 +842,7 @@ module('Acceptance | tokens', function (hooks) {
 
       // Pop over to the jobs page and make sure the Run button is disabled
       await visit('/jobs');
-      assert.dom('[data-test-run-job]').hasTagName('button');
-      assert.dom('[data-test-run-job]').isDisabled();
+      assert.dom('[data-test-run-job]').hasAttribute('disabled');
 
       // Sign out, and sign back in as a high-level role token
       await Tokens.visit();
