7eb911a29ef3f1d6566555e4e6559cbbc9d6e850
Tim Gross <tgross@hashicorp.com>
2024-03-27 14:24:23 -0400

    tproxy: networking hook changes (#20183)
    When `transparent_proxy` block is present and the network mode is `bridge`, use
    a different CNI configuration that includes the `consul-cni` plugin. Before
    invoking the CNI plugins, create a Consul SDK `iptables.Config` struct for the
    allocation. This includes:
    
    * Use all the `transparent_proxy` block fields
    * The reserved ports are added to the inbound exclusion list so the alloc is
      reachable from outside the mesh
    * The `expose` blocks and `check` blocks with `expose=true` are added to the
      inbound exclusion list so health checks work.
    
    The `iptables.Config` is then passed as a CNI argument to the `consul-cni`
    plugin.
    
    Ref: https://github.com/hashicorp/nomad/issues/10628

--- client/allocrunner/networking_bridge_linux_test.go
+++ client/allocrunner/networking_bridge_linux_test.go
@@ -14,8 +14,9 @@ import (
 func Test_buildNomadBridgeNetConfig(t *testing.T) {
 	ci.Parallel(t)
 	testCases := []struct {
-		name string
-		b    *bridgeNetworkConfigurator
+		name          string
+		withConsulCNI bool
+		b             *bridgeNetworkConfigurator
 	}{
 		{
 			name: "empty",
@@ -38,14 +39,28 @@ func Test_buildNomadBridgeNetConfig(t *testing.T) {
 				hairpinMode: true,
 			},
 		},
+		{
+			name:          "consul-cni",
+			withConsulCNI: true,
+			b: &bridgeNetworkConfigurator{
+				bridgeName:  defaultNomadBridgeName,
+				allocSubnet: defaultNomadAllocSubnet,
+				hairpinMode: true,
+			},
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			tc := tc
 			ci.Parallel(t)
-			bCfg := buildNomadBridgeNetConfig(*tc.b)
+			bCfg := buildNomadBridgeNetConfig(*tc.b, tc.withConsulCNI)
 			// Validate that the JSON created is rational
 			must.True(t, json.Valid(bCfg))
+			if tc.withConsulCNI {
+				must.StrContains(t, string(bCfg), "consul-cni")
+			} else {
+				must.StrNotContains(t, string(bCfg), "consul-cni")
+			}
 		})
 	}
 }
