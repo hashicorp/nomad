27dfa980d59bb6093103ecb4cc906685af0eb3b3
Tim Gross <tgross@hashicorp.com>
2024-04-03 14:20:50 -0400

    E2E: refactor Connect tests to use stdlib testing
    Migrate our E2E tests for Connect off the old framework in preparation for
    writing E2E tests for transparent proxy and the updated workload identity
    workflow. Mark the tests that cover the legacy Consul token submitted workflow.
    
    Ref: https://github.com/hashicorp/nomad/pull/20175

---
+++ e2e/connect/connect_test.go
@@ -0,0 +1,132 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package connect
+
+import (
+	"testing"
+	"time"
+
+	capi "github.com/hashicorp/consul/api"
+	"github.com/hashicorp/nomad/e2e/e2eutil"
+	"github.com/hashicorp/nomad/e2e/v3/jobs3"
+	"github.com/shoenig/test"
+	"github.com/shoenig/test/must"
+)
+
+func TestConnect(t *testing.T) {
+
+	nomadClient := e2eutil.NomadClient(t)
+	e2eutil.WaitForLeader(t, nomadClient)
+	e2eutil.WaitForNodesReady(t, nomadClient, 2)
+
+	t.Cleanup(func() {
+		_, err := e2eutil.Command("nomad", "system", "gc")
+		test.NoError(t, err)
+	})
+
+	t.Run("ConnectDemo", testConnectDemo)
+	t.Run("ConnectCustomSidecarExposed", testConnectCustomSidecarExposed)
+	t.Run("ConnectNativeDemo", testConnectNativeDemo)
+	t.Run("ConnectIngressGatewayDemo", testConnectIngressGatewayDemo)
+	t.Run("ConnectMultiIngress", testConnectMultiIngressGateway)
+	t.Run("ConnectTerminatingGateway", testConnectTerminatingGateway)
+	t.Run("ConnectMultiService", testConnectMultiService)
+}
+
+// testConnectDemo tests the demo job file used in Connect Integration examples.
+func testConnectDemo(t *testing.T) {
+	_, cleanup := jobs3.Submit(t, "./input/demo.nomad", jobs3.Timeout(time.Second*60))
+	t.Cleanup(cleanup)
+
+	cc := e2eutil.ConsulClient(t)
+
+	ixn := &capi.Intention{
+		SourceName:      "count-dashboard",
+		DestinationName: "count-api",
+		Action:          "allow",
+	}
+	_, err := cc.Connect().IntentionUpsert(ixn, nil)
+	must.NoError(t, err, must.Sprint("could not create intention"))
+
+	t.Cleanup(func() {
+		_, err := cc.Connect().IntentionDeleteExact("count-dashboard", "count-api", nil)
+		test.NoError(t, err)
+	})
+
+	assertServiceOk(t, cc, "count-api-sidecar-proxy")
+	assertServiceOk(t, cc, "count-dashboard-sidecar-proxy")
+}
+
+// testConnectCustomSidecarExposed tests that a connect sidecar with custom task
+// definition can also make use of the expose service check feature.
+func testConnectCustomSidecarExposed(t *testing.T) {
+	_, cleanup := jobs3.Submit(t, "./input/expose-custom.nomad", jobs3.Timeout(time.Second*60))
+	t.Cleanup(cleanup)
+}
+
+// testConnectNativeDemo tests the demo job file used in Connect Native
+// Integration examples.
+func testConnectNativeDemo(t *testing.T) {
+	_, cleanup := jobs3.Submit(t, "./input/native-demo.nomad", jobs3.Timeout(time.Second*60))
+	t.Cleanup(cleanup)
+}
+
+// testConnectIngressGatewayDemo tests a job with an ingress gateway
+func testConnectIngressGatewayDemo(t *testing.T) {
+	_, cleanup := jobs3.Submit(t, "./input/ingress-gateway.nomad", jobs3.Timeout(time.Second*60))
+	t.Cleanup(cleanup)
+}
+
+// testConnectMultiIngressGateway tests a job with multiple ingress gateways
+func testConnectMultiIngressGateway(t *testing.T) {
+	_, cleanup := jobs3.Submit(t, "./input/multi-ingress.nomad", jobs3.Timeout(time.Second*60))
+	t.Cleanup(cleanup)
+}
+
+// testConnectTerminatingGateway tests a job with a terminating gateway
+func testConnectTerminatingGateway(t *testing.T) {
+	_, cleanup := jobs3.Submit(t, "./input/terminating-gateway.nomad", jobs3.Timeout(time.Second*60))
+	t.Cleanup(cleanup)
+
+	cc := e2eutil.ConsulClient(t)
+
+	ixn := &capi.Intention{
+		SourceName:      "count-dashboard",
+		DestinationName: "count-api",
+		Action:          "allow",
+	}
+	_, err := cc.Connect().IntentionUpsert(ixn, nil)
+	must.NoError(t, err, must.Sprint("could not create intention"))
+
+	t.Cleanup(func() {
+		_, err := cc.Connect().IntentionDeleteExact("count-dashboard", "count-api", nil)
+		test.NoError(t, err)
+	})
+
+	assertServiceOk(t, cc, "api-gateway")
+	assertServiceOk(t, cc, "count-dashboard-sidecar-proxy")
+	assertServiceOk(t, cc, "count-api")
+}
+
+// testConnectMultiService tests a job with multiple Connect blocks in the same
+// group
+func testConnectMultiService(t *testing.T) {
+	_, cleanup := jobs3.Submit(t, "./input/multi-service.nomad", jobs3.Timeout(time.Second*60))
+	t.Cleanup(cleanup)
+
+	cc := e2eutil.ConsulClient(t)
+	assertServiceOk(t, cc, "echo1-sidecar-proxy")
+	assertServiceOk(t, cc, "echo2-sidecar-proxy")
+}
+
+// assertServiceOk is a test helper to assert a service is passing health checks, if any
+func assertServiceOk(t *testing.T, cc *capi.Client, name string) {
+	t.Helper()
+	services, _, err := cc.Health().Service(name, "", false, nil)
+	must.NoError(t, err)
+	must.Greater(t, 0, len(services), must.Sprintf("found no services for %q", name))
+
+	status := services[0].Checks.AggregatedStatus()
+	must.Eq(t, "passing", status)
+}
