8cfc9434af36fe65d9b43cd62e5bb7219d03efe1
Luiz Aoqui <luiz@hashicorp.com>
2024-04-10 20:03:00 -0400

    mock_driver: fix fingreprint key
    The `mock_driver` is an internal task driver used mostly for testing and
    simulating workloads. During the allocrunner v2 work (#4792) its name
    changed from `mock_driver` to just `mock` and then back to
    `mock_driver`, but the fingreprint key was kept as `driver.mock`.
    
    This results in tasks configured with `driver = "mock"` to be scheduled
    (because Nomad thinks the client has a task driver called `mock`), but
    fail to actually run (because the Nomad client can't find a driver
    called `mock` in its catalog).
    
    Fingerprinting the right name prevents the job from being scheduled in
    the first place.
    
    Also removes mentions of the mock driver from documentation since its an
    internal driver and not available in any production release.

--- drivers/mock/driver.go
+++ drivers/mock/driver.go
@@ -345,7 +345,7 @@ func (d *Driver) buildFingerprint() *drivers.Fingerprint {
 		desc = "disabled"
 	} else {
 		health = drivers.HealthStateHealthy
-		attrs["driver.mock"] = pstructs.NewBoolAttribute(true)
+		attrs["driver.mock_driver"] = pstructs.NewBoolAttribute(true)
 		desc = drivers.DriverHealthy
 	}
 
