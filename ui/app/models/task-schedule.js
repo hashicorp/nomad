159dd5f705ec2d7c6ca7c403108c9166067acdab
Phil Renaud <phil@riotindustries.com>
2024-05-15 16:59:35 -0400

    Fragment and init

---
+++ ui/app/models/task-schedule.js
@@ -0,0 +1,8 @@
+import Fragment from 'ember-data-model-fragments/fragment';
+import { attr } from '@ember-data/model';
+import { fragmentOwner } from 'ember-data-model-fragments/attributes';
+
+export default class TaskScheduleModel extends Fragment {
+  @fragmentOwner('task') task;
+  @attr() cron;
+}
