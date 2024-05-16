ece96b63766827673558ed667e6d25db5113b936
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2024-05-16 10:46:12 -0400

    Backport of CSI: allow plugin GC to detect jobs with updated plugin IDs into release/1.7.x (#20613)
    Co-authored-by: Tim Gross <tgross@hashicorp.com>

--- nomad/structs/errors.go
+++ nomad/structs/errors.go
@@ -81,6 +81,7 @@ var (
 	ErrCSIClientRPCRetryable  = errors.New("CSI client error (retryable)")
 	ErrCSIVolumeMaxClaims     = errors.New("volume max claims reached")
 	ErrCSIVolumeUnschedulable = errors.New("volume is currently unschedulable")
+	ErrCSIPluginInUse         = errors.New("plugin in use")
 )
 
 // IsErrNoLeader returns whether the error is due to there being no leader.
