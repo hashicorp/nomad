e20c8f8634c1cbed4519ff97cc35700e7611997f
Phil Renaud <phil@riotindustries.com>
2024-06-05 15:06:27 -0400

    A few more style tweaks and the beginnings of a recent-searches list

--- ui/tests/unit/initializers/history-test.js
+++ ui/tests/unit/initializers/history-test.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import Application from '@ember/application';
 
 import config from 'nomad-ui/config/environment';
