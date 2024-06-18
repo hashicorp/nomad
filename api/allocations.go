581221b51eab427b5aabb2bcd5d5adc48c200aec
Daniel Bennett <dbennett@hashicorp.com>
2024-06-18 11:36:55 -0400

    api: use the task in Allocations.GetPauseState

--- api/allocations.go
+++ api/allocations.go
@@ -251,7 +251,7 @@ func (a *Allocations) SetPauseState(alloc *Allocation, q *QueryOptions, task, st
 // The ?task=<task> query parameter must be set.
 func (a *Allocations) GetPauseState(alloc *Allocation, q *QueryOptions, task string) (string, *QueryMeta, error) {
 	var resp AllocGetPauseResponse
-	qm, err := a.client.query("/v1/client/allocation/"+alloc.ID+"/pause", &resp, q)
+	qm, err := a.client.query("/v1/client/allocation/"+alloc.ID+"/pause?task="+task, &resp, q)
 	state := resp.ScheduleState
 	return state, qm, err
 }
