cd444b9045ca40636340ea5f52004693720aaf25
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-07-16 19:42:24 +0200

    check for docker-ce vs docker-ee on windows

--- drivers/shared/capabilities/defaults_default.go
+++ drivers/shared/capabilities/defaults_default.go
@@ -5,12 +5,14 @@
 
 package capabilities
 
+import docker "github.com/fsouza/go-dockerclient"
+
 // DockerDefaults is a list of Linux capabilities enabled by Docker by default
 // and is used to compute the set of capabilities to add/drop given docker driver
 // configuration.
 //
 // https://docs.docker.com/engine/reference/run/#runtime-privilege-and-linux-capabilities
-func DockerDefaults() *Set {
+func DockerDefaults(info *docker.DockerInfo) *Set {
 	defaults := NomadDefaults()
 	defaults.Add("NET_RAW")
 	return defaults
--- drivers/shared/capabilities/defaults_windows.go
+++ drivers/shared/capabilities/defaults_windows.go
@@ -5,10 +5,25 @@
 
 package capabilities
 
+import (
+	"strings"
+
+	docker "github.com/fsouza/go-dockerclient"
+)
+
 // DockerDefaults is a list of Windows capabilities enabled by Docker by default
 // and is used to compute the set of capabilities to add/drop given docker driver
 // configuration.
-func DockerDefaults() *Set {
+//
+// Doing this on windows is somewhat tricky, because capabilities differ by
+// runtime, so we have to perform some extra checks.
+func DockerDefaults(info *docker.DockerInfo) *Set {
 	defaults := NomadDefaults()
+
+	// Docker CE doesn't support NET_RAW on Windows, Mirantis (aka Docker EE) does
+	if info != nil && !strings.Contains(info.ServerVersion, "-ce") {
+		defaults.Add("NET_RAW")
+	}
+
 	return defaults
 }
