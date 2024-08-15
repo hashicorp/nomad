15773b6fcd1bbd7c81a4a1bab597d01196fea199
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-08-15 16:31:27 +0000

    backport of commit 0bc9796d3b892b0f0c86fb37c5644f41a4a5c076

--- client/fingerprint/cpu.go
+++ client/fingerprint/cpu.go
@@ -164,6 +164,10 @@ func (f *CPUFingerprint) setTotalCompute(response *FingerprintResponse) {
 	totalCompute := f.top.TotalCompute()
 	usableCompute := f.top.UsableCompute()
 
+	if totalCompute == 0 {
+		f.logger.Error("cpu.totalcompute is zero, CPU fingerprinting likely failed", "cpu.totalcompute", totalCompute)
+	}
+
 	response.AddAttribute("cpu.totalcompute", f.frequency(totalCompute))
 	response.AddAttribute("cpu.usablecompute", f.frequency(usableCompute))
 }
