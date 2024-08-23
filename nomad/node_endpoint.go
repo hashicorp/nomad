d0e541aff4f54ae044741aabbe5481aa48019640
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-08-23 09:56:18 +0200

    rephrase comment

--- nomad/node_endpoint.go
+++ nomad/node_endpoint.go
@@ -1695,8 +1695,11 @@ func (n *Node) createNodeEvals(node *structs.Node, nodeIndex uint64) ([]string, 
 		}
 		jobIDs[alloc.JobNamespacedID()] = struct{}{}
 
-		// If it's a sysbatch job, skip it. Sysbatch job evals should only
-		// ever be created by structs.EvalTriggerPeriodicJob
+		// If it's a sysbatch job, skip it. Sysbatch job evals should only ever
+		// be created by periodic-job if they are periodic, and job-register or
+		// job-scaling if they are not. Calling the system scheduler by
+		// node-update trigger can cause unnecessary or premature allocations
+		// to be created.
 		if alloc.Job.Type == structs.JobTypeSysBatch {
 			continue
 		}
