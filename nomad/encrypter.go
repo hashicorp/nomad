ff20710101d4f149b6c71790f2853413c9d685b6
James Rasell <jrasell@users.noreply.github.com>
2024-11-07 16:08:18 +0000

    backport of commit 316430b094fd1637ce14a723a0c3441dfa94e0a9

--- nomad/encrypter.go
+++ nomad/encrypter.go
@@ -805,7 +805,7 @@ func (e *Encrypter) loadKeyFromStore(path string) (*structs.UnwrappedRootKey, er
 	// 1.7 an ed25519 key derived from the root key was used instead of an RSA
 	// key.
 	var rsaKey []byte
-	if kekWrapper.WrappedRSAKey != nil {
+	if kekWrapper.WrappedRSAKey != nil && len(kekWrapper.WrappedRSAKey.Ciphertext) > 0 {
 		rsaKey, err = wrapper.Decrypt(e.srv.shutdownCtx, kekWrapper.WrappedRSAKey)
 		if err != nil {
 			return nil, fmt.Errorf("%w (rsa key): %w", ErrDecryptFailed, err)
