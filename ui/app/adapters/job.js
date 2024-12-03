9bcbf821abd35cfadaaa1b98a1538b851d650aac
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2024-12-03 16:38:50 +0100

    backport of commit 4708e06199d539292dd80eda331998e3d20871b8 (#24490)
    Co-authored-by: Phil Renaud <phil.renaud@hashicorp.com>

--- ui/app/adapters/job.js
+++ ui/app/adapters/job.js
@@ -204,22 +204,8 @@ export default class JobAdapter extends WatchableNamespaceIDs {
   }
 
   getVersions(job, diffVersion) {
-    const url = addToPath(
-      this.urlForFindRecord(job.get('id'), 'job'),
-      '/versions'
-    );
-
-    const namespace = job.get('namespace.name') || 'default';
-
-    const query = {
-      namespace,
-      diffs: true,
-    };
-
-    if (diffVersion) {
-      query.diff_version = diffVersion;
-    }
-    return this.ajax(url, 'GET', { data: query });
+    let url = this.urlForVersions(job, diffVersion);
+    return this.ajax(url, 'GET');
   }
 
   /**
@@ -306,6 +292,17 @@ export default class JobAdapter extends WatchableNamespaceIDs {
       }
     }
     return result;
+  }
+
+  urlForVersions(job, diffVersion) {
+    let url = this.urlForFindRecord(job.get('id'), 'job', null, 'versions');
+
+    let paramString = 'diffs=true';
+    if (diffVersion) {
+      paramString += `&diff_version=${diffVersion}`;
+    }
+    url = addToPath(url, '', paramString);
+    return url;
   }
 
   urlForQuery(query, modelName, method) {
