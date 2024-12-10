aa0ae7c0bbbebb948406f46ad7082faea1394630
Charles Zaffery <czaffery@roblox.com>
2024-12-10 14:51:37 -0800

    add noswap to secretdir tmpfs

--- client/allocdir/fs_linux.go
+++ client/allocdir/fs_linux.go
@@ -73,7 +73,7 @@ func createSecretDir(dir string, size int) error {
 		}
 
 		flags := uintptr(syscall.MS_NOEXEC)
-		// Permanantly disable swap for tmpfs for SecretDir.
+		// Permanently disable swap for tmpfs for SecretDir.
 		options := fmt.Sprintf("size=%dm,noswap", size)
 		if err := syscall.Mount("tmpfs", dir, "tmpfs", flags, options); err != nil {
 			return os.NewSyscallError("mount", err)
