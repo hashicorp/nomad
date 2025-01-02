03b72f0fc641f9c797092b84464fd697966ba933
Juana De La Cuesta <juanita.delacuestamorales@hashicorp.com>
2025-01-02 15:43:13 +0100

    Update e2e/terraform/main.tf
    Co-authored-by: Tim Gross <tgross@hashicorp.com>

--- e2e/terraform/main.tf
+++ e2e/terraform/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 module "provision-infra" {
   source = "./provision-infra"
 
-  server_count                    = var.client_count_linux
+  server_count                    = var.server_count
   client_count_linux              = var.client_count_linux
   client_count_windows_2016_amd64 = var.client_count_windows_2016_amd64
   nomad_local_binary              = var.nomad_local_binary
