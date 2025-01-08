4f3e78dc356055510b331c468590fd491d10bed5
Juanadelacuesta <8647634+Juanadelacuesta@users.noreply.github.com>
2025-01-08 15:33:12 -0500

    Add tests for servers and clients

--- enos/modules/run_workloads/outputs.tf
+++ enos/modules/run_workloads/outputs.tf
@@ -1,15 +1,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: BUSL-1.1
 
-/* output "raw-exec-service_id" {
-    value = nomad_job.raw-exec-service.name
-}
-
-output "docker-service_id" {
-    value = nomad_job.docker-service.name
-}
- */
-
 output "job_names" {
   value = [for j in nomad_job.workloads : j.name]
 }
