7776501f35ba0c97ee028fce4083aa106b34cc2e
Juanadelacuesta <8647634+Juanadelacuesta@users.noreply.github.com>
2025-01-16 22:57:14 +0100

    func: unify keys and make them cluster grouped

--- e2e/terraform/main.tf
+++ e2e/terraform/main.tf
@@ -8,13 +8,13 @@ provider "aws" {
 module "provision-infra" {
   source = "./provision-infra"
 
-  server_count                    = var.server_count
-  client_count_linux              = var.client_count_linux
-  client_count_windows_2016       = var.client_count_windows_2016
-  nomad_local_binary              = var.nomad_local_binary
-  nomad_license                   = var.nomad_license
-  consul_license                  = var.consul_license
-  nomad_region                    = var.nomad_region
-  instance_arch                   = var.instance_arch
-  name                            = var.name
+  server_count              = var.server_count
+  client_count_linux        = var.client_count_linux
+  client_count_windows_2016 = var.client_count_windows_2016
+  nomad_local_binary        = var.nomad_local_binary
+  nomad_license             = var.nomad_license
+  consul_license            = var.consul_license
+  nomad_region              = var.nomad_region
+  instance_arch             = var.instance_arch
+  name                      = var.name
 }
