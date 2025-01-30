ae40fc4d44fe2e9fbbd40447c55915dcab52b85c
Juanadelacuesta <8647634+Juanadelacuesta@users.noreply.github.com>
2025-01-30 10:49:54 +0100

    fix: move taken and restoring snapshots out of the upgrade_single_server to avoid possible race conditions

--- enos/modules/upgrade_servers/main.tf
+++ enos/modules/upgrade_servers/main.tf
@@ -9,7 +9,27 @@ terraform {
   }
 }
 
+locals {
+  snap_file          = "${var.name}.snap"
+}
+
+resource "enos_local_exec" "take_server_snapshot" {
+  environment = {
+    NOMAD_ADDR        = var.nomad_addr
+    NOMAD_CACERT      = var.ca_file
+    NOMAD_CLIENT_CERT = var.cert_file
+    NOMAD_CLIENT_KEY  = var.key_file
+    NOMAD_TOKEN       = var.nomad_token
+  }
+
+  inline = [
+    "nomad operator snapshot save ${local.snap_file}",
+  ]
+}
+
 module upgrade_single_cluster {
+  depends_on = [enos_local_exec.take_server_snapshot]
+  
   source   = "../upgrade_single_server"
   for_each = { for i, val in var.servers : i => val }
 
@@ -22,4 +42,20 @@ module upgrade_single_cluster {
   server_address             = each.value
   nomad_local_upgrade_binary = var.nomad_local_upgrade_binary
   ssh_key_path               = var.ssh_key_path
+}
+
+resource "enos_local_exec" "restore_server_snapshot" {
+  depends_on = [module.upgrade_single_cluster]
+
+  environment = {
+    NOMAD_ADDR        = var.nomad_addr
+    NOMAD_CACERT      = var.ca_file
+    NOMAD_CLIENT_CERT = var.cert_file
+    NOMAD_CLIENT_KEY  = var.key_file
+    NOMAD_TOKEN       = var.nomad_token
+  }
+
+  inline = [
+    "nomad operator snapshot restore ${local.snap_file}",
+  ]
 }
--- enos/modules/upgrade_single_server/main.tf
+++ enos/modules/upgrade_single_server/main.tf
@@ -10,11 +10,8 @@ terraform {
 }
 
 locals {
-  clean_token        = trimspace(var.nomad_token) # Somewhere in the process, a newline is added to the token.
   binary_destination = var.platform == "windows" ? "C:/opt/nomad.exe" : "/usr/local/bin/nomad"
   ssh_user           = var.platform == "windows" ? "Administrator" : "ubuntu"
-  tmp                = replace(var.server_address, ".", "_")
-  snap_file          = "server-${local.tmp}.snap"
 }
 
 resource "enos_file" "copy_upgraded_binary" {
@@ -31,23 +28,10 @@ resource "enos_file" "copy_upgraded_binary" {
   }
 }
 
-resource "enos_local_exec" "take_server_snapshot" {
-  environment = {
-    NOMAD_ADDR        = var.nomad_addr
-    NOMAD_CACERT      = var.ca_file
-    NOMAD_CLIENT_CERT = var.cert_file
-    NOMAD_CLIENT_KEY  = var.key_file
-    NOMAD_TOKEN       = local.clean_token
-  }
-
-  inline = [
-    "nomad operator snapshot save -stale=true ${local.snap_file}",
-  ]
-}
 
 resource "enos_remote_exec" "restart_linux_services" {
   count      = var.platform == "linux" ? 1 : 0
-  depends_on = [enos_local_exec.take_server_snapshot, enos_file.copy_upgraded_binary]
+  depends_on = [ enos_file.copy_upgraded_binary ]
 
 
   transport = {
@@ -66,7 +50,7 @@ resource "enos_remote_exec" "restart_linux_services" {
 
 resource "enos_remote_exec" "restart_windows_services" {
   count      = var.platform == "windows" ? 1 : 0
-  depends_on = [enos_local_exec.take_server_snapshot, enos_file.copy_upgraded_binary]
+  depends_on = [ enos_file.copy_upgraded_binary ]
 
   transport = {
     ssh = {
@@ -78,21 +62,5 @@ resource "enos_remote_exec" "restart_windows_services" {
 
   inline = [
     "powershell Restart-Service Nomad"
-  ]
-}
-
-resource "enos_local_exec" "restore_server_snapshot" {
-  depends_on = [enos_remote_exec.restart_windows_services, enos_remote_exec.restart_linux_services]
-
-  environment = {
-    NOMAD_ADDR        = var.nomad_addr
-    NOMAD_CACERT      = var.ca_file
-    NOMAD_CLIENT_CERT = var.cert_file
-    NOMAD_CLIENT_KEY  = var.key_file
-    NOMAD_TOKEN       = local.clean_token
-  }
-
-  inline = [
-    "nomad operator snapshot restore ${local.snap_file}",
   ]
 }
