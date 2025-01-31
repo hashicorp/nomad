d29e3d9f929f47a09017345884c257b86f907311
Juanadelacuesta <8647634+Juanadelacuesta@users.noreply.github.com>
2025-01-31 16:01:37 +0100

    func: fix the last_log_index check and add a versions check

--- enos/modules/test_cluster_health/scripts/servers.sh
+++ enos/modules/test_cluster_health/scripts/servers.sh
@@ -23,8 +23,18 @@ if [ "$servers_length" -ne "$SERVER_COUNT" ]; then
     error_exit "Unexpected number of servers are alive: $servers_length\n$(echo $servers | jq '.[] | select(.Status != "alive") | .Name')"
 fi
 
-if [ $(echo "$running_servers" | jq -r "map(.last_log_index ) | unique | length == 1") != "true" ]; then
-    error_exit "Servers not up to date"
-fi
+# Quality: nomad_agent_info_self: A GET call to /v1/agent/self against every server returns the same last_log_index for all of them"
+
+last_index=""
+
+for ip in $SERVERS; do
+    
+    last_log_index=$(nomad agent-info -address "https://$ip:4646" -json | jq -r '.stats.raft.last_log_index')
+    if [ -n "$last_index" ] && [ "$last_log_index" -ne "$last_index" ]; then
+        error_exit "Servers not on the same index. $ip on  index: $last_index,  previous read index: $last_log_index"
+    fi
+
+    last_index="$last_log_index"
+done
 
 echo "All SERVERS are alive and up to date."
