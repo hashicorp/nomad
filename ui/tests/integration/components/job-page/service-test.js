b2606ec4bc6fb11dbe4814dbefc844091f916f41
Phil Renaud <phil@riotindustries.com>
2025-02-05 16:26:12 -0500

    Re-fix my test now that fast-fail is off

--- ui/tests/integration/components/job-page/service-test.js
+++ ui/tests/integration/components/job-page/service-test.js
@@ -114,6 +114,8 @@ module('Integration | Component | job-page/service', function (hooks) {
 
     const mirageJob = makeMirageJob(this.server, {
       status: 'dead',
+      withPreviousStableVersion: true,
+      stopped: true,
     });
     await this.store.findAll('job');
 
