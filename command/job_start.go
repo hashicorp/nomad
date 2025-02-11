9420b85830058df844243cb93f2f8c6bd2e0d84d
Michael Smithhisler <michael.smithhisler@hashicorp.com>
2025-02-11 15:49:18 -0500

    refactor to start only one job and add docs

--- command/job_start.go
+++ command/job_start.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"sync"
+	"time"
 
 	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/nomad/api/contexts"
@@ -16,8 +16,6 @@ import (
 
 type JobStartCommand struct {
 	Meta
-	versionCh chan uint64
-	Eval      string
 }
 
 func (c *JobStartCommand) Help() string {
@@ -26,11 +24,10 @@ Usage: nomad job start [options] <job>
 Alias: nomad start
 
 
- Start one or multiple stopped jobs. This command is used to start a previously stopped job's
- most recent version. Upon successful start, an interactive
- monitor session will start to display log lines as the job starts its
- allocations based on its most recent version. It is safe to exit the monitor
- early using ctrl+c.
+ Starts a stopped job. The job must be currently registered with Nomad. Upon
+ successful start, an interactive monitor session will start to display log
+ lines as the job starts its allocations based on its most recent version.
+ It is safe to exit the monitor early using ctrl+c.
 
 
  When ACLs are enabled, this command requires a token with the 'submit-job'
@@ -114,18 +111,15 @@ func (c *JobStartCommand) Run(args []string) int {
 		return 1
 	}
 
-	// Check that we got at least one job
+	// Check that we got exactly one arguement
 	args = flags.Args()
-	if len(args) < 1 {
-		c.Ui.Error("This command takes at least one argument: <job>")
+	if len(args) != 1 {
+		c.Ui.Error("This command takes one argument: <job>")
 		c.Ui.Error(commandErrorText(c))
 		return 1
 	}
 
-	var jobIDs []string
-	for _, jobID := range flags.Args() {
-		jobIDs = append(jobIDs, strings.TrimSpace(jobID))
-	}
+	jobIDPrefix := strings.TrimSpace(args[0])
 
 	// Get the HTTP client
 	client, err := c.Meta.Client()
@@ -141,121 +135,98 @@ func (c *JobStartCommand) Run(args []string) int {
 		vaultToken = os.Getenv("VAULT_TOKEN")
 	}
 
-	statusCh := make(chan int, len(jobIDs))
-	c.versionCh = make(chan uint64, len(args))
-	var wg sync.WaitGroup
-
-	for _, jobIDPrefix := range jobIDs {
-
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-
-			// Truncate the id unless full length is requested
-			length := shortId
-			if verbose {
-				length = fullId
-			}
-
-			job, err := c.JobByPrefix(client, jobIDPrefix, nil)
-			if err != nil {
-				c.Ui.Error(err.Error())
-				statusCh <- 1
-				return
-			}
-
-			if *job.Status != "dead" {
-				c.Ui.Info(fmt.Sprintf("Job  %v has not been stopped and has the following status: %v", *job.Name, *job.Status))
-				statusCh <- 0
-				return
-
-			}
-			chosenVersion, err := c.GetSelectedVersion(client, job, *job.Namespace)
-			if err != nil {
-				c.Ui.Error(err.Error())
-				statusCh <- 1
-				return
-			}
-
-			c.versionCh <- chosenVersion
-
-			// Revert to most recent running version!
-			m := &api.WriteOptions{Namespace: *job.Namespace}
-
-			resp, _, err := client.Jobs().Revert(*job.ID, chosenVersion, nil, m, consulToken, vaultToken)
-			if err != nil {
-				c.Ui.Error(fmt.Sprintf("Error retrieving job version %v for job %s: %s,", chosenVersion, *job.ID, err))
-				statusCh <- 1
-				return
-			}
-
-			// Nothing to do: periodic or dispatch job
-			evalCreated := resp.EvalID != ""
-
-			if !evalCreated {
-				statusCh <- 0
-				return
-			}
-			c.Eval = resp.EvalID
-
-			if detach {
-				c.Ui.Output("Evaluation ID: " + resp.EvalID)
-				statusCh <- 0
-				return
-			}
-
-			mon := newMonitor(c.Ui, client, length)
-			statusCh <- mon.monitor(resp.EvalID)
-
-		}()
+	// Truncate the id unless full length is requested
+	length := shortId
+	if verbose {
+		length = fullId
 	}
-	// users will still see
-	// errors if any while we
-	// wait for the goroutines
-	// to finish processing
-	wg.Wait()
 
-	// close the channel to ensure
-	// the range statement below
-	// doesn't go on indefinitely
-	close(c.versionCh)
-	close(statusCh)
+	job, err := c.JobByPrefix(client, jobIDPrefix, nil)
+	if err != nil {
+		fmt.Println("error getting job!")
+		c.Ui.Error(err.Error())
+		return 1
+	}
 
-	// return a non-zero exit code
-	// if even a single job start fails
-	for status := range statusCh {
-		if status != 0 {
-			return status
+	var jobID, jobName, namespace string
+
+	if job.ID != nil {
+		jobID = *job.ID
+	}
+	if job.Name != nil {
+		jobName = *job.Name
+	}
+	if job.Namespace != nil {
+		namespace = *job.Namespace
+	}
+
+	if job.Stop != nil && !*job.Stop {
+		c.Ui.Error(fmt.Sprintf("Job '%v' has not been stopped", jobName))
+		return 1
+	}
+
+	chosenVersion, err := c.GetSelectedVersion(client, jobID, namespace)
+	if err != nil {
+		c.Ui.Error(err.Error())
+		return 1
+	}
+
+	// Revert to most recent non-stopped version!
+	m := &api.WriteOptions{Namespace: namespace}
+	resp, _, err := client.Jobs().Revert(jobID, chosenVersion, nil, m, consulToken, vaultToken)
+	if err != nil {
+		c.Ui.Error(fmt.Sprintf("Error retrieving job version %v for job %s: %s,", chosenVersion, jobID, err))
+		return 1
+	}
+
+	// Check if the job is periodic or is a parameterized job
+	periodic := job.IsPeriodic()
+	paramjob := job.IsParameterized()
+	multiregion := job.IsMultiregion()
+	if detach || periodic || paramjob || multiregion {
+		c.Ui.Output("Job start successful")
+		if periodic && !paramjob {
+			loc, err := job.Periodic.GetLocation()
+			if err == nil {
+				now := time.Now().In(loc)
+				next, err := job.Periodic.Next(now)
+				if err != nil {
+					c.Ui.Error(fmt.Sprintf("Error determining next launch time: %v", err))
+				} else {
+					c.Ui.Output(fmt.Sprintf("Approximate next launch time: %s (%s from now)",
+						formatTime(next), formatTimeDifference(now, next, time.Second)))
+				}
+			}
+		} else if !paramjob {
+			c.Ui.Output("Evaluation ID: " + resp.EvalID)
 		}
+
+		return 0
 	}
-	return 0
+
+	mon := newMonitor(c.Ui, client, length)
+	return mon.monitor(resp.EvalID)
 }
-func (c *JobStartCommand) GetSelectedVersion(client *api.Client, job *api.Job, namespace string) (uint64, error) {
+
+func (c *JobStartCommand) GetSelectedVersion(client *api.Client, jobID string, namespace string) (uint64, error) {
 
 	// Get all versions associated to current job
-	q := &api.QueryOptions{Namespace: *job.Namespace}
+	q := &api.QueryOptions{Namespace: namespace}
 
-	versions, _, _, err := client.Jobs().Versions(*job.ID, true, q)
+	// Versions are returned in sorted order
+	versions, _, _, err := client.Jobs().Versions(jobID, true, q)
 	if err != nil {
-		c.Ui.Error(fmt.Sprintf("Error retrieving job versions: %s", err))
-		return 0, fmt.Errorf("error retrieving job versions: %s", err)
+		return 0, fmt.Errorf("Error retrieving job versions: %s", err)
 	}
 
 	// Find the most recent version for this job that has not been stopped
-	var chosenVersion uint64
-	versionAvailable := false
 	for _, version := range versions {
-		if !*version.Stop && *version.Status == "running" {
-			chosenVersion = *version.Version
-			versionAvailable = true
-			break
+		if version.Stop == nil {
+			continue
+		}
+		if !*version.Stop {
+			return *version.Version, nil
 		}
 	}
-	if !versionAvailable {
-		c.Ui.Error(fmt.Sprintf("No previous available versions of job %v", *job.Name))
-		return 0, fmt.Errorf("no previous available versions of job %v", *job.Name)
-	}
-
-	return chosenVersion, nil
-
+	return 0, fmt.Errorf("No valid job version available to start")
 }
