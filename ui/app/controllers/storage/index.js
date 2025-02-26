5ef2c2847bcff57876edc6d8a48f958e53dd2b15
Phil Renaud <phil@riotindustries.com>
2025-02-26 01:19:36 -0500

    Experimental fetch of alloc data dirs

--- ui/app/controllers/storage/index.js
+++ ui/app/controllers/storage/index.js
@@ -5,6 +5,7 @@
 
 import { inject as service } from '@ember/service';
 import { action, computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import Controller from '@ember/controller';
 
 export default class IndexController extends Controller {
@@ -84,5 +85,53 @@ export default class IndexController extends Controller {
   }
   @action openDHV(dhv) {
     this.router.transitionTo('storage.dhv', dhv.name);
+  }
+
+  @tracked sortedEphemeralDisks = [];
+  @tracked sortedStaticHostVolumes = [];
+
+  @action async scanForEphemeralDisks() {
+    console.log('scanForEphemeralDisks');
+    // Check store for all jobs
+    const allAllocs = await this.store.findAll('allocation');
+    console.log('allAllocs', allAllocs);
+    const allocDataDirs = await Promise.all(
+      allAllocs.map(async (alloc) => {
+        // TODO: /logs is demo-only, /data is actual.
+        // return alloc.ls('alloc/logs');
+        return {
+          // job: alloc.get('jobID'),
+          // allocID: alloc.id,
+          alloc,
+          files: await alloc.ls('alloc/logs'),
+        };
+      })
+    );
+    console.log('allocDataDirs', allocDataDirs);
+    this.sortedEphemeralDisks = allocDataDirs.map((dir, index) => {
+      const files = dir.files;
+      console.log('files', files);
+      const totalFileSize = files.reduce((acc, file) => {
+        return acc + file.Size;
+      }, 0);
+      console.log('indexed', index, allAllocs.objectAt(index));
+      return {
+        // job: allAllocs.objectAt(index).get('job.name'),
+        // job: dir.job,
+        name: dir.alloc.name,
+        id: dir.alloc.id,
+        size: totalFileSize,
+      };
+    });
+    // console.log('sortedEphemeralDisks', sortedEphemeralDisks);
+    // const allocsWithEphemeralDisks = allAllocs.filter((alloc) => {
+    //   return alloc.job.ephemeralDisk;
+    // });
+    // console.log('allAllocs', allAllocs);
+    // console.log('allocsWithEphemeralDisks', allocsWithEphemeralDisks);
+  }
+
+  @action scanForStaticHostVolumes() {
+    console.log('scanForStaticHostVolumes');
   }
 }
