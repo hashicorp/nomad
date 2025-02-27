b1faea5f4b09cb3221d5a8fc50967b75fc78853d
Michael Smithhisler <michael.smithhisler@hashicorp.com>
2025-02-27 17:06:58 -0500

    refactors workload identity to separate resource

--- e2e/terraform/provision-infra/scripts/bootstrap-consul.sh
+++ e2e/terraform/provision-infra/scripts/bootstrap-consul.sh
@@ -31,40 +31,4 @@ echo "writing Consul cluster policy and token"
 consul acl policy create -name consul-agents -rules @${DIR}/consul-agents-policy.hcl
 consul acl token create -policy-name=consul-agents -secret "$CONSUL_AGENT_TOKEN"
 
-# The following ACL's are used so Nomad services and tasks can register
-# via Workload Identity
-echo "writing ACLs for Nomad Workload Identity integration..."
-
-CONF=$(sed 's\NOMAD_SERVER_ADDR\'${NOMAD_SERVER_ADDR}'\g' ${DIR}/consul-workload-identity/auth-method.json) 
-
-echo "writing Consul auth-method"
-consul acl auth-method create \
-  -name 'nomad-workloads' \
-  -type 'jwt' \
-  -description 'Login method for Nomad workloads using workload identities' \
-  -token-locality 'local' \
-  -config "${CONF}" \
-  -namespace-rule-selector '"consul_namespace" in value' \
-  -namespace-rule-bind-namespace '${value.consul_namespace}'
-
-echo "writing binding-rule for Nomad services"
-consul acl binding-rule create \
-    -method 'nomad-workloads' \
-    -bind-type 'service' \
-    -bind-name '${value.nomad_service}' \
-    -selector '"nomad_service" in value'
-
-echo "writing binding-rule for Nomad tasks"
-consul acl binding-rule create \
-  -method 'nomad-workloads' \
-  -bind-type 'role' \
-  -bind-name 'nomad-tasks-${value.nomad_namespace}' \
-  -selector '"nomad_service" not in value'
-
-echo "writing policy for Nomad tasks"
-consul acl policy create -name policy-nomad-tasks -rules @${DIR}/consul-workload-identity/nomad-task-policy.hcl
-
-echo "creating role for Nomad tasks using previously created policy"
-consul acl role create -name nomad-default-tasks -policy-name policy-nomad-tasks
-
 echo "Consul successfully bootstraped!"
