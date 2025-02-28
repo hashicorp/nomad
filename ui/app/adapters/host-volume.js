e93003cd77ac347388b03798a9d3faf87d1f2045
Phil Renaud <phil@riotindustries.com>
2025-02-28 11:31:14 -0500

    Bones of a dynamic host volume individual page

---
+++ ui/app/adapters/host-volume.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import WatchableNamespaceIDs from './watchable-namespace-ids';
+import classic from 'ember-classic-decorator';
+
+@classic
+export default class HostVolumeAdapter extends WatchableNamespaceIDs {
+  pathForType = () => 'volume/host';
+
+  urlForFindRecord(fullID) {
+    const [id, namespace] = JSON.parse(fullID);
+
+    let url = `/v1/${this.pathForType()}/${id}`;
+
+    if (namespace && namespace !== 'default') {
+      url += `?namespace=${namespace}`;
+    }
+
+    return url;
+  }
+  // queryParamsToAttrs = {
+  //   type: 'type',
+  //   plugin_id: 'plugin.id',
+  // };
+}
