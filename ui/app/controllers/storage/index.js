f629f5221323f2568c94e20bd31dd7ed1298d94d
Phil Renaud <phil@riotindustries.com>
2025-03-04 17:07:59 -0500

    Pagination and searching and query params oh my

--- ui/app/controllers/storage/index.js
+++ ui/app/controllers/storage/index.js
@@ -7,21 +7,37 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import Controller from '@ember/controller';
-import { restartableTask, timeout } from 'ember-concurrency';
+import { restartableTask } from 'ember-concurrency';
+import { reduceBytes } from 'nomad-ui/utils/units';
+
 export default class IndexController extends Controller {
   @service router;
+  @service userSettings;
+  @service system;
 
-  queryParams = ['qpNamespace'];
+  queryParams = [
+    'qpNamespace',
+    'dhvPage',
+    'csiPage',
+    'shvPage',
+    'edPage',
+    'dhvFilter',
+    'csiFilter',
+    'shvFilter',
+    'edFilter',
+    'dhvSortProperty',
+    'csiSortProperty',
+    'shvSortProperty',
+    'edSortProperty',
+    'dhvSortDescending',
+    'csiSortDescending',
+    'shvSortDescending',
+    'edSortDescending',
+  ];
 
   qpNamespace = 'default';
 
-  get sortedDynamicHostVolumes() {
-    return this.model.dynamicHostVolumes.sortBy('name');
-  }
-
-  get sortedCSIVolumes() {
-    return this.model.csiVolumes.sortBy('name');
-  }
+  pageSizes = [10, 25, 50];
 
   get dhvColumns() {
     return [
@@ -30,11 +46,15 @@ export default class IndexController extends Controller {
         label: 'Name',
         isSortable: true,
       },
-      {
-        key: 'namespace',
-        label: 'Namespace',
-        isSortable: true,
-      },
+      ...(this.system.shouldShowNamespaces
+        ? [
+            {
+              key: 'namespace',
+              label: 'Namespace',
+              isSortable: true,
+            },
+          ]
+        : []),
       {
         key: 'pluginID',
         label: 'Plugin ID',
@@ -54,17 +74,21 @@ export default class IndexController extends Controller {
   }
 
   get csiColumns() {
-    return [
+    let cols = [
       {
         key: 'name',
         label: 'Name',
         isSortable: true,
       },
-      {
-        key: 'namespace.name',
-        label: 'Namespace',
-        isSortable: true,
-      },
+      ...(this.system.shouldShowNamespaces
+        ? [
+            {
+              key: 'namespace',
+              label: 'Namespace',
+              isSortable: true,
+            },
+          ]
+        : []),
       {
         key: 'schedulable',
         label: 'Volume Health',
@@ -85,51 +109,204 @@ export default class IndexController extends Controller {
       {
         key: 'allocationCount',
         label: '# Allocs',
+        isSortable: true,
       },
-    ];
+    ].filter(Boolean);
+    return cols;
   }
+
+  // For all volume types:
+  // Filter, then Sort, then Paginate
+  // all handled client-side
+
+  get filteredCSIVolumes() {
+    if (!this.csiFilter) {
+      return this.model.csiVolumes;
+    } else {
+      return this.model.csiVolumes.filter((volume) => {
+        return volume.name.toLowerCase().includes(this.csiFilter.toLowerCase());
+      });
+    }
+  }
+
+  get sortedCSIVolumes() {
+    let sorted = this.filteredCSIVolumes.sortBy(this.csiSortProperty);
+    if (this.csiSortDescending) {
+      sorted.reverse();
+    }
+    return sorted;
+  }
+
+  get paginatedCSIVolumes() {
+    return this.sortedCSIVolumes.slice(
+      (this.csiPage - 1) * this.userSettings.pageSize,
+      this.csiPage * this.userSettings.pageSize
+    );
+  }
+
+  get filteredDynamicHostVolumes() {
+    if (!this.dhvFilter) {
+      return this.model.dynamicHostVolumes;
+    } else {
+      return this.model.dynamicHostVolumes.filter((volume) => {
+        return volume.name.toLowerCase().includes(this.dhvFilter.toLowerCase());
+      });
+    }
+  }
+
+  get sortedDynamicHostVolumes() {
+    let sorted = this.filteredDynamicHostVolumes.sortBy(this.dhvSortProperty);
+    if (this.dhvSortDescending) {
+      sorted.reverse();
+    }
+    return sorted;
+  }
+
+  get paginatedDynamicHostVolumes() {
+    return this.sortedDynamicHostVolumes.slice(
+      (this.dhvPage - 1) * this.userSettings.pageSize,
+      this.dhvPage * this.userSettings.pageSize
+    );
+  }
+
+  get filteredEphemeralDisks() {
+    if (!this.edFilter) {
+      return this.fetchedEphemeralDisks;
+    } else {
+      return this.fetchedEphemeralDisks.filter((disk) => {
+        return disk.name.toLowerCase().includes(this.edFilter.toLowerCase());
+      });
+    }
+  }
+
+  get sortedEphemeralDisks() {
+    let sorted = this.filteredEphemeralDisks.sortBy(this.edSortProperty);
+    if (this.edSortDescending) {
+      sorted.reverse();
+    }
+    return sorted;
+  }
+
+  get paginatedEphemeralDisks() {
+    return this.sortedEphemeralDisks.slice(
+      (this.edPage - 1) * this.userSettings.pageSize,
+      this.edPage * this.userSettings.pageSize
+    );
+  }
+
+  get filteredStaticHostVolumes() {
+    if (!this.shvFilter) {
+      return this.fetchedStaticHostVolumes;
+    } else {
+      return this.fetchedStaticHostVolumes.filter((volume) => {
+        return volume.name.toLowerCase().includes(this.shvFilter.toLowerCase());
+      });
+    }
+  }
+
+  get sortedStaticHostVolumes() {
+    let sorted = this.filteredStaticHostVolumes.sortBy(this.shvSortProperty);
+    if (this.shvSortDescending) {
+      sorted.reverse();
+    }
+    return sorted;
+  }
+
+  get paginatedStaticHostVolumes() {
+    return this.sortedStaticHostVolumes.slice(
+      (this.shvPage - 1) * this.userSettings.pageSize,
+      this.shvPage * this.userSettings.pageSize
+    );
+  }
+
+  @tracked csiSortProperty = 'name';
+  @tracked csiSortDescending = false;
+  @tracked csiPage = 1;
+
+  @tracked dhvSortProperty = 'name';
+  @tracked dhvSortDescending = false;
+  @tracked dhvPage = 1;
+
+  @tracked shvSortProperty = 'name';
+  @tracked shvSortDescending = false;
+  @tracked shvPage = 1;
+
+  @tracked edSortProperty = 'sizeBytes';
+  @tracked edSortDescending = true;
+  @tracked edPage = 1;
+
+  @tracked csiFilter = '';
+  @tracked dhvFilter = '';
+  @tracked shvFilter = '';
+  @tracked edFilter = '';
+
+  @action handlePageChange(type, page) {
+    if (type === 'csi') {
+      this.csiPage = page;
+    } else if (type === 'dhv') {
+      this.dhvPage = page;
+    } else if (type === 'shv') {
+      this.shvPage = page;
+    } else if (type === 'ed') {
+      this.edPage = page;
+    }
+  }
+
+  @action handleSort(type, sortBy, sortOrder) {
+    this[`${type}SortProperty`] = sortBy;
+    this[`${type}SortDescending`] = sortOrder === 'desc';
+    // this.handlePageChange(type, 1);
+  }
+
   @action openDHV(dhv) {
     this.router.transitionTo('storage.dhv', dhv.name);
   }
 
-  @tracked sortedEphemeralDisks = [];
-  @tracked sortedStaticHostVolumes = [];
+  @tracked fetchedEphemeralDisks = [];
+  @tracked fetchedStaticHostVolumes = [];
 
   @restartableTask *scanForEphemeralDisks() {
+    // Reset filters and pagination
+    this.edPage = 1;
+    this.edFilter = '';
+    this.edSortProperty = 'sizeBytes';
+    this.edSortDescending = true;
+
     const allAllocs = yield this.store.findAll('allocation', { reload: true });
     const allocDataDirs = yield Promise.all(
       allAllocs.map(async (alloc) => {
         return {
           alloc,
-          files: await alloc.ls('alloc/logs'),
+          files: await alloc.ls('alloc/data'),
         };
       })
     );
-    // TODO: demonstrative timeout
-    yield timeout(2000);
 
-    yield (this.sortedEphemeralDisks = allocDataDirs.map((dir) => {
+    yield (this.fetchedEphemeralDisks = allocDataDirs.map((dir) => {
       const files = dir.files;
       const totalFileSize = files.reduce((acc, file) => {
         return acc + file.Size;
       }, 0);
+      const [size, unit] = reduceBytes(totalFileSize);
       return {
         name: dir.alloc.name,
         id: dir.alloc.id,
-        size: totalFileSize,
+        sizeBytes: totalFileSize,
+        size: `${+size.toFixed(2)} ${unit}`,
       };
     }));
-    // TODO: demonstrative timeout
-    yield timeout(2000);
   }
 
   @restartableTask *scanForStaticHostVolumes() {
+    // Reset filters and pagination
+    this.shvPage = 1;
+    this.shvFilter = '';
+    this.shvSortProperty = 'name';
+    this.shvSortDescending = false;
+
     const allNodes = yield this.store.findAll('node', { reload: true });
 
-    // TODO: demonstrative timeout
-    yield timeout(2000);
-
-    this.sortedStaticHostVolumes = allNodes
+    yield (this.fetchedStaticHostVolumes = allNodes
       .map((node) => {
         const hostVolumes = node.hostVolumes.filter((volume) => {
           return !volume.volumeID;
@@ -143,7 +320,7 @@ export default class IndexController extends Controller {
           };
         });
       })
-      .flat();
+      .flat());
   }
 
   // keyboard-shortcut-friendly actions
