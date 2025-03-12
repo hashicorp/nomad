e1b9bd8ab0a1174952e601a873c557481bf784a1
hc-github-team-nomad-core <github-team-nomad-core@hashicorp.com>
2025-03-12 10:37:46 +0000

    Generate files for 1.10.0-beta.1 release

--- version/version.go
+++ version/version.go
@@ -19,12 +19,12 @@ var (
 	GitDescribe string
 
 	// The main version number that is being run at the moment.
-	Version = "1.9.8"
+	Version = "1.10.0"
 
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
 	// such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
+	VersionPrerelease = "beta.1"
 
 	// VersionMetadata is metadata further describing the build type.
 	VersionMetadata = ""
