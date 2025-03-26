a223b8716fbb56ce9f0da87620bcbd1e9112261f
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2025-03-26 21:57:09 +0100

    backport of commit 0e121b3c2947e46827ab2f2c5e27ae7183d27397 (#25530)
    Co-authored-by: Daniel Bennett <dbennett@hashicorp.com>

--- nomad/acl_endpoint.go
+++ nomad/acl_endpoint.go
@@ -1936,7 +1936,7 @@ func (a *ACL) UpsertAuthMethods(
 			return structs.NewErrRPCCodedf(400, "ACL auth method lookup failed: %v", err)
 		}
 		if lookupAuthMethod != nil {
-			reply.AuthMethods = append(reply.AuthMethods, lookupAuthMethod)
+			reply.AuthMethods = append(reply.AuthMethods, lookupAuthMethod.Sanitize())
 		}
 	}
 
