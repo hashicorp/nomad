38df7db32d3ce6f3c40d47bd8424889930e4b5de
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2025-04-03 18:22:34 +0200

    oopsie forgot to remove some copy pasta from Schmichael

--- command/acl_policy_self_test.go
+++ command/acl_policy_self_test.go
@@ -14,56 +14,6 @@ import (
 	"github.com/shoenig/test/must"
 )
 
-// ci.Parallel(t)
-
-// s1, _, rootToken, cleanupS1 := testACLServer(t, nil)
-// defer cleanupS1()
-// testutil.WaitForLeader(t, s1.RPC)
-
-// c1, cleanup := TestClient(t, func(c *config.Config) {
-// 	c.RPCHandler = s1
-// 	c.ACLEnabled = true
-// })
-// defer cleanup()
-
-// // Create a minimal job
-// job := mock.MinJob()
-
-// // Add a job policy
-// polArgs := structs.ACLPolicyUpsertRequest{
-// 	Policies: []*structs.ACLPolicy{
-// 		{
-// 			Name:        "nw",
-// 			Description: "test job can write to nodes",
-// 			Rules:       `node { policy = "write" }`,
-// 			JobACL: &structs.JobACL{
-// 				Namespace: job.Namespace,
-// 				JobID:     job.ID,
-// 			},
-// 		},
-// 	},
-// 	WriteRequest: structs.WriteRequest{
-// 		Region:    job.Region,
-// 		AuthToken: rootToken.SecretID,
-// 		Namespace: job.Namespace,
-// 	},
-// }
-// polReply := structs.GenericResponse{}
-// must.NoError(t, s1.RPC("ACL.UpsertPolicies", &polArgs, &polReply))
-// must.NonZero(t, polReply.WriteMeta.Index)
-
-// allocs := testutil.WaitForRunningWithToken(t, s1.RPC, job, rootToken.SecretID)
-// must.Len(t, 1, allocs)
-
-// alloc, err := s1.State().AllocByID(nil, allocs[0].ID)
-// must.NoError(t, err)
-// must.MapContainsKey(t, alloc.SignedIdentities, "t")
-// wid := alloc.SignedIdentities["t"]
-
-// aclObj, err := c1.ResolveToken(wid)
-// must.NoError(t, err)
-// must.True(t, aclObj.AllowNodeWrite(), must.Sprintf("expected workload id to allow node write"))
-
 func TestACLPolicySelfCommand_ViaEnvVar(t *testing.T) {
 	config := func(c *agent.Config) {
 		c.ACL.Enabled = true
