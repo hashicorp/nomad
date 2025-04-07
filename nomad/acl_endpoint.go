51e8a2ad3246f96709e46e7cd4e1079a1806ba76
Daniel Bennett <dbennett@hashicorp.com>
2025-04-07 17:36:09 +0000

    backport of commit 5c8e436de98b18bd3b7d0da05d8fa83c1529b4b2

--- nomad/acl_endpoint.go
+++ nomad/acl_endpoint.go
@@ -20,8 +20,6 @@ import (
 	"github.com/hashicorp/go-memdb"
 	metrics "github.com/hashicorp/go-metrics/compat"
 	"github.com/hashicorp/go-set/v3"
-	"github.com/hashicorp/nomad/helper/pointer"
-
 	policy "github.com/hashicorp/nomad/acl"
 	"github.com/hashicorp/nomad/helper"
 	"github.com/hashicorp/nomad/helper/uuid"
@@ -1912,12 +1910,6 @@ func (a *ACL) UpsertAuthMethods(
 			}
 		}
 
-		// if this is a new auth method, and PKCE is not explicitly disabled
-		// (by setting Enable=false), then enable it. existing auth methods
-		// need it to be enabled explicitly (Enable=True).
-		if existingMethod == nil && authMethod.Config.OIDCEnablePKCE == nil {
-			authMethod.Config.OIDCEnablePKCE = pointer.Of(true)
-		}
 		// if there is a client assertion, ensure it is valid.
 		if authMethod.Config.OIDCClientAssertion.IsSet() {
 			_, err := a.oidcClientAssertion(authMethod.Config)
@@ -3071,7 +3063,7 @@ func (a *ACL) oidcRequest(nonce, redirect string, config *structs.ACLAuthMethodC
 		opts = append(opts, capOIDC.WithAudiences(config.BoundAudiences...))
 	}
 
-	if config.OIDCEnablePKCE != nil && *config.OIDCEnablePKCE {
+	if config.OIDCEnablePKCE {
 		verifier, err := capOIDC.NewCodeVerifier()
 		if err != nil {
 			return nil, fmt.Errorf("failed to make pkce verifier: %w", err)
