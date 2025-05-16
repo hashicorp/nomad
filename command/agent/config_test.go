891c5faa51c9d8d16cb44a25b5acba27d84a9805
Tehut Geathun <tehut.m.g@gmail.com>
2025-05-15 17:22:47 -0700

    add node_max_alloc info to nomad node status -verbose and -self

--- command/agent/config_test.go
+++ command/agent/config_test.go
@@ -1847,13 +1847,24 @@ func Test_mergeKEKProviderConfigs(t *testing.T) {
 
 func TestConfig_LoadClientNodeMaxAllocs(t *testing.T) {
 	ci.Parallel(t)
-
-	t.Run("minimal client expect defaults", func(t *testing.T) {
-		defaultConfig := DefaultConfig()
-		agentConfig, err := LoadConfig("test-resources/maxalloc.hcl")
-		must.NoError(t, err)
-		agentConfig = defaultConfig.Merge(agentConfig)
-		must.Eq(t, 5, agentConfig.Client.NodeMaxAllocs)
-	})
+	testCases := []struct {
+		fileName string
+	}{
+		{
+			fileName: "test-resources/client_with_maxallocs.hcl",
+		},
+		{
+			fileName: "test-resources/client_with_maxallocs.json",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run("minimal client expect defaults", func(t *testing.T) {
+			defaultConfig := DefaultConfig()
+			agentConfig, err := LoadConfig(tc.fileName)
+			must.NoError(t, err)
+			agentConfig = defaultConfig.Merge(agentConfig)
+			must.Eq(t, 5, agentConfig.Client.NodeMaxAllocs)
+		})
+	}
 
 }
