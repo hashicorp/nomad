800f393baf80b50b2b8ca0e5e1a03cc237d94bca
dduzgun-security <deniz.duzgun@hashicorp.com>
2025-06-05 13:36:58 -0400

    sec:add sprig template functions in denylists

--- client/config/config.go
+++ client/config/config.go
@@ -74,7 +74,7 @@ var (
 
 	DefaultTemplateMaxStale = 87600 * time.Hour
 
-	DefaultTemplateFunctionDenylist = []string{"executeTemplate", "plugin", "writeToFile"}
+	DefaultTemplateFunctionDenylist = []string{"executeTemplate", "plugin", "writeToFile", "sprig_*"}
 )
 
 // RPCHandler can be provided to the Client if there is a local server
