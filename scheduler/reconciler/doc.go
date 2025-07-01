36e7148247cd7b59d4a7ba6f63ef7ad29b883e71
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2025-07-01 16:28:33 +0200

    scheduler: doc.go files for new packages (#26177)

---
+++ scheduler/reconciler/doc.go
@@ -0,0 +1,5 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+// Package reconciler contains the reconciliation logic for all scheduler types.
+package reconciler
---
+++ scheduler/structs/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+// Package structs contains definitions of shared objects, constants, and
+// top-level interfaces for the scheduler.
+package structs
