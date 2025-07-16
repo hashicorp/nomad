953a1491808232b0c091a96ebf28600c509ac625
James Rasell <jrasell@users.noreply.github.com>
2025-07-16 15:56:00 +0200

    client: Allow operators to force a client to renew its identity. (#26277)
    The Nomad client will have its identity renewed according to the
    TTL which defaults to 24h. In certain situations such as root
    keyring rotation, operators may want to force clients to renew
    their identities before the TTL threshold is met. This change
    introduces a client HTTP and RPC endpoint which will instruct the
    node to request a new identity at its next heartbeat. This can be
    used via the API or a new command.
    
    While this is a manual intervention step on top of the any keyring
    rotation, it dramatically reduces the initial feature complexity
    as it provides an asynchronous and efficient method of renewal that
    utilises existing functionality.

--- nomad/server.go
+++ nomad/server.go
@@ -1282,6 +1282,7 @@ func (s *Server) setupRpcServer(server *rpc.Server, ctx *RPCContext) {
 	// These endpoints are client RPCs and don't include a connection context
 	_ = server.Register(NewClientStatsEndpoint(s))
 	_ = server.Register(newNodeMetaEndpoint(s))
+	_ = server.Register(newNodeIdentityEndpoint(s))
 
 	// These endpoints have their streaming component registered in
 	// setupStreamingEndpoints, but their non-streaming RPCs are registered
