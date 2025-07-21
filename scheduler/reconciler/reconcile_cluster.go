ca1193d9c6277c3e49c0b9d4229aff76c9139111
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2025-07-21 08:58:51 +0200

    comment update

--- scheduler/reconciler/reconcile_cluster.go
+++ scheduler/reconciler/reconcile_cluster.go
@@ -482,8 +482,8 @@ func (a *AllocReconciler) computeGroup(group string, all allocSet) (*ReconcileRe
 		reconnect, stopAllocSet, stopAllocResult := a.reconcileReconnecting(reconnecting, all, tg)
 		result.Stop = append(result.Stop, stopAllocResult...)
 
-		// Stop the reconciled allocations and remove them from the other sets
-		// since they have been already handled.
+		// Stop the reconciled allocations and remove them from untainted, migrate, lost
+		// and disconnecting sets, since they have been already handled.
 		result.DesiredTGUpdates[group].Stop += uint64(len(stopAllocSet))
 
 		untainted = untainted.difference(stopAllocSet)
