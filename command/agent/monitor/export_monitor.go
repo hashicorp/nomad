ac0f87a1a80c80c0b4c72845bde1470dcc5a1365
Tehut Getahun <tehut.m.g@gmail.com>
2025-07-28 13:29:54 -0700

    exportMonitor tidy

--- command/agent/monitor/export_monitor.go
+++ command/agent/monitor/export_monitor.go
@@ -20,6 +20,8 @@ import (
 	"github.com/hashicorp/go-hclog"
 )
 
+const defaultBufSize = 512
+
 // ExportMonitor implements the Monitor interface for testing
 type ExportMonitor struct {
 	sync.Mutex
@@ -33,15 +35,7 @@ type ExportMonitor struct {
 	// ExportReader can read from the cli or the NomadFilePath
 	ExportReader ExportReader
 
-	// droppedCount is the current count of messages
-	// that were dropped from the logCh buffer.
-	// only access under lock
-	droppedCount int
-	bufSize      int
-	// droppedDuration is the amount of time we should
-	// wait to check for dropped messages. Defaults
-	// to 3 seconds
-	droppedDuration time.Duration
+	bufSize int
 }
 
 type MonitorExportOpts struct {
@@ -70,8 +64,6 @@ type MonitorExportOpts struct {
 
 	bufSize int
 }
-
-const defaultBufSize = 512
 
 type ExportReader struct {
 	io.Reader
