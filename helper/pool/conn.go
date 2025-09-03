0ac2de98e1fd784540e9fc38815eb2bea00761c8
James Rasell <jrasell@users.noreply.github.com>
2025-09-03 06:45:29 +0000

    backport of commit 270ab1011e8f5269235ce71964344c65f11d6923

--- helper/pool/conn.go
+++ helper/pool/conn.go
@@ -7,12 +7,12 @@ type RPCType byte
 
 const (
 	RpcNomad     RPCType = 0x01
-	RpcRaft              = 0x02
-	RpcMultiplex         = 0x03
-	RpcTLS               = 0x04
-	RpcStreaming         = 0x05
+	RpcRaft      RPCType = 0x02
+	RpcMultiplex RPCType = 0x03
+	RpcTLS       RPCType = 0x04
+	RpcStreaming RPCType = 0x05
 
 	// RpcMultiplexV2 allows a multiplexed connection to switch modes between
 	// RpcNomad and RpcStreaming per opened stream.
-	RpcMultiplexV2 = 0x06
+	RpcMultiplexV2 RPCType = 0x06
 )
