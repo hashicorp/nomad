f29da87e6113b33354cf6a4ea29550c4b2497840
Tim Gross <tgross@hashicorp.com>
2025-09-16 16:46:04 -0400

    [wip] what if we just kill it?

--- api/internal/testutil/server_windows.go
+++ api/internal/testutil/server_windows.go
@@ -6,7 +6,6 @@
 package testutil
 
 import (
-	"fmt"
 	"syscall"
 )
 
@@ -19,17 +18,20 @@ var (
 // API does not implement SIGINT even though it's supported on Windows via
 // CTRL_C_EVENT
 func (s *TestServer) gracefulStop() error {
-	pid := s.cmd.Process.Pid
-	result, _, err := proc.Call(syscall.CTRL_C_EVENT, uintptr(pid))
-	if result == 0 {
-		// note: err is always non-nil because Call always populates it from
-		// GetLastError and you need to check the result returned against the
-		// docs. from
-		// https://learn.microsoft.com/en-us/windows/console/generateconsolectrlevent
-		// "If the function fails, the return value is zero. To get extended
-		// error information, call GetLastError."
-		return fmt.Errorf("failed to send ctrl-C event to pid %d: %w", pid, err)
-	}
+
+	s.cmd.Process.Kill()
+
+	// pid := s.cmd.Process.Pid
+	// result, _, err := proc.Call(syscall.CTRL_C_EVENT, uintptr(pid))
+	// if result == 0 {
+	// 	// note: err is always non-nil because Call always populates it from
+	// 	// GetLastError and you need to check the result returned against the
+	// 	// docs. from
+	// 	// https://learn.microsoft.com/en-us/windows/console/generateconsolectrlevent
+	// 	// "If the function fails, the return value is zero. To get extended
+	// 	// error information, call GetLastError."
+	// 	return fmt.Errorf("failed to send ctrl-C event to pid %d: %w", pid, err)
+	// }
 
 	return nil
 }
