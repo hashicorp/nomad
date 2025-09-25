b85421ec02c160f8b868d643e398ce5dd9902289
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2025-09-25 10:02:43 -0400

    Backport of CSI: ensure only client-terminal allocs are treated as past claims into release/1.10.x (#26837)
    Co-authored-by: Tim Gross <tgross@hashicorp.com>

--- nomad/state/state_store.go
+++ nomad/state/state_store.go
@@ -2905,6 +2905,7 @@ func (s *StateStore) volSafeToForce(txn Txn, v *structs.CSIVolume) bool {
 	}
 
 	for _, alloc := range vol.ReadAllocs {
+		// note we check that both server and client agree on terminal status
 		if alloc != nil && !alloc.TerminalStatus() {
 			return false
 		}
@@ -3016,7 +3017,7 @@ func (s *StateStore) csiVolumeDenormalizeTxn(txn Txn, ws memdb.WatchSet, vol *st
 			}
 
 			currentAllocs[id] = a
-			if (a == nil || a.TerminalStatus()) && pastClaim == nil {
+			if (a == nil || a.ClientTerminalStatus()) && pastClaim == nil {
 				// the alloc is garbage collected but nothing has written a PastClaim,
 				// so create one now
 				pastClaim = &structs.CSIVolumeClaim{
