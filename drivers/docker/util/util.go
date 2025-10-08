2e89966d4a4b0f5f831eefa6e664d75e75bea89e
Allison Larson <allison.larson@hashicorp.com>
2025-10-08 10:03:53 -0700

    docker: calculate zero cpu percent correctly (#26902)
    * docker: calculate zero cpu percent correctly
    
    Corrects the guard clause in the docker driver `CalculateCPUPercent` to
    return zero if the two uint64 values can't be subtracted, by checking if
    the value being subtracted isn't larger than the other. This has to be
    done prior to subtracting the values, because subtracting two uint64
    values will never result in a value lower than zero, which is what the
    original clause checked, but instead wraps around on itself.
    
    These values are used in metrics, where it would appear that a docker
    container/alloc that was shutting down would use all of the cpu at the
    time of shutdown. This was manifesting in strange DAS autoscaling
    behavior during development while deploying new jobs.
    
    * add changelog

--- drivers/docker/util/util.go
+++ drivers/docker/util/util.go
@@ -4,11 +4,11 @@
 package util
 
 func CalculateCPUPercent(newSample, oldSample, newTotal, oldTotal uint64, cores int) float64 {
-	numerator := newSample - oldSample
-	denom := newTotal - oldTotal
-	if numerator <= 0 || denom <= 0 {
+	if (newSample <= oldSample) || (newTotal <= oldTotal) {
 		return 0.0
 	}
+	numerator := newSample - oldSample
+	denom := newTotal - oldTotal
 
 	return (float64(numerator) / float64(denom)) * float64(cores) * 100.0
 }
