56b9cbe57a786e8569904bb594fae22904156340
Chris Roberts <croberts@hashicorp.com>
2025-11-24 17:31:09 -0800

    ci: fix jira sync for closing issues
    Resolution must be explicitly set when transitioning an issue to
    closed. This adds ability to set resolution on transition and updates
    the sync workflow to resolve issue to done when closing.
    
    Also updated the jira-query helper to output the response body on
    error to make it easier to determine the cause.

--- .github/actions/jira/transition/jira-transition.bash
+++ .github/actions/jira/transition/jira-transition.bash
@@ -25,17 +25,51 @@ if [ -z "${transition_id}" ]; then
     exit 1
 fi
 
-template='
+# If a resolution is set, find it and generate the template
+# with it included
+if [ -n "${RESOLUTION}" ]; then
+    # Grab the resolution ID
+    result="$(jira-request "${JIRA_BASE_URL}/rest/api/3/resolution")" || exit
+    query="$(printf '.[] | select(.name == "%s").id' "${RESOLUTION}")"
+    resolution_id="$(jq -r "${query}" <<< "${result}")"
+
+    if [ -z "${resolution_id}" ]; then
+        error "Could not find matching resolution with name matching '%s'" "${RESOLUTION}"
+        exit 1
+    fi
+
+    template='
+{
+  transition: {
+    id: $transition
+  },
+  fields: {
+    resolution: {
+      id: $resolution
+    }
+  }
+}
+'
+    issue_transition="$(jq -n --arg transition "${transition_id}" --arg resolution "${resolution_id}" "${template}")" || exit
+else
+    # No resolution so the template only includes the transition
+    template='
 {
   transition: {
     id: $transition
   }
 }
 '
-issue_transition="$(jq -n --arg transition "${transition_id}" "${template}")" || exit
+    issue_transition="$(jq -n --arg transition "${transition_id}" "${template}")" || exit
+fi
 
 info "Transitioning JIRA issue '%s' to %s (ID: %s)" "${ISSUE}" \
     "${TRANSITION}" "${transition_id}"
+if [ -n "${resolution_id}" ]; then
+    info "Resolving JIRA issue '%s' as %s (ID: %s)" "${ISSUE}" \
+        "${RESOLUTION}" "${resolution_id}"
+fi
+
 info "Transition payload:\n%s" "${issue_transition}"
 
 jira-request --request "POST" --data "${issue_transition}" \
