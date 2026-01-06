3a5a90b52fda147a747bcd33adc87a1b5147fdbd
James Rasell <jrasell@ibm.com>
2026-01-06 15:02:24 +0000

    core: Update 1.11 feature gate identifiers to GA version.

--- nomad/leader.go
+++ nomad/leader.go
@@ -93,18 +93,12 @@ var minVersionDynamicHostVolumes = version.Must(version.NewVersion("1.10.0"))
 // minVersionNodeIdentity is the Nomad version at which the node identity
 // feature was introduced. It forms the minimum version all local servers must
 // meet before the feature can be used.
-//
-// TODO(jrasell): Update this when we have a stable release with node identity
-// support.
-var minVersionNodeIdentity = version.Must(version.NewVersion("1.11.0-beta.1"))
+var minVersionNodeIdentity = version.Must(version.NewVersion("1.11.0"))
 
 // minVersionNodeIntro is the Nomad version at which the node introduction
 // feature was introduced. It forms the minimum version all local servers must
 // meet before the feature can be used.
-//
-// TODO(jrasell): Update this when we have a stable release with node
-// introduction support.
-var minVersionNodeIntro = version.Must(version.NewVersion("1.11.0-beta.1"))
+var minVersionNodeIntro = version.Must(version.NewVersion("1.11.0"))
 
 // monitorLeadership is used to monitor if we acquire or lose our role
 // as the leader in the Raft cluster. There is some work the leader is
