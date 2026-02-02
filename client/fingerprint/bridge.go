b3fa329a4be5e5dd290744c14764fdd1a69a0b29
Tim Gross <tgross@hashicorp.com>
2026-02-02 14:05:43 -0500

    fingerprint: make networking interface and bridge fingerprints reloadable

--- client/fingerprint/bridge.go
+++ client/fingerprint/bridge.go
@@ -14,3 +14,6 @@ type BridgeFingerprint struct {
 func NewBridgeFingerprint(logger log.Logger) Fingerprint {
 	return &BridgeFingerprint{logger: logger}
 }
+
+// Reload is a no-op but implements ReloadableFingerprint
+func (f *BridgeFingerprint) Reload() {}
