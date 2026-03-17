635408e734d3cd41314acef940293f11c3aea6e2
Alexey Kulakov <uni_que@me.com>
2026-03-17 04:01:11 -0700

    ember 6.10 LTS

--- ui/app/deprecation-workflow.ts
+++ ui/app/deprecation-workflow.ts
@@ -49,33 +49,27 @@ setupDeprecationWorkflow({
       handler: 'silence',
       matchId: 'ember-concurrency.deprecate-decorator-task',
     },
-    {
-      handler: 'silence',
-      matchId: 'ember-data:deprecate-store-find',
-    },
-    {
-      handler: 'silence',
-      matchId: 'ember-basic-dropdown.config-environment',
-    },
+    { handler: 'silence', matchId: 'ember-data:deprecate-store-find' },
+    { handler: 'silence', matchId: 'ember-basic-dropdown.config-environment' },
     {
       handler: 'silence',
       matchId: 'ember-data:deprecate-promise-many-array-behaviors',
     },
+    { handler: 'silence', matchId: 'deprecate-array-prototype-extensions' },
+    { handler: 'silence', matchId: 'ember-data:deprecate-model-reopenclass' },
+    { handler: 'silence', matchId: 'ember-data:deprecate-early-static' },
+    { handler: 'silence', matchId: 'ember-data:deprecate-array-like' },
+    { handler: 'silence', matchId: 'deprecate-import-application-from-ember' },
+    { handler: 'silence', matchId: 'deprecate-import-comparable-from-ember' },
+    { handler: 'silence', matchId: 'deprecate-import-libraries-from-ember' },
+    { handler: 'silence', matchId: 'deprecate-import-router-from-ember' },
     {
       handler: 'silence',
-      matchId: 'deprecate-array-prototype-extensions',
+      matchId: 'deprecate-import--set-classic-decorator-from-ember',
     },
-    {
-      handler: 'silence',
-      matchId: 'ember-data:deprecate-model-reopenclass',
-    },
-    {
-      handler: 'silence',
-      matchId: 'ember-data:deprecate-early-static',
-    },
-    {
-      handler: 'silence',
-      matchId: 'ember-data:deprecate-array-like',
-    },
+    { handler: 'silence', matchId: 'deprecate-import-meta-from-ember' },
+    { handler: 'silence', matchId: 'importing-inject-from-ember-service' },
+    { handler: 'silence', matchId: 'deprecate-import-testing-from-ember' },
+    { handler: 'silence', matchId: 'deprecate-import-env-from-ember' },
   ],
 });
