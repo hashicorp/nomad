089ffeb276bda2669cadd76e90e495ee71838d8c
navyadas-dev <navya.das@ibm.com>
2026-03-18 22:21:27 +0530

    NMD-531

--- ui/app/utils/json-to-hcl.js
+++ ui/app/utils/json-to-hcl.js
@@ -12,12 +12,30 @@
  * @returns {string} The HCL string representation of the JSON object.
  */
 export default function jsonToHcl(obj) {
+  if (!obj) return '';
+
   const hclLines = [];
 
   for (const key in obj) {
     const value = obj[key];
-    const hclValue = typeof value === 'string' ? value : JSON.stringify(value);
-    hclLines.push(`${key}=${hclValue}\n`);
+    let hclValue;
+
+    if (typeof value === 'string') {
+      //
+      if (
+        (value.startsWith('[') && value.endsWith(']')) ||
+        (value.startsWith('{') && value.endsWith('}'))
+      ) {
+        hclValue = value; // Keep it as a JSON string
+      } else {
+        // Escape double quotes and backslashes
+        hclValue = JSON.stringify(value);
+      }
+    } else {
+      hclValue = JSON.stringify(value);
+    }
+
+    hclLines.push(`${key}=${hclValue}`);
   }
 
   return hclLines.join('\n');
