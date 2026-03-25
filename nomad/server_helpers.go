19c97751ae4384b17c96c52ae6cc2d74c15e7321
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2026-03-25 10:05:07 +0100

    optimize resolveCallerNodePool

--- nomad/server_helpers.go
+++ nomad/server_helpers.go
@@ -26,22 +26,25 @@ func resolveCallerNodePool(s *Server, ctx *RPCContext, identity *structs.Authent
 		}
 	}
 
+	snap, err := s.fsm.State().Snapshot()
+	if err != nil {
+		// fail fast if we can't get the state; this means other lookups would
+		// fail anyway
+		return structs.NodePoolDefault
+	}
+
 	// 2) If RPC context has a NodeID (connected client), prefer that.
 	if ctx != nil && ctx.NodeID != "" && s != nil {
-		if snap, err := s.fsm.State().Snapshot(); err == nil {
-			if node, err := snap.NodeByID(nil, ctx.NodeID); err == nil && node != nil && node.NodePool != "" {
-				return node.NodePool
-			}
+		if node, err := snap.NodeByID(nil, ctx.NodeID); err == nil && node != nil && node.NodePool != "" {
+			return node.NodePool
 		}
 	}
 
 	// 3) If the authenticated identity has a ClientID (node secret), look up
 	// node.
 	if identity != nil && identity.ClientID != "" && s != nil {
-		if snap, err := s.fsm.State().Snapshot(); err == nil {
-			if node, err := snap.NodeByID(nil, identity.ClientID); err == nil && node != nil && node.NodePool != "" {
-				return node.NodePool
-			}
+		if node, err := snap.NodeByID(nil, identity.ClientID); err == nil && node != nil && node.NodePool != "" {
+			return node.NodePool
 		}
 	}
 
