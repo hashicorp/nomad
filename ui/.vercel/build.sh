0356e7ccb79192930be41b4058306af9c915e446
Michael Smithhisler <michael.smithhisler@hashicorp.com>
2026-03-31 09:59:09 -0400

    testing some more things

--- ui/.vercel/build.sh
+++ ui/.vercel/build.sh
@@ -1,6 +1,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: BUSL-1.1
 
-pnpm exec ember build
+echo $PWD
+cd .. && pnpm exec ember build && cd .vercel
 mkdir -p ui-dist/ui
 mv dist/* ui-dist/ui/
