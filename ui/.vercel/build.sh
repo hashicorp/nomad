a0ab9c28370a76d5c81578e7c792d23e468efb38
James Rasell <jrasell@ibm.com>
2026-04-08 11:27:05 +0100

    ui: Add sleep to Vercel build to fix current CI problems.

--- ui/.vercel/build.sh
+++ ui/.vercel/build.sh
@@ -1,6 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: BUSL-1.1
 
+# Vercel seemingly has a bootstrap timing issue which causes the build to fail
+# when we do not have this delay.
+sleep 2s
+
 ember build
 mkdir -p ui-dist/ui
 mv dist/* ui-dist/ui/
