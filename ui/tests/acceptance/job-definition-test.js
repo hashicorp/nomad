1bb5a157025eca0b59a9694b7984f32b01c93f34
Arun Stanislavose <arun.stanislavose@hashicorp.com>
2026-04-17 00:18:03 +0530

    Update test to handle change in logic to load allocations and relationships of the job explicitly

--- ui/tests/acceptance/job-definition-test.js
+++ ui/tests/acceptance/job-definition-test.js
@@ -12,7 +12,6 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 import a11yAudit from 'nomad-ui/tests/helpers/a11y-audit';
 import setupCodeMirror from 'nomad-ui/tests/helpers/codemirror';
 import Definition from 'nomad-ui/tests/pages/jobs/job/definition';
-import { JOB_JSON } from 'nomad-ui/tests/utils/generate-raw-json-job';
 
 let job;
 
@@ -154,7 +153,7 @@ module('Acceptance | job definition | full specification', function (hooks) {
   });
 
   test('it allows users to select between full specification and JSON definition', async function (assert) {
-    assert.expect(3);
+    assert.expect(5);
     const specification_response = {
       Format: 'hcl2',
       JobID: 'example',
@@ -166,7 +165,7 @@ module('Acceptance | job definition | full specification', function (hooks) {
       Variables: '',
       Version: 0,
     };
-    server.get('/job/:id', () => JOB_JSON);
+
     server.get('/job/:id/submission', () => specification_response);
 
     await Definition.visit({ id: job.id });
@@ -184,6 +183,20 @@ module('Acceptance | job definition | full specification', function (hooks) {
 
     await click('[data-test-select-full]');
     codeMirror = getCodeMirrorInstance('[data-test-editor]');
-    assert.propContains(JSON.parse(codeMirror.getValue()), JOB_JSON);
+    const fullDefinition = JSON.parse(codeMirror.getValue());
+    assert.equal(
+      fullDefinition.ID,
+      job.id,
+      'Full definition shows the correct job ID'
+    );
+    assert.equal(
+      fullDefinition.Name,
+      job.name,
+      'Full definition shows the correct job name'
+    );
+    assert.ok(
+      fullDefinition.TaskGroups,
+      'Full definition includes task groups'
+    );
   });
 });
