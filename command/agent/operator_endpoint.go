dc61f5e1e9ddeb18390c71a8e8d8e231b077ead6
Luke Palmer <luke@lukepalmer.net>
2026-04-28 14:53:08 +0000

    backport of commit 2907fd889a13960ed85475b80412b387f40ac1a0

--- command/agent/operator_endpoint.go
+++ command/agent/operator_endpoint.go
@@ -311,6 +311,7 @@ func (s *HTTPServer) schedulerUpdateConfig(resp http.ResponseWriter, req *http.R
 		MemoryOversubscriptionEnabled: conf.MemoryOversubscriptionEnabled,
 		RejectJobRegistration:         conf.RejectJobRegistration,
 		PauseEvalBroker:               conf.PauseEvalBroker,
+		NodeLimitForFeasibilityChecks: conf.NodeLimitForFeasibilityChecks,
 		PreemptionConfig: structs.PreemptionConfig{
 			SystemSchedulerEnabled:   conf.PreemptionConfig.SystemSchedulerEnabled,
 			SysBatchSchedulerEnabled: conf.PreemptionConfig.SysBatchSchedulerEnabled,
