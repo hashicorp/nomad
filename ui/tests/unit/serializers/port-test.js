00de0466807346d448c6fbbba6dde43bebb4fd2f
hc-github-team-nomad-core <82989552+hc-github-team-nomad-core@users.noreply.github.com>
2026-05-22 03:03:04 -0400

    Backport of UI: fix client detail page failing to render into release/2.0.x (#28012)
    * no-op commit due to failed cherry-picking
    
    * UI: fix client detail page failing to render (#27958)
    
    * fix(ui): NMD-1454
    
    * add guard for emoty IP
    
    * add regrations test for missing IP
    
    * fix(ui): correct configuration for `polyfillUUID`
    
    ---------
    
    Co-authored-by: temp <temp@hashicorp.com>
    Co-authored-by: Alexey Kulakov <alexey.kulakov@hashicorp.com>

---
+++ ui/tests/unit/serializers/port-test.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright IBM Corp. 2015, 2025
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import PortModel from 'nomad-ui/models/port';
+
+module('Unit | Serializer | Port', function (hooks) {
+  setupTest(hooks);
+  hooks.beforeEach(function () {
+    this.store = this.owner.lookup('service:store');
+    this.subject = () => this.store.serializerFor('port');
+  });
+
+  test('v4 HostIPs are passed through', async function (assert) {
+    const ip = '10.0.13.12';
+    const original = { HostIP: ip };
+
+    const { data } = this.subject().normalize(PortModel, original);
+    assert.deepEqual(data.attributes.hostIp, ip);
+  });
+
+  test('v6 HostIPs are wrapped in square brackets', async function (assert) {
+    const ip = '2001:0dac:aba3:0000:0000:8a2e:0370:7334';
+    const original = { HostIP: ip };
+
+    const { data } = this.subject().normalize(PortModel, original);
+    assert.deepEqual(data.attributes.hostIp, `[${ip}]`);
+  });
+
+  test('missing HostIP does not throw', async function (assert) {
+    const original = { Label: 'http', Value: 80 };
+
+    assert.deepEqual(
+      this.subject().normalize(PortModel, original).data.attributes.hostIp,
+      undefined,
+    );
+  });
+});
