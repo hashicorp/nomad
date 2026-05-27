e7b9d254e5aa83839778538de3833b7c4a996e5f
Tim Gross <tgross@hashicorp.com>
2026-05-27 14:32:11 -0400

    copyright: update remaining headers to 2026 (2.0.x)
    Manual backport of https://github.com/hashicorp/nomad/pull/28060
    
    The centrally-owned `copywrite` automation is about to become more disruptive by
    pushing machine-generated commits directly onto PRs. Let's work around this by
    updating copyright headers to match the current year. By the logic of the
    `copywrite` bot, these files will have their most recent git commit date in the
    current year, and therefore they meet the compliance requirement.
    
    Arguably this doesn't make a lot of sense, but there are already dozens of files
    in the repository that had their copyright header dates revised because their
    copyright headers were revised at some point (mostly due to the BUSL
    re-license), so we'll be following the same logic there. This will avoid us
    having to mess with any headers again until 2027, when we can do it again.

--- drivers/docker/cmd/main.go
+++ drivers/docker/cmd/main.go
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2015, 2025
+// Copyright IBM Corp. 2015, 2026
 // SPDX-License-Identifier: BUSL-1.1
 
 // This package provides a mechanism to build the Docker driver plugin as an
--- e2e/example/input/env.hcl
+++ e2e/example/input/env.hcl
@@ -1,4 +1,4 @@
-# Copyright IBM Corp. 2015, 2025
+# Copyright IBM Corp. 2015, 2026
 # SPDX-License-Identifier: BUSL-1.1
 
 # This "env" job simply invokes 'env' using raw_exec.
