e0e0f6f9f21d26d282d348fa621867b0050e2737
Tehut Getahun <tehut.m.g@gmail.com>
2026-06-11 09:16:41 -0700

    remove mock-nvidia customization from file plugin

--- plugins/device/cmd/example/cmd/main.go
+++ plugins/device/cmd/example/cmd/main.go
@@ -4,8 +4,6 @@
 package main
 
 import (
-	"context"
-
 	log "github.com/hashicorp/go-hclog"
 
 	"github.com/hashicorp/nomad/plugins"
@@ -18,6 +16,6 @@ func main() {
 }
 
 // factory returns a new instance of our example device plugin
-func factory(log log.Logger) any {
+func factory(log log.Logger) interface{} {
 	return example.NewExampleDevice(log)
 }
