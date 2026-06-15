331d9baaaf831d7a7303fe7b3089f2b0747fb2a9
Tehut Getahun <tehut.m.g@gmail.com>
2026-06-15 13:04:29 -0700

    revert plugsins/device/cmd to main, use example branch for local builds instead

--- plugins/device/cmd/example/device.go
+++ plugins/device/cmd/example/device.go
@@ -5,7 +5,6 @@ package example
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -13,82 +12,41 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hashicorp/go-hclog"
-	"github.com/hashicorp/nomad/helper/pluginutils/loader"
+	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/nomad/plugins/base"
 	"github.com/hashicorp/nomad/plugins/device"
 	"github.com/hashicorp/nomad/plugins/shared/hclspec"
 	"github.com/hashicorp/nomad/plugins/shared/structs"
-	"github.com/hashicorp/nomad/version"
+	"github.com/kr/pretty"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 const (
 	// pluginName is the name of the plugin
-	pluginName = "nvidia-example"
+	pluginName = "example-fs-device"
 
 	// vendor is the vendor providing the devices
-	vendor = "nvidia"
+	vendor = "nomad"
 
 	// deviceType is the type of device being returned
-	deviceType = device.DeviceTypeGPU
+	deviceType = "file"
 
-	// notAvailable value is returned to nomad server in case some properties were
-	// undetected by nvml driver
-	notAvailable = "N/A"
-
-	// Nvidia-container-runtime environment variable names
-	NvidiaVisibleDevices = "NVIDIA_VISIBLE_DEVICES"
-
-	// MPS runtime environment variables
-	MpsPipeDirectoryKey = "MPS_PIPE_DIRECTORY"
-	MpsLogDirectoryKey  = "MPS_LOG_DIRECTORY"
-	CustomMpsUserKey    = "MPS_USER"
-
-	DefaultMpsSockFileAddr = "control"
-
-	deviceName1 = "T4"
-	deviceName2 = "T4"
+	// deviceName is the name of the devices being exposed
+	deviceName = "mock"
 )
 
 var (
-	// PluginID is the nvidia plugin metadata registered in the plugin
-	// catalog.
-	PluginID = loader.PluginID{
-		Name:       pluginName,
-		PluginType: base.PluginTypeDevice,
-	}
-
-	// PluginConfig is the nvidia factory function registered in the
-	// plugin catalog.
-	PluginConfig = &loader.InternalPluginConfig{
-		Factory: func(ctx context.Context, l hclog.Logger) interface{} { return NewNvidiaDevice(ctx, l) },
-	}
-
 	// pluginInfo describes the plugin
 	pluginInfo = &base.PluginInfoResponse{
 		Type:              base.PluginTypeDevice,
 		PluginApiVersions: []string{device.ApiVersion010},
-		PluginVersion:     version.Version,
+		PluginVersion:     "v0.1.0",
 		Name:              pluginName,
 	}
 
 	// configSpec is the specification of the plugin's configuration
 	configSpec = hclspec.NewObject(map[string]*hclspec.Spec{
-		"enabled": hclspec.NewDefault(
-			hclspec.NewAttr("enabled", "bool", false),
-			hclspec.NewLiteral("true"),
-		),
-
-		"ignored_gpu_ids": hclspec.NewDefault(
-			hclspec.NewAttr("ignored_gpu_ids", "list(string)", false),
-			hclspec.NewLiteral("[]"),
-		),
-		"fingerprint_period": hclspec.NewDefault(
-			hclspec.NewAttr("fingerprint_period", "string", false),
-			hclspec.NewLiteral("\"1m\""),
-		),
 		"dir": hclspec.NewDefault(
 			hclspec.NewAttr("dir", "string", false),
 			hclspec.NewLiteral("\".\""),
@@ -101,62 +59,22 @@ var (
 			hclspec.NewAttr("unhealthy_perm", "string", false),
 			hclspec.NewLiteral("\"-rwxrwxrwx\""),
 		),
-		"mps": hclspec.NewBlock("mps", false,
-			hclspec.NewObject(map[string]*hclspec.Spec{
-				"enabled":            hclspec.NewAttr("enabled", "bool", true),
-				"mps_user":           hclspec.NewAttr("mps_user", "string", false),
-				"mps_pipe_directory": hclspec.NewAttr("mps_pipe_directory", "string", false),
-				"mps_log_directory":  hclspec.NewAttr("mps_log_directory", "string", false),
-				"mps_sock_addr":      hclspec.NewAttr("mps_sock_addr", "string", false),
-				"device_specific_mps_config": hclspec.NewBlockList("device_specific_mps_config",
-					hclspec.NewObject(map[string]*hclspec.Spec{
-						"uuid":               hclspec.NewAttr("uuid", "string", true),
-						"mps_pipe_directory": hclspec.NewAttr("mps_pipe_directory", "string", true),
-						"mps_log_directory":  hclspec.NewAttr("mps_log_directory", "string", true),
-					}),
-				),
-			}),
-		),
 	})
 )
 
 // Config contains configuration information for the plugin.
 type Config struct {
-	Enabled           bool       `codec:"enabled"`
-	IgnoredGPUIDs     []string   `codec:"ignored_gpu_ids"`
-	FingerprintPeriod string     `codec:"fingerprint_period"`
-	MpsConfig         *MpsConfig `codec:"mps"`
-	Dir               string     `codec:"dir"`
-	ListPeriod        string     `codec:"list_period"`
-	UnhealthyPerm     string     `codec:"unhealthy_perm"`
+	Dir           string `codec:"dir"`
+	ListPeriod    string `codec:"list_period"`
+	UnhealthyPerm string `codec:"unhealthy_perm"`
 }
 
-// MpsConfig contains configuration for mps sharing
-type MpsConfig struct {
-	MpsUser          string            `codec:"mps_user"`
-	MpsSockFile      string            `codec:"mps_sock_addr"`
-	MpsPipeDirectory string            `codec:"mps_pipe_directory"`
-	MpsLogDirectory  string            `codec:"mps_log_directory"`
-	DeviceConfig     []DeviceMpsConfig `codec:"device_specific_mps_config"`
-	DeviceMpsConfig  map[string]DeviceMpsConfig
-}
-
-// DeviceMpsConfig contains configuration GPU level mps sharing
-type DeviceMpsConfig struct {
-	UUID             string `codec:"uuid"`
-	MpsPipeDirectory string `codec:"mps_pipe_directory"`
-	MpsLogDirectory  string `codec:"mps_log_directory"`
-}
-
-type NvidiaDevice struct {
-	// enabled indicates whether the plugin should be enabled
-	enabled bool
-
-	//nvmlClient nvml.NvmlClient
-
-	// initErr holds an error retrieved during
-	// nvmlClient initialization
-	//initErr error
+// FsDevice is an example device plugin. The device plugin exposes files as
+// devices and periodically polls the directory for new files. If a file has a
+// given file permission, it is considered unhealthy. This device plugin is
+// purely for use as an example.
+type FsDevice struct {
+	logger log.Logger
 
 	// deviceDir is the directory we expose as devices
 	deviceDir string
@@ -168,104 +86,37 @@ type NvidiaDevice struct {
 	// devices
 	listPeriod time.Duration
 
-	// ignoredGPUIDs is a set of UUIDs that would not be exposed to nomad
-	ignoredGPUIDs map[string]struct{}
-
-	// fingerprintPeriod is how often we should call nvml to get list of devices
-	//fingerprintPeriod time.Duration
-
-	//MpsConfig holds a pointer to the MPS configuration
-	MpsConfig *MpsConfig
-
-	// devices is the set of detected eligible devices
-	devices    map[string]device.Shared
+	// devices is the set of detected devices and maps whether they are healthy
+	devices    map[string]bool
 	deviceLock sync.RWMutex
-
-	logger hclog.Logger
 }
 
-// NewNvidiaDevice returns a new nvidia device plugin.
-func NewNvidiaDevice(_ context.Context, log hclog.Logger) *NvidiaDevice {
-	//nvmlClient, err := nvml.NewNvmlClient()
-	logger := log.Named(pluginName)
-	return &NvidiaDevice{
-		logger:        logger,
-		devices:       make(map[string]device.Shared),
-		ignoredGPUIDs: make(map[string]struct{}),
+// NewExampleDevice returns a new example device plugin.
+func NewExampleDevice(log log.Logger) *FsDevice {
+	return &FsDevice{
+		logger:  log.Named(pluginName),
+		devices: make(map[string]bool),
 	}
 }
 
 // PluginInfo returns information describing the plugin.
-func (d *NvidiaDevice) PluginInfo() (*base.PluginInfoResponse, error) {
+func (d *FsDevice) PluginInfo() (*base.PluginInfoResponse, error) {
 	return pluginInfo, nil
 }
 
 // ConfigSchema returns the plugins configuration schema.
-func (d *NvidiaDevice) ConfigSchema() (*hclspec.Spec, error) {
+func (d *FsDevice) ConfigSchema() (*hclspec.Spec, error) {
 	return configSpec, nil
 }
 
-func checkAndSetDefault(c string, d string) string {
-	if config := c; config != "" {
-		return c
-	}
-	return d
-}
-
 // SetConfig is used to set the configuration of the plugin.
-func (d *NvidiaDevice) SetConfig(cfg *base.Config) error {
+func (d *FsDevice) SetConfig(c *base.Config) error {
 	var config Config
-	if len(cfg.PluginConfig) != 0 {
-		if err := base.MsgPackDecode(cfg.PluginConfig, &config); err != nil {
-			return err
-		}
-	}
-	d.enabled = config.Enabled
-	// set MPS config values
-	if config.MpsConfig != nil {
-		d.MpsConfig = &MpsConfig{}
-		d.logger.Info("not nil", "config", fmt.Sprintf("%+v", config.MpsConfig))
-		// ensure only global or device specific config are set
-		if (config.MpsConfig.MpsPipeDirectory != "" || config.MpsConfig.MpsLogDirectory != "") &&
-			len(config.MpsConfig.DeviceMpsConfig) != 0 {
-			return errors.New("only global mps variables or device_specific_mps_config block may be set ")
-		}
-		// set straightforward value on device
-		d.MpsConfig.MpsUser = checkAndSetDefault(config.MpsConfig.MpsUser, "unset")
-		d.MpsConfig.MpsSockFile = checkAndSetDefault(config.MpsConfig.MpsSockFile, DefaultMpsSockFileAddr)
-
-		// if present set device specific mps config, otherwise set top level config
-		if len(config.MpsConfig.DeviceMpsConfig) != 0 {
-
-			// build map of device UUIDs to config
-			deviceConfigMap := make(map[string]DeviceMpsConfig, len(config.MpsConfig.DeviceMpsConfig))
-			for _, devConfig := range config.MpsConfig.DeviceMpsConfig {
-				deviceConfigMap[devConfig.UUID] = DeviceMpsConfig{
-					UUID:             devConfig.UUID,
-					MpsPipeDirectory: devConfig.MpsPipeDirectory,
-					MpsLogDirectory:  devConfig.MpsLogDirectory,
-				}
-			}
-			// set device specific mpsConfig
-			d.MpsConfig.DeviceMpsConfig = deviceConfigMap
-		} else {
-			// set top level mps directories if no device specific config
-			// we have defaults so always use config values
-			d.MpsConfig.MpsPipeDirectory = config.MpsConfig.MpsPipeDirectory
-			d.MpsConfig.MpsLogDirectory = config.MpsConfig.MpsLogDirectory
-			if pipe_dir := config.MpsConfig.MpsPipeDirectory; pipe_dir != "" {
-				d.MpsConfig.MpsPipeDirectory = pipe_dir
-			} else {
-				d.MpsConfig.MpsPipeDirectory = checkAndSetDefault(config.MpsConfig.MpsPipeDirectory, "/tmp/nvidia-mps")
-
-				d.MpsConfig.MpsLogDirectory = checkAndSetDefault(config.MpsConfig.MpsLogDirectory, "/var/log/nvidia-mps")
-			}
-		}
-	}
-	for _, ignoredGPUId := range config.IgnoredGPUIDs {
-		d.ignoredGPUIDs[ignoredGPUId] = struct{}{}
+	if err := base.MsgPackDecode(c.PluginConfig, &config); err != nil {
+		return err
 	}
 
+	// Save the device directory and the unhealthy permissions
 	d.deviceDir = config.Dir
 	d.unhealthyPerm = config.UnhealthyPerm
 
@@ -276,14 +127,16 @@ func (d *NvidiaDevice) SetConfig(cfg *base.Config) error {
 	}
 	d.listPeriod = period
 
+	d.logger.Debug("test debug")
+	d.logger.Info("config set", "config", log.Fmt("% #v", pretty.Formatter(config)))
 	return nil
 }
 
 // Fingerprint streams detected devices. If device changes are detected or the
 // devices health changes, messages will be emitted.
-func (d *NvidiaDevice) Fingerprint(ctx context.Context) (<-chan *device.FingerprintResponse, error) {
-	if !d.enabled {
-		return nil, device.ErrPluginDisabled
+func (d *FsDevice) Fingerprint(ctx context.Context) (<-chan *device.FingerprintResponse, error) {
+	if d.deviceDir == "" {
+		return nil, status.New(codes.Internal, "device directory not set in config").Err()
 	}
 
 	outCh := make(chan *device.FingerprintResponse)
@@ -292,7 +145,7 @@ func (d *NvidiaDevice) Fingerprint(ctx context.Context) (<-chan *device.Fingerpr
 }
 
 // fingerprint is the long running goroutine that detects hardware
-func (d *NvidiaDevice) fingerprint(ctx context.Context, devices chan *device.FingerprintResponse) {
+func (d *FsDevice) fingerprint(ctx context.Context, devices chan *device.FingerprintResponse) {
 	defer close(devices)
 
 	// Create a timer that will fire immediately for the first detection
@@ -306,7 +159,7 @@ func (d *NvidiaDevice) fingerprint(ctx context.Context, devices chan *device.Fin
 			ticker.Reset(d.listPeriod)
 		}
 
-		d.logger.Info("scanning for changes")
+		d.logger.Trace("scanning for changes")
 
 		files, err := ioutil.ReadDir(d.deviceDir)
 		if err != nil {
@@ -314,122 +167,97 @@ func (d *NvidiaDevice) fingerprint(ctx context.Context, devices chan *device.Fin
 			devices <- device.NewFingerprintError(err)
 			return
 		}
-		deviceGroups := make([]*device.DeviceGroup, 0)
-		shared, inactive := d.diffFiles(files)
-		if len(inactive) != 0 {
-			deviceGroups = append(deviceGroups, d.getDeviceGroup(inactive, deviceName2))
+
+		detected := d.diffFiles(files)
+		if len(detected) == 0 {
+			continue
 		}
 
-		if len(shared) != 0 {
-			deviceGroups = append(deviceGroups, d.getDeviceGroup(shared, deviceName1))
-		}
-		d.logger.Info("files to fingerprint", "inactive files", len(inactive), "active files", len(shared))
-		devices <- device.NewFingerprint(deviceGroups...)
+		devices <- device.NewFingerprint(getDeviceGroup(detected))
 
 	}
 }
-func (d *NvidiaDevice) diffFiles(files []os.FileInfo) ([]*device.Device, []*device.Device) {
+
+func (d *FsDevice) diffFiles(files []os.FileInfo) []*device.Device {
 	d.deviceLock.Lock()
 	defer d.deviceLock.Unlock()
 
 	// Build an unhealthy message
 	unhealthyDesc := fmt.Sprintf("Device has bad permissions %q", d.unhealthyPerm)
 
-	//var changes bool
+	var changes bool
 	fnames := make(map[string]struct{})
 	for _, f := range files {
 		name := f.Name()
 		fnames[name] = struct{}{}
 		if f.IsDir() {
-			d.logger.Info("skipping directory", "directory", name)
+			d.logger.Trace("skipping directory", "directory", name)
 			continue
 		}
 
 		// Determine the health
 		perms := f.Mode().Perm().String()
-		//turn health into sharing status
-		healthBool := perms != d.unhealthyPerm
-		var healthy string
-		if healthBool {
-			healthy = device.SharingActive.String()
-		} else {
-			healthy = device.SharingInactive.String()
-		}
-		d.logger.Info("checking health", "file perm", perms, "unhealthy perms", d.unhealthyPerm, "healthy", healthy)
+		healthy := perms != d.unhealthyPerm
+		d.logger.Trace("checking health", "file perm", perms, "unhealthy perms", d.unhealthyPerm, "healthy", healthy)
 
-		// See if we already have the device
+		// See if we alreay have the device
 		oldHealth, ok := d.devices[name]
-		if ok && oldHealth.String() == healthy {
+		if ok && oldHealth == healthy {
 			continue
 		}
 
 		// Health has changed or we have a new object
-		//changes = true
-
-		d.devices[name] = device.Shared(healthy)
+		changes = true
+		d.devices[name] = healthy
 	}
 
 	for id := range d.devices {
 		if _, ok := fnames[id]; !ok {
 			delete(d.devices, id)
-			//changes = true
+			changes = true
 		}
+	}
+
+	// Nothing to do
+	if !changes {
+		return nil
 	}
 
 	// Build the devices
-	shared := make([]*device.Device, 0, len(d.devices))
-	inactive := make([]*device.Device, 0, len(d.devices))
-
+	detected := make([]*device.Device, 0, len(d.devices))
 	for name, healthy := range d.devices {
 		var desc string
-		if healthy != device.SharingActive {
+		if !healthy {
 			desc = unhealthyDesc
-			inactive = append(inactive, &device.Device{
-				ID:         name,
-				Shared:     healthy,
-				HealthDesc: desc,
-				Healthy:    true,
-			})
-			continue
 		}
-		shared = append(shared, &device.Device{
+
+		detected = append(detected, &device.Device{
 			ID:         name,
-			Shared:     healthy,
-			HealthDesc: "healthy",
-			Healthy:    true,
+			Healthy:    healthy,
+			HealthDesc: desc,
 		})
 	}
 
-	return shared, inactive
+	return detected
 }
 
 // getDeviceGroup is a helper to build the DeviceGroup given a set of devices.
-func (d *NvidiaDevice) getDeviceGroup(devices []*device.Device, name string) *device.DeviceGroup {
-	//d.logger.Error("getDeviceGroup", "device count", len(devices))
-	var shared string
-	for _, v := range devices {
-		if shared == "" {
-			shared = v.Shared.String()
-		}
-		//d.logger.Error("getDeviceGroup", "loop", n, "deviceID", v.ID, "shared", v.Shared.String())
-	}
+func getDeviceGroup(devices []*device.Device) *device.DeviceGroup {
 	return &device.DeviceGroup{
 		Vendor:  vendor,
 		Type:    deviceType,
-		Name:    name,
+		Name:    deviceName,
 		Devices: devices,
 		Attributes: map[string]*structs.Attribute{
-
 			"cool-attribute": {
 				String: new("attribute-wearing-sunglasses"),
 			},
 		},
 	}
-
 }
 
 // Reserve returns information on how to mount the given devices.
-func (d *NvidiaDevice) Reserve(deviceIDs []string) (*device.ContainerReservation, error) {
+func (d *FsDevice) Reserve(deviceIDs []string) (*device.ContainerReservation, error) {
 	if len(deviceIDs) == 0 {
 		return nil, status.New(codes.InvalidArgument, "no device ids given").Err()
 	}
@@ -440,15 +268,11 @@ func (d *NvidiaDevice) Reserve(deviceIDs []string) (*device.ContainerReservation
 	}
 
 	resp := &device.ContainerReservation{}
-	containerEnvs := make(map[string]string)
+
 	for _, id := range deviceIDs {
 		// Check if the device is known
 		if _, ok := d.devices[id]; !ok {
 			return nil, status.Newf(codes.InvalidArgument, "unknown device %q", id).Err()
-		}
-		if d.devices[id] == device.SharingActive {
-			containerEnvs[MpsPipeDirectoryKey] = d.MpsConfig.MpsPipeDirectory
-			containerEnvs[MpsLogDirectoryKey] = d.MpsConfig.MpsLogDirectory
 		}
 
 		// Add a mount
@@ -463,14 +287,14 @@ func (d *NvidiaDevice) Reserve(deviceIDs []string) (*device.ContainerReservation
 }
 
 // Stats streams statistics for the detected devices.
-func (d *NvidiaDevice) Stats(ctx context.Context, interval time.Duration) (<-chan *device.StatsResponse, error) {
+func (d *FsDevice) Stats(ctx context.Context, interval time.Duration) (<-chan *device.StatsResponse, error) {
 	outCh := make(chan *device.StatsResponse)
 	go d.stats(ctx, outCh, interval)
 	return outCh, nil
 }
 
 // stats is the long running goroutine that streams device statistics
-func (d *NvidiaDevice) stats(ctx context.Context, stats chan *device.StatsResponse, interval time.Duration) {
+func (d *FsDevice) stats(ctx context.Context, stats chan *device.StatsResponse, interval time.Duration) {
 	defer close(stats)
 
 	// Create a timer that will fire immediately for the first detection
@@ -501,7 +325,7 @@ func (d *NvidiaDevice) stats(ctx context.Context, stats chan *device.StatsRespon
 	}
 }
 
-func (d *NvidiaDevice) collectStats() (*device.DeviceGroupStats, error) {
+func (d *FsDevice) collectStats() (*device.DeviceGroupStats, error) {
 	d.deviceLock.RLock()
 	defer d.deviceLock.RUnlock()
 	l := len(d.devices)
@@ -513,7 +337,7 @@ func (d *NvidiaDevice) collectStats() (*device.DeviceGroupStats, error) {
 	group := &device.DeviceGroupStats{
 		Vendor:        vendor,
 		Type:          deviceType,
-		Name:          deviceName1,
+		Name:          deviceName,
 		InstanceStats: make(map[string]*device.DeviceStats, l),
 	}
 
