654194490968daac964750e9a2c024c5e34c640f
Michael Smithhisler <michael.smithhisler@hashicorp.com>
2026-06-16 16:51:15 -0400

    ui: fix error getting task logs when refreshing page

--- ui/app/routes/allocations/allocation/task/logs.js
+++ ui/app/routes/allocations/allocation/task/logs.js
@@ -4,9 +4,20 @@
  */
 
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class LogsRoute extends Route {
+  @service abilities;
   model() {
-    return this.modelFor('allocations.allocation.task');
+    const task = this.modelFor('allocations.allocation.task')
+    // streaming logs can require the node object to be loaded
+    // in order for the logUrl() function to correctly use the
+    // allocation.node.httpAddr property.
+    // An alternative option here is to use just allocation.node
+    // as the computed property for logUrl()
+    if (this.abilities.can('read client')){
+      return task.get('allocation.node').then(() => task)
+    }
+    return task
   }
 }
