c314fda0ddc18c9f0cf656089214d8588932fe43
Tehut Getahun <tehut.m.g@gmail.com>
2026-09-16 13:20:39 -0700

    remove e2e test setup in favor of #28440

--- e2e/devices/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright IBM Corp. 2015, 2025
-// SPDX-License-Identifier: BUSL-1.1
-
-// Package devices provides end-to-end tests for Nomad's device scheduling
-// functionality, including the first_available feature for flexible device
-// selection.
-package devices
